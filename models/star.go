ada59e4dc81a41642295e43ffba8825183b839b2
Andrey Nering <andrey.nering@gmail.com>
2016-11-19 14:13:36 -0200

    Make golint happy and use transactions on StarRepo function

--- models/star.go
+++ models/star.go
@@ -1,39 +1,59 @@
 package models
 
-import (
-	"code.gitea.io/gitea/modules/setting"
-)
-
+// Star represents a starred repo by an user.
 type Star struct {
 	ID     int64 `xorm:"pk autoincr"`
 	UID    int64 `xorm:"UNIQUE(s)"`
 	RepoID int64 `xorm:"UNIQUE(s)"`
 }
 
-// Star or unstar repository.
-func StarRepo(userID, repoID int64, star bool) (err error) {
+// StarRepo or unstar repository.
+func StarRepo(userID, repoID int64, star bool) error {
+	sess := x.NewSession()
+
+	if err := sess.Begin(); err != nil {
+		return err
+	}
+
 	if star {
 		if IsStaring(userID, repoID) {
+			sess.Rollback()
 			return nil
 		}
-		if _, err = x.Insert(&Star{UID: userID, RepoID: repoID}); err != nil {
-			return err
-		} else if _, err = x.Exec("UPDATE `repository` SET num_stars = num_stars + 1 WHERE id = ?", repoID); err != nil {
+
+		if _, err := sess.Insert(&Star{UID: userID, RepoID: repoID}); err != nil {
+			sess.Rollback()
 			return err
 		}
-		_, err = x.Exec("UPDATE `user` SET num_stars = num_stars + 1 WHERE id = ?", userID)
+		if _, err := x.Exec("UPDATE `repository` SET num_stars = num_stars + 1 WHERE id = ?", repoID); err != nil {
+			sess.Rollback()
+			return err
+		}
+		if _, err := x.Exec("UPDATE `user` SET num_stars = num_stars + 1 WHERE id = ?", userID); err != nil {
+			sess.Rollback()
+			return err
+		}
 	} else {
 		if !IsStaring(userID, repoID) {
+			sess.Rollback()
 			return nil
 		}
-		if _, err = x.Delete(&Star{0, userID, repoID}); err != nil {
-			return err
-		} else if _, err = x.Exec("UPDATE `repository` SET num_stars = num_stars - 1 WHERE id = ?", repoID); err != nil {
+
+		if _, err := x.Delete(&Star{0, userID, repoID}); err != nil {
+			sess.Rollback()
 			return err
 		}
-		_, err = x.Exec("UPDATE `user` SET num_stars = num_stars - 1 WHERE id = ?", userID)
+		if _, err := x.Exec("UPDATE `repository` SET num_stars = num_stars - 1 WHERE id = ?", repoID); err != nil {
+			sess.Rollback()
+			return err
+		}
+		if _, err := x.Exec("UPDATE `user` SET num_stars = num_stars - 1 WHERE id = ?", userID); err != nil {
+			sess.Rollback()
+			return err
+		}
 	}
-	return err
+
+	return sess.Commit()
 }
 
 // IsStaring checks if user has starred given repository.
@@ -42,19 +62,18 @@ func IsStaring(userID, repoID int64) bool {
 	return has
 }
 
+// GetStargazers returns the users that starred the repo.
 func (repo *Repository) GetStargazers(page int) ([]*User, error) {
 	users := make([]*User, 0, ItemsPerPage)
-	sess := x.
+	err := x.
 		Limit(ItemsPerPage, (page-1)*ItemsPerPage).
-		Where("star.repo_id = ?", repo.ID)
-	if setting.UsePostgreSQL {
-		sess = sess.Join("LEFT", "star", `"user".id = star.uid`)
-	} else {
-		sess = sess.Join("LEFT", "star", "user.id = star.uid")
-	}
-	return users, sess.Find(&users)
+		Where("star.repo_id = ?", repo.ID).
+		Join("LEFT", "star", "`user`.id = star.uid").
+		Find(&users)
+	return users, err
 }
 
+// GetStarredRepos returns the repos the user starred.
 func (u *User) GetStarredRepos(private bool) (repos []*Repository, err error) {
 	sess := x.
 		Join("INNER", "star", "star.repo_id = repository.id").
