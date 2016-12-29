a2326883084c7c1ed300cfd564752d9077070975
Avelino <thiago@avelino.xxx>
2016-12-29 11:08:21 -0200

    Recommendation made by @tboerger https://github.com/go-gitea/gitea/pull/336/commits/66a1c59fe730eff019ce100673c6800cce7d102d#r94122732

--- modules/setting/setting.go
+++ modules/setting/setting.go
@@ -821,7 +821,7 @@ please consider changing to GITEA_CUSTOM`)
 	ShowFooterVersion = Cfg.Section("other").Key("SHOW_FOOTER_VERSION").MustBool(true)
 	ShowFooterTemplateLoadTime = Cfg.Section("other").Key("SHOW_FOOTER_TEMPLATE_LOAD_TIME").MustBool(true)
 
-	UI.ShowUserEmailInExplore = Cfg.Section("ui").Key("SHOW_USER_EMAIL_IN_EXPLORE").MustBool()
+	UI.ShowUserEmailInExplore = Cfg.Section("ui").Key("SHOW_USER_EMAIL").MustBool()
 
 	HasRobotsTxt = com.IsFile(path.Join(CustomPath, "robots.txt"))
 }
