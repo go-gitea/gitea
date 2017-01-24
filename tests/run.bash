6ab462250812675166c880acd157c9db59b37abb
Mura Li <mura_li@castech.com.tw>
2017-01-24 14:36:30 +0800

    Enhance run.sh

--- tests/run.bash
+++ tests/run.bash
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-go test -ldflags "-X code.gitea.io/gitea/tests.Version=$(git describe --tags --always | sed 's/-/+/' | sed 's/^v//')"
+go test -ldflags "-X code.gitea.io/gitea/tests.Version=$(git describe --tags --always | sed 's/-/+/' | sed 's/^v//')" $@
