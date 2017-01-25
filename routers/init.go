go-gitea/gitea

commit 833f8b94c2cd88277eba32984594aad2b7b2b05d

Search bar for issues/pulls (#530)

routers/init.go
@@ -18,6 +18,7 @@ import (
 	"code.gitea.io/gitea/modules/setting"
 	"code.gitea.io/gitea/modules/ssh"
 	macaron "gopkg.in/macaron.v1"
+	"code.gitea.io/gitea/modules/indexer"
 )
 
 func checkRunMode() {
@@ -36,6 +37,7 @@ func checkRunMode() {
 func NewServices() {
 	setting.NewServices()
 	mailer.NewContext()
+	indexer.NewContext()
 }
 
 // GlobalInit is for global configuration reload-able.

commit 6214c79a9619035830dd72c50fdabf260720106c

bug fixed caused by #530

routers/init.go
@@ -37,7 +37,6 @@ func checkRunMode() {
 func NewServices() {
 	setting.NewServices()
 	mailer.NewContext()
-	indexer.NewContext()
 }
 
 // GlobalInit is for global configuration reload-able.
@@ -61,6 +60,7 @@ func GlobalInit() {
 
 		// Booting long running goroutines.
 		cron.NewContext()
+		indexer.NewContext()
 		models.InitSyncMirrors()
 		models.InitDeliverHooks()
 		models.InitTestPullRequests()