d7d094bd8a5520b758f34d3fb0439c6a1ddb39d3
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-02-02 20:33:36 +0800

    fix: ignore email notifications if user is not active. (#820)

---
+++ models/user_test.go
@@ -0,0 +1,19 @@
+// Copyright 2017 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package models
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetUserEmailsByNames(t *testing.T) {
+	assert.NoError(t, PrepareTestDatabase())
+
+	// ignore none active user email
+	assert.Equal(t, []string{"user8@example.com"}, GetUserEmailsByNames([]string{"user8", "user9"}))
+	assert.Equal(t, []string{"user8@example.com", "user5@example.com"}, GetUserEmailsByNames([]string{"user8", "user5"}))
+}
