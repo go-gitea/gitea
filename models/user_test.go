d67b278a0d05e122bf510ac52bfbd69eab4f654d
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-02-14 20:16:00 +0800

    feat: Able to disable non-admin to create new organization (#927)

--- models/user_test.go
+++ models/user_test.go
@@ -7,6 +7,8 @@ package models
 import (
 	"testing"
 
+	"code.gitea.io/gitea/modules/setting"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -16,4 +18,23 @@ func TestGetUserEmailsByNames(t *testing.T) {
 	// ignore none active user email
 	assert.Equal(t, []string{"user8@example.com"}, GetUserEmailsByNames([]string{"user8", "user9"}))
 	assert.Equal(t, []string{"user8@example.com", "user5@example.com"}, GetUserEmailsByNames([]string{"user8", "user5"}))
+}
+
+func TestCanCreateOrganization(t *testing.T) {
+	assert.NoError(t, PrepareTestDatabase())
+
+	admin := AssertExistsAndLoadBean(t, &User{ID: 1}).(*User)
+	assert.True(t, admin.CanCreateOrganization())
+
+	user := AssertExistsAndLoadBean(t, &User{ID: 2}).(*User)
+	assert.True(t, user.CanCreateOrganization())
+	// Disable user create organization permission.
+	user.AllowCreateOrganization = false
+	assert.False(t, user.CanCreateOrganization())
+
+	setting.Admin.DisableRegularOrgCreation = true
+	user.AllowCreateOrganization = true
+	assert.True(t, admin.CanCreateOrganization())
+	assert.False(t, user.CanCreateOrganization())
+
 }
