83a4697e0bcf8dcbae7a378ef82e9fb363c30d45
Mura Li <mura_li@castech.com.tw>
2017-02-23 09:42:11 +0800

    Fix comment which breaks the linter's rule

--- routers/api/v1/misc/version.go
+++ routers/api/v1/misc/version.go
@@ -10,7 +10,7 @@ import (
 	"code.gitea.io/sdk/gitea"
 )
 
-// ServerVersion shows the version of the Gitea server
+// Version shows the version of the Gitea server
 func Version(ctx *context.APIContext) {
 	ctx.JSON(200, &gitea.ServerVersion{Version: setting.AppVer})
 }
