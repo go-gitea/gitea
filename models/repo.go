bd0bf97328b482aa2bc2e2f89b53bbb16d8a803c
kanoulis <kanoulis@gmail.com>
2017-03-03 08:46:33 +0200

    fix ssh repo link
    Added missing RepoRootPath

--- models/repo.go
+++ models/repo.go
@@ -681,9 +681,9 @@ func (repo *Repository) cloneLink(isWiki bool) *CloneLink {
 	repo.Owner = repo.MustOwner()
 	cl := new(CloneLink)
 	if setting.SSH.Port != 22 {
-		cl.SSH = fmt.Sprintf("ssh://%s@%s:%d/%s/%s.git", setting.RunUser, setting.SSH.Domain, setting.SSH.Port, repo.Owner.Name, repoName)
+		cl.SSH = fmt.Sprintf("ssh://%s@%s:%d%s/%s/%s.git", setting.RunUser, setting.SSH.Domain, setting.SSH.Port, setting.RepoRootPath, repo.Owner.Name, repoName)
 	} else {
-		cl.SSH = fmt.Sprintf("%s@%s:%s/%s.git", setting.RunUser, setting.SSH.Domain, repo.Owner.Name, repoName)
+		cl.SSH = fmt.Sprintf("%s@%s:%s/%s/%s.git", setting.RunUser, setting.SSH.Domain, setting.RepoRootPath, repo.Owner.Name, repoName)
 	}
 	cl.HTTPS = ComposeHTTPSCloneURL(repo.Owner.Name, repoName)
 	return cl
