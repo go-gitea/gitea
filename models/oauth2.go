6aeea3f5591c4b72256291c23394c1b752e2524e
willemvd <willem@opencirclesolutions.nl>
2017-03-10 08:28:04 +0100

    small typo in comment

--- models/oauth2.go
+++ models/oauth2.go
@@ -81,7 +81,7 @@ func GetActiveOAuth2LoginSourceByName(name string) (*LoginSource, error) {
 // key is used as technical name (like in the callbackURL)
 // values to display
 func GetActiveOAuth2Providers() ([]string, map[string]OAuth2Provider, error) {
-	// Maybe also seperate used and unused providers so we can force the registration of only 1 active provider for each type
+	// Maybe also separate used and unused providers so we can force the registration of only 1 active provider for each type
 
 	loginSources, err := GetActiveOAuth2ProviderLoginSources()
 	if err != nil {
