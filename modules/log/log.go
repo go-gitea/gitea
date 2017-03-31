f20a68633a9ea60be9af6f205cbe3e92f5dac40d
Mura Li <mura_li@castech.com.tw>
2017-03-31 09:40:11 +0800

    Fix read-and-update race

--- modules/log/log.go
+++ modules/log/log.go
@@ -215,16 +215,12 @@ func (l *Logger) SetLogger(adapter string, config string) error {
 // DelLogger removes a logger adapter instance.
 func (l *Logger) DelLogger(adapter string) error {
 
-	l.lock.RLock()
-	lg, ok := l.outputs[adapter]
-	l.lock.RUnlock()
+	l.lock.Lock()
+	defer l.lock.Unlock()
 
-	if ok {
+	if lg, ok := l.outputs[adapter]; ok {
 		lg.Destroy()
-
-		l.lock.Lock()
 		delete(l.outputs, adapter)
-		l.lock.Unlock()
 	} else {
 		panic("log: unknown adapter \"" + adapter + "\" (forgotten register?)")
 	}
