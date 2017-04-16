cf91cfb993bed1a412590305f594db81dc83bca8
Antoine GIRARD <sapk@users.noreply.github.com>
2017-04-16 14:51:04 +0200

    Migrate WatchInfo struct to api (#1492)
    * Remove WatchInfo and link to api struct
    
    * Add vendor temporary update
    
    * Return to gitea vendor source

--- routers/api/v1/user/watch.go
+++ routers/api/v1/user/watch.go
@@ -5,24 +5,12 @@
 package user
 
 import (
-	"time"
-
 	api "code.gitea.io/sdk/gitea"
 
 	"code.gitea.io/gitea/models"
 	"code.gitea.io/gitea/modules/context"
 	"code.gitea.io/gitea/modules/setting"
 )
-
-// WatchInfo contains information about a watched repository
-type WatchInfo struct {
-	Subscribed    bool        `json:"subscribed"`
-	Ignored       bool        `json:"ignored"`
-	Reason        interface{} `json:"reason"`
-	CreatedAt     time.Time   `json:"created_at"`
-	URL           string      `json:"url"`
-	RepositoryURL string      `json:"repository_url"`
-}
 
 // getWatchedRepos returns the repos that the user with the specified userID is
 // watching
@@ -67,7 +55,7 @@ func GetMyWatchedRepos(ctx *context.APIContext) {
 // specified in ctx
 func IsWatching(ctx *context.APIContext) {
 	if models.IsWatching(ctx.User.ID, ctx.Repo.Repository.ID) {
-		ctx.JSON(200, WatchInfo{
+		ctx.JSON(200, api.WatchInfo{
 			Subscribed:    true,
 			Ignored:       false,
 			Reason:        nil,
@@ -87,7 +75,7 @@ func Watch(ctx *context.APIContext) {
 		ctx.Error(500, "WatchRepo", err)
 		return
 	}
-	ctx.JSON(200, WatchInfo{
+	ctx.JSON(200, api.WatchInfo{
 		Subscribed:    true,
 		Ignored:       false,
 		Reason:        nil,
