f4254ee8c7d51323c009391b76176c6093edefee
Lunny Xiao <xiaolunwen@gmail.com>
2017-04-27 17:05:49 +0800

    bug fixed for API to get user's repos

--- routers/api/v1/user/repo.go
+++ routers/api/v1/user/repo.go
@@ -9,7 +9,7 @@ import (
 // listUserRepos - List the repositories owned by the given user.
 func listUserRepos(ctx *context.APIContext, u *models.User) {
 	userID := u.ID
-	showPrivateRepos := (ctx.User.ID == userID || ctx.User.IsAdmin) && ctx.IsSigned
+	showPrivateRepos := ctx.IsSigned && (ctx.User.ID == userID || ctx.User.IsAdmin)
 	ownRepos, err := models.GetUserRepositories(userID, showPrivateRepos, 1, u.NumRepos, "")
 	if err != nil {
 		ctx.Error(500, "GetUserRepositories", err)
