e31c02d4bf055a9d2a107eb89e0d39062d70f606
Lunny Xiao <xiaolunwen@gmail.com>
2017-04-29 13:52:25 +0800

    fix bug on issue view when not login (#1624)
    * fix bug on issue view when not login
    
    * hide issue watch when not login
    
    * update the tests
    
    * fix test on issue

--- routers/repo/issue.go
+++ routers/repo/issue.go
@@ -465,16 +465,20 @@ func ViewIssue(ctx *context.Context) {
 	}
 	ctx.Data["Title"] = fmt.Sprintf("#%d - %s", issue.Index, issue.Title)
 
-	iw, exists, err := models.GetIssueWatch(ctx.User.ID, issue.ID)
-	if err != nil {
-		ctx.Handle(500, "GetIssueWatch", err)
-		return
-	}
-	if !exists {
-		iw = &models.IssueWatch{
-			UserID:     ctx.User.ID,
-			IssueID:    issue.ID,
-			IsWatching: models.IsWatching(ctx.User.ID, ctx.Repo.Repository.ID),
+	var iw *models.IssueWatch
+	var exists bool
+	if ctx.User != nil {
+		iw, exists, err = models.GetIssueWatch(ctx.User.ID, issue.ID)
+		if err != nil {
+			ctx.Handle(500, "GetIssueWatch", err)
+			return
+		}
+		if !exists {
+			iw = &models.IssueWatch{
+				UserID:     ctx.User.ID,
+				IssueID:    issue.ID,
+				IsWatching: models.IsWatching(ctx.User.ID, ctx.Repo.Repository.ID),
+			}
 		}
 	}
 	ctx.Data["IssueWatch"] = iw
