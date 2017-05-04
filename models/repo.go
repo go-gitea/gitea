d4f9cf87aaeaa201c78931d3d43c2df42bab0f65
Guyzmo <guyzmo+github+pub@m0g.net>
2017-05-04 14:41:15 +0200

    Exposes in API the Repo entity's IsBare property as IsEmpty
    Signed-off-by: Guyzmo <guyzmo+github+pub@m0g.net>

--- models/repo.go
+++ models/repo.go
@@ -285,6 +285,7 @@ func (repo *Repository) APIFormat(mode AccessMode) *api.Repository {
 		FullName:      repo.FullName(),
 		Description:   repo.Description,
 		Private:       repo.IsPrivate,
+		Empty:         repo.IsBare,
 		Fork:          repo.IsFork,
 		Mirror:        repo.IsMirror,
 		HTMLURL:       repo.HTMLURL(),
