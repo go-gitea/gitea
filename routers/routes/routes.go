0a9a812838b6f967c1f0e3acda3980f6d18f2799
ergü <ergu@erguder.tk>
2017-05-10 11:56:10 +0200

    Reverted changes from Staff code review mistake

--- routers/routes/routes.go
+++ routers/routes/routes.go
@@ -486,7 +486,6 @@ func RegisterRoutes(m *macaron.Macaron) {
 			m.Get("/:id/:action", repo.ChangeMilestonStatus)
 			m.Post("/delete", repo.DeleteMilestone)
 		}, reqRepoWriter, context.RepoRef())
-		// New redirection from fake url
 		m.Group("/releases", func() {
 			m.Get("/download/:vTag/:fileName", repo.RedirectDownload)
 		}, repo.MustBeNotBare, reqRepoWriter, context.RepoRef())
