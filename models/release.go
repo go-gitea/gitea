1b8ce6e4d37276e7862772e227c0a6d0e20906c8
ergü <ergu@erguder.tk>
2017-05-13 01:01:47 +0200

    Import bothered someone and wanted his wishlist to

--- models/release.go
+++ models/release.go
@@ -15,6 +15,7 @@ import (
 	"code.gitea.io/gitea/modules/process"
 	"code.gitea.io/gitea/modules/setting"
 	"code.gitea.io/gitea/modules/util"
+
 	api "code.gitea.io/sdk/gitea"
 	"github.com/go-xorm/xorm"
 )
