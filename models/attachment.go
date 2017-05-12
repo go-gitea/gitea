7570b10448979f6c65ffc1d482a1a0ba8804f8eb
ergü <ergu@erguder.tk>
2017-05-13 00:58:12 +0200

    Reverted returned errors to correct values

--- models/attachment.go
+++ models/attachment.go
@@ -114,7 +114,7 @@ func getAttachmentByUUID(e Engine, uuid string) (*Attachment, error) {
 	attach := &Attachment{UUID: uuid}
 	has, err := e.Get(attach)
 	if err != nil {
-		return nil, nil
+		return nil, err
 	} else if !has {
 		return nil, ErrAttachmentNotExist{0, uuid}
 	}
@@ -164,7 +164,7 @@ func getAttachmentByReleaseIDFileName(e Engine, releaseID int64, fileName string
 	if err != nil {
 		return nil, err
 	} else if !has {
-		return nil, nil
+		return nil, err
 	}
 	return attach, nil
 }
