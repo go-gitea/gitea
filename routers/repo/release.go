87b22bfb77ce2ec794a753ac1c4556d684f0bfec
Ethan Koenig <ethantkoenig@gmail.com>
2017-06-01 22:45:07 -0400

    Include ctx.User in user cache

--- routers/repo/release.go
+++ routers/repo/release.go
@@ -79,7 +79,7 @@ func Releases(ctx *context.Context) {
 
 	// Temporary cache commits count of used branches to speed up.
 	countCache := make(map[string]int64)
-	var cacheUsers = make(map[int64]*models.User)
+	cacheUsers := map[int64]*models.User{ctx.User.ID: ctx.User}
 	var ok bool
 
 	releasesToDisplay := make([]*models.Release, 0, len(releases))
