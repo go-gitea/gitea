ff07bc65491d12f091960e02a7a1456b4ab2dba4
Roland Singer <roland.singer@desertbit.com>
2017-06-08 11:35:07 +0200

    added some comments to the sendmail send function.

--- modules/mailer/sendmail.go
+++ modules/mailer/sendmail.go
@@ -36,33 +36,31 @@ func (s *sendmailSender) Send(msg *Message) error {
 	return gomail.Send(s.sender, msg.Message)
 }
 
-// TODO: Cleanup the error handing and use defers.
 // send email.
 func (s *sendmailSender) send(from string, to []string, msg io.WriterTo) error {
-	var err error
-	var closeError error
-	var waitError error
-
 	args := []string{"-F", from, "-i"}
 	args = append(args, to...)
 	log.Trace("Sending with: %s %v", setting.MailService.SendmailPath, args)
 	cmd := exec.Command(setting.MailService.SendmailPath, args...)
-	pipe, err := cmd.StdinPipe()
 
+	// Stdin Pipe for message content.
+	pipe, err := cmd.StdinPipe()
 	if err != nil {
 		return err
 	}
 
-	if err = cmd.Start(); err != nil {
+	// Execute the command.
+	err = cmd.Start()
+	if err != nil {
 		return err
 	}
 
-	_, err = msg.WriteTo(pipe)
-
-	// we MUST close the pipe or sendmail will hang waiting for more of the message
+	// Write the message to the pipe and wait for the command to finish.
+	// We MUST close the pipe or sendmail will hang waiting for more of the message
 	// Also we should wait on our sendmail command even if something fails
-	closeError = pipe.Close()
-	waitError = cmd.Wait()
+	_, err = msg.WriteTo(pipe)
+	closeError := pipe.Close()
+	waitError := cmd.Wait()
 	if err != nil {
 		return err
 	} else if closeError != nil {
