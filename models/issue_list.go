eca84ae8006a9d0e22130720318a4811ff23e89f
Ethan Koenig <ethantkoenig@gmail.com>
2017-06-08 10:38:50 -0400

    Fix ghost user bug
    Fix bug where unassigned issues would appear to be assigned to a ghost user

--- models/issue_list.go
+++ models/issue_list.go
@@ -69,6 +69,9 @@ func (issues IssueList) loadPosters(e Engine) error {
 	}
 
 	for _, issue := range issues {
+		if issue.PosterID <= 0 {
+			continue
+		}
 		var ok bool
 		if issue.Poster, ok = posterMaps[issue.PosterID]; !ok {
 			issue.Poster = NewGhostUser()
@@ -176,6 +179,9 @@ func (issues IssueList) loadAssignees(e Engine) error {
 	}
 
 	for _, issue := range issues {
+		if issue.AssigneeID <= 0 {
+			continue
+		}
 		var ok bool
 		if issue.Assignee, ok = assigneeMaps[issue.AssigneeID]; !ok {
 			issue.Assignee = NewGhostUser()
