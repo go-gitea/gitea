31b5e9177f7c0ddcdb97e667f0634fdd13a8d59b
Lunny Xiao <xiaolunwen@gmail.com>
2017-06-15 10:50:12 +0800

    fix collborators lack of units on orgnization repositories (#1968)
    * fix collborators lack of units on orgnization repositories
    
    * also change log level to debug

--- integrations/repo_test.go
+++ integrations/repo_test.go
@@ -17,4 +17,30 @@ func TestViewRepo(t *testing.T) {
 	req := NewRequest(t, "GET", "/user2/repo1")
 	resp := MakeRequest(req)
 	assert.EqualValues(t, http.StatusOK, resp.HeaderCode)
+
+	req = NewRequest(t, "GET", "/user3/repo3")
+	resp = MakeRequest(req)
+	assert.EqualValues(t, http.StatusNotFound, resp.HeaderCode)
+
+	session := loginUser(t, "user1", "password")
+	resp = session.MakeRequest(t, req)
+	assert.EqualValues(t, http.StatusNotFound, resp.HeaderCode)
+}
+
+func TestViewRepo2(t *testing.T) {
+	prepareTestEnv(t)
+
+	req := NewRequest(t, "GET", "/user3/repo3")
+	session := loginUser(t, "user2", "password")
+	resp := session.MakeRequest(t, req)
+	assert.EqualValues(t, http.StatusOK, resp.HeaderCode)
+}
+
+func TestViewRepo3(t *testing.T) {
+	prepareTestEnv(t)
+
+	req := NewRequest(t, "GET", "/user3/repo3")
+	session := loginUser(t, "user3", "password")
+	resp := session.MakeRequest(t, req)
+	assert.EqualValues(t, http.StatusOK, resp.HeaderCode)
 }
