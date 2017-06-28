60c82cb35e1cfddad66494f466f8cd3f15a7f339
Ethan Koenig <ethantkoenig@gmail.com>
2017-06-28 14:04:53 -0400

    Integration test

--- integrations/user_test.go
+++ integrations/user_test.go
@@ -8,6 +8,9 @@ import (
 	"net/http"
 	"testing"
 
+	"code.gitea.io/gitea/models"
+
+	"github.com/Unknwon/i18n"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -17,4 +20,28 @@ func TestViewUser(t *testing.T) {
 	req := NewRequest(t, "GET", "/user2")
 	resp := MakeRequest(req)
 	assert.EqualValues(t, http.StatusOK, resp.HeaderCode)
+}
+
+func TestRenameInvalidUsername(t *testing.T) {
+	prepareTestEnv(t)
+
+	session := loginUser(t, "user2")
+	req := NewRequest(t, "GET", "/user/settings")
+	resp := session.MakeRequest(t, req)
+	assert.EqualValues(t, http.StatusOK, resp.HeaderCode)
+
+	htmlDoc := NewHTMLParser(t, resp.Body)
+	req = NewRequestWithValues(t, "POST", "/user/settings", map[string]string{
+		"_csrf": htmlDoc.GetCSRF(),
+		"name":  "%2f*", // not valid
+	})
+	resp = session.MakeRequest(t, req)
+	assert.EqualValues(t, http.StatusOK, resp.HeaderCode)
+	htmlDoc = NewHTMLParser(t, resp.Body)
+	assert.Contains(t,
+		htmlDoc.doc.Find(".ui.negative.message").Text(),
+		i18n.Tr("en", "form.alpha_dash_dot_error"),
+	)
+
+	models.AssertNotExistsBean(t, &models.User{Name: "%2f*"})
 }
