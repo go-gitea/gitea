bc1aec9857d463df3ee62aa796eb1794b8191cec
Ethan Koenig <ethantkoenig@gmail.com>
2017-06-30 12:50:57 -0400

    Fix SQL bug in models.PullRequests

--- models/pull.go
+++ models/pull.go
@@ -688,8 +688,6 @@ func listPullRequestStatement(baseRepoID int64, opts *PullRequestsOptions) (*xor
 		sess.And("issue.is_closed=?", opts.State == "closed")
 	}
 
-	sortIssuesSession(sess, opts.SortType)
-
 	if labelIDs, err := base.StringsToInt64s(opts.Labels); err != nil {
 		return nil, err
 	} else if len(labelIDs) > 0 {
@@ -723,6 +721,7 @@ func PullRequests(baseRepoID int64, opts *PullRequestsOptions) ([]*PullRequest, 
 
 	prs := make([]*PullRequest, 0, ItemsPerPage)
 	findSession, err := listPullRequestStatement(baseRepoID, opts)
+	sortIssuesSession(findSession, opts.SortType)
 	if err != nil {
 		log.Error(4, "listPullRequestStatement", err)
 		return nil, maxResults, err
