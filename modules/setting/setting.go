c9b9b5b7ebc6be73b421286ba688cc2e1ae2a201
Lauris Bukšis-Haberkorns <lauris@nix.lv>
2017-07-30 22:32:56 +0300

    Fix internal requests when gitea listens to unix socket or only external IP

--- modules/setting/setting.go
+++ modules/setting/setting.go
@@ -658,7 +658,24 @@ func NewContext() {
 	AppSubURL = strings.TrimSuffix(url.Path, "/")
 	AppSubURLDepth = strings.Count(AppSubURL, "/")
 
-	LocalURL = sec.Key("LOCAL_ROOT_URL").MustString(string(Protocol) + "://localhost:" + HTTPPort + "/")
+	var defaultLocalURL string
+	switch Protocol {
+	case UnixSocket:
+		defaultLocalURL = "http://unix/"
+	case FCGI:
+		if !SSH.Disabled && !SSH.StartBuiltinServer {
+			log.Fatal(4, "External SSH server can not be used with Gitea configured to listen using FastCGI protocol")
+		}
+	default:
+		defaultLocalURL = string(Protocol) + "://"
+		if HTTPAddr == "0.0.0.0" {
+			defaultLocalURL += "localhost"
+		} else {
+			defaultLocalURL += HTTPAddr
+		}
+		defaultLocalURL += ":" + HTTPPort + "/"
+	}
+	LocalURL = sec.Key("LOCAL_ROOT_URL").MustString(defaultLocalURL)
 	OfflineMode = sec.Key("OFFLINE_MODE").MustBool()
 	DisableRouterLog = sec.Key("DISABLE_ROUTER_LOG").MustBool()
 	StaticRootPath = sec.Key("STATIC_ROOT_PATH").MustString(workDir)
