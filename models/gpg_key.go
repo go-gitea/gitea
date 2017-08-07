831329f1c56482342e12b1b631c4265601bfad68
Antoine GIRARD <sapk@sapk.fr>
2017-08-07 02:31:30 +0200

    Fix typo

--- models/gpg_key.go
+++ models/gpg_key.go
@@ -220,7 +220,7 @@ func parseGPGKey(ownerID int64, e *openpgp.Entity) (*GPGKey, error) {
 		}
 	}
 
-	//In the case not email as been found
+	//In the case no email as been found
 	if len(emails) == 0 {
 		failedEmails := make([]string, 0, len(e.Identities))
 		for _, ident := range e.Identities {
@@ -386,7 +386,7 @@ func ParseCommitWithSignature(c *git.Commit) *CommitVerification {
 		}
 
 		//Find Committer account
-		committer, err := GetUserByEmail(c.Committer.Email) //This find the user by primary mail or activated email do commit will not be valid if email is not
+		committer, err := GetUserByEmail(c.Committer.Email) //This find the user by primary email or activated email so commit will not be valid if email is not
 		if err != nil {                                     //Skipping not user for commiter
 			log.Error(3, "NoCommitterAccount: %v", err)
 			return &CommitVerification{
