29f3a6e492daa80a2ce5e3693743aa2448f391d9
Ethan Koenig <etk39@cornell.edu>
2017-08-13 15:30:28 -0700

    Fix rendering of external links (#2292)

--- routers/init.go
+++ routers/init.go
@@ -50,6 +50,7 @@ func GlobalInit() {
 
 	if setting.InstallLock {
 		highlight.NewContext()
+		markdown.InitMarkdown()
 		markdown.NewSanitizer()
 		if err := models.NewEngine(migrations.Migrate); err != nil {
 			log.Fatal(4, "Failed to initialize ORM engine: %v", err)
