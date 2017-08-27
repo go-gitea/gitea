78e755c812233bded3188660226e3703357fad87
Konrad Langenberg <konrad@kola-entertainments.de>
2017-08-27 21:30:28 +0200

    Added UI to delete a dependency
    Signed-off-by: Konrad <konrad@kola-entertainments.de>

--- public/js/index.js
+++ public/js/index.js
@@ -1824,11 +1824,25 @@ function cancelStopwatch() {
 }
 
 function showAddDependencyModal() {
-    $('.mini.modal')
+    $('.add-dependency')
         .modal({
             duration: 200,
             onApprove: function() {
                 $('#addDependencyForm').submit();
+            }
+        }).modal('show')
+    ;
+}
+
+function deleteDependencyModal(id, type) {
+    $('.remove-dependency')
+        .modal({
+            closable: false,
+            duration: 200,
+            onApprove: function() {
+                $('#removeDependencyID').val(id);
+                $('#dependencyType').val(type);
+                $('#removeDependencyForm').submit();
             }
         }).modal('show')
     ;
