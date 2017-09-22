412e6951111b449c319f089401084cc7f55853bc
Thorben Braeutigam <thorben.braeutigam@gmail.com>
2017-09-22 14:57:45 +0100

    Streamline Snap packaging:  - Take advantage of install-hooks (snapd 2.27)  - Use snapctl configuration storage for unchanging values

--- snap/helpers/simple_launcher.sh
+++ snap/helpers/simple_launcher.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-source $SNAP/bin/directorySetup.sh
+if ! env | grep -q root; then
+  echo "
+   +----------------------------------------+
+   | You are not running gitea as root.     |
+   | This is required for the snap package. |
+   | Please re-run as root.                 |
+   +----------------------------------------+
+"
+  $SNAP/gitea/gitea --help
+  exit 1
+fi
 
 # Set usernames for gitea
 export USERNAME=root
 export USER=root
 
-export GITEA_WORK_DIR=$SCOMMON
-export GITEA_CUSTOM=$SDATA/custom
+export GITEA_WORK_DIR=$(snapctl get GITEA_WORK_DIR)
+export GITEA_CUSTOM=$(snapctl get GITEA_CUSTOM)
 
 cd $SNAP/gitea; ./gitea $@
