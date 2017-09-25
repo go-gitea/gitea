6035eda15cc183e36ee30c7d214e04459ccee7e0
Lauris Bukšis-Haberkorns <lauris@nix.lv>
2017-09-26 00:56:39 +0300

    Fix time tracking migration

--- models/migrations/v39.go
+++ models/migrations/v39.go
@@ -5,34 +5,63 @@
 package migrations
 
 import (
+	"encoding/json"
 	"fmt"
 	"time"
 
 	"code.gitea.io/gitea/modules/setting"
 
+	"github.com/go-xorm/core"
 	"github.com/go-xorm/xorm"
 )
 
-// Stopwatch see models/issue_stopwatch.go
-type Stopwatch struct {
-	ID          int64     `xorm:"pk autoincr"`
-	IssueID     int64     `xorm:"INDEX"`
-	UserID      int64     `xorm:"INDEX"`
-	Created     time.Time `xorm:"-"`
-	CreatedUnix int64
+// IssuesConfigV39 describes issues config
+type IssuesConfigV39 struct {
+	EnableTimetracker                bool
+	AllowOnlyContributorsToTrackTime bool
 }
 
-// TrackedTime see models/issue_tracked_time.go
-type TrackedTime struct {
-	ID          int64     `xorm:"pk autoincr" json:"id"`
-	IssueID     int64     `xorm:"INDEX" json:"issue_id"`
-	UserID      int64     `xorm:"INDEX" json:"user_id"`
-	Created     time.Time `xorm:"-" json:"created"`
-	CreatedUnix int64     `json:"-"`
-	Time        int64     `json:"time"`
+// FromDB fills up a IssuesConfigV39 from serialized format.
+func (cfg *IssuesConfigV39) FromDB(bs []byte) error {
+	return json.Unmarshal(bs, &cfg)
+}
+
+// ToDB exports a IssuesConfigV39 to a serialized format.
+func (cfg *IssuesConfigV39) ToDB() ([]byte, error) {
+	return json.Marshal(cfg)
 }
 
 func addTimetracking(x *xorm.Engine) error {
+	// RepoUnit describes all units of a repository
+	type RepoUnit struct {
+		ID          int64
+		RepoID      int64 `xorm:"INDEX(s)"`
+		Type        int   `xorm:"INDEX(s)"`
+		Index       int
+		Config      core.Conversion `xorm:"TEXT"`
+		CreatedUnix int64           `xorm:"INDEX CREATED"`
+		Created     time.Time       `xorm:"-"`
+	}
+
+	// Stopwatch see models/issue_stopwatch.go
+	type Stopwatch struct {
+		ID          int64     `xorm:"pk autoincr"`
+		IssueID     int64     `xorm:"INDEX"`
+		UserID      int64     `xorm:"INDEX"`
+		Created     time.Time `xorm:"-"`
+		CreatedUnix int64
+	}
+
+	// TrackedTime see models/issue_tracked_time.go
+	type TrackedTime struct {
+		ID          int64     `xorm:"pk autoincr" json:"id"`
+		IssueID     int64     `xorm:"INDEX" json:"issue_id"`
+		UserID      int64     `xorm:"INDEX" json:"user_id"`
+		Created     time.Time `xorm:"-" json:"created"`
+		CreatedUnix int64     `json:"-"`
+		Time        int64     `json:"time"`
+	}
+
 	if err := x.Sync2(new(Stopwatch)); err != nil {
 		return fmt.Errorf("Sync2: %v", err)
 	}
@@ -44,18 +73,10 @@ func addTimetracking(x *xorm.Engine) error {
 	x.Where("type = ?", V16UnitTypeIssues).Find(&units)
 	for _, unit := range units {
 		if unit.Config == nil {
-			unit.Config = make(map[string]interface{})
-		}
-		changes := false
-		if _, ok := unit.Config["EnableTimetracker"]; !ok {
-			unit.Config["EnableTimetracker"] = setting.Service.DefaultEnableTimetracking
-			changes = true
-		}
-		if _, ok := unit.Config["AllowOnlyContributorsToTrackTime"]; !ok {
-			unit.Config["AllowOnlyContributorsToTrackTime"] = setting.Service.DefaultAllowOnlyContributorsToTrackTime
-			changes = true
-		}
-		if changes {
+			unit.Config = &IssuesConfigV39{
+				EnableTimetracker:                setting.Service.DefaultEnableTimetracking,
+				AllowOnlyContributorsToTrackTime: setting.Service.DefaultAllowOnlyContributorsToTrackTime,
+			}
 			if _, err := x.Id(unit.ID).Cols("config").Update(unit); err != nil {
 				return err
 			}
