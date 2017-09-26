893858be25b900204c3f5784bf5ccb9d15013c10
Peter Žeby <morlinest@gmail.com>
2017-09-26 12:53:38 +0200

    Add migration

---
+++ models/migrations/v44.go
@@ -0,0 +1,25 @@
+// Copyright 2017 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package migrations
+
+import (
+	"os"
+
+	"code.gitea.io/gitea/modules/setting"
+	"github.com/go-xorm/xorm"
+)
+
+func regenerateIssueIndexer(x *xorm.Engine) error {
+	_, err := os.Stat(setting.Indexer.IssuePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		} else {
+			return err
+		}
+	}
+
+	return os.RemoveAll(setting.Indexer.IssuePath)
+}
