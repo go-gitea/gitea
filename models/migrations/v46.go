96096096272955365d1da6b967d9a744f5389bf0
Konrad Langenberg <konrad@kola-entertainments.de>
2017-10-04 23:36:38 +0200

    Merge branch 'master' of https://github.com/go-gitea/gitea
    Signed-off-by: Konrad <konrad@kola-entertainments.de>
    
    # Conflicts:
    #	models/migrations/migrations.go
    #	models/migrations/v44.go

--- models/migrations/v46.go
+++ models/migrations/v46.go
@@ -1,1 +1,24 @@
+// Copyright 2017 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 package migrations
+
+import (
+	"fmt"
+
+	"code.gitea.io/gitea/models"
+
+	"github.com/go-xorm/xorm"
+)
+
+func addIssueDependencyTables(x *xorm.Engine) (err error) {
+
+	err = x.Sync(new(models.IssueDependency))
+
+	if err != nil {
+		return fmt.Errorf("Error creating issue_dependency_table column definition: %v", err)
+	}
+
+	return err
+}
