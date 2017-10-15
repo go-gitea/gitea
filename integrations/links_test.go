054b5597721575a37cad9d5718daed3205eb6df8
Lunny Xiao <xiaolunwen@gmail.com>
2017-10-15 11:01:32 +0800

    fix tests

--- integrations/links_test.go
+++ integrations/links_test.go
@@ -72,7 +72,7 @@ func testLinksAsUser(userName string, t *testing.T) {
 		"/user/settings",
 		"/user/settings/avatar",
 		"/user/settings/security",
-		"/security/two_factor/enroll",
+		"/user/settings/security/two_factor/enroll",
 		"/user/settings/email",
 		"/user/settings/keys",
 		"/user/settings/applications",
