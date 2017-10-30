751bbee53439ec2f6d6c05de8ee68060c97af129
Konrad Langenberg <konrad@kola-entertainments.de>
2017-10-30 21:26:38 +0100

    Merge branch 'master' of https://github.com/go-gitea/gitea
    Signed-off-by: Konrad <konrad@kola-entertainments.de>
    
    # Conflicts:
    #	models/migrations/migrations.go
    #	models/migrations/v46.go
    #	models/repo.go
    #	public/js/index.js

--- models/migrations/v49.go
+++ models/migrations/v49.go
@@ -1,1 +1,34 @@
+// Copyright 2017 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 package migrations
+
+import (
+	"fmt"
+
+	"github.com/go-xorm/xorm"
+	"time"
+)
+
+func addIssueDependencyTables(x *xorm.Engine) (err error) {
+
+	type IssueDependency struct {
+		ID           int64     `xorm:"pk autoincr"`
+		UserID       int64     `xorm:"UNIQUE(watch) NOT NULL"`
+		IssueID      int64     `xorm:"UNIQUE(watch) NOT NULL"`
+		DependencyID int64     `xorm:"UNIQUE(watch) NOT NULL"`
+		Created      time.Time `xorm:"-"`
+		CreatedUnix  int64     `xorm:"INDEX created"`
+		Updated      time.Time `xorm:"-"`
+		UpdatedUnix  int64     `xorm:"updated"`
+	}
+
+	err = x.Sync(new(IssueDependency))
+
+	if err != nil {
+		return fmt.Errorf("Error creating issue_dependency_table column definition: %v", err)
+	}
+
+	return err
+}
