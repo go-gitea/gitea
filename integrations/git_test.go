75b617237e91bde0fa66c989a589f1d3ba70a0ef
Lunny Xiao <xiaolunwen@gmail.com>
2017-11-02 13:47:45 +0800

    use random listen port instead a fixed port

--- integrations/git_test.go
+++ integrations/git_test.go
@@ -6,7 +6,9 @@ package integrations
 
 import (
 	"context"
+	"fmt"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -19,13 +21,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestClonePush_ViaHTTP_NoLogin(t *testing.T) {
-	prepareTestEnv(t)
-
+func onGiteaWebRun(t *testing.T, callback func(*testing.T, string)) {
 	s := http.Server{
-		Addr:    ":3000",
 		Handler: mac,
 	}
+
+	listener, err := net.Listen("tcp", "")
+	assert.NoError(t, err)
 
 	defer func() {
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
@@ -33,14 +35,26 @@ func TestClonePush_ViaHTTP_NoLogin(t *testing.T) {
 		cancel()
 	}()
 
-	go s.ListenAndServe()
+	go s.Serve(listener)
 
-	dstPath, err := ioutil.TempDir("", "repo1")
-	assert.NoError(t, err)
-	defer os.RemoveAll(dstPath)
-
-	err = git.Clone("http://localhost:3000/user2/repo1.git", dstPath, git.CloneRepoOptions{})
+	_, port, err := net.SplitHostPort(listener.Addr().String())
 	assert.NoError(t, err)
 
-	assert.True(t, com.IsExist(filepath.Join(dstPath, "README.md")))
+	callback(t, fmt.Sprintf("http://localhost:%s/", port))
+}
+
+func TestClone_ViaHTTP_NoLogin(t *testing.T) {
+	prepareTestEnv(t)
+
+	onGiteaWebRun(t, func(t *testing.T, urlPrefix string) {
+		dstPath, err := ioutil.TempDir("", "repo1")
+		assert.NoError(t, err)
+		defer os.RemoveAll(dstPath)
+
+		err = git.Clone(fmt.Sprintf("%suser2/repo1.git", urlPrefix),
+			dstPath, git.CloneRepoOptions{})
+		assert.NoError(t, err)
+
+		assert.True(t, com.IsExist(filepath.Join(dstPath, "README.md")))
+	})
 }
