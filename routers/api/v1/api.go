bf7f223ee5d4afb95f143217bbf13fa18e97bbf8
Ethan Koenig <ethantkoenig@gmail.com>
2017-11-12 18:34:07 -0800

    Restore delete comments

--- routers/api/v1/api.go
+++ routers/api/v1/api.go
@@ -1,6 +1,10 @@
+// Copyright 2015 The Gogs Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
 // Package v1 Gitea API.
+//
+// This documentation describes the Gitea API.
 //
 //     Schemes: http, https
 //     BasePath: /api/v1
