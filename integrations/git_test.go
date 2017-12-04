95be3d7258e8dca549bb9d486d2a5214b606e8ac
Antoine GIRARD <sapk@sapk.fr>
2017-12-04 23:33:53 +0100

    Add failing test

--- integrations/git_test.go
+++ integrations/git_test.go
@@ -8,8 +8,10 @@ import (
 	"context"
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"net"
 	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"testing"
@@ -43,7 +45,7 @@ func onGiteaWebRun(t *testing.T, callback func(*testing.T, string)) {
 	callback(t, fmt.Sprintf("http://localhost:%s/", port))
 }
 
-func TestClone_ViaHTTP_NoLogin(t *testing.T) {
+func TestGit(t *testing.T) {
 	prepareTestEnv(t)
 
 	onGiteaWebRun(t, func(t *testing.T, urlPrefix string) {
@@ -51,10 +53,57 @@ func TestClone_ViaHTTP_NoLogin(t *testing.T) {
 		assert.NoError(t, err)
 		defer os.RemoveAll(dstPath)
 
-		err = git.Clone(fmt.Sprintf("%suser2/repo1.git", urlPrefix),
-			dstPath, git.CloneRepoOptions{})
-		assert.NoError(t, err)
+		t.Run("CloneNoLogin", func(t *testing.T) {
+			err = git.Clone(fmt.Sprintf("%suser2/repo1.git", urlPrefix),
+				dstPath, git.CloneRepoOptions{})
+			assert.NoError(t, err)
+			assert.True(t, com.IsExist(filepath.Join(dstPath, "README.md")))
+		})
 
-		assert.True(t, com.IsExist(filepath.Join(dstPath, "README.md")))
+		t.Run("LFS", func(t *testing.T) {
+			/* Generate random file */
+			data := make([]byte, 1024)
+			_, err := rand.Read(data)
+			assert.NoError(t, err)
+			tmpFile, err := ioutil.TempFile(dstPath, "data-file-")
+			defer tmpFile.Close()
+			_, err = tmpFile.Write(data)
+			assert.NoError(t, err)
+
+			//Setup git LFS
+			_, err = git.NewCommand("lfs").AddArguments("install").RunInDir(dstPath)
+			assert.NoError(t, err)
+			_, err = git.NewCommand("lfs").AddArguments("track", "data-file-*").RunInDir(dstPath)
+			assert.NoError(t, err)
+
+			//Commit
+			err = git.AddChanges(dstPath, false, ".gitattributes", tmpFile.Name())
+			assert.NoError(t, err)
+			err = git.CommitChanges(dstPath, git.CommitChangesOptions{
+				Committer: &git.Signature{
+					Email: "test@email.com",
+					Name:  "User2",
+					When:  time.Now(),
+				},
+				Author: &git.Signature{
+					Email: "test@email.com",
+					Name:  "User2",
+					When:  time.Now(),
+				},
+				Message: "Testing LFS ",
+			})
+			assert.NoError(t, err)
+
+			//Push
+			u, err := url.Parse(fmt.Sprintf("%suser2/repo1.git", urlPrefix))
+			u.User = url.UserPassword("user2", "password")
+			assert.NoError(t, err)
+			err = git.Push(dstPath, git.PushOptions{
+				Branch: "master",
+				Remote: u.String(),
+				Force:  false,
+			})
+			assert.NoError(t, err)
+		})
 	})
 }
