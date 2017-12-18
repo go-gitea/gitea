4694b2f7772614a04e88f522d3063405d1762d9f
Antoine GIRARD <sapk@sapk.fr>
2017-12-18 22:15:09 +0100

    Apply recommendations of @ethantkoenig

--- integrations/benchmarks_test.go
+++ integrations/benchmarks_test.go
@@ -5,7 +5,6 @@
 package integrations
 
 import (
-	"fmt"
 	"math/rand"
 	"net/http"
 	"testing"
@@ -54,7 +53,7 @@ func BenchmarkRepo(b *testing.B) {
 					}
 				})
 				branchCount := len(branches)
-				b.Run("WebBranchCommit", func(b *testing.B) {
+				b.Run("WebViewCommit", func(b *testing.B) {
 					for i := 0; i < b.N; i++ {
 						req := NewRequestf(b, "GET", "/%s/%s/commit/%s", "user2", s.name, branches[i%branchCount].Commit.ID)
 						session.MakeRequest(b, req, http.StatusOK)
@@ -75,46 +74,35 @@ func StringWithCharset(length int, charset string) string {
 }
 
 func BenchmarkRepoBranchCommit(b *testing.B) {
-	samples := []struct {
-		repoID int64
-		repo   *models.Repository
-		owner  *models.User
-	}{
-		{repoID: 1},
-		{repoID: 3},
-		{repoID: 15},
-		{repoID: 16},
-	}
-
+	samples := []int64{1, 3, 15, 16}
 	prepareTestEnv(b)
-	for i, s := range samples {
-		samples[i].repo = models.AssertExistsAndLoadBean(b, &models.Repository{ID: s.repoID}).(*models.Repository)
-		samples[i].owner = models.AssertExistsAndLoadBean(b, &models.User{ID: samples[i].repo.OwnerID}).(*models.User)
-	}
 	b.ResetTimer()
-	for _, s := range samples {
-		b.Run(s.repo.Name, func(b *testing.B) {
-			session := loginUser(b, s.owner.LoginName)
+
+	for _, repoID := range samples {
+		b.StopTimer()
+		repo := models.AssertExistsAndLoadBean(b, &models.Repository{ID: repoID}).(*models.Repository)
+		b.StartTimer()
+		b.Run(repo.Name, func(b *testing.B) {
+			owner := models.AssertExistsAndLoadBean(b, &models.User{ID: repo.OwnerID}).(*models.User)
+			session := loginUser(b, owner.LoginName)
 			b.ResetTimer()
 			b.Run("Create", func(b *testing.B) {
 				for i := 0; i < b.N; i++ {
 					b.StopTimer()
-					branchName := StringWithCharset(5+rand.Intn(10), "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/")
-					fmt.Println(branchName)
+					branchName := StringWithCharset(5+rand.Intn(10), "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 					b.StartTimer()
-					testCreateBranch(b, session, s.owner.LoginName, s.repo.Name, "branch/master", branchName, http.StatusFound)
+					testCreateBranch(b, session, owner.LoginName, repo.Name, "branch/master", branchName, http.StatusFound)
 				}
 			})
 			b.Run("Access", func(b *testing.B) {
 				var branches []*api.Branch
-				req := NewRequestf(b, "GET", "/api/v1/%s/branches", s.repo.FullName())
+				req := NewRequestf(b, "GET", "/api/v1/%s/branches", repo.FullName())
 				resp := session.MakeRequest(b, req, http.StatusOK)
 				DecodeJSON(b, resp, &branches)
-				fmt.Println(branches)
 				branchCount := len(branches)
 				b.ResetTimer() //We measure from here
 				for i := 0; i < b.N; i++ {
-					req := NewRequestf(b, "GET", "/%s/%s/commits/%s", s.owner.Name, s.repo.Name, branches[i%branchCount])
+					req := NewRequestf(b, "GET", "/%s/%s/commits/%s", owner.Name, repo.Name, branches[i%branchCount])
 					session.MakeRequest(b, req, http.StatusOK)
 				}
 			})
