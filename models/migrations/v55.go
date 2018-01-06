b3e094eaa47143615e381fa0b2947c1fb76178e1
Vlad Temian <vladtemian@gmail.com>
2018-01-06 22:55:35 +0200

    Fix migration hell

--- models/migrations/v55.go
+++ models/migrations/v55.go
@@ -1,4 +1,4 @@
-// Copyright 2017 The Gitea Authors. All rights reserved.
+// Copyright 2018 The Gitea Authors. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
@@ -7,51 +7,17 @@ package migrations
 import (
 	"fmt"
 
-	"code.gitea.io/gitea/modules/util"
-
+	"code.gitea.io/gitea/models"
 	"github.com/go-xorm/xorm"
 )
 
-func addPullRequestOptions(x *xorm.Engine) error {
-	// RepoUnit describes all units of a repository
-	type RepoUnit struct {
-		ID          int64
-		RepoID      int64                  `xorm:"INDEX(s)"`
-		Type        int                    `xorm:"INDEX(s)"`
-		Config      map[string]interface{} `xorm:"JSON"`
-		CreatedUnix util.TimeStamp         `xorm:"INDEX CREATED"`
+func addModeToDeploKeys(x *xorm.Engine) error {
+	type DeployKey struct {
+		Mode models.AccessMode `xorm:"NOT NULL DEFAULT 1"`
 	}
 
-	sess := x.NewSession()
-	defer sess.Close()
-	if err := sess.Begin(); err != nil {
-		return err
+	if err := x.Sync2(new(DeployKey)); err != nil {
+		return fmt.Errorf("Sync2: %v", err)
 	}
-
-	//Updating existing issue units
-	units := make([]*RepoUnit, 0, 100)
-	if err := sess.Where("`type` = ?", V16UnitTypePRs).Find(&units); err != nil {
-		return fmt.Errorf("Query repo units: %v", err)
-	}
-	for _, unit := range units {
-		if unit.Config == nil {
-			unit.Config = make(map[string]interface{})
-		}
-		if _, ok := unit.Config["IgnoreWhitespaceConflicts"]; !ok {
-			unit.Config["IgnoreWhitespaceConflicts"] = false
-		}
-		if _, ok := unit.Config["AllowMerge"]; !ok {
-			unit.Config["AllowMerge"] = true
-		}
-		if _, ok := unit.Config["AllowRebase"]; !ok {
-			unit.Config["AllowRebase"] = true
-		}
-		if _, ok := unit.Config["AllowSquash"]; !ok {
-			unit.Config["AllowSquash"] = true
-		}
-		if _, err := sess.ID(unit.ID).Cols("config").Update(unit); err != nil {
-			return err
-		}
-	}
-	return sess.Commit()
+	return nil
 }
