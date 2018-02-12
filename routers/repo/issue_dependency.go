be51a479dcb3898136bed0a48e1309329dfdb941
kolaente <k@knt.li>
2018-02-12 17:22:43 +0100

    Merged 'routers/issue_dependency_add' and 'routers/issue_dependency_remove' into one single file

--- routers/repo/issue_dependency.go
+++ routers/repo/issue_dependency.go
@@ -67,3 +67,53 @@ func AddDependency(ctx *context.Context) {
 	url := fmt.Sprintf("%s/issues/%d", ctx.Repo.RepoLink, issueIndex)
 	ctx.Redirect(url, http.StatusSeeOther)
 }
+
+
+// RemoveDependency removes the dependency
+func RemoveDependency(ctx *context.Context) {
+	depID := ctx.QueryInt64("removeDependencyID")
+
+	issueIndex := ctx.ParamsInt64("index")
+	issue, err := models.GetIssueByIndex(ctx.Repo.Repository.ID, issueIndex)
+	if err != nil {
+		ctx.ServerError("GetIssueByIndex", err)
+		return
+	}
+
+	// Check if the Repo is allowed to have dependencies
+	if !ctx.Repo.CanCreateIssueDependencies(ctx.User) {
+		ctx.NotFound("NotAllowedToCreateIssueDependencies", nil)
+		return
+	}
+
+	// Dependency Type
+	depTypeStr := ctx.Req.PostForm.Get("dependencyType")
+
+	var depType models.DependencyType
+
+	switch depTypeStr {
+	case "blockedBy":
+		depType = models.DependencyTypeBlockedBy
+	case "blocking":
+		depType = models.DependencyTypeBlocking
+	default:
+		ctx.Error(http.StatusBadRequest, "GetDependecyType")
+		//ctx.Handle(http.StatusBadRequest, "GetDependecyType", nil)
+		return
+	}
+
+	// Dependency
+	dep, err := models.GetIssueByID(depID)
+	if err != nil {
+		ctx.ServerError("GetIssueByID", err)
+		return
+	}
+
+	if err = models.RemoveIssueDependency(ctx.User, issue, dep, depType); err != nil {
+		ctx.ServerError("RemoveIssueDependency", err)
+		return
+	}
+
+	url := fmt.Sprintf("%s/issues/%d", ctx.Repo.RepoLink, issueIndex)
+	ctx.Redirect(url, http.StatusSeeOther)
+}
