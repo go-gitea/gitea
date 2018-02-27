0d5afc22f557cf696a5289b6597f8420a054aebc
Steve Day <Steven.Day@cityscience.com>
2018-02-27 11:08:16 +0000

    fixup! Move default highlight.js highlighting into a web worker

--- public/js/highlight.js
+++ public/js/highlight.js
@@ -4,4 +4,4 @@ onmessage = function(event) {
     importScripts('/vendor/plugins/highlight/highlight.pack.js');
     var result = self.hljs.highlightAuto(event.data);
     postMessage(result.value);
-}
+};
