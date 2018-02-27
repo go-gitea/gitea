96acb4ececce4fe18f21db9805ea8d096ef711e6
Jonas Franz <info@jonasfranz.software>
2018-02-27 19:17:40 +0100

    Update go-sdk
    Signed-off-by: Jonas Franz <info@jonasfranz.de>

--- vendor/code.gitea.io/sdk/gitea/attachment.go
+++ vendor/code.gitea.io/sdk/gitea/attachment.go
@@ -7,9 +7,10 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"mime/multipart"
-	"os"
+	"net/http"
 	"time"
 )
 
@@ -30,7 +31,7 @@ type Attachment struct {
 func (c *Client) ListReleaseAttachments(user, repo string, release int64) ([]*Attachment, error) {
 	attachments := make([]*Attachment, 0, 10)
 	err := c.getParsedResponse("GET",
-		fmt.Sprintf("/repos/%s/%s/releases/%d/attachments", user, repo, release),
+		fmt.Sprintf("/repos/%s/%s/releases/%d/assets", user, repo, release),
 		nil, nil, &attachments)
 	return attachments, err
 }
@@ -39,43 +40,33 @@ func (c *Client) ListReleaseAttachments(user, repo string, release int64) ([]*At
 func (c *Client) GetReleaseAttachment(user, repo string, release int64, id int64) (*Attachment, error) {
 	a := new(Attachment)
 	err := c.getParsedResponse("GET",
-		fmt.Sprintf("/repos/%s/%s/releases/%d/attachments/%d", user, repo, release, id),
+		fmt.Sprintf("/repos/%s/%s/releases/%d/assets/%d", user, repo, release, id),
 		nil, nil, &a)
 	return a, err
 }
 
 // CreateReleaseAttachment creates an attachment for the given release
-func (c *Client) CreateReleaseAttachment(user, repo string, release int64, file *os.File) (*Attachment, error) {
-	// Read file to upload
-	fileContents, err := ioutil.ReadAll(file)
-	if err != nil {
-		return nil, err
-	}
-
-	fi, err := file.Stat()
-	if err != nil {
-		return nil, err
-	}
-	file.Close()
-
+func (c *Client) CreateReleaseAttachment(user, repo string, release int64, file *io.Reader, filename string) (*Attachment, error) {
 	// Write file to body
 	body := new(bytes.Buffer)
 	writer := multipart.NewWriter(body)
-	part, err := writer.CreateFormFile("attachment", fi.Name())
+	part, err := writer.CreateFormFile("attachment", filename)
 	if err != nil {
 		return nil, err
 	}
-	part.Write(fileContents)
-	err = writer.Close()
-	if err != nil {
+
+	if _, err = io.Copy(part, file); err != nil {
+		return nil, err
+	}
+	if err = writer.Close(); err != nil {
 		return nil, err
 	}
 
 	// Send request
 	attachment := new(Attachment)
 	err = c.getParsedResponse("POST",
-		fmt.Sprintf("/repos/%s/%s/releases/%d/attachments", user, repo, release),
-		nil, body, &attachment)
+		fmt.Sprintf("/repos/%s/%s/releases/%d/assets", user, repo, release),
+		http.Header{"Content-Type": {writer.FormDataContentType()}}, body, &attachment)
 	return attachment, err
 }
 
@@ -86,12 +77,12 @@ func (c *Client) EditReleaseAttachment(user, repo string, release int64, attachm
 		return nil, err
 	}
 	attach := new(Attachment)
-	return attach, c.getParsedResponse("PATCH", fmt.Sprintf("/repos/%s/%s/releases/%d/attachments/%d", user, repo, release, attachment), jsonHeader, bytes.NewReader(body), attach)
+	return attach, c.getParsedResponse("PATCH", fmt.Sprintf("/repos/%s/%s/releases/%d/assets/%d", user, repo, release, attachment), jsonHeader, bytes.NewReader(body), attach)
 }
 
 // DeleteReleaseAttachment deletes the given attachment including the uploaded file
 func (c *Client) DeleteReleaseAttachment(user, repo string, release int64, id int64) error {
-	_, err := c.getResponse("DELETE", fmt.Sprintf("/repos/%s/%s/releases/%d/attachments/%d", user, repo, release, id), nil, nil)
+	_, err := c.getResponse("DELETE", fmt.Sprintf("/repos/%s/%s/releases/%d/assets/%d", user, repo, release, id), nil, nil)
 	return err
 }
 
