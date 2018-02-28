8e55f944c5c983262d3bbcbc383c55087540ee2e
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-02-28 23:03:58 +0800

    fix: testing

--- models/repo_watch.go
+++ models/repo_watch.go
@@ -87,6 +87,13 @@ func notifyWatchers(e Engine, act *Action) error {
 		return fmt.Errorf("insert new actioner: %v", err)
 	}
 
+	// check repo owner exist.
+	if act.Repo != nil && act.Repo.Owner == nil {
+		if err := act.Repo.getOwner(e); err != nil {
+			return fmt.Errorf("can't get repo owner: %v", err)
+		}
+	}
+
 	// Add feed for organization
 	if act.Repo.Owner.IsOrganization() && act.ActUserID != act.Repo.Owner.ID {
 		act.ID = 0
