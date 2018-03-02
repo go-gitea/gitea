e5c1b820d2c6c6791d47be2532b2899c3d5d654d
Jonas Franz <info@jonasfranz.software>
2018-03-02 11:29:43 +0100

    Updating go-sdk and regenerating swagger
    Signed-off-by: Jonas Franz <info@jonasfranz.software>

--- vendor/code.gitea.io/sdk/gitea/release.go
+++ vendor/code.gitea.io/sdk/gitea/release.go
@@ -28,7 +28,7 @@ type Release struct {
 	// swagger:strfmt date-time
 	PublishedAt time.Time     `json:"published_at"`
 	Publisher   *User         `json:"author"`
-	Attachments []*Attachment `json:"attachments"`
+	Attachments []*Attachment `json:"assets"`
 }
 
 // ListReleases list releases of a repository
