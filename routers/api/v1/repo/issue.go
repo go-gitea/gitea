c9bd49a0a35de2bd14377571e771f7cfc0403a78
kolaente <konrad@kola-entertainments.de>
2018-03-28 23:08:25 +0200

    Deleted unnessecary license headers

--- routers/api/v1/repo/issue.go
+++ routers/api/v1/repo/issue.go
@@ -2,10 +2,6 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
-// Copyright 2018 The Gitea Authors. All rights reserved.
-// Use of this source code is governed by a MIT-style
-// license that can be found in the LICENSE file.
-
 package repo
 
 import (
