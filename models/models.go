d877bf7e15ebd5d9a74e58f3999e3276e76fa15e
Lunny Xiao <xiaolunwen@gmail.com>
2018-03-30 22:49:46 +0800

    Add config option to enable or disable log executed SQL (#3726)
    * add config option to enable or disable log executed SQL
    
    * rename ShowSQL to LogSQL

--- models/models.go
+++ models/models.go
@@ -270,7 +270,7 @@ func SetEngine() (err error) {
 	// WARNING: for serv command, MUST remove the output to os.stdout,
 	// so use log file to instead print to stdout.
 	x.SetLogger(log.XORMLogger)
-	x.ShowSQL(true)
+	x.ShowSQL(setting.LogSQL)
 	return nil
 }
 
