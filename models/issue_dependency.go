29dbc1748230f887b292157dbb071bd394b38180
kolaente <konrad@kola-entertainments.de>
2018-04-13 15:15:03 +0200

    removed unused import

--- models/issue_dependency.go
+++ models/issue_dependency.go
@@ -8,7 +8,6 @@ import (
 	"code.gitea.io/gitea/modules/log"
 	"code.gitea.io/gitea/modules/setting"
 	"code.gitea.io/gitea/modules/util"
-	"fmt"
 )
 
 // IssueDependency represents an issue dependency
