288aede62fe77619b1955dd5f4f532eeb118cd3a
Tris Forster <tris.git@tfconsulting.com.au>
2018-04-19 14:22:09 +1000

    Added FollowLink() method to TreeEntry (#114)
    Signed-off-by: Tris Forster <tris.git@shoddynet.org>

--- tree_entry_test.go
+++ tree_entry_test.go
@@ -50,3 +50,43 @@ func TestEntriesCustomSort(t *testing.T) {
 	assert.Equal(t, "bcd", entries[6].Name())
 	assert.Equal(t, "abc", entries[7].Name())
 }
+
+func TestFollowLink(t *testing.T) {
+	r, err := OpenRepository("tests/repos/repo1_bare")
+	assert.NoError(t, err)
+
+	commit, err := r.GetCommit("6fbd69e9823458e6c4a2fc5c0f6bc022b2f2acd1")
+	assert.NoError(t, err)
+
+	// get the symlink
+	lnk, err := commit.Tree.GetTreeEntryByPath("foo/bar/link_to_hello")
+	assert.NoError(t, err)
+	assert.True(t, lnk.IsLink())
+
+	// should be able to dereference to target
+	target, err := lnk.FollowLink()
+	assert.NoError(t, err)
+	assert.Equal(t, target.Name(), "hello")
+	assert.False(t, target.IsLink())
+	assert.Equal(t, target.ID.String(), "b14df6442ea5a1b382985a6549b85d435376c351")
+
+	// should error when called on normal file
+	target, err = commit.Tree.GetTreeEntryByPath("file1.txt")
+	assert.NoError(t, err)
+	_, err = target.FollowLink()
+	assert.Equal(t, err.Error(), "file1.txt: not a symlink")
+
+	// should error for broken links
+	target, err = commit.Tree.GetTreeEntryByPath("foo/broken_link")
+	assert.NoError(t, err)
+	assert.True(t, target.IsLink())
+	_, err = target.FollowLink()
+	assert.Equal(t, err.Error(), "broken_link: broken link")
+
+	// should error for external links
+	target, err = commit.Tree.GetTreeEntryByPath("foo/outside_repo")
+	assert.NoError(t, err)
+	assert.True(t, target.IsLink())
+	_, err = target.FollowLink()
+	assert.Equal(t, err.Error(), "outside_repo: points outside of repo")
+}
