85a1e3b653933606b9f829980c12aa43f149cfda
Lauris BH <lauris@nix.lv>
2018-05-02 12:35:45 +0300

    Do not allow inactive users to access repositories using private keys

--- cmd/serv.go
+++ cmd/serv.go
@@ -230,6 +230,12 @@ func runServ(c *cli.Context) error {
 				fail("internal error", "Failed to get user by key ID(%d): %v", keyID, err)
 			}
 
+			if !user.IsActive {
+				fail("Your account is not active",
+					"User %s is disabled and have no access to repository %s",
+					user.Name, repoPath)
+			}
+
 			mode, err := models.AccessLevel(user.ID, repo)
 			if err != nil {
 				fail("Internal error", "Failed to check access: %v", err)
