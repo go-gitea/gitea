go-gitea/gitea

commit 24941a10464dc27eaebafda2a208fa827b74ff8d

Add more webhooks support and refactor webhook templates directory (#3929)
* add more webhook support

* move hooks templates to standalone dir and add more webhooks ui

* fix tests

* update vendor checksum

* add more webhook support

* move hooks templates to standalone dir and add more webhooks ui

* fix tests

* update vendor checksum

* update vendor

Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

* load attributes when created release

* update comparsion doc

routers/repo/webhook.go
@@ -23,9 +23,9 @@ import (
 )
 
 const (
-	tplHooks      base.TplName = "repo/settings/hooks"
-	tplHookNew    base.TplName = "repo/settings/hook_new"
-	tplOrgHookNew base.TplName = "org/settings/hook_new"
+	tplHooks      base.TplName = "repo/settings/webhook/base"
+	tplHookNew    base.TplName = "repo/settings/webhook/new"
+	tplOrgHookNew base.TplName = "org/settings/webhook/new"
 )
 
 // Webhooks render web hooks list page
@@ -118,10 +118,15 @@ func ParseHookEvent(form auth.WebhookForm) *models.HookEvent {
 		SendEverything: form.SendEverything(),
 		ChooseEvents:   form.ChooseEvents(),
 		HookEvents: models.HookEvents{
-			Create:      form.Create,
-			Push:        form.Push,
-			PullRequest: form.PullRequest,
-			Repository:  form.Repository,
+			Create:       form.Create,
+			Delete:       form.Delete,
+			Fork:         form.Fork,
+			Issues:       form.Issues,
+			IssueComment: form.IssueComment,
+			Release:      form.Release,
+			Push:         form.Push,
+			PullRequest:  form.PullRequest,
+			Repository:   form.Repository,
 		},
 	}
 }

models/webhook.go
@@ -66,10 +66,15 @@ func IsValidHookContentType(name string) bool {
 
 // HookEvents is a set of web hook events
 type HookEvents struct {
-	Create      bool `json:"create"`
-	Push        bool `json:"push"`
-	PullRequest bool `json:"pull_request"`
-	Repository  bool `json:"repository"`
+	Create       bool `json:"create"`
+	Delete       bool `json:"delete"`
+	Fork         bool `json:"fork"`
+	Issues       bool `json:"issues"`
+	IssueComment bool `json:"issue_comment"`
+	Push         bool `json:"push"`
+	PullRequest  bool `json:"pull_request"`
+	Repository   bool `json:"repository"`
+	Release      bool `json:"release"`
 }
 
 // HookEvent represents events that will delivery hook.
@@ -155,6 +160,30 @@ func (w *Webhook) HasCreateEvent() bool {
 		(w.ChooseEvents && w.HookEvents.Create)
 }
 
+// HasDeleteEvent returns true if hook enabled delete event.
+func (w *Webhook) HasDeleteEvent() bool {
+	return w.SendEverything ||
+		(w.ChooseEvents && w.HookEvents.Delete)
+}
+
+// HasForkEvent returns true if hook enabled fork event.
+func (w *Webhook) HasForkEvent() bool {
+	return w.SendEverything ||
+		(w.ChooseEvents && w.HookEvents.Fork)
+}
+
+// HasIssuesEvent returns true if hook enabled issues event.
+func (w *Webhook) HasIssuesEvent() bool {
+	return w.SendEverything ||
+		(w.ChooseEvents && w.HookEvents.Issues)
+}
+
+// HasIssueCommentEvent returns true if hook enabled issue_comment event.
+func (w *Webhook) HasIssueCommentEvent() bool {
+	return w.SendEverything ||
+		(w.ChooseEvents && w.HookEvents.IssueComment)
+}
+
 // HasPushEvent returns true if hook enabled push event.
 func (w *Webhook) HasPushEvent() bool {
 	return w.PushOnly || w.SendEverything ||
@@ -167,23 +196,46 @@ func (w *Webhook) HasPullRequestEvent() bool {
 		(w.ChooseEvents && w.HookEvents.PullRequest)
 }
 
+// HasReleaseEvent returns if hook enabled release event.
+func (w *Webhook) HasReleaseEvent() bool {
+	return w.SendEverything ||
+		(w.ChooseEvents && w.HookEvents.Release)
+}
+
 // HasRepositoryEvent returns if hook enabled repository event.
 func (w *Webhook) HasRepositoryEvent() bool {
 	return w.SendEverything ||
 		(w.ChooseEvents && w.HookEvents.Repository)
 }
 
+func (w *Webhook) eventCheckers() []struct {
+	has func() bool
+	typ HookEventType
+} {
+	return []struct {
+		has func() bool
+		typ HookEventType
+	}{
+		{w.HasCreateEvent, HookEventCreate},
+		{w.HasDeleteEvent, HookEventDelete},
+		{w.HasForkEvent, HookEventFork},
+		{w.HasPushEvent, HookEventPush},
+		{w.HasIssuesEvent, HookEventIssues},
+		{w.HasIssueCommentEvent, HookEventIssueComment},
+		{w.HasPullRequestEvent, HookEventPullRequest},
+		{w.HasRepositoryEvent, HookEventRepository},
+		{w.HasReleaseEvent, HookEventRelease},
+	}
+}
+
 // EventsArray returns an array of hook events
 func (w *Webhook) EventsArray() []string {
-	events := make([]string, 0, 3)
-	if w.HasCreateEvent() {
-		events = append(events, "create")
-	}
-	if w.HasPushEvent() {
-		events = append(events, "push")
-	}
-	if w.HasPullRequestEvent() {
-		events = append(events, "pull_request")
+	events := make([]string, 0, 7)
+
+	for _, c := range w.eventCheckers() {
+		if c.has() {
+			events = append(events, string(c.typ))
+		}
 	}
 	return events
 }
@@ -373,10 +425,15 @@ type HookEventType string
 
 // Types of hook events
 const (
-	HookEventCreate      HookEventType = "create"
-	HookEventPush        HookEventType = "push"
-	HookEventPullRequest HookEventType = "pull_request"
-	HookEventRepository  HookEventType = "repository"
+	HookEventCreate       HookEventType = "create"
+	HookEventDelete       HookEventType = "delete"
+	HookEventFork         HookEventType = "fork"
+	HookEventPush         HookEventType = "push"
+	HookEventIssues       HookEventType = "issues"
+	HookEventIssueComment HookEventType = "issue_comment"
+	HookEventPullRequest  HookEventType = "pull_request"
+	HookEventRepository   HookEventType = "repository"
+	HookEventRelease      HookEventType = "release"
 )
 
 // HookRequest represents hook task request information.
@@ -488,22 +545,11 @@ func PrepareWebhook(w *Webhook, repo *Repository, event HookEventType, p api.Pay
 }
 
 func prepareWebhook(e Engine, w *Webhook, repo *Repository, event HookEventType, p api.Payloader) error {
-	switch event {
-	case HookEventCreate:
-		if !w.HasCreateEvent() {
-			return nil
-		}
-	case HookEventPush:
-		if !w.HasPushEvent() {
-			return nil
-		}
-	case HookEventPullRequest:
-		if !w.HasPullRequestEvent() {
-			return nil
-		}
-	case HookEventRepository:
-		if !w.HasRepositoryEvent() {
-			return nil
+	for _, e := range w.eventCheckers() {
+		if event == e.typ {
+			if !e.has() {
+				return nil
+			}
 		}
 	}
 

models/action.go
@@ -618,6 +618,16 @@ func CommitRepoAction(opts CommitRepoActionOptions) error {
 	case ActionDeleteBranch: // Delete Branch
 		isHookEventPush = true
 
+		if err = PrepareWebhooks(repo, HookEventDelete, &api.DeletePayload{
+			Ref:        refName,
+			RefType:    "branch",
+			PusherType: api.PusherTypeUser,
+			Repo:       apiRepo,
+			Sender:     apiPusher,
+		}); err != nil {
+			return fmt.Errorf("PrepareWebhooks.(delete branch): %v", err)
+		}
+
 	case ActionPushTag: // Create
 		isHookEventPush = true
 
@@ -640,6 +650,16 @@ func CommitRepoAction(opts CommitRepoActionOptions) error {
 		}
 	case ActionDeleteTag: // Delete Tag
 		isHookEventPush = true
+
+		if err = PrepareWebhooks(repo, HookEventDelete, &api.DeletePayload{
+			Ref:        refName,
+			RefType:    "tag",
+			PusherType: api.PusherTypeUser,
+			Repo:       apiRepo,
+			Sender:     apiPusher,
+		}); err != nil {
+			return fmt.Errorf("PrepareWebhooks.(delete tag): %v", err)
+		}
 	}
 
 	if isHookEventPush {

models/issue_comment.go
@@ -83,9 +83,10 @@ const (
 type Comment struct {
 	ID              int64 `xorm:"pk autoincr"`
 	Type            CommentType
-	PosterID        int64 `xorm:"INDEX"`
-	Poster          *User `xorm:"-"`
-	IssueID         int64 `xorm:"INDEX"`
+	PosterID        int64  `xorm:"INDEX"`
+	Poster          *User  `xorm:"-"`
+	IssueID         int64  `xorm:"INDEX"`
+	Issue           *Issue `xorm:"-"`
 	LabelID         int64
 	Label           *Label `xorm:"-"`
 	OldMilestoneID  int64
@@ -114,6 +115,15 @@ type Comment struct {
 
 	// For view issue page.
 	ShowTag CommentTag `xorm:"-"`
+}
+
+// LoadIssue loads issue from database
+func (c *Comment) LoadIssue() (err error) {
+	if c.Issue != nil {
+		return nil
+	}
+	c.Issue, err = GetIssueByID(c.IssueID)
+	return
 }
 
 // AfterLoad is invoked from XORM after setting the values of all fields of this object.
@@ -146,40 +156,40 @@ func (c *Comment) AfterDelete() {
 
 // HTMLURL formats a URL-string to the issue-comment
 func (c *Comment) HTMLURL() string {
-	issue, err := GetIssueByID(c.IssueID)
+	err := c.LoadIssue()
 	if err != nil { // Silently dropping errors :unamused:
-		log.Error(4, "GetIssueByID(%d): %v", c.IssueID, err)
+		log.Error(4, "LoadIssue(%d): %v", c.IssueID, err)
 		return ""
 	}
-	return fmt.Sprintf("%s#%s", issue.HTMLURL(), c.HashTag())
+	return fmt.Sprintf("%s#%s", c.Issue.HTMLURL(), c.HashTag())
 }
 
 // IssueURL formats a URL-string to the issue
 func (c *Comment) IssueURL() string {
-	issue, err := GetIssueByID(c.IssueID)
+	err := c.LoadIssue()
 	if err != nil { // Silently dropping errors :unamused:
-		log.Error(4, "GetIssueByID(%d): %v", c.IssueID, err)
+		log.Error(4, "LoadIssue(%d): %v", c.IssueID, err)
 		return ""
 	}
 
-	if issue.IsPull {
+	if c.Issue.IsPull {
 		return ""
 	}
-	return issue.HTMLURL()
+	return c.Issue.HTMLURL()
 }
 
 // PRURL formats a URL-string to the pull-request
 func (c *Comment) PRURL() string {
-	issue, err := GetIssueByID(c.IssueID)
+	err := c.LoadIssue()
 	if err != nil { // Silently dropping errors :unamused:
-		log.Error(4, "GetIssueByID(%d): %v", c.IssueID, err)
+		log.Error(4, "LoadIssue(%d): %v", c.IssueID, err)
 		return ""
 	}
 
-	if !issue.IsPull {
+	if !c.Issue.IsPull {
 		return ""
 	}
-	return issue.HTMLURL()
+	return c.Issue.HTMLURL()
 }
 
 // APIFormat converts a Comment to the api.Comment format
@@ -196,9 +206,14 @@ func (c *Comment) APIFormat() *api.Comment {
 	}
 }
 
+// CommentHashTag returns unique hash tag for comment id.
+func CommentHashTag(id int64) string {
+	return fmt.Sprintf("issuecomment-%d", id)
+}
+
 // HashTag returns unique hash tag for comment.
 func (c *Comment) HashTag() string {
-	return "issuecomment-" + com.ToStr(c.ID)
+	return CommentHashTag(c.ID)
 }
 
 // EventTag returns unique event hash tag for comment.
@@ -576,7 +591,7 @@ func CreateComment(opts *CreateCommentOptions) (comment *Comment, err error) {
 
 // CreateIssueComment creates a plain issue comment.
 func CreateIssueComment(doer *User, repo *Repository, issue *Issue, content string, attachments []string) (*Comment, error) {
-	return CreateComment(&CreateCommentOptions{
+	comment, err := CreateComment(&CreateCommentOptions{
 		Type:        CommentTypeComment,
 		Doer:        doer,
 		Repo:        repo,
@@ -584,6 +599,21 @@ func CreateIssueComment(doer *User, repo *Repository, issue *Issue, content stri
 		Content:     content,
 		Attachments: attachments,
 	})
+	if err != nil {
+		return nil, fmt.Errorf("CreateComment: %v", err)
+	}
+
+	mode, _ := AccessLevel(doer.ID, repo)
+	if err = PrepareWebhooks(repo, HookEventIssueComment, &api.IssueCommentPayload{
+		Action:     api.HookIssueCommentCreated,
+		Issue:      issue.APIFormat(),
+		Comment:    comment.APIFormat(),
+		Repository: repo.APIFormat(mode),
+		Sender:     doer.APIFormat(),
+	}); err != nil {
+		log.Error(2, "PrepareWebhooks [comment_id: %d]: %v", comment.ID, err)
+	}
+	return comment, nil
 }
 
 // CreateRefComment creates a commit reference comment to issue.
@@ -696,17 +726,41 @@ func GetCommentsByRepoIDSince(repoID, since int64) ([]*Comment, error) {
 }
 
 // UpdateComment updates information of comment.
-func UpdateComment(c *Comment) error {
+func UpdateComment(doer *User, c *Comment, oldContent string) error {
 	if _, err := x.ID(c.ID).AllCols().Update(c); err != nil {
 		return err
 	} else if c.Type == CommentTypeComment {
 		UpdateIssueIndexer(c.IssueID)
 	}
+
+	if err := c.LoadIssue(); err != nil {
+		return err
+	}
+	if err := c.Issue.LoadAttributes(); err != nil {
+		return err
+	}
+
+	mode, _ := AccessLevel(doer.ID, c.Issue.Repo)
+	if err := PrepareWebhooks(c.Issue.Repo, HookEventIssueComment, &api.IssueCommentPayload{
+		Action:  api.HookIssueCommentEdited,
+		Issue:   c.Issue.APIFormat(),
+		Comment: c.APIFormat(),
+		Changes: &api.ChangesPayload{
+			Body: &api.ChangesFromPayload{
+				From: oldContent,
+			},
+		},
+		Repository: c.Issue.Repo.APIFormat(mode),
+		Sender:     doer.APIFormat(),
+	}); err != nil {
+		log.Error(2, "PrepareWebhooks [comment_id: %d]: %v", c.ID, err)
+	}
+
 	return nil
 }
 
 // DeleteComment deletes the comment
-func DeleteComment(comment *Comment) error {
+func DeleteComment(doer *User, comment *Comment) error {
 	sess := x.NewSession()
 	defer sess.Close()
 	if err := sess.Begin(); err != nil {
@@ -733,5 +787,25 @@ func DeleteComment(comment *Comment) error {
 	} else if comment.Type == CommentTypeComment {
 		UpdateIssueIndexer(comment.IssueID)
 	}
+
+	if err := comment.LoadIssue(); err != nil {
+		return err
+	}
+	if err := comment.Issue.LoadAttributes(); err != nil {
+		return err
+	}
+
+	mode, _ := AccessLevel(doer.ID, comment.Issue.Repo)
+
+	if err := PrepareWebhooks(comment.Issue.Repo, HookEventIssueComment, &api.IssueCommentPayload{
+		Action:     api.HookIssueCommentDeleted,
+		Issue:      comment.Issue.APIFormat(),
+		Comment:    comment.APIFormat(),
+		Repository: comment.Issue.Repo.APIFormat(mode),
+		Sender:     doer.APIFormat(),
+	}); err != nil {
+		log.Error(2, "PrepareWebhooks [comment_id: %d]: %v", comment.ID, err)
+	}
+
 	return nil
 }

models/issue_milestone.go
@@ -5,6 +5,9 @@
 package models
 
 import (
+	"fmt"
+
+	"code.gitea.io/gitea/modules/log"
 	"code.gitea.io/gitea/modules/setting"
 	"code.gitea.io/gitea/modules/util"
 	api "code.gitea.io/sdk/gitea"
@@ -358,7 +361,49 @@ func ChangeMilestoneAssign(issue *Issue, doer *User, oldMilestoneID int64) (err 
 	if err = changeMilestoneAssign(sess, doer, issue, oldMilestoneID); err != nil {
 		return err
 	}
-	return sess.Commit()
+
+	if err = sess.Commit(); err != nil {
+		return fmt.Errorf("Commit: %v", err)
+	}
+
+	var hookAction api.HookIssueAction
+	if issue.MilestoneID > 0 {
+		hookAction = api.HookIssueMilestoned
+	} else {
+		hookAction = api.HookIssueDemilestoned
+	}
+
+	if err = issue.LoadAttributes(); err != nil {
+		return err
+	}
+
+	mode, _ := AccessLevel(doer.ID, issue.Repo)
+	if issue.IsPull {
+		err = issue.PullRequest.LoadIssue()
+		if err != nil {
+			log.Error(2, "LoadIssue: %v", err)
+			return
+		}
+		err = PrepareWebhooks(issue.Repo, HookEventPullRequest, &api.PullRequestPayload{
+			Action:      hookAction,
+			Index:       issue.Index,
+			PullRequest: issue.PullRequest.APIFormat(),
+			Repository:  issue.Repo.APIFormat(mode),
+			Sender:      doer.APIFormat(),
+		})
+	} else {
+		err = PrepareWebhooks(issue.Repo, HookEventIssues, &api.IssuePayload{
+			Action:     hookAction,
+			Index:      issue.Index,
+			Issue:      issue.APIFormat(),
+			Repository: issue.Repo.APIFormat(mode),
+			Sender:     doer.APIFormat(),
+		})
+	}
+	if err != nil {
+		log.Error(2, "PrepareWebhooks [is_pull: %v]: %v", issue.IsPull, err)
+	}
+	return nil
 }
 
 // DeleteMilestoneByRepoID deletes a milestone from a repository.

models/issue_milestone_test.go
@@ -232,6 +232,8 @@ func TestChangeMilestoneAssign(t *testing.T) {
 	assert.NoError(t, PrepareTestDatabase())
 	issue := AssertExistsAndLoadBean(t, &Issue{RepoID: 1}).(*Issue)
 	doer := AssertExistsAndLoadBean(t, &User{ID: 2}).(*User)
+	assert.NotNil(t, issue)
+	assert.NotNil(t, doer)
 
 	oldMilestoneID := issue.MilestoneID
 	issue.MilestoneID = 2

models/release.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 
 	"code.gitea.io/git"
+	"code.gitea.io/gitea/modules/log"
 	"code.gitea.io/gitea/modules/process"
 	"code.gitea.io/gitea/modules/setting"
 	"code.gitea.io/gitea/modules/util"
@@ -190,8 +191,27 @@ func CreateRelease(gitRepo *git.Repository, rel *Release, attachmentUUIDs []stri
 	}
 
 	err = addReleaseAttachments(rel.ID, attachmentUUIDs)
+	if err != nil {
+		return err
+	}
 
-	return err
+	if !rel.IsDraft {
+		if err := rel.LoadAttributes(); err != nil {
+			log.Error(2, "LoadAttributes: %v", err)
+		} else {
+			mode, _ := AccessLevel(rel.PublisherID, rel.Repo)
+			if err := PrepareWebhooks(rel.Repo, HookEventRelease, &api.ReleasePayload{
+				Action:     api.HookReleasePublished,
+				Release:    rel.APIFormat(),
+				Repository: rel.Repo.APIFormat(mode),
+				Sender:     rel.Publisher.APIFormat(),
+			}); err != nil {
+				log.Error(2, "PrepareWebhooks: %v", err)
+			}
+		}
+	}
+
+	return nil
 }
 
 // GetRelease returns release by given ID.

models/repo.go
@@ -2456,6 +2456,17 @@ func ForkRepository(doer, u *User, oldRepo *Repository, name, desc string) (_ *R
 		return nil, err
 	}
 
+	oldMode, _ := AccessLevel(doer.ID, oldRepo)
+	mode, _ := AccessLevel(doer.ID, repo)
+
+	if err = PrepareWebhooks(oldRepo, HookEventFork, &api.ForkPayload{
+		Forkee: repo.APIFormat(mode),
+		Repo:   oldRepo.APIFormat(oldMode),
+		Sender: doer.APIFormat(),
+	}); err != nil {
+		log.Error(2, "PrepareWebhooks [repo_id: %d]: %v", oldRepo.ID, err)
+	}
+
 	if err = repo.UpdateSize(); err != nil {
 		log.Error(4, "Failed to update size for repository: %v", err)
 	}

commit ec25b95d517461b1ebaba0659f70ae0512c17bba

fix bugs caused by #3929

routers/repo/webhook.go
@@ -25,7 +25,7 @@ import (
 const (
 	tplHooks      base.TplName = "repo/settings/webhook/base"
 	tplHookNew    base.TplName = "repo/settings/webhook/new"
-	tplOrgHookNew base.TplName = "org/settings/webhook/new"
+	tplOrgHookNew base.TplName = "org/settings/hook_new"
 )
 
 // Webhooks render web hooks list page

templates/org/settings/hook_new.tmpl
@@ -23,14 +23,14 @@
 					</div>
 				</h4>
 				<div class="ui attached segment">
-					{{template "repo/settings/hook_gitea" .}}
-					{{template "repo/settings/hook_gogs" .}}
-					{{template "repo/settings/hook_slack" .}}
-					{{template "repo/settings/hook_discord" .}}
-					{{template "repo/settings/hook_dingtalk" .}}
+					{{template "repo/settings/webhook/gitea" .}}
+					{{template "repo/settings/webhook/gogs" .}}
+					{{template "repo/settings/webhook/slack" .}}
+					{{template "repo/settings/webhook/discord" .}}
+					{{template "repo/settings/webhook/dingtalk" .}}
 				</div>
 
-				{{template "repo/settings/hook_history" .}}
+				{{template "repo/settings/webhook/history" .}}
 			</div>
 		</div>
 	</div>

templates/org/settings/hooks.tmpl
@@ -5,7 +5,7 @@
 		<div class="ui grid">
 			{{template "org/settings/navbar" .}}
 			<div class="twelve wide column content">
-				{{template "repo/settings/hook_list" .}}
+				{{template "repo/settings/webhook/list" .}}
 			</div>
 		</div>
 	</div>