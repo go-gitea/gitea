ee67d054268afa72f382185440f8903c885bef44
Steve Traugott <stevegt@t7a.org>
2018-05-30 08:25:46 -0700

    add simple descriptions for swagger validate
    * Partial fix for #4010
    
    Swagger needs a comment line above each swagger:response comment -- it
    uses these to populate the description: fields.  Adding minimal text
    for now on the way to getting swagger validate to pass.  Many standard
    swagger client libraries will not work at all with gitea until validate
    passes, so prioritizing that over better descriptions for now.
    
    Signed-off-by: Steve Traugott <stevegt@t7a.org>

--- routers/api/v1/swagger/repo.go
+++ routers/api/v1/swagger/repo.go
@@ -8,95 +8,111 @@ import (
 	api "code.gitea.io/sdk/gitea"
 )
 
+// Repository
 // swagger:response Repository
 type swaggerResponseRepository struct {
 	// in:body
 	Body api.Repository `json:"body"`
 }
 
+// RepositoryList
 // swagger:response RepositoryList
 type swaggerResponseRepositoryList struct {
 	// in:body
 	Body []api.Repository `json:"body"`
 }
 
+// Branch
 // swagger:response Branch
 type swaggerResponseBranch struct {
 	// in:body
 	Body api.Branch `json:"body"`
 }
 
+// BranchList
 // swagger:response BranchList
 type swaggerResponseBranchList struct {
 	// in:body
 	Body []api.Branch `json:"body"`
 }
 
+// Hook
 // swagger:response Hook
 type swaggerResponseHook struct {
 	// in:body
 	Body []api.Branch `json:"body"`
 }
 
+// HookList
 // swagger:response HookList
 type swaggerResponseHookList struct {
 	// in:body
 	Body []api.Branch `json:"body"`
 }
 
+// Release
 // swagger:response Release
 type swaggerResponseRelease struct {
 	// in:body
 	Body api.Release `json:"body"`
 }
 
+// ReleaseList
 // swagger:response ReleaseList
 type swaggerResponseReleaseList struct {
 	// in:body
 	Body []api.Release `json:"body"`
 }
 
+// PullRequest
 // swagger:response PullRequest
 type swaggerResponsePullRequest struct {
 	// in:body
 	Body api.PullRequest `json:"body"`
 }
 
+// PullRequestList
 // swagger:response PullRequestList
 type swaggerResponsePullRequestList struct {
 	// in:body
 	Body []api.PullRequest `json:"body"`
 }
 
+// Status
 // swagger:response Status
 type swaggerResponseStatus struct {
 	// in:body
 	Body api.Status `json:"body"`
 }
 
+// StatusList
 // swagger:response StatusList
 type swaggerResponseStatusList struct {
 	// in:body
 	Body []api.Status `json:"body"`
 }
 
+// WatchInfo
 // swagger:response WatchInfo
 type swaggerResponseWatchInfo struct {
 	// in:body
 	Body api.WatchInfo `json:"body"`
 }
 
+// SearchResults
 // swagger:response SearchResults
 type swaggerResponseSearchResults struct {
 	Body api.SearchResults `json:"body"`
 }
 
+// AttachmentList
 // swagger:response AttachmentList
 type swaggerResponseAttachmentList struct {
 	//in: body
 	Body []api.Attachment `json:"body"`
 }
 
+// Attachment
 // swagger:response Attachment
 type swaggerResponseAttachment struct {
 	//in: body
