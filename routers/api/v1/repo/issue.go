ca83a546514fac39c57c2f515669a69e80efb0d2
kolaente <konrad@kola-entertainments.de>
2018-06-12 08:31:00 +0200

    added documentation for error codes

--- routers/api/v1/repo/issue.go
+++ routers/api/v1/repo/issue.go
@@ -363,6 +363,14 @@ func UpdateIssueDeadline(ctx *context.APIContext, form api.EditDeadlineOption) {
 	// responses:
 	//   "201":
 	//     "$ref": "#/responses/IssueDeadline"
+	//   "403":
+	//     description: Not repo writer
+	//     schema:
+	//       "$ref": "#/responses/forbidden"
+	//   "404":
+	//     description: Issue not found
+	//     schema:
+	//       "$ref": "#/responses/empty"
 
 	issue, err := models.GetIssueByIndex(ctx.Repo.Repository.ID, ctx.ParamsInt64(":index"))
 	if err != nil {
