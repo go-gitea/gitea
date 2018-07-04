ab249704dd8697755af60f3011da3e3aed926c94
tarelda <lukasz@jarosz.in>
2018-07-04 02:09:55 +0200

    Keep preseeded database password (#4284)

--- routers/install.go
+++ routers/install.go
@@ -58,6 +58,7 @@ func Install(ctx *context.Context) {
 	// Database settings
 	form.DbHost = models.DbCfg.Host
 	form.DbUser = models.DbCfg.User
+	form.DbPasswd = models.DbCfg.Passwd
 	form.DbName = models.DbCfg.Name
 	form.DbPath = models.DbCfg.Path
 
