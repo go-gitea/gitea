5cb540c48179d85786830aa60f73ce19fddf256e
Will <william.le-pommelet@capgemini.com>
2018-07-19 08:34:26 +0200

    fix language list
    Standardized the languages' names within the list displayed when picking up a language at the bottom of the webpages

--- modules/setting/defaults.go
+++ modules/setting/defaults.go
@@ -6,5 +6,5 @@ import (
 
 var (
 	defaultLangs     = strings.Split("en-US,zh-CN,zh-HK,zh-TW,de-DE,fr-FR,nl-NL,lv-LV,ru-RU,uk-UA,ja-JP,es-ES,pt-BR,pl-PL,bg-BG,it-IT,fi-FI,tr-TR,cs-CZ,sr-SP,sv-SE,ko-KR", ",")
-	defaultLangNames = strings.Split("English,简体中文,繁體中文（香港）,繁體中文（台灣）,Deutsch,français,Nederlands,latviešu,русский,Українська,日本語,español,português do Brasil,polski,български,italiano,suomi,Türkçe,čeština,српски,svenska,한국어", ",")
+	defaultLangNames = strings.Split("English,简体中文,繁體中文（香港）,繁體中文（台灣）,Deutsch,Français,Nederlands,Latviešu,Pусский,Українська,日本語,Español,Português do Brasil,Polski,български,Italiano,Suomi,Türkçe,Čeština,Cрпски,Svenska,한국어", ",")
 )
