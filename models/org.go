6b915551f0a62b410988b326c9078fb69ba01051
Sage Pointer <sageptr@gmail.com>
2018-07-26 19:53:48 +0200

    Fix out-of-transaction query in removeOrgUser (#4521)

--- models/org.go
+++ models/org.go
@@ -454,7 +454,7 @@ func AddOrgUser(orgID, uid int64) error {
 func removeOrgUser(sess *xorm.Session, orgID, userID int64) error {
 	ou := new(OrgUser)
 
-	has, err := x.
+	has, err := sess.
 		Where("uid=?", userID).
 		And("org_id=?", orgID).
 		Get(ou)
