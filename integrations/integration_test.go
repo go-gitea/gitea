fe457315b0dea7d854a87d9ea5cb34c9e1c529f0
techknowlogick <techknowlogick@users.noreply.github.com>
2018-09-05 15:23:15 -0400

    update function

--- integrations/integration_test.go
+++ integrations/integration_test.go
@@ -223,22 +223,22 @@ func loginUserWithPassword(t testing.TB, userName, password string) *TestSession
 	return session
 }
 
-func getTokenForLoggedInUser(t testing.TB) (*TestSession, string) {
+func getTokenForLoggedInUser(t testing.TB, session *TestSession) (string) {
 	req := NewRequest(t, "GET", "/user/settings/applications")
-	resp := MakeRequest(t, req, http.StatusOK)
+	resp := session.MakeRequest(t, req, http.StatusOK)
 	doc := NewHTMLParser(t, resp.Body)
 	req = NewRequestWithValues(t, "POST", "/user/settings/applications", map[string]string{
 		"_csrf": doc.GetCSRF(),
 		"name":  "api-testing-token",
 	})
-	resp = MakeRequest(t, req, http.StatusFound)
+	resp = session.MakeRequest(t, req, http.StatusFound)
 	htmlDoc := NewHTMLParser(t, resp.Body)
 	if err != nil {
 		return t, "" // probably a non-HTML response
 	}
 	token := htmlDoc.Find(".ui.info.message p").Text()
 
-	return t, token
+	return token
 }
 
 func NewRequest(t testing.TB, method, urlStr string) *http.Request {
