5787fd2b14f26cc083cb590a825ace8bb5c61549
techknowlogick <techknowlogick@users.noreply.github.com>
2018-09-05 16:07:08 -0400

    Update integration_test.go

--- integrations/integration_test.go
+++ integrations/integration_test.go
@@ -233,7 +233,7 @@ func getTokenForLoggedInUser(t testing.TB, session *TestSession) string {
 	})
 	resp = session.MakeRequest(t, req, http.StatusFound)
 	htmlDoc := NewHTMLParser(t, resp.Body)
-	token := htmlDoc.doc.Find(".ui.info.message p").Text()
+	token := htmlDoc.doc.Find(".ui.info.message").Text()
 	fmt.Println(token)
 	return token
 }
