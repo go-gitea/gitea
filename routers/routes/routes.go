126ba796dcc9ccdf9c25ed7d441786478be2825b
Lanre Adelowo <adelowomailbox@gmail.com>
2018-09-13 13:04:25 +0100

    Force user to change password (#4489)
    * redirect to login page after successfully activating account
    
    * force users to change password if account was created by an admin
    
    * force users to change password if account was created by an admin
    
    * fixed build
    
    * fixed build
    
    * fix pending issues with translation and wrong routes
    
    * make sure path check is safe
    
    * remove unneccessary newline
    
    * make sure users that don't have to view the form get redirected
    
    * move route to use /settings prefix so as to make sure unauthenticated users can't view the page
    
    * update as per @lafriks review
    
    * add necessary comment
    
    * remove unrelated changes
    
    * support redirecting to location the user actually want to go to before being forced to change his/her password
    
    * run make fmt
    
    * added tests
    
    * improve assertions
    
    * add assertion
    
    * fix copyright year
    
    Signed-off-by: Lanre Adelowo <yo@lanre.wtf>

--- routers/routes/routes.go
+++ routers/routes/routes.go
@@ -230,6 +230,8 @@ func RegisterRoutes(m *macaron.Macaron) {
 	m.Group("/user/settings", func() {
 		m.Get("", userSetting.Profile)
 		m.Post("", bindIgnErr(auth.UpdateProfileForm{}), userSetting.ProfilePost)
+		m.Get("/change_password", user.MustChangePassword)
+		m.Post("/change_password", bindIgnErr(auth.MustChangePasswordForm{}), user.MustChangePasswordPost)
 		m.Post("/avatar", binding.MultipartForm(auth.AvatarForm{}), userSetting.AvatarPost)
 		m.Post("/avatar/delete", userSetting.DeleteAvatar)
 		m.Group("/account", func() {
