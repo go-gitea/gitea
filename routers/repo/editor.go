b365cd9a0140f567f6160d0279c359ffaddfeee6
kolaente <konrad@kola-entertainments.de>
2018-10-06 00:39:58 +0200

    Prevent adding/editing/removing of files via web ui when a repo is archived

--- routers/repo/editor.go
+++ routers/repo/editor.go
@@ -56,6 +56,12 @@ func getParentTreeFields(treePath string) (treeNames []string, treePaths []strin
 }
 
 func editFile(ctx *context.Context, isNewFile bool) {
+	// Don't allow editing if the repo is archived
+	if ctx.Repo.Repository.IsArchived {
+		ctx.NotFound("", nil)
+		return
+	}
+
 	ctx.Data["PageIsEdit"] = true
 	ctx.Data["IsNewFile"] = isNewFile
 	ctx.Data["RequireHighlightJS"] = true
@@ -147,6 +153,12 @@ func NewFile(ctx *context.Context) {
 }
 
 func editFilePost(ctx *context.Context, form auth.EditRepoFileForm, isNewFile bool) {
+	// Don't allow editing if the repo is archived
+	if ctx.Repo.Repository.IsArchived {
+		ctx.NotFound("", nil)
+		return
+	}
+
 	ctx.Data["PageIsEdit"] = true
 	ctx.Data["IsNewFile"] = isNewFile
 	ctx.Data["RequireHighlightJS"] = true
@@ -352,6 +364,12 @@ func DiffPreviewPost(ctx *context.Context, form auth.EditPreviewDiffForm) {
 
 // DeleteFile render delete file page
 func DeleteFile(ctx *context.Context) {
+	// Don't allow deleting if the repo is archived
+	if ctx.Repo.Repository.IsArchived {
+		ctx.NotFound("", nil)
+		return
+	}
+
 	ctx.Data["PageIsDelete"] = true
 	ctx.Data["BranchLink"] = ctx.Repo.RepoLink + "/src/" + ctx.Repo.BranchNameSubURL()
 	ctx.Data["TreePath"] = ctx.Repo.TreePath
@@ -371,6 +389,12 @@ func DeleteFile(ctx *context.Context) {
 
 // DeleteFilePost response for deleting file
 func DeleteFilePost(ctx *context.Context, form auth.DeleteRepoFileForm) {
+	// Don't allow deleting if the repo is archived
+	if ctx.Repo.Repository.IsArchived {
+		ctx.NotFound("", nil)
+		return
+	}
+
 	ctx.Data["PageIsDelete"] = true
 	ctx.Data["BranchLink"] = ctx.Repo.RepoLink + "/src/" + ctx.Repo.BranchNameSubURL()
 	ctx.Data["TreePath"] = ctx.Repo.TreePath
