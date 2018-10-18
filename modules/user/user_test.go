0f822610850b43067e50708694c9f41afe8db7d8
SagePtr <sageptr@gmail.com>
2018-10-18 09:08:20 +0200

    Use native go method to get current user rather than environment variable (#4930)
    * Use native go method to get current user rather than environment var
    
    * Use t.Skip instead of return in test

--- modules/user/user_test.go
+++ modules/user/user_test.go
@@ -2,21 +2,40 @@ package user
 
 import (
 	"os"
+	"os/exec"
+	"runtime"
+	"strings"
 	"testing"
 )
 
-func TestCurrentUsername(t *testing.T) {
-	os.Setenv("USER", "")
-	os.Setenv("USERNAME", "foobar")
-
-	user := CurrentUsername()
-	if user != "foobar" {
-		t.Errorf("expected foobar as user, got: %s", user)
+func getWhoamiOutput() (string, error) {
+	output, err := exec.Command("whoami").Output()
+	if err != nil {
+		return "", err
 	}
+	return strings.TrimSpace(string(output[:])), nil
+}
 
-	os.Setenv("USER", "gitea")
+func TestCurrentUsername(t *testing.T) {
+	user := CurrentUsername()
+	if len(user) <= 0 {
+		t.Errorf("expected non-empty user, got: %s", user)
+	}
+	// Windows whoami is weird, so just skip remaining tests
+	if runtime.GOOS == "windows" {
+		t.Skip("skipped test because of weird whoami on Windows")
+	}
+	whoami, err := getWhoamiOutput()
+	if err != nil {
+		t.Errorf("failed to run whoami to test current user: %f", err)
+	}
 	user = CurrentUsername()
-	if user != "gitea" {
-		t.Errorf("expected gitea as user, got: %s", user)
+	if user != whoami {
+		t.Errorf("expected %s as user, got: %s", whoami, user)
+	}
+	os.Setenv("USER", "spoofed")
+	user = CurrentUsername()
+	if user != whoami {
+		t.Errorf("expected %s as user, got: %s", whoami, user)
 	}
 }
