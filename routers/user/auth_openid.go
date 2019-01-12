6b08894f01a9e29f1373fc1a194fea5f09ef02ec
zeripath <art27@cantab.net>
2019-01-12 08:59:59 +0000

    Update auth_openid.go
    Place error log within the `err != nil` branch.

--- routers/user/auth_openid.go
+++ routers/user/auth_openid.go
@@ -114,8 +114,8 @@ func SignInOpenIDPost(ctx *context.Context, form auth.SignInOpenIDForm) {
 
 	redirectTo := setting.AppURL + "user/login/openid"
 	url, err := openid.RedirectURL(id, redirectTo, setting.AppURL)
-	log.Error(1, "Error in OpenID redirect URL: %s, %v", redirectTo, err.Error())
 	if err != nil {
+		log.Error(1, "Error in OpenID redirect URL: %s, %v", redirectTo, err.Error())
 		ctx.RenderWithErr(fmt.Sprintf("Unable to find OpenID provider in %s", redirectTo), tplSignInOpenID, &form)
 		return
 	}
