b3f3a13e7be4bbbb0963de2c5b62dae5019cbdc4
Lunny Xiao <xiaolunwen@gmail.com>
2019-01-13 19:05:33 +0800

    use NullNotifier as parent struct of notifiers

--- modules/notification/mail/mail.go
+++ modules/notification/mail/mail.go
@@ -5,13 +5,13 @@
 package mail
 
 import (
-	"code.gitea.io/git"
 	"code.gitea.io/gitea/models"
 	"code.gitea.io/gitea/modules/log"
 	"code.gitea.io/gitea/modules/notification/base"
 )
 
 type mailNotifier struct {
+	base.NullNotifier
 }
 
 var (
@@ -21,9 +21,6 @@ var (
 // NewNotifier create a new mailNotifier notifier
 func NewNotifier() base.Notifier {
 	return &mailNotifier{}
-}
-
-func (m *mailNotifier) Run() {
 }
 
 func (m *mailNotifier) NotifyCreateIssueComment(doer *models.User, repo *models.Repository,
@@ -74,53 +71,4 @@ func (m *mailNotifier) NotifyPullRequestReview(pr *models.PullRequest, r *models
 	if err := comment.MailParticipants(act, pr.Issue); err != nil {
 		log.Error(4, "MailParticipants: %v", err)
 	}
-}
-
-func (m *mailNotifier) NotifyMergePullRequest(pr *models.PullRequest, doer *models.User, baseRepo *git.Repository) {
-}
-
-func (m *mailNotifier) NotifyUpdateComment(doer *models.User, c *models.Comment, oldContent string) {
-}
-
-func (m *mailNotifier) NotifyDeleteComment(doer *models.User, c *models.Comment) {
-}
-
-func (m *mailNotifier) NotifyDeleteRepository(doer *models.User, repo *models.Repository) {
-}
-
-func (m *mailNotifier) NotifyForkRepository(doer *models.User, oldRepo, repo *models.Repository) {
-}
-
-func (m *mailNotifier) NotifyNewRelease(rel *models.Release) {
-}
-
-func (m *mailNotifier) NotifyUpdateRelease(doer *models.User, rel *models.Release) {
-}
-
-func (m *mailNotifier) NotifyDeleteRelease(doer *models.User, rel *models.Release) {
-}
-
-func (m *mailNotifier) NotifyIssueChangeMilestone(doer *models.User, issue *models.Issue) {
-}
-
-func (m *mailNotifier) NotifyIssueChangeContent(doer *models.User, issue *models.Issue, oldContent string) {
-}
-
-func (m *mailNotifier) NotifyIssueChangeAssignee(doer *models.User, issue *models.Issue, removed bool) {
-}
-
-func (m *mailNotifier) NotifyIssueClearLabels(doer *models.User, issue *models.Issue) {
-}
-
-func (m *mailNotifier) NotifyIssueChangeTitle(doer *models.User, issue *models.Issue, oldTitle string) {
-}
-
-func (m *mailNotifier) NotifyIssueChangeLabels(doer *models.User, issue *models.Issue,
-	addedLabels []*models.Label, removedLabels []*models.Label) {
-}
-
-func (m *mailNotifier) NotifyCreateRepository(doer *models.User, u *models.User, repo *models.Repository) {
-}
-
-func (m *mailNotifier) NotifyMigrateRepository(doer *models.User, u *models.User, repo *models.Repository) {
 }
