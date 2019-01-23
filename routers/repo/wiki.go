31c2cdc056747799874ba32a18b2cd95562e8499
Gabriel Silva Simões <simoes.sgabriel@gmail.com>
2019-01-23 15:18:54 -0500

    Fix serving of raw wiki files other than .md
    Closes #4690.
    Closes #4395.
    
    Signed-off-by: Gabriel Silva Simões <simoes.sgabriel@gmail.com>

--- routers/repo/wiki.go
+++ routers/repo/wiki.go
@@ -296,25 +296,41 @@ func WikiRaw(ctx *context.Context) {
 		}
 	}
 	providedPath := ctx.Params("*")
-	if strings.HasSuffix(providedPath, ".md") {
-		providedPath = providedPath[:len(providedPath)-3]
-	}
-	wikiPath := models.WikiNameToFilename(providedPath)
+
 	var entry *git.TreeEntry
 	if commit != nil {
+		// Try to find a file with that name
+		entry, err = findEntryForFile(commit, providedPath)
+		if err != nil {
+			ctx.ServerError("findFile", err)
+			return
+		} else if entry != nil {
+			if err = ServeBlob(ctx, entry.Blob()); err != nil {
+				ctx.ServerError("ServeBlob", err)
+			}
+			return
+		}
+
+		// Try to find a wiki page with that name
+		if strings.HasSuffix(providedPath, ".md") {
+			providedPath = providedPath[:len(providedPath)-3]
+		}
+
+		wikiPath := models.WikiNameToFilename(providedPath)
 		entry, err = findEntryForFile(commit, wikiPath)
-	}
-	if err != nil {
-		ctx.ServerError("findFile", err)
-		return
-	} else if entry == nil {
-		ctx.NotFound("findEntryForFile", nil)
-		return
+		if err != nil {
+			ctx.ServerError("findFile", err)
+			return
+		} else if entry != nil {
+			if err = ServeBlob(ctx, entry.Blob()); err != nil {
+				ctx.ServerError("ServeBlob", err)
+			}
+			return
+		}
 	}
 
-	if err = ServeBlob(ctx, entry.Blob()); err != nil {
-		ctx.ServerError("ServeBlob", err)
-	}
+	ctx.NotFound("findEntryForFile", nil)
+	return
 }
 
 // NewWiki render wiki create page
