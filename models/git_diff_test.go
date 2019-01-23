740bf276a822522b98efe126520504a1cf5ceec0
Matti Ranta <matti@mdranta.net>
2019-01-23 12:33:00 -0500

    Make ParsePatch more performant

--- models/git_diff_test.go
+++ models/git_diff_test.go
@@ -1,3 +1,6 @@
+// Copyright 2017 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
 package models
 
 import (
@@ -138,4 +141,67 @@ func TestDiffLine_CanComment(t *testing.T) {
 func TestDiffLine_GetCommentSide(t *testing.T) {
 	assert.Equal(t, "previous", (&DiffLine{Comments: []*Comment{{Line: -3}}}).GetCommentSide())
 	assert.Equal(t, "proposed", (&DiffLine{Comments: []*Comment{{Line: 3}}}).GetCommentSide())
+}
+
+func benchParsePatch(b *testing.B, diffStr string) {
+	log.DelLogger("console")
+	log.DelLogger("file")
+	b.ResetTimer() //Disable logger for becnh
+	for i := 0; i < b.N; i++ {
+		ParsePatch(1000, 5000, 100, strings.NewReader(diffStr))
+	}
+}
+
+func BenchmarkParsePatchSimple(b *testing.B) {
+	benchParsePatch(b, `diff --git a/integrations/api_issue_test.go b/integrations/api_issue_test.go
+index 74436ffe9..ff316cec3 100644
+--- a/integrations/api_issue_test.go
++++ b/integrations/api_issue_test.go
+@@ -5,13 +5,13 @@
+package integrations
+import (
++       "fmt"
+		"net/http"
+		"testing"
+		"code.gitea.io/gitea/models"
+		api "code.gitea.io/sdk/gitea"
+-       "fmt"
+		"github.com/stretchr/testify/assert"
+)
+`)
+}
+
+func TestParsePatch(t *testing.T) {
+	testCases := []struct {
+		result   error
+		files    int
+		addition int
+		deletion int
+		diff     string
+	}{
+		{nil, 1, 1, 1,
+			`diff --git a/integrations/api_issue_test.go b/integrations/api_issue_test.go
+index 74436ffe9..ff316cec3 100644
+--- a/integrations/api_issue_test.go
++++ b/integrations/api_issue_test.go
+@@ -5,13 +5,13 @@
+ package integrations
+ import (
++       "fmt"
+        "net/http"
+        "testing"
+        "code.gitea.io/gitea/models"
+        api "code.gitea.io/sdk/gitea"
+-       "fmt"
+        "github.com/stretchr/testify/assert"
+ )
+`},
+	}
+	for _, tc := range testCases {
+		diff, err := ParsePatch(1000, 5000, 100, strings.NewReader(tc.diff))
+		assert.Equal(t, tc.result, err)
+		assert.Equal(t, tc.files, diff.NumFiles())
+		assert.Equal(t, tc.addition, diff.TotalAddition)
+		assert.Equal(t, tc.deletion, diff.TotalDeletion)
+	}
 }
