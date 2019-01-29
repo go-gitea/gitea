a09ae7b40347686dccdffb61ca2fe7b767a24cd4
zeripath <art27@cantab.net>
2019-01-29 21:26:17 +0000

    Ensure issue.Poster is loaded in mailIssueCommentToParticipants (#5891)
    Previous code could potentially dereference nil - this PR ensures
    that the poster is loaded before dereferencing it.
    
    Signed-off-by: Andrew Thornton <art27@cantab.net>

--- models/issue_mail.go
+++ models/issue_mail.go
@@ -39,11 +39,11 @@ func mailIssueCommentToParticipants(e Engine, issue *Issue, doer *User, content 
 
 	// In case the issue poster is not watching the repository and is active,
 	// even if we have duplicated in watchers, can be safely filtered out.
-	poster, err := getUserByID(e, issue.PosterID)
+	err = issue.loadPoster(e)
 	if err != nil {
 		return fmt.Errorf("GetUserByID [%d]: %v", issue.PosterID, err)
 	}
-	if issue.PosterID != doer.ID && poster.IsActive && !poster.ProhibitLogin {
+	if issue.PosterID != doer.ID && issue.Poster.IsActive && !issue.Poster.ProhibitLogin {
 		participants = append(participants, issue.Poster)
 	}
 
