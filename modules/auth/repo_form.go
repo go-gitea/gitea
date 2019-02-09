275aa34a28b9f94677734af52b26e93b86fb4210
Thomas Loubiou <xabufr@gmail.com>
2019-02-09 11:24:09 +0100

    Remove extra spaces

--- modules/auth/repo_form.go
+++ modules/auth/repo_form.go
@@ -367,7 +367,7 @@ func (f *InitializeLabelsForm) Validate(ctx *macaron.Context, errs binding.Error
 // swagger:model MergePullRequestOption
 type MergePullRequestForm struct {
 	// required: true
-	// enum: merge, rebase, rebase-merge, squash
+	// enum: merge,rebase,rebase-merge,squash
 	Do                string `binding:"Required;In(merge,rebase,rebase-merge,squash)"`
 	MergeTitleField   string
 	MergeMessageField string
