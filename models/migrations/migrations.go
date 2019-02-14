c632e48dc1ebf6271e16a57b717c1f5916118745
Lanre Adelowo <yo@lanre.wtf>
2019-02-14 10:42:54 +0100

    add migration

--- models/migrations/migrations.go
+++ models/migrations/migrations.go
@@ -227,6 +227,8 @@ var migrations = []Migration{
 	NewMigration("hash application token", hashAppToken),
 	// v86 -> v87
 	NewMigration("add http method to webhook", addHTTPMethodToWebhook),
+	// v87 -> v88
+	NewMigration("delete orphaned attachments", deleteOrphanedAttachments),
 }
 
 // Migrate database to current version
