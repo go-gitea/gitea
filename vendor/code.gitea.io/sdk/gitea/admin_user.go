812154a2a1e4c60ac3ee1cc58db38a31e8521676
Segev Finer <segev@codeocean.com>
2019-02-25 20:05:05 +0200

    Dep update code.gitea.io/sdk
    Signed-off-by: Segev Finer <segev@codeocean.com>

--- vendor/code.gitea.io/sdk/gitea/admin_user.go
+++ vendor/code.gitea.io/sdk/gitea/admin_user.go
@@ -1,4 +1,5 @@
 // Copyright 2015 The Gogs Authors. All rights reserved.
+// Copyright 2019 The Gitea Authors. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
@@ -15,14 +16,15 @@ type CreateUserOption struct {
 	SourceID  int64  `json:"source_id"`
 	LoginName string `json:"login_name"`
 	// required: true
-	Username string `json:"username" binding:"Required;AlphaDashDot;MaxSize(35)"`
+	Username string `json:"username" binding:"Required;AlphaDashDot;MaxSize(40)"`
 	FullName string `json:"full_name" binding:"MaxSize(100)"`
 	// required: true
 	// swagger:strfmt email
 	Email string `json:"email" binding:"Required;Email;MaxSize(254)"`
 	// required: true
-	Password   string `json:"password" binding:"Required;MaxSize(255)"`
-	SendNotify bool   `json:"send_notify"`
+	Password           string `json:"password" binding:"Required;MaxSize(255)"`
+	MustChangePassword bool   `json:"must_change_password"`
+	SendNotify         bool   `json:"send_notify"`
 }
 
 // AdminCreateUser create a user
@@ -44,6 +46,7 @@ type EditUserOption struct {
 	// swagger:strfmt email
 	Email                   string `json:"email" binding:"Required;Email;MaxSize(254)"`
 	Password                string `json:"password" binding:"MaxSize(255)"`
+	MustChangePassword      bool   `json:"must_change_password"`
 	Website                 string `json:"website" binding:"MaxSize(50)"`
 	Location                string `json:"location" binding:"MaxSize(50)"`
 	Active                  *bool  `json:"active"`
