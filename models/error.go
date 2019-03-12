570bfc91c6c97bba52f47635d181d6fb57a447b5
Richard Mahn <rich.mahn@unfoldingword.org>
2019-03-12 12:28:28 -0600

    Gitea copyright in error.go

--- models/error.go
+++ models/error.go
@@ -1,12 +1,11 @@
 // Copyright 2015 The Gogs Authors. All rights reserved.
+// Copyright 2019 The Gitea Authors. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
 package models
 
-import (
-	"fmt"
-)
+import "fmt"
 
 // ErrNameReserved represents a "reserved name" error.
 type ErrNameReserved struct {
