109af5975f73a08abafd416ef58edf629befcaaa
Lunny Xiao <xiaolunwen@gmail.com>
2019-03-26 08:40:34 +0800

    fix tests

--- modules/git/commit_test.go
+++ modules/git/commit_test.go
@@ -11,18 +11,18 @@ import (
 )
 
 func TestCommitsCount(t *testing.T) {
-	commitsCount, _ := CommitsCount(".", "4836fea8767c38f175f59f8f66579e76fe6354f5")
+	commitsCount, _ := CommitsCount("./", "4836fea8767c38f175f59f8f66579e76fe6354f5")
 	assert.Equal(t, int64(3), commitsCount)
 }
 
 func TestGetFullCommitID(t *testing.T) {
-	id, err := GetFullCommitID(".", "4836fea8")
+	id, err := GetFullCommitID("./", "4836fea8")
 	assert.NoError(t, err)
 	assert.Equal(t, "4836fea8767c38f175f59f8f66579e76fe6354f5", id)
 }
 
 func TestGetFullCommitIDError(t *testing.T) {
-	id, err := GetFullCommitID(".", "unknown")
+	id, err := GetFullCommitID("./", "unknown")
 	assert.Empty(t, id)
 	if assert.Error(t, err) {
 		assert.EqualError(t, err, "object does not exist [id: unknown, rel_path: ]")
