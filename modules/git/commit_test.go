1abe8c20104f3b08045bd29d1379ac63f3b74be9
Lunny Xiao <xiaolunwen@gmail.com>
2019-03-26 22:07:09 +0800

    fix tests

--- modules/git/commit_test.go
+++ modules/git/commit_test.go
@@ -5,25 +5,32 @@
 package git
 
 import (
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestCommitsCount(t *testing.T) {
-	commitsCount, err := CommitsCount(".", "4836fea8767c38f175f59f8f66579e76fe6354f5")
+	p, err := filepath.Abs(".")
+	assert.NoError(t, err)
+	commitsCount, err := CommitsCount(p, "4836fea8767c38f175f59f8f66579e76fe6354f5")
 	assert.NoError(t, err)
 	assert.Equal(t, int64(3), commitsCount)
 }
 
 func TestGetFullCommitID(t *testing.T) {
-	id, err := GetFullCommitID(".", "4836fea8")
+	p, err := filepath.Abs(".")
+	assert.NoError(t, err)
+	id, err := GetFullCommitID(p, "4836fea8")
 	assert.NoError(t, err)
 	assert.Equal(t, "4836fea8767c38f175f59f8f66579e76fe6354f5", id)
 }
 
 func TestGetFullCommitIDError(t *testing.T) {
-	id, err := GetFullCommitID(".", "unknown")
+	p, err := filepath.Abs(".")
+	assert.NoError(t, err)
+	id, err := GetFullCommitID(p, "unknown")
 	assert.Empty(t, id)
 	if assert.Error(t, err) {
 		assert.EqualError(t, err, "object does not exist [id: unknown, rel_path: ]")
