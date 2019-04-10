bd34043e213710d3e260484a24d504a1c7c5982a
AJ ONeal <coolaj86@gmail.com>
2019-04-10 16:04:10 +0000

    update LICENSE comments (and a bulid flag)

--- tools/tools.go
+++ tools/tools.go
@@ -1,4 +1,8 @@
-//+build tools
+// Copyright 2019-present The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+// +build tools
 
 // tools is a shim package to cause go-bindata to register as a dependency
 package tools
