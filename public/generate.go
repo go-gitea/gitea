57d475a1aaed03ee37971624546e8d8c1b31fe8f
AJ ONeal <coolaj86@gmail.com>
2019-04-10 17:56:39 +0000

    only build bindata when flagged

--- public/generate.go
+++ public/generate.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
+// +build bindata
+
 //go:generate go run github.com/jteeuwen/go-bindata/go-bindata -tags "bindata" -ignore "\\.go|\\.less" -pkg "public" -o "../modules/public/bindata.go" ./...
 //go:generate go fmt ../modules/public/bindata.go
 
--- templates/generate.go
+++ templates/generate.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
+// +build bindata
+
 //go:generate go run github.com/jteeuwen/go-bindata/go-bindata -tags "bindata" -ignore "\\.go" -pkg "templates" -o "../modules/templates/bindata.go" ./...
 //go:generate go fmt ../modules/templates/bindata.go
 
