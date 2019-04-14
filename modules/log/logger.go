8f81b779cf7e18fb4013077be6c3060f3ab2f8e9
Andrew Thornton <art27@cantab.net>
2019-04-14 19:12:21 +0100

    Add log.ColorFormat and log.ColorFormatted
    Structs can now implement log.ColorFormatted to provide their own
    colored format when logged with `%-v` or additional flags.
    
    Signed-off-by: Andrew Thornton <art27@cantab.net>

--- modules/log/logger.go
+++ modules/log/logger.go
@@ -69,11 +69,7 @@ func (l *Logger) Log(skip int, level Level, format string, v ...interface{}) err
 	}
 	msg := format
 	if len(v) > 0 {
-		args := make([]interface{}, len(v))
-		for i := 0; i < len(args); i++ {
-			args[i] = NewColoredValuePointer(&v[i])
-		}
-		msg = fmt.Sprintf(format, args...)
+		msg = ColorSprintf(format, v...)
 	}
 	stack := ""
 	if l.GetStacktraceLevel() <= level {
