c9ff5c5e4cb8d6cdb6cdf8d2b3e3650fc86bfadb
Filip Navara <filip.navara@gmail.com>
2019-04-14 21:30:13 +0200

    Clean up GPG signature verification and fix it for tagged commits

--- modules/git/commit.go
+++ modules/git/commit.go
@@ -38,22 +38,6 @@ type CommitGPGSignature struct {
 	Payload   string //TODO check if can be reconstruct from the rest of commit information to not have duplicate data
 }
 
-// similar to https://github.com/git/git/blob/3bc53220cb2dcf709f7a027a3f526befd021d858/commit.c#L1128
-func newGPGSignatureFromCommitline(data []byte, signatureStart int, tag bool) (*CommitGPGSignature, error) {
-	sig := new(CommitGPGSignature)
-	signatureEnd := bytes.LastIndex(data, []byte("-----END PGP SIGNATURE-----"))
-	if signatureEnd == -1 {
-		return nil, fmt.Errorf("end of commit signature not found")
-	}
-	sig.Signature = strings.Replace(string(data[signatureStart:signatureEnd+27]), "\n ", "\n", -1)
-	if tag {
-		sig.Payload = string(data[:signatureStart-1])
-	} else {
-		sig.Payload = string(data[:signatureStart-8]) + string(data[signatureEnd+27:])
-	}
-	return sig, nil
-}
-
 func convertPGPSignature(c *object.Commit) *CommitGPGSignature {
 	if c.PGPSignature == "" {
 		return nil
