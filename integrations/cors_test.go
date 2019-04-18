ab866c30dd89bf8612bac39e41b7f9d9906af244
Matti Ranta <matti@mdranta.net>
2019-04-17 21:06:30 -0400

    refactor

--- integrations/cors_test.go
+++ integrations/cors_test.go
@@ -14,28 +14,11 @@ import (
 )
 
 func Test_CORSNotSet(t *testing.T) {
-	setting.NewContext()
 	prepareTestEnv(t)
 	req := NewRequestf(t, "GET", "/api/v1/version")
 	session := loginUser(t, "user2")
 	resp := session.MakeRequest(t, req, http.StatusOK)
 	assert.Equal(t, resp.Code, http.StatusOK)
-	// body, _ := ioutil.ReadAll(resp.Body)
 	corsHeader := resp.Header().Get("Access-Control-Allow-Origin")
 	assert.Equal(t, corsHeader, "", "Access-Control-Allow-Origin: generated header should match") // header not set
-}
-
-func Test_CORSBasic(t *testing.T) {
-	setting.NewContext()
-	setting.EnableCORS = true
-	prepareTestEnv(t)
-
-	req := NewRequestf(t, "GET", "/api/v1/version")
-	session := loginUser(t, "user2")
-	resp := session.MakeRequest(t, req, http.StatusOK)
-	assert.Equal(t, resp.Code, http.StatusOK)
-
-	corsHeader := resp.Header().Get("Access-Control-Allow-Origin")
-	assert.Equal(t, corsHeader, "*", "Access-Control-Allow-Origin: generated header should match")
-
 }
