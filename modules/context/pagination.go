4ff56b2f00bd1fd486168b9afd550c47b0031fd2
Mario Lubenka <mario.lubenka@googlemail.com>
2019-04-19 13:50:45 +0200

    Store urlParams as array of strings instead of template.URL (#6531)
    Signed-off-by: Mario Lubenka <mario.lubenka@googlemail.com>

--- modules/context/pagination.go
+++ modules/context/pagination.go
@@ -8,21 +8,21 @@ import (
 	"fmt"
 	"html/template"
 	"net/url"
+	"strings"
 
 	"github.com/Unknwon/paginater"
 )
 
 // Pagination provides a pagination via Paginater and additional configurations for the link params used in rendering
 type Pagination struct {
-	Paginater  *paginater.Paginater
-	linkParams template.URL
+	Paginater *paginater.Paginater
+	urlParams []string
 }
 
 // NewPagination creates a new instance of the Pagination struct
 func NewPagination(total int, page int, issueNum int, numPages int) *Pagination {
 	p := &Pagination{}
 	p.Paginater = paginater.New(total, page, issueNum, numPages)
-	p.linkParams = ""
 	return p
 }
 
@@ -32,13 +32,14 @@ func (p *Pagination) AddParam(ctx *Context, paramKey string, ctxKey string) {
 	if !exists {
 		return
 	}
-	paramData := fmt.Sprintf("%v", ctx.Data[ctxKey])
-	p.linkParams = template.URL(fmt.Sprintf("%v%s=%v&", p.linkParams, url.QueryEscape(paramKey), url.QueryEscape(paramData)))
+	paramData := fmt.Sprintf("%v", ctx.Data[ctxKey]) // cast interface{} to string
+	urlParam := fmt.Sprintf("%s=%v", url.QueryEscape(paramKey), url.QueryEscape(paramData))
+	p.urlParams = append(p.urlParams, urlParam)
 }
 
 // GetParams returns the configured URL params
 func (p *Pagination) GetParams() template.URL {
-	return p.linkParams
+	return template.URL(strings.Join(p.urlParams[:], "&"))
 }
 
 // SetDefaultParams sets common pagination params that are often used
