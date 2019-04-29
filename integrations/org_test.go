152577af87d815ba1134dfd452f0091f664cfffd
Antoine GIRARD <sapk@sapk.fr>
2019-04-29 19:15:28 +0200

    add more test

--- integrations/org_test.go
+++ integrations/org_test.go
@@ -5,6 +5,7 @@
 package integrations
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 	"testing"
@@ -174,8 +175,77 @@ func TestOrgSettingsHooks(t *testing.T) {
 		})
 	}
 }
+func TestOrgSettingsHooksAdd(t *testing.T) {
+	prepareTestEnv(t)
 
-//TODO add webhook
+	type test struct {
+		User            string
+		Repo            string
+		Type            string
+		GetResult       int
+		HookData        map[string]string
+		PostResult      int
+		HookCountBefore int
+		DeleteResult    int
+	}
+	var (
+		tests = []test{
+			test{"user1", "privated_org", "gitea", http.StatusOK, map[string]string{
+				"payload_url":  "http://localhost:8080",
+				"content_type": "1",
+				"secret":       "some_secret",
+				"events":       "send_everything",
+				"active":       "on",
+			}, http.StatusFound, 0, http.StatusOK},
+		}
+	)
+
+	for _, te := range tests {
+		t.Run(te.User+"/"+te.Repo, func(t *testing.T) {
+			session := loginUser(t, te.User)
+			req := NewRequest(t, "GET", "/org/"+te.Repo+"/settings/hooks/"+te.Type+"/new")
+			resp := session.MakeRequest(t, req, te.GetResult)
+
+			//t.Logf("debug: %s", resp.Body)
+			htmlDoc := NewHTMLParser(t, resp.Body)
+			sel := htmlDoc.doc.Find("a.dont-break-out")
+			assert.EqualValues(t, te.HookCountBefore, len(sel.Nodes))
+
+			te.HookData["_csrf"] = GetCSRF(t, session, "/org/"+te.Repo+"/settings/hooks/"+te.Type+"/new")
+			req = NewRequestWithValues(t, "POST", "/org/"+te.Repo+"/settings/hooks/"+te.Type+"/new", te.HookData)
+			session.MakeRequest(t, req, te.PostResult)
+
+			req = NewRequest(t, "GET", fmt.Sprintf("/org/%s/settings/hooks", te.Repo))
+			resp = session.MakeRequest(t, req, te.GetResult)
+			session.MakeRequest(t, req, te.GetResult)
+			//t.Logf("debug: %s", resp.Body)
+			htmlDoc = NewHTMLParser(t, resp.Body)
+			sel = htmlDoc.doc.Find("a.dont-break-out")
+			assert.EqualValues(t, te.HookCountBefore+1, len(sel.Nodes))
+			hookEl := sel.Nodes[len(sel.Nodes)-1]
+			hookID := "0"
+			for _, a := range hookEl.Attr {
+				if a.Key == "href" {
+					tmp := strings.Split(a.Val, "/")
+					hookID = tmp[len(tmp)-1]
+				}
+			}
+
+			req = NewRequest(t, "GET", fmt.Sprintf("/org/%s/settings/hooks/%s", te.Repo, hookID))
+			resp = session.MakeRequest(t, req, te.GetResult)
+			htmlDoc = NewHTMLParser(t, resp.Body)
+
+			//t.Logf("debug: %s", htmlDoc.GetCSRF())
+			req = NewRequestWithValues(t, "POST", "/org/"+te.Repo+"/settings/hooks/delete", map[string]string{
+				"_csrf": htmlDoc.GetCSRF(),
+				"id":    hookID,
+			})
+			session.MakeRequest(t, req, te.DeleteResult)
+		})
+	}
+}
+
+//TODO delete webhook
 
 func TestOrgSettingsDelete(t *testing.T) {
 	prepareTestEnv(t)
