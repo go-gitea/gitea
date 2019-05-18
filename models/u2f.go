895764e7f5a6ecb0feb905aef8c17c067340788f
David Schneiderbauer <daviian@users.noreply.github.com>
2019-05-18 16:15:41 +0200

    fix u2f registrationlist ToRegistrations() method (#6980) (#6982)

--- models/u2f.go
+++ models/u2f.go
@@ -48,7 +48,7 @@ type U2FRegistrationList []*U2FRegistration
 
 // ToRegistrations will convert all U2FRegistrations to u2f.Registrations
 func (list U2FRegistrationList) ToRegistrations() []u2f.Registration {
-	regs := make([]u2f.Registration, len(list))
+	regs := make([]u2f.Registration, 0, len(list))
 	for _, reg := range list {
 		r, err := reg.Parse()
 		if err != nil {
