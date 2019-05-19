025b66b17923f120091c0eff8f9af1eb86facd2c
Sergey Dryabzhinsky <sergey@rusoft.ru>
2019-05-20 01:43:17 +0300

    Use Sync2 to migrate

--- models/migrations/v87.go
+++ models/migrations/v87.go
@@ -1,42 +1,17 @@
-// Copyright 2017 Gitea. All rights reserved.
+// Copyright 2019 Gitea. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
 package migrations
 
 import (
-	"fmt"
-
 	"github.com/go-xorm/xorm"
 )
 
-func addAvatarFieldToRepository(x *xorm.Engine) (err error) {
-
-	sess := x.NewSession()
-	defer sess.Close()
-
-	if err := sess.Begin(); err != nil {
-		return err
+func addAvatarFieldToRepository(x *xorm.Engine) error {
+	type Repository struct {
+		Avatar string `xorm:"VARCHAR(2048)"`
 	}
 
-	dialect := x.Dialect().DriverName()
-
-	switch dialect {
-	case "mysql":
-		_, err = sess.Exec("ALTER TABLE repository ADD COLUMN `avatar` TEXT")
-	case "postgres":
-		_, err = sess.Exec("ALTER TABLE repository ADD COLUMN \"avatar\" VARCHAR")
-	case "tidb":
-		_, err = sess.Exec("ALTER TABLE repository ADD `avatar` TEXT")
-	case "mssql":
-		_, err = sess.Exec("ALTER TABLE repository ADD \"avatar\" VARCHAR")
-	case "sqlite3":
-		_, err = sess.Exec("ALTER TABLE repository ADD COLUMN `avatar` TEXT")
-	}
-
-	if err != nil {
-		return fmt.Errorf("Error while adding new column to repository table: %v", err)
-	}
-
-	return sess.Commit()
+	return x.Sync2(new(Repository))
 }
