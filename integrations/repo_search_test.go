dde55bd5f9f108032746256749bb79d2659a270f
Andrew Thornton <art27@cantab.net>
2019-05-20 18:33:31 +0100

    Fix TestSearchRepo by waiting till indexing is done

--- integrations/repo_search_test.go
+++ integrations/repo_search_test.go
@@ -5,8 +5,12 @@
 package integrations
 
 import (
+	"log"
 	"net/http"
 	"testing"
+	"time"
+
+	"code.gitea.io/gitea/models"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/stretchr/testify/assert"
@@ -26,6 +30,23 @@ func resultFilenames(t testing.TB, doc *HTMLDoc) []string {
 
 func TestSearchRepo(t *testing.T) {
 	prepareTestEnv(t)
+
+	repo, err := models.GetRepositoryByOwnerAndName("user2", "repo1")
+	assert.NoError(t, err)
+
+	models.UpdateRepoIndexer(repo)
+
+	log.Printf("Waiting for indexing\n")
+
+	i := 0
+	for {
+		if repo.IndexerStatus != nil && len(repo.IndexerStatus.CommitSha) != 0 && i <= 60 {
+			break
+		}
+		time.Sleep(1 * time.Second)
+		i++
+	}
+	log.Printf("Indexing took: %d s\n", i)
 
 	req := NewRequestf(t, "GET", "/user2/repo1/search?q=Description&page=1")
 	resp := MakeRequest(t, req, http.StatusOK)
