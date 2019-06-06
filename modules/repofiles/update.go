af9b8f8af8ec2658a96147c79b4527be0e95f373
Lunny Xiao <xiaolunwen@gmail.com>
2019-06-06 14:04:29 +0800

    remove push_update

--- modules/repofiles/update.go
+++ modules/repofiles/update.go
@@ -394,7 +394,8 @@ func CreateOrUpdateRepoFile(repo *models.Repository, doer *models.User, opts *Up
 	if err = repo.GetOwner(); err != nil {
 		return nil, fmt.Errorf("GetOwner: %v", err)
 	}
-	err = models.PushUpdate(
+	err = PushUpdate(
+		repo,
 		opts.NewBranch,
 		models.PushUpdateOptions{
 			PusherID:     doer.ID,
@@ -409,7 +410,6 @@ func CreateOrUpdateRepoFile(repo *models.Repository, doer *models.User, opts *Up
 	if err != nil {
 		return nil, fmt.Errorf("PushUpdate: %v", err)
 	}
-	models.UpdateRepoIndexer(repo)
 
 	commit, err = t.GetCommit(commitHash)
 	if err != nil {
@@ -421,4 +421,18 @@ func CreateOrUpdateRepoFile(repo *models.Repository, doer *models.User, opts *Up
 		return nil, err
 	}
 	return file, nil
+}
+
+// PushUpdate must be called for any push actions in order to
+// generates necessary push action history feeds and other operations
+func PushUpdate(repo *models.Repository, branch string, opts models.PushUpdateOptions) error {
+	err := models.PushUpdate(branch, opts)
+	if err != nil {
+		return fmt.Errorf("PushUpdate: %v", err)
+	}
+
+	if opts.RefFullName == git.BranchPrefix+repo.DefaultBranch {
+		models.UpdateRepoIndexer(repo)
+	}
+	return nil
 }
