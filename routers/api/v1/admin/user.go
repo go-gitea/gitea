fefc6207f4537b89118689d186a77e44d2be11ff
Maxim Tkachenko <Maxim.Tkachenko@gmail.com>
2019-06-12 08:15:50 +0700

    fix fmt-check

--- routers/api/v1/admin/user.go
+++ routers/api/v1/admin/user.go
@@ -10,7 +10,7 @@ import (
 	"code.gitea.io/gitea/modules/context"
 	"code.gitea.io/gitea/modules/log"
 	"code.gitea.io/gitea/modules/setting"
-  api "code.gitea.io/gitea/modules/structs"
+	api "code.gitea.io/gitea/modules/structs"
 	"code.gitea.io/gitea/modules/util"
 	"code.gitea.io/gitea/routers/api/v1/convert"
 	"code.gitea.io/gitea/routers/api/v1/user"
