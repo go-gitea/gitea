2f153faba314371244d7e8e102fb00333f7e610d
Filip Navara <filip.navara@gmail.com>
2019-06-27 17:22:41 +0200

    Remove mmap usage

--- modules/git/repo_commitgraph.go
+++ modules/git/repo_commitgraph.go
@@ -1,18 +1,18 @@
 package git
 
 import (
+	"os"
 	"path"
 
-	"golang.org/x/exp/mmap"
 	"gopkg.in/src-d/go-git.v4/plumbing/format/commitgraph"
 	cgobject "gopkg.in/src-d/go-git.v4/plumbing/object/commitgraph"
 )
 
 // CommitNodeIndex returns the index for walking commit graph
-func (r *Repository) CommitNodeIndex() (cgobject.CommitNodeIndex, *mmap.ReaderAt) {
+func (r *Repository) CommitNodeIndex() (cgobject.CommitNodeIndex, *os.File) {
 	indexPath := path.Join(r.Path, "objects", "info", "commit-graph")
 
-	file, err := mmap.Open(indexPath)
+	file, err := os.Open(indexPath)
 	if err == nil {
 		index, err := commitgraph.OpenFileIndex(file)
 		if err == nil {
