a9b7776f433f544750979003dca13b32f87b1209
Andrew Thornton <art27@cantab.net>
2019-06-30 19:27:00 +0100

    Add copyright header and log commit-graph error

--- modules/git/repo_commitgraph.go
+++ modules/git/repo_commitgraph.go
@@ -1,9 +1,15 @@
+// Copyright 2019 The Gitea Authors.
+// All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 package git
 
 import (
 	"os"
 	"path"
 
+	gitealog "code.gitea.io/gitea/modules/log"
 	"gopkg.in/src-d/go-git.v4/plumbing/format/commitgraph"
 	cgobject "gopkg.in/src-d/go-git.v4/plumbing/object/commitgraph"
 )
@@ -14,10 +20,15 @@ func (r *Repository) CommitNodeIndex() (cgobject.CommitNodeIndex, *os.File) {
 
 	file, err := os.Open(indexPath)
 	if err == nil {
-		index, err := commitgraph.OpenFileIndex(file)
+		var index commitgraph.Index
+		index, err = commitgraph.OpenFileIndex(file)
 		if err == nil {
 			return cgobject.NewGraphCommitNodeIndex(index, r.gogitRepo.Storer), file
 		}
+	}
+
+	if !os.IsNotExist(err) {
+		gitealog.Warn("Unable to read commit-graph for %s: %v", r.Path, err)
 	}
 
 	return cgobject.NewObjectCommitNodeIndex(r.gogitRepo.Storer), nil
