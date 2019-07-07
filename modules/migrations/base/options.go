af4f2f7edd4cfce99f56b4afce347a7a1cc2346f
j. mccann <info@mrsdizzie.com>
2019-07-06 23:05:38 -0400

    Remove no longer used IgnoreIssueAuthor option

--- modules/migrations/base/options.go
+++ modules/migrations/base/options.go
@@ -14,14 +14,13 @@ type MigrateOptions struct {
 	Description  string
 	OriginalURL  string
 
-	Wiki              bool
-	Issues            bool
-	Milestones        bool
-	Labels            bool
-	Releases          bool
-	Comments          bool
-	PullRequests      bool
-	Private           bool
-	Mirror            bool
-	IgnoreIssueAuthor bool // if true will not add original author information before issues or comments content.
+	Wiki         bool
+	Issues       bool
+	Milestones   bool
+	Labels       bool
+	Releases     bool
+	Comments     bool
+	PullRequests bool
+	Private      bool
+	Mirror       bool
 }
