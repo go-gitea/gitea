go-gitea/gitea

commit 831288cc916d5301beaa0adc1d912d6748a4cdaa

fix Dropzone.js integration (#7445)

public/js/index.js
@@ -10,6 +10,9 @@ function htmlEncode(text) {
 var csrf;
 var suburl;
 
+// Disable Dropzone auto-discover because it's manually initialized
+Dropzone.autoDiscover = false;
+
 // Polyfill for IE9+ support (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from)
 if (!Array.from) {
     Array.from = (function () {
@@ -2005,13 +2008,11 @@ $(document).ready(function () {
     }
 
     // Dropzone
-    var $dropzone = $('#dropzone');
+    const $dropzone = $('#dropzone');
     if ($dropzone.length > 0) {
-        // Disable auto discover for all elements:
-        Dropzone.autoDiscover = false;
+        const filenameDict = {};
 
-        var filenameDict = {};
-        $dropzone.dropzone({
+        new Dropzone("#dropzone", {
             url: $dropzone.data('upload-url'),
             headers: {"X-Csrf-Token": csrf},
             maxFiles: $dropzone.data('max-file'),
@@ -2039,7 +2040,7 @@ $(document).ready(function () {
                         });
                     }
                 })
-            }
+            },
         });
     }
 

commit 8f7335ff64621314b2dd93e5a2c45d79c9fc2b0c

Fix an issue with some pages throwing 'not defined' js exceptions
Fix an issue introduced by 831288cc916d5301beaa0adc1d912d6748a4cdaa

Signed-off-by: Gary Kim <gary@garykim.dev>

public/js/index.js
@@ -11,7 +11,9 @@ var csrf;
 var suburl;
 
 // Disable Dropzone auto-discover because it's manually initialized
-Dropzone.autoDiscover = false;
+if (typeof(Dropzone) !== "undefined") {
+    Dropzone.autoDiscover = false;
+}
 
 // Polyfill for IE9+ support (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from)
 if (!Array.from) {