fd16091db29da020ef301bb0d9402611f6e2a661
Antoine GIRARD <sapk@sapk.fr>
2019-07-27 23:24:10 +0200

    test new UserList methods

--- models/userlist_test.go
+++ models/userlist_test.go
@@ -5,21 +5,79 @@
 package models
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestUserListIsPublicMember(t *testing.T) {
 	assert.NoError(t, PrepareTestDatabase())
-	//TODO
+	tt := []struct {
+		orgid    int64
+		expected []bool
+	}{
+		{3, []bool{true, false}},
+		{6, []bool{true}},
+		{7, []bool{false}},
+	}
+	for _, v := range tt {
+		t.Run(fmt.Sprintf("IsPublicMemberOfOrdIg%d", v.orgid), func(t *testing.T) {
+			testUserListIsPublicMember(t, v.orgid, v.expected)
+		})
+	}
+}
+func testUserListIsPublicMember(t *testing.T, orgID int64, expected []bool) {
+	org, err := GetUserByID(orgID)
+	assert.NoError(t, err)
+	assert.NoError(t, org.GetMembers())
+	assert.Equal(t, expected, org.Members.IsPublicMember(orgID))
+
 }
 
 func TestUserListIsUserOrgOwner(t *testing.T) {
 	assert.NoError(t, PrepareTestDatabase())
-	//TODO
+	tt := []struct {
+		orgid    int64
+		expected []bool
+	}{
+		{3, []bool{true, false}},
+		{6, []bool{true}},
+		{7, []bool{false}},
+	}
+	for _, v := range tt {
+		t.Run(fmt.Sprintf("IsUserOrgOwnerOfOrdIg%d", v.orgid), func(t *testing.T) {
+			testUserListIsUserOrgOwner(t, v.orgid, v.expected)
+		})
+	}
+}
+func testUserListIsUserOrgOwner(t *testing.T, orgID int64, expected []bool) {
+	org, err := GetUserByID(orgID)
+	assert.NoError(t, err)
+	assert.NoError(t, org.GetMembers())
+	assert.Equal(t, expected, org.Members.IsPublicMember(orgID))
+
 }
 
 func TestUserListIsTwoFaEnrolled(t *testing.T) {
 	assert.NoError(t, PrepareTestDatabase())
-	//TODO
+	tt := []struct {
+		orgid    int64
+		expected []bool
+	}{
+		{3, []bool{true, false}},
+		{6, []bool{false}},
+		{7, []bool{false}},
+	}
+	for _, v := range tt {
+		t.Run(fmt.Sprintf("IsTwoFaEnrolledOfOrdIg%d", v.orgid), func(t *testing.T) {
+			testUserListIsTwoFaEnrolled(t, v.orgid, v.expected)
+		})
+	}
+}
+func testUserListIsTwoFaEnrolled(t *testing.T, orgID int64, expected []bool) {
+	org, err := GetUserByID(orgID)
+	assert.NoError(t, err)
+	assert.NoError(t, org.GetMembers())
+	assert.Equal(t, expected, org.Members.IsTwoFaEnrolled())
+
 }
