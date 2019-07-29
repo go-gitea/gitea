d827b0bfb71f48eb998cdd864a94a267c7781ba2
Lunny Xiao <xiaolunwen@gmail.com>
2019-07-29 12:15:13 +0800

    change length of some repository's columns (#7652) (#7655)

--- models/repo.go
+++ models/repo.go
@@ -134,8 +134,8 @@ type Repository struct {
 	Owner         *User  `xorm:"-"`
 	LowerName     string `xorm:"UNIQUE(s) INDEX NOT NULL"`
 	Name          string `xorm:"INDEX NOT NULL"`
-	Description   string
-	Website       string
+	Description   string `xorm:"TEXT"`
+	Website       string `xorm:"VARCHAR(2048)"`
 	DefaultBranch string
 
 	NumWatches          int
