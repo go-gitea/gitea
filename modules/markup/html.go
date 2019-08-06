024057138d6c5d5c01746a83d02122fbd48d8908
Gary Kim <gary@garykim.dev>
2019-08-06 09:47:54 +0800

    Import Gitea log module
    Signed-off-by: Gary Kim <gary@garykim.dev>

--- modules/markup/html.go
+++ modules/markup/html.go
@@ -14,6 +14,7 @@ import (
 
 	"code.gitea.io/gitea/modules/base"
 	"code.gitea.io/gitea/modules/git"
+	"code.gitea.io/gitea/modules/log"
 	"code.gitea.io/gitea/modules/setting"
 	"code.gitea.io/gitea/modules/util"
 
