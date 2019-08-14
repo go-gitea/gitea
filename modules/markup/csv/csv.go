28e5897b534b52dddaf06727af011dc884e72340
Norwin Roosen <github@nroo.de>
2019-08-14 21:19:23 +0200

    detect csv delimiter in csv rendering
    fixes #7868

--- modules/markup/csv/csv.go
+++ modules/markup/csv/csv.go
@@ -9,6 +9,8 @@ import (
 	"encoding/csv"
 	"html"
 	"io"
+	"math"
+	"strings"
 
 	"code.gitea.io/gitea/modules/markup"
 )
@@ -28,12 +30,13 @@ func (Parser) Name() string {
 
 // Extensions implements markup.Parser
 func (Parser) Extensions() []string {
-	return []string{".csv"}
+	return []string{".csv", ".tsv"}
 }
 
 // Render implements markup.Parser
-func (Parser) Render(rawBytes []byte, urlPrefix string, metas map[string]string, isWiki bool) []byte {
+func (p Parser) Render(rawBytes []byte, urlPrefix string, metas map[string]string, isWiki bool) []byte {
 	rd := csv.NewReader(bytes.NewReader(rawBytes))
+	rd.Comma = p.bestDelimiter(rawBytes)
 	var tmpBlock bytes.Buffer
 	tmpBlock.WriteString(`<table class="table">`)
 	for {
@@ -55,4 +58,53 @@ func (Parser) Render(rawBytes []byte, urlPrefix string, metas map[string]string,
 	tmpBlock.WriteString("</table>")
 
 	return tmpBlock.Bytes()
+}
+
+func (p Parser) bestDelimiter(data []byte) rune {
+	// Scores the input data against delimiters, and returns the best matching.
+	// Reads at most 10k bytes & 10 lines.
+	maxLines := 10
+	maxBytes := int(math.Min(float64(len(data)), 1e4))
+	text := string(data[:maxBytes])
+	lines := strings.SplitN(text, "\n", maxLines+1)[:maxLines]
+
+	delimiters := []rune{',', ';', '\t', '|'}
+	bestDelim := delimiters[0]
+	bestScore := 0.0
+
+	for _, delim := range delimiters {
+		score := p.scoreDelimiter(lines, delim)
+		if score > bestScore {
+			bestScore = score
+			bestDelim = delim
+		}
+	}
+
+	return bestDelim
+}
+
+func (Parser) scoreDelimiter(lines []string, delim rune) (score float64) {
+	// Scores a delimiter against input csv data with a count and regularity metric.
+
+	countTotal := 0.0
+	countLineMax := 0.0
+	linesNotEqual := 0.0
+
+	for _, line := range lines {
+		if len(line) == 0 {
+			continue
+		}
+
+		countLine := float64(strings.Count(line, string(delim)))
+		countTotal += countLine
+
+		if countLine != countLineMax {
+			if countLineMax != 0 {
+				linesNotEqual += 1
+			}
+			countLineMax = math.Max(countLine, countLineMax)
+		}
+	}
+
+	return countTotal * (1 - linesNotEqual/float64(len(lines)))
 }
