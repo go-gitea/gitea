7596c4ba843fb6ae6fe9d7fa89b3b5f934160c08
Norwin Roosen <github@nroo.de>
2019-08-15 12:32:56 +0200

    fix failing testcase & use ints where possible

--- modules/markup/csv/csv.go
+++ modules/markup/csv/csv.go
@@ -9,10 +9,10 @@ import (
 	"encoding/csv"
 	"html"
 	"io"
-	"math"
 	"strings"
 
 	"code.gitea.io/gitea/modules/markup"
+	"code.gitea.io/gitea/modules/util"
 )
 
 func init() {
@@ -60,18 +60,18 @@ func (p Parser) Render(rawBytes []byte, urlPrefix string, metas map[string]strin
 	return tmpBlock.Bytes()
 }
 
+// bestDelimiter scores the input CSV data against delimiters, and returns the best match.
+// Reads at most 10k bytes & 10 lines.
 func (p Parser) bestDelimiter(data []byte) rune {
-	// Scores the input data against delimiters, and returns the best matching.
-	// Reads at most 10k bytes & 10 lines.
 	maxLines := 10
-	maxBytes := int(math.Min(float64(len(data)), 1e4))
+	maxBytes := util.Min(len(data), 1e4)
 	text := string(data[:maxBytes])
-	lines := strings.SplitN(text, "\n", maxLines+1)[:maxLines]
+	lines := strings.SplitN(text, "\n", maxLines+1)
+	lines = lines[:util.Min(maxLines, len(lines))]
 
 	delimiters := []rune{',', ';', '\t', '|'}
 	bestDelim := delimiters[0]
 	bestScore := 0.0
-
 	for _, delim := range delimiters {
 		score := p.scoreDelimiter(lines, delim)
 		if score > bestScore {
@@ -83,28 +83,26 @@ func (p Parser) bestDelimiter(data []byte) rune {
 	return bestDelim
 }
 
+// scoreDelimiter uses a count & regularity metric to evaluate a delimiter against lines of CSV
 func (Parser) scoreDelimiter(lines []string, delim rune) (score float64) {
-	// Scores a delimiter against input csv data with a count and regularity metric.
-
-	countTotal := 0.0
-	countLineMax := 0.0
-	linesNotEqual := 0.0
+	countTotal := 0
+	countLineMax := 0
+	linesNotEqual := 0
 
 	for _, line := range lines {
 		if len(line) == 0 {
 			continue
 		}
 
-		countLine := float64(strings.Count(line, string(delim)))
+		countLine := strings.Count(line, string(delim))
 		countTotal += countLine
-
 		if countLine != countLineMax {
 			if countLineMax != 0 {
 				linesNotEqual++
 			}
-			countLineMax = math.Max(countLine, countLineMax)
+			countLineMax = util.Max(countLine, countLineMax)
 		}
 	}
 
-	return countTotal * (1 - linesNotEqual/float64(len(lines)))
+	return float64(countTotal) * (1 - float64(linesNotEqual)/float64(len(lines)))
 }
--- modules/markup/csv/csv_test.go
+++ modules/markup/csv/csv_test.go
@@ -18,7 +18,7 @@ func TestRenderCSV(t *testing.T) {
 		"1;2":                      "<table class=\"table\"><tr><td>1</td><td>2</td><tr></table>",
 		"1\t2":                     "<table class=\"table\"><tr><td>1</td><td>2</td><tr></table>",
 		"1|2":                      "<table class=\"table\"><tr><td>1</td><td>2</td><tr></table>",
-		"1,2,3;4,5,6;7,8,9\na;b;c": "<table class=\"table\"><tr><td>1,2,3</td><td>4,5,6</td><td>7,8,9</td><tr><tr><td>a</td><td>b</td<td>c</td><tr></table>",
+		"1,2,3;4,5,6;7,8,9\na;b;c": "<table class=\"table\"><tr><td>1,2,3</td><td>4,5,6</td><td>7,8,9</td><tr><tr><td>a</td><td>b</td><td>c</td><tr></table>",
 		"<br/>":                    "<table class=\"table\"><tr><td>&lt;br/&gt;</td><tr></table>",
 	}
 
