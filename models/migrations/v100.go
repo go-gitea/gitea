37eb777a13b8ab34e93dedb343cf347c486e3984
Lunny Xiao <xiaolunwen@gmail.com>
2019-09-14 20:44:40 +0800

    add migrations for repository service type

---
+++ models/migrations/v100.go
@@ -0,0 +1,42 @@
+// Copyright 2019 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package migrations
+
+import (
+	"strings"
+
+	"code.gitea.io/gitea/modules/structs"
+
+	"github.com/go-xorm/xorm"
+)
+
+func updateMigrationServiceTypes(x *xorm.Engine) error {
+	for {
+		sql := "SELECT id, original_url FROM WHERE original_url <> '' LIMIT 50 ORDER BY id"
+		var results = make([]struct {
+			ID          int64
+			OriginalURL string
+		}, 0, 50)
+		err := x.SQL(sql).Find(results)
+		if err != nil {
+			return err
+		}
+		if len(results) == 0 {
+			return nil
+		}
+
+		for _, res := range results {
+			u := strings.ToLower(res.OriginalURL)
+			var serviceType = structs.PlainGitService
+			if strings.HasPrefix(u, "https://github.com") || strings.HasPrefix(u, "https://github.com") {
+				serviceType = structs.GithubService
+			}
+			_, err = x.Exec("UPDATE repository SET original_service_type = ? WHERE id = ?", serviceType, res.ID)
+			if err != nil {
+				return err
+			}
+		}
+	}
+}
