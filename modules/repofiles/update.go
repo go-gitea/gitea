5f53dd3bb1e464f510c0ffd82056c1f695d7f576
Lunny Xiao <xiaolunwen@gmail.com>
2019-09-30 21:08:22 +0800

    move AddTestPullRequestTask to pull service package from models

--- modules/repofiles/update.go
+++ modules/repofiles/update.go
@@ -19,9 +19,11 @@ import (
 	"code.gitea.io/gitea/modules/log"
 	"code.gitea.io/gitea/modules/setting"
 	"code.gitea.io/gitea/modules/structs"
+	pull_service "code.gitea.io/gitea/services/pull"
 
 	stdcharset "golang.org/x/net/html/charset"
 	"golang.org/x/text/transform"
+	
 )
 
 // IdentityOptions for a person's identity like an author or committer
@@ -497,7 +499,7 @@ func PushUpdate(repo *models.Repository, branch string, opts models.PushUpdateOp
 
 	log.Trace("TriggerTask '%s/%s' by %s", repo.Name, branch, pusher.Name)
 
-	go models.AddTestPullRequestTask(pusher, repo.ID, branch, true)
+	go pull_service.AddTestPullRequestTask(pusher, repo.ID, branch, true)
 
 	if opts.RefFullName == git.BranchPrefix+repo.DefaultBranch {
 		models.UpdateRepoIndexer(repo)
