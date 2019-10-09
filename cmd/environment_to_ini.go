019e1247eca219eb73bdfe256bf575ab4c133477
Andrew Thornton <art27@cantab.net>
2019-10-09 18:33:15 +0100

    Fix Unknwon to unknwon in cmd/environment_to_ini

--- cmd/environment_to_ini.go
+++ cmd/environment_to_ini.go
@@ -12,7 +12,7 @@ import (
 
 	"code.gitea.io/gitea/modules/log"
 	"code.gitea.io/gitea/modules/setting"
-	"github.com/Unknwon/com"
+	"github.com/unknwon/com"
 	"github.com/urfave/cli"
 	ini "gopkg.in/ini.v1"
 )
