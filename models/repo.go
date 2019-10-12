5e759b60cca3cd8484a6235fcc9120d18e8cd455
zeripath <art27@cantab.net>
2019-10-12 01:13:27 +0100

    Restore functionality for early gits (#7775)
    * Change tests to make it possible to run TestGit with 1.7.2
    
    * Make merge run on 1.7.2
    
    * Fix tracking and staging branch name problem
    
    * Ensure that git 1.7.2 works on tests
    
    * ensure that there is no chance for conflicts
    
    * Fix-up missing merge issues
    
    * Final rm
    
    * Ensure LFS filters run on the tests
    
    * Do not sign commits from temp repo
    
    * Restore tracking fetch change
    
    * Apply suggestions from code review
    
    * Update modules/repofiles/temp_repo.go

--- models/repo.go
+++ models/repo.go
@@ -1093,7 +1093,7 @@ func CleanUpMigrateInfo(repo *Repository) (*Repository, error) {
 		}
 	}
 
-	_, err := git.NewCommand("remote", "remove", "origin").RunInDir(repoPath)
+	_, err := git.NewCommand("remote", "rm", "origin").RunInDir(repoPath)
 	if err != nil && !strings.HasPrefix(err.Error(), "exit status 128 - fatal: No such remote ") {
 		return repo, fmt.Errorf("CleanUpMigrateInfo: %v", err)
 	}
