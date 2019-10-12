4cf51c5b7202bc132dc0cb7a4998ea16bbb30f8a
Andrew Thornton <art27@cantab.net>
2019-10-12 18:07:44 +0100

    Allow Protected Branches to Whitelist Deploy Keys

--- models/branches.go
+++ models/branches.go
@@ -31,9 +31,10 @@ type ProtectedBranch struct {
 	BranchName                string `xorm:"UNIQUE(s)"`
 	CanPush                   bool   `xorm:"NOT NULL DEFAULT false"`
 	EnableWhitelist           bool
-	WhitelistUserIDs          []int64            `xorm:"JSON TEXT"`
-	WhitelistTeamIDs          []int64            `xorm:"JSON TEXT"`
-	EnableMergeWhitelist      bool               `xorm:"NOT NULL DEFAULT false"`
+	WhitelistUserIDs          []int64 `xorm:"JSON TEXT"`
+	WhitelistTeamIDs          []int64 `xorm:"JSON TEXT"`
+	EnableMergeWhitelist      bool    `xorm:"NOT NULL DEFAULT false"`
+	WhitelistDeployKeys       bool
 	MergeWhitelistUserIDs     []int64            `xorm:"JSON TEXT"`
 	MergeWhitelistTeamIDs     []int64            `xorm:"JSON TEXT"`
 	EnableStatusCheck         bool               `xorm:"NOT NULL DEFAULT false"`
