4309549f5af1018f64d802e4c6e5aaa13e3d4add
Raphael Borg Ellul Vincenti <raphael@neo-tix.com>
2019-10-12 02:49:37 +0200

    Add v99.go to migrations.go

--- models/migrations/migrations.go
+++ models/migrations/migrations.go
@@ -252,6 +252,8 @@ var migrations = []Migration{
 	NewMigration("add repo_admin_change_team_access to user", addRepoAdminChangeTeamAccessColumnForUser),
 	// v98 -> v99
 	NewMigration("add original author name and id on migrated release", addOriginalAuthorOnMigratedReleases),
+	// v99 -> v100
+	NewMigration("rename signature column to support sha1 and sha256 webhook signatures", specifyWebhookSignatureType),
 }
 
 // Migrate database to current version
