1e9b33052553bb546ca526f04816579e2853c8da
oscar.lofwenhamn <44643697+oscarlofwenhamn@users.noreply.github.com>
2019-10-15 10:40:42 +0200

    Update CodeMirror to version 5.49.0 (#8381)
    * Update CodeMirror to version 5.49.0
    
    * Update CodeMirror versions in librejs and VERSIONS

--- public/vendor/plugins/codemirror/mode/modelica/modelica.js
+++ public/vendor/plugins/codemirror/mode/modelica/modelica.js
@@ -1,5 +1,5 @@
 // CodeMirror, copyright (c) by Marijn Haverbeke and others
-// Distributed under an MIT license: http://codemirror.net/LICENSE
+// Distributed under an MIT license: https://codemirror.net/LICENSE
 
 // Modelica support for CodeMirror, copyright (c) by Lennart Ochel
 
