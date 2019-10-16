8edf3d58048fce20cdde776c3deb2ca1d94ad8e7
Lunny Xiao <xiaolunwen@gmail.com>
2019-10-16 17:56:59 +0800

    Move sync mirror actions to mirror service package (#8518)
    * move sync mirror actions to mirror service
    
    * fix go.mod
    
    * fix lint
    
    * fix lint

--- services/mirror/mirror.go
+++ services/mirror/mirror.go
@@ -329,16 +329,16 @@ func SyncMirrors() {
 
 			// Create reference
 			if result.oldCommitID == gitShortEmptySha {
-				if err = models.MirrorSyncCreateAction(m.Repo, result.refName); err != nil {
-					log.Error("MirrorSyncCreateAction [repo_id: %d]: %v", m.RepoID, err)
+				if err = SyncCreateAction(m.Repo, result.refName); err != nil {
+					log.Error("SyncCreateAction [repo_id: %d]: %v", m.RepoID, err)
 				}
 				continue
 			}
 
 			// Delete reference
 			if result.newCommitID == gitShortEmptySha {
-				if err = models.MirrorSyncDeleteAction(m.Repo, result.refName); err != nil {
-					log.Error("MirrorSyncDeleteAction [repo_id: %d]: %v", m.RepoID, err)
+				if err = SyncDeleteAction(m.Repo, result.refName); err != nil {
+					log.Error("SyncDeleteAction [repo_id: %d]: %v", m.RepoID, err)
 				}
 				continue
 			}
@@ -359,13 +359,13 @@ func SyncMirrors() {
 				log.Error("CommitsBetweenIDs [repo_id: %d, new_commit_id: %s, old_commit_id: %s]: %v", m.RepoID, newCommitID, oldCommitID, err)
 				continue
 			}
-			if err = models.MirrorSyncPushAction(m.Repo, models.MirrorSyncPushActionOptions{
+			if err = SyncPushAction(m.Repo, SyncPushActionOptions{
 				RefName:     result.refName,
 				OldCommitID: oldCommitID,
 				NewCommitID: newCommitID,
 				Commits:     models.ListToPushCommits(commits),
 			}); err != nil {
-				log.Error("MirrorSyncPushAction [repo_id: %d]: %v", m.RepoID, err)
+				log.Error("SyncPushAction [repo_id: %d]: %v", m.RepoID, err)
 				continue
 			}
 		}
