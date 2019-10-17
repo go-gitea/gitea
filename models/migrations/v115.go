7947e9c29d418de88aa70ac23da99a995296922c
Masudur Rahman <masudjuly02@gmail.com>
2019-10-18 03:14:03 +0600

    Update user avatar name

--- models/migrations/v115.go
+++ models/migrations/v115.go
@@ -5,12 +5,11 @@
 package migrations
 
 import (
+	"crypto/md5"
 	"fmt"
 	"io"
 	"os"
 	"path/filepath"
-	"strconv"
-	"strings"
 
 	"code.gitea.io/gitea/modules/setting"
 
@@ -36,37 +35,35 @@ func renameExistingUserAvatarName(x *xorm.Engine) error {
 
 	deleteList := make([]string, 0, len(users))
 	for _, user := range users {
-		if !strings.HasPrefix(user.Avatar, strconv.FormatInt(user.ID, 10)+"-") {
-			oldAvatar := user.Avatar
-			newAvatar := fmt.Sprintf("%d-%s", user.ID, user.Avatar)
+		oldAvatar := user.Avatar
+		newAvatar := fmt.Sprintf("%x", md5.Sum([]byte(fmt.Sprintf("%d-%s", user.ID, user.Avatar))))
 
-			if _, err := os.Stat(filepath.Join(setting.AvatarUploadPath, oldAvatar)); err != nil {
-				continue
-			}
-
-			fr, err := os.Open(filepath.Join(setting.AvatarUploadPath, oldAvatar))
-			if err != nil {
-				return err
-			}
-			defer fr.Close()
-
-			fw, err := os.Create(filepath.Join(setting.AvatarUploadPath, newAvatar))
-			if err != nil {
-				return err
-			}
-			defer fw.Close()
-
-			if _, err := io.Copy(fw, fr); err != nil {
-				return err
-			}
-
-			user.Avatar = newAvatar
-			if _, err := sess.ID(user.ID).Update(&user); err != nil {
-				return err
-			}
-
-			deleteList = append(deleteList, filepath.Join(setting.AvatarUploadPath, oldAvatar))
+		if _, err := os.Stat(filepath.Join(setting.AvatarUploadPath, oldAvatar)); err != nil {
+			continue
 		}
+
+		fr, err := os.Open(filepath.Join(setting.AvatarUploadPath, oldAvatar))
+		if err != nil {
+			return err
+		}
+		defer fr.Close()
+
+		fw, err := os.Create(filepath.Join(setting.AvatarUploadPath, newAvatar))
+		if err != nil {
+			return err
+		}
+		defer fw.Close()
+
+		if _, err := io.Copy(fw, fr); err != nil {
+			return err
+		}
+
+		user.Avatar = newAvatar
+		if _, err := sess.ID(user.ID).Update(&user); err != nil {
+			return err
+		}
+
+		deleteList = append(deleteList, filepath.Join(setting.AvatarUploadPath, oldAvatar))
 	}
 	for _, file := range deleteList {
 		if err := os.Remove(file); err != nil {
