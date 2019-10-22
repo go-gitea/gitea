e07d21fa0a540fa0c7088b9a96129970f16186ea
Guillermo Prandi <guillep2k@users.noreply.github.com>
2019-10-22 18:38:26 -0300

    Add migration to drop unneeded columns

---
+++ models/migrations/v104.go
@@ -0,0 +1,27 @@
+// Copyright 2019 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package migrations
+
+import (
+	"xorm.io/xorm"
+)
+
+func removeLabelUneededCols(x *xorm.Engine) error {
+	sess := x.NewSession()
+	defer sess.Close()
+	if err := sess.Begin(); err != nil {
+		return err
+	}
+	if err := dropTableColumns(sess, "label", "query_string"); err != nil {
+		return err
+	}
+	if err := dropTableColumns(sess, "label", "is_selected"); err != nil {
+		return err
+	}
+	if err := sess.Commit(); err != nil {
+		return err
+	}
+	return nil
+}
