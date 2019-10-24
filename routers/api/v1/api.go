1cc00d1e4bea63b7e49a3d0d4efe9157c02bd14e
Lunny Xiao <xiaolunwen@gmail.com>
2019-10-24 13:32:40 +0800

    API should follow RequireSignInView (#8654)

--- routers/api/v1/api.go
+++ routers/api/v1/api.go
@@ -860,7 +860,7 @@ func RegisterRoutes(m *macaron.Macaron) {
 		m.Group("/topics", func() {
 			m.Get("/search", repo.TopicSearch)
 		})
-	}, securityHeaders(), context.APIContexter(), sudo())
+	}, securityHeaders(), reqTokenBySetting(), context.APIContexter(), sudo())
 }
 
 func securityHeaders() macaron.Handler {
@@ -871,4 +871,11 @@ func securityHeaders() macaron.Handler {
 			w.Header().Set("x-content-type-options", "nosniff")
 		})
 	}
+}
+
+func reqTokenBySetting() macaron.Handler {
+	if setting.Service.RequireSignInView {
+		return reqToken()
+	}
+	return func(ctx *macaron.Context) {}
 }
