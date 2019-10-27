c0bf6cb6778158aece0232901ea6fdb92f40adc2
LukBukkit <luk.bukkit@gmail.com>
2019-10-27 10:43:37 +0100

    A single variable to check whether NumLines is set
    Signed-off-by: LukBukkit <luk.bukkit@gmail.com>

--- routers/repo/view.go
+++ routers/repo/view.go
@@ -307,12 +307,11 @@ func renderFile(ctx *context.Context, entry *git.TreeEntry, treeLink, rawLink st
 			var output bytes.Buffer
 			lines := strings.Split(fileContent, "\n")
 			ctx.Data["NumLines"] = len(lines)
-			ctx.Data["NumLinesZero"] = false
 			if len(lines) == 1 && lines[0] == "" {
 				// If the file is completely empty, we show zero lines at the line counter
 				ctx.Data["NumLines"] = 0
-				ctx.Data["NumLinesZero"] = true
 			}
+			ctx.Data["NumLinesSet"] = true
 
 			//Remove blank line at the end of file
 			if len(lines) > 0 && lines[len(lines)-1] == "" {
