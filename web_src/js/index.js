20f10af3cf442d4f16f040f82b330fd3a9a8ed8a
Gitea <gitea@fake.local>
2019-11-21 23:18:09 +0000

    Branch/tag refreshed without page reload

--- web_src/js/index.js
+++ web_src/js/index.js
@@ -99,11 +99,9 @@ function initBranchSelector() {
   const $selectBranch = $('.ui.select-branch');
   const $branchMenu = $selectBranch.find('.reference-list-menu');
   $branchMenu.find('.item:not(.no-select)').click(function () {
-    let newValue = $selectBranch.find('.ui .branch-name').text();
     const selectedValue = $(this).data('id');
     const editMode = $('#editing_mode').val();
     $($(this).data('id-selector')).val(selectedValue);
-    $selectBranch.find('.ui .branch-name').text(selectedValue);
 
     if (editMode === 'true') {
       const form = $('#update_issueref_form');
@@ -113,11 +111,9 @@ function initBranchSelector() {
         ref: selectedValue
       },
       (data) => {
-        newValue = data.ref;
+        $selectBranch.find('.ui .branch-name').text(data.ref);
       });
     }
-
-    $selectBranch.find('.ui .branch-name').text(newValue);
   });
   $selectBranch.find('.reference.column').click(function () {
     $selectBranch.find('.scrolling.reference-list-menu').css('display', 'none');
