d5f2247afcf8b491028dbd695f0d8d284ff14e4c
jolheiser <john.olheiser@gmail.com>
2019-11-22 11:47:40 -0600

    Restructure for services
    Signed-off-by: jolheiser <john.olheiser@gmail.com>

---
+++ services/repository/generate.go
@@ -0,0 +1,61 @@
+// Copyright 2019 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package repository
+
+import (
+	"code.gitea.io/gitea/models"
+	"code.gitea.io/gitea/modules/log"
+	"code.gitea.io/gitea/modules/notification"
+)
+
+// GenerateRepository generates a repository from a template
+func GenerateRepository(doer, owner *models.User, templateRepo *models.Repository, opts models.GenerateRepoOptions) (*models.Repository, error) {
+	generateRepo, err := models.GenerateRepository(doer, owner, templateRepo, opts)
+	if err != nil {
+		if generateRepo != nil {
+			if errDelete := models.DeleteRepository(doer, owner.ID, generateRepo.ID); errDelete != nil {
+				log.Error("Rollback deleteRepository: %v", errDelete)
+			}
+		}
+		return nil, err
+	}
+
+	ctx, sess, err := models.TxDBContext()
+	if err != nil {
+		return generateRepo, nil
+	}
+
+	// Git Content
+	if opts.GitContent && !templateRepo.IsEmpty {
+		if err := models.GenerateGitContent(ctx, templateRepo, generateRepo); err != nil {
+			return generateRepo, nil
+		}
+	}
+
+	// Topics
+	if opts.Topics {
+		if err := models.GenerateTopics(ctx, templateRepo, generateRepo); err != nil {
+			return generateRepo, nil
+		}
+	}
+
+	// Git Hooks
+	if opts.GitHooks {
+		if err := models.GenerateGitHooks(ctx, templateRepo, generateRepo); err != nil {
+			return generateRepo, nil
+		}
+	}
+
+	// Webhooks
+	if opts.Webhooks {
+		if err := models.GenerateWebhooks(ctx, templateRepo, generateRepo); err != nil {
+			return generateRepo, nil
+		}
+	}
+
+	notification.NotifyCreateRepository(doer, owner, generateRepo)
+
+	return generateRepo, sess.Commit()
+}
