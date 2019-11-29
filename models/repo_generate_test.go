981e18294e5a599dee5ac9cf76926d7ed6a03ec4
jolheiser <john.olheiser@gmail.com>
2019-11-28 21:26:19 -0600

    Nits
    Signed-off-by: jolheiser <john.olheiser@gmail.com>

--- models/repo_generate_test.go
+++ models/repo_generate_test.go
@@ -1,8 +1,13 @@
+// Copyright 2019 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 package models
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 var giteaTemplate = []byte(`
