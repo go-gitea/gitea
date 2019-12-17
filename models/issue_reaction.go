fa70f4a68d41f3497e7d743774f0b26582636aa3
6543 <6543@obermui.de>
2019-12-17 17:05:43 +0100

    Hide not allowed Reactions

--- models/issue_reaction.go
+++ models/issue_reaction.go
@@ -68,9 +68,18 @@ func FindIssueReactions(issue *Issue) (ReactionList, error) {
 func findReactions(e Engine, opts FindReactionsOptions) ([]*Reaction, error) {
 	reactions := make([]*Reaction, 0, 10)
 	sess := e.Where(opts.toConds())
-	return reactions, sess.
-		Asc("reaction.issue_id", "reaction.comment_id", "reaction.created_unix", "reaction.id").
+	err := sess.Asc("reaction.issue_id", "reaction.comment_id", "reaction.created_unix", "reaction.id").
 		Find(&reactions)
+	if err != nil {
+		return nil, err
+	}
+	var result []*Reaction
+	for _, r := range reactions {
+		if setting.UI.ReactionsMap[r.Type] {
+			result = append(result, r)
+		}
+	}
+	return result, nil
 }
 
 func createReaction(e *xorm.Session, opts *ReactionOptions) (*Reaction, error) {
