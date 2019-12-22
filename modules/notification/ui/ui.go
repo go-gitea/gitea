ff3bba261e0c370d8dcf8f8af4a470e3ba43769b
Andrew Thornton <art27@cantab.net>
2019-12-22 23:46:55 +0000

    broken merge

--- modules/notification/ui/ui.go
+++ modules/notification/ui/ui.go
@@ -71,7 +71,7 @@ func (ns *notificationService) NotifyNewIssue(issue *models.Issue) {
 }
 
 func (ns *notificationService) NotifyIssueChangeStatus(doer *models.User, issue *models.Issue, actionComment *models.Comment, isClosed bool) {
-	ns.issueQueue <- issueNotificationOpts{
+	_ = ns.issueQueue.Push(issueNotificationOpts{
 		issueID:              issue.ID,
 		notificationAuthorID: doer.ID,
 	})
