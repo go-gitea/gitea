bae6724216f5b3bf367910e15a9b3d0b4197d495
6543 <6543@obermui.de>
2020-01-04 03:45:41 +0100

    use Where not ID to get mssql

--- models/org_team.go
+++ models/org_team.go
@@ -590,7 +590,7 @@ func UpdateTeam(t *Team, authChanged bool, includeAllChanged bool) (err error) {
 		return ErrTeamAlreadyExist{t.OrgID, t.LowerName}
 	}
 
-	if _, err = sess.ID(t.ID).AllCols().Update(t); err != nil {
+	if _, err = sess.Where("team.id = ?", t.ID).AllCols().Update(t); err != nil {
 		return fmt.Errorf("update: %v", err)
 	}
 
