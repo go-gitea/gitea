bc573b636dad7efad17919f487a6142935eb1bb1
Lunny Xiao <xiaolunwen@gmail.com>
2020-01-11 19:42:33 +0800

    Add documentation

--- cmd/doctor.go
+++ cmd/doctor.go
@@ -6,6 +6,7 @@ package cmd
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
@@ -31,16 +32,18 @@ type check struct {
 	f     func(ctx *cli.Context) ([]string, error)
 }
 
+// checklist represents list for all checks
 var checklist = []check{
 	{
-		title: "Check if openssh authorized_keys file id correct",
+		title: "Check if OpenSSH authorized_keys file id correct",
 		f:     runDoctorLocationMoved,
 	},
+	// more checks please append here
 }
 
 func runDoctor(ctx *cli.Context) error {
 	err := initDB()
-	fmt.Println("Using app.ini at ", setting.CustomConf)
+	fmt.Println("Using app.ini at", setting.CustomConf)
 	if err != nil {
 		fmt.Println(err)
 		fmt.Println("Check if you are using the right config file. You can use a --config directive to specify one.")
@@ -49,7 +52,6 @@ func runDoctor(ctx *cli.Context) error {
 
 	for i, check := range checklist {
 		fmt.Println("[", i+1, "]", check.title)
-		fmt.Println()
 		if messages, err := check.f(ctx); err != nil {
 			fmt.Println("Error:", err)
 		} else if len(messages) > 0 {
@@ -73,7 +75,7 @@ func exePath() (string, error) {
 }
 
 func runDoctorLocationMoved(ctx *cli.Context) ([]string, error) {
-	if setting.SSH.StartBuiltinServer {
+	if setting.SSH.StartBuiltinServer || !setting.SSH.CreateAuthorizedKeysFile {
 		return nil, nil
 	}
 
@@ -87,10 +89,11 @@ func runDoctorLocationMoved(ctx *cli.Context) ([]string, error) {
 	var firstline string
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
-		firstline = scanner.Text()
-		if !strings.HasPrefix(firstline, "#") {
-			break
+		firstline = strings.TrimSpace(scanner.Text())
+		if len(firstline) == 0 || firstline[0] == '#' {
+			continue
 		}
+		break
 	}
 
 	// command="/Volumes/data/Projects/gitea/gitea/gitea --config
@@ -98,7 +101,10 @@ func runDoctorLocationMoved(ctx *cli.Context) ([]string, error) {
 		exp := regexp.MustCompile(`^[ \t]*(?:command=")([^ ]+) --config='([^']+)' serv key-([^"]+)",(?:[^ ]+) ssh-rsa ([^ ]+) ([^ ]+)[ \t]*$`)
 
 		// command="/home/user/gitea --config='/home/user/etc/app.ini' serv key-999",option-1,option-2,option-n ssh-rsa public-key-value key-name
-		res := exp.FindAllStringSubmatch(firstline, -1)
+		res := exp.FindStringSubmatch(firstline)
+		if res == nil {
+			return nil, errors.New("Unknow authorized_keys format")
+		}
 
 		giteaPath := res[1] // => /home/user/gitea
 		iniPath := res[2]   // => /home/user/etc/app.ini
@@ -112,11 +118,11 @@ func runDoctorLocationMoved(ctx *cli.Context) ([]string, error) {
 			return nil, err
 		}
 
-		if len(giteaPath) > 0 && giteaPath[0] != p {
-			return []string{fmt.Sprintf("Gitea exe path wants %s but %s on %s", p, giteaPath[0], fPath)}, nil
+		if len(giteaPath) > 0 && giteaPath != p {
+			return []string{fmt.Sprintf("Gitea exe path wants %s but %s on %s", p, giteaPath, fPath)}, nil
 		}
-		if len(iniPath) > 0 && iniPath[0] != setting.CustomConf {
-			return []string{fmt.Sprintf("Gitea config path wants %s but %s on %s", setting.CustomConf, iniPath[0], fPath)}, nil
+		if len(iniPath) > 0 && iniPath != setting.CustomConf {
+			return []string{fmt.Sprintf("Gitea config path wants %s but %s on %s", setting.CustomConf, iniPath, fPath)}, nil
 		}
 	}
 
