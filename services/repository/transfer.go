61b9c306c5478104aaa9596a5ab718b26c72a958
Lunny Xiao <xiaolunwen@gmail.com>
2020-01-14 10:33:04 +0800

    Move RepoWorkingPool to services/repository package

--- services/repository/transfer.go
+++ services/repository/transfer.go
@@ -7,9 +7,13 @@ package repository
 import (
 	"code.gitea.io/gitea/models"
 	"code.gitea.io/gitea/modules/notification"
+	"code.gitea.io/gitea/modules/sync"
 
 	"github.com/unknwon/com"
 )
+
+// RepoWorkingPool represents a working pool to order the parallel changes to the same repository
+var RepoWorkingPool = sync.NewExclusivePool()
 
 // TransferOwnership transfers all corresponding setting from old user to new one.
 func TransferOwnership(doer *models.User, newOwnerName string, repo *models.Repository) error {
@@ -19,12 +23,12 @@ func TransferOwnership(doer *models.User, newOwnerName string, repo *models.Repo
 
 	oldOwner := repo.Owner
 
-	models.RepoWorkingPool.CheckIn(com.ToStr(repo.ID))
+	RepoWorkingPool.CheckIn(com.ToStr(repo.ID))
 	if err := models.TransferOwnership(doer, newOwnerName, repo); err != nil {
-		models.RepoWorkingPool.CheckOut(com.ToStr(repo.ID))
+		RepoWorkingPool.CheckOut(com.ToStr(repo.ID))
 		return err
 	}
-	models.RepoWorkingPool.CheckOut(com.ToStr(repo.ID))
+	RepoWorkingPool.CheckOut(com.ToStr(repo.ID))
 
 	notification.NotifyTransferRepository(doer, repo, oldOwner.Name)
 
@@ -39,12 +43,12 @@ func ChangeRepositoryName(doer *models.User, repo *models.Repository, newRepoNam
 	// repo so that we can atomically rename the repo path and updates the
 	// local copy's origin accordingly.
 
-	models.RepoWorkingPool.CheckIn(com.ToStr(repo.ID))
+	RepoWorkingPool.CheckIn(com.ToStr(repo.ID))
 	if err := models.ChangeRepositoryName(doer, repo, newRepoName); err != nil {
-		models.RepoWorkingPool.CheckOut(com.ToStr(repo.ID))
+		RepoWorkingPool.CheckOut(com.ToStr(repo.ID))
 		return err
 	}
-	models.RepoWorkingPool.CheckOut(com.ToStr(repo.ID))
+	RepoWorkingPool.CheckOut(com.ToStr(repo.ID))
 
 	notification.NotifyRenameRepository(doer, repo, oldRepoName)
 
