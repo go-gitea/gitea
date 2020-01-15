a223c1255855f23a0450803adcd312df03b644ff
Guillermo Prandi <guillep2k@users.noreply.github.com>
2020-01-15 20:44:27 -0300

    Fix test

--- integrations/user_repo_unit_test.go
+++ integrations/user_repo_unit_test.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
-package migrations
+package integrations
 
 import (
 	"testing"
@@ -16,5 +16,5 @@ import (
 
 func TestUserRepoUnit(t *testing.T) {
 
-	return models.UserRepoUnitTest(t)
+	models.UserRepoUnitTest(t)
 }
