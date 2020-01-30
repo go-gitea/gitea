3144375a1c0a3110ca095c1b1a406b6c42e59bc3
guillep2k <18600385+guillep2k@users.noreply.github.com>
2020-01-29 21:29:52 -0300

    Isn't the linter a sweet? (1)

--- modules/options/static.go
+++ modules/options/static.go
@@ -113,6 +113,7 @@ func fileFromDir(name string) ([]byte, error) {
 	return ioutil.ReadAll(f)
 }
 
+// IsDynamic will return false when using embedded data (-tags bindata)
 func IsDynamic() bool {
 	return false
 }
