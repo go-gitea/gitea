f8ca63f503986b730aa8d9975112446f6e0fbed4
Lunny Xiao <xiaolunwen@gmail.com>
2020-01-30 13:03:32 +0800

    Cache last commit to accelerate the repository directory page visit

--- modules/setting/cache.go
+++ modules/setting/cache.go
@@ -13,31 +13,81 @@ import (
 
 // Cache represents cache settings
 type Cache struct {
+	Enabled  bool
 	Adapter  string
 	Interval int
 	Conn     string
-	TTL      time.Duration
+	TTL      time.Duration `ini:"ITEM_TTL"`
 }
 
 var (
 	// CacheService the global cache
-	CacheService *Cache
+	CacheService = struct {
+		Cache
+
+		LastCommit struct {
+			Cache
+			CommitsCount int64
+		} `ini:"cache.last_commit"`
+	}{
+		Cache: Cache{
+			Enabled:  true,
+			Adapter:  "memory",
+			Interval: 60,
+			TTL:      16 * time.Hour,
+		},
+		LastCommit: struct {
+			Cache
+			CommitsCount int64
+		}{
+			Cache: Cache{
+				Enabled:  true,
+				Adapter:  "memory",
+				Interval: 60,
+				TTL:      86400 * time.Hour,
+			},
+			CommitsCount: 1000,
+		},
+	}
 )
 
 func newCacheService() {
 	sec := Cfg.Section("cache")
-	CacheService = &Cache{
-		Adapter: sec.Key("ADAPTER").In("memory", []string{"memory", "redis", "memcache"}),
+	if err := sec.MapTo(&CacheService); err != nil {
+		log.Fatal("Failed to map Cache settings: %v", err)
 	}
+
+	CacheService.Adapter = sec.Key("ADAPTER").In("memory", []string{"memory", "redis", "memcache"})
 	switch CacheService.Adapter {
 	case "memory":
-		CacheService.Interval = sec.Key("INTERVAL").MustInt(60)
 	case "redis", "memcache":
 		CacheService.Conn = strings.Trim(sec.Key("HOST").String(), "\" ")
+	case "": // disable cache
+		CacheService.Enabled = false
 	default:
 		log.Fatal("Unknown cache adapter: %s", CacheService.Adapter)
 	}
-	CacheService.TTL = sec.Key("ITEM_TTL").MustDuration(16 * time.Hour)
 
-	log.Info("Cache Service Enabled")
+	if CacheService.Enabled {
+		log.Info("Cache Service Enabled")
+	}
+
+	sec = Cfg.Section("cache.last_commit")
+
+	CacheService.LastCommit.Adapter = sec.Key("ADAPTER").In("memory", []string{"memory", "redis", "memcache"})
+	switch CacheService.LastCommit.Adapter {
+	case "memory":
+	case "redis", "memcache":
+		CacheService.LastCommit.Conn = strings.Trim(sec.Key("HOST").String(), "\" ")
+	case "": // disable cache
+		CacheService.LastCommit.Enabled = false
+	default:
+		log.Fatal("Unknown cache.last_commit adapter: %s", CacheService.LastCommit.Adapter)
+	}
+
+	CacheService.LastCommit.CommitsCount = sec.Key("COMMITS_COUNT").MustInt64(1000)
+
+	if CacheService.LastCommit.Enabled {
+		log.Info("Last Commit Cache Service Enabled")
+	}
 }
