265497d07ba42518d92ed77c0de1fc949411fa05
Lauris Bukšis-Haberkorns <lauris@nix.lv>
2020-02-03 17:33:01 +0200

    Use go-humanize

--- modules/base/tool.go
+++ modules/base/tool.go
@@ -13,7 +13,6 @@ import (
 	"encoding/hex"
 	"fmt"
 	"io"
-	"math"
 	"net/http"
 	"net/url"
 	"os"
@@ -29,6 +28,7 @@ import (
 	"code.gitea.io/gitea/modules/log"
 	"code.gitea.io/gitea/modules/setting"
 
+	"github.com/dustin/go-humanize"
 	"github.com/unknwon/com"
 )
 
@@ -214,62 +214,15 @@ func AvatarLink(email string) string {
 	return SizedAvatarLink(email, DefaultAvatarSize)
 }
 
-// Storage space size types
-const (
-	Byte  = 1
-	KByte = Byte * 1024
-	MByte = KByte * 1024
-	GByte = MByte * 1024
-	TByte = GByte * 1024
-	PByte = TByte * 1024
-	EByte = PByte * 1024
-)
-
-func logn(n, b float64) float64 {
-	return math.Log(n) / math.Log(b)
-}
-
-func humanateBytes(s uint64, base float64, sizes []string) string {
-	if s < 10 {
-		return fmt.Sprintf("%dB", s)
-	}
-	e := math.Floor(logn(float64(s), base))
-	suffix := sizes[int(e)]
-	val := float64(s) / math.Pow(base, math.Floor(e))
-	f := "%.0f"
-	if val < 10 {
-		f = "%.1f"
-	}
-
-	return fmt.Sprintf(f+"%s", val, suffix)
-}
-
 // FileSize calculates the file size and generate user-friendly string.
 func FileSize(s int64) string {
-	sizes := []string{"B", "KB", "MB", "GB", "TB", "PB", "EB"}
-	return humanateBytes(uint64(s), 1024, sizes)
+	return humanize.IBytes(uint64(s))
 }
 
-// PrettyNumber formats a number string representation with thousand separator as spaces.
-func PrettyNumber(i int64) string {
-	s := strconv.FormatInt(i, 10)
-	r1 := ""
-	idx := 0
-
-	for p := len(s) - 1; p >= 0; p-- {
-		idx++
-		if idx == 4 {
-			idx = 1
-			r1 += ","
-		}
-		r1 += string(s[p])
-	}
-
-	r2 := ""
-	for p := len(r1) - 1; p >= 0; p-- {
-		r2 += string(r1[p])
-	}
-	return r2
+// PrettyNumber produces a string form of the given number in base 10 with
+// commas after every three orders of magnitud
+func PrettyNumber(v int64) string {
+	return humanize.Comma(v)
 }
 
 // Subtract deals with subtraction of all types of number.
