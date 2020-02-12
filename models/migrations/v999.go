757b29b77b5b7a00c31b0e352b8894a602099118
6543 <6543@obermui.de>
2020-02-13 00:04:30 +0100

    make migration great again

--- models/migrations/v999.go
+++ models/migrations/v999.go
@@ -24,34 +24,37 @@ func addIssueWatchModes(x *xorm.Engine) error {
 		UpdatedUnix timeutil.TimeStamp    `xorm:"updated NOT NULL"`
 		//since it it is not used anymore and has NOT NULL constrain
 		//it is altered to have a default value - we can drop it later ...
-		IsWatching bool `xorm:"DEFAULT NULL"`
+		IsWatching bool
 	}
 
+	if x.Dialect().DBType() == core.SQLITE {
+		_, _ = x.Exec("DROP TABLE `issue_watch_old`;")
+		_, _ = x.Exec("DROP INDEX `UQE_issue_watch_watch`;")
+
+		if _, err := x.Exec("ALTER TABLE `issue_watch` RENAME TO `issue_watch_old`;"); err != nil {
+			return err
+		}
+
+		if err := x.Sync2(new(IssueWatch)); err != nil {
+			_, _ = x.Exec("ALTER TABLE `issue_watch_old` RENAME TO `issue_watch`;")
+			return fmt.Errorf("Sync2: %v", err)
+		}
+
+		if _, err := x.Exec("INSERT INTO `issue_watch` (user_id,issue_id,is_watching,created_unix,updated_unix) SELECT user_id,issue_id,is_watching,created_unix,updated_unix FROM `issue_watch_old`;"); err != nil {
+			return err
+		}
+		if _, err := x.Exec("DROP TABLE `issue_watch_old`;"); err != nil {
+			return err
+		}
+	}
 	sess := x.NewSession()
 	defer sess.Close()
 	if err := sess.Begin(); err != nil {
 		return err
 	}
-
-	if x.Dialect().DBType() == core.SQLITE {
-		if _, err := sess.Exec("ALTER TABLE `issue_watch` RENAME TO `issue_watch_old`;"); err != nil {
-			return err
-		}
-	}
-	if x.Dialect().DBType() == core.MSSQL {
-		if _, err := sess.Exec("sp_rename `issue_watch` `issue_watch_old`;"); err != nil {
-			return err
-		}
-	}
-	if err := x.Sync2(new(IssueWatch)); err != nil {
-		return fmt.Errorf("Sync2: %v", err)
-	}
-	if x.Dialect().DBType() == core.SQLITE || x.Dialect().DBType() == core.MSSQL {
-		if _, err := sess.Exec("INSERT INTO `issue_watch` SELECT * FROM `issue_watch_old`;"); err != nil {
-			return err
-		}
-		if err := sess.DropTable("issue_watch_old"); err != nil {
-			return err
+	if x.Dialect().DBType() != core.SQLITE {
+		if err := sess.Sync2(new(IssueWatch)); err != nil {
+			return fmt.Errorf("Sync2: %v", err)
 		}
 	}
 
@@ -65,12 +68,18 @@ func addIssueWatchModes(x *xorm.Engine) error {
 	//sqlite is done from L36-50 (you cant alter a column)
 	switch x.Dialect().DBType() {
 	case core.POSTGRES:
-	case core.ORACLE:
+		if _, err := sess.Exec("ALTER TABLE issue_watch ALTER COLUMN is_watching DROP NOT NULL;"); err != nil {
+			return err
+		}
+	case core.MSSQL:
+		if _, err := sess.Exec("ALTER TABLE issue_watch ALTER COLUMN is_watching bit NULL;"); err != nil {
+			return err
+		}
 	case core.MYSQL:
 		if _, err := sess.Exec("ALTER TABLE `issue_watch` MODIFY `is_watching` tinyint(1) NULL;"); err != nil {
 			return err
 		}
 	}
 
-	return nil
+	return sess.Commit()
 }
