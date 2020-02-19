7a0c75b198dd64740bcb4bb075e1983984b60ab9
6543 <6543@obermui.de>
2020-02-19 01:41:14 +0100

    test async

--- models/notification.go
+++ models/notification.go
@@ -176,9 +176,11 @@ func createOrUpdateIssueNotifications(e Engine, issueID, commentID int64, notifi
 
 	// if Issue/Comment creator/updater is not subscribed ... auto subscribe to issue
 	if _, ok := toNotify[notificationAuthorID]; !ok {
-		if err = CreateOrUpdateIssueWatchMode(notificationAuthorID, issueID, IssueWatchModeAuto); err != nil {
-			return err
-		}
+		go func() {
+			if err := CreateOrUpdateIssueWatchMode(notificationAuthorID, issueID, IssueWatchModeAuto); err != nil {
+				log.Error(err.Error())
+			}
+		}()
 	}
 
 	// notify
