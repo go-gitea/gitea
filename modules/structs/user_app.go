d4fdfd6589700fd3a82c0ccebc75a1bb3c2791eb
Gustavo Marin <gustavo.marin@intelygenz.com>
2020-02-21 09:04:07 +0100

    move endpoint to /user. Add swagger documentations and proper response type.

--- modules/structs/user_app.go
+++ modules/structs/user_app.go
@@ -32,3 +32,19 @@ type AccessTokenList []*AccessToken
 type CreateAccessTokenOption struct {
 	Name string `json:"name" binding:"Required"`
 }
+
+// CreateOAuth2ApplicationOptions holds options to create an oauth2 application
+// swagger:parameters userCreateOAuth2Application
+type CreateOAuth2ApplicationOptions struct {
+	Name         string   `json:"name" binding:"Required"`
+	RedirectURIs []string `json:"redirect-uris" binding:"Required"`
+}
+
+// OAuth2Application represents an OAuth2 application.
+// swagger:response OAuth2Application
+type OAuth2Application struct {
+	Name         string   `json:"name"`
+	ClientID     string   `json:"client-id"`
+	ClientSecret string   `json:"client-secret"`
+	RedirectURIs []string `json:"redirect-uris"`
+}
