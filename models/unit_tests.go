96abb4b537b7e6d1ccf2ad8292134efecd8886f6
guillep2k <18600385+guillep2k@users.noreply.github.com>
2020-02-26 20:51:37 -0300

    Fix SQLite concurrency problems by using BEGIN IMMEDIATE (#10368)
    * Test locking immediate for SQLite3
    
    * fix url field separator
    
    Co-authored-by: Lunny Xiao <xiaolunwen@gmail.com>
    Co-authored-by: zeripath <art27@cantab.net>

--- models/unit_tests.go
+++ models/unit_tests.go
@@ -84,7 +84,7 @@ func MainTest(m *testing.M, pathToGiteaRoot string) {
 
 func createTestEngine(fixturesDir string) error {
 	var err error
-	x, err = xorm.NewEngine("sqlite3", "file::memory:?cache=shared")
+	x, err = xorm.NewEngine("sqlite3", "file::memory:?cache=shared&_txlock=immediate")
 	if err != nil {
 		return err
 	}
