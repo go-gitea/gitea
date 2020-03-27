13fadae071770d1ec330bc413aacfe20be73d9a4
j. mccann <info@mrsdizzie.com>
2020-03-27 10:15:39 -0400

    update migration again after rebase with master

--- models/migrations/migrations.go
+++ models/migrations/migrations.go
@@ -200,8 +200,8 @@ var migrations = []Migration{
 	NewMigration("Add Branch Protection Protected Files Column", addBranchProtectionProtectedFilesColumn),
 	// v133 -> v134
 	NewMigration("Add EmailHash Table", addEmailHashTable),
-  // v134 -> v135
-  NewMigration("Add OrgID column to Labels table", addOrgIDLabelColumn),
+	// v134 -> v135
+	NewMigration("Add OrgID column to Labels table", addOrgIDLabelColumn),
 }
 
 // Migrate database to current version
