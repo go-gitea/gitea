1f7effec03768034c5009367f9b67a1bb4f5a41d
6543 <6543@obermui.de>
2020-04-04 00:23:29 +0200

    migrate already open pulls

--- models/migrations/v999.go
+++ models/migrations/v999.go
@@ -5,6 +5,12 @@
 package migrations
 
 import (
+	"fmt"
+
+	"code.gitea.io/gitea/models"
+	"code.gitea.io/gitea/modules/setting"
+	pull_service "code.gitea.io/gitea/services/pull"
+
 	"xorm.io/xorm"
 )
 
@@ -13,5 +19,44 @@ func addCommitDivergenceToPulls(x *xorm.Engine) error {
 		CommitsAhead  int
 		CommitsBehind int
 	}
-	return x.Sync2(new(PullRequest))
+	if err := x.Sync2(new(PullRequest)); err != nil {
+		return fmt.Errorf("Sync2: %v", err)
+	}
+
+	var last int
+	batchSize := setting.Database.IterateBufferSize
+	sess := x.NewSession()
+	defer sess.Close()
+	for {
+		if err := sess.Begin(); err != nil {
+			return err
+		}
+		var results = make([]*models.PullRequest, 0, batchSize)
+		err := sess.Where("has_merged = ?", false).OrderBy("id").Limit(batchSize, last).Find(&results)
+		if err != nil {
+			return err
+		}
+		if len(results) == 0 {
+			break
+		}
+		last += len(results)
+
+		for _, pr := range results {
+			divergence, err := pull_service.GetDiverging(pr)
+			if err != nil {
+				return fmt.Errorf("GetDiverging: %v", err)
+			} else {
+				pr.CommitsAhead = divergence.Ahead
+				pr.CommitsBehind = divergence.Behind
+				if _, err = sess.ID(pr.ID).Cols("commits_ahead", "commits_behind").Update(pr); err != nil {
+					return fmt.Errorf("Update Cols: %v", err)
+				}
+			}
+		}
+
+		if err := sess.Commit(); err != nil {
+			return err
+		}
+	}
+	return nil
 }
