f3195c4386bd75387cc44caa51b5ac3df0640f6a
Guillermo Prandi <guillep2k@users.noreply.github.com>
2020-04-04 19:09:32 -0300

    Fix tag position

--- build/vendor.go
+++ build/vendor.go
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
-package build
-
 //+build vendor
+
+package build
 
 import (
 	// for lint
