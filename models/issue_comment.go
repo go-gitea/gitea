ef89e75d0eb232e98ca7a7ef278b8681c7f4fe50
赵智超 <1012112796@qq.com>
2020-04-07 00:33:34 +0800

    add request review from specific reviewers feature in pull request (#10756)
    * add request review feature in pull request
    
    add a way to notify specific reviewers to review like github , by add  or delet a  special type
    review . The acton is  is similar to Assign ,  so many code reuse the function and items of
    Assignee, but the meaning and result is different.
    
    The Permission style is is similar to github, that only writer can add a review request from Reviewers,
    but the poster can recall and remove a review request after a reviwer has revied even if he don't have
    Write Premission. only manager , the poster and reviewer of a request review can remove it.
    
    The reviewers can be requested to review contain all readers for private repo , for public, contain
    all writers and watchers.
    
    The offical Review Request will block merge if Reject can block it.
    
    an other change: add ui otify for Assignees.
    
    Co-authored-by: guillep2k <18600385+guillep2k@users.noreply.github.com>
    Co-authored-by: Lauris BH <lauris@nix.lv>
    
    Signed-off-by: a1012112796 <1012112796@qq.com>
    
    * new change
    
    * add placeholder string
    
    * do some changes follow #10238 to add review requests num on lists also
    change icon for review requests to eye
    
    Co-authored-by: Lauris BH <lauris@nix.lv>

--- models/issue_comment.go
+++ models/issue_comment.go
@@ -86,6 +86,8 @@ const (
 	CommentTypeChangeTargetBranch
 	// Delete time manual for time tracking
 	CommentTypeDeleteTimeManual
+	// add or remove Request from one
+	CommentTypeReviewRequest
 )
 
 // CommentTag defines comment tag type
