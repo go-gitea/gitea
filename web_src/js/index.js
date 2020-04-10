f962c5e9dedfdf839bea8d34891140cb5ef962a5
a1012112796 <1012112796@qq.com>
2020-04-10 22:44:18 +0800

    fix lint

--- web_src/js/index.js
+++ web_src/js/index.js
@@ -2562,18 +2562,17 @@ $(document).ready(async () => {
     $(e).click();
   });
 
-  
   $('.resolve-conversation').on('click', function (e) {
     e.preventDefault();
     const id = $(this).data('comment-id');
     const action = $(this).data('action');
-    const issue = $(this).data('issue-id');
+    const issue_id = $(this).data('issue-id');
     const url = $(this).data('update-url');
 
     $.post(url, {
       _csrf: csrf,
-      action: action,
-      issue_id: issue,
+      action,
+      issue_id,
       comment_id: id,
     }).then(reload);
   });
