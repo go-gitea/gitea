4ce64a155ca9320d43e56ed4ba4406e3cae0af0f
6543 <6543@obermui.de>
2020-04-13 19:19:15 +0200

    Fix MergeConflict relicts and fronted Lint (#6)
    * some fixes
    clectecd from conflict resolve and so on
    
    * Fix Frontend LINT errors

--- web_src/js/features/projects.js
+++ web_src/js/features/projects.js
@@ -3,7 +3,7 @@ export default async function initProject(csrf) {
     return;
   }
 
-  const { Sortable } = await import(
+  const {Sortable} = await import(
     /* webpackChunkName: "sortable" */ 'sortablejs'
   );
 
@@ -17,7 +17,7 @@ export default async function initProject(csrf) {
       {
         group: 'shared',
         animation: 150,
-        onAdd: e => {
+        onAdd: (e) => {
           $.ajax(`${e.to.dataset.url}/${e.item.dataset.issue}`, {
             headers: {
               'X-Csrf-Token': csrf,
@@ -34,7 +34,7 @@ export default async function initProject(csrf) {
     );
   }
 
-  $('.edit-project-board').each(function() {
+  $('.edit-project-board').each(function () {
     // const modal = $(this);
 
     const projectTitle = $(this).find(
@@ -43,26 +43,27 @@ export default async function initProject(csrf) {
 
     $(this)
       .find('.content > .form > .actions > .red')
-      .click(function(e) {
+      .click(function (e) {
         e.preventDefault();
         $.ajax({
           url: $(this).data('url'),
-          data: JSON.stringify({ title: projectTitle.val() }),
+          data: JSON.stringify({title: projectTitle.val()}),
           headers: {
             'X-Csrf-Token': csrf,
             'X-Remote': true,
           },
           contentType: 'application/json',
           method: 'PUT',
-        }).done(res => {
+          // eslint-disable-next-line no-unused-vars
+        }).done((res) => {
           // modal.modal('hide');
           setTimeout(window.location.reload(true), 2000);
         });
       });
   });
 
-  $('.delete-project-board').each(function() {
-    $(this).click(function(e) {
+  $('.delete-project-board').each(function () {
+    $(this).click(function (e) {
       e.preventDefault();
 
       $.ajax({
