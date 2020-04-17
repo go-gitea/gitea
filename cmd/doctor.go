5af01b412e8b7e52ba7092e0919d89ac7db73b30
6543 <6543@obermui.de>
2020-04-17 16:29:57 +0200

    rename check-db to check-db-version to have multible db checks

--- cmd/doctor.go
+++ cmd/doctor.go
@@ -84,7 +84,7 @@ var checklist = []check{
 	},
 	{
 		title:         "Check Database Version",
-		name:          "check-db",
+		name:          "check-db-version",
 		isDefault:     true,
 		f:             runDoctorCheckDBVersion,
 		abortIfFailed: true,
