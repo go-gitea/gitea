1b86f174ce99b579df35c32e800551b1fd1df807
赵智超 <1012112796@qq.com>
2020-04-18 21:50:25 +0800

    Add a way to mark Conversation (code comment) resolved (#11037)
    * Add a way to mark Conversation (code comment) resolved
    
    mark Conversation is a way to mark a Conversation is stale
    or be solved. when it's marked as stale, will be hided like
    stale. all Pull Request writer , Offical Reviewers and poster
    can add or remove Conversation resolved mark.
    
    Signed-off-by: a1012112796 <1012112796@qq.com>
    
    * fix lint
    
    * Apply suggestions from code review
    
    * Add ResolveDoer
    * fix ui
    
    Co-Authored-By: Lauris BH <lauris@nix.lv>
    Co-Authored-By: 6543 <6543@obermui.de>
    
    * change IsResolved to an function
    Add permission check in UpdateResolveConversation
    
    * Apply suggestions from code review
    
    * change return error for permisson check
    * add default message for deleted user
    * get issue message from comment
    * add migration for ``ResolveDoerID`` column
    
    another  change:
    * block mark pending review as resolved because it's not necessary
    
    Co-authored-by: guillep2k <18600385+guillep2k@users.noreply.github.com>
    
    * change button color
    
    * resolve button size
    
    * fix code style
    
    * remove unusefull code
    
    Co-authored-by: guillep2k <18600385+guillep2k@users.noreply.github.com>
    
    Co-authored-by: Lauris BH <lauris@nix.lv>
    Co-authored-by: 6543 <6543@obermui.de>
    Co-authored-by: guillep2k <18600385+guillep2k@users.noreply.github.com>

--- web_src/js/index.js
+++ web_src/js/index.js
@@ -2566,6 +2566,19 @@ $(document).ready(async () => {
     $(e).click();
   });
 
+  $('.resolve-conversation').on('click', function (e) {
+    e.preventDefault();
+    const id = $(this).data('comment-id');
+    const action = $(this).data('action');
+    const url = $(this).data('update-url');
+
+    $.post(url, {
+      _csrf: csrf,
+      action,
+      comment_id: id,
+    }).then(reload);
+  });
+
   buttonsClickOnEnter();
   searchUsers();
   searchTeams();
