d9875ff2e1af91a1d73cd99ee5869aedf7fa0703
zeripath <art27@cantab.net>
2020-04-18 17:55:13 +0100

    Refresh codemirror on show pull comment tab (#11100) (#11122)
    Fix #10975
    
    Signed-off-by: Andrew Thornton <art27@cantab.net>
    
    Co-authored-by: John Olheiser <john.olheiser@gmail.com>
    Co-authored-by: techknowlogick <techknowlogick@gitea.io>
    Co-authored-by: guillep2k <18600385+guillep2k@users.noreply.github.com>
    
    Co-authored-by: John Olheiser <john.olheiser@gmail.com>
    Co-authored-by: techknowlogick <techknowlogick@gitea.io>
    Co-authored-by: guillep2k <18600385+guillep2k@users.noreply.github.com>

--- web_src/js/index.js
+++ web_src/js/index.js
@@ -1098,6 +1098,7 @@ function initRepository() {
     $repoComparePull.find('button.show-form').on('click', function (e) {
       e.preventDefault();
       $repoComparePull.find('.pullrequest-form').show();
+      autoSimpleMDE.codemirror.refresh();
       $(this).parent().hide();
     });
   }
