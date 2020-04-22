876c1f82d4e6ae78bc57a4f8dcf9418273eb1b43
Andrew Thornton <art27@cantab.net>
2020-04-22 19:08:32 +0100

    as per @silverwind
    Signed-off-by: Andrew Thornton <art27@cantab.net>

--- web_src/js/features/notification.js
+++ web_src/js/features/notification.js
@@ -1,6 +1,6 @@
 const {AppSubUrl, csrf} = window.config;
 
-export async function initNotificationsTable() {
+export function initNotificationsTable() {
   $('#notification_table .button').click(function () {
     updateNotification(
       $(this).data('url'),
@@ -17,66 +17,74 @@ export async function initNotificationsTable() {
   });
 }
 
-export async function initNotificationCount() {
+export function initNotificationCount() {
   if ($('.notification_count').length > 0) {
-    const fn = (callback, timeout) => {
+    const lastCount = $('.notification_count').text();
+    const fn = (callback, timeout, lastCount) => {
       setTimeout(() => {
-        updateNotificationCount(callback, timeout);
+        updateNotificationCount(callback, timeout, lastCount);
       }, timeout);
     };
 
-    fn(fn, 10000);
+    fn(fn, 10000, lastCount);
   }
 }
 
-function updateNotificationCount(callback, timeout) {
+function updateNotificationCount(callback, timeout, lastCount) {
+  const currentCount = $('.notification_count').text();
+  if (callback && (lastCount !== currentCount)) {
+    callback(callback, 10000, currentCount);
+    return;
+  }
   $.ajax({
     type: 'GET',
     url: `${AppSubUrl}/api/v1/notifications/new`,
     data: {
       _csrf: csrf,
     },
-    success: (data) => {
-      const notificationCount = $('.notification_count');
-      const notificationDependent = $('.notification_dependent');
-      if (data.new === 0) {
-        notificationCount.addClass('hidden');
-        notificationDependent.addClass('hide');
-      } else {
-        notificationCount.removeClass('hidden');
-        notificationDependent.removeClass('hide');
-      }
-      const currentCount = notificationCount.text();
-      if (currentCount !== `${data.new}`) {
-        notificationCount.text(data.new);
-        timeout = 10000;
-      } else if (timeout < 60000) {
-        timeout += 10000;
-      }
-      if (callback) {
-        callback(callback, timeout);
-      }
-    },
+  }).then((data) => {
+    const notificationCount = $('.notification_count');
+    const notificationDependent = $('.notification_dependent');
+    if (data.new === 0) {
+      notificationCount.addClass('hidden');
+      notificationDependent.addClass('hide');
+    } else {
+      notificationCount.removeClass('hidden');
+      notificationDependent.removeClass('hide');
+    }
+    const currentCount = $('.notification_count').text();
+    if (lastCount !== `${data.new}` || currentCount !== `${data.new}`) {
+      notificationCount.text(data.new);
+      timeout = 10000;
+    } else if (timeout < 60000) {
+      timeout += 10000;
+    }
+    return {
+      timeout,
+      nextCount: `${data.new}`,
+    };
+  }).then((data) => {
+    if (callback) {
+      callback(callback, data.timeout, data.nextCount);
+    }
   });
 }
 
-function updateNotification(url, status, page, q, notificationID) {
-  return new Promise(((resolve) => {
-    if (status !== 'pinned') {
-      $(`#notification_${notificationID}`).remove();
-    }
-    $.ajax({
-      type: 'POST',
-      url,
-      data: {
-        _csrf: csrf,
-        notification_id: notificationID,
-        status,
-        page,
-        q,
-        noredirect: true,
-      },
-      success: resolve
-    });
-  }));
+async function updateNotification(url, status, page, q, notificationID) {
+  if (status !== 'pinned') {
+    $(`#notification_${notificationID}`).remove();
+  }
+
+  return $.ajax({
+    type: 'POST',
+    url,
+    data: {
+      _csrf: csrf,
+      notification_id: notificationID,
+      status,
+      page,
+      q,
+      noredirect: true,
+    },
+  });
 }
