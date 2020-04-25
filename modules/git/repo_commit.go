bd363e5d44da5760ea426fb86489c716060a2fe8
a1012112796 <1012112796@qq.com>
2020-04-25 11:06:14 +0800

    fix style again, I forgot something before

--- modules/git/repo_commit.go
+++ modules/git/repo_commit.go
@@ -457,7 +457,7 @@ func (repo *Repository) getBranches(commit *Commit, limit int) ([]string, error)
 
 // GetCommitsFromIDs get commits from commit IDs
 func (repo *Repository) GetCommitsFromIDs(commitIDs []string) (commits *list.List) {
-	if commitIDs == nil || len(commitIDs) == 0 {
+	if len(commitIDs) == 0 {
 		return nil
 	}
 
