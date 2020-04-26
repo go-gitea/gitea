e69f38b7133b7c59657e6ad77949cbaf9aed89fe
6543 <6543@obermui.de>
2020-04-26 22:57:15 +0200

    let Projects disabled on old Repos (migration)

--- models/migrations/v139.go
+++ models/migrations/v139.go
@@ -8,7 +8,6 @@ import (
 	"code.gitea.io/gitea/modules/timeutil"
 
 	"xorm.io/xorm"
-	"xorm.io/xorm/convert"
 )
 
 func addProjectsInfo(x *xorm.Engine) error {
@@ -87,48 +86,5 @@ func addProjectsInfo(x *xorm.Engine) error {
 		return err
 	}
 
-	// enable projects by default on migration
-	// ToDo:do we really need this?
-	sess := x.NewSession()
-	defer sess.Close()
-
-	if err := sess.Begin(); err != nil {
-		return err
-	}
-
-	type RepoUnit struct {
-		ID          int64
-		RepoID      int64              `xorm:"INDEX(s)"`
-		Type        int                `xorm:"INDEX(s)"`
-		Config      convert.Conversion `xorm:"TEXT"`
-		CreatedUnix timeutil.TimeStamp `xorm:"INDEX CREATED"`
-	}
-
-	const batchSize = 100
-
-	const unitTypeProject int = 8 // see UnitTypeProjects in models/units.go
-
-	for start := 0; ; start += batchSize {
-		repos := make([]*Repository, 0, batchSize)
-
-		if err := sess.Limit(batchSize, start).Find(&repos); err != nil {
-			return err
-		}
-
-		if len(repos) == 0 {
-			break
-		}
-
-		for _, r := range repos {
-			if _, err := sess.ID(r.ID).Insert(&RepoUnit{
-				RepoID:      r.ID,
-				Type:        unitTypeProject,
-				CreatedUnix: timeutil.TimeStampNow(),
-			}); err != nil {
-				return err
-			}
-		}
-	}
-
-	return sess.Commit()
+	return nil
 }
