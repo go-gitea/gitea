b844276411d2522daa4fad2d5272983f3f4ac11f
Ilias Tsakiridis <ilias.tsakiridis@outlook.com>
2020-05-04 22:22:21 +0300

    Fixes error 500 when trying to create a User or na Organization project

--- models/project.go
+++ models/project.go
@@ -64,7 +64,7 @@ func GetProjectsConfig() []ProjectsConfig {
 // IsProjectTypeValid checks if a project typeis valid
 func IsProjectTypeValid(p ProjectType) bool {
 	switch p {
-	case ProjectTypeRepository:
+	case ProjectTypeRepository, ProjectTypeIndividual, ProjectTypeOrganization:
 		return true
 	default:
 		return false
