8fbe2ec6ea5fcca1e73b35cd596c2124ce87802e
kolaente <k@knt.li>
2020-05-07 12:35:34 +0200

    Update routers/repo/issue.go
    Co-authored-by: 赵智超 <1012112796@qq.com>

--- routers/repo/issue.go
+++ routers/repo/issue.go
@@ -1108,7 +1108,7 @@ func ViewIssue(ctx *context.Context) {
 		// Check if there is a pending pr merge
 		ctx.Data["HasPendingPullRequestMerge"], ctx.Data["PendingPullRequestMerge"], err = models.GetScheduledMergeRequestByPullID(pull.ID)
 		if err != nil {
-			ctx.ServerError("GetReviewersByIssueID", err)
+			ctx.ServerError("GetScheduledMergeRequestByPullID", err)
 			return
 		}
 	}
