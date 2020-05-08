6b55f072996a13ddf7b7a7d2d36a6a885c6ef752
Andrew Thornton <art27@cantab.net>
2020-05-08 13:36:00 +0100

    As @lunny add utility function to stop timer
    Signed-off-by: Andrew Thornton <art27@cantab.net>

--- modules/queue/queue_wrapped.go
+++ modules/queue/queue_wrapped.go
@@ -12,6 +12,7 @@ import (
 	"time"
 
 	"code.gitea.io/gitea/modules/log"
+	"code.gitea.io/gitea/modules/util"
 )
 
 // WrappedQueueType is the type for a wrapped delayed starting queue
@@ -88,12 +89,7 @@ func (q *delayedStarter) setInternal(atShutdown func(context.Context, func()), h
 			t := time.NewTimer(sleepTime)
 			select {
 			case <-ctx.Done():
-				if !t.Stop() {
-					select {
-					case <-t.C:
-					default:
-					}
-				}
+				util.StopTimer(t)
 			case <-t.C:
 			}
 		}
