0c3bad9d554b5ca162f74ff855ef60efb2a3a46f
Lunny Xiao <xiaolunwen@gmail.com>
2020-05-13 09:34:16 +0800

    Fix copyright head and lint

--- cmd/migrate_storage.go
+++ cmd/migrate_storage.go
@@ -1,4 +1,4 @@
-// Copyright 2018 The Gitea Authors. All rights reserved.
+// Copyright 2020 The Gitea Authors. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
