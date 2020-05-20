99f8349b97a85ee60cb589d3aa5fda34ab6120ef
silverwind <me@silverwind.io>
2020-05-21 00:33:31 +0200

    Fix webpack chunk loading with STATIC_URL_PREFIX (#11526)
    Previously, we had only set __webpack_public_path__ to a path which
    caused webpack chunks to be loaded from the current origin which is
    incorrect when STATIC_URL_PREFIX points to another origin.
    
    This should fix the issue curretnly seen on gitea.com.
    
    Co-authored-by: zeripath <art27@cantab.net>
    Co-authored-by: Lauris BH <lauris@nix.lv>

--- web_src/js/publicpath.js
+++ web_src/js/publicpath.js
@@ -1,7 +1,10 @@
-// This sets up webpack's chunk loading to load resources from the 'public'
-// directory. This file must be imported before any lazy-loading is being attempted.
+// This sets up the URL prefix used in webpack's chunk loading.
+// This file must be imported before any lazy-loading is being attempted.
+const {StaticUrlPrefix} = window.config;
 
-if (document.currentScript && document.currentScript.src) {
+if (StaticUrlPrefix) {
+  __webpack_public_path__ = StaticUrlPrefix.endsWith('/') ? StaticUrlPrefix : `${StaticUrlPrefix}/`;
+} else if (document.currentScript && document.currentScript.src) {
   const url = new URL(document.currentScript.src);
   __webpack_public_path__ = url.pathname.replace(/\/[^/]*?\/[^/]*?$/, '/');
 } else {
