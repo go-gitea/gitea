d2716c98803e2827a04e505837c24bde544e60f6
silverwind <me@silverwind.io>
2020-05-22 03:03:51 +0200

    more refactor

--- web_src/js/features/serviceworker.js
+++ web_src/js/features/serviceworker.js
@@ -9,27 +9,36 @@ async function unregister() {
   }
 }
 
-export default async function initServiceWorker() {
-  if (!('serviceWorker' in navigator)) return;
+async function invalidateCache() {
+  await caches.delete(cacheName);
+}
 
+async function checkCacheValidity() {
   const cacheKey = AppVer;
   const storedCacheKey = localStorage.getItem('serviceWorkerCacheKey');
 
   // invalidate cache if it belongs to a different gitea version
   if (cacheKey && storedCacheKey !== cacheKey) {
-    await caches.delete(cacheName);
+    invalidateCache();
     localStorage.setItem('serviceWorkerCacheKey', cacheKey);
   }
+}
+
+export default async function initServiceWorker() {
+  if (!('serviceWorker' in navigator)) return;
 
   // register or unregister the service worker script
   if (UseServiceWorker) {
+    await checkCacheValidity();
     try {
       await navigator.serviceWorker.register(`${AppSubUrl}/serviceworker.js`);
     } catch (err) {
       console.error(err);
+      await invalidateCache();
       await unregister();
     }
   } else {
+    await invalidateCache();
     await unregister();
   }
 }
