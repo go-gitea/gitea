6f3206759e77738962c5f97b4629ca0faa6ce7ee
6543 <6543@obermui.de>
2020-05-27 11:49:42 +0200

    Revert "return 500 error msg only if not Production mode"
    This reverts commit 8467b2cee674ad205b452780ca88abb1b27643c8.

--- modules/context/api.go
+++ modules/context/api.go
@@ -69,7 +69,6 @@ type APIRedirect struct{}
 // If status is 500, also it prints error to log.
 func (ctx *APIContext) Error(status int, title string, obj interface{}) {
 	var message string
-
 	if err, ok := obj.(error); ok {
 		message = err.Error()
 	} else {
@@ -78,10 +77,6 @@ func (ctx *APIContext) Error(status int, title string, obj interface{}) {
 
 	if status == http.StatusInternalServerError {
 		log.ErrorWithSkip(1, "%s: %s", title, message)
-
-		if macaron.Env == macaron.PROD {
-			message = ""
-		}
 	}
 
 	ctx.JSON(status, APIError{
@@ -95,13 +90,8 @@ func (ctx *APIContext) Error(status int, title string, obj interface{}) {
 func (ctx *APIContext) InternalServerError(err error) {
 	log.ErrorWithSkip(1, "InternalServerError: %v", err)
 
-	var message string
-	if macaron.Env != macaron.PROD {
-		message = err.Error()
-	}
-
 	ctx.JSON(http.StatusInternalServerError, APIError{
-		Message: message,
+		Message: err.Error(),
 		URL:     setting.API.SwaggerURL,
 	})
 }
