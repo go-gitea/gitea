02ff88bceeeaa6eeef48a2de999d35777486c412
赵智超 <1012112796@qq.com>
2020-05-29 22:07:58 +0800

    Update routers/repo/issue.go
    Co-authored-by: Lauris BH <lauris@nix.lv>

--- routers/repo/issue.go
+++ routers/repo/issue.go
@@ -907,8 +907,7 @@ func ViewIssue(ctx *context.Context) {
 	// check if dependencies can be created across repositories
 	ctx.Data["AllowCrossRepositoryDependencies"] = setting.Service.AllowCrossRepositoryDependencies
 
-	issue.ShowTag, err = commentTag(repo, issue.Poster, issue)
-	if err != nil {
+	if issue.ShowTag, err = commentTag(repo, issue.Poster, issue); err ! = nil {
 		ctx.ServerError("commentTag", err)
 		return
 	}
