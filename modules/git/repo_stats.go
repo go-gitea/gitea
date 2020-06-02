3edfaaf8b7857d83231cdca104a6cc48fc44ee71
Matti R <matti@mdranta.net>
2020-06-01 23:44:07 -0400

    refactor repo_stats to use os.pipe

--- modules/git/repo_stats.go
+++ modules/git/repo_stats.go
@@ -6,8 +6,9 @@ package git
 
 import (
 	"bufio"
-	"bytes"
+	"context"
 	"fmt"
+	"os"
 	"sort"
 	"strconv"
 	"strings"
@@ -49,6 +50,15 @@ func (repo *Repository) GetCodeActivityStats(fromTime time.Time, branch string) 
 	}
 	stats.CommitCountInAllBranches = c
 
+	stderrReader, stderrWriter, err := os.Pipe()
+	if err != nil {
+		return nil, fmt.Errorf("Unable to open stderr pipe: %v", err)
+	}
+	defer func() {
+		_ = stderrReader.Close()
+		_ = stderrWriter.Close()
+	}()
+
 	args := []string{"log", "--numstat", "--no-merges", "--pretty=format:---%n%h%n%an%n%ae%n", "--date=iso", fmt.Sprintf("--since='%s'", since)}
 	if len(branch) == 0 {
 		args = append(args, "--branches=*")
@@ -56,79 +66,87 @@ func (repo *Repository) GetCodeActivityStats(fromTime time.Time, branch string) 
 		args = append(args, "--first-parent", branch)
 	}
 
-	stdout, err = NewCommand(args...).RunInDirBytes(repo.Path)
+	err = NewCommand(args...).RunInDirTimeoutEnvFullPipelineFunc(
+		nil, -1, repo.Path,
+		nil, stderrWriter, nil,
+		func(ctx context.Context, cancel context.CancelFunc) error {
+			_ = stderrWriter.Close()
+
+			scanner := bufio.NewScanner(stderrReader)
+			scanner.Split(bufio.ScanLines)
+			stats.CommitCount = 0
+			stats.Additions = 0
+			stats.Deletions = 0
+			authors := make(map[string]*CodeActivityAuthor)
+			files := make(map[string]bool)
+			var author string
+			p := 0
+			for scanner.Scan() {
+				l := strings.TrimSpace(scanner.Text())
+				if l == "---" {
+					p = 1
+				} else if p == 0 {
+					continue
+				} else {
+					p++
+				}
+				if p > 4 && len(l) == 0 {
+					continue
+				}
+				switch p {
+				case 1: // Separator
+				case 2: // Commit sha-1
+					stats.CommitCount++
+				case 3: // Author
+					author = l
+				case 4: // E-mail
+					email := strings.ToLower(l)
+					if _, ok := authors[email]; !ok {
+						authors[email] = &CodeActivityAuthor{
+							Name:    author,
+							Email:   email,
+							Commits: 0,
+						}
+					}
+					authors[email].Commits++
+				default: // Changed file
+					if parts := strings.Fields(l); len(parts) >= 3 {
+						if parts[0] != "-" {
+							if c, err := strconv.ParseInt(strings.TrimSpace(parts[0]), 10, 64); err == nil {
+								stats.Additions += c
+							}
+						}
+						if parts[1] != "-" {
+							if c, err := strconv.ParseInt(strings.TrimSpace(parts[1]), 10, 64); err == nil {
+								stats.Deletions += c
+							}
+						}
+						if _, ok := files[parts[2]]; !ok {
+							files[parts[2]] = true
+						}
+					}
+				}
+			}
+
+			a := make([]*CodeActivityAuthor, 0, len(authors))
+			for _, v := range authors {
+				a = append(a, v)
+			}
+			// Sort authors descending depending on commit count
+			sort.Slice(a, func(i, j int) bool {
+				return a[i].Commits > a[j].Commits
+			})
+
+			stats.AuthorCount = int64(len(authors))
+			stats.ChangedFiles = int64(len(files))
+			stats.Authors = a
+
+			_ = stderrReader.Close()
+			return nil
+		})
 	if err != nil {
 		return nil, err
 	}
-
-	scanner := bufio.NewScanner(bytes.NewReader(stdout))
-	scanner.Split(bufio.ScanLines)
-	stats.CommitCount = 0
-	stats.Additions = 0
-	stats.Deletions = 0
-	authors := make(map[string]*CodeActivityAuthor)
-	files := make(map[string]bool)
-	var author string
-	p := 0
-	for scanner.Scan() {
-		l := strings.TrimSpace(scanner.Text())
-		if l == "---" {
-			p = 1
-		} else if p == 0 {
-			continue
-		} else {
-			p++
-		}
-		if p > 4 && len(l) == 0 {
-			continue
-		}
-		switch p {
-		case 1: // Separator
-		case 2: // Commit sha-1
-			stats.CommitCount++
-		case 3: // Author
-			author = l
-		case 4: // E-mail
-			email := strings.ToLower(l)
-			if _, ok := authors[email]; !ok {
-				authors[email] = &CodeActivityAuthor{
-					Name:    author,
-					Email:   email,
-					Commits: 0,
-				}
-			}
-			authors[email].Commits++
-		default: // Changed file
-			if parts := strings.Fields(l); len(parts) >= 3 {
-				if parts[0] != "-" {
-					if c, err := strconv.ParseInt(strings.TrimSpace(parts[0]), 10, 64); err == nil {
-						stats.Additions += c
-					}
-				}
-				if parts[1] != "-" {
-					if c, err := strconv.ParseInt(strings.TrimSpace(parts[1]), 10, 64); err == nil {
-						stats.Deletions += c
-					}
-				}
-				if _, ok := files[parts[2]]; !ok {
-					files[parts[2]] = true
-				}
-			}
-		}
-	}
-
-	a := make([]*CodeActivityAuthor, 0, len(authors))
-	for _, v := range authors {
-		a = append(a, v)
-	}
-	// Sort authors descending depending on commit count
-	sort.Slice(a, func(i, j int) bool {
-		return a[i].Commits > a[j].Commits
-	})
-
-	stats.AuthorCount = int64(len(authors))
-	stats.ChangedFiles = int64(len(files))
-	stats.Authors = a
 
 	return stats, nil
 }
