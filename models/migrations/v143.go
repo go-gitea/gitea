20dd5551cfd293b5b0ca5e21e8e1e87f7ed94fdc
kolaente <k@knt.li>
2020-06-14 12:38:13 +0200

    Add createdunix timestamp to migration
    Signed-off-by: kolaente <k@knt.li>

--- models/migrations/v143.go
+++ models/migrations/v143.go
@@ -4,16 +4,21 @@
 
 package migrations
 
-import "xorm.io/xorm"
+import (
+	"code.gitea.io/gitea/modules/timeutil"
+
+	"xorm.io/xorm"
+)
 
 func addAutoMergeTable(x *xorm.Engine) error {
 	type MergeStyle string
 	type ScheduledPullRequestMerge struct {
-		ID         int64      `xorm:"pk autoincr"`
-		PullID     int64      `xorm:"BIGINT"`
-		UserID     int64      `xorm:"BIGINT"`
-		MergeStyle MergeStyle `xorm:"varchar(50)"`
-		Message    string     `xorm:"TEXT"`
+		ID          int64              `xorm:"pk autoincr"`
+		PullID      int64              `xorm:"BIGINT"`
+		UserID      int64              `xorm:"BIGINT"`
+		MergeStyle  MergeStyle         `xorm:"varchar(50)"`
+		Message     string             `xorm:"TEXT"`
+		CreatedUnix timeutil.TimeStamp `xorm:"created"`
 	}
 
 	return x.Sync2(&ScheduledPullRequestMerge{})
