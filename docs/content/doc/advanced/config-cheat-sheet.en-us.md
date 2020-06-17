18abd2e59a5a7e0f055e5960b2c1aa14b91018b8
techknowlogick <techknowlogick@gitea.io>
2020-06-17 16:53:55 -0400

    Global default branch setting (#11918)
    * Global default branch setting
    
    * add to app.ini example per @silverwind
    
    * update per @lunny
    
    Co-authored-by: John Olheiser <john.olheiser@gmail.com>

--- docs/content/doc/advanced/config-cheat-sheet.en-us.md
+++ docs/content/doc/advanced/config-cheat-sheet.en-us.md
@@ -69,6 +69,7 @@ Values containing `#` or `;` must be quoted using `` ` `` or `"""`.
 - `ENABLE_PUSH_CREATE_USER`:  **false**: Allow users to push local repositories to Gitea and have them automatically created for a user.
 - `ENABLE_PUSH_CREATE_ORG`:  **false**: Allow users to push local repositories to Gitea and have them automatically created for an org.
 - `PREFIX_ARCHIVE_FILES`: **true**: Prefix archive files by placing them in a directory named after the repository.
+- `DEFAULT_BRANCH`: **master**: Default branch name of all repositories.
 
 ### Repository - Pull Request (`repository.pull-request`)
 
--- modules/setting/repository.go
+++ modules/setting/repository.go
@@ -39,7 +39,8 @@ var (
 		EnablePushCreateOrg                     bool
 		DisabledRepoUnits                       []string
 		DefaultRepoUnits                        []string
-		PrefixArchiveFiles                      bool
+		PrefixArchiveFiles                       bool
+		DefaultBranch                           string
 
 		// Repository editor settings
 		Editor struct {
@@ -201,6 +202,7 @@ func newRepository() {
 	Repository.DisableHTTPGit = sec.Key("DISABLE_HTTP_GIT").MustBool()
 	Repository.UseCompatSSHURI = sec.Key("USE_COMPAT_SSH_URI").MustBool()
 	Repository.MaxCreationLimit = sec.Key("MAX_CREATION_LIMIT").MustInt(-1)
+	Repository.DefaultBranch = sec.Key("DEFAULT_BRANCH").MustString("master")
 	RepoRootPath = sec.Key("ROOT").MustString(path.Join(homeDir, "gitea-repositories"))
 	forcePathSeparator(RepoRootPath)
 	if !filepath.IsAbs(RepoRootPath) {
