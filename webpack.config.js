aecf32ecf774eed0cdc21782ec9725845deeaa30
silverwind <me@silverwind.io>
2020-07-01 19:40:51 +0200

    Fix missing gitgraph css import
    The filter was wrongly excluding the gitGraph.css file. Need to clean
    this up later so that imports are always relative to the source file
    (which is not the case for fonts right now).
    
    Regressed by: https://github.com/go-gitea/gitea/pull/11997

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,8 @@ const isProduction = process.env.NODE_ENV !== 'development';
 const filterCssImport = (parsedImport, cssFile) => {
   const url = parsedImport && parsedImport.url ? parsedImport.url : parsedImport;
   const importedFile = url.replace(/[?#].+/, '').toLowerCase();
+  if (/vendor\/assets/.test(url)) return false; // font imports
+  if (/web_src[/\\]less/.test(cssFile)) return true; // relative imports
   if (cssFile.includes('monaco')) return true;
   if (cssFile.includes('fomantic')) {
     if (/brand-icons/.test(importedFile)) return false;
