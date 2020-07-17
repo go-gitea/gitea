0e24af6951fa65d5094ab6da0f182697aab2a6f0
silverwind <me@silverwind.io>
2020-07-17 17:15:12 +0200

    Update Octicons to v10 (#12240)
    * Update Octicons to v10
    
    Besides a few renames, these icons are no longer present in v10 that we've
    used, so had to change:
    
    file-symlink-directory -> file-submodule
    internal-repo -> repo
    repo-force-push -> repo-push
    repo-template-private -> repo-template
    
    Fixes: https://github.com/go-gitea/gitea/issues/11889
    Ref: https://github.com/primer/octicons/releases/tag/v10.0.0
    
    * add custom sliders svg for removed octicon-settings
    
    * apply suggestion
    
    * fix triangles and use play on admin dashboard
    
    * add custom mirror svg
    
    * add missing build files
    
    * unify custom svgs
    
    * move to octicon-repo-clone to gitea-mirror
    
    * use octicon-x on conflicts
    
    * tweak timeline icons
    
    * tweak comment buttons
    
    * update settings icon to octicons v1
    
    * switch to octicon-mirror and octicon-tools
    
    * replace two wiki buttons with octicons
    
    * remove whitespace in svg sources
    
    * Fix filepath basename on Windows for SVG bindata (#12241)
    
    * move octicons to devDependencies
    
    * move back to dependencies
    
    * move svgo to devDependencies again
    
    Co-authored-by: Cirno the Strongest <1447794+CirnoT@users.noreply.github.com>
    Co-authored-by: techknowlogick <techknowlogick@gitea.io>

--- web_src/js/svg.js
+++ web_src/js/svg.js
@@ -2,32 +2,28 @@ import octiconChevronDown from '../../public/img/svg/octicon-chevron-down.svg';
 import octiconChevronRight from '../../public/img/svg/octicon-chevron-right.svg';
 import octiconGitMerge from '../../public/img/svg/octicon-git-merge.svg';
 import octiconGitPullRequest from '../../public/img/svg/octicon-git-pull-request.svg';
-import octiconInternalRepo from '../../public/img/svg/octicon-internal-repo.svg';
 import octiconIssueClosed from '../../public/img/svg/octicon-issue-closed.svg';
 import octiconIssueOpened from '../../public/img/svg/octicon-issue-opened.svg';
 import octiconLink from '../../public/img/svg/octicon-link.svg';
 import octiconLock from '../../public/img/svg/octicon-lock.svg';
+import octiconMirror from '../../public/img/svg/octicon-mirror.svg';
 import octiconRepo from '../../public/img/svg/octicon-repo.svg';
-import octiconRepoClone from '../../public/img/svg/octicon-repo-clone.svg';
 import octiconRepoForked from '../../public/img/svg/octicon-repo-forked.svg';
 import octiconRepoTemplate from '../../public/img/svg/octicon-repo-template.svg';
-import octiconRepoTemplatePrivate from '../../public/img/svg/octicon-repo-template-private.svg';
 
 export const svgs = {
   'octicon-chevron-down': octiconChevronDown,
   'octicon-chevron-right': octiconChevronRight,
   'octicon-git-merge': octiconGitMerge,
   'octicon-git-pull-request': octiconGitPullRequest,
-  'octicon-internal-repo': octiconInternalRepo,
   'octicon-issue-closed': octiconIssueClosed,
   'octicon-issue-opened': octiconIssueOpened,
   'octicon-link': octiconLink,
   'octicon-lock': octiconLock,
+  'octicon-mirror': octiconMirror,
   'octicon-repo': octiconRepo,
-  'octicon-repo-clone': octiconRepoClone,
   'octicon-repo-forked': octiconRepoForked,
   'octicon-repo-template': octiconRepoTemplate,
-  'octicon-repo-template-private': octiconRepoTemplatePrivate,
 };
 
 const parser = new DOMParser();
