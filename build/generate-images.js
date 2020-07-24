9e1edbf61810774b6d29216cb030af2d94875237
silverwind <me@silverwind.io>
2020-07-24 16:08:04 +0200

    Rework 'make generate-images'
    - Remove external dependencies and replace it with a node script that
      does does the same.
    - Move detail removal from gitea-sm.png to favicon.png
    - Remove favicon.ico and its generation, it is unused and we already serve
      favicon.png in its place.
    
    Fixes: https://github.com/go-gitea/gitea/issues/12314

---
+++ build/generate-images.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+'use strict';
+
+const imageminZopfli = require('imagemin-zopfli');
+const svg2img = require('svg2img');
+const {DOMParser, XMLSerializer} = require('xmldom');
+const {promisify} = require('util');
+const {readFile, writeFile} = require('fs').promises;
+const {resolve} = require('path');
+
+function exit(err) {
+  if (err) console.error(err);
+  process.exit(err ? 1 : 0);
+}
+
+async function generate(svg, outputFile, {size, bg, removeDetail} = {}) {
+  const parser = new DOMParser();
+  const serializer = new XMLSerializer();
+  const document = parser.parseFromString(svg);
+
+  if (bg) {
+    const rect = parser.parseFromString('<rect width="100%" height="100%" fill="white"/>');
+    document.documentElement.insertBefore(rect, document.documentElement.firstChild);
+  }
+
+  if (removeDetail) {
+    for (const el of Array.from(document.getElementsByTagName('g') || [])) {
+      for (const attribute of Array.from(el.attributes || [])) {
+        if (attribute.name === 'class' && attribute.value === 'detail-remove') {
+          el.parentNode.removeChild(el);
+        }
+      }
+    }
+  }
+
+  const processedSvg = serializer.serializeToString(document);
+  const png = await promisify(svg2img)(processedSvg, {
+    width: size,
+    height: size,
+    preserveAspectRatio: 'xMinYMid meet',
+  });
+
+  const optimizedPng = await imageminZopfli({more: true})(png);
+  await writeFile(outputFile, optimizedPng);
+}
+
+async function main() {
+  const svg = await readFile(resolve(__dirname, '../assets/logo.svg'), 'utf8');
+  await generate(svg, resolve(__dirname, '../public/img/gitea-lg.png'), {size: 880});
+  await generate(svg, resolve(__dirname, '../public/img/gitea-512.png'), {size: 512});
+  await generate(svg, resolve(__dirname, '../public/img/gitea-192.png'), {size: 192});
+  await generate(svg, resolve(__dirname, '../public/img/gitea-sm.png'), {size: 120});
+  await generate(svg, resolve(__dirname, '../public/img/avatar_default.png'), {size: 200});
+  await generate(svg, resolve(__dirname, '../public/img/favicon.png'), {size: 180, removeDetail: true});
+  await generate(svg, resolve(__dirname, '../public/img/apple-touch-icon.png'), {size: 180, bg: true});
+}
+
+main().then(exit).catch(exit);
+
