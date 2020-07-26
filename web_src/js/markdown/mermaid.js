367a234ca23533508ddd2cd6257b0f9b0112adb7
silverwind <me@silverwind.io>
2020-07-26 22:40:10 +0200

    Add mermaid JS renderer
    For feature parity with GitLab. Tested in files, issues, wiki, editor.
    arc-green only does an inversion because the renderer seems to like to
    render white backgrounds on boxes.
    
    Ref: https://github.com/go-gitea/gitea/issues/3340
    Fixes: https://github.com/go-gitea/gitea/issues/12307

---
+++ web_src/js/markdown/mermaid.js
@@ -0,0 +1,18 @@
+import {random} from '../utils.js';
+
+export async function renderMermaid(els) {
+  if (!els || !els.length) return;
+
+  const {mermaidAPI} = await import(/* webpackChunkName: "mermaid" */'mermaid');
+
+  mermaidAPI.initialize({startOnLoad: false});
+
+  for (const el of els) {
+    mermaidAPI.render(`mermaid-${random(12)}`, el.textContent, (svg) => {
+      const div = document.createElement('div');
+      div.classList.add('mermaid-chart');
+      div.innerHTML = svg;
+      el.closest('pre').replaceWith(div);
+    });
+  }
+}
