cc0aa8b535921626acf6d89e478032c92130ad22
techknowlogick <techknowlogick@gitea.io>
2020-07-26 19:32:57 -0400

    conflicting variable name

--- modules/setting/setting.go
+++ modules/setting/setting.go
@@ -626,11 +626,11 @@ func NewContext() {
 	StaticURLPrefix = strings.TrimSuffix(sec.Key("STATIC_URL_PREFIX").MustString(AppSubURL), "/")
 	AppSubURLDepth = strings.Count(AppSubURL, "/")
 	// Check if Domain differs from AppURL domain than update it to AppURL's domain
-	u, err := url.Parse(fmt.Sprintf("http://%s",appURL.Host))
+	tempURL, err := url.Parse(fmt.Sprintf("http://%s",appURL.Host))
 	if err != nil {
 		log.Fatal("Invalid host in ROOT_URL '%s': %s", appURL.Host, err)
 	}
-	urlHostname := u.Hostname()
+	urlHostname := tempURL.Hostname()
 	if urlHostname != Domain && net.ParseIP(urlHostname) == nil {
 		Domain = urlHostname
 	}
