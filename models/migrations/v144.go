206f4ac38da07c61c882e58db1de256980234f95
Till Faelligen <tfaelligen@gmail.com>
2020-07-30 07:32:01 +0200

    Use local variable instead of constant value

--- models/migrations/v144.go
+++ models/migrations/v144.go
@@ -5,18 +5,18 @@
 package migrations
 
 import (
-	"code.gitea.io/gitea/models"
 	"code.gitea.io/gitea/modules/log"
 	"xorm.io/builder"
 	"xorm.io/xorm"
 )
 
 func updateMatrixWebhookHTTPMethod(x *xorm.Engine) error {
+	var matrixHookTaskType = 9 // value comes from the models package
 	type Webhook struct {
 		HTTPMethod string
 	}
 
-	cond := builder.Eq{"hook_task_type": models.MATRIX}.And(builder.Neq{"http_method": "PUT"})
+	cond := builder.Eq{"hook_task_type": matrixHookTaskType}.And(builder.Neq{"http_method": "PUT"})
 	count, err := x.Where(cond).Cols("http_method").Update(&Webhook{HTTPMethod: "PUT"})
 	if err == nil {
 		log.Debug("Updated %d Matrix webhooks with http_method 'PUT'", count)
