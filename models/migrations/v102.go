a01467ccbabad79504ac8fc8337fa8648ec9e9ef
Andrew Thornton <art27@cantab.net>
2020-08-02 11:53:17 +0100

    as per @lunny
    Signed-off-by: Andrew Thornton <art27@cantab.net>

--- models/migrations/v102.go
+++ models/migrations/v102.go
@@ -11,6 +11,9 @@ import (
 func dropColumnHeadUserNameOnPullRequest(x *xorm.Engine) error {
 	sess := x.NewSession()
 	defer sess.Close()
+	if err := sess.Begin(); err != nil {
+		return err
+	}
 	if err := dropTableColumns(sess, "pull_request", "head_user_name"); err != nil {
 		return err
 	}
