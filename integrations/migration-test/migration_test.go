19b64a1c51ad03e6d5d92fe35cdfccc83d5fde65
Andrew Thornton <art27@cantab.net>
2020-08-29 17:19:27 +0100

    restore migration set-schema
    Signed-off-by: Andrew Thornton <art27@cantab.net>

--- integrations/migration-test/migration_test.go
+++ integrations/migration-test/migration_test.go
@@ -193,12 +193,12 @@ func restoreOldDB(t *testing.T, version string) bool {
 				_, err = db.Exec(fmt.Sprintf("CREATE SCHEMA %s", setting.Database.Schema))
 				assert.NoError(t, err)
 			}
+			schrows.Close()
 
 			// Make the user's default search path the created schema; this will affect new connections
 			_, err = db.Exec(fmt.Sprintf(`ALTER USER "%s" SET search_path = %s`, setting.Database.User, setting.Database.Schema))
 			assert.NoError(t, err)
 
-			schrows.Close()
 			db.Close()
 		}
 
