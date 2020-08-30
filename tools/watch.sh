f13d864f4fec859b642d7e35906b69fa94d78850
silverwind <me@silverwind.io>
2020-08-30 18:13:08 +0200

    simplify

--- tools/watch.sh
+++ tools/watch.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -euo pipefail
-trap "kill 0" SIGINT SIGTERM SIGQUIT
 
 make watch-frontend &
-PIDS+="$! ";
 make watch-backend &
-PIDS+="$!";
 
-trap "kill ${PIDS[*]}" SIGINT SIGTERM SIGQUIT
-wait $PIDS
+trap 'kill $(jobs -p)' EXIT
+wait
