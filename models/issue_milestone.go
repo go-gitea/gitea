e794970b7429703db984bbf03cc403b39c199695
6543 <6543@obermui.de>
2020-09-01 04:47:21 +0200

    Expose to UI - To Overloaded ?!?

--- models/issue_milestone.go
+++ models/issue_milestone.go
@@ -7,6 +7,7 @@ package models
 import (
 	"fmt"
 	"strings"
+	"time"
 
 	"code.gitea.io/gitea/modules/setting"
 	api "code.gitea.io/gitea/modules/structs"
@@ -38,6 +39,7 @@ type Milestone struct {
 	DeadlineString string `xorm:"-"`
 
 	TotalTrackedTime int64 `xorm:"-"`
+	TimeSinceUpdate  int64 `xorm:"-"`
 }
 
 // BeforeUpdate is invoked from XORM before updating this object.
@@ -52,6 +54,9 @@ func (m *Milestone) BeforeUpdate() {
 // AfterLoad is invoked from XORM after setting the value of a field of
 // this object.
 func (m *Milestone) AfterLoad() {
+	if !m.UpdatedUnix.IsZero() {
+		m.TimeSinceUpdate = time.Now().Unix() - m.UpdatedUnix.AsTime().Unix()
+	}
 	m.NumOpenIssues = m.NumIssues - m.NumClosedIssues
 	if m.DeadlineUnix.Year() == 9999 {
 		return
