3f6b108c11d32ef0f9b74b6d75076e5aafc9bd1b
6543 <6543@obermui.de>
2020-09-04 01:35:56 +0200

    exchange as per @lunny

--- models/migrations/v148.go
+++ models/migrations/v148.go
@@ -9,6 +9,6 @@ import (
 )
 
 func purgeInvalidDependenciesComments(x *xorm.Engine) error {
-	_, err := x.Exec("DELETE FROM comment WHERE dependent_issue_id NOT IN (SELECT id FROM issue) AND dependent_issue_id != 0")
+	_, err := x.Exec("DELETE FROM comment WHERE dependent_issue_id != 0 AND dependent_issue_id NOT IN (SELECT id FROM issue)")
 	return err
 }
