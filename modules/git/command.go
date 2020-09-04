e8370a91c987719ac48e91971b41a07e11fc0d56
6543 <6543@obermui.de>
2020-09-05 01:15:34 +0200

    Copyright header

--- modules/git/command.go
+++ modules/git/command.go
@@ -1,3 +1,4 @@
+// Copyright 2020 The Gitea Authors. All rights reserved.
 // Copyright 2015 The Gogs Authors. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
