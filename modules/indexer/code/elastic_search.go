7129a578ebc8ae2f28547c14780f67a3fdbd8074
Jui-Nan Lin <jnlinn@gmail.com>
2020-09-17 00:41:43 +0800

    refactor: use constant for multi match types

--- modules/indexer/code/elastic_search.go
+++ modules/indexer/code/elastic_search.go
@@ -27,6 +27,10 @@ import (
 
 const (
 	esRepoIndexerLatestVersion = 1
+	// multi-match-types, currently only 2 types are used
+	// Reference: https://www.elastic.co/guide/en/elasticsearch/reference/7.0/query-dsl-multi-match-query.html#multi-match-types
+	esMultiMatchTypeBestFields   = "best_fields"
+	esMultiMatchTypePhrasePrefix = "phrase_prefix"
 )
 
 var (
@@ -331,9 +335,9 @@ func extractAggs(searchResult *elastic.SearchResult) []*SearchResultLanguages {
 
 // Search searches for codes and language stats by given conditions.
 func (b *ElasticSearchIndexer) Search(repoIDs []int64, language, keyword string, page, pageSize int, isMatch bool) (int64, []*SearchResult, []*SearchResultLanguages, error) {
-	searchType := "best_fields"
+	searchType := esMultiMatchTypeBestFields
 	if isMatch {
-		searchType = "phrase_prefix"
+		searchType = esMultiMatchTypePhrasePrefix
 	}
 
 	kwQuery := elastic.NewMultiMatchQuery(keyword, "content").Type(searchType)
