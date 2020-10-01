a3e7351c6a68b9de9873d1743cd9bf9f62a14edb
techknowlogick <techknowlogick@gitea.io>
2020-10-01 08:49:49 -0400

    allow U2F with default settings for gitea in subpath (#12990)
    * allow U2F with default settings for gitea in subpath
    
    * use trim suffix
    
    Co-authored-by: zeripath <art27@cantab.net>

--- modules/setting/setting.go
+++ modules/setting/setting.go
@@ -1032,8 +1032,8 @@ func NewContext() {
 	newMarkup()
 
 	sec = Cfg.Section("U2F")
-	U2F.TrustedFacets, _ = shellquote.Split(sec.Key("TRUSTED_FACETS").MustString(strings.TrimRight(AppURL, "/")))
-	U2F.AppID = sec.Key("APP_ID").MustString(strings.TrimRight(AppURL, "/"))
+	U2F.TrustedFacets, _ = shellquote.Split(sec.Key("TRUSTED_FACETS").MustString(strings.TrimSuffix(AppURL, AppSubURL+"/")))
+	U2F.AppID = sec.Key("APP_ID").MustString(strings.TrimSuffix(AppURL, "/"))
 
 	zip.Verbose = false
 
