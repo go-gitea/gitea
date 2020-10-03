d9f98d4dbc06867178eac6061ab633c4862dd091
Lucas Queiroz <me@lucasqueiroz.dev>
2020-10-03 14:31:20 -0300

    Add Timestamp to Tag list API

--- modules/structs/repo_commit.go
+++ modules/structs/repo_commit.go
@@ -20,6 +20,8 @@ type Identity struct {
 type CommitMeta struct {
 	URL string `json:"url"`
 	SHA string `json:"sha"`
+	// swagger:strfmt date-time
+	Timestamp time.Time `json:"timestamp"`
 }
 
 // CommitUser contains information of a user in the context of a commit.
