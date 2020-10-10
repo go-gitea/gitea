89df8c389454d957c1680b694ea815f6130b9dec
Brad Albright <brad.albright@gmail.com>
2020-10-09 22:59:54 -0500

    gofmt

--- models/webhook_test.go
+++ models/webhook_test.go
@@ -243,7 +243,7 @@ func TestUpdateHookTask(t *testing.T) {
 	hook.PayloadContent = "new payload content"
 	hook.DeliveredString = "new delivered string"
 	hook.IsDelivered = true
-  hook.Delivered = time.Now().UnixNano()
+	hook.Delivered = time.Now().UnixNano()
 	AssertNotExistsBean(t, hook)
 	assert.NoError(t, UpdateHookTask(hook))
 	AssertExistsAndLoadBean(t, hook)
