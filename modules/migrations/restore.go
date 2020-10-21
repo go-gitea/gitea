d1746bcc72d3ab65c95f1d02e58e35cee825f0d8
Lunny Xiao <xiaolunwen@gmail.com>
2020-10-21 13:03:54 +0800

    Add comments

--- modules/migrations/restore.go
+++ modules/migrations/restore.go
@@ -25,6 +25,7 @@ type RepositoryRestorer struct {
 	repoName  string
 }
 
+// NewRepositoryRestorer creates a repository restorer which could restore repository from a dumped folder
 func NewRepositoryRestorer(ctx context.Context, baseDir string, owner, repoName string) *RepositoryRestorer {
 	return &RepositoryRestorer{
 		ctx:       ctx,
@@ -34,54 +35,65 @@ func NewRepositoryRestorer(ctx context.Context, baseDir string, owner, repoName 
 	}
 }
 
-func (g *RepositoryRestorer) gitPath() string {
-	return filepath.Join(g.baseDir, "git")
+func (r *RepositoryRestorer) gitPath() string {
+	return filepath.Join(r.baseDir, "git")
 }
 
-func (g *RepositoryRestorer) wikiPath() string {
-	return filepath.Join(g.baseDir, "wiki")
+func (r *RepositoryRestorer) wikiPath() string {
+	return filepath.Join(r.baseDir, "wiki")
 }
 
-func (g *RepositoryRestorer) topicDir() string {
-	return filepath.Join(g.baseDir)
+func (r *RepositoryRestorer) topicDir() string {
+	return filepath.Join(r.baseDir)
 }
 
-func (g *RepositoryRestorer) milestoneDir() string {
-	return filepath.Join(g.baseDir)
+func (r *RepositoryRestorer) milestoneDir() string {
+	return filepath.Join(r.baseDir)
 }
 
-func (g *RepositoryRestorer) labelDir() string {
-	return filepath.Join(g.baseDir)
+func (r *RepositoryRestorer) labelDir() string {
+	return filepath.Join(r.baseDir)
 }
 
-func (g *RepositoryRestorer) releaseDir() string {
-	return filepath.Join(g.baseDir)
+func (r *RepositoryRestorer) releaseDir() string {
+	return filepath.Join(r.baseDir)
 }
 
-func (g *RepositoryRestorer) issueDir() string {
-	return filepath.Join(g.baseDir)
+func (r *RepositoryRestorer) issueDir() string {
+	return filepath.Join(r.baseDir)
 }
 
-func (g *RepositoryRestorer) commentDir() string {
-	return filepath.Join(g.baseDir, "comments")
+func (r *RepositoryRestorer) commentDir() string {
+	return filepath.Join(r.baseDir, "comments")
 }
 
-func (g *RepositoryRestorer) pullrequestDir() string {
-	return filepath.Join(g.baseDir)
+func (r *RepositoryRestorer) pullrequestDir() string {
+	return filepath.Join(r.baseDir)
 }
 
-func (g *RepositoryRestorer) reviewDir() string {
-	return filepath.Join(g.baseDir, "reviews")
+func (r *RepositoryRestorer) reviewDir() string {
+	return filepath.Join(r.baseDir, "reviews")
 }
 
+// SetContext set context
 func (r *RepositoryRestorer) SetContext(ctx context.Context) {
 	r.ctx = ctx
 }
 
+// GetRepoInfo returns a repository information
 func (r *RepositoryRestorer) GetRepoInfo() (*base.Repository, error) {
-	return nil, nil
+	return &base.Repository{
+		Owner:         r.repoOwner,
+		Name:          r.repoName,
+		IsPrivate:     true,
+		//Description:   gr.GetDescription(), // FIXME
+		//OriginalURL:   gr.GetHTMLURL(), // FIXME
+		//CloneURL:      gr.GetCloneURL(), // FIXME
+		DefaultBranch: "master", // FIXME
+	}, nil
 }
 
+// GetTopics return github topics
 func (r *RepositoryRestorer) GetTopics() ([]string, error) {
 	p := filepath.Join(r.topicDir(), "topic.yml")
 	
@@ -101,6 +113,7 @@ func (r *RepositoryRestorer) GetTopics() ([]string, error) {
 	return topics.Topics, nil
 }
 
+// GetMilestones returns milestones
 func (r *RepositoryRestorer) GetMilestones() ([]*base.Milestone, error) {
 	var milestones = make([]*base.Milestone, 0, 10)
 	p := filepath.Join(r.milestoneDir(), "milestone.yml")
@@ -116,6 +129,7 @@ func (r *RepositoryRestorer) GetMilestones() ([]*base.Milestone, error) {
 	return milestones, nil
 }
 
+// GetReleases returns releases
 func (r *RepositoryRestorer) GetReleases() ([]*base.Release, error) {
 	var releases = make([]*base.Release, 0, 10)
 	p := filepath.Join(r.releaseDir(), "release.yml")
@@ -131,12 +145,14 @@ func (r *RepositoryRestorer) GetReleases() ([]*base.Release, error) {
 	return releases, nil
 }
 
+// GetAsset returns an asset
 func (r *RepositoryRestorer) GetAsset(tagName string, relID, assetID int64) (io.ReadCloser, error) {
 	attachDir := filepath.Join(r.releaseDir(), "release_assets", tagName)
 	attachLocalPath := filepath.Join(attachDir, fmt.Sprintf("%d", assetID))
 	return os.Open(attachLocalPath)
 }
 
+// GetLabels returns labels
 func (r *RepositoryRestorer) GetLabels() ([]*base.Label, error) {
 	var labels = make([]*base.Label, 0, 10)
 	p := filepath.Join(r.labelDir(), "label.yml")
@@ -152,6 +168,7 @@ func (r *RepositoryRestorer) GetLabels() ([]*base.Label, error) {
 	return labels, nil
 }
 
+// GetIssues returns issues according start and limit
 func (r *RepositoryRestorer) GetIssues(page, perPage int) ([]*base.Issue, bool, error) {
 	var issues = make([]*base.Issue, 0, 10)
 	p := filepath.Join(r.issueDir(), "issue.yml")
@@ -167,6 +184,7 @@ func (r *RepositoryRestorer) GetIssues(page, perPage int) ([]*base.Issue, bool, 
 	return issues, true, nil
 }
 
+// GetComments returns comments according issueNumber
 func (r *RepositoryRestorer) GetComments(issueNumber int64) ([]*base.Comment, error) {
 	var comments = make([]*base.Comment, 0, 10)
 	p := filepath.Join(r.commentDir(), fmt.Sprintf("%d.yml", issueNumber))
@@ -182,6 +200,7 @@ func (r *RepositoryRestorer) GetComments(issueNumber int64) ([]*base.Comment, er
 	return comments, nil
 }
 
+// GetPullRequests returns pull requests according page and perPage
 func (r *RepositoryRestorer) GetPullRequests(page, perPage int) ([]*base.PullRequest, bool, error) {
 	var pulls = make([]*base.PullRequest, 0, 10)
 	p := filepath.Join(r.pullrequestDir(), "pull_request.yml")
@@ -197,6 +216,7 @@ func (r *RepositoryRestorer) GetPullRequests(page, perPage int) ([]*base.PullReq
 	return pulls, true, nil
 }
 
+// GetReviews returns pull requests review
 func (r *RepositoryRestorer) GetReviews(pullRequestNumber int64) ([]*base.Review, error) {
 	var reviews = make([]*base.Review, 0, 10)
 	p := filepath.Join(r.reviewDir(), fmt.Sprintf("%d.yml", pullRequestNumber))
