0b7786f13b278caeffd4e1210da2b0e552df9060
Lunny Xiao <xiaolunwen@gmail.com>
2020-10-23 11:28:52 +0800

    Fix bug

--- modules/migrations/dump.go
+++ modules/migrations/dump.go
@@ -313,7 +313,7 @@ func (g *RepositoryDumper) CreateReleases(downloader base.Downloader, releases .
 				return err
 			}
 			for _, asset := range release.Assets {
-				attachLocalPath := filepath.Join(attachDir, asset.ID)
+				attachLocalPath := filepath.Join(attachDir, fmt.Sprintf("%d", asset.ID))
 				// download attachment
 
 				err := func(attachLocalPath string) error {
