b92707850a569b56ab2f5bf703b99010dc339474
Andrew Thornton <art27@cantab.net>
2020-10-27 21:52:49 +0000

    use webpack to webpack the easymde css
    Signed-off-by: Andrew Thornton <art27@cantab.net>

--- web_src/js/easymde.js
+++ web_src/js/easymde.js
@@ -1,4 +1,6 @@
 import EasyMDE from 'easymde';
+import 'easymde/dist/easymde.min.css';
+
 import CodeMirror from 'codemirror/lib/codemirror.js';
 import 'codemirror/addon/mode/loadmode.js';
 import 'codemirror/mode/meta.js';
