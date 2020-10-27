259b78ea46115abaa014172c70b2d8b4a8e99576
Andrew Thornton <art27@cantab.net>
2020-10-27 22:06:14 +0000

    move loading codemirror modes and addons back in to footer.tmpl
    Signed-off-by: Andrew Thornton <art27@cantab.net>

--- web_src/js/easymde.js
+++ web_src/js/easymde.js
@@ -1,8 +1,6 @@
 import EasyMDE from 'easymde';
 
 import CodeMirror from 'codemirror/lib/codemirror.js';
-import 'codemirror/addon/mode/loadmode.js';
-import 'codemirror/mode/meta.js';
 
 window.EasyMDE = EasyMDE;
 window.SimpleMDE = EasyMDE;
