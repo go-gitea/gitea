b40038895131b3c9f4f0eb4ae4e396a76882d160
Chris Shyi <chris.shyi@yyc.com.tw>
2020-11-09 11:31:16 +0800

    Add further tests

--- integrations/signup_test.go
+++ integrations/signup_test.go
@@ -6,9 +6,12 @@ package integrations
 
 import (
 	"net/http"
+	"strings"
 	"testing"
 
 	"code.gitea.io/gitea/modules/setting"
+	"github.com/stretchr/testify/assert"
+	"github.com/unknwon/i18n"
 )
 
 func TestSignup(t *testing.T) {
@@ -27,4 +30,23 @@ func TestSignup(t *testing.T) {
 	// should be able to view new user's page
 	req = NewRequest(t, "GET", "/exampleUser")
 	MakeRequest(t, req, http.StatusOK)
+}
+
+func TestSignupInvalidEmail(t *testing.T) {
+	defer prepareTestEnv(t)()
+
+	setting.Service.EnableCaptcha = false
+
+	req := NewRequestWithValues(t, "POST", "/user/sign_up", map[string]string{
+		"user_name": "exampleUser",
+		"email":     "exampleUser@example.com\r\n",
+		"password":  "examplePassword!1",
+		"retype":    "examplePassword!1",
+	})
+	resp := MakeRequest(t, req, http.StatusOK)
+	htmlDoc := NewHTMLParser(t, resp.Body)
+	assert.Equal(t,
+		i18n.Tr("en", "form.email_invalid", nil),
+		strings.TrimSpace(htmlDoc.doc.Find(".ui.message").Text()),
+	)
 }
