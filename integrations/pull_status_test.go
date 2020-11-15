5b7aae82f0abe094f9b59fb6c21eb61c42bd0dba
silverwind <me@silverwind.io>
2020-11-15 20:15:18 +0100

    attempt to fix test

--- integrations/pull_status_test.go
+++ integrations/pull_status_test.go
@@ -114,7 +114,7 @@ func TestPullCreate_EmptyChangesWithCommits(t *testing.T) {
 		resp := session.MakeRequest(t, req, http.StatusOK)
 		doc := NewHTMLParser(t, resp.Body)
 
-		text := strings.TrimSpace(doc.doc.Find(".item.text.green").Text())
+		text := strings.TrimSpace(doc.doc.Find(".item").Text())
 		assert.EqualValues(t, "This pull request can be merged automatically.", text)
 	})
 }
