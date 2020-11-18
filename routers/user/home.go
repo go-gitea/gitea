24bf24758454e36dae3567677f5f652ef995169b
silverwind <me@silverwind.io>
2020-11-18 19:27:16 +0100

    Remove fetch request from heatmap
    Render heatmap data directly to HTML, eliminating one HTTP request on
    frontpage and user profile. Also added min-height to the container so
    the page content will no longer move after loading.

--- routers/user/home.go
+++ routers/user/home.go
@@ -114,8 +114,12 @@ func Dashboard(ctx *context.Context) {
 	ctx.Data["SearchLimit"] = setting.UI.User.RepoPagingNum
 	// no heatmap access for admins; GetUserHeatmapDataByUser ignores the calling user
 	// so everyone would get the same empty heatmap
-	ctx.Data["EnableHeatmap"] = setting.Service.EnableUserHeatmap && !ctxUser.KeepActivityPrivate
-	ctx.Data["HeatmapUser"] = ctxUser.Name
+	if setting.Service.EnableUserHeatmap && !ctxUser.KeepActivityPrivate {
+		data, err := models.GetUserHeatmapDataByUser(ctxUser)
+		if err == nil {
+			ctx.Data["HeatmapData"] = data
+		}
+	}
 
 	var err error
 	var mirrors []*models.Repository
--- web_src/js/features/userheatmap.js
+++ web_src/js/features/userheatmap.js
@@ -5,6 +5,14 @@ import ActivityHeatmap from '../components/ActivityHeatmap.vue';
 export default async function initUserHeatmap() {
   const el = document.getElementById('user-heatmap');
   if (!el) return;
-  const View = Vue.extend(ActivityHeatmap);
+
+  const values = JSON.parse(el.dataset.heatmapData).map(({contributions, timestamp}) => {
+    return {date: new Date(timestamp * 1000), count: contributions};
+  });
+
+  const View = Vue.extend({
+    render: (createElement) => createElement(ActivityHeatmap, {props: {values}}),
+  });
+
   new View().$mount(el);
 }
