00edcc722d4ba069ae26f667bb05782463b16611
silverwind <me@silverwind.io>
2020-12-01 22:54:15 +0100

    add migration

---
+++ models/migrations/v161.go
@@ -0,0 +1,28 @@
+// Copyright 2020 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package migrations
+
+import (
+	"fmt"
+
+	"code.gitea.io/gitea/modules/log"
+
+	"xorm.io/xorm"
+)
+
+func dropEmailHashTable(x *xorm.Engine) error {
+	sess := x.NewSession()
+	defer sess.Close()
+	if err := sess.Begin(); err != nil {
+		return err
+	}
+
+	if _, err := sess.Exec(fmt.Sprintf("DROP TABLE email_hash")); err != nil {
+		log.Error("Unable to drop table email_hash. Error: %v", err)
+		return err
+	}
+
+	return sess.Commit()
+}
