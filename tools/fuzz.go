8c8744facfd40fb97bd5782388791ed64f1ccc78
AdamKorcz <44787359+AdamKorcz@users.noreply.github.com>
2020-12-04 15:41:39 +0000

    Update tools/fuzz.go
    Co-authored-by: 6543 <6543@obermui.de>

--- tools/fuzz.go
+++ tools/fuzz.go
@@ -11,6 +11,13 @@ import (
 	"code.gitea.io/gitea/modules/markup/markdown"
 )
 
+// Contains fuzzing functions executed by
+// fuzzing engine https://github.com/dvyukov/go-fuzz
+//
+// The function must return 1 if the fuzzer should increase priority of the given input during subsequent fuzzing
+// (for example, the input is lexically correct and was parsed successfully).
+// -1 if the input must not be added to corpus even if gives new coverage and 0 otherwise.
+
 func FuzzMarkdownRenderRaw(data []byte) int {
 	_ = markdown.RenderRaw(data, "", false)
 	return 1
