a3a3410151d8c6897e131a6b248c1dac88bace87
Lunny Xiao <xiaolunwen@gmail.com>
2020-12-19 16:12:00 +0800

    Fix fmt

--- modules/auth/sso/sso.go
+++ modules/auth/sso/sso.go
@@ -12,8 +12,8 @@ import (
 	"strings"
 
 	"code.gitea.io/gitea/models"
-	"code.gitea.io/gitea/modules/middlewares"
 	"code.gitea.io/gitea/modules/log"
+	"code.gitea.io/gitea/modules/middlewares"
 	"code.gitea.io/gitea/modules/setting"
 )
 
