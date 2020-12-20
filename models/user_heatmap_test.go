e4291745558334582dc9c42bd8c2f5194a8962f9
Norwin Roosen <git@nroo.de>
2020-12-20 22:35:28 +0100

    extend tests

--- models/user_heatmap_test.go
+++ models/user_heatmap_test.go
@@ -14,24 +14,27 @@ import (
 func TestGetUserHeatmapDataByUser(t *testing.T) {
 	testCases := []struct {
 		userID      int64
+		doerID      int64
 		CountResult int
 		JSONResult  string
 	}{
-		{2, 1, `[{"timestamp":1603152000,"contributions":1}]`},
-		{3, 0, `[]`},
+		{2, 2, 1, `[{"timestamp":1603152000,"contributions":1}]`},
+		{2, 0, 0, `[]`},
+		{3, 3, 0, `[]`},
 	}
 	// Prepare
 	assert.NoError(t, PrepareTestDatabase())
 
 	for _, tc := range testCases {
-
-		// Insert some action
 		user := AssertExistsAndLoadBean(t, &User{ID: tc.userID}).(*User)
+		doer := &User{ID: tc.doerID}
+		_, err := loadBeanIfExists(doer)
+		assert.NoError(t, err)
 
 		// get the action for comparison
 		actions, err := GetFeeds(GetFeedsOptions{
 			RequestedUser:   user,
-			Actor:           user,
+			Actor:           doer,
 			IncludePrivate:  true,
 			OnlyPerformedBy: false,
 			IncludeDeleted:  true,
@@ -39,7 +42,7 @@ func TestGetUserHeatmapDataByUser(t *testing.T) {
 		assert.NoError(t, err)
 
 		// Get the heatmap and compare
-		heatmap, err := GetUserHeatmapDataByUser(user)
+		heatmap, err := GetUserHeatmapDataByUser(user, doer)
 		assert.NoError(t, err)
 		assert.Equal(t, len(actions), len(heatmap), "invalid action count: did the test data became too old?")
 		assert.Equal(t, tc.CountResult, len(heatmap))
