6c5044dc21987bfb123684aa96a6b03745431d6d
Lunny Xiao <xiaolunwen@gmail.com>
2020-12-22 21:44:46 +0800

    Fix lint

--- modules/migrations/dump.go
+++ modules/migrations/dump.go
@@ -320,7 +320,7 @@ func (g *RepositoryDumper) CreateReleases(releases ...*base.Release) error {
 				return err
 			}
 			for _, asset := range release.Assets {
-				attachLocalPath := filepath.Join(attachDir, fmt.Sprintf("%s", asset.Name))
+				attachLocalPath := filepath.Join(attachDir, asset.Name)
 				// download attachment
 
 				err := func(attachLocalPath string) error {
