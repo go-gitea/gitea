ed6b86b763d64a2f58957605aab5dc357813779b
Norwin Roosen <git@nroo.de>
2020-12-25 06:01:17 +0100

    new variant: hover dialog

--- web_src/js/features/stopwatch.js
+++ web_src/js/features/stopwatch.js
@@ -1,7 +1,18 @@
 const {AppSubUrl, csrf, NotificationSettings} = window.config;
 
 export async function initStopwatch() {
-  const stopwatchEl = $('.active-stopwatch');
+  const stopwatchEl = $('.active-stopwatch-trigger');
+
+  stopwatchEl.popup({
+    position: 'bottom right',
+    hoverable: true,
+  });
+
+  // form handlers
+  $('form > button', stopwatchEl).on('click', function () {
+    $(this).parent().trigger('submit');
+  });
+
   if (!stopwatchEl || NotificationSettings.MinTimeout <= 0) {
     return;
   }
@@ -35,12 +46,17 @@ async function updateStopwatch() {
   });
 
   const watch = data[0];
-  const btnEl = $('.active-stopwatch');
+  const btnEl = $('.active-stopwatch-trigger');
   if (!watch) {
     btnEl.addClass('hidden');
   } else {
-    $('.stopwatch-time').text(`${watch.duration}`);
-    btnEl.attr('href', `${AppSubUrl}/${watch.repo_owner_name}/${watch.repo_name}/issues/${watch.issue_index}`);
+    const {repo_owner_name, repo_name, issue_index, duration} = watch;
+    const issueUrl = `${AppSubUrl}/${repo_owner_name}/${repo_name}/issues/${issue_index}`;
+    $('.stopwatch-link').attr('href', issueUrl);
+    $('.stopwatch-commit').attr('action', `${issueUrl}/times/stopwatch/toggle`);
+    $('.stopwatch-cancel').attr('action', `${issueUrl}/times/stopwatch/cancel`);
+    $('.stopwatch-issue').text(`${repo_owner_name}/${repo_name}#${issue_index}`);
+    $('.stopwatch-time').text(`${duration}`);
     btnEl.removeClass('hidden');
   }
 
