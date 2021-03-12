3499d32e1971b6d0532840fcb6b523bbd49b072c
Andrew Thornton <art27@cantab.net>
2021-03-12 21:44:48 +0000

    Do not show full lfs file on error in git_test.go:rawTest()
    If there is a problem uploading to LFS it is possible for the raw
    endpoint to return a very large file when a pointer file is expected
    This will then cause the drone logs to fill up unnecessarily with
    the contents of the very large file.
    
    If the file returned from raw is of the incorrect size we should
    therefore not test it see if it contains the pointer file
    and just declare that it is incorrect.
    
    Signed-off-by: Andrew Thornton <art27@cantab.net>

--- integrations/git_test.go
+++ integrations/git_test.go
@@ -216,7 +216,9 @@ func rawTest(t *testing.T, ctx *APITestContext, little, big, littleLFS, bigLFS s
 			req = NewRequest(t, "GET", path.Join("/", username, reponame, "/raw/branch/master/", littleLFS))
 			resp = session.MakeRequest(t, req, http.StatusOK)
 			assert.NotEqual(t, littleSize, resp.Body.Len())
-			assert.Contains(t, resp.Body.String(), models.LFSMetaFileIdentifier)
+			if resp.Body.Len() != littleSize {
+				assert.Contains(t, resp.Body.String(), models.LFSMetaFileIdentifier)
+			}
 		}
 
 		if !testing.Short() {
@@ -228,7 +230,9 @@ func rawTest(t *testing.T, ctx *APITestContext, little, big, littleLFS, bigLFS s
 				req = NewRequest(t, "GET", path.Join("/", username, reponame, "/raw/branch/master/", bigLFS))
 				resp = session.MakeRequest(t, req, http.StatusOK)
 				assert.NotEqual(t, bigSize, resp.Body.Len())
-				assert.Contains(t, resp.Body.String(), models.LFSMetaFileIdentifier)
+				if resp.Body.Len() != bigSize {
+					assert.Contains(t, resp.Body.String(), models.LFSMetaFileIdentifier)
+				}
 			}
 		}
 	})
