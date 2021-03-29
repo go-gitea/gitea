1f087a837ed2633c66a585dff75041f82a4a9090
KN4CK3R <admin@oldschoolhack.me>
2021-03-29 23:15:00 +0200

    Execute image diff only when container is visible.

--- web_src/js/features/imagediff.js
+++ web_src/js/features/imagediff.js
@@ -61,204 +61,223 @@ export default async function initImageDiff() {
 
   $('.image-diff').each(function() {
     const $container = $(this);
-    const pathAfter = $container.data('path-after');
-    const pathBefore = $container.data('path-before');
 
-    const imageInfos = [{
-      loaded: false,
-      path: pathAfter,
-      $image: $container.find('img.image-after'),
-      $boundsInfo: $container.find('.bounds-info-after')
-    }, {
-      loaded: false,
-      path: pathBefore,
-      $image: $container.find('img.image-before'),
-      $boundsInfo: $container.find('.bounds-info-before')
-    }];
+    if ($container.is(':visible')) {
+      onContainerVisibleHandler();
+    } else {
+      const observer = new IntersectionObserver((entries) => {
+        for (const entry of entries) {
+          if (entry.intersectionRatio > 0) {
+            observer.disconnect();
+            onContainerVisibleHandler();
+            break;
+          }
+        }
+      }, {
+        root: document.documentElement
+      });
+      observer.observe(this);
+    }
 
-    for (const info of imageInfos) {
-      if (info.$image.length > 0) {
-        $.ajax({
-          url: info.path,
-          success: (data, _, jqXHR) => {
-            info.$image.on('load', () => {
-              info.loaded = true;
-              setReadyIfLoaded();
-            });
-            info.$image.attr('src', info.path);
+    function onContainerVisibleHandler() {
+      const diffContainerWidth = $container.width() - 300;
+      const pathAfter = $container.data('path-after');
+      const pathBefore = $container.data('path-before');
 
-            if (jqXHR.getResponseHeader('Content-Type') === 'image/svg+xml') {
-              const bounds = getDefaultSvgBoundsIfUndefined(data, info.path);
-              if (bounds) {
-                info.$image.attr('width', bounds.width);
-                info.$image.attr('height', bounds.height);
-                info.$boundsInfo.hide();
+      const imageInfos = [{
+        loaded: false,
+        path: pathAfter,
+        $image: $container.find('img.image-after'),
+        $boundsInfo: $container.find('.bounds-info-after')
+      }, {
+        loaded: false,
+        path: pathBefore,
+        $image: $container.find('img.image-before'),
+        $boundsInfo: $container.find('.bounds-info-before')
+      }];
+
+      for (const info of imageInfos) {
+        if (info.$image.length > 0) {
+          $.ajax({
+            url: info.path,
+            success: (data, _, jqXHR) => {
+              info.$image.on('load', () => {
+                info.loaded = true;
+                setReadyIfLoaded();
+              });
+              info.$image.attr('src', info.path);
+
+              if (jqXHR.getResponseHeader('Content-Type') === 'image/svg+xml') {
+                const bounds = getDefaultSvgBoundsIfUndefined(data, info.path);
+                if (bounds) {
+                  info.$image.attr('width', bounds.width);
+                  info.$image.attr('height', bounds.height);
+                  info.$boundsInfo.hide();
+                }
               }
             }
-          }
+          });
+        } else {
+          info.loaded = true;
+          setReadyIfLoaded();
+        }
+      }
+
+      function setReadyIfLoaded() {
+        if (imageInfos[0].loaded && imageInfos[1].loaded) {
+          initViews(imageInfos[0].$image, imageInfos[1].$image);
+        }
+      }
+
+      function initViews($imageAfter, $imageBefore) {
+        initSideBySide(createContext($imageAfter[0], $imageBefore[0]));
+        if ($imageAfter.length > 0 && $imageBefore.length > 0) {
+          initSwipe(createContext($imageAfter[1], $imageBefore[1]));
+          initOverlay(createContext($imageAfter[2], $imageBefore[2]));
+        }
+
+        $container.find('> .loader').hide();
+        $container.find('> .hide').removeClass('hide');
+      }
+
+      function initSideBySide(sizes) {
+        let factor = 1;
+        if (sizes.max.width > (diffContainerWidth - 24) / 2) {
+          factor = (diffContainerWidth - 24) / 2 / sizes.max.width;
+        }
+
+        const widthChanged = sizes.image1.length !== 0 && sizes.image2.length !== 0 && sizes.image1[0].naturalWidth !== sizes.image2[0].naturalWidth;
+        const heightChanged = sizes.image1.length !== 0 && sizes.image2.length !== 0 && sizes.image1[0].naturalHeight !== sizes.image2[0].naturalHeight;
+        if (sizes.image1.length !== 0) {
+          $container.find('.bounds-info-after .bounds-info-width').text(`${sizes.image1[0].naturalWidth}px`).addClass(widthChanged ? 'green' : '');
+          $container.find('.bounds-info-after .bounds-info-height').text(`${sizes.image1[0].naturalHeight}px`).addClass(heightChanged ? 'green' : '');
+        }
+        if (sizes.image2.length !== 0) {
+          $container.find('.bounds-info-before .bounds-info-width').text(`${sizes.image2[0].naturalWidth}px`).addClass(widthChanged ? 'red' : '');
+          $container.find('.bounds-info-before .bounds-info-height').text(`${sizes.image2[0].naturalHeight}px`).addClass(heightChanged ? 'red' : '');
+        }
+
+        sizes.image1.css({
+          width: sizes.size1.width * factor,
+          height: sizes.size1.height * factor
         });
-      } else {
-        info.loaded = true;
-        setReadyIfLoaded();
-      }
-    }
-
-    const diffContainerWidth = $container.width() - 300;
-
-    function setReadyIfLoaded() {
-      if (imageInfos[0].loaded && imageInfos[1].loaded) {
-        initViews(imageInfos[0].$image, imageInfos[1].$image);
-      }
-    }
-
-    function initViews($imageAfter, $imageBefore) {
-      initSideBySide(createContext($imageAfter[0], $imageBefore[0]));
-      if ($imageAfter.length > 0 && $imageBefore.length > 0) {
-        initSwipe(createContext($imageAfter[1], $imageBefore[1]));
-        initOverlay(createContext($imageAfter[2], $imageBefore[2]));
+        sizes.image1.parent().css({
+          margin: `${sizes.ratio[1] * factor + 15}px ${sizes.ratio[0] * factor}px ${sizes.ratio[1] * factor}px`,
+          width: sizes.size1.width * factor + 2,
+          height: sizes.size1.height * factor + 2
+        });
+        sizes.image2.css({
+          width: sizes.size2.width * factor,
+          height: sizes.size2.height * factor
+        });
+        sizes.image2.parent().css({
+          margin: `${sizes.ratio[3] * factor}px ${sizes.ratio[2] * factor}px`,
+          width: sizes.size2.width * factor + 2,
+          height: sizes.size2.height * factor + 2
+        });
       }
 
-      $container.find('> .loader').hide();
-      $container.find('> .hide').removeClass('hide');
-    }
+      function initSwipe(sizes) {
+        let factor = 1;
+        if (sizes.max.width > diffContainerWidth - 12) {
+          factor = (diffContainerWidth - 12) / sizes.max.width;
+        }
 
-    function initSideBySide(sizes) {
-      let factor = 1;
-      if (sizes.max.width > (diffContainerWidth - 24) / 2) {
-        factor = (diffContainerWidth - 24) / 2 / sizes.max.width;
-      }
+        sizes.image1.css({
+          width: sizes.size1.width * factor,
+          height: sizes.size1.height * factor
+        });
+        sizes.image1.parent().css({
+          margin: `0px ${sizes.ratio[0] * factor}px`,
+          width: sizes.size1.width * factor + 2,
+          height: sizes.size1.height * factor + 2
+        });
+        sizes.image1.parent().parent().css({
+          padding: `${sizes.ratio[1] * factor}px 0 0 0`,
+          width: sizes.max.width * factor + 2
+        });
+        sizes.image2.css({
+          width: sizes.size2.width * factor,
+          height: sizes.size2.height * factor
+        });
+        sizes.image2.parent().css({
+          margin: `${sizes.ratio[3] * factor}px ${sizes.ratio[2] * factor}px`,
+          width: sizes.size2.width * factor + 2,
+          height: sizes.size2.height * factor + 2
+        });
+        sizes.image2.parent().parent().css({
+          width: sizes.max.width * factor + 2,
+          height: sizes.max.height * factor + 2
+        });
+        $container.find('.diff-swipe').css({
+          width: sizes.max.width * factor + 2,
+          height: sizes.max.height * factor + 4
+        });
+        $container.find('.swipe-bar').on('mousedown', function(e) {
+          e.preventDefault();
 
-      const widthChanged = sizes.image1.length !== 0 && sizes.image2.length !== 0 && sizes.image1[0].naturalWidth !== sizes.image2[0].naturalWidth;
-      const heightChanged = sizes.image1.length !== 0 && sizes.image2.length !== 0 && sizes.image1[0].naturalHeight !== sizes.image2[0].naturalHeight;
-      if (sizes.image1.length !== 0) {
-        $container.find('.bounds-info-after .bounds-info-width').text(`${sizes.image1[0].naturalWidth}px`).addClass(widthChanged ? 'green' : '');
-        $container.find('.bounds-info-after .bounds-info-height').text(`${sizes.image1[0].naturalHeight}px`).addClass(heightChanged ? 'green' : '');
-      }
-      if (sizes.image2.length !== 0) {
-        $container.find('.bounds-info-before .bounds-info-width').text(`${sizes.image2[0].naturalWidth}px`).addClass(widthChanged ? 'red' : '');
-        $container.find('.bounds-info-before .bounds-info-height').text(`${sizes.image2[0].naturalHeight}px`).addClass(heightChanged ? 'red' : '');
-      }
+          const $swipeBar = $(this);
+          const $swipeFrame = $swipeBar.parent();
+          const width = $swipeFrame.width() - $swipeBar.width() - 2;
 
-      sizes.image1.css({
-        width: sizes.size1.width * factor,
-        height: sizes.size1.height * factor
-      });
-      sizes.image1.parent().css({
-        margin: `${sizes.ratio[1] * factor + 15}px ${sizes.ratio[0] * factor}px ${sizes.ratio[1] * factor}px`,
-        width: sizes.size1.width * factor + 2,
-        height: sizes.size1.height * factor + 2
-      });
-      sizes.image2.css({
-        width: sizes.size2.width * factor,
-        height: sizes.size2.height * factor
-      });
-      sizes.image2.parent().css({
-        margin: `${sizes.ratio[3] * factor}px ${sizes.ratio[2] * factor}px`,
-        width: sizes.size2.width * factor + 2,
-        height: sizes.size2.height * factor + 2
-      });
-    }
+          $(document).on('mousemove.diff-swipe', (e2) => {
+            e2.preventDefault();
 
-    function initSwipe(sizes) {
-      let factor = 1;
-      if (sizes.max.width > diffContainerWidth - 12) {
-        factor = (diffContainerWidth - 12) / sizes.max.width;
-      }
+            const value = Math.max(0, Math.min(e2.clientX - $swipeFrame.offset().left, width));
 
-      sizes.image1.css({
-        width: sizes.size1.width * factor,
-        height: sizes.size1.height * factor
-      });
-      sizes.image1.parent().css({
-        margin: `0px ${sizes.ratio[0] * factor}px`,
-        width: sizes.size1.width * factor + 2,
-        height: sizes.size1.height * factor + 2
-      });
-      sizes.image1.parent().parent().css({
-        padding: `${sizes.ratio[1] * factor}px 0 0 0`,
-        width: sizes.max.width * factor + 2
-      });
-      sizes.image2.css({
-        width: sizes.size2.width * factor,
-        height: sizes.size2.height * factor
-      });
-      sizes.image2.parent().css({
-        margin: `${sizes.ratio[3] * factor}px ${sizes.ratio[2] * factor}px`,
-        width: sizes.size2.width * factor + 2,
-        height: sizes.size2.height * factor + 2
-      });
-      sizes.image2.parent().parent().css({
-        width: sizes.max.width * factor + 2,
-        height: sizes.max.height * factor + 2
-      });
-      $container.find('.diff-swipe').css({
-        width: sizes.max.width * factor + 2,
-        height: sizes.max.height * factor + 4
-      });
-      $container.find('.swipe-bar').on('mousedown', function(e) {
-        e.preventDefault();
-
-        const $swipeBar = $(this);
-        const $swipeFrame = $swipeBar.parent();
-        const width = $swipeFrame.width() - $swipeBar.width() - 2;
-
-        $(document).on('mousemove.diff-swipe', (e2) => {
-          e2.preventDefault();
-
-          const value = Math.max(0, Math.min(e2.clientX - $swipeFrame.offset().left, width));
-
-          $swipeBar.css({
-            left: value
-          });
-          $container.find('.swipe-container').css({
-            width: $swipeFrame.width() - value
-          });
-          $(document).on('mouseup.diff-swipe', () => {
-            $(document).off('.diff-swipe');
+            $swipeBar.css({
+              left: value
+            });
+            $container.find('.swipe-container').css({
+              width: $swipeFrame.width() - value
+            });
+            $(document).on('mouseup.diff-swipe', () => {
+              $(document).off('.diff-swipe');
+            });
           });
         });
-      });
-    }
-
-    function initOverlay(sizes) {
-      let factor = 1;
-      if (sizes.max.width > diffContainerWidth - 12) {
-        factor = (diffContainerWidth - 12) / sizes.max.width;
       }
 
-      sizes.image1.css({
-        width: sizes.size1.width * factor,
-        height: sizes.size1.height * factor
-      });
-      sizes.image2.css({
-        width: sizes.size2.width * factor,
-        height: sizes.size2.height * factor
-      });
-      sizes.image1.parent().css({
-        margin: `${sizes.ratio[1] * factor}px ${sizes.ratio[0] * factor}px`,
-        width: sizes.size1.width * factor + 2,
-        height: sizes.size1.height * factor + 2
-      });
-      sizes.image2.parent().css({
-        margin: `${sizes.ratio[3] * factor}px ${sizes.ratio[2] * factor}px`,
-        width: sizes.size2.width * factor + 2,
-        height: sizes.size2.height * factor + 2
-      });
-      sizes.image2.parent().parent().css({
-        width: sizes.max.width * factor + 2,
-        height: sizes.max.height * factor + 2
-      });
-      $container.find('.onion-skin').css({
-        width: sizes.max.width * factor + 2,
-        height: sizes.max.height * factor + 4
-      });
+      function initOverlay(sizes) {
+        let factor = 1;
+        if (sizes.max.width > diffContainerWidth - 12) {
+          factor = (diffContainerWidth - 12) / sizes.max.width;
+        }
 
-      const $range = $container.find("input[type='range'");
-      const onInput = () => sizes.image1.parent().css({
-        opacity: $range.val() / 100
-      });
-      $range.on('input', onInput);
-      onInput();
+        sizes.image1.css({
+          width: sizes.size1.width * factor,
+          height: sizes.size1.height * factor
+        });
+        sizes.image2.css({
+          width: sizes.size2.width * factor,
+          height: sizes.size2.height * factor
+        });
+        sizes.image1.parent().css({
+          margin: `${sizes.ratio[1] * factor}px ${sizes.ratio[0] * factor}px`,
+          width: sizes.size1.width * factor + 2,
+          height: sizes.size1.height * factor + 2
+        });
+        sizes.image2.parent().css({
+          margin: `${sizes.ratio[3] * factor}px ${sizes.ratio[2] * factor}px`,
+          width: sizes.size2.width * factor + 2,
+          height: sizes.size2.height * factor + 2
+        });
+        sizes.image2.parent().parent().css({
+          width: sizes.max.width * factor + 2,
+          height: sizes.max.height * factor + 2
+        });
+        $container.find('.onion-skin').css({
+          width: sizes.max.width * factor + 2,
+          height: sizes.max.height * factor + 4
+        });
+
+        const $range = $container.find("input[type='range'");
+        const onInput = () => sizes.image1.parent().css({
+          opacity: $range.val() / 100
+        });
+        $range.on('input', onInput);
+        onInput();
+      }
     }
   });
 }
