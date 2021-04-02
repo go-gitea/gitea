739dc41cdf048ff1ad23af56c2cbc7d0cbb00b74
6543 <6543@obermui.de>
2021-04-02 04:35:17 +0200

    Fix BenchmarkRepoBranchCommit beside Create new Branch

--- integrations/benchmarks_test.go
+++ integrations/benchmarks_test.go
@@ -5,8 +5,9 @@
 package integrations
 
 import (
-	"fmt"
+	"math/rand"
 	"net/http"
+	"net/url"
 	"testing"
 
 	"code.gitea.io/gitea/models"
@@ -66,40 +67,58 @@ func BenchmarkRepo(b *testing.B) {
 	}
 }
 
-func BenchmarkRepoBranchCommit(b *testing.B) {
-	b.Skip("benchmark broken") // TODO fix
-	samples := []int64{1, 15, 16}
-	defer prepareTestEnv(b)()
-	b.ResetTimer()
-
-	for _, repoID := range samples {
-		b.StopTimer()
-		repo := models.AssertExistsAndLoadBean(b, &models.Repository{ID: repoID}).(*models.Repository)
-		b.StartTimer()
-		b.Run(repo.Name, func(b *testing.B) {
-			owner := models.AssertExistsAndLoadBean(b, &models.User{ID: repo.OwnerID}).(*models.User)
-			session := loginUser(b, owner.LoginName)
-			b.ResetTimer()
-			b.Run("CreateBranch", func(b *testing.B) {
-				for i := 0; i < b.N; i++ {
-					testCreateBranch(b, session, owner.LoginName, repo.Name, "branch/master", fmt.Sprintf("new_branch_nr%d", i), http.StatusFound)
-				}
-			})
-			b.Run("AccessBranchCommits", func(b *testing.B) {
-				var branches []*api.Branch
-				req := NewRequestf(b, "GET", "/api/v1/%s/branches", repo.FullName())
-				resp := session.MakeRequest(b, req, http.StatusOK)
-				DecodeJSON(b, resp, &branches)
-				b.ResetTimer() //We measure from here
-				if len(branches) != 0 {
-					for i := 0; i < b.N; i++ {
-						req := NewRequestf(b, "GET", "/api/v1/%s/commits?sha=%s", repo.FullName(), branches[i%len(branches)].Name)
-						session.MakeRequest(b, req, http.StatusOK)
-					}
-				}
-			})
-		})
+//StringWithCharset random string (from https://www.calhoun.io/creating-random-strings-in-go/)
+func StringWithCharset(length int, charset string) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
 	}
+	return string(b)
 }
 
-//TODO list commits /repos/{owner}/{repo}/commits
+func BenchmarkRepoBranchCommit(b *testing.B) {
+	onGiteaRunTB(b, func(t testing.TB, u *url.URL) {
+		b := t.(*testing.B)
+
+		samples := []int64{1, 2, 3}
+		b.ResetTimer()
+
+		for _, repoID := range samples {
+			b.StopTimer()
+			repo := models.AssertExistsAndLoadBean(b, &models.Repository{ID: repoID}).(*models.Repository)
+			b.StartTimer()
+			b.Run(repo.Name, func(b *testing.B) {
+				session := loginUser(b, "user2")
+				b.ResetTimer()
+				b.Run("CreateBranch", func(b *testing.B) {
+					b.Skip("benchmark broken") // TODO fix
+					b.StopTimer()
+					branchName := "new_" + StringWithCharset(5+rand.Intn(10), "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+					b.StartTimer()
+					for i := 0; i < b.N; i++ {
+						b.Run(branchName, func(b *testing.B) {
+							testAPICreateBranch(b, session, repo.OwnerName, repo.Name, repo.DefaultBranch, branchName, http.StatusCreated)
+						})
+					}
+				})
+				b.Run("GetBranches", func(b *testing.B) {
+					req := NewRequestf(b, "GET", "/api/v1/repos/%s/branches", repo.FullName())
+					session.MakeRequest(b, req, http.StatusOK)
+				})
+				b.Run("AccessCommits", func(b *testing.B) {
+					var branches []*api.Branch
+					req := NewRequestf(b, "GET", "/api/v1/repos/%s/branches", repo.FullName())
+					resp := session.MakeRequest(b, req, http.StatusOK)
+					DecodeJSON(b, resp, &branches)
+					b.ResetTimer() //We measure from here
+					if len(branches) != 0 {
+						for i := 0; i < b.N; i++ {
+							req := NewRequestf(b, "GET", "/api/v1/repos/%s/commits?sha=%s", repo.FullName(), branches[i%len(branches)].Name)
+							session.MakeRequest(b, req, http.StatusOK)
+						}
+					}
+				})
+			})
+		}
+	})
+}
