cd7241e57dd70d5dad14b685c2f4c83a2290b828
Martin Michaelis <code@mgjm.de>
2021-04-03 08:01:42 +0000

    Move oauth2 client settings to new file

--- modules/setting/setting.go
+++ modules/setting/setting.go
@@ -1168,6 +1168,7 @@ func MakeManifestData(appName string, appURL string, absoluteAssetURL string) []
 func NewServices() {
 	InitDBConfig()
 	newService()
+	newOAuth2Client()
 	NewLogServices(false)
 	newCacheService()
 	newSessionService()
