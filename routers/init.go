edc919b712fd7f052382398e9d34acdcf84f9b07
Andrew Thornton <art27@cantab.net>
2021-04-10 22:13:37 +0100

    Add Generator for RegisterServices
    Signed-off-by: Andrew Thornton <art27@cantab.net>

--- routers/init.go
+++ routers/init.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
+//go:generate go run -mod=vendor ../build/generate-services.go .. generated_init.go code.gitea.io/gitea/modules/services RegisterService
 package routers
 
 import (
