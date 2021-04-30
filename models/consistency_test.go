bf63993b306ef52d22ccf0defbfc9d75999aeb87
6543 <6543@obermui.de>
2021-04-30 18:47:13 +0200

    extend test

--- models/consistency_test.go
+++ models/consistency_test.go
@@ -13,6 +13,20 @@ import (
 func TestDeleteOrphanedObjects(t *testing.T) {
 	assert.NoError(t, PrepareTestDatabase())
 
-	err := DeleteOrphanedObjects("pull_request", "issue", "pull_request.issue_id=issue.id")
+	countBefore, err := x.Count(&PullRequest{})
 	assert.NoError(t, err)
+
+	_, err = x.Insert(&PullRequest{IssueID: 1000}, &PullRequest{IssueID: 1001}, &PullRequest{IssueID: 1003})
+	assert.NoError(t, err)
+
+	orphaned, err := CountOrphanedObjects("pull_request", "issue", "pull_request.issue_id=issue.id")
+	assert.NoError(t, err)
+	assert.EqualValues(t, 3, orphaned)
+
+	err = DeleteOrphanedObjects("pull_request", "issue", "pull_request.issue_id=issue.id")
+	assert.NoError(t, err)
+
+	countAfter, err := x.Count(&PullRequest{})
+	assert.NoError(t, err)
+	assert.EqualValues(t, countBefore, countAfter)
 }
