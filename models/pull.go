9250d938447a240639496e8c634c4091cff5b58e
JustusBunsi <61625851+justusbunsi@users.noreply.github.com>
2021-05-02 15:58:01 +0200

    Display conflict-free merge messages for pull requests
    Repositories using external issue tracker tend to use numeric issues in
    commits. To prevent conflicts during issue reference parsing or inside
    commit hooks, this change respects these configuration and uses the !
    character to refer to pull requests in merge commit messages.
    
    For repositories using squash merges, this was already handled.
    
    Signed-off-by: JustusBunsi <61625851+justusbunsi@users.noreply.github.com>

--- models/pull.go
+++ models/pull.go
@@ -212,12 +212,21 @@ func (pr *PullRequest) GetDefaultMergeMessage() string {
 		log.Error("Cannot load issue %d for PR id %d: Error: %v", pr.IssueID, pr.ID, err)
 		return ""
 	}
-
-	if pr.BaseRepoID == pr.HeadRepoID {
-		return fmt.Sprintf("Merge pull request '%s' (#%d) from %s into %s", pr.Issue.Title, pr.Issue.Index, pr.HeadBranch, pr.BaseBranch)
+	if err := pr.LoadBaseRepo(); err != nil {
+		log.Error("LoadBaseRepo: %v", err)
+		return ""
 	}
 
-	return fmt.Sprintf("Merge pull request '%s' (#%d) from %s:%s into %s", pr.Issue.Title, pr.Issue.Index, pr.HeadRepo.FullName(), pr.HeadBranch, pr.BaseBranch)
+	issueReference := fmt.Sprintf("#%d", pr.Issue.Index)
+	if pr.BaseRepo.UnitEnabled(UnitTypeExternalTracker) {
+		issueReference = fmt.Sprintf("!%d", pr.Issue.Index)
+	}
+
+	if pr.BaseRepoID == pr.HeadRepoID {
+		return fmt.Sprintf("Merge pull request '%s' (%s) from %s into %s", pr.Issue.Title, issueReference, pr.HeadBranch, pr.BaseBranch)
+	}
+
+	return fmt.Sprintf("Merge pull request '%s' (%s) from %s:%s into %s", pr.Issue.Title, issueReference, pr.HeadRepo.FullName(), pr.HeadBranch, pr.BaseBranch)
 }
 
 // ReviewCount represents a count of Reviews
