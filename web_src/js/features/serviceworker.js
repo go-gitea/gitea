de220c14270ed90f46d5e2a2f3640e65f62eab26
silverwind <me@silverwind.io>
2021-05-11 19:43:33 +0200

    Unregister non-matching serviceworkers
    With the addition of the /assets url, users who visited a previous
    version of the site now may have two active service workers, one with
    the old scope `/` and one with scope `/assets`. This check for
    serviceworkers that do not match the current script path and unregisters
    them.
    
    Also included is a small refactor to publicpath.js which was simplified
    because AssetUrlPrefix is always present now. Also it makes use of the
    new joinPaths helper too.
    
    Fixes: https://github.com/go-gitea/gitea/pull/15823

--- web_src/js/features/serviceworker.js
+++ web_src/js/features/serviceworker.js
@@ -1,11 +1,21 @@
-const {UseServiceWorker, AppSubUrl, AppVer} = window.config;
+import {joinPaths} from '../utils.js';
+
+const {UseServiceWorker, AppSubUrl, AssetUrlPrefix, AppVer} = window.config;
 const cachePrefix = 'static-cache-v'; // actual version is set in the service worker script
+const workerAssetPath = joinPaths(AssetUrlPrefix, 'serviceworker.js');
 
 async function unregister() {
   const registrations = await navigator.serviceWorker.getRegistrations();
   await Promise.all(registrations.map((registration) => {
     return registration.active && registration.unregister();
   }));
+}
+
+async function unregisterOtherWorkers() {
+  for (const registration of await navigator.serviceWorker.getRegistrations()) {
+    const scriptURL = registration?.active?.scriptURL || '';
+    if (!scriptURL.endsWith(workerAssetPath)) registration.unregister();
+  }
 }
 
 async function invalidateCache() {
@@ -29,13 +39,16 @@ async function checkCacheValidity() {
 export default async function initServiceWorker() {
   if (!('serviceWorker' in navigator)) return;
 
+  // unregister all service workers where scriptURL does not match the current one
+  await unregisterOtherWorkers();
+
   if (UseServiceWorker) {
     try {
       // normally we'd serve the service worker as a static asset from AssetUrlPrefix but
       // the spec strictly requires it to be same-origin so it has to be AppSubUrl to work
       await Promise.all([
         checkCacheValidity(),
-        navigator.serviceWorker.register(`${AppSubUrl}/assets/serviceworker.js`),
+        navigator.serviceWorker.register(joinPaths(AppSubUrl, workerAssetPath)),
       ]);
     } catch (err) {
       console.error(err);
