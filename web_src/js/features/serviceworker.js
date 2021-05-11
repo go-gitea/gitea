3d1624006b59b9e1282ba619ccd1ce9c6204c97e
silverwind <me@silverwind.io>
2021-05-11 21:34:41 +0200

    refactor serviceworker init, avoid double unregister

--- web_src/js/features/serviceworker.js
+++ web_src/js/features/serviceworker.js
@@ -4,25 +4,23 @@ const {UseServiceWorker, AppSubUrl, AssetUrlPrefix, AppVer} = window.config;
 const cachePrefix = 'static-cache-v'; // actual version is set in the service worker script
 const workerAssetPath = joinPaths(AssetUrlPrefix, 'serviceworker.js');
 
-async function unregister() {
-  const registrations = await navigator.serviceWorker.getRegistrations();
-  await Promise.all(registrations.map((registration) => {
-    return registration.active && registration.unregister();
-  }));
+async function unregisterAll() {
+  for (const registration of await navigator.serviceWorker.getRegistrations()) {
+    if (registration?.active) await registration.unregister();
+  }
 }
 
 async function unregisterOtherWorkers() {
   for (const registration of await navigator.serviceWorker.getRegistrations()) {
-    const scriptURL = registration?.active?.scriptURL || '';
+    const scriptURL = registration.active?.scriptURL || '';
     if (!scriptURL.endsWith(workerAssetPath)) await registration.unregister();
   }
 }
 
 async function invalidateCache() {
-  const cacheKeys = await caches.keys();
-  await Promise.all(cacheKeys.map((key) => {
-    return key.startsWith(cachePrefix) && caches.delete(key);
-  }));
+  for (const key of await caches.keys()) {
+    if (key.startsWith(cachePrefix)) caches.delete(key);
+  }
 }
 
 async function checkCacheValidity() {
@@ -39,28 +37,21 @@ async function checkCacheValidity() {
 export default async function initServiceWorker() {
   if (!('serviceWorker' in navigator)) return;
 
-  // unregister all service workers where scriptURL does not match the current one
-  await unregisterOtherWorkers();
-
   if (UseServiceWorker) {
+    // unregister all service workers where scriptURL does not match the current one
+    await unregisterOtherWorkers();
     try {
       // normally we'd serve the service worker as a static asset from AssetUrlPrefix but
       // the spec strictly requires it to be same-origin so it has to be AppSubUrl to work
-      await Promise.all([
-        checkCacheValidity(),
-        navigator.serviceWorker.register(joinPaths(AppSubUrl, workerAssetPath)),
-      ]);
+      await checkCacheValidity();
+      await navigator.serviceWorker.register(joinPaths(AppSubUrl, workerAssetPath));
     } catch (err) {
       console.error(err);
-      await Promise.all([
-        invalidateCache(),
-        unregister(),
-      ]);
+      await invalidateCache();
+      await unregisterAll();
     }
   } else {
-    await Promise.all([
-      invalidateCache(),
-      unregister(),
-    ]);
+    await invalidateCache();
+    await unregisterAll();
   }
 }
