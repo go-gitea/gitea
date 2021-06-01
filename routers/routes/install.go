a05339e810d3801f6c1767c1dc257d4b26afb4a0
a1012112796 <1012112796@qq.com>
2021-06-02 03:12:50 +0800

    Fix language switch for install page (#16043)
    Signed-off-by: a1012112796 <1012112796@qq.com>

--- routers/routes/install.go
+++ routers/routes/install.go
@@ -111,7 +111,7 @@ func InstallRoutes() *web.Route {
 	r.Get("/", routers.Install)
 	r.Post("/", web.Bind(forms.InstallForm{}), routers.InstallPost)
 	r.NotFound(func(w http.ResponseWriter, req *http.Request) {
-		http.Redirect(w, req, setting.AppURL, 302)
+		http.Redirect(w, req, setting.AppURL, http.StatusFound)
 	})
 	return r
 }
