98a2a8e906be446084e9866bd3b6a3698f0d6dc4
KN4CK3R <admin@oldschoolhack.me>
2021-06-16 19:52:59 +0000

    Fixed migration.

--- models/migrations/v184.go
+++ models/migrations/v184.go
@@ -9,6 +9,27 @@ import (
 )
 
 func dropWebhookColumns(x *xorm.Engine) error {
+	// Make sure the columns exist before dropping them
+	type Webhook struct {
+		Signature string `xorm:"TEXT"`
+		IsSSL     bool   `xorm:"is_ssl"`
+	}
+	if err := x.Sync2(new(Webhook)); err != nil {
+		return err
+	}
+
+	type HookTask struct {
+		Typ         models.HookType `xorm:"VARCHAR(16) index"`
+		URL         string          `xorm:"TEXT"`
+		Signature   string          `xorm:"TEXT"`
+		HTTPMethod  string          `xorm:"http_method"`
+		ContentType models.HookContentType
+		IsSSL       bool
+	}
+	if err := x.Sync2(new(HookTask)); err != nil {
+		return err
+	}
+
 	sess := x.NewSession()
 	defer sess.Close()
 	if err := sess.Begin(); err != nil {
