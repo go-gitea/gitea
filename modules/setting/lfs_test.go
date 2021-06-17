5208d9bcde8745d8747980d67d24d5950a88524a
Parnic <github@parnic.com>
2021-06-17 12:18:07 -0500

    Add tests per PR feedback
    This helps to explain why we are forcing a trailing slash on the URL in
    newLFSService() (see commit f927b00fa300d3251be03ace39eb198e38fe8a6e for
    more context).

--- modules/setting/lfs_test.go
+++ modules/setting/lfs_test.go
@@ -7,7 +7,9 @@ package setting
 import (
 	"testing"
 
+	"code.gitea.io/gitea/modules/generate"
 	"github.com/stretchr/testify/assert"
+	"gopkg.in/ini.v1"
 )
 
 func TestLFSRootURL(t *testing.T) {
@@ -20,4 +22,40 @@ func TestLFSRootURL(t *testing.T) {
 	LFS.RootURL = "http://localhost:3001"
 	rootURL = GetLFSRootURL()
 	assert.Equal(t, rootURL, LFS.RootURL)
+}
+
+func TestLFSRootURLFormatNoTrailingSlash(t *testing.T) {
+	Cfg = ini.Empty()
+
+	serverSec, err := Cfg.NewSection("server")
+	assert.Equal(t, err, nil)
+	_, err = serverSec.NewKey("LFS_START_SERVER", "true")
+	assert.Equal(t, err, nil)
+	_, err = serverSec.NewKey("LFS_ROOT_URL", "http://localhost:3001")
+	assert.Equal(t, err, nil)
+	LFS.JWTSecretBase64, err = generate.NewJwtSecret()
+	assert.Equal(t, err, nil)
+
+	newLFSService()
+
+	rootURL := GetLFSRootURL()
+	assert.Equal(t, rootURL, "http://localhost:3001/")
+}
+
+func TestLFSRootURLFormatWithTrailingSlash(t *testing.T) {
+	Cfg = ini.Empty()
+
+	serverSec, err := Cfg.NewSection("server")
+	assert.Equal(t, err, nil)
+	_, err = serverSec.NewKey("LFS_START_SERVER", "true")
+	assert.Equal(t, err, nil)
+	_, err = serverSec.NewKey("LFS_ROOT_URL", "http://localhost:3001/")
+	assert.Equal(t, err, nil)
+	LFS.JWTSecretBase64, err = generate.NewJwtSecret()
+	assert.Equal(t, err, nil)
+
+	newLFSService()
+
+	rootURL := GetLFSRootURL()
+	assert.Equal(t, rootURL, "http://localhost:3001/")
 }
