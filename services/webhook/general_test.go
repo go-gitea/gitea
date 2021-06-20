2474d63474f16617d9f7de070d2fd0c2b9c1bb13
KN4CK3R <admin@oldschoolhack.me>
2021-06-20 18:27:02 +0000

    Added general tests.

--- services/webhook/general_test.go
+++ services/webhook/general_test.go
@@ -5,7 +5,11 @@
 package webhook
 
 import (
+	"testing"
+
 	api "code.gitea.io/gitea/modules/structs"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func createTestPayload() *api.CreatePayload {
@@ -119,6 +123,17 @@ func issueTestPayload() *api.IssuePayload {
 			HTMLURL: "http://localhost:3000/test/repo/issues/2",
 			Title:   "crash",
 			Body:    "issue body",
+			Assignees: []*api.User{
+				{
+					UserName:  "user1",
+					AvatarURL: "http://localhost:3000/user1/avatar",
+				},
+			},
+			Milestone: &api.Milestone{
+				ID:          1,
+				Title:       "Milestone Title",
+				Description: "Milestone Description",
+			},
 		},
 	}
 }
@@ -223,6 +238,17 @@ func pullRequestTestPayload() *api.PullRequestPayload {
 			Title:     "Fix bug",
 			Body:      "fixes bug #2",
 			Mergeable: true,
+			Assignees: []*api.User{
+				{
+					UserName:  "user1",
+					AvatarURL: "http://localhost:3000/user1/avatar",
+				},
+			},
+			Milestone: &api.Milestone{
+				ID:          1,
+				Title:       "Milestone Title",
+				Description: "Milestone Description",
+			},
 		},
 		Review: &api.ReviewPayload{
 			Content: "good job",
@@ -242,5 +268,274 @@ func repositoryTestPayload() *api.RepositoryPayload {
 			Name:     "repo",
 			FullName: "test/repo",
 		},
+	}
+}
+
+func TestGetIssuesPayloadInfo(t *testing.T) {
+	p := issueTestPayload()
+
+	cases := []struct {
+		action         api.HookIssueAction
+		text           string
+		issueTitle     string
+		attachmentText string
+		color          int
+	}{
+		{
+			api.HookIssueOpened,
+			"[test/repo] Issue opened: #2 crash by user1",
+			"#2 crash",
+			"issue body",
+			orangeColor,
+		},
+		{
+			api.HookIssueClosed,
+			"[test/repo] Issue closed: #2 crash by user1",
+			"#2 crash",
+			"",
+			redColor,
+		},
+		{
+			api.HookIssueReOpened,
+			"[test/repo] Issue re-opened: #2 crash by user1",
+			"#2 crash",
+			"",
+			yellowColor,
+		},
+		{
+			api.HookIssueEdited,
+			"[test/repo] Issue edited: #2 crash by user1",
+			"#2 crash",
+			"issue body",
+			yellowColor,
+		},
+		{
+			api.HookIssueAssigned,
+			"[test/repo] Issue assigned to user1: #2 crash by user1",
+			"#2 crash",
+			"",
+			greenColor,
+		},
+		{
+			api.HookIssueUnassigned,
+			"[test/repo] Issue unassigned: #2 crash by user1",
+			"#2 crash",
+			"",
+			yellowColor,
+		},
+		{
+			api.HookIssueLabelUpdated,
+			"[test/repo] Issue labels updated: #2 crash by user1",
+			"#2 crash",
+			"",
+			yellowColor,
+		},
+		{
+			api.HookIssueLabelCleared,
+			"[test/repo] Issue labels cleared: #2 crash by user1",
+			"#2 crash",
+			"",
+			yellowColor,
+		},
+		{
+			api.HookIssueSynchronized,
+			"[test/repo] Issue synchronized: #2 crash by user1",
+			"#2 crash",
+			"",
+			yellowColor,
+		},
+		{
+			api.HookIssueMilestoned,
+			"[test/repo] Issue milestoned to Milestone Title: #2 crash by user1",
+			"#2 crash",
+			"",
+			yellowColor,
+		},
+		{
+			api.HookIssueDemilestoned,
+			"[test/repo] Issue milestone cleared: #2 crash by user1",
+			"#2 crash",
+			"",
+			yellowColor,
+		},
+	}
+
+	for i, c := range cases {
+		p.Action = c.action
+		text, issueTitle, attachmentText, color := getIssuesPayloadInfo(p, noneLinkFormatter, true)
+		assert.Equal(t, c.text, text, "case %d", i)
+		assert.Equal(t, c.issueTitle, issueTitle, "case %d", i)
+		assert.Equal(t, c.attachmentText, attachmentText, "case %d", i)
+		assert.Equal(t, c.color, color, "case %d", i)
+	}
+}
+
+func TestGetPullRequestPayloadInfo(t *testing.T) {
+	p := pullRequestTestPayload()
+
+	cases := []struct {
+		action         api.HookIssueAction
+		text           string
+		issueTitle     string
+		attachmentText string
+		color          int
+	}{
+		{
+			api.HookIssueOpened,
+			"[test/repo] Pull request opened: #12 Fix bug by user1",
+			"#12 Fix bug",
+			"fixes bug #2",
+			greenColor,
+		},
+		{
+			api.HookIssueClosed,
+			"[test/repo] Pull request closed: #12 Fix bug by user1",
+			"#12 Fix bug",
+			"",
+			redColor,
+		},
+		{
+			api.HookIssueReOpened,
+			"[test/repo] Pull request re-opened: #12 Fix bug by user1",
+			"#12 Fix bug",
+			"",
+			yellowColor,
+		},
+		{
+			api.HookIssueEdited,
+			"[test/repo] Pull request edited: #12 Fix bug by user1",
+			"#12 Fix bug",
+			"fixes bug #2",
+			yellowColor,
+		},
+		{
+			api.HookIssueAssigned,
+			"[test/repo] Pull request assigned to user1: #12 Fix bug by user1",
+			"#12 Fix bug",
+			"",
+			greenColor,
+		},
+		{
+			api.HookIssueUnassigned,
+			"[test/repo] Pull request unassigned: #12 Fix bug by user1",
+			"#12 Fix bug",
+			"",
+			yellowColor,
+		},
+		{
+			api.HookIssueLabelUpdated,
+			"[test/repo] Pull request labels updated: #12 Fix bug by user1",
+			"#12 Fix bug",
+			"",
+			yellowColor,
+		},
+		{
+			api.HookIssueLabelCleared,
+			"[test/repo] Pull request labels cleared: #12 Fix bug by user1",
+			"#12 Fix bug",
+			"",
+			yellowColor,
+		},
+		{
+			api.HookIssueSynchronized,
+			"[test/repo] Pull request synchronized: #12 Fix bug by user1",
+			"#12 Fix bug",
+			"",
+			yellowColor,
+		},
+		{
+			api.HookIssueMilestoned,
+			"[test/repo] Pull request milestoned to Milestone Title: #12 Fix bug by user1",
+			"#12 Fix bug",
+			"",
+			yellowColor,
+		},
+		{
+			api.HookIssueDemilestoned,
+			"[test/repo] Pull request milestone cleared: #12 Fix bug by user1",
+			"#12 Fix bug",
+			"",
+			yellowColor,
+		},
+	}
+
+	for i, c := range cases {
+		p.Action = c.action
+		text, issueTitle, attachmentText, color := getPullRequestPayloadInfo(p, noneLinkFormatter, true)
+		assert.Equal(t, c.text, text, "case %d", i)
+		assert.Equal(t, c.issueTitle, issueTitle, "case %d", i)
+		assert.Equal(t, c.attachmentText, attachmentText, "case %d", i)
+		assert.Equal(t, c.color, color, "case %d", i)
+	}
+}
+
+func TestGetReleasePayloadInfo(t *testing.T) {
+	p := pullReleaseTestPayload()
+
+	cases := []struct {
+		action api.HookReleaseAction
+		text   string
+		color  int
+	}{
+		{
+			api.HookReleasePublished,
+			"[test/repo] Release created: v1.0 by user1",
+			greenColor,
+		},
+		{
+			api.HookReleaseUpdated,
+			"[test/repo] Release updated: v1.0 by user1",
+			yellowColor,
+		},
+		{
+			api.HookReleaseDeleted,
+			"[test/repo] Release deleted: v1.0 by user1",
+			redColor,
+		},
+	}
+
+	for i, c := range cases {
+		p.Action = c.action
+		text, color := getReleasePayloadInfo(p, noneLinkFormatter, true)
+		assert.Equal(t, c.text, text, "case %d", i)
+		assert.Equal(t, c.color, color, "case %d", i)
+	}
+}
+
+func TestGetIssueCommentPayloadInfo(t *testing.T) {
+	p := pullRequestCommentTestPayload()
+
+	cases := []struct {
+		action     api.HookIssueCommentAction
+		text       string
+		issueTitle string
+		color      int
+	}{
+		{
+			api.HookIssueCommentCreated,
+			"[test/repo] New comment on pull request #12 Fix bug by user1",
+			"#12 Fix bug",
+			greenColorLight,
+		},
+		{
+			api.HookIssueCommentEdited,
+			"[test/repo] Comment edited on pull request #12 Fix bug by user1",
+			"#12 Fix bug",
+			yellowColor,
+		},
+		{
+			api.HookIssueCommentDeleted,
+			"[test/repo] Comment deleted on pull request #12 Fix bug by user1",
+			"#12 Fix bug",
+			redColor,
+		},
+	}
+
+	for i, c := range cases {
+		p.Action = c.action
+		text, issueTitle, color := getIssueCommentPayloadInfo(p, noneLinkFormatter, true)
+		assert.Equal(t, c.text, text, "case %d", i)
+		assert.Equal(t, c.issueTitle, issueTitle, "case %d", i)
+		assert.Equal(t, c.color, color, "case %d", i)
 	}
 }
