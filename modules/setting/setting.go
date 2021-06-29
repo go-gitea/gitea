a26e840bac28a3e59ce597396bc29d3182e5a9cc
6543 <6543@obermui.de>
2021-06-29 17:20:13 +0200

    add gitlab :gitlab:

--- modules/setting/setting.go
+++ modules/setting/setting.go
@@ -258,8 +258,8 @@ var (
 		DefaultTheme:        `gitea`,
 		Themes:              []string{`gitea`, `arc-green`},
 		Reactions:           []string{`+1`, `-1`, `laugh`, `hooray`, `confused`, `heart`, `rocket`, `eyes`},
-		CustomEmojis:        []string{`gitea`, `codeberg`},
-		CustomEmojisMap:     map[string]string{"gitea": ":gitea:", "codeberg": ":codeberg:"},
+		CustomEmojis:        []string{`gitea`, `codeberg`, `gitlab`},
+		CustomEmojisMap:     map[string]string{"gitea": ":gitea:", "codeberg": ":codeberg:", "gitlab": ":gitlab:"},
 		Notification: struct {
 			MinTimeout            time.Duration
 			TimeoutStep           time.Duration
