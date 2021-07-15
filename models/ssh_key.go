eda977b01055e4161b1857305b1eb97e627a0377
zeripath <art27@cantab.net>
2021-07-15 16:46:07 +0100

    Retry rename on lock induced failures (#16435)
    Backport #16435
    
    Due to external locking on Windows it is possible for an
    os.Rename to fail if the files or directories are being
    used elsewhere.
    
    This PR simply suggests retrying the rename again similar
    to how we handle the os.Remove problems.
    
    Fix #16427
    
    Signed-off-by: Andrew Thornton <art27@cantab.net>
    Co-authored-by: techknowlogick <techknowlogick@gitea.io>

--- models/ssh_key.go
+++ models/ssh_key.go
@@ -834,7 +834,7 @@ func rewriteAllPublicKeys(e Engine) error {
 	}
 
 	t.Close()
-	return os.Rename(tmpPath, fPath)
+	return util.Rename(tmpPath, fPath)
 }
 
 // RegeneratePublicKeys regenerates the authorized_keys file
@@ -1316,7 +1316,7 @@ func rewriteAllPrincipalKeys(e Engine) error {
 	}
 
 	t.Close()
-	return os.Rename(tmpPath, fPath)
+	return util.Rename(tmpPath, fPath)
 }
 
 // ListPrincipalKeys returns a list of principals belongs to given user.
