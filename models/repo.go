ca55e49cc0403a28eaa22d9181a421bb4eaa0048
zeripath <art27@cantab.net>
2021-07-15 20:57:51 +0100

    Retry rename on lock induced failures (#16435) (#16439)
    Backport #16435
    
    Due to external locking on Windows it is possible for an
    os.Rename to fail if the files or directories are being
    used elsewhere.
    
    This PR simply suggests retrying the rename again similar
    to how we handle the os.Remove problems.
    
    Fix #16427
    
    Signed-off-by: Andrew Thornton <art27@cantab.net>
    Co-authored-by: techknowlogick <techknowlogick@gitea.io>
    
    Co-authored-by: techknowlogick <techknowlogick@gitea.io>

--- models/repo.go
+++ models/repo.go
@@ -1215,7 +1215,7 @@ func ChangeRepositoryName(doer *User, repo *Repository, newRepoName string) (err
 	}
 
 	newRepoPath := RepoPath(repo.Owner.Name, newRepoName)
-	if err = os.Rename(repo.RepoPath(), newRepoPath); err != nil {
+	if err = util.Rename(repo.RepoPath(), newRepoPath); err != nil {
 		return fmt.Errorf("rename repository directory: %v", err)
 	}
 
@@ -1226,7 +1226,7 @@ func ChangeRepositoryName(doer *User, repo *Repository, newRepoName string) (err
 		return err
 	}
 	if isExist {
-		if err = os.Rename(wikiPath, WikiPath(repo.Owner.Name, newRepoName)); err != nil {
+		if err = util.Rename(wikiPath, WikiPath(repo.Owner.Name, newRepoName)); err != nil {
 			return fmt.Errorf("rename repository wiki: %v", err)
 		}
 	}
