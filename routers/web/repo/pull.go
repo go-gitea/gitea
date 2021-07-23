6e1c07261255d84acdef1714d75d1d6e818b89b1
zeripath <art27@cantab.net>
2021-07-23 09:05:39 +0100

    Update routers/web/repo/pull.go

--- routers/web/repo/pull.go
+++ routers/web/repo/pull.go
@@ -1042,7 +1042,7 @@ func CompareAndPullRequestPost(ctx *context.Context) {
 		}
 
 		if len(form.Title) > 255 {
-			form.Content = "..." + form.Title[252:] + "\n\n" + form.Content
+			form.Content = "…" + form.Title[254:] + "\n\n" + form.Content
 			form.Title = form.Title[:252] + "..."
 		}
 		middleware.AssignForm(form, ctx.Data)
