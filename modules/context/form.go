bffe40d493381f206cfc02c87fdbc2f64fe6449f
6543 <6543@obermui.de>
2021-07-29 05:38:41 +0200

    enhance func desc

--- modules/context/form.go
+++ modules/context/form.go
@@ -11,12 +11,12 @@ import (
 	"code.gitea.io/gitea/modules/util"
 )
 
-// FormString returns fist value of a query based on key as string
+// FormString returns fist value matching a specific key in form as string
 func (ctx *Context) FormString(key string) string {
 	return ctx.Req.FormValue(key)
 }
 
-// FormStrings returns request form as strings
+// FormStrings returns a string slice by key from form
 func (ctx *Context) FormStrings(key string) []string {
 	if ctx.Req.Form == nil {
 		if err := ctx.Req.ParseMultipartForm(32 << 20); err != nil {
@@ -29,30 +29,31 @@ func (ctx *Context) FormStrings(key string) []string {
 	return nil
 }
 
-// FormTrim returns space trimmed value of a query based on key as string
+// FormTrim returns space trimmed fist value matching a specific key in form as string
 func (ctx *Context) FormTrim(key string) string {
 	return strings.TrimSpace(ctx.Req.FormValue(key))
 }
 
-// FormInt returns request form as int
+// FormInt returns fist value matching a specific key in form as int
 func (ctx *Context) FormInt(key string) int {
 	v, _ := strconv.Atoi(ctx.Req.FormValue(key))
 	return v
 }
 
-// FormInt64 returns request form as int64
+// FormInt64 returns fist value matching a specific key in form as int64
 func (ctx *Context) FormInt64(key string) int64 {
 	v, _ := strconv.ParseInt(ctx.Req.FormValue(key), 10, 64)
 	return v
 }
 
-// FormBool returns value of a query based on key as bool
+// FormBool returns true if value matching a specific key in form "1" or "true"
 func (ctx *Context) FormBool(key string) bool {
 	v, _ := strconv.ParseBool(ctx.Req.FormValue(key))
 	return v
 }
 
-// FormOptionalBool returns value of a query based on key as OptionalBool
+// FormOptionalBool returns an OptionalBoolTrue or OptionalBoolFalse if value for key exist
+// else it return OptionalBoolNone
 func (ctx *Context) FormOptionalBool(key string) util.OptionalBool {
 	value := ctx.Req.FormValue(key)
 	if len(value) == 0 {
