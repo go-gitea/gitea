d1087857483c721b184e1b68544af608b289262b
Lunny Xiao <xiaolunwen@gmail.com>
2021-07-29 09:53:07 +0800

    Fix bug

--- routers/web/web.go
+++ routers/web/web.go
@@ -1357,7 +1357,7 @@ func RegisterRoutes(m *web.Route) {
 				m.Get("/objects/{head:[0-9a-f]{2}}/{hash:[0-9a-f]{38}}", repo.GetLooseObject)
 				m.Get("/objects/pack/pack-{file:[0-9a-f]{40}}.pack", repo.GetPackFile)
 				m.Get("/objects/pack/pack-{file:[0-9a-f]{40}}.idx", repo.GetIdxFile)
-			}, repo.HTTPMustEnabled, ignSignInAndCsrf, repo.HTTPCors, context_service.UserAssignmentWeb())
+			}, repo.HTTPMustEnabled(), ignSignInAndCsrf, repo.HTTPCors(), context_service.UserAssignmentWeb())
 		})
 	})
 	// ***** END: Repository *****
