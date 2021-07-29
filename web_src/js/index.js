1372bac4adab03e041c97329a6abdba90f61b7f4
Andrew Thornton <art27@cantab.net>
2021-07-29 21:50:32 +0100

    Restore #10096/#8638 and re-fix #15172
    This PR restores the vendored and patched dropdow from #8638. It
    however, abandons the calls to `click()` using instead the default
    dropdown click calls instead. This prevents the issue of the dropdown
    grabbing focus permanently however, this may have negative effects on
    the effect of focus on the dropdowns.
    
    Of note, the behaviour of the template selector dropdown on the repo
    creation page is slightly odd - I don't believe that this odd behaviour
    is caused by this PR but rather by the feed source for this. I suspect
    that the dropdown should be adding a delete button to its selection.
    
    Fix #15172
    References: #7057
    
    Signed-off-by: Andrew Thornton <art27@cantab.net>

--- web_src/js/index.js
+++ web_src/js/index.js
@@ -3,6 +3,7 @@ import './publicpath.js';
 import Vue from 'vue';
 import {htmlEscape} from 'escape-goat';
 import 'jquery.are-you-sure';
+import './vendor/dropdown.js';
 
 import ActivityTopAuthors from './components/ActivityTopAuthors.vue';
 import attachTribute from './features/tribute.js';
