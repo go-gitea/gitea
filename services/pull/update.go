2aa7b6c0e573b98e8b371f3774d572a58f24745f
a1012112796 <1012112796@qq.com>
2021-07-31 11:20:06 +0800

    refactor

--- services/pull/update.go
+++ services/pull/update.go
@@ -6,9 +6,6 @@ package pull
 
 import (
 	"fmt"
-	"os"
-	"strings"
-	"time"
 
 	"code.gitea.io/gitea/models"
 	"code.gitea.io/gitea/modules/git"
@@ -17,12 +14,23 @@ import (
 
 // Update updates pull request with base branch.
 func Update(pull *models.PullRequest, doer *models.User, message string, rebase bool) error {
-	//use merge functions but switch repo's and branch's
-	pr := &models.PullRequest{
-		HeadRepoID: pull.BaseRepoID,
-		BaseRepoID: pull.HeadRepoID,
-		HeadBranch: pull.BaseBranch,
-		BaseBranch: pull.HeadBranch,
+	var (
+		pr    *models.PullRequest
+		style models.MergeStyle
+	)
+
+	if rebase {
+		pr = pull
+		style = models.MergeStyleRebaseUpdate
+	} else {
+		//use merge functions but switch repo's and branch's
+		pr = &models.PullRequest{
+			HeadRepoID: pull.BaseRepoID,
+			BaseRepoID: pull.HeadRepoID,
+			HeadBranch: pull.BaseBranch,
+			BaseBranch: pull.HeadBranch,
+		}
+		style = models.MergeStyleMerge
 	}
 
 	if pull.Flow == models.PullRequestFlowAGit {
@@ -45,11 +53,7 @@ func Update(pull *models.PullRequest, doer *models.User, message string, rebase 
 		return fmt.Errorf("HeadBranch of PR %d is up to date", pull.Index)
 	}
 
-	if rebase {
-		err = doRebase(pr, doer)
-	} else {
-		_, err = rawMerge(pr, doer, models.MergeStyleMerge, message)
-	}
+	_, err = rawMerge(pr, doer, style, message)
 
 	defer func() {
 		go AddTestPullRequestTask(doer, pr.HeadRepo.ID, pr.HeadBranch, false, "", "")
@@ -122,78 +126,4 @@ func GetDiverging(pr *models.PullRequest) (*git.DivergeObject, error) {
 
 	diff, err := git.GetDivergingCommits(tmpRepo, "base", "tracking")
 	return &diff, err
-}
-
-func doRebase(pr *models.PullRequest, doer *models.User) error {
-	// 1. Clone base repo.
-	tmpBasePath, err := createTemporaryRepo(pr)
-	if err != nil {
-		log.Error("CreateTemporaryPath: %v", err)
-		return err
-	}
-	defer func() {
-		if err := models.RemoveTemporaryPath(tmpBasePath); err != nil {
-			log.Error("Update-By-Rebase: RemoveTemporaryPath: %s", err)
-		}
-	}()
-
-	baseBranch := "base"
-	trackingBranch := "tracking"
-
-	// 2. do rebase to ttacking branch
-	sig := doer.NewGitSig()
-	committer := sig
-
-	// Determine if we should sign
-	signArg := ""
-	if git.CheckGitVersionAtLeast("1.7.9") == nil {
-		sign, keyID, signer, _ := pr.SignMerge(doer, tmpBasePath, "HEAD", trackingBranch)
-		if sign {
-			signArg = "-S" + keyID
-			if pr.BaseRepo.GetTrustModel() == models.CommitterTrustModel || pr.BaseRepo.GetTrustModel() == models.CollaboratorCommitterTrustModel {
-				committer = signer
-			}
-		} else if git.CheckGitVersionAtLeast("2.0.0") == nil {
-			signArg = "--no-gpg-sign"
-		}
-	}
-
-	commitTimeStr := time.Now().Format(time.RFC3339)
-
-	// Because this may call hooks we should pass in the environment
-	env := append(os.Environ(),
-		"GIT_AUTHOR_NAME="+sig.Name,
-		"GIT_AUTHOR_EMAIL="+sig.Email,
-		"GIT_AUTHOR_DATE="+commitTimeStr,
-		"GIT_COMMITTER_NAME="+committer.Name,
-		"GIT_COMMITTER_EMAIL="+committer.Email,
-		"GIT_COMMITTER_DATE="+commitTimeStr,
-	)
-
-	var outbuf, errbuf strings.Builder
-	err = git.NewCommand("rebase", trackingBranch, signArg).RunInDirTimeoutEnvFullPipeline(env, -1, tmpBasePath, &outbuf, &errbuf, nil)
-	if err != nil {
-		log.Error("git rebase [%s:%s -> %s:%s]: %v\n%s\n%s", pr.BaseRepo.FullName(), pr.BaseBranch, pr.HeadRepo.FullName(), pr.HeadBranch, err, outbuf.String(), errbuf.String())
-		return fmt.Errorf("git rebase [%s:%s -> %s:%s]: %v\n%s\n%s", pr.BaseRepo.FullName(), pr.BaseBranch, pr.HeadRepo.FullName(), pr.HeadBranch, err, outbuf.String(), errbuf.String())
-	}
-
-	// 3. force push to base branch
-	env = models.FullPushingEnvironment(doer, doer, pr.BaseRepo, pr.BaseRepo.Name, pr.ID)
-
-	outbuf.Reset()
-	errbuf.Reset()
-	if err := git.NewCommand("push", "-f", "origin", baseBranch+":refs/heads/"+pr.BaseBranch).RunInDirTimeoutEnvPipeline(env, -1, tmpBasePath, &outbuf, &errbuf); err != nil {
-		if strings.Contains(errbuf.String(), "! [remote rejected]") {
-			err := &git.ErrPushRejected{
-				StdOut: outbuf.String(),
-				StdErr: errbuf.String(),
-				Err:    err,
-			}
-			err.GenerateMessage()
-			return err
-		}
-		return fmt.Errorf("git force push: %s", errbuf.String())
-	}
-
-	return nil
 }
