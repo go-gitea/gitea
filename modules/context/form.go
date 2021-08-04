1e1da6740cedb296e89789628552b8b58ac9e974
6543 <6543@obermui.de>
2021-08-04 09:59:52 +0200

    Apply suggestions from code review
    Co-authored-by: zeripath <art27@cantab.net>

--- modules/context/form.go
+++ modules/context/form.go
@@ -11,12 +11,12 @@ import (
 	"code.gitea.io/gitea/modules/util"
 )
 
-// FormString returns fist value matching a specific key in form as string
+// FormString returns the first value matching the provided key in the form as a string
 func (ctx *Context) FormString(key string) string {
 	return ctx.Req.FormValue(key)
 }
 
-// FormStrings returns a string slice by key from form
+// FormStrings returns a string slice for the provided key from the form
 func (ctx *Context) FormStrings(key string) []string {
 	if ctx.Req.Form == nil {
 		if err := ctx.Req.ParseMultipartForm(32 << 20); err != nil {
@@ -29,31 +29,31 @@ func (ctx *Context) FormStrings(key string) []string {
 	return nil
 }
 
-// FormTrim returns space trimmed fist value matching a specific key in form as string
+// FormTrim returns the first value for the provided key in the form as a space trimmed string
 func (ctx *Context) FormTrim(key string) string {
 	return strings.TrimSpace(ctx.Req.FormValue(key))
 }
 
-// FormInt returns fist value matching a specific key in form as int
+// FormInt returns the first value for the provided key in the form as an int
 func (ctx *Context) FormInt(key string) int {
 	v, _ := strconv.Atoi(ctx.Req.FormValue(key))
 	return v
 }
 
-// FormInt64 returns fist value matching a specific key in form as int64
+// FormInt64 returns the first value for the provided key in the form as an int64
 func (ctx *Context) FormInt64(key string) int64 {
 	v, _ := strconv.ParseInt(ctx.Req.FormValue(key), 10, 64)
 	return v
 }
 
-// FormBool returns true if value matching a specific key in form "1" or "true"
+// FormBool returns true if the value for the provided key in the form is "1" or "true"
 func (ctx *Context) FormBool(key string) bool {
 	v, _ := strconv.ParseBool(ctx.Req.FormValue(key))
 	return v
 }
 
-// FormOptionalBool returns an OptionalBoolTrue or OptionalBoolFalse if value for key exist
-// else it return OptionalBoolNone
+// FormOptionalBool returns an OptionalBoolTrue or OptionalBoolFalse if the value 
+// for the provided key exists in the form else it returns OptionalBoolNone
 func (ctx *Context) FormOptionalBool(key string) util.OptionalBool {
 	value := ctx.Req.FormValue(key)
 	if len(value) == 0 {
