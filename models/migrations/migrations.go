86bdaedb1d287b4d6beda6e43506823cbde0a89b
Andrew Thornton <art27@cantab.net>
2021-08-05 13:03:55 +0100

    try just dropping the index only
    Signed-off-by: Andrew Thornton <art27@cantab.net>

--- models/migrations/migrations.go
+++ models/migrations/migrations.go
@@ -858,9 +858,6 @@ func dropTableColumns(sess *xorm.Session, tableName string, columnNames ...strin
 			return fmt.Errorf("Find constraints: %v", err)
 		}
 		for _, constraint := range constraints {
-			if _, err := sess.Exec(fmt.Sprintf("ALTER TABLE `%s` DROP CONSTRAINT `%s`", tableName, constraint)); err != nil {
-				return fmt.Errorf("Drop table `%s` index constraint `%s`: %v", tableName, constraint, err)
-			}
 			if _, err := sess.Exec(fmt.Sprintf("DROP INDEX `%[2]s` ON `%[1]s`", tableName, constraint)); err != nil {
 				return fmt.Errorf("Drop index `%[2]s` on `%[1]s`: %v", tableName, constraint, err)
 			}
