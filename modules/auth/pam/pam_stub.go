07130e8f4b73674699fc3eeceeee72f959c89d1a
jolheiser <john.olheiser@gmail.com>
2021-08-22 22:45:57 -0500

    Fix build tags
    Signed-off-by: jolheiser <john.olheiser@gmail.com>

--- modules/auth/pam/pam_stub.go
+++ modules/auth/pam/pam_stub.go
@@ -1,9 +1,9 @@
-//go:build !pam
-// +build !pam
-
 // Copyright 2014 The Gogs Authors. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
+
+//go:build !pam
+// +build !pam
 
 package pam
 
--- routers/private/manager_unix.go
+++ routers/private/manager_unix.go
@@ -1,9 +1,9 @@
-//go:build !windows
-// +build !windows
-
 // Copyright 2020 The Gitea Authors. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
+
+//go:build !windows
+// +build !windows
 
 package private
 
