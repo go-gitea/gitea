328ca6ed3f6d59163c1ec3f5eab20e35e1c207c4
Sysoev, Vladimir <vladimir.sysoev@sarov-itc.ru>
2021-08-26 21:56:06 +0300

    implemented date adding for tracked time. it is required to calculate monthly timesheets

--- routers/web/repo/issue_timetrack.go
+++ routers/web/repo/issue_timetrack.go
@@ -26,7 +26,6 @@ func AddTimeManually(c *context.Context) {
 		return
 	}
 	url := issue.HTMLURL()
-
 	if c.HasError() {
 		c.Flash.Error(c.GetErrMsg())
 		c.Redirect(url)
@@ -40,8 +39,11 @@ func AddTimeManually(c *context.Context) {
 		c.Redirect(url, http.StatusSeeOther)
 		return
 	}
-
-	if _, err := models.AddTime(c.User, issue, int64(total.Seconds()), time.Now()); err != nil {
+	created, err := time.Parse("2006-01-02", form.Created)
+	if err != nil {
+		created = time.Now()
+	}
+	if _, err := models.AddTime(c.User, issue, int64(total.Seconds()), created); err != nil {
 		c.ServerError("AddTime", err)
 		return
 	}
