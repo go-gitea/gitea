cd1bc10fa35ea050de089554e6e03dde9fceee24
KN4CK3R <admin@oldschoolhack.me>
2021-08-26 18:06:00 +0200

    Implemented io.Writer.

--- modules/util/filebuffer/file_backed_buffer.go
+++ modules/util/filebuffer/file_backed_buffer.go
@@ -9,51 +9,94 @@ import (
 	"errors"
 	"io"
 	"io/ioutil"
-	"math"
 	"os"
 )
+
+const maxInt = int(^uint(0) >> 1) // taken from bytes.Buffer
 
 var (
 	// ErrInvalidMemorySize occurs if the memory size is not in a valid range
 	ErrInvalidMemorySize = errors.New("Memory size must be greater 0 and lower math.MaxInt32")
+	// ErrWriteAfterRead occurs if Write is called after a read operation
+	ErrWriteAfterRead = errors.New("Write is unsupported after a read operation")
 )
 
-// FileBackedBuffer implements io.ReadSeekCloser and io.ReaderAt
-type FileBackedBuffer struct {
-	size    int64
-	buffer  *bytes.Reader
-	tmpFile *os.File
+type readAtSeeker interface {
+	io.ReadSeeker
+	io.ReaderAt
 }
 
-// CreateFromReader creates a file backed buffer which uses a buffer with a fixed memory size.
-// If more data is available a temporary file is used to store the data.
-func CreateFromReader(r io.Reader, maxMemorySize int) (*FileBackedBuffer, error) {
-	if maxMemorySize < 0 || maxMemorySize == math.MaxInt32 {
+// FileBackedBuffer uses a memory buffer with a fixed size.
+// If more data is written a temporary file is used instead.
+// It implements io.ReadWriteCloser, io.ReadSeekCloser and io.ReaderAt
+type FileBackedBuffer struct {
+	maxMemorySize int64
+	size          int64
+	buffer        bytes.Buffer
+	file          *os.File
+	reader        readAtSeeker
+}
+
+// New creates a file backed buffer with a specific maximum memory size
+func New(maxMemorySize int) (*FileBackedBuffer, error) {
+	if maxMemorySize < 0 || maxMemorySize > maxInt {
 		return nil, ErrInvalidMemorySize
 	}
 
-	var buf bytes.Buffer
-	n, err := io.CopyN(&buf, r, int64(maxMemorySize+1))
-	if err == io.EOF {
-		return &FileBackedBuffer{
-			size:   n,
-			buffer: bytes.NewReader(buf.Bytes()),
-		}, nil
-	}
-	file, err := ioutil.TempFile("", "buffer-")
-	if err != nil {
-		return nil, err
-	}
-
-	n, err = io.Copy(file, io.MultiReader(&buf, r))
-	if err != nil {
-		return nil, err
-	}
-
 	return &FileBackedBuffer{
-		size:    n,
-		tmpFile: file,
+		maxMemorySize: int64(maxMemorySize),
 	}, nil
+}
+
+// CreateFromReader creates a file backed buffer and copies the provided reader data into it.
+func CreateFromReader(r io.Reader, maxMemorySize int) (*FileBackedBuffer, error) {
+	b, err := New(maxMemorySize)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = io.Copy(b, r)
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+// Write implements io.Writer
+func (b *FileBackedBuffer) Write(p []byte) (int, error) {
+	if b.reader != nil {
+		return 0, ErrWriteAfterRead
+	}
+
+	var n int
+	var err error
+
+	if b.file != nil {
+		n, err = b.file.Write(p)
+	} else {
+		if b.size+int64(len(p)) > b.maxMemorySize {
+			b.file, err = ioutil.TempFile("", "buffer-")
+			if err != nil {
+				return 0, err
+			}
+
+			_, err = io.Copy(b.file, &b.buffer)
+			if err != nil {
+				return 0, err
+			}
+
+			return b.Write(p)
+		}
+
+		n, err = b.buffer.Write(p)
+	}
+
+	if err != nil {
+		return n, err
+	}
+	b.size += int64(n)
+	return n, nil
 }
 
 // Size returns the byte size of the buffered data
@@ -61,35 +104,44 @@ func (b *FileBackedBuffer) Size() int64 {
 	return b.size
 }
 
+func (b *FileBackedBuffer) switchToReader() {
+	if b.reader != nil {
+		return
+	}
+
+	if b.file != nil {
+		b.reader = b.file
+	} else {
+		b.reader = bytes.NewReader(b.buffer.Bytes())
+	}
+}
+
 // Read implements io.Reader
 func (b *FileBackedBuffer) Read(p []byte) (int, error) {
-	if b.tmpFile != nil {
-		return b.tmpFile.Read(p)
-	}
-	return b.buffer.Read(p)
+	b.switchToReader()
+
+	return b.reader.Read(p)
 }
 
 // ReadAt implements io.ReaderAt
 func (b *FileBackedBuffer) ReadAt(p []byte, off int64) (int, error) {
-	if b.tmpFile != nil {
-		return b.tmpFile.ReadAt(p, off)
-	}
-	return b.buffer.ReadAt(p, off)
+	b.switchToReader()
+
+	return b.reader.ReadAt(p, off)
 }
 
 // Seek implements io.Seeker
 func (b *FileBackedBuffer) Seek(offset int64, whence int) (int64, error) {
-	if b.tmpFile != nil {
-		return b.tmpFile.Seek(offset, whence)
-	}
-	return b.buffer.Seek(offset, whence)
+	b.switchToReader()
+
+	return b.reader.Seek(offset, whence)
 }
 
 // Close implements io.Closer
 func (b *FileBackedBuffer) Close() error {
-	if b.tmpFile != nil {
-		err := b.tmpFile.Close()
-		os.Remove(b.tmpFile.Name())
+	if b.file != nil {
+		err := b.file.Close()
+		os.Remove(b.file.Name())
 		return err
 	}
 	return nil
