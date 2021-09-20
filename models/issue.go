62a748ddca35beeb04b04b02039ea76f0948cef0
qwerty287 <ndev@web.de>
2021-09-20 17:11:57 +0200

    Fix condition on postgres

--- models/issue.go
+++ models/issue.go
@@ -1320,9 +1320,9 @@ func applyReviewRequestedCondition(sess *xorm.Session, reviewRequestedID int64) 
 }
 
 func applySubscribedCondition(sess *xorm.Session, subscribedID int64) *xorm.Session {
-	return sess.And("(issue.id IN (SELECT issue_id FROM issue_watch WHERE is_watching = 1 AND user_id = ?)) "+
-		"OR ((issue.id IN ((SELECT issue_id FROM comment WHERE poster_id = ?))) AND (NOT issue.id IN (SELECT issue_id FROM issue_watch WHERE user_id = ? AND is_watching = 0))) "+
-		"OR ((issue.id IN (SELECT id FROM issue WHERE poster_id = ?)) AND (NOT issue.id IN (SELECT issue_id FROM issue_watch WHERE user_id = ? AND is_watching = 0)))",
+	return sess.And("(issue.id IN (SELECT issue_id FROM issue_watch WHERE is_watching = true AND user_id = ?)) "+
+		"OR ((issue.id IN ((SELECT issue_id FROM comment WHERE poster_id = ?))) AND (NOT issue.id IN (SELECT issue_id FROM issue_watch WHERE user_id = ? AND is_watching = false))) "+
+		"OR ((issue.id IN (SELECT id FROM issue WHERE poster_id = ?)) AND (NOT issue.id IN (SELECT issue_id FROM issue_watch WHERE user_id = ? AND is_watching = false)))",
 		subscribedID, subscribedID, subscribedID, subscribedID, subscribedID)
 }
 
