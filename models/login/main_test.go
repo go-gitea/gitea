9fa83dcae7e9c6c19408f7b49b82b4469d5cb91f
Lunny Xiao <xiaolunwen@gmail.com>
2021-09-22 10:47:40 +0800

    Fix test

---
+++ models/login/main_test.go
@@ -0,0 +1,15 @@
+// Copyright 2020 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package login
+
+import (
+	"testing"
+
+	"code.gitea.io/gitea/models/db"
+)
+
+func TestMain(m *testing.M) {
+	db.MainTest(m, "..", "..")
+}
