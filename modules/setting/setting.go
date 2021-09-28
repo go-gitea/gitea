9834049d2f668eef353648d77f5fb59645ab2501
Matti R <matti@mdranta.net>
2021-09-28 00:11:38 -0400

    placate lint

--- modules/setting/setting.go
+++ modules/setting/setting.go
@@ -900,7 +900,7 @@ func NewContext() {
 	RunUser = Cfg.Section("").Key("RUN_USER").MustString(user.CurrentUsername())
 	// The following an unsafe option, purposely left out of documentation. Please do not run Gitea as root. It will only cause future headaches.
 	// Please don't use root as a bandaid to "fix" something that is brokenn, instead the broken thing should instead be fixed properly.
-	UnsafeAllowRunAsRoot = Cfg.Section("").Key("I_AM_BEING_UNSAFE_RUNNING_AS_ROOT").MustBool(false)
+	unsafeAllowRunAsRoot = Cfg.Section("").Key("I_AM_BEING_UNSAFE_RUNNING_AS_ROOT").MustBool(false)
 	RunMode = Cfg.Section("").Key("RUN_MODE").MustString("prod")
 	// Does not check run user when the install lock is off.
 	if InstallLock {
@@ -910,10 +910,10 @@ func NewContext() {
 		}
 	}
 
-	if RunUser == "root" && !UnsafeAllowRunAsRoot {
-		if !UnsafeAllowRunAsRoot {
+	if RunUser == "root" {
+		if !unsafeAllowRunAsRoot {
 			// Special thanks to VLC which inspired the wording of this messaging.
-			log.Fatal("Gitea is not supposed to be run as root. Sorry. If you need to use privileged TCP ports please instead use setcap and the `cap_net_bind_service` permission")	
+			log.Fatal("Gitea is not supposed to be run as root. Sorry. If you need to use privileged TCP ports please instead use setcap and the `cap_net_bind_service` permission")
 		}
 		log.Warn("You are running Gitea using the root user, and have purposely chosen to skip built-in protections around this. You have been warned against this.")
 	}
