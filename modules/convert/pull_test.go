f387b03f95119ead0b32db745430e19bd4da56f8
Danila Kryukov <pricly_yellow@dismail.de>
2021-10-04 14:30:29 +0700

    API pull's head/base have correct permission
    * for all pull requests API return permissions of caller
    * for all webhook return empty permissions
    
    Signed-off-by: Danila Kryukov <pricly_yellow@dismail.de>

--- modules/convert/pull_test.go
+++ modules/convert/pull_test.go
@@ -21,7 +21,7 @@ func TestPullRequest_APIFormat(t *testing.T) {
 	pr := db.AssertExistsAndLoadBean(t, &models.PullRequest{ID: 1}).(*models.PullRequest)
 	assert.NoError(t, pr.LoadAttributes())
 	assert.NoError(t, pr.LoadIssue())
-	apiPullRequest := ToAPIPullRequest(pr)
+	apiPullRequest := ToAPIPullRequest(pr, nil)
 	assert.NotNil(t, apiPullRequest)
 	assert.EqualValues(t, &structs.PRBranchInfo{
 		Name:       "branch1",
@@ -38,7 +38,7 @@ func TestPullRequest_APIFormat(t *testing.T) {
 	// simulate fork deletion
 	pr.HeadRepo = nil
 	pr.HeadRepoID = 100000
-	apiPullRequest = ToAPIPullRequest(pr)
+	apiPullRequest = ToAPIPullRequest(pr, nil)
 	assert.NotNil(t, apiPullRequest)
 	assert.Nil(t, apiPullRequest.Head.Repository)
 	assert.EqualValues(t, -1, apiPullRequest.Head.RepoID)
