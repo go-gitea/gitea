ea566ccd20dbb947cd490c0f8bfb408d9809e90c
qwerty287 <ndev@web.de>
2021-10-04 21:13:28 +0200

    Don't load projects if permissions don't allow it

--- routers/web/repo/issue.go
+++ routers/web/repo/issue.go
@@ -328,17 +328,19 @@ func issues(ctx *context.Context, milestoneID, projectID int64, isPullOption uti
 		return 0
 	}
 
-	projects, _, err := models.GetProjects(models.ProjectSearchOptions{
-		RepoID:   repo.ID,
-		Type:     models.ProjectTypeRepository,
-		IsClosed: util.OptionalBoolOf(isShowClosed),
-	})
-	if err != nil {
-		ctx.ServerError("GetProjects", err)
-		return
+	if ctx.Repo.CanWriteIssuesOrPulls(ctx.Params(":type") == "pulls") {
+		projects, _, err := models.GetProjects(models.ProjectSearchOptions{
+			RepoID:   repo.ID,
+			Type:     models.ProjectTypeRepository,
+			IsClosed: util.OptionalBoolOf(isShowClosed),
+		})
+		if err != nil {
+			ctx.ServerError("GetProjects", err)
+			return
+		}
+		ctx.Data["Projects"] = projects
 	}
 
-	ctx.Data["Projects"] = projects
 	ctx.Data["IssueStats"] = issueStats
 	ctx.Data["SelLabelIDs"] = labelIDs
 	ctx.Data["SelectLabels"] = selectLabels
