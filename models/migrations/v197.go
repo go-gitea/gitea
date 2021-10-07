0314c5637b93cd9afeca20de4e08d4ce55050d4e
techknowlogick <techknowlogick@gitea.io>
2021-10-06 22:42:24 -0400

    Apply suggestions from code review

--- models/migrations/v197.go
+++ models/migrations/v197.go
@@ -13,7 +13,8 @@ import (
 func createUserSettingsTable(x *xorm.Engine) error {
 	type UserSetting struct {
 		ID     int64  `xorm:"pk autoincr"`
-		UserID int64  `xorm:"index"`              // to load all of someone's settings
+		UserID int64  `xorm:"index unique(key_userid)"`              // to load all of someone's settings
+		Key    string `xorm:"varchar(255) index unique(key_userid)"` // ensure key is always lowercase
 		Key    string `xorm:"varchar(255) index"` // ensure key is always lowercase
 		Value  string `xorm:"text"`
 	}
