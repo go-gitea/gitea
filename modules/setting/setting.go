d1b3d1ce1f2aafddfcb8c235e7745212e42c0e2d
wxiaoguang <wxiaoguang@gmail.com>
2021-10-19 12:41:09 +0800

    fix comment

--- modules/setting/setting.go
+++ modules/setting/setting.go
@@ -688,13 +688,13 @@ func NewContext() {
 	StaticCacheTime = sec.Key("STATIC_CACHE_TIME").MustDuration(6 * time.Hour)
 	AppDataPath = sec.Key("APP_DATA_PATH").MustString(path.Join(AppWorkPath, "data"))
 	if _, err = os.Stat(AppDataPath); err != nil {
-		// FIXME: Many calls to MkdirAll are using 0644 (not executable) or `os.ModePerm`(0o777, world-wide writable), which are incorrect.
 		// FIXME: There are too many calls to MkdirAll in old code. It is incorrect.
 		// For example, if someDir=/mnt/vol1/gitea-home/data, if the mount point /mnt/vol1 is not mounted when Gitea runs,
 		// then gitea will make new empty directories in /mnt/vol1, all are stored in the root filesystem.
 		// The correct behavior should be: creating parent directories is end users' duty. We only create sub-directories in existing parent directories.
-		// Now we can take the first step to do correctly (Mkdir) in other packages, and prepare the AppDataPath here, then make a refactor in future.
-		err = os.MkdirAll(AppDataPath, os.FileMode(0755))
+		// For quickstart, the parent directories should be created automatically for first startup (eg: a flag or a check of INSTALL_LOCK).
+		// Now we can take the first step to do correctly (using Mkdir) in other packages, and prepare the AppDataPath here, then make a refactor in future.
+		err = os.MkdirAll(AppDataPath, os.ModePerm)
 		if err != nil {
 			log.Fatal("Failed to create the directory for app data path '%s'", AppDataPath)
 		}
