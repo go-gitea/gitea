83df0caf15c4a8c3b9336987f329501507c6d527
wxiaoguang <wxiaoguang@gmail.com>
2021-10-21 17:22:43 +0800

    Sync gitea app path for git hooks and authorized keys when starting (#17335)
    Gitea writes its own AppPath into git hook scripts. If Gitea's AppPath changes, then the git push will fail.
    
    This PR:
    
    * Introduce an AppState module, it can persist app states into database
    * During GlobalInit, Gitea will check if the current AppPath is the same as last one. If they don't match, Gitea will sync git hooks.
    * Refactor some code to make them more clear.
    * Also, "Detect if gitea binary's name changed" #11341 is related, we call models.RewriteAllPublicKeys to update ssh authorized_keys file

---
+++ models/migrations/v200.go
@@ -0,0 +1,23 @@
+// Copyright 2021 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package migrations
+
+import (
+	"fmt"
+
+	"xorm.io/xorm"
+)
+
+func addTableAppState(x *xorm.Engine) error {
+	type AppState struct {
+		ID       string `xorm:"pk varchar(200)"`
+		Revision int64
+		Content  string `xorm:"LONGTEXT"`
+	}
+	if err := x.Sync2(new(AppState)); err != nil {
+		return fmt.Errorf("Sync2: %v", err)
+	}
+	return nil
+}
--- modules/setting/setting.go
+++ modules/setting/setting.go
@@ -683,6 +683,18 @@ func NewContext() {
 	StaticRootPath = sec.Key("STATIC_ROOT_PATH").MustString(StaticRootPath)
 	StaticCacheTime = sec.Key("STATIC_CACHE_TIME").MustDuration(6 * time.Hour)
 	AppDataPath = sec.Key("APP_DATA_PATH").MustString(path.Join(AppWorkPath, "data"))
+	if _, err = os.Stat(AppDataPath); err != nil {
+		// FIXME: There are too many calls to MkdirAll in old code. It is incorrect.
+		// For example, if someDir=/mnt/vol1/gitea-home/data, if the mount point /mnt/vol1 is not mounted when Gitea runs,
+		// then gitea will make new empty directories in /mnt/vol1, all are stored in the root filesystem.
+		// The correct behavior should be: creating parent directories is end users' duty. We only create sub-directories in existing parent directories.
+		// For quickstart, the parent directories should be created automatically for first startup (eg: a flag or a check of INSTALL_LOCK).
+		// Now we can take the first step to do correctly (using Mkdir) in other packages, and prepare the AppDataPath here, then make a refactor in future.
+		err = os.MkdirAll(AppDataPath, os.ModePerm)
+		if err != nil {
+			log.Fatal("Failed to create the directory for app data path '%s'", AppDataPath)
+		}
+	}
 	EnableGzip = sec.Key("ENABLE_GZIP").MustBool()
 	EnablePprof = sec.Key("ENABLE_PPROF").MustBool(false)
 	PprofDataPath = sec.Key("PPROF_DATA_PATH").MustString(path.Join(AppWorkPath, "data/tmp/pprof"))
