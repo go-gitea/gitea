cca722cb85aee6f94cfdda29026cdf573182c82c
wxiaoguang <wxiaoguang@gmail.com>
2021-10-28 14:36:16 +0800

    we can use `time.NewTicker` safely and it reduces memory allocations.

--- routers/common/logger_context.go
+++ routers/common/logger_context.go
@@ -199,11 +199,14 @@ func (lh *logContextHandler) startSlowQueryDetector(threshold time.Duration) {
 		// This go-routine checks all active requests every second.
 		// If a request has been running for a long time (eg: /user/events), we also print a log with "still-executing" message
 		// After the "still-executing" log is printed, the record will be removed from the map to prevent from duplicated logs in future
+
+		// We do not care about accurate duration here. It just does the check periodically, 0.5s or 1.5ms are all OK.
+		t := time.NewTicker(time.Second)
 		for {
 			select {
 			case <-baseCtx.Done():
 				return
-			case <-time.After(time.Second):
+			case <-t.C:
 				now := time.Now()
 
 				var slowRequests []*logRequestRecord
