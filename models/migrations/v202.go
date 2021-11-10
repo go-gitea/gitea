cbb145cb267f4cc490c8c6e51ec7eb40b80d3f0f
techknowlogick <techknowlogick@gitea.io>
2021-11-09 21:46:46 -0500

    match migration struct to user setting struct

--- models/migrations/v202.go
+++ models/migrations/v202.go
@@ -12,10 +12,10 @@ import (
 
 func createUserSettingsTable(x *xorm.Engine) error {
 	type UserSetting struct {
-		ID     int64  `xorm:"pk autoincr"`
-		UserID int64  `xorm:"index unique(key_userid)"`              // to load all of someone's settings
-		Key    string `xorm:"varchar(255) index unique(key_userid)"` // ensure key is always lowercase
-		Value  string `xorm:"text"`
+		ID           int64  `xorm:"pk autoincr"`
+		UserID       int64  `xorm:"index unique(key_userid)"`              // to load all of someone's settings
+		SettingKey   string `xorm:"varchar(255) index unique(key_userid)"` // ensure key is always lowercase
+		SettingValue string `xorm:"text"`
 	}
 	if err := x.Sync2(new(UserSetting)); err != nil {
 		return fmt.Errorf("sync2: %v", err)
