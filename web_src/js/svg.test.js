139d7d29e2863626803ebc0c115786e45ef2d45e
silverwind <me@silverwind.io>
2021-11-14 03:37:11 +0100

    add test for different sizes

--- web_src/js/svg.test.js
+++ web_src/js/svg.test.js
@@ -6,4 +6,6 @@ test('svg', () => {
 
 test('svgNode', () => {
   expect(svgNode('octicon-repo')).toBeInstanceOf(Element);
+  expect(svgNode('octicon-repo', 16).getAttribute('width')).toEqual('16');
+  expect(svgNode('octicon-repo', 32).getAttribute('width')).toEqual('32');
 });
