f7a960b91941fe20e550ec3e8203bea482331031
wxiaoguang <wxiaoguang@gmail.com>
2021-11-16 09:45:48 +0800

    Update web_src/js/features/repo-legacy.js
    Co-authored-by: silverwind <me@silverwind.io>

--- web_src/js/features/repo-legacy.js
+++ web_src/js/features/repo-legacy.js
@@ -536,9 +536,7 @@ export function initRepository() {
 
 function initRepoIssueCommentEdit() {
   // Issue/PR Context Menus
-  $('.comment-header-right .context-dropdown').dropdown({
-    action: 'hide',
-  });
+  $('.comment-header-right .context-dropdown').dropdown({action: 'hide'});
 
   // Edit issue or comment content
   $(document).on('click', '.edit-content', onEditContent);
