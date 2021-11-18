386bb7821bc2f18a176f06c0853483a443742584
Andrew Thornton <art27@cantab.net>
2021-11-18 16:36:01 +0000

    Reset locale on login
    When logging in reset the user's locale to ensure that it matches their
    preferred locale.
    
    Fix #15612
    
    Signed-off-by: Andrew Thornton <art27@cantab.net>

--- modules/context/context.go
+++ modules/context/context.go
@@ -614,6 +614,9 @@ func Auth(authMethod auth.Method) func(*Context) {
 	return func(ctx *Context) {
 		ctx.User = authMethod.Verify(ctx.Req, ctx.Resp, ctx, ctx.Session)
 		if ctx.User != nil {
+			if ctx.Locale.Language() != ctx.User.Language {
+				ctx.Locale = middleware.Locale(ctx.Resp, ctx.Req)
+			}
 			ctx.IsBasicAuth = ctx.Data["AuthedMethod"].(string) == new(auth.Basic).Name()
 			ctx.IsSigned = true
 			ctx.Data["IsSigned"] = ctx.IsSigned
