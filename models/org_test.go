0313af8cc1f8b64623f1394006417c4eaeba4dae
Lunny Xiao <xiaolunwen@gmail.com>
2021-11-20 17:55:03 +0800

    revert unnecessary change

--- models/org_test.go
+++ models/org_test.go
@@ -324,7 +324,7 @@ func TestFindOrgs(t *testing.T) {
 		IncludePrivate: false,
 	})
 	assert.NoError(t, err)
-	assert.EqualValues(t, 0, len(orgs))
+	assert.Len(t, orgs, 0)
 
 	total, err := CountOrgs(FindOrgOptions{
 		UserID:         4,
