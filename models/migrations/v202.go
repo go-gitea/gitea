108ae1f309768ff8b09f95d0065065d275f91a80
Gusted <williamzijl7@hotmail.com>
2021-11-20 21:39:28 +0100

    Add migration

---
+++ models/migrations/v202.go
@@ -0,0 +1,23 @@
+// Copyright 2021 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package migrations
+
+import (
+	"fmt"
+
+	"xorm.io/xorm"
+)
+
+func addPrivateIssuesToRepo(x *xorm.Engine) error {
+	type Repository struct {
+		NumPrivateIssues       int
+		NumClosedPrivateIssues int
+	}
+
+	if err := x.Sync2(new(Repository)); err != nil {
+		return fmt.Errorf("Sync2: %v", err)
+	}
+	return nil
+}
