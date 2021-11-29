6a2e274adc68863f180c9299660140ffd3d37368
Andrew Thornton <art27@cantab.net>
2021-11-29 18:44:33 +0000

    ensure buttons work on loaded diffs
    Signed-off-by: Andrew Thornton <art27@cantab.net>

--- web_src/js/features/repo-unicode-escape.js
+++ web_src/js/features/repo-unicode-escape.js
@@ -1,17 +1,17 @@
 export function initEscapeButton() {
-  $('a.escape-button').on('click', (e) => {
+  $(document, 'a.escape-button').on('click', (e) => {
     e.preventDefault();
     $(e.target).parents('.file-content, .non-diff-file-content').find('.file-code, .file-view').addClass('unicode-escaped');
     $(e.target).hide();
     $(e.target).siblings('a.unescape-button').show();
   });
-  $('a.unescape-button').on('click', (e) => {
+  $(document, 'a.unescape-button').on('click', (e) => {
     e.preventDefault();
     $(e.target).parents('.file-content, .non-diff-file-content').find('.file-code, .file-view').removeClass('unicode-escaped');
     $(e.target).hide();
     $(e.target).siblings('a.escape-button').show();
   });
-  $('a.toggle-escape-button').on('click', (e) => {
+  $(document, 'a.toggle-escape-button').on('click', (e) => {
     e.preventDefault();
     const fileContent = $(e.target).parents('.file-content, .non-diff-file-content');
     const fileView = fileContent.find('.file-code, .file-view');
