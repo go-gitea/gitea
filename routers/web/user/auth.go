eff2bc7a5b7fbc76af2f3e38d2d3456f3786a865
Andrew Thornton <art27@cantab.net>
2021-12-19 14:26:36 +0000

    fix mismerged
    Signed-off-by: Andrew Thornton <art27@cantab.net>

--- routers/web/user/auth.go
+++ routers/web/user/auth.go
@@ -106,12 +106,12 @@ func AutoSignIn(ctx *context.Context) (bool, error) {
 	return true, nil
 }
 
-func resetLocale(ctx *context.Context, u *user_model.User) error {
+func resetLocale(ctx *context.Context, u *models.User) error {
 	// Language setting of the user overwrites the one previously set
 	// If the user does not have a locale set, we save the current one.
 	if len(u.Language) == 0 {
 		u.Language = ctx.Locale.Language()
-		if err := user_model.UpdateUserCols(db.DefaultContext, u, "language"); err != nil {
+		if err := models.UpdateUserCols(u, "language"); err != nil {
 			return err
 		}
 	}
