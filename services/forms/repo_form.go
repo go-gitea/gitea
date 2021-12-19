a100c9007497bd368136fc93c5bdfe5e3c5d89b5
Andrew Thornton <art27@cantab.net>
2021-12-19 20:09:44 +0000

    adjust swagger
    Signed-off-by: Andrew Thornton <art27@cantab.net>

--- services/forms/repo_form.go
+++ services/forms/repo_form.go
@@ -571,7 +571,7 @@ type MergePullRequestForm struct {
 	MergeTitleField        string
 	MergeMessageField      string
 	MergeCommitID          string // only used for manually-merged
-	HeadCommitID           string `json:head_commit_id,omitempty`
+	HeadCommitID           string `json:"head_commit_id",omitempty`
 	ForceMerge             *bool  `json:"force_merge,omitempty"`
 	DeleteBranchAfterMerge bool   `json:"delete_branch_after_merge,omitempty"`
 }
