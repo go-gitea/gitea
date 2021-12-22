d043ac9b36479b4d4c51b539afa27eb45960d28d
KN4CK3R <admin@oldschoolhack.me>
2021-12-22 07:28:08 +0000

    Fix status code.

--- routers/install/routes.go
+++ routers/install/routes.go
@@ -107,7 +107,7 @@ func Routes() *web.Route {
 	r.Get("/", Install)
 	r.Post("/", web.Bind(forms.InstallForm{}), SubmitInstall)
 	r.NotFound(func(w http.ResponseWriter, req *http.Request) {
-		http.Redirect(w, req, setting.AppURL, http.StatusPermanentRedirect)
+		http.Redirect(w, req, setting.AppURL, http.StatusTemporaryRedirect)
 	})
 	return r
 }
