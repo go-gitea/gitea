f6ebf806a9cb69a1244aa397345e03310c12443c
wxiaoguang <wxiaoguang@gmail.com>
2021-12-24 21:53:02 +0800

    Add more comments

--- routers/web/repo/view.go
+++ routers/web/repo/view.go
@@ -850,6 +850,11 @@ func renderCode(ctx *context.Context) {
 			return
 		}
 		// the repo is not really empty, so we should update the modal in database
+		// such problem may be caused by:
+		// 1) an error occurs during pushing/receiving.  2) the user replaces an empty git repo manually
+		// and even more: the IsEmpty flag is deeply broken and should be removed with the UI changed to manage to cope with empty repos.
+		// it's possible for a repository to be non-empty by that flag but still 500
+		// because there are no branches - only tags -or the default branch is non-extant as it has been 0-pushed.
 		ctx.Repo.Repository.IsEmpty = false
 		if err = repo_model.UpdateRepositoryCols(ctx.Repo.Repository, "is_empty"); err != nil {
 			ctx.ServerError("UpdateRepositoryCols", err)
