f9bbed028cd13ff039f6cde227f3da2920c44e5e
Gusted <williamzijl7@hotmail.com>
2022-01-02 02:39:23 +0000

    Use correct translation key for error messages due to max repo limits (#18135 & #18153) (#18152)
    - Backport #18135
    - Backport #18153

--- routers/web/repo/migrate.go
+++ routers/web/repo/migrate.go
@@ -77,7 +77,14 @@ func handleMigrateError(ctx *context.Context, owner *models.User, err error, nam
 	case migrations.IsTwoFactorAuthError(err):
 		ctx.RenderWithErr(ctx.Tr("form.2fa_auth_required"), tpl, form)
 	case models.IsErrReachLimitOfRepo(err):
-		ctx.RenderWithErr(ctx.Tr("repo.form.reach_limit_of_creation", owner.MaxCreationLimit()), tpl, form)
+		var msg string
+		maxCreationLimit := owner.MaxCreationLimit()
+		if maxCreationLimit == 1 {
+			msg = ctx.Tr("repo.form.reach_limit_of_creation_1", maxCreationLimit)
+		} else {
+			msg = ctx.Tr("repo.form.reach_limit_of_creation_n", maxCreationLimit)
+		}
+		ctx.RenderWithErr(msg, tpl, form)
 	case models.IsErrRepoAlreadyExist(err):
 		ctx.Data["Err_RepoName"] = true
 		ctx.RenderWithErr(ctx.Tr("form.repo_name_been_taken"), tpl, form)
