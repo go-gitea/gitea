07c057c3f1237c681c6438c2addb9a4b396ac8ad
wxiaoguang <wxiaoguang@gmail.com>
2022-01-12 20:55:25 +0800

    Fix documents

--- build/code-batch-process.go
+++ build/code-batch-process.go
@@ -21,9 +21,9 @@ import (
 )
 
 // Windows has a limitation for command line arguments, the size can not exceed 32KB.
-// So we have to feed the files to some tools (like gofmt/misspell`) batch by batch
+// So we have to feed the files to some tools (like gofmt/misspell) batch by batch
 
-// We also introduce a `gitea-fmt` command, it does better import formatting than gofmt/goimports
+// We also introduce a `gitea-fmt` command, it does better import formatting than gofmt/goimports. `gitea-fmt` calls `gofmt` internally.
 
 var optionLogVerbose bool
 
