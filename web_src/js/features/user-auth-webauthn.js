01b11d64f1fc5e9f8c17edfcbb2e81b334203d55
Andrew Thornton <art27@cantab.net>
2022-01-13 21:00:31 +0000

    use uint8-to-base64 package
    Signed-off-by: Andrew Thornton <art27@cantab.net>

--- web_src/js/features/user-auth-webauthn.js
+++ web_src/js/features/user-auth-webauthn.js
@@ -1,3 +1,5 @@
+import {encode} from 'uint8-to-base64';
+
 const {appSubUrl, csrfToken} = window.config;
 
 
@@ -69,52 +71,9 @@ function verifyAssertion(assertedCredential) {
   });
 }
 
-const lookup = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
-
-function encode (num) {
-  return lookup.charAt(num);
-}
-
-function tripletToBase64 (num) {
-  return encode(num >> 18 & 0x3F) + encode(num >> 12 & 0x3F) + encode(num >> 6 & 0x3F) + encode(num & 0x3F);
-}
-
-function uint8ToBase64 (uint8) {
-  const extraBytes = uint8.length % 3; // if we have 1 byte left, pad 2 bytes
-  let output = '';
-  let temp = 0;
-
-  // go through the array every three bytes, we'll deal with trailing stuff later
-  for (let i = 0, length = uint8.length - extraBytes; i < length; i += 3) {
-    temp = (uint8[i] << 16) + (uint8[i + 1] << 8) + (uint8[i + 2]);
-    output += tripletToBase64(temp);
-  }
-
-  // pad the end with zeros, but make sure to not forget the extra bytes
-  switch (extraBytes) {
-    case 1:
-      temp = uint8[uint8.length - 1];
-      output += encode(temp >> 2);
-      output += encode((temp << 4) & 0x3F);
-      output += '==';
-      break;
-    case 2:
-      temp = (uint8[uint8.length - 2] << 8) + (uint8[uint8.length - 1]);
-      output += encode(temp >> 10);
-      output += encode((temp >> 4) & 0x3F);
-      output += encode((temp << 2) & 0x3F);
-      output += '=';
-      break;
-    default:
-      break;
-  }
-
-  return output;
-}
-
 // Encode an ArrayBuffer into a base64 string.
 function bufferEncode(value) {
-  return uint8ToBase64(value)
+  return encode(value)
     .replace(/\+/g, '-')
     .replace(/\//g, '_')
     .replace(/=/g, '');
