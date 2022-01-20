1b6f8f634ae4f6ff43f23338db72580e18cfb476
Norwin <git@nroo.de>
2022-01-21 00:57:56 +0100

    apply shellcheck

--- contrib/upgrade.sh
+++ contrib/upgrade.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
+set -euo pipefail
 
 # This is an update script for gitea installed via the binary distribution
 # from dl.gitea.io on linux as systemd service. It performs a backup and updates
@@ -14,27 +14,27 @@ set -e
 #   giteahome=/opt/gitea giteaconf=$giteahome/app.ini upgrade.sh
 
 # apply variables from environment
-: ${giteabin:="/usr/local/bin/gitea"}
-: ${giteahome:="/var/lib/gitea"}
-: ${giteaconf:="/etc/gitea/app.ini"}
-: ${giteauser:="git"}
-: ${sudocmd:="sudo"}
-: ${arch:="linux-amd64"}
-: ${backupopts:=""} # see `gitea dump --help` for available options
+: "${giteabin:="/usr/local/bin/gitea"}"
+: "${giteahome:="/var/lib/gitea"}"
+: "${giteaconf:="/etc/gitea/app.ini"}"
+: "${giteauser:="git"}"
+: "${sudocmd:="sudo"}"
+: "${arch:="linux-amd64"}"
+: "${backupopts:=""}" # see `gitea dump --help` for available options
 
 function giteacmd {
-  "$sudocmd" --user "$giteauser" "$giteabin" --config "$giteaconf" --work-path "$giteahome" $@
+  "$sudocmd" --user "$giteauser" "$giteabin" --config "$giteaconf" --work-path "$giteahome" "$@"
 }
 
 function require {
-  for exe in $@; do
-    which $exe &>/dev/null || (echo "missing dependency '$exe'"; exit 1)
+  for exe in "$@"; do
+    which "$exe" &>/dev/null || (echo "missing dependency '$exe'"; exit 1)
   done
 }
-require curl xz sha256sum gpg
+require curl xz sha256sum gpg "$sudocmd"
 
 # select version to install
-if [[ -z "$1" ]]; then
+if [[ -z "${1:-}" ]]; then
   require jq
   giteaversion=$(curl --connect-timeout 10 -sL https://dl.gitea.io/gitea/version.json | jq -r .latest.version)
 else
@@ -46,10 +46,10 @@ current=$(giteacmd --version | cut --delimiter=' ' --fields=3)
 [[ "$current" == "$giteaversion" ]] && echo "$current is already installed, stopping." && exit 1
 echo "Make sure to read the changelog first: https://github.com/go-gitea/gitea/blob/main/CHANGELOG.md"
 echo "Are you ready to update Gitea from ${current} to ${giteaversion}? (y/N)"
-read confirm
+read -r confirm
 [[ "$confirm" == "y" ]] || [[ "$confirm" == "Y" ]] || exit 1
 
-pushd $(pwd)
+pushd "$(pwd)"
 cd "$giteahome" # needed for gitea dump later
 
 # download new binary
@@ -59,14 +59,14 @@ echo "Downloading $binurl..."
 curl --connect-timeout 10 --silent --show-error --fail --location -O "$binurl{,.sha256,.asc}"
 
 # validate checksum & gpg signature (exit script if error)
-sha256sum --check ${binname}.xz.sha256
+sha256sum --check "${binname}.xz.sha256"
 # TODO 2022-06-24: this gpg key will expire!
 gpg --keyserver keys.openpgp.org --recv 7C9E68152594688862D62AF62D9AE806EC1592E2
-gpg --verify ${binname}.xz.asc ${binname}.xz
-rm ${binname}.xz.{sha256,asc}
+gpg --verify "${binname}.xz.asc" "${binname}.xz"
+rm "${binname}.xz.{sha256,asc}"
 
 # unpack binary + make executable
-xz --decompress ${binname}.xz
+xz --decompress "${binname}.xz"
 chown "$giteauser" "$binname"
 chmod +x "$binname"
 
