3e441629295685832bc9e08a2785c86048408fe4
confusedsushi <confused.sushi@googlemail.com>
2022-02-01 20:08:33 +0100

    Fix review excerpt (#18502)
    Backport #18502
    
    Currently the "File Changed" tab of a PR is somehow broken. This is also true for the current release 1.16.0.
    
    When you are on the "File Changed" tab, and want to look at code excerpt before or after the code changes, the layout breaks. You can test this on try.gitea.io here: https://try.gitea.io/testnotexisting/magic_enum/pulls/2/files
    
    The problem occurs for the unified view and for the split view.
    
    Kind of the same problem was there for commenting a line of code, this was fixed in #18321 and #18403.
    
    For consistency, I changed the solution of #18321, I removed the ``colspan`` and instead added a ``<td>``. The goal was to have code similarly with the split view.
    
    Also the separator line in the split view was in the wrong column, this was fixed too.* more consistent unified review comment
    
    Fix #18516
    
    Co-authored-by: Andrew Thornton <art27@cantab.net>

--- web_src/js/features/repo-issue.js
+++ web_src/js/features/repo-issue.js
@@ -504,7 +504,9 @@ export function initRepoPullRequestReview() {
             <td class="lines-type-marker"></td>
             <td class="add-comment-right"></td>
           ` : `
-            <td colspan="3" class="lines-num"></td>
+            <td class="lines-num"></td>
+            <td class="lines-num"></td>
+            <td class="lines-escape"></td>
             <td class="add-comment-left add-comment-right" colspan="2"></td>
           `}
         </tr>`);
