bc196a35e1c74f26d941aefbfde1733c2a989a6b
zeripath <art27@cantab.net>
2022-02-03 16:20:37 +0000

    Collaborator trust model should trust collaborators (#18539) (#18557)
    Backport #18539
    
    There was an unintended regression in #17917 which leads to only
    repository admin commits being trusted. This PR restores the old logic.
    
    Fix #18501
    
    Signed-off-by: Andrew Thornton <art27@cantab.net>
    
    Co-authored-by: Lunny Xiao <xiaolunwen@gmail.com>

--- routers/web/repo/view.go
+++ routers/web/repo/view.go
@@ -800,7 +800,7 @@ func renderDirectoryFiles(ctx *context.Context, timeout time.Duration) git.Entri
 		verification := asymkey_model.ParseCommitWithSignature(latestCommit)
 
 		if err := asymkey_model.CalculateTrustStatus(verification, ctx.Repo.Repository.GetTrustModel(), func(user *user_model.User) (bool, error) {
-			return models.IsUserRepoAdmin(ctx.Repo.Repository, user)
+			return models.IsOwnerMemberCollaborator(ctx.Repo.Repository, user.ID)
 		}, nil); err != nil {
 			ctx.ServerError("CalculateTrustStatus", err)
 			return nil
