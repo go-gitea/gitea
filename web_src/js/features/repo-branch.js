1f6e6e9ecfcf80b6c91d3165d07558880fb4e50d
Andrew Thornton <art27@cantab.net>
2022-02-07 21:08:52 +0000

    use attr
    Signed-off-by: Andrew Thornton <art27@cantab.net>

--- web_src/js/features/repo-branch.js
+++ web_src/js/features/repo-branch.js
@@ -2,17 +2,17 @@ import $ from 'jquery';
 
 export function initRepoBranchButton() {
   $('.show-create-branch-modal').on('click', function () {
-    let modalFormName = $(this).data('modal-form');
+    let modalFormName = $(this).attr('data-modal-form');
     if (!modalFormName) {
       modalFormName = '#create-branch-form';
     }
-    $(modalFormName)[0].action = $(modalFormName).data('base-action') + $(this).data('branch-from-urlcomponent');
-    let fromSpanName = $(this).data('modal-from-span');
+    $(modalFormName)[0].action = $(modalFormName).attr('data-base-action') + $(this).attr('data-branch-from-urlcomponent');
+    let fromSpanName = $(this).attr('data-modal-from-span');
     if (!fromSpanName) {
       fromSpanName = '#modal-create-branch-from-span';
     }
 
-    $(fromSpanName).text($(this).data('branch-from'));
-    $($(this).data('modal')).modal('show');
+    $(fromSpanName).text($(this).attr('data-branch-from'));
+    $($(this).attr('data-modal')).modal('show');
   });
 }
