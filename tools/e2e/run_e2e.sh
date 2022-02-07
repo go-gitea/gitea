0a4132554ff7ea612f69885d00676e34c8805af2
Kyle D <kdumontnu@gmail.com>
2022-02-07 10:39:28 -0700

    Init postgres database

--- tools/e2e/run_e2e.sh
+++ tools/e2e/run_e2e.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 [[ -z "${GITEA_CONF}" ]] && GiteaConfig='' || GiteaConfig="-C . -c ${GITEA_CONF}"
 
 ./${GITEA_EXECUTABLE:-gitea} ${GiteaConfig} --quiet web &
+sleep 30
 npx playwright test ${E2E_TESTS:-""}
 
 trap 'kill $(jobs -p)' EXIT
