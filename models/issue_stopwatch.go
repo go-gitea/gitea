db7c4a793078ae8f94f7ac87d52ea1d0a9a9e1bf
René Schaar <rene@schaar.priv.at>
2022-02-13 18:25:42 +0100

    Fix display time of milestones

--- models/issue_stopwatch.go
+++ models/issue_stopwatch.go
@@ -268,22 +268,32 @@ func cancelStopwatch(ctx context.Context, user *user_model.User, issue *Issue) e
 func SecToTime(duration int64) string {
 	seconds := duration % 60
 	minutes := (duration / (60)) % 60
-	hours := duration / (60 * 60)
+	hours := duration / (60 * 60) % 24
+	days := duration / (60 * 60) / 24
 
 	var hrs string
 
+	if days > 1 {
+		hrs = fmt.Sprintf("%d days", days)
+	} else {
+		hrs = fmt.Sprintf("%d day", days)
+	}
 	if hours > 0 {
-		hrs = fmt.Sprintf("%dh", hours)
+		if days == 0 {
+			hrs = fmt.Sprintf("%dh", hours)
+		} else {
+			hrs = fmt.Sprintf("%s %dh", hrs, hours)
+		}
 	}
 	if minutes > 0 {
-		if hours == 0 {
+		if days == 0 && hours == 0 {
 			hrs = fmt.Sprintf("%dmin", minutes)
 		} else {
 			hrs = fmt.Sprintf("%s %dmin", hrs, minutes)
 		}
 	}
 	if seconds > 0 {
-		if hours == 0 && minutes == 0 {
+		if days == 0 && hours == 0 && minutes == 0 {
 			hrs = fmt.Sprintf("%ds", seconds)
 		} else {
 			hrs = fmt.Sprintf("%s %ds", hrs, seconds)
