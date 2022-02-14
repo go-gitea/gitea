c6eca22ad3f8c3803ab81a9169a53d6aa2f653d1
KN4CK3R <admin@oldschoolhack.me>
2022-02-14 22:00:20 +0000

    Use longer TEXT column type.

--- models/migrations/v210.go
+++ models/migrations/v210.go
@@ -46,7 +46,7 @@ func addPackageTables(x *xorm.Engine) error {
 		RefType int64  `xorm:"INDEX NOT NULL"`
 		RefID   int64  `xorm:"INDEX NOT NULL"`
 		Name    string `xorm:"INDEX NOT NULL"`
-		Value   string `xorm:"INDEX NOT NULL"`
+		Value   string `xorm:"TEXT INDEX NOT NULL"`
 	}
 
 	if err := x.Sync2(new(PackageProperty)); err != nil {
