609c91665e5e4d6da50af0b2168d6cb46f9d6273
René Schaar <rene@schaar.priv.at>
2022-02-15 17:50:10 +0100

    Fix display time of milestones (#18753)
    * Fix display time of milestones
    
    * Move the SecToTime function
    
    From the models/issue_stopwatch.go file to the modules/util package
    
    * Rename the sec_to_time file
    
    * Updated formatting
    
    * Include copyright notice in sec_to_time.go
    
    * Apply PR review suggestions
    
    - Update copyright notice dates to 2022
    - Change `1 day 3h 5min 7s` to `1d 3h 5m 7s`
    
    * Rename hrs var and combine conditions
    
    * Update unit tests to match new time pattern
    
    Changed `1min` to `1m`
    
    Co-authored-by: Lunny Xiao <xiaolunwen@gmail.com>

--- modules/templates/helper.go
+++ modules/templates/helper.go
@@ -256,7 +256,7 @@ func NewFuncMap() []template.FuncMap {
 		},
 		"Printf":   fmt.Sprintf,
 		"Escape":   Escape,
-		"Sec2Time": models.SecToTime,
+		"Sec2Time": util.SecToTime,
 		"ParseDeadline": func(deadline string) []string {
 			return strings.Split(deadline, "|")
 		},
@@ -447,7 +447,7 @@ func NewTextFuncMap() []texttmpl.FuncMap {
 		},
 		"Printf":   fmt.Sprintf,
 		"Escape":   Escape,
-		"Sec2Time": models.SecToTime,
+		"Sec2Time": util.SecToTime,
 		"ParseDeadline": func(deadline string) []string {
 			return strings.Split(deadline, "|")
 		},
