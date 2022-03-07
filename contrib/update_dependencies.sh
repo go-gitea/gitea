d1a803ecdafa4c1b4f54317c2d9be5025bdecf65
techknowlogick <matti@mdranta.net>
2022-03-07 15:04:35 -0500

    Update update_dependencies.sh

--- contrib/update_dependencies.sh
+++ contrib/update_dependencies.sh
@@ -2,6 +2,7 @@
 
 grep 'git' go.mod | grep '\.com' | grep -v indirect | grep -v replace | cut -f 2 | cut -d ' ' -f 1 | while read line; do
   go get -u "$line"
+  make vendor
   git add .
   git commit -m "update $line"
 done
