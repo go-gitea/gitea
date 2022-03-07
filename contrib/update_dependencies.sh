69dfe7555eebdd775073acff472fa52260c40483
techknowlogick <techknowlogick@gitea.io>
2022-03-07 14:30:52 -0500

    update gitea.com/go-chi/binding

--- contrib/update_dependencies.sh
+++ contrib/update_dependencies.sh
@@ -2,7 +2,6 @@
 
 grep 'git' go.mod | grep '\.com' | grep -v indirect | grep -v replace | cut -f 2 | cut -d ' ' -f 1 | while read line; do
   go get -u "$line"
-  make vendor
   git add .
-  git commit -S -m "update $line"
+  git commit -m "update $line"
 done
