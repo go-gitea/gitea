a9cd1878039376bc931e8a54aa62e363d20336bf
zhengge2012 <zhengge2012@gmail.com>
2022-03-22 21:48:53 +0800

    add model migration file

---
+++ models/migrations/v222.go
@@ -0,0 +1,21 @@
+// Copyright 2022 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package migrations
+
+import (
+	"fmt"
+	"xorm.io/xorm"
+)
+
+func addColorColToMirror(x *xorm.Engine)  error{
+	type Mirror struct {
+		EnableSafeMirror bool `xorm:"NOT NULL DEFAULT false"`
+	}
+
+	if err := x.Sync2(new(Mirror)); err != nil {
+		return fmt.Errorf("Sync2: %v", err)
+	}
+	return nil
+}
