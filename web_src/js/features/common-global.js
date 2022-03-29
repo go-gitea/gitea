223900ef35578d56b392a9a013f864debd686e14
wxiaoguang <wxiaoguang@gmail.com>
2022-03-29 17:04:18 +0800

    add comments

--- web_src/js/features/common-global.js
+++ web_src/js/features/common-global.js
@@ -354,14 +354,21 @@ function showErrorMessageHtml(msgHtml) {
   $($pageContent[0]).prepend($tip);
 }
 
+/**
+ * @param {ErrorEvent} e
+ */
 function processWindowErrorEvent(e) {
   showErrorMessageHtml(`JavaScript error: ${e.message} (${e.filename} @ ${e.lineno}:${e.colno}). Open browser console to see more details.`);
 }
 
 export function initGlobalErrorHandler() {
+  // we added an event handler for window error at the very beginning of head.tmpl
+  // the handler calls `_globalHandlerErrors.push` (array method) to record all errors occur before this init
+  // then in this init, we can collect all error events and show them
   for (const e of window._globalHandlerErrors || []) {
     processWindowErrorEvent(e);
   }
+  // then, change _globalHandlerErrors to an object with push method, to process further error events directly
   window._globalHandlerErrors = {'push': (e) => processWindowErrorEvent(e)};
 }
 
