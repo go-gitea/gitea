889a8c268ca6a54ff5be19e61b29b10feb4a12e8
zeripath <art27@cantab.net>
2022-03-29 18:12:33 +0100

    Use full output of git show-ref --tags to get tags for PushUpdateAddTag (#19235)
    Strangely #19038 appears to relate to an issue whereby a tag appears to
    be listed in `git show-ref --tags` but then does not appear when `git
    show-ref --tags -- short_name` is called.
    
    As a solution though I propose to stop the second call as it is
    unnecessary and only likely to cause problems.
    
    I've also noticed that the tags calls are wildly inefficient and aren't using the common cat-files - so these have been added.
    
    I've also noticed that the git commit-graph is not being written on mirroring - so I've also added writing this to the migration which should improve mirror rendering somewhat. 
    
    Fix #19038
    
    Signed-off-by: Andrew Thornton <art27@cantab.net>
    Co-authored-by: 6543 <6543@obermui.de>

--- services/mirror/mirror_pull.go
+++ services/mirror/mirror_pull.go
@@ -204,6 +204,7 @@ func runSync(ctx context.Context, m *repo_model.Mirror) ([]*mirrorSyncResult, bo
 	timeout := time.Duration(setting.Git.Timeout.Mirror) * time.Second
 
 	log.Trace("SyncMirrors [repo: %-v]: running git remote update...", m.Repo)
+
 	gitArgs := []string{"remote", "update"}
 	if m.EnablePrune {
 		gitArgs = append(gitArgs, "--prune")
@@ -275,6 +276,10 @@ func runSync(ctx context.Context, m *repo_model.Mirror) ([]*mirrorSyncResult, bo
 		}
 	}
 	output := stderrBuilder.String()
+
+	if err := git.WriteCommitGraph(ctx, repoPath); err != nil {
+		log.Error("SyncMirrors [repo: %-v]: %v", m.Repo, err)
+	}
 
 	gitRepo, err := git.OpenRepositoryCtx(ctx, repoPath)
 	if err != nil {
@@ -367,6 +372,10 @@ func runSync(ctx context.Context, m *repo_model.Mirror) ([]*mirrorSyncResult, bo
 					log.Error("CreateRepositoryNotice: %v", err)
 				}
 				return nil, false
+			}
+
+			if err := git.WriteCommitGraph(ctx, wikiPath); err != nil {
+				log.Error("SyncMirrors [repo: %-v]: %v", m.Repo, err)
 			}
 		}
 		log.Trace("SyncMirrors [repo: %-v Wiki]: git remote update complete", m.Repo)
