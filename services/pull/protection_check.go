120d84dab969742e8101484ec6267f3698a495b2
6543 <6543@obermui.de>
2022-03-31 16:07:33 +0200

    use package "errors"

--- services/pull/protection_check.go
+++ services/pull/protection_check.go
@@ -6,94 +6,38 @@ package pull
 
 import (
 	"context"
+	"errors"
 
 	"code.gitea.io/gitea/models"
 	user_model "code.gitea.io/gitea/models/user"
 	asymkey_service "code.gitea.io/gitea/services/asymkey"
 )
 
-type ErrIsClosed struct{}
-
-func IsErrIsClosed(err error) bool {
-	_, ok := err.(ErrIsClosed)
-	return ok
-}
-
-func (err ErrIsClosed) Error() string {
-	return "pull is cosed"
-}
-
-type ErrUserNotAllowedToMerge struct{}
-
-func IsErrUserNotAllowedToMerge(err error) bool {
-	_, ok := err.(ErrUserNotAllowedToMerge)
-	return ok
-}
-
-func (err ErrUserNotAllowedToMerge) Error() string {
-	return "user not allowed to merge"
-}
-
-type ErrHasMerged struct{}
-
-func IsErrHasMerged(err error) bool {
-	_, ok := err.(ErrHasMerged)
-	return ok
-}
-
-func (err ErrHasMerged) Error() string {
-	return "has already been merged"
-}
-
-type ErrIsWorkInProgress struct{}
-
-func IsErrIsWorkInProgress(err error) bool {
-	_, ok := err.(ErrIsWorkInProgress)
-	return ok
-}
-
-func (err ErrIsWorkInProgress) Error() string {
-	return "work in progress PRs cannot be merged"
-}
-
-type ErrNotMergableState struct{}
-
-func IsErrNotMergableState(err error) bool {
-	_, ok := err.(ErrNotMergableState)
-	return ok
-}
-
-func (err ErrNotMergableState) Error() string {
-	return "not in mergeable state"
-}
-
-type ErrDependenciesLeft struct{}
-
-func IsErrDependenciesLeft(err error) bool {
-	_, ok := err.(ErrDependenciesLeft)
-	return ok
-}
-
-func (err ErrDependenciesLeft) Error() string {
-	return "is blocked by an open dependency"
-}
+var (
+	ErrIsClosed              = errors.New("pull is cosed")
+	ErrUserNotAllowedToMerge = errors.New("user not allowed to merge")
+	ErrHasMerged             = errors.New("has already been merged")
+	ErrIsWorkInProgress      = errors.New("work in progress PRs cannot be merged")
+	ErrNotMergableState      = errors.New("not in mergeable state")
+	ErrDependenciesLeft      = errors.New("is blocked by an open dependency")
+)
 
 // CheckPullMergable check if the pull mergable based on all conditions (branch protection, merge options, ...)
 func CheckPullMergable(ctx context.Context, doer *user_model.User, perm *models.Permission, pr *models.PullRequest, manuallMerge, force bool) error {
 	if pr.HasMerged {
-		return ErrHasMerged{}
+		return ErrHasMerged
 	}
 
 	if err := pr.LoadIssue(); err != nil {
 		return err
 	} else if pr.Issue.IsClosed {
-		return ErrIsClosed{}
+		return ErrIsClosed
 	}
 
 	if allowedMerge, err := IsUserAllowedToMerge(pr, *perm, doer); err != nil {
 		return err
 	} else if !allowedMerge {
-		return ErrUserNotAllowedToMerge{}
+		return ErrUserNotAllowedToMerge
 	}
 
 	if manuallMerge {
@@ -102,11 +46,11 @@ func CheckPullMergable(ctx context.Context, doer *user_model.User, perm *models.
 	}
 
 	if pr.IsWorkInProgress() {
-		return ErrIsWorkInProgress{}
+		return ErrIsWorkInProgress
 	}
 
 	if !pr.CanAutoMerge() {
-		return ErrNotMergableState{}
+		return ErrNotMergableState
 	}
 
 	if err := CheckPRReadyToMerge(ctx, pr, false); err != nil {
@@ -115,7 +59,7 @@ func CheckPullMergable(ctx context.Context, doer *user_model.User, perm *models.
 				if isRepoAdmin, err := models.IsUserRepoAdmin(pr.BaseRepo, doer); err != nil {
 					return err
 				} else if !isRepoAdmin {
-					return ErrUserNotAllowedToMerge{}
+					return ErrUserNotAllowedToMerge
 				}
 			}
 		} else {
@@ -130,7 +74,7 @@ func CheckPullMergable(ctx context.Context, doer *user_model.User, perm *models.
 	if noDeps, err := models.IssueNoDependenciesLeft(pr.Issue); err != nil {
 		return err
 	} else if !noDeps {
-		return ErrDependenciesLeft{}
+		return ErrDependenciesLeft
 	}
 
 	return nil
