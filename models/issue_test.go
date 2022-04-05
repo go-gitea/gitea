60b58e8a14b1e18894954791363e1c54bb9c0e54
Lunny Xiao <xiaolunwen@gmail.com>
2022-04-05 10:14:53 +0800

    Fix lint

--- models/issue_test.go
+++ models/issue_test.go
@@ -15,10 +15,10 @@ import (
 
 	"code.gitea.io/gitea/models/db"
 	"code.gitea.io/gitea/models/foreignreference"
+	issues_model "code.gitea.io/gitea/models/issues"
 	repo_model "code.gitea.io/gitea/models/repo"
 	"code.gitea.io/gitea/models/unittest"
 	user_model "code.gitea.io/gitea/models/user"
-	issues_model "code.gitea.io/gitea/models/issues"
 
 	"github.com/stretchr/testify/assert"
 )
