4a87d16e88201519a221f4d708d0aa30d602dd52
qwerty287 <ndev@web.de>
2022-04-28 13:36:51 +0200

    Add API to check if team has repo access

--- routers/api/v1/api.go
+++ routers/api/v1/api.go
@@ -1109,7 +1109,8 @@ func Routes() *web.Route {
 				m.Get("", org.GetTeamRepos)
 				m.Combo("/{org}/{reponame}").
 					Put(org.AddTeamRepository).
-					Delete(org.RemoveTeamRepository)
+					Delete(org.RemoveTeamRepository).
+					Get(org.GetTeamRepo)
 			})
 		}, orgAssignment(false, true), reqToken(), reqTeamMembership())
 
