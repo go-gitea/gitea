c1c26f86a6605ca661ca0f07d7085a393eb213fe
6543 <6543@obermui.de>
2022-04-30 01:54:42 +0200

    complete

--- routers/api/v1/misc/nodeinfo.go
+++ routers/api/v1/misc/nodeinfo.go
@@ -8,11 +8,14 @@ import (
 	"net/http"
 	"time"
 
+	"code.gitea.io/gitea/models"
 	user_model "code.gitea.io/gitea/models/user"
 	"code.gitea.io/gitea/modules/context"
 	"code.gitea.io/gitea/modules/setting"
 	"code.gitea.io/gitea/modules/structs"
 )
+
+const cacheKeyNodeInfoUsage = "API_NodeInfoUsage"
 
 // NodeInfo returns the NodeInfo for the Gitea instance to allow for federation
 func NodeInfo(ctx *context.APIContext) {
@@ -25,14 +28,35 @@ func NodeInfo(ctx *context.APIContext) {
 	//   "200":
 	//     "$ref": "#/responses/NodeInfo"
 
-	infoUsageUsers := structs.NodeInfoUsageUsers{}
+	nodeInfoUsage := structs.NodeInfoUsage{}
 	if setting.Federation.ShareUserStatistics {
-		infoUsageUsers.Total = int(user_model.CountUsers(nil))
-		now := time.Now()
-		timeOneMonthAgo := now.AddDate(0, -1, 0).Unix()
-		timeHaveYearAgo := now.AddDate(0, -6, 0).Unix()
-		infoUsageUsers.ActiveMonth = int(user_model.CountUsers(&user_model.CountUserFilter{LastLoginSince: &timeOneMonthAgo}))
-		infoUsageUsers.ActiveHalfyear = int(user_model.CountUsers(&user_model.CountUserFilter{LastLoginSince: &timeHaveYearAgo}))
+		info, ok := ctx.Cache.Get(cacheKeyNodeInfoUsage).(structs.NodeInfoUsage)
+		if !ok {
+			usersTotal := int(user_model.CountUsers(nil))
+			now := time.Now()
+			timeOneMonthAgo := now.AddDate(0, -1, 0).Unix()
+			timeHaveYearAgo := now.AddDate(0, -6, 0).Unix()
+			usersActiveMonth := int(user_model.CountUsers(&user_model.CountUserFilter{LastLoginSince: &timeOneMonthAgo}))
+			usersActiveHalfyear := int(user_model.CountUsers(&user_model.CountUserFilter{LastLoginSince: &timeHaveYearAgo}))
+
+			allIssues, _ := models.CountIssues(&models.IssuesOptions{})
+			allComments, _ := models.CountComments(&models.FindCommentsOptions{})
+
+			info = structs.NodeInfoUsage{
+				Users: structs.NodeInfoUsageUsers{
+					Total:          usersTotal,
+					ActiveMonth:    usersActiveMonth,
+					ActiveHalfyear: usersActiveHalfyear,
+				},
+				LocalPosts:    int(allIssues),
+				LocalComments: int(allComments),
+			}
+			if err := ctx.Cache.Put(cacheKeyNodeInfoUsage, nodeInfoUsage, 180); err != nil {
+				ctx.InternalServerError(err)
+				return
+			}
+		}
+		nodeInfoUsage = info
 	}
 
 	nodeInfo := &structs.NodeInfo{
@@ -46,12 +70,10 @@ func NodeInfo(ctx *context.APIContext) {
 		Protocols: []string{"activitypub"},
 		Services: structs.NodeInfoServices{
 			Inbound:  []string{},
-			Outbound: []string{},
+			Outbound: []string{"rss2.0"},
 		},
 		OpenRegistrations: setting.Service.ShowRegistrationButton,
-		Usage: structs.NodeInfoUsage{
-			Users: infoUsageUsers,
-		},
+		Usage:             nodeInfoUsage,
 	}
 	ctx.JSON(http.StatusOK, nodeInfo)
 }
