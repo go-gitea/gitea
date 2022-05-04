3911dcf37382c88271516dca28c760dc6e4d5470
6543 <6543@obermui.de>
2022-05-04 17:35:38 +0200

    Update services/repository/files/commit.go
    Co-authored-by: wxiaoguang <wxiaoguang@gmail.com>

--- services/repository/files/commit.go
+++ services/repository/files/commit.go
@@ -47,7 +47,7 @@ func CreateCommitStatus(ctx context.Context, repo *repo_model.Repository, creato
 
 	if status.State.IsSuccess() {
 		if err := automerge.MergeScheduledPullRequest(ctx, sha, repo); err != nil {
-			return fmt.Errorf("MergeScheduledPullRequest[repo_id: %d, user_id: %d, sha: %s]: %v", repo.ID, creator.ID, sha, err)
+			return fmt.Errorf("MergeScheduledPullRequest[repo_id: %d, user_id: %d, sha: %s]: %w", repo.ID, creator.ID, sha, err)
 		}
 	}
 
