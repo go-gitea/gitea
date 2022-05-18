1698b92bcbef2365b018df4017ae5a8e4ec664ff
Eekle <96976531+Eekle@users.noreply.github.com>
2022-05-18 10:43:38 +0100

    Makes comments in body text/title return the base page URL instead of "" in RefCommentHTMLURL()

--- models/issue_xref.go
+++ models/issue_xref.go
@@ -296,7 +296,7 @@ func CommentTypeIsRef(t CommentType) bool {
 // RefCommentHTMLURL returns the HTML URL for the comment that created this reference
 func (comment *Comment) RefCommentHTMLURL() string {
 	if comment.RefCommentID == 0 {
-		return ""
+		return comment.RefIssueHTMLURL()
 	}
 	if err := comment.LoadRefComment(); err != nil { // Silently dropping errors :unamused:
 		log.Error("LoadRefComment(%d): %v", comment.RefCommentID, err)
