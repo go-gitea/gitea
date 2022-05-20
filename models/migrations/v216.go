9d186bde3b8704358b24230730034014b19a7798
Andrew Thornton <art27@cantab.net>
2022-05-20 21:02:16 +0100

    Drop indexes in migration
    Signed-off-by: Andrew Thornton <art27@cantab.net>

--- models/migrations/v216.go
+++ models/migrations/v216.go
@@ -10,18 +10,41 @@ import (
 )
 
 func improveActionTableIndices(x *xorm.Engine) error {
-	type Action struct {
-		ID          int64 `xorm:"pk autoincr"`
-		UserID      int64 `xorm:"INDEX(u_ua_and_r)"` // Receiver user id.
-		OpType      int
-		ActUserID   int64 `xorm:"INDEX(u_ua_and_r) INDEX(ua_and_r)"` // Action user id.
-		RepoID      int64 `xorm:"INDEX(u_ua_and_r) INDEX(ua_and_r) INDEX(r)"`
-		CommentID   int64 `xorm:"INDEX"`
-		IsDeleted   bool  `xorm:"NOT NULL DEFAULT false"`
-		RefName     string
-		IsPrivate   bool               `xorm:"NOT NULL DEFAULT false"`
-		Content     string             `xorm:"TEXT"`
-		CreatedUnix timeutil.TimeStamp `xorm:"INDEX(u_ua_and_r) INDEX(ua_and_r) INDEX(r) created"`
+	{
+		type Action struct {
+			ID          int64 `xorm:"pk autoincr"`
+			UserID      int64 `xorm:"INDEX"` // Receiver user id.
+			OpType      int
+			ActUserID   int64 `xorm:"INDEX"` // Action user id.
+			RepoID      int64 `xorm:"INDEX"`
+			CommentID   int64 `xorm:"INDEX"`
+			IsDeleted   bool  `xorm:"INDEX NOT NULL DEFAULT false"`
+			RefName     string
+			IsPrivate   bool               `xorm:"INDEX NOT NULL DEFAULT false"`
+			Content     string             `xorm:"TEXT"`
+			CreatedUnix timeutil.TimeStamp `xorm:"INDEX created"`
+		}
+		if err := x.Sync2(&Action{}); err != nil {
+			return err
+		}
+		if err := x.DropIndexes(&Action{}); err != nil {
+			return err
+		}
 	}
-	return x.Sync2(&Action{})
+	{
+		type Action struct {
+			ID          int64 `xorm:"pk autoincr"`
+			UserID      int64 `xorm:"INDEX(u_ua_and_r)"` // Receiver user id.
+			OpType      int
+			ActUserID   int64 `xorm:"INDEX(u_ua_and_r) INDEX(ua_and_r)"` // Action user id.
+			RepoID      int64 `xorm:"INDEX(u_ua_and_r) INDEX(ua_and_r) INDEX(r)"`
+			CommentID   int64 `xorm:"INDEX"`
+			IsDeleted   bool  `xorm:"NOT NULL DEFAULT false"`
+			RefName     string
+			IsPrivate   bool               `xorm:"NOT NULL DEFAULT false"`
+			Content     string             `xorm:"TEXT"`
+			CreatedUnix timeutil.TimeStamp `xorm:"INDEX(u_ua_and_r) INDEX(ua_and_r) INDEX(r) created"`
+		}
+		return x.Sync2(&Action{})
+	}
 }
