2d9d4423a98ec25cff8cd8e7d8a688bea4e4b7dc
wxiaoguang <wxiaoguang@gmail.com>
2022-05-21 22:51:52 +0800

    Enable spellcheck for EasyMDE

--- web_src/js/features/comp/EasyMDE.js
+++ web_src/js/features/comp/EasyMDE.js
@@ -38,6 +38,8 @@ export async function createCommentEasyMDE(textarea, easyMDEOptions = {}) {
     indentWithTabs: false,
     tabSize: 4,
     spellChecker: false,
+    inputStyle: 'contenteditable', // nativeSpellcheck requires contenteditable
+    nativeSpellcheck: true,
     toolbar: ['bold', 'italic', 'strikethrough', '|',
       'heading-1', 'heading-2', 'heading-3', 'heading-bigger', 'heading-smaller', '|',
       'code', 'quote', '|', {
