97d5110d3f87595fabc114c81142fef1b6d24670
qwerty287 <ndev@web.de>
2022-05-22 12:36:01 +0200

    Use different repo name

--- integrations/api_repo_file_get_test.go
+++ integrations/api_repo_file_get_test.go
@@ -25,7 +25,7 @@ func TestAPIGetRawFileOrLFS(t *testing.T) {
 
 	// Test with LFS
 	onGiteaRun(t, func(t *testing.T, u *url.URL) {
-		httpContext := NewAPITestContext(t, "user2", "repo1")
+		httpContext := NewAPITestContext(t, "user2", "repo-lfs-test")
 		doAPICreateRepository(httpContext, false, func(t *testing.T, repository api.Repository) {
 			u.Path = httpContext.GitPath()
 			dstPath, err := os.MkdirTemp("", httpContext.Reponame)
