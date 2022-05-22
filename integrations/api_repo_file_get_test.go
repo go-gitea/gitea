95b746611c23d16055696df652855b03ca3c0883
qwerty287 <ndev@web.de>
2022-05-22 12:35:00 +0200

    Use own repo for test

--- integrations/api_repo_file_get_test.go
+++ integrations/api_repo_file_get_test.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"testing"
 
+	api "code.gitea.io/gitea/modules/structs"
 	"code.gitea.io/gitea/modules/util"
 	"github.com/stretchr/testify/assert"
 )
@@ -23,29 +24,32 @@ func TestAPIGetRawFileOrLFS(t *testing.T) {
 	assert.Equal(t, "# repo1\n\nDescription for repo1", resp.Body.String())
 
 	// Test with LFS
-	httpContext := NewAPITestContext(t, "user2", "repo1")
-
 	onGiteaRun(t, func(t *testing.T, u *url.URL) {
-		u.Path = httpContext.GitPath()
-		dstPath, err := os.MkdirTemp("", httpContext.Reponame)
-		assert.NoError(t, err)
-		defer util.RemoveAll(dstPath)
+		httpContext := NewAPITestContext(t, "user2", "repo1")
+		doAPICreateRepository(httpContext, false, func(t *testing.T, repository api.Repository) {
+			u.Path = httpContext.GitPath()
+			dstPath, err := os.MkdirTemp("", httpContext.Reponame)
+			assert.NoError(t, err)
+			defer util.RemoveAll(dstPath)
 
-		u.Path = httpContext.GitPath()
-		u.User = url.UserPassword("user2", userPassword)
+			u.Path = httpContext.GitPath()
+			u.User = url.UserPassword("user2", userPassword)
 
-		t.Run("Clone", doGitClone(dstPath, u))
+			t.Run("Clone", doGitClone(dstPath, u))
 
-		dstPath2, err := os.MkdirTemp("", httpContext.Reponame)
-		assert.NoError(t, err)
-		defer util.RemoveAll(dstPath2)
+			dstPath2, err := os.MkdirTemp("", httpContext.Reponame)
+			assert.NoError(t, err)
+			defer util.RemoveAll(dstPath2)
 
-		t.Run("Partial Clone", doPartialGitClone(dstPath2, u))
+			t.Run("Partial Clone", doPartialGitClone(dstPath2, u))
 
-		lfs, _ := lfsCommitAndPushTest(t, dstPath)
+			lfs, _ := lfsCommitAndPushTest(t, dstPath)
 
-		reqLFS := NewRequest(t, "GET", "/api/v1/repos/user2/repo1/media/"+lfs)
-		respLFS := MakeRequestNilResponseRecorder(t, reqLFS, http.StatusOK)
-		assert.Equal(t, littleSize, respLFS.Length)
+			reqLFS := NewRequest(t, "GET", "/api/v1/repos/user2/repo1/media/"+lfs)
+			respLFS := MakeRequestNilResponseRecorder(t, reqLFS, http.StatusOK)
+			assert.Equal(t, littleSize, respLFS.Length)
+
+			doAPIDeleteRepository(httpContext)
+		})
 	})
 }
