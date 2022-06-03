44982950437bf054f86293ff4859255abf35e5e3
zeripath <art27@cantab.net>
2022-06-03 14:28:07 +0100

    Update modules/process/manager_unix.go
    Co-authored-by: wxiaoguang <wxiaoguang@gmail.com>

--- modules/process/manager_unix.go
+++ modules/process/manager_unix.go
@@ -13,5 +13,6 @@ import (
 
 // SetSysProcAttribute sets the common SysProcAttrs for commands
 func SetSysProcAttribute(cmd *exec.Cmd) {
+	// When Gitea runs SubProcessA -> SubProcessB and SubProcessA gets killed by context timeout, use setpgid to make sure the sub processes can be reaped instead of leaving defunct(zombie) processes.
 	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
 }
