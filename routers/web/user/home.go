go-gitea/gitea

commit 02b45051503d4330da9757ff084c9cc5e6e60d84

Disable unnecessary mirroring elements
This mod fixes disabling unnecessary mirroring elements.

Related: https://github.com/go-gitea/gitea/pull/16957
Related: https://github.com/go-gitea/gitea/pull/13084
Author-Change-Id: IB#1105104

routers/web/user/home.go
@@ -77,6 +77,7 @@ func Dashboard(ctx *context.Context) {
 	ctx.Data["PageIsNews"] = true
 	cnt, _ := models.GetOrganizationCount(db.DefaultContext, ctxUser)
 	ctx.Data["UserOrgsCount"] = cnt
+	ctx.Data["DisableNewPullMirrors"] = setting.Mirror.DisableNewPull
 
 	var uid int64
 	if ctxUser != nil {

templates/user/dashboard/repolist.tmpl
@@ -109,10 +109,12 @@
 						{{.i18n.Tr "forks"}}
 						<div v-show="reposFilter === 'forks'" class="ui circular mini grey label">${repoTypeCount}</div>
 					</a>
+					{{if not .DisableNewPullMirrors}}
 					<a class="item" :class="{active: reposFilter === 'mirrors'}" @click="changeReposFilter('mirrors')">
 						{{.i18n.Tr "mirrors"}}
 						<div v-show="reposFilter === 'mirrors'" class="ui circular mini grey label">${repoTypeCount}</div>
 					</a>
+					{{end}}
 					<a class="item" :class="{active: reposFilter === 'collaborative'}" @click="changeReposFilter('collaborative')">
 						{{.i18n.Tr "collaborative"}}
 						<div v-show="reposFilter === 'collaborative'" class="ui circular mini grey label">${repoTypeCount}</div>

custom/conf/app.example.ini
@@ -1996,7 +1996,7 @@ PATH =
 ;[mirror]
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Enables the mirror functionality. Set to **false** to disable all mirrors.
+;; Enables the mirror functionality. Set to **false** to disable all mirrors (pre-existing mirrors remain valid but won't be updated; may be converted to regular repo).
 ;ENABLED = true
 ;; Disable the creation of **new** pull mirrors. Pre-existing mirrors remain valid. Will be ignored if `mirror.ENABLED` is `false`.
 ;DISABLE_NEW_PULL = false

options/locale/locale_en-US.ini
@@ -908,7 +908,6 @@ need_auth = Authorization
 migrate_options = Migration Options
 migrate_service = Migration Service
 migrate_options_mirror_helper = This repository will be a <span class="text blue">mirror</span>
-migrate_options_mirror_disabled = Your site administrator has disabled new mirrors.
 migrate_options_lfs = Migrate LFS files
 migrate_options_lfs_endpoint.label = LFS Endpoint
 migrate_options_lfs_endpoint.description = Migration will attempt to use your Git remote to <a target="_blank" rel="noopener noreferrer" href="%s">determine the LFS server</a>. You can also specify a custom endpoint if the repository LFS data is stored somewhere else.

services/cron/tasks_basic.go
@@ -141,7 +141,9 @@ func registerCleanupHookTaskTable() {
 }
 
 func initBasicTasks() {
-	registerUpdateMirrorTask()
+	if setting.Mirror.Enabled {
+		registerUpdateMirrorTask()
+	}
 	registerRepoHealthCheck()
 	registerCheckRepoStats()
 	registerArchiveCleanup()

templates/repo/migrate/options.tmpl
@@ -1,13 +1,8 @@
-<div class="inline field">
+<div class="inline field" {{if .DisableNewPullMirrors}} hidden{{end}}>
 	<label>{{.i18n.Tr "repo.migrate_options"}}</label>
 	<div class="ui checkbox">
-		{{if .DisableNewPullMirrors}}
-			<input id="mirror" name="mirror" type="checkbox" readonly>
-			<label>{{.i18n.Tr "repo.migrate_options_mirror_disabled"}}</label>
-		{{else}}
-			<input id="mirror" name="mirror" type="checkbox" {{if .mirror}} checked{{end}}>
-			<label>{{.i18n.Tr "repo.migrate_options_mirror_helper" | Safe}}</label>
-		{{end}}
+		<input id="mirror" name="mirror" type="checkbox" {{if and .mirror (not .DisableNewPullMirrors)}} checked{{end}}>
+		<label>{{.i18n.Tr "repo.migrate_options_mirror_helper" | Safe}}</label>
 	</div>
 </div>
 {{if .LFSActive}}

commit b1c36319891a8729e54f04681107e1f1ec5d38a5

Mirror filter removed only when whole mirroring feature is disabled
Fixes: 02b45051503d4330da9757ff084c9cc5e6e60d84
Related: https://github.com/go-gitea/gitea/pull/18527#discussion_r883268890
Author-Change-Id: IB#1105104

routers/web/user/home.go
@@ -77,7 +77,7 @@ func Dashboard(ctx *context.Context) {
 	ctx.Data["PageIsNews"] = true
 	cnt, _ := models.GetOrganizationCount(db.DefaultContext, ctxUser)
 	ctx.Data["UserOrgsCount"] = cnt
-	ctx.Data["DisableNewPullMirrors"] = setting.Mirror.DisableNewPull
+	ctx.Data["MirrorsEnabled"] = setting.Mirror.Enabled
 
 	var uid int64
 	if ctxUser != nil {

templates/user/dashboard/repolist.tmpl
@@ -109,7 +109,7 @@
 						{{.i18n.Tr "forks"}}
 						<div v-show="reposFilter === 'forks'" class="ui circular mini grey label">${repoTypeCount}</div>
 					</a>
-					{{if not .DisableNewPullMirrors}}
+					{{if .MirrorsEnabled}}
 					<a class="item" :class="{active: reposFilter === 'mirrors'}" @click="changeReposFilter('mirrors')">
 						{{.i18n.Tr "mirrors"}}
 						<div v-show="reposFilter === 'mirrors'" class="ui circular mini grey label">${repoTypeCount}</div>