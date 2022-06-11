88f2e457d891c4e91a9eddab8143d0109e08dfa9
wxiaoguang <wxiaoguang@gmail.com>
2022-06-11 11:56:27 +0800

    Fix data-race problems in git module (quick patch) (#19934)
    * Fix data-race problems in git module
    
    * use HomeDir instead of setting.RepoRootPath
    
    Co-authored-by: Lunny Xiao <xiaolunwen@gmail.com>

--- models/migrations/migrations_test.go
+++ models/migrations/migrations_test.go
@@ -203,7 +203,7 @@ func prepareTestEnv(t *testing.T, skip int, syncModels ...interface{}) (*xorm.En
 	deferFn := PrintCurrentTest(t, ourSkip)
 	assert.NoError(t, os.RemoveAll(setting.RepoRootPath))
 	assert.NoError(t, unittest.CopyDir(path.Join(filepath.Dir(setting.AppPath), "integrations/gitea-repositories-meta"), setting.RepoRootPath))
-	assert.NoError(t, git.InitWithConfigSync(context.Background()))
+	assert.NoError(t, git.InitOnceWithSync(context.Background()))
 	ownerDirs, err := os.ReadDir(setting.RepoRootPath)
 	if err != nil {
 		assert.NoError(t, err, "unable to read the new repo root: %v\n", err)
