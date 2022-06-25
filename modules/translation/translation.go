8334002f72d12796c528fa1335e26969dbba1397
Gusted <williamzijl7@hotmail.com>
2022-06-25 22:51:06 +0200

    Refactor i18n
    - Separate dev and production locale stores.
    - Allow for live-reloading in dev mode.
    
    Co-authored-by: zeripath <art27@cantab.net>

--- modules/translation/translation.go
+++ modules/translation/translation.go
@@ -42,7 +42,7 @@ func AllLangs() []*LangType {
 
 // InitLocales loads the locales
 func InitLocales() {
-	i18n.ResetDefaultLocales()
+	i18n.ResetDefaultLocales(setting.IsProd)
 	localeNames, err := options.Dir("locale")
 	if err != nil {
 		log.Fatal("Failed to list locale files: %v", err)
