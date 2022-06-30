c22d07df4dc4d5baf21d6840c592c89282c10397
Tyrone Yeh <tyrone_yeh@draytek.com>
2022-06-30 13:57:15 +0800

    Add select all checkbox function

--- web_src/js/features/common-issue.js
+++ web_src/js/features/common-issue.js
@@ -2,14 +2,35 @@ import $ from 'jquery';
 import {updateIssuesMeta} from './repo-issue.js';
 
 export function initCommonIssue() {
-  $('.issue-checkbox').on('click', () => {
-    const numChecked = $('.issue-checkbox').children('input:checked').length;
-    if (numChecked > 0) {
+  $('.issue-checkbox,.issue-checkbox-all').on('click', (e) => {
+    const issuecheckbox = $('.issue-checkbox input');
+    let allcheckbox;
+    if (e.currentTarget.className.indexOf('issue-checkbox-all') !== -1) {
+      allcheckbox = $('.issue-checkbox-all input');
+      if (allcheckbox.prop('checked')) {
+        const selected = $('.issue-checkbox input:checked');
+        $('.issue-checkbox input:not(:checked)').prop('checked', 1);
+        selected.prop('checked', 0);
+      } else {
+        $('.issue-checkbox input:checked').prop('checked', 0);
+      }
+    }
+    if (e.shiftKey && config.checkboxfirst !== undefined) {
+      for (let i = config.checkboxfirst + 1, j = issuecheckbox.index($(e.currentTarget).find('input')); i < j; i++) {
+        issuecheckbox[i].checked = 1;
+      }
+      delete config.checkboxfirst;
+    } else {
+      config.checkboxfirst = issuecheckbox.index($(e.currentTarget).find('input'));
+    }
+    if (issuecheckbox.is(':checked')) {
       $('#issue-filters').addClass('hide');
       $('#issue-actions').removeClass('hide');
+      $('#issue-actions .six').prepend($('.issue-checkbox-all'))
     } else {
       $('#issue-filters').removeClass('hide');
       $('#issue-actions').addClass('hide');
+      $('#issue-filters .six').prepend($('.issue-checkbox-all'))
     }
   });
 
