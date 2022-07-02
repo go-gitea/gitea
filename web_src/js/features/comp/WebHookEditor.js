1fdababf7c519722280ff2cd43eefece70fc2158
justusbunsi <61625851+justusbunsi@users.noreply.github.com>
2022-07-02 19:13:13 +0200

    Implement frontend part
    Signed-off-by: justusbunsi <61625851+justusbunsi@users.noreply.github.com>

--- web_src/js/features/comp/WebHookEditor.js
+++ web_src/js/features/comp/WebHookEditor.js
@@ -1,10 +1,68 @@
 import $ from 'jquery';
 const {csrfToken} = window.config;
 
+const initAuthenticationHeaderSection = function() {
+  const $authHeaderSection = $('.auth-headers');
+
+  if ($authHeaderSection.length === 0) {
+    return;
+  }
+
+  const $checkbox = $authHeaderSection.find('.checkbox input');
+
+  const updateHeaderContentType = function() {
+    const isBasicAuth = $authHeaderSection.find('#auth_header_type').val() === 'basic';
+    const $basicAuthFields = $authHeaderSection.find('.basic-auth');
+    const $tokenAuthFields = $authHeaderSection.find('.token-auth');
+
+    if (isBasicAuth) {
+      $basicAuthFields.addClass("required");
+      $basicAuthFields.find('input').attr("required", "");
+      $basicAuthFields.show();
+
+      $tokenAuthFields.removeClass("required");
+      $tokenAuthFields.find('input').removeAttr("required");
+      $tokenAuthFields.hide();
+    } else {
+      $basicAuthFields.removeClass("required");
+      $basicAuthFields.find('input').removeAttr("required");
+      $basicAuthFields.hide();
+
+      $tokenAuthFields.addClass("required");
+      $tokenAuthFields.find('input').attr("required", "");
+      $tokenAuthFields.show();
+    }
+  };
+
+  const updateHeaderCheckbox = function() {
+    if ($checkbox.is(':checked')) {
+      const $headerName = $authHeaderSection.find('#auth_header_name');
+      $headerName.attr("required", "");
+      $headerName.parent().addClass("required");
+      $headerName.parent().show();
+      $authHeaderSection.find('#auth_header_type').parent().parent().show();
+      updateHeaderContentType();
+    } else {
+      $authHeaderSection.find('.auth-header').hide();
+      $authHeaderSection.find('.auth-header').removeClass("required");
+      $authHeaderSection.find('.auth-header input').removeAttr("required");
+    }
+  };
+
+  if ($checkbox.is(':checked')) {
+    updateHeaderCheckbox();
+  }
+
+  $checkbox.on('change', updateHeaderCheckbox);
+  $authHeaderSection.find('#auth_header_type').on('change', updateHeaderContentType);
+};
+
 export function initCompWebHookEditor() {
   if ($('.new.webhook').length === 0) {
     return;
   }
+
+  initAuthenticationHeaderSection();
 
   $('.events.checkbox input').on('change', function () {
     if ($(this).is(':checked')) {
