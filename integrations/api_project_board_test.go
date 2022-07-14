c060beb07d7121434ec28b5f17f16451c38ecf01
Dinesh Salunke <dineshsalunke@consultbop.com>
2022-07-14 21:17:39 +0530

    refactor(integrations): add boilerplate file project and board tests

---
+++ integrations/api_project_board_test.go
@@ -0,0 +1,29 @@
+// Copyright 2017 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package integrations
+
+import (
+	"testing"
+)
+
+func TestAPIListProjectBoads(t *testing.T) {
+	defer prepareTestEnv(t)()
+}
+
+func TestAPICreateProjectBoard(t *testing.T) {
+	defer prepareTestEnv(t)()
+}
+
+func TestAPIGetProjectBoard(t *testing.T) {
+	defer prepareTestEnv(t)()
+}
+
+func TestAPIUpdateProjectBoard(t *testing.T) {
+	defer prepareTestEnv(t)()
+}
+
+func TestAPIDeleteProjectBoard(t *testing.T) {
+	defer prepareTestEnv(t)()
+}
---
+++ integrations/api_project_test.go
@@ -0,0 +1,29 @@
+// Copyright 2022 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package integrations
+
+import (
+	"testing"
+)
+
+func TestAPIListRepositoryProjects(t *testing.T) {
+	defer prepareTestEnv(t)()
+}
+
+func TestAPICreateRepositoryProject(t *testing.T) {
+	defer prepareTestEnv(t)()
+}
+
+func TestAPIGetProject(t *testing.T) {
+	defer prepareTestEnv(t)()
+}
+
+func TestAPIUpdateProject(t *testing.T) {
+	defer prepareTestEnv(t)()
+}
+
+func TestAPIDeleteProject(t *testing.T) {
+	defer prepareTestEnv(t)()
+}
