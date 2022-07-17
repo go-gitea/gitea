468019557ae8c7c8dcb64999578e1526aff3159e
Andrew Thornton <art27@cantab.net>
2022-07-17 20:54:40 +0100

    placate linter
    Signed-off-by: Andrew Thornton <art27@cantab.net>

--- modules/queue/setting.go
+++ modules/queue/setting.go
@@ -37,7 +37,7 @@ var KnownUniqueQueueNames = []UniqueQueueName{
 }
 
 // QueueName represents an expected name for Queue
-type QueueName string // nolint (allow this to stutter)
+type QueueName string // nolint // allow this to stutter
 
 // list of all expected Queues
 const (
