536d3a8f9ba9a0c28452ad92f074c7fe896918ad
6543 <6543@obermui.de>
2022-07-19 21:52:30 +0200

    make genKeyPar reusable

--- modules/ssh/testhelper.go
+++ modules/ssh/testhelper.go
@@ -5,6 +5,7 @@
 package ssh
 
 import (
+	"bytes"
 	"crypto/ed25519"
 	"crypto/rand"
 	"encoding/pem"
@@ -19,42 +20,58 @@ import (
 // Public key is encoded in the format for inclusion in an OpenSSH authorized_keys file.
 // Private Key generated is PEM encoded
 func GenKeyPair(keyPath string) error {
-	publicKey, privateKey, err := ed25519.GenerateKey(rand.Reader)
+	publicKey, privateKey, err := genKeyPair()
 	if err != nil {
 		return err
 	}
 
-	privateKeyPEM := &pem.Block{Type: "OPENSSH PRIVATE KEY", Bytes: privateKey}
-	f, err := os.OpenFile(keyPath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0o600)
+	privKeyFile, err := os.OpenFile(keyPath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0o600)
 	if err != nil {
 		return err
 	}
 	defer func() {
-		if err = f.Close(); err != nil {
+		if err = privKeyFile.Close(); err != nil {
+			log.Error("Close: %v", err)
+		}
+	}()
+	if _, err := privKeyFile.Write(privateKey); err != nil {
+		return err
+	}
+
+	pubKeyFile, err := os.OpenFile(keyPath+".pub", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0o600)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err = pubKeyFile.Close(); err != nil {
 			log.Error("Close: %v", err)
 		}
 	}()
 
-	if err := pem.Encode(f, privateKeyPEM); err != nil {
-		return err
+	_, err = pubKeyFile.Write(publicKey)
+	return err
+}
+
+func genKeyPair() (publicK, privateK []byte, err error) {
+	publicKey, privateKey, err := ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	privateKeyPEM := &pem.Block{Type: "OPENSSH PRIVATE KEY", Bytes: privateKey}
+	bufPriv := new(bytes.Buffer)
+
+	if err := pem.Encode(bufPriv, privateKeyPEM); err != nil {
+		return nil, nil, err
 	}
 
 	// generate public key
 	pub, err := gossh.NewPublicKey(&publicKey)
 	if err != nil {
-		return err
+		return nil, nil, err
 	}
 
 	public := gossh.MarshalAuthorizedKey(pub)
-	p, err := os.OpenFile(keyPath+".pub", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0o600)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		if err = p.Close(); err != nil {
-			log.Error("Close: %v", err)
-		}
-	}()
-	_, err = p.Write(public)
-	return err
+
+	return bufPriv.Bytes(), public, nil
 }
