05b128a9f7543771fac6986f87a2cfa387313e06
Tyrone Yeh <tyrone_yeh@draytek.com>
2022-07-22 21:54:47 +0800

    Add repository condition for issue count

--- routers/web/user/home.go
+++ routers/web/user/home.go
@@ -591,6 +591,7 @@ func buildIssueOverview(ctx *context.Context, unitType unit.Type) {
 			LabelIDs:   opts.LabelIDs,
 			Org:        org,
 			Team:       team,
+			RepoIDs:    repoIDs,
 		}
 
 		issueStats, err = issues_model.GetUserIssueStats(statsOpts)
