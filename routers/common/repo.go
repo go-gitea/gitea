76331e7e79dc1a051a22468aac77fb1141414364
Tyrone Yeh <tyrone_yeh@draytek.com>
2022-07-23 07:42:29 +0800

    Remove or isPDF in condition

--- routers/common/repo.go
+++ routers/common/repo.go
@@ -88,14 +88,12 @@ func ServeData(ctx *context.Context, name string, size int64, reader io.Reader) 
 		}
 		if (st.IsImage() || st.IsPDF()) && (setting.UI.SVG.Enabled || !st.IsSvgImage()) {
 			ctx.Resp.Header().Set("Content-Disposition", fmt.Sprintf(`inline; filename="%s"`, name))
-			if st.IsSvgImage() || st.IsPDF() {
-				if !st.IsPDF() {
-					ctx.Resp.Header().Set("Content-Security-Policy", "default-src 'none'; style-src 'unsafe-inline'; sandbox")
-				}
+			if st.IsSvgImage() {
+				ctx.Resp.Header().Set("Content-Security-Policy", "default-src 'none'; style-src 'unsafe-inline'; sandbox")
 				ctx.Resp.Header().Set("X-Content-Type-Options", "nosniff")
 				if st.IsSvgImage() {
 					ctx.Resp.Header().Set("Content-Type", typesniffer.SvgMimeType)
-				} else if mappedMimeType == "" {
+				} else {
 					ctx.Resp.Header().Set("Content-Type", typesniffer.ApplicationOctetStream)
 				}
 			}
