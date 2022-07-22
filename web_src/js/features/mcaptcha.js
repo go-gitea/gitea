62ea258e63e5c073b01a9a64ea1d1788fd15dad3
Gusted <williamzijl7@hotmail.com>
2022-07-22 17:00:04 +0200

    Code-style

--- web_src/js/features/mcaptcha.js
+++ web_src/js/features/mcaptcha.js
@@ -1,11 +1,12 @@
 export async function initMcaptcha() {
   const siteKeyEl = document.querySelector('.m-captcha');
-
   if (!siteKeyEl) {
     return;
   }
+
   const {default: mCaptcha} = await import(/* webpackChunkName: "mcaptcha-vanilla-glue" */'@mcaptcha/vanilla-glue');
   const siteKey = siteKeyEl.getAttribute('data-sitekey');
+
   mCaptcha.default({
     siteKey: {
       instanceUrl: new URL('http://localhost:7000'),
