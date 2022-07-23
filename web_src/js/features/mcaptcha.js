5015c38cdd5cc7aaef6073bcefc6542d6cd99aa7
Gusted <williamzijl7@hotmail.com>
2022-07-23 06:02:20 +0200

    Don't use hardcoded instance

--- web_src/js/features/mcaptcha.js
+++ web_src/js/features/mcaptcha.js
@@ -1,16 +1,17 @@
 export async function initMcaptcha() {
-  const siteKeyEl = document.querySelector('.m-captcha');
-  if (!siteKeyEl) {
+  const mCaptchaEl = document.querySelector('.m-captcha');
+  if (!mCaptchaEl) {
     return;
   }
 
   const {default: mCaptcha} = await import(/* webpackChunkName: "mcaptcha-vanilla-glue" */'@mcaptcha/vanilla-glue');
   mCaptcha.INPUT_NAME = 'm-captcha-response';
-  const siteKey = siteKeyEl.getAttribute('data-sitekey');
+  const siteKey = mCaptchaEl.getAttribute('data-sitekey');
+  const instanceURL = mCaptchaEl.getAttribute('data-instance-url');
 
   mCaptcha.default({
     siteKey: {
-      instanceUrl: new URL('http://localhost:7000'),
+      instanceUrl: new URL(instanceURL),
       key: siteKey,
     }
   });
