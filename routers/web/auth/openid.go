4c52706d759794e74e4e422bdbd49af71dad0892
6543 <6543@obermui.de>
2022-07-26 15:39:45 +0200

    fix lint

--- routers/web/auth/openid.go
+++ routers/web/auth/openid.go
@@ -15,6 +15,7 @@ import (
 	"code.gitea.io/gitea/modules/context"
 	"code.gitea.io/gitea/modules/hcaptcha"
 	"code.gitea.io/gitea/modules/log"
+	"code.gitea.io/gitea/modules/mcaptcha"
 	"code.gitea.io/gitea/modules/recaptcha"
 	"code.gitea.io/gitea/modules/session"
 	"code.gitea.io/gitea/modules/setting"
