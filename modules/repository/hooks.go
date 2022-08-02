a609ae4e17588b394355ddaa863f093611fe9773
JonRB <jon.roadleybattin@gmail.com>
2022-08-02 20:52:22 +0100

    gofmt comment line

--- modules/repository/hooks.go
+++ modules/repository/hooks.go
@@ -154,7 +154,7 @@ func createDelegateHooks(repoPath string) (err error) {
 }
 
 func checkExecutable(filename string) bool {
-  // windows has no concept of a executable bit
+	// windows has no concept of a executable bit
 	if runtime.GOOS == "windows" {
 		return true
 	}
