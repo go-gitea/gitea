f76545f22f2930745e446a9b0ce90906261b6dd3
Gusted <williamzijl7@hotmail.com>
2022-08-07 14:27:53 +0200

    Backport changes

--- modules/util/sec_to_time.go
+++ modules/util/sec_to_time.go
@@ -22,15 +22,19 @@ import (
 // 86400 = 60 * 60 * 24 (amount of seconds in a day)
 func SecToTime(duration int64) string {
 	formattedTime := ""
-	years := (duration / 86400) / 365
 
-	// The following three variables are calculated with taking
-	// into account the previous calculated variables, this avoids
+	// The following four variables are calculated by taking
+	// into account the previously calculated variables, this avoids
 	// pitfalls when using remainders. As that could lead to incorrect
 	// results when the calculated number equals the quotient number.
-	months := (duration/86400 - years*365) / 30
-	weeks := (duration/86400 - years*365 - months*30) / 7
-	days := duration/86400 - years*365 - months*30 - weeks*7
+	remainingDays := duration / (60 * 60 * 24)
+	years := remainingDays / 365
+	remainingDays -= years * 365
+	months := remainingDays * 12 / 365
+	remainingDays -= months * 365 / 12
+	weeks := remainingDays / 7
+	remainingDays -= weeks * 7
+	days := remainingDays
 
 	// The following three variables are calculated without depending
 	// on the previous calculated variables.
