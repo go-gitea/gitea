6d9596653e89223b61b6b33850b17f3a791248c4
silverwind <me@silverwind.io>
2022-08-18 18:52:00 +0200

    use base option of URL

--- web_src/js/utils.js
+++ web_src/js/utils.js
@@ -100,5 +100,9 @@ export function prettyNumber(num, locale = 'en-US') {
 
 // parse a URL, either relative '/path' or absolute 'https://localhost/path'
 export function parseUrl(str) {
-  return new URL(`${str.startsWith('http') ? '' : window.location.origin}${str}`);
+  if (str.startsWith('http')) {
+    return new URL(str);
+  } else {
+    return new URL(str, window.location.origin);
+  }
 }
