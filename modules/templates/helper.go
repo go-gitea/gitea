c0d4c87e82db53153f00e9b9180a72ab4e65d16c
wxiaoguang <wxiaoguang@gmail.com>
2022-09-11 19:36:44 +0800

    revert label nil check

--- modules/templates/helper.go
+++ modules/templates/helper.go
@@ -379,6 +379,10 @@ func NewFuncMap() []template.FuncMap {
 		"RenderLabels": func(labels []*issues_model.Label, repoLink string) template.HTML {
 			htmlStr := `<span class="labels-list">`
 			for _, label := range labels {
+				// Protect against nil value in labels - shouldn't happen but would cause a panic if so
+				if label == nil {
+					continue
+				}
 				htmlStr += fmt.Sprintf("<a href='%s/%s' class='ui label' style='color: %s !important; background-color: %s !important'>%s</a> ",
 					repoLink, "issues?labels="+strconv.FormatInt(label.ID, 10), label.ForegroundColor(), label.Color, RenderEmoji(label.Name))
 			}
