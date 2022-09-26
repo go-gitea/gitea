a8891d22b8d1b39082fc6ec9942e4540936b5ba3
wxiaoguang <wxiaoguang@gmail.com>
2022-09-26 17:09:38 +0800

    make code more readable

--- web_src/js/components/DiffFileTree.vue
+++ web_src/js/components/DiffFileTree.vue
@@ -114,9 +114,10 @@ export default {
       this.adjustToggleButton(this.fileTreeIsVisible);
     },
     adjustToggleButton(visible) {
-      const icons = document.querySelectorAll('.diff-toggle-file-tree-button .icon'); // icons[0] is "ToShow", icons[1] is "ToHide"
-      icons[0 + visible].classList.remove('hide'); // 0+true=1 (show the "ToShow"), 0+false=0 (show the "ToHide")
-      icons[1 - visible].classList.add('hide'); //    1-true=0 (hide the "ToHide"), 1-false=1 (hide the "ToShow")
+      const [toShow, toHide] = document.querySelectorAll('.diff-toggle-file-tree-button .icon');
+      toShow.classList.remove('hide');
+      toHide.classList.remove('hide');
+      (visible ? toShow : toHide).classList.add('hide');
     },
     loadMoreData() {
       this.isLoadingNewData = true;
