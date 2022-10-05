go-gitea/gitea

commit 7b9435e3946cc1d5f843e71b61d8a8b78a4cb79d

Disabled registration notification e-mail in reverse proxy mode
Disabled registration notification e-mail in reverse proxy mode to avoid
spamming with invalid instructions (such accounts are created by admin
in exteral systems not during self registration and no gitea passwords
are used for auth).

Related: https://github.com/go-gitea/gitea/pull/18601
Author-Change-Id: IB#1122610

services/auth/reverseproxy.go
@@ -1,5 +1,5 @@
 // Copyright 2014 The Gogs Authors. All rights reserved.
-// Copyright 2019 The Gitea Authors. All rights reserved.
+// Copyright 2022 The Gitea Authors. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
@@ -119,8 +119,6 @@ func (r *ReverseProxy) newUser(req *http.Request) *user_model.User {
 		log.Error("CreateUser: %v", err)
 		return nil
 	}
-
-	mailer.SendRegisterNotifyMail(user)
 
 	return user
 }

commit 4b7dd1f4217b2da006d26a49ee5e1bf8a4614bb4

Unused import removed
Fixes: 7b9435e3946cc1d5f843e71b61d8a8b78a4cb79d
Author-Change-Id: IB#1122610

services/auth/reverseproxy.go
@@ -14,7 +14,6 @@ import (
 	"code.gitea.io/gitea/modules/setting"
 	"code.gitea.io/gitea/modules/util"
 	"code.gitea.io/gitea/modules/web/middleware"
-	"code.gitea.io/gitea/services/mailer"
 
 	gouuid "github.com/google/uuid"
 )