19eb6bc5f8c6f2b2d9cad5958f941004a66bc2e0
wxiaoguang <wxiaoguang@gmail.com>
2022-10-07 23:14:15 +0800

    improve find file

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,13 @@ export default {
   rootDir: 'web_src',
   setupFilesAfterEnv: ['jest-extended/all'],
   testEnvironment: 'jest-environment-jsdom',
-  testMatch: ['<rootDir>/**/*.test.js'],
+  testMatch: ['<rootDir>/**/*.test.js'], // to run tests with ES6 module, node must run with "--experimental-vm-modules"
   testTimeout: 20000,
   transform: {
     '\\.svg$': '<rootDir>/js/testUtils/jestRawLoader.js',
   },
+  setupFiles: [
+    './js/testUtils/jestSetup.js', // prepare global variables used by our code (eg: window.config)
+  ],
   verbose: false,
 };
-
