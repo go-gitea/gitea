974a39d7f4d8643acd7886a83f5f40c7e4200868
wxiaoguang <wxiaoguang@gmail.com>
2022-10-08 16:47:32 +0800

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
+// to run tests with ES6 module, node must run with "--experimental-vm-modules", or see Makefile's "test-frontend" for reference
 export default {
   rootDir: 'web_src',
   setupFilesAfterEnv: ['jest-extended/all'],
   testEnvironment: 'jest-environment-jsdom',
-  testMatch: ['<rootDir>/**/*.test.js'], // to run tests with ES6 module, node must run with "--experimental-vm-modules"
+  testMatch: ['<rootDir>/**/*.test.js'],
   testTimeout: 20000,
   transform: {
     '\\.svg$': '<rootDir>/js/testUtils/jestRawLoader.js',
