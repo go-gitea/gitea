cf1b23c001098411b6b18ea14a2bef17f6002e63
Yarden Shoham <hrsi88@gmail.com>
2022-10-11 13:05:17 +0000

    Respect user's locale when rendering the date range in the repo activity page
    Previously, to make the date range understood by all, we used the format "2006-01-02" for the dates as it's locale-generic.
    
    This commit changes the rendering logic. Instead of rendering the date on the server, we send a formatted computer-readable timestamp. The client's javascript then renders it according to the user's locale.
    
    This approach is reusable across the codebase, any <time></time> tag with the "data-format-text-according-to-datetime" attribute would get rendered according to the user's chosen locale.
    
    Signed-off-by: Yarden Shoham <hrsi88@gmail.com>

--- web_src/js/features/formatting.js
+++ web_src/js/features/formatting.js
@@ -11,4 +11,14 @@ export function initFormattingReplacements() {
       el.textContent = formatted;
     }
   }
+
+  // for each <time></time> tag, if it has the data-format-text-according-to-datetime, format
+  // the text according to the user's chosen locale
+  const locale = document.getElementsByTagName('html')[0].lang;
+  const formatter = new Intl.DateTimeFormat(locale, {year: 'numeric', month: 'long', day: 'numeric'});
+  for (const timeElement of document.getElementsByTagName('time')) {
+    if (timeElement.hasAttribute('data-format-text-according-to-datetime')) {
+      timeElement.innerText = formatter.format(new Date(timeElement.dateTime));
+    }
+  }
 }
