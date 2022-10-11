c964835d08da051b3341df6e6735241125aa5762
Yarden Shoham <hrsi88@gmail.com>
2022-10-11 21:54:00 +0000

    Rename data-format-text-according-to-datetime to data-date-format
    Signed-off-by: Yarden Shoham <hrsi88@gmail.com>

--- web_src/js/features/formatting.js
+++ web_src/js/features/formatting.js
@@ -12,12 +12,12 @@ export function initFormattingReplacements() {
     }
   }
 
-  // for each <time></time> tag, if it has the data-format-text-according-to-datetime, format
+  // for each <time></time> tag, if it has the data-date-format, format
   // the text according to the user's chosen locale
   const locale = document.getElementsByTagName('html')[0].lang;
   const formatter = new Intl.DateTimeFormat(locale, {year: 'numeric', month: 'long', day: 'numeric'});
   for (const timeElement of document.getElementsByTagName('time')) {
-    if (timeElement.hasAttribute('data-format-text-according-to-datetime')) {
+    if (timeElement.hasAttribute('data-date-format')) {
       timeElement.innerText = formatter.format(new Date(timeElement.dateTime));
     }
   }
