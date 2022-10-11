f777fac494019b8ff13079d6d06355966738f14b
Yarden Shoham <hrsi88@gmail.com>
2022-10-12 01:35:38 +0300

    Update web_src/js/features/formatting.js
    Co-authored-by: silverwind <me@silverwind.io>

--- web_src/js/features/formatting.js
+++ web_src/js/features/formatting.js
@@ -14,8 +14,8 @@ export function initFormattingReplacements() {
 
   // for each <time></time> tag, if it has the data-date-format, format
   // the text according to the user's chosen locale
-  const locale = document.getElementsByTagName('html')[0].lang;
-  const formatter = new Intl.DateTimeFormat(locale, {year: 'numeric', month: 'long', day: 'numeric'});
+  const {lang} = document.documentElement;
+  const formatter = new Intl.DateTimeFormat(lang, {year: 'numeric', month: 'long', day: 'numeric'});
   for (const timeElement of document.getElementsByTagName('time')) {
     if (timeElement.hasAttribute('data-date-format')) {
       timeElement.innerText = formatter.format(new Date(timeElement.dateTime));
