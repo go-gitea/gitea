ab1e62f0d87ffa84c1ac7b977f9e8cd44c000641
qwerty287 <ndev@web.de>
2022-10-11 21:20:14 +0200

    Reuse code

--- routers/web/admin/applications.go
+++ routers/web/admin/applications.go
@@ -11,10 +11,8 @@ import (
 	"code.gitea.io/gitea/models/auth"
 	"code.gitea.io/gitea/modules/base"
 	"code.gitea.io/gitea/modules/context"
-	"code.gitea.io/gitea/modules/log"
 	"code.gitea.io/gitea/modules/setting"
-	"code.gitea.io/gitea/modules/web"
-	"code.gitea.io/gitea/services/forms"
+	user_setting "code.gitea.io/gitea/routers/web/user/setting"
 )
 
 var (
@@ -24,15 +22,22 @@ var (
 	tplSettingsEditApplication base.TplName = "admin/applications/edit"
 )
 
-const instanceOwnerUserID = 0
+func newOAuth2CommonHandlers() *user_setting.OAuth2CommonHandlers {
+	return &user_setting.OAuth2CommonHandlers{
+		OwnerID:            0,
+		BasePathList:       fmt.Sprintf("%s/admin/applications", setting.AppSubURL),
+		BasePathEditPrefix: fmt.Sprintf("%s/admin/applications", setting.AppSubURL),
+		TplAppEdit:         tplSettingsEditApplication,
+	}
+}
 
-// Applications render admin applications page
+// Applications render org applications page (for org, at the moment, there are only OAuth2 applications)
 func Applications(ctx *context.Context) {
 	ctx.Data["Title"] = ctx.Tr("settings.applications")
 	ctx.Data["PageIsAdmin"] = true
 	ctx.Data["PageIsAdminApplications"] = true
 
-	apps, err := auth.GetOAuth2ApplicationsByUserID(ctx, instanceOwnerUserID)
+	apps, err := auth.GetOAuth2ApplicationsByUserID(ctx, 0)
 	if err != nil {
 		ctx.ServerError("GetOAuth2ApplicationsByUserID", err)
 		return
@@ -44,134 +49,47 @@ func Applications(ctx *context.Context) {
 
 // ApplicationsPost response for adding an oauth2 application
 func ApplicationsPost(ctx *context.Context) {
-	form := web.GetForm(ctx).(*forms.EditOAuth2ApplicationForm)
 	ctx.Data["Title"] = ctx.Tr("settings.applications")
 	ctx.Data["PageIsAdmin"] = true
 	ctx.Data["PageIsAdminApplications"] = true
 
-	if ctx.HasError() {
-		apps, err := auth.GetOAuth2ApplicationsByUserID(ctx, instanceOwnerUserID)
-		if err != nil {
-			ctx.ServerError("GetOAuth2ApplicationsByUserID", err)
-			return
-		}
-		ctx.Data["Applications"] = apps
-
-		ctx.HTML(http.StatusOK, tplSettingsApplications)
-		return
-	}
-
-	app, err := auth.CreateOAuth2Application(ctx, auth.CreateOAuth2ApplicationOptions{
-		Name:         form.Name,
-		RedirectURIs: []string{form.RedirectURI},
-		UserID:       instanceOwnerUserID,
-	})
-	if err != nil {
-		ctx.ServerError("CreateOAuth2Application", err)
-		return
-	}
-	ctx.Data["App"] = app
-	ctx.Data["ClientSecret"], err = app.GenerateClientSecret()
-	if err != nil {
-		ctx.ServerError("GenerateClientSecret", err)
-		return
-	}
-	ctx.Flash.Success(ctx.Tr("settings.create_oauth2_application_success"))
-	ctx.HTML(http.StatusOK, tplSettingsEditApplication)
+	oa := newOAuth2CommonHandlers()
+	oa.AddApp(ctx)
 }
 
-// EditApplication response for editing oauth2 application
+// EditApplication displays the given application
 func EditApplication(ctx *context.Context) {
-	app, err := auth.GetOAuth2ApplicationByID(ctx, ctx.ParamsInt64("id"))
-	if err != nil {
-		if auth.IsErrOAuthApplicationNotFound(err) {
-			ctx.NotFound("Application not found", err)
-			return
-		}
-		ctx.ServerError("GetOAuth2ApplicationByID", err)
-		return
-	}
-	if app.UID != instanceOwnerUserID {
-		ctx.NotFound("Application not found", nil)
-		return
-	}
 	ctx.Data["PageIsAdmin"] = true
 	ctx.Data["PageIsAdminApplications"] = true
-	ctx.Data["App"] = app
-	ctx.HTML(http.StatusOK, tplSettingsEditApplication)
+
+	oa := newOAuth2CommonHandlers()
+	oa.EditShow(ctx)
 }
 
 // EditApplicationPost response for editing oauth2 application
 func EditApplicationPost(ctx *context.Context) {
-	form := web.GetForm(ctx).(*forms.EditOAuth2ApplicationForm)
 	ctx.Data["Title"] = ctx.Tr("settings.applications")
 	ctx.Data["PageIsAdmin"] = true
 	ctx.Data["PageIsAdminApplications"] = true
 
-	if ctx.HasError() {
-		apps, err := auth.GetOAuth2ApplicationsByUserID(ctx, instanceOwnerUserID)
-		if err != nil {
-			ctx.ServerError("GetOAuth2ApplicationsByUserID", err)
-			return
-		}
-		ctx.Data["Applications"] = apps
-
-		ctx.HTML(http.StatusOK, tplSettingsApplications)
-		return
-	}
-	var err error
-	if ctx.Data["App"], err = auth.UpdateOAuth2Application(auth.UpdateOAuth2ApplicationOptions{
-		ID:           ctx.ParamsInt64("id"),
-		Name:         form.Name,
-		RedirectURIs: []string{form.RedirectURI},
-		UserID:       instanceOwnerUserID,
-	}); err != nil {
-		ctx.ServerError("UpdateOAuth2Application", err)
-		return
-	}
-	ctx.Flash.Success(ctx.Tr("settings.update_oauth2_application_success"))
-	ctx.HTML(http.StatusOK, tplSettingsEditApplication)
+	oa := newOAuth2CommonHandlers()
+	oa.EditSave(ctx)
 }
 
 // ApplicationsRegenerateSecret handles the post request for regenerating the secret
 func ApplicationsRegenerateSecret(ctx *context.Context) {
 	ctx.Data["Title"] = ctx.Tr("settings")
-	ctx.Data["PageIsAdminApplications"] = true
 	ctx.Data["PageIsAdmin"] = true
+	ctx.Data["PageIsAdminApplications"] = true
 
-	app, err := auth.GetOAuth2ApplicationByID(ctx, ctx.ParamsInt64("id"))
-	if err != nil {
-		if auth.IsErrOAuthApplicationNotFound(err) {
-			ctx.NotFound("Application not found", err)
-			return
-		}
-		ctx.ServerError("GetOAuth2ApplicationByID", err)
-		return
-	}
-	if app.UID != instanceOwnerUserID {
-		ctx.NotFound("Application not found", nil)
-		return
-	}
-	ctx.Data["App"] = app
-	ctx.Data["ClientSecret"], err = app.GenerateClientSecret()
-	if err != nil {
-		ctx.ServerError("GenerateClientSecret", err)
-		return
-	}
-	ctx.Flash.Success(ctx.Tr("settings.update_oauth2_application_success"))
-	ctx.HTML(http.StatusOK, tplSettingsEditApplication)
+	oa := newOAuth2CommonHandlers()
+	oa.RegenerateSecret(ctx)
 }
 
 // DeleteApplication deletes the given oauth2 application
 func DeleteApplication(ctx *context.Context) {
-	if err := auth.DeleteOAuth2Application(ctx.FormInt64("id"), instanceOwnerUserID); err != nil {
-		ctx.ServerError("DeleteOAuth2Application", err)
-		return
-	}
-	log.Trace("OAuth2 Application deleted: %s", ctx.Doer.Name)
-
-	ctx.Flash.Success(ctx.Tr("settings.remove_oauth2_application_success"))
-	ctx.JSON(http.StatusOK, map[string]interface{}{
-		"redirect": fmt.Sprintf("%s/admin/applications", setting.AppSubURL),
-	})
+	oa := newOAuth2CommonHandlers()
+	oa.DeleteApp(ctx)
 }
+
+// TODO: revokes the grant with the given id
