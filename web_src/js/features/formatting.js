f354132d96601faa775734e402df2750fbaa058f
Yarden Shoham <hrsi88@gmail.com>
2022-10-13 14:13:31 +0000

    Localize all timestamps
    Signed-off-by: Yarden Shoham <hrsi88@gmail.com>

--- web_src/js/features/formatting.js
+++ web_src/js/features/formatting.js
@@ -1,7 +1,10 @@
 import {prettyNumber} from '../utils.js';
 
 const {lang} = document.documentElement;
+
 const dateFormatter = new Intl.DateTimeFormat(lang, {year: 'numeric', month: 'long', day: 'numeric'});
+const shortDateFormatter = new Intl.DateTimeFormat(lang, {year: 'numeric', month: 'short', day: 'numeric'});
+const dateTimeFormatter = new Intl.DateTimeFormat(lang, {year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'});
 
 export function initFormattingReplacements() {
   // replace english formatted numbers with locale-specific separators
@@ -13,9 +16,34 @@ export function initFormattingReplacements() {
     }
   }
 
-  // for each <time></time> tag, if it has the data-format="date" attribute, format
-  // the text according to the user's chosen locale
-  for (const timeElement of document.querySelectorAll('time[data-format="date"]')) {
-    timeElement.textContent = dateFormatter.format(new Date(timeElement.dateTime));
+  // for each <time></time> tag, if it has the data-forma attribute, format
+  // the text according to the user's chosen locale and formatter
+  formatAllTimeElements();
+}
+
+function formatAllTimeElements() {
+  const formats = ['date', 'short-date', 'date-time'];
+  for (const f of formats) {
+    formatTimeElements(f);
+  }
+}
+
+function formatTimeElements(format) {
+  let formatter;
+  switch (format) {
+    case 'date':
+      formatter = dateFormatter;
+      break;
+    case 'short-date':
+      formatter = shortDateFormatter;
+      break;
+    case 'date-time':
+      formatter = dateTimeFormatter;
+      break;
+    default:
+      throw new Error('Unknown format');
+  }
+  for (const timeElement of document.querySelectorAll(`time[data-format="${format}"]`)) {
+    timeElement.textContent = formatter.format(new Date(timeElement.dateTime));
   }
 }
