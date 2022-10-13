868d03e36c73b614e10d917ad0fb26f64230196c
KN4CK3R <admin@oldschoolhack.me>
2022-10-13 15:50:04 +0000

    Add second package.

--- tests/integration/api_packages_nuget_test.go
+++ tests/integration/api_packages_nuget_test.go
@@ -84,14 +84,14 @@ func TestPackageNuGet(t *testing.T) {
 	symbolFilename := "test.pdb"
 	symbolID := "d910bb6948bd4c6cb40155bcf52c3c94"
 
-	createPackage := func(version string) io.Reader {
+	createPackage := func(id, version string) io.Reader {
 		var buf bytes.Buffer
 		archive := zip.NewWriter(&buf)
 		w, _ := archive.Create("package.nuspec")
 		w.Write([]byte(`<?xml version="1.0" encoding="utf-8"?>
 		<package xmlns="http://schemas.microsoft.com/packaging/2013/05/nuspec.xsd">
 			<metadata>
-				<id>` + packageName + `</id>
+				<id>` + id + `</id>
 				<version>` + version + `</version>
 				<authors>` + packageAuthors + `</authors>
 				<description>` + packageDescription + `</description>
@@ -106,7 +106,7 @@ func TestPackageNuGet(t *testing.T) {
 		return &buf
 	}
 
-	content, _ := ioutil.ReadAll(createPackage(packageVersion))
+	content, _ := ioutil.ReadAll(createPackage(packageName, packageVersion))
 
 	url := fmt.Sprintf("/api/packages/%s/nuget", user.Name)
 
@@ -446,7 +446,11 @@ AAAjQmxvYgAAAGm7ENm9SGxMtAFVvPUsPJTF6PbtAAAAAFcVogEJAAAAAQAAAA==`)
 			t.Run("EnforceGrouped", func(t *testing.T) {
 				defer tests.PrintCurrentTest(t)()
 
-				req := NewRequestWithBody(t, "PUT", url, createPackage("1.0.99"))
+				req := NewRequestWithBody(t, "PUT", url, createPackage(packageName + ".dummy", "1.0.0"))
+				req = AddBasicAuthHeader(req, user.Name)
+				MakeRequest(t, req, http.StatusCreated)
+
+				req = NewRequestWithBody(t, "PUT", url, createPackage(packageName, "1.0.99"))
 				req = AddBasicAuthHeader(req, user.Name)
 				MakeRequest(t, req, http.StatusCreated)
 
@@ -457,9 +461,13 @@ AAAjQmxvYgAAAGm7ENm9SGxMtAFVvPUsPJTF6PbtAAAAAFcVogEJAAAAAQAAAA==`)
 				var result nuget.SearchResultResponse
 				DecodeJSON(t, resp, &result)
 
-				assert.EqualValues(t, 2, result.TotalHits)
-				assert.Len(t, result.Data, 1)
+				assert.EqualValues(t, 3, result.TotalHits)
+				assert.Len(t, result.Data, 2)
 				assert.Len(t, result.Data[0].Versions, 2)
+
+				req = NewRequest(t, "DELETE", fmt.Sprintf("%s/%s/%s", url, packageName + ".dummy", "1.0.0"))
+				req = AddBasicAuthHeader(req, user.Name)
+				MakeRequest(t, req, http.StatusNoContent)
 
 				req = NewRequest(t, "DELETE", fmt.Sprintf("%s/%s/%s", url, packageName, "1.0.99"))
 				req = AddBasicAuthHeader(req, user.Name)
