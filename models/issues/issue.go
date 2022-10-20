176b24d31029f6626595106ef01eaabf0aec92d0
6543 <6543@obermui.de>
2022-10-20 10:06:32 +0200

    Update models/issues/issue.go

--- models/issues/issue.go
+++ models/issues/issue.go
@@ -1732,7 +1732,6 @@ func getIssueStatsChunk(opts *IssueStatsOptions, issueIDs []int64) (*IssueStats,
 						sess.Join("INNER", fmt.Sprintf("issue_label il%d", i),
 							fmt.Sprintf("issue.id = il%[1]d.issue_id AND il%[1]d.label_id = %[2]d", i, labelID))
 					} else if labelID < 0 {
-						// sess.Where("issue.id NOT IN (SELECT issue_id FROM issue_label WHERE label_id = ?)", -labelID)
 						noLabelsIDs = append(noLabelsIDs, -labelID)
 						hasNoLabel = true
 					}
