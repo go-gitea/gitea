a1c2cb3f46fb49b25c8b2d427f146da851d937ee
Yarden Shoham <hrsi88@gmail.com>
2022-10-22 18:31:12 +0000

    Link mentioned user in markdown only if they are visible to viewer
    We need to make sure a user can't confirm the existence of a user with private visibility
    
    Signed-off-by: Yarden Shoham <hrsi88@gmail.com>

--- services/markup/processorhelper.go
+++ services/markup/processorhelper.go
@@ -8,22 +8,24 @@ import (
 	"context"
 
 	"code.gitea.io/gitea/models/user"
-	"code.gitea.io/gitea/modules/log"
+	module_context "code.gitea.io/gitea/modules/context"
 	"code.gitea.io/gitea/modules/markup"
 )
 
 func ProcessorHelper() *markup.ProcessorHelper {
 	return &markup.ProcessorHelper{
 		IsUsernameMentionable: func(ctx context.Context, username string) bool {
-			// TODO: cast ctx to modules/context.Context and use IsUserVisibleToViewer
-
-			// Only link if the user actually exists
-			userExists, err := user.IsUserExist(ctx, 0, username)
+			mentionedUser, err := user.GetUserByName(ctx, username)
 			if err != nil {
-				log.Error("Failed to validate user in mention %q exists, assuming it does", username)
-				userExists = true
+				return false
 			}
-			return userExists
+
+			moduleCtx, ok := ctx.(*module_context.Context)
+			if !ok {
+				return false
+			}
+
+			return user.IsUserVisibleToViewer(moduleCtx, mentionedUser, moduleCtx.Doer)
 		},
 	}
 }
