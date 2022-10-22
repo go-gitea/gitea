a9d7856a260fe3422a6868cdb523b826442f1469
Lunny Xiao <xiaolunwen@gmail.com>
2022-10-22 20:27:20 +0800

    Split migrations folder

---
+++ models/migrations/v1_11/v102.go
@@ -0,0 +1,22 @@
+// Copyright 2019 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package v1_11
+
+import (
+	"code.gitea.io/gitea/models/migrations/base"
+	"xorm.io/xorm"
+)
+
+func DropColumnHeadUserNameOnPullRequest(x *xorm.Engine) error {
+	sess := x.NewSession()
+	defer sess.Close()
+	if err := sess.Begin(); err != nil {
+		return err
+	}
+	if err := base.DropTableColumns(sess, "pull_request", "head_user_name"); err != nil {
+		return err
+	}
+	return sess.Commit()
+}
