49aaa81bd558576548997fa0b27531c7cc6a4ead
kolaente <k@knt.li>
2022-10-22 22:13:58 +0200

    fix: lint

--- web_src/js/components/PullRequestMergeForm.vue
+++ web_src/js/components/PullRequestMergeForm.vue
@@ -178,7 +178,7 @@ export default {
       this.autoMergeWhenSucceed = autoMerge;
     },
     clearMergeMessage() {
-      this.mergeMessageFieldValue = this.mergeForm.defaultMergeMessage
+      this.mergeMessageFieldValue = this.mergeForm.defaultMergeMessage;
     },
   },
 };
