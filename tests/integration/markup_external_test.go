a9402135202c1981bdb95e50cca182b3f8e840c9
Lunny Xiao <xiaolunwen@gmail.com>
2022-10-23 12:26:05 +0800

    Fix test

--- tests/integration/markup_external_test.go
+++ tests/integration/markup_external_test.go
@@ -27,10 +27,14 @@ func TestExternalMarkupRenderer(t *testing.T) {
 	const repoURL = "user30/renderer"
 	req := NewRequest(t, "GET", repoURL+"/src/branch/master/README.html")
 	resp := MakeRequest(t, req, http.StatusOK)
+	assert.EqualValues(t, "text/html; charset=UTF-8", resp.Header()["Content-Type"][0])
+
 	bs, err := io.ReadAll(resp.Body)
 	assert.NoError(t, err)
 
 	doc := NewHTMLParser(t, bytes.NewBuffer(bs))
 	div := doc.Find("div.file-view")
-	assert.EqualValues(t, "<div>	test external renderer</div>", strings.TrimSpace(div.Text()))
+	data, err := div.Html()
+	assert.NoError(t, err)
+	assert.EqualValues(t, "<div>\n\ttest external renderer\n</div>", strings.TrimSpace(data))
 }
