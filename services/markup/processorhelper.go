7cb1417a185fd946623d4cd06564ad890e189e75
wxiaoguang <wxiaoguang@gmail.com>
2022-10-23 11:55:26 +0800

    fix tests

--- services/markup/processorhelper.go
+++ services/markup/processorhelper.go
@@ -8,8 +8,7 @@ import (
 	"context"
 
 	"code.gitea.io/gitea/models/user"
-	module_context "code.gitea.io/gitea/modules/context"
-	"code.gitea.io/gitea/modules/log"
+	gitea_context "code.gitea.io/gitea/modules/context"
 	"code.gitea.io/gitea/modules/markup"
 )
 
@@ -21,13 +20,14 @@ func ProcessorHelper() *markup.ProcessorHelper {
 				return false
 			}
 
-			moduleCtx, ok := ctx.(*module_context.Context)
+			giteaCtx, ok := ctx.(*gitea_context.Context)
 			if !ok {
-				log.Warn("couldn't cast context, assuming user should be visible based on their visibility")
+				// when using general context, use user's visibility to check
 				return mentionedUser.Visibility.IsPublic()
 			}
 
-			return user.IsUserVisibleToViewer(moduleCtx, mentionedUser, moduleCtx.Doer)
+			// when using gitea context (web context), use user's visibility and user's permission to check
+			return user.IsUserVisibleToViewer(giteaCtx, mentionedUser, giteaCtx.Doer)
 		},
 	}
 }
