49a4464160254604d2c42b760a901952d8bc3c8b
Wayne Starr <Racer159@users.noreply.github.com>
2022-10-24 08:50:22 -0500

    Allow for resolution of NPM registry paths that match upstream (#21568)
    This PR fixes issue #21567 allowing for package tarball URLs to match
    the upstream registry (and GitLab/JFrog Artifactory URLs). It uses a
    regex to parse the filename (which contains the NPM version) and does a
    fuzzy search to pull it out. The regex was built/expanded from
    http://json.schemastore.org/package,
    https://github.com/Masterminds/semver, and
    https://docs.npmjs.com/cli/v6/using-npm/semver and is testable here:
    https://regex101.com/r/OydBJq/5
    
    Co-authored-by: Lunny Xiao <xiaolunwen@gmail.com>

--- routers/api/packages/api.go
+++ routers/api/packages/api.go
@@ -215,6 +215,7 @@ func Routes(ctx gocontext.Context) *web.Route {
 					r.Get("", npm.DownloadPackageFile)
 					r.Delete("/-rev/{revision}", reqPackageAccess(perm.AccessModeWrite), npm.DeletePackageVersion)
 				})
+				r.Get("/-/{filename}", npm.DownloadPackageFileByName)
 				r.Group("/-rev/{revision}", func() {
 					r.Delete("", npm.DeletePackage)
 					r.Put("", npm.DeletePreview)
@@ -227,6 +228,7 @@ func Routes(ctx gocontext.Context) *web.Route {
 					r.Get("", npm.DownloadPackageFile)
 					r.Delete("/-rev/{revision}", reqPackageAccess(perm.AccessModeWrite), npm.DeletePackageVersion)
 				})
+				r.Get("/-/{filename}", npm.DownloadPackageFileByName)
 				r.Group("/-rev/{revision}", func() {
 					r.Delete("", npm.DeletePackage)
 					r.Put("", npm.DeletePreview)
