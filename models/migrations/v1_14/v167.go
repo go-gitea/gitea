e72acd5e5b2d043fcf0a0182a1eedaed8120c155
Lunny Xiao <xiaolunwen@gmail.com>
2022-11-02 16:54:36 +0800

    Split migrations folder (#21549)
    There are too many files in `models/migrations` folder so that I split
    them into sub folders.

---
+++ models/migrations/v1_14/v167.go
@@ -0,0 +1,24 @@
+// Copyright 2021 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package v1_14 //nolint
+
+import (
+	"fmt"
+
+	"xorm.io/xorm"
+)
+
+func AddUserRedirect(x *xorm.Engine) (err error) {
+	type UserRedirect struct {
+		ID             int64  `xorm:"pk autoincr"`
+		LowerName      string `xorm:"UNIQUE(s) INDEX NOT NULL"`
+		RedirectUserID int64
+	}
+
+	if err := x.Sync2(new(UserRedirect)); err != nil {
+		return fmt.Errorf("Sync2: %w", err)
+	}
+	return nil
+}
