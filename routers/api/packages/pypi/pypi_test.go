9035c418302f1c8c1a3af26d54e011b5ca37e5aa
Wayne Starr <me@racer159.com>
2022-11-07 14:26:15 -0600

    Simplify tests and refactor name/version checks

---
+++ routers/api/packages/pypi/pypi_test.go
@@ -0,0 +1,36 @@
+// Copyright 2022 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package pypi
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHasInvalidMetadata(t *testing.T) {
+	// The test cases below were created from the following Python PEPs:
+	// https://peps.python.org/pep-0426/#name
+	// https://peps.python.org/pep-0440/#appendix-b-parsing-version-strings-with-regular-expressions
+
+	// Valid Cases
+	assert.False(t, hasInvalidMetadata("Test.Name.1234", "1.0.1"))
+	assert.False(t, hasInvalidMetadata("test_name", "1.0.1"))
+	assert.False(t, hasInvalidMetadata("test-name", "1.0.1"))
+	assert.False(t, hasInvalidMetadata("test-name", "v1.0.1"))
+	assert.False(t, hasInvalidMetadata("test-name", "2012.4"))
+	assert.False(t, hasInvalidMetadata("test-name", "1.0.1-alpha"))
+	assert.False(t, hasInvalidMetadata("test-name", "1.0.1a1"))
+	assert.False(t, hasInvalidMetadata("test-name", "1.0b2.r345.dev456"))
+	assert.False(t, hasInvalidMetadata("test-name", "1!1.0.1"))
+	assert.False(t, hasInvalidMetadata("test-name", "1.0.1+local.1"))
+
+	// Invalid Cases
+	assert.True(t, hasInvalidMetadata(".test-name", "1.0.1"))
+	assert.True(t, hasInvalidMetadata("test!name", "1.0.1"))
+	assert.True(t, hasInvalidMetadata("test-name", "a1.0.1"))
+	assert.True(t, hasInvalidMetadata("test-name", "1.0.1aa"))
+	assert.True(t, hasInvalidMetadata("test-name", "1.0.0-alpha.beta"))
+}
