fad12ad31cd38866dfcb392e58908665ee1c8c3f
Lunny Xiao <xiaolunwen@gmail.com>
2022-11-08 22:16:56 +0800

    use const for default size

--- modules/svg/svg.go
+++ modules/svg/svg.go
@@ -21,6 +21,8 @@ var (
 	heightRe = regexp.MustCompile(`height="[0-9]+?"`)
 )
 
+const defaultSize = 16
+
 // Init discovers SVGs and populates the `SVGs` variable
 func Init() {
 	SVGs = Discover()
@@ -28,10 +30,10 @@ func Init() {
 
 // Render render icons - arguments icon name (string), size (int), class (string)
 func RenderHTML(icon string, others ...interface{}) template.HTML {
-	size, class := html.ParseSizeAndClass(16, "", others...)
+	size, class := html.ParseSizeAndClass(defaultSize, "", others...)
 
 	if svgStr, ok := SVGs[icon]; ok {
-		if size != 16 {
+		if size != defaultSize {
 			svgStr = widthRe.ReplaceAllString(svgStr, fmt.Sprintf(`width="%d"`, size))
 			svgStr = heightRe.ReplaceAllString(svgStr, fmt.Sprintf(`height="%d"`, size))
 		}
