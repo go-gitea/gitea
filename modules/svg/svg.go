3f86f38b5d23b963bcb40bd73fec51e3273f5076
Lunny Xiao <xiaolunwen@gmail.com>
2022-11-08 11:11:19 +0800

    move svg html render to modules/svg

--- modules/svg/svg.go
+++ modules/svg/svg.go
@@ -4,10 +4,67 @@
 
 package svg
 
+import (
+	"fmt"
+	"html/template"
+	"regexp"
+	"strings"
+)
+
 // SVGs contains discovered SVGs
 var SVGs map[string]string
 
 // Init discovers SVGs and populates the `SVGs` variable
 func Init() {
 	SVGs = Discover()
+}
+
+var (
+	widthRe  = regexp.MustCompile(`width="[0-9]+?"`)
+	heightRe = regexp.MustCompile(`height="[0-9]+?"`)
+)
+
+// ParseOthers get size and class from string with default values
+func ParseOthers(defaultSize int, defaultClass string, others ...interface{}) (int, string) {
+	if len(others) == 0 {
+		return defaultSize, defaultClass
+	}
+
+	size := defaultSize
+	_size, ok := others[0].(int)
+	if ok && _size != 0 {
+		size = _size
+	}
+
+	if len(others) == 1 {
+		return size, defaultClass
+	}
+
+	class := defaultClass
+	if _class, ok := others[1].(string); ok && _class != "" {
+		if defaultClass == "" {
+			class = _class
+		} else {
+			class = defaultClass + " " + _class
+		}
+	}
+
+	return size, class
+}
+
+// Render render icons - arguments icon name (string), size (int), class (string)
+func RenderHTML(icon string, others ...interface{}) template.HTML {
+	size, class := ParseOthers(16, "", others...)
+
+	if svgStr, ok := SVGs[icon]; ok {
+		if size != 16 {
+			svgStr = widthRe.ReplaceAllString(svgStr, fmt.Sprintf(`width="%d"`, size))
+			svgStr = heightRe.ReplaceAllString(svgStr, fmt.Sprintf(`height="%d"`, size))
+		}
+		if class != "" {
+			svgStr = strings.Replace(svgStr, `class="`, fmt.Sprintf(`class="%s `, class), 1)
+		}
+		return template.HTML(svgStr)
+	}
+	return template.HTML("")
 }
