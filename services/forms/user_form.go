28b1326642deeca79942e5125c92998a85aba75c
harryzcy <harry@harryzheng.com>
2022-11-09 01:49:43 -0500

    Update frontend to add the new scope

--- services/forms/user_form.go
+++ services/forms/user_form.go
@@ -371,33 +371,36 @@ func (f *AddKeyForm) Validate(req *http.Request, errs binding.Errors) binding.Er
 
 // NewAccessTokenForm form for creating access token
 type NewAccessTokenForm struct {
-	Name                string `binding:"Required;MaxSize(255)"`
-	ScopeRepo           bool
-	ScopeRepoStatus     bool
-	ScopePublicRepo     bool
-	ScopeAdminOrg       bool
-	ScopeWriteOrg       bool
-	ScopeReadOrg        bool
-	ScopeAdminPublicKey bool
-	ScopeWritePublicKey bool
-	ScopeReadPublicKey  bool
-	ScopeAdminRepoHook  bool
-	ScopeWriteRepoHook  bool
-	ScopeReadRepoHook   bool
-	ScopeNotification   bool
-	ScopeUser           bool
-	ScopeReadUser       bool
-	ScopeUserEmail      bool
-	ScopeUserFollow     bool
-	ScopeDeleteRepo     bool
-	ScopePackage        bool
-	ScopeWritePackage   bool
-	ScopeReadPackage    bool
-	ScopeDeletePackage  bool
-	ScopeAdminGPGKey    bool
-	ScopeWriteGPGKey    bool
-	ScopeReadGPGKey     bool
-	ScopeSudo           bool
+	Name                  string `binding:"Required;MaxSize(255)"`
+	ScopeRepo             bool
+	ScopeRepoStatus       bool
+	ScopePublicRepo       bool
+	ScopeAdminOrg         bool
+	ScopeWriteOrg         bool
+	ScopeReadOrg          bool
+	ScopeAdminPublicKey   bool
+	ScopeWritePublicKey   bool
+	ScopeReadPublicKey    bool
+	ScopeAdminRepoHook    bool
+	ScopeWriteRepoHook    bool
+	ScopeReadRepoHook     bool
+	ScopeNotification     bool
+	ScopeUser             bool
+	ScopeReadUser         bool
+	ScopeUserEmail        bool
+	ScopeUserFollow       bool
+	ScopeDeleteRepo       bool
+	ScopePackage          bool
+	ScopeWritePackage     bool
+	ScopeReadPackage      bool
+	ScopeDeletePackage    bool
+	ScopeAdminGPGKey      bool
+	ScopeWriteGPGKey      bool
+	ScopeReadGPGKey       bool
+	ScopeAdminApplication bool
+	ScopeWriteApplication bool
+	ScopeReadApplication  bool
+	ScopeSudo             bool
 }
 
 // Validate validates the fields
@@ -408,32 +411,35 @@ func (f *NewAccessTokenForm) Validate(req *http.Request, errs binding.Errors) bi
 
 func (f *NewAccessTokenForm) GetScope() auth_model.AccessTokenScope {
 	scopesMapping := map[string]string{
-		"Repo":           auth_model.AccessTokenScopeRepo,
-		"RepoStatus":     auth_model.AccessTokenScopeRepoStatus,
-		"PublicRepo":     auth_model.AccessTokenScopePublicRepo,
-		"AdminOrg":       auth_model.AccessTokenScopeAdminOrg,
-		"WriteOrg":       auth_model.AccessTokenScopeWriteOrg,
-		"ReadOrg":        auth_model.AccessTokenScopeReadOrg,
-		"AdminPublicKey": auth_model.AccessTokenScopeAdminPublicKey,
-		"WritePublicKey": auth_model.AccessTokenScopeWritePublicKey,
-		"ReadPublicKey":  auth_model.AccessTokenScopeReadPublicKey,
-		"AdminRepoHook":  auth_model.AccessTokenScopeAdminRepoHook,
-		"WriteRepoHook":  auth_model.AccessTokenScopeWriteRepoHook,
-		"ReadRepoHook":   auth_model.AccessTokenScopeReadRepoHook,
-		"Notification":   auth_model.AccessTokenScopeNotification,
-		"User":           auth_model.AccessTokenScopeUser,
-		"ReadUser":       auth_model.AccessTokenScopeReadUser,
-		"UserEmail":      auth_model.AccessTokenScopeUserEmail,
-		"UserFollow":     auth_model.AccessTokenScopeUserFollow,
-		"DeleteRepo":     auth_model.AccessTokenScopeDeleteRepo,
-		"Package":        auth_model.AccessTokenScopePackage,
-		"WritePackage":   auth_model.AccessTokenScopeWritePackage,
-		"ReadPackage":    auth_model.AccessTokenScopeReadPackage,
-		"DeletePackage":  auth_model.AccessTokenScopeDeletePackage,
-		"AdminGPGKey":    auth_model.AccessTokenScopeAdminGPGKey,
-		"WriteGPGKey":    auth_model.AccessTokenScopeWriteGPGKey,
-		"ReadGPGKey":     auth_model.AccessTokenScopeReadGPGKey,
-		"Sudo":           auth_model.AccessTokenScopeSudo,
+		"Repo":             auth_model.AccessTokenScopeRepo,
+		"RepoStatus":       auth_model.AccessTokenScopeRepoStatus,
+		"PublicRepo":       auth_model.AccessTokenScopePublicRepo,
+		"AdminOrg":         auth_model.AccessTokenScopeAdminOrg,
+		"WriteOrg":         auth_model.AccessTokenScopeWriteOrg,
+		"ReadOrg":          auth_model.AccessTokenScopeReadOrg,
+		"AdminPublicKey":   auth_model.AccessTokenScopeAdminPublicKey,
+		"WritePublicKey":   auth_model.AccessTokenScopeWritePublicKey,
+		"ReadPublicKey":    auth_model.AccessTokenScopeReadPublicKey,
+		"AdminRepoHook":    auth_model.AccessTokenScopeAdminRepoHook,
+		"WriteRepoHook":    auth_model.AccessTokenScopeWriteRepoHook,
+		"ReadRepoHook":     auth_model.AccessTokenScopeReadRepoHook,
+		"Notification":     auth_model.AccessTokenScopeNotification,
+		"User":             auth_model.AccessTokenScopeUser,
+		"ReadUser":         auth_model.AccessTokenScopeReadUser,
+		"UserEmail":        auth_model.AccessTokenScopeUserEmail,
+		"UserFollow":       auth_model.AccessTokenScopeUserFollow,
+		"DeleteRepo":       auth_model.AccessTokenScopeDeleteRepo,
+		"Package":          auth_model.AccessTokenScopePackage,
+		"WritePackage":     auth_model.AccessTokenScopeWritePackage,
+		"ReadPackage":      auth_model.AccessTokenScopeReadPackage,
+		"DeletePackage":    auth_model.AccessTokenScopeDeletePackage,
+		"AdminGPGKey":      auth_model.AccessTokenScopeAdminGPGKey,
+		"WriteGPGKey":      auth_model.AccessTokenScopeWriteGPGKey,
+		"ReadGPGKey":       auth_model.AccessTokenScopeReadGPGKey,
+		"AdminApplication": auth_model.AccessTokenScopeAdminApplication,
+		"WriteApplication": auth_model.AccessTokenScopeWriteApplication,
+		"ReadApplication":  auth_model.AccessTokenScopeReadApplication,
+		"Sudo":             auth_model.AccessTokenScopeSudo,
 	}
 
 	scope := ""
