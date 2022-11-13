9a831b9545b06db8e82e2220d1e96d66bf88f9b0
silverwind <me@silverwind.io>
2022-11-13 20:10:59 +0100

    fix TestCommitFromReader and rename file to commit_reader_test.go

--- modules/git/commit_reader_test.go
+++ modules/git/commit_reader_test.go
@@ -100,10 +100,8 @@ committer silverwind <me@silverwind.io> 1563741793 +0200
 empty commit`, commitFromReader.Signature.Payload)
 	assert.EqualValues(t, "silverwind <me@silverwind.io>", commitFromReader.Author.String())
 
-	commitFromReader2, err := CommitFromReader(gitRepo, sha, strings.NewReader(commitString+"\n\n"))
+	commitFromReader2, err := CommitFromReader(gitRepo, sha, strings.NewReader(commitString))
 	assert.NoError(t, err)
-	commitFromReader.CommitMessage += "\n\n"
-	commitFromReader.Signature.Payload += "\n\n"
 	assert.EqualValues(t, commitFromReader, commitFromReader2)
 }
 
