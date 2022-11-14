d8b401ab0699eaf3cd22091f6164b16ff55e01fe
Lunny Xiao <xiaolunwen@gmail.com>
2022-11-14 10:44:20 +0800

    Fix virutal user

--- models/bots/teabot.go
+++ models/bots/teabot.go
@@ -4,13 +4,24 @@
 
 package bots
 
-import user_model "code.gitea.io/gitea/models/user"
+import (
+	user_model "code.gitea.io/gitea/models/user"
+	"code.gitea.io/gitea/modules/structs"
+)
 
 // NewBotUser creates and returns a fake user for running the build.
 func NewBotUser() *user_model.User {
 	return &user_model.User{
-		ID:        -2,
-		Name:      "gitea-bots",
-		LowerName: "gitea-bots",
+		ID:                      -2,
+		Name:                    "gitea-bots",
+		LowerName:               "gitea-bots",
+		IsActive:                true,
+		FullName:                "Gitea Bots",
+		Email:                   "teabot@gitea.io",
+		KeepEmailPrivate:        true,
+		LoginName:               "gitea-bots",
+		Type:                    user_model.UserTypeIndividual,
+		AllowCreateOrganization: true,
+		Visibility:              structs.VisibleTypePublic,
 	}
 }
