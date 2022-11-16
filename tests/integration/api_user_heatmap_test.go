06b7eb601350e8951f01627b76881729c4ba5a02
flynnnnnnnnnn <flynnnnnnnnnn@github>
2022-11-16 22:34:32 +0000

    remove extra comment

--- tests/integration/api_user_heatmap_test.go
+++ tests/integration/api_user_heatmap_test.go
@@ -1,6 +1,5 @@
 // Copyright 2018 The Gitea Authors. All rights reserved.
 // SPDX-License-Identifier: MIT
-package models
 
 package integration
 
--- tests/integration/api_user_search_test.go
+++ tests/integration/api_user_search_test.go
@@ -1,6 +1,5 @@
 // Copyright 2019 The Gitea Authors. All rights reserved.
 // SPDX-License-Identifier: MIT
-package models
 
 package integration
 
