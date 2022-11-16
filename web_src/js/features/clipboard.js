914399bc759848fe5cccad1e59aa62908b63a63f
silverwind <me@silverwind.io>
2022-11-17 00:44:46 +0100

    Improvements for Content Copy
    It now supports copying Markdown, SVG and Images (not in Firefox
    currently because of lacking ClipboardItem support). It will fetch the
    data if in a rendered view or when it's an image.

--- web_src/js/features/clipboard.js
+++ web_src/js/features/clipboard.js
@@ -2,11 +2,16 @@ import {showTemporaryTooltip} from '../modules/tippy.js';
 
 const {copy_success, copy_error} = window.config.i18n;
 
-export async function copyToClipboard(text) {
-  try {
-    await navigator.clipboard.writeText(text);
-  } catch {
-    return fallbackCopyToClipboard(text);
+export async function copyToClipboard(content) {
+  if (typeof content === 'string') {
+    try {
+      await navigator.clipboard.writeText(content);
+    } catch {
+      return fallbackCopyToClipboard(content);
+    }
+  } else { // blob, this may throw in unsupporting browsers
+    const item = new window.ClipboardItem({[content.type]: content});
+    await navigator.clipboard.write([item]);
   }
   return true;
 }
---
+++ web_src/js/features/copycontent.js
@@ -0,0 +1,44 @@
+import {copyToClipboard} from './clipboard.js';
+import {showTemporaryTooltip} from '../modules/tippy.js';
+const {i18n} = window.config;
+
+export function initCopyContent() {
+  const btn = document.getElementById('copy-content');
+  if (!btn || btn.classList.contains('disabled') || btn.classList.contains('is-loading')) return;
+
+  btn.addEventListener('click', async () => {
+    let content;
+    const link = btn.getAttribute('data-link');
+
+    // when data-link is present, we perform a fetch. this is either because
+    // the text to copy is not in the DOM or it is an image which should be
+    // fetched to copy in full resolution
+    if (link) {
+      btn.classList.add('is-loading');
+      try {
+        const res = await fetch(link, {credentials: 'include', redirect: 'follow'});
+        const contentType = res.headers.get('content-type');
+
+        if (contentType.startsWith('image/') && !contentType.startsWith('image/svg')) {
+          content = await res.blob();
+        } else {
+          content = await res.text();
+        }
+      } catch {
+        return showTemporaryTooltip(btn, i18n.copy_error);
+      } finally {
+        btn.classList.remove('is-loading');
+      }
+    } else { // text, copy from DOM
+      const lineEls = document.querySelectorAll('.file-view .lines-code');
+      content = Array.from(lineEls).map((el) => el.textContent).join('');
+    }
+
+    try {
+      const success = await copyToClipboard(content);
+      showTemporaryTooltip(btn, success ? i18n.copy_success : i18n.copy_error);
+    } catch {
+      showTemporaryTooltip(btn, i18n.copy_error);
+    }
+  });
+}
