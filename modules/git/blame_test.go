753de06a03624b306b5f264f5a8fd632502f4427
flynnnnnnnnnn <flynnnnnnnnnn@github>
2022-11-16 21:59:53 +0000

    Implement FSFE REUSE for golang files

--- modules/git/blame_test.go
+++ modules/git/blame_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 The Gitea Authors. All rights reserved.
-// Use of this source code is governed by a MIT-style
-// license that can be found in the LICENSE file.
+// SPDX-License-Identifier: MIT
+
 
 package git
 
@@ -51,7 +51,7 @@ committer-tz -0500
 summary Add code of delete user
 previous be0ba9ea88aff8a658d0495d36accf944b74888d gogs.go
 filename gogs.go
-	// Use of this source code is governed by a MIT-style
+	// SPDX-License-Identifier: MIT
 4b92a6c2df28054ad766bc262f308db9f6066596 3 4
 author Unknown
 author-mail <joe2010xtmf@163.com>
@@ -64,7 +64,7 @@ committer-tz -0500
 summary Add code of delete user
 previous be0ba9ea88aff8a658d0495d36accf944b74888d gogs.go
 filename gogs.go
-	// license that can be found in the LICENSE file.
+	
 	
 e2aa991e10ffd924a828ec149951f2f20eecead2 6 6 2
 author Lunny Xiao
@@ -119,8 +119,8 @@ func TestReadingBlameOutput(t *testing.T) {
 		{
 			"4b92a6c2df28054ad766bc262f308db9f6066596",
 			[]string{
-				"// Use of this source code is governed by a MIT-style",
-				"// license that can be found in the LICENSE file.",
+				"// SPDX-License-Identifier: MIT",
+				"",
 				"",
 			},
 		},
