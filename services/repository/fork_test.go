fc21dbbe16ae79c5724febd9ee2789d02639698c
Xinyu Zhou <i@sourcehut.net>
2022-11-17 22:08:17 +0800

    Add test
    Signed-off-by: Xinyu Zhou <i@sourcehut.net>

--- services/repository/fork_test.go
+++ services/repository/fork_test.go
@@ -21,7 +21,6 @@ func TestForkRepository(t *testing.T) {
 	// user 13 has already forked repo10
 	user := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: 13})
 	repo := unittest.AssertExistsAndLoadBean(t, &repo_model.Repository{ID: 10})
-
 	fork, err := ForkRepository(git.DefaultContext, user, user, ForkRepoOptions{
 		BaseRepo:    repo,
 		Name:        "test",
@@ -30,4 +29,16 @@ func TestForkRepository(t *testing.T) {
 	assert.Nil(t, fork)
 	assert.Error(t, err)
 	assert.True(t, IsErrForkAlreadyExist(err))
+
+	// user not reached maximum limit of repositories
+	assert.False(t, repo_model.IsErrReachLimitOfRepo(err))
+	// user has reached maximum limit of repositories
+	user.MaxRepoCreation = 0
+	fork2, err := ForkRepository(git.DefaultContext, user, user, ForkRepoOptions{
+		BaseRepo:    repo,
+		Name:        "test",
+		Description: "test",
+	})
+	assert.Nil(t, fork2)
+	assert.True(t, repo_model.IsErrReachLimitOfRepo(err))
 }
