e6d2846bed3931622977eb255067c94f5769ed9a
silverwind <me@silverwind.io>
2022-11-24 20:21:03 +0100

    Fix markdown anchor re-clicking
    The hashchange event did not fire on re-click of a active anchor.
    Instead, use the click event.
    
    Fixes: https://github.com/go-gitea/gitea/issues/21680

--- web_src/js/markup/anchors.js
+++ web_src/js/markup/anchors.js
@@ -2,10 +2,10 @@ import {svg} from '../svg.js';
 
 const headingSelector = '.markup h1, .markup h2, .markup h3, .markup h4, .markup h5, .markup h6';
 
-function scrollToAnchor() {
-  if (document.querySelector(':target')) return;
-  if (!window.location.hash || window.location.hash.length <= 1) return;
-  const id = decodeURIComponent(window.location.hash.substring(1));
+function scrollToAnchor(hash) {
+  if (document.querySelector(':target')) return; // something else on the page matches
+  if (hash?.length <= 1) return;
+  const id = decodeURIComponent(hash.substring(1));
   const el = document.getElementById(`user-content-${id}`);
   if (el) {
     el.scrollIntoView();
@@ -24,9 +24,11 @@ export function initMarkupAnchors() {
     a.classList.add('anchor');
     a.setAttribute('href', `#${encodeURIComponent(originalId)}`);
     a.innerHTML = svg('octicon-link');
+    a.addEventListener('click', (e) => {
+      scrollToAnchor(e.currentTarget.getAttribute('href'));
+    });
     heading.prepend(a);
   }
 
-  scrollToAnchor();
-  window.addEventListener('hashchange', scrollToAnchor);
+  scrollToAnchor(window.location.hash);
 }
