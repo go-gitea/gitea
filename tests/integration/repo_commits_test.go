d0408c4ecd8ec3cf5ebbd474cc79c3dcdaf9cb05
Lunny Xiao <xiaolunwen@gmail.com>
2022-11-25 00:08:05 +0800

    ignore sqlite3

--- tests/integration/repo_commits_test.go
+++ tests/integration/repo_commits_test.go
@@ -53,17 +53,19 @@ func doTestRepoCommitWithStatus(t *testing.T, state string, classes ...string) {
 	// Call API to add status for commit
 	t.Run("CreateStatus", doAPICreateCommitStatus(NewAPITestContext(t, "user2", "repo1"), path.Base(commitURL), api.CommitStatusState(state)))
 
-	runBody := doAPICreateCommitStatus(NewAPITestContext(t, "user2", "repo1"), path.Base(commitURL), api.CommitStatusState(state))
-	var wg sync.WaitGroup
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		t.Run(fmt.Sprintf("ParallelCreateStatus_%d", i), func(t *testing.T) {
-			t.Parallel()
-			runBody(t)
-			wg.Done()
-		})
+	if !setting.Database.UseSQLite3 {
+		runBody := doAPICreateCommitStatus(NewAPITestContext(t, "user2", "repo1"), path.Base(commitURL), api.CommitStatusState(state))
+		var wg sync.WaitGroup
+		for i := 0; i < 10; i++ {
+			wg.Add(1)
+			t.Run(fmt.Sprintf("ParallelCreateStatus_%d", i), func(t *testing.T) {
+				t.Parallel()
+				runBody(t)
+				wg.Done()
+			})
+		}
+		wg.Wait()
 	}
-	wg.Wait()
 
 	req = NewRequest(t, "GET", "/user2/repo1/commits/branch/master")
 	resp = session.MakeRequest(t, req, http.StatusOK)
