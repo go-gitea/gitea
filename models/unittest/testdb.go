dccddd045ef034fda0e076adaacafcf7581045b8
Jason Song <i@wolfogre.com>
2022-11-28 17:19:15 +0800

    test: init bot storage path

--- models/unittest/testdb.go
+++ models/unittest/testdb.go
@@ -105,6 +105,8 @@ func MainTest(m *testing.M, testOpts *TestOptions) {
 
 	setting.Packages.Storage.Path = filepath.Join(setting.AppDataPath, "packages")
 
+	setting.Bots.Storage.Path = filepath.Join(setting.AppDataPath, "bots_log")
+
 	setting.Git.HomePath = filepath.Join(setting.AppDataPath, "home")
 
 	if err = storage.Init(); err != nil {
