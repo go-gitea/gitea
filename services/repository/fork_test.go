2bb727d2b71924b67b5a771ff39c155f57236225
Xinyu Zhou <i@sourcehut.net>
2022-12-24 10:03:45 +0800

    Update services/repository/fork_test.go
    Signed-off-by: Xinyu Zhou <i@sourcehut.net>

--- services/repository/fork_test.go
+++ services/repository/fork_test.go
@@ -10,6 +10,7 @@ import (
 	"code.gitea.io/gitea/models/unittest"
 	user_model "code.gitea.io/gitea/models/user"
 	"code.gitea.io/gitea/modules/git"
+	"code.gitea.io/gitea/modules/setting"
 
 	"github.com/stretchr/testify/assert"
 )
