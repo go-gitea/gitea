f4edc75be3a6337f889ef42fdfb33e1fe2536cc0
Lunny Xiao <xiaolunwen@gmail.com>
2022-12-28 13:53:28 +0800

    refactor auth interface to return error when verify failure (#22119)
    This PR changed the Auth interface signature from 
    `Verify(http *http.Request, w http.ResponseWriter, store DataStore, sess
    SessionStore) *user_model.User`
    to 
    `Verify(http *http.Request, w http.ResponseWriter, store DataStore, sess
    SessionStore) (*user_model.User, error)`.
    
    There is a new return argument `error` which means the verification
    condition matched but verify process failed, we should stop the auth
    process.
    
    Before this PR, when return a `nil` user, we don't know the reason why
    it returned `nil`. If the match condition is not satisfied or it
    verified failure? For these two different results, we should have
    different handler. If the match condition is not satisfied, we should
    try next auth method and if there is no more auth method, it's an
    anonymous user. If the condition matched but verify failed, the auth
    process should be stop and return immediately.
    
    This will fix #20563
    
    Co-authored-by: KN4CK3R <admin@oldschoolhack.me>
    Co-authored-by: Jason Song <i@wolfogre.com>

--- routers/api/packages/api.go
+++ routers/api/packages/api.go
@@ -12,6 +12,7 @@ import (
 
 	"code.gitea.io/gitea/models/perm"
 	"code.gitea.io/gitea/modules/context"
+	"code.gitea.io/gitea/modules/log"
 	"code.gitea.io/gitea/modules/setting"
 	"code.gitea.io/gitea/modules/web"
 	"code.gitea.io/gitea/routers/api/packages/composer"
@@ -57,7 +58,13 @@ func Routes(ctx gocontext.Context) *web.Route {
 
 	authGroup := auth.NewGroup(authMethods...)
 	r.Use(func(ctx *context.Context) {
-		ctx.Doer = authGroup.Verify(ctx.Req, ctx.Resp, ctx, ctx.Session)
+		var err error
+		ctx.Doer, err = authGroup.Verify(ctx.Req, ctx.Resp, ctx, ctx.Session)
+		if err != nil {
+			log.Error("Verify: %v", err)
+			ctx.Error(http.StatusUnauthorized, "authGroup.Verify")
+			return
+		}
 		ctx.IsSigned = ctx.Doer != nil
 	})
 
@@ -317,7 +324,13 @@ func ContainerRoutes(ctx gocontext.Context) *web.Route {
 
 	authGroup := auth.NewGroup(authMethods...)
 	r.Use(func(ctx *context.Context) {
-		ctx.Doer = authGroup.Verify(ctx.Req, ctx.Resp, ctx, ctx.Session)
+		var err error
+		ctx.Doer, err = authGroup.Verify(ctx.Req, ctx.Resp, ctx, ctx.Session)
+		if err != nil {
+			log.Error("Failed to verify user: %v", err)
+			ctx.Error(http.StatusUnauthorized, "Verify")
+			return
+		}
 		ctx.IsSigned = ctx.Doer != nil
 	})
 
