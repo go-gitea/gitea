8f3f1cef85186e84b237615f2dd1d698c1433bcf
Jason Song <i@wolfogre.com>
2023-01-09 16:47:54 +0800

    repalce: IsSliceInt64Eq

--- modules/util/compare.go
+++ modules/util/compare.go
@@ -4,31 +4,8 @@
 package util
 
 import (
-	"sort"
 	"strings"
 )
-
-// Int64Slice attaches the methods of Interface to []int64, sorting in increasing order.
-type Int64Slice []int64
-
-func (p Int64Slice) Len() int           { return len(p) }
-func (p Int64Slice) Less(i, j int) bool { return p[i] < p[j] }
-func (p Int64Slice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
-
-// IsSliceInt64Eq returns if the two slice has the same elements but different sequences.
-func IsSliceInt64Eq(a, b []int64) bool {
-	if len(a) != len(b) {
-		return false
-	}
-	sort.Sort(Int64Slice(a))
-	sort.Sort(Int64Slice(b))
-	for i := 0; i < len(a); i++ {
-		if a[i] != b[i] {
-			return false
-		}
-	}
-	return true
-}
 
 // IsInSlice returns true if the target exists in the slice.
 func IsInSlice[T comparable](target T, slice []T) bool {
