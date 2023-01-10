go-gitea/gitea

commit 31f934c1d8005cdd87c47ce104d00c0efaec374b

Add filetree on left of diff view (#21012)
This PR adds a filetree to the left side of the files/diff view.

Initially the filetree will not be shown and may be shown via a new
"Show file tree" button.

Showing and hiding is using the same icon as github. Folders are
collapsible. On small devices (max-width 991 PX) the file tree will be
hidden.

Close #18192

Co-authored-by: wxiaoguang <wxiaoguang@gmail.com>

web_src/js/components/DiffFileTree.vue
@@ -1,0 +1,129 @@
+<template>
+  <div
+    v-show="fileTreeIsVisible"
+    id="diff-file-tree"
+    class="mr-3 mt-3 diff-detail-box"
+  >
+    <!-- only render the tree if we're visible. in many cases this is something that doesn't change very often -->
+    <div class="ui list" v-if="fileTreeIsVisible">
+      <DiffFileTreeItem v-for="item in fileTree" :key="item.name" :item="item" />
+    </div>
+    <div v-if="isIncomplete" id="diff-too-many-files-stats" class="pt-2">
+      <span>{{ tooManyFilesMessage }}</span><a :class="['ui', 'basic', 'tiny', 'button', isLoadingNewData === true ? 'disabled' : '']" id="diff-show-more-files-stats" @click.stop="loadMoreData">{{ showMoreMessage }}</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import DiffFileTreeItem from './DiffFileTreeItem.vue';
+import {doLoadMoreFiles} from '../features/repo-diff.js';
+
+const {pageData} = window.config;
+const LOCAL_STORAGE_KEY = 'diff_file_tree_visible';
+
+export default {
+  name: 'DiffFileTree',
+  components: {DiffFileTreeItem},
+
+  data: () => {
+    const fileTreeIsVisible = localStorage.getItem(LOCAL_STORAGE_KEY) === 'true';
+    pageData.diffFileInfo.fileTreeIsVisible = fileTreeIsVisible;
+    return pageData.diffFileInfo;
+  },
+
+  computed: {
+    fileTree() {
+      const result = [];
+      for (const file of this.files) {
+        // Split file into directories
+        const splits = file.Name.split('/');
+        let index = 0;
+        let parent = null;
+        let isFile = false;
+        for (const split of splits) {
+          index += 1;
+          // reached the end
+          if (index === splits.length) {
+            isFile = true;
+          }
+          let newParent = {
+            name: split,
+            children: [],
+            isFile
+          };
+
+          if (isFile === true) {
+            newParent.file = file;
+          }
+
+          if (parent) {
+            // check if the folder already exists
+            const existingFolder = parent.children.find(
+              (x) => x.name === split
+            );
+            if (existingFolder) {
+              newParent = existingFolder;
+            } else {
+              parent.children.push(newParent);
+            }
+          } else {
+            const existingFolder = result.find((x) => x.name === split);
+            if (existingFolder) {
+              newParent = existingFolder;
+            } else {
+              result.push(newParent);
+            }
+          }
+          parent = newParent;
+        }
+      }
+      const mergeChildIfOnlyOneDir = (entries) => {
+        for (const entry of entries) {
+          if (entry.children) {
+            mergeChildIfOnlyOneDir(entry.children);
+          }
+          if (entry.children.length === 1 && entry.children[0].isFile === false) {
+            // Merge it to the parent
+            entry.name = `${entry.name}/${entry.children[0].name}`;
+            entry.children = entry.children[0].children;
+          }
+        }
+      };
+      // Merge folders with just a folder as children in order to
+      // reduce the depth of our tree.
+      mergeChildIfOnlyOneDir(result);
+      return result;
+    }
+  },
+
+  mounted() {
+    // ensure correct buttons when we are mounted to the dom
+    this.adjustToggleButton(this.fileTreeIsVisible);
+    document.querySelector('.diff-toggle-file-tree-button').addEventListener('click', this.toggleVisibility);
+  },
+  unmounted() {
+    document.querySelector('.diff-toggle-file-tree-button').removeEventListener('click', this.toggleVisibility);
+  },
+  methods: {
+    toggleVisibility() {
+      this.updateVisibility(!this.fileTreeIsVisible);
+    },
+    updateVisibility(visible) {
+      this.fileTreeIsVisible = visible;
+      localStorage.setItem(LOCAL_STORAGE_KEY, this.fileTreeIsVisible);
+      this.adjustToggleButton(this.fileTreeIsVisible);
+    },
+    adjustToggleButton(visible) {
+      const [toShow, toHide] = document.querySelectorAll('.diff-toggle-file-tree-button .icon');
+      toShow.classList.toggle('hide', visible);  // hide the toShow icon if the tree is visible
+      toHide.classList.toggle('hide', !visible); // similarly
+    },
+    loadMoreData() {
+      this.isLoadingNewData = true;
+      doLoadMoreFiles(this.link, this.diffEnd, () => {
+        this.isLoadingNewData = false;
+      });
+    }
+  },
+};
+</script>

web_src/js/features/repo-diff.js
@@ -68,7 +68,6 @@ export function initRepoDiffConversationForm() {
     initCompReactionSelector($newConversationHolder);
   });
 
-
   $(document).on('click', '.resolve-conversation', async function (e) {
     e.preventDefault();
     const comment_id = $(this).data('comment-id');
@@ -118,32 +117,27 @@ function onShowMoreFiles() {
   countAndUpdateViewedFiles();
 }
 
-export function initRepoDiffShowMore() {
-  $('#diff-files, #diff-file-boxes').on('click', '#diff-show-more-files, #diff-show-more-files-stats', (e) => {
-    e.preventDefault();
-
-    if ($(e.target).hasClass('disabled')) {
+export function doLoadMoreFiles(link, diffEnd, callback) {
+  const url = `${link}?skip-to=${diffEnd}&file-only=true`;
+  $.ajax({
+    type: 'GET',
+    url,
+  }).done((resp) => {
+    if (!resp) {
+      callback(resp);
       return;
     }
-    $('#diff-show-more-files, #diff-show-more-files-stats').addClass('disabled');
-
-    const url = $('#diff-show-more-files, #diff-show-more-files-stats').data('href');
-    $.ajax({
-      type: 'GET',
-      url,
-    }).done((resp) => {
-      if (!resp) {
-        $('#diff-show-more-files, #diff-show-more-files-stats').removeClass('disabled');
-        return;
-      }
-      $('#diff-too-many-files-stats').remove();
-      $('#diff-files').append($(resp).find('#diff-files li'));
-      $('#diff-incomplete').replaceWith($(resp).find('#diff-file-boxes').children());
-      onShowMoreFiles();
-    }).fail(() => {
-      $('#diff-show-more-files, #diff-show-more-files-stats').removeClass('disabled');
-    });
+    // By simply rerunning the script we add the new data to our existing
+    // pagedata object. this triggers vue and the filetree and filelist will
+    // render the new elements.
+    $('body').append($(resp).find('script#diff-data-script'));
+    callback(resp);
+  }).fail(() => {
+    callback();
   });
+}
+
+export function initRepoDiffShowMore() {
   $(document).on('click', 'a.diff-show-more-button', (e) => {
     e.preventDefault();
     const $target = $(e.target);
@@ -163,7 +157,6 @@ export function initRepoDiffShowMore() {
         $target.removeClass('disabled');
         return;
       }
-
       $target.parent().replaceWith($(resp).find('#diff-file-boxes .diff-file-body .file-body').children());
       onShowMoreFiles();
     }).fail(() => {

templates/repo/commit_page.tmpl
@@ -1,7 +1,7 @@
 {{template "base/head" .}}
 <div class="page-content repository diff">
 	{{template "repo/header" .}}
-	<div class="ui container {{if .IsSplitStyle}}fluid padded{{end}}">
+	<div class="ui container fluid padded">
 		{{$class := ""}}
 		{{if .Commit.Signature}}
 			{{$class = (printf "%s%s" $class " isSigned")}}

templates/repo/diff/box.tmpl
@@ -14,6 +14,11 @@
 {{else}}
 	<div>
 		<div class="diff-detail-box diff-box sticky df sb ac fw">
+			<a class="diff-toggle-file-tree-button">
+				{{/* the icon meaning is reversed here, "octicon-sidebar-collapse" means show the file tree */}}
+				{{svg "octicon-sidebar-collapse" 16 "icon hide"}}
+				{{svg "octicon-sidebar-expand" 16 "icon"}}
+			</a>
 			<div class="diff-detail-stats df ac">
 				{{svg "octicon-diff" 16 "mr-2"}}{{.locale.Tr "repo.diff.stats_desc" .Diff.NumFiles .Diff.TotalAddition .Diff.TotalDeletion | Str2html}}
 			</div>
@@ -31,145 +36,151 @@
 				{{end}}
 			</div>
 		</div>
-		<ol class="diff-detail-box diff-stats m-0 hide" id="diff-files">
-			{{range .Diff.Files}}
-				<li>
-					<div class="bold df ac pull-right">
-						{{if .IsBin}}
-							<span class="ml-1 mr-3">
-								{{$.locale.Tr "repo.diff.bin"}}
-							</span>
-						{{else}}
-							{{template "repo/diff/stats" dict "file" . "root" $}}
-						{{end}}
-					</div>
-					<!-- todo finish all file status, now modify, add, delete and rename -->
-					<span class="status {{DiffTypeToStr .GetType}} tooltip" data-content="{{DiffTypeToStr .GetType}}" data-position="right center">&nbsp;</span>
-					<a class="file mono" href="#diff-{{.NameHash}}">{{.Name}}</a>
-				</li>
-			{{end}}
-			{{if .Diff.IsIncomplete}}
-				<li id="diff-too-many-files-stats" class="pt-2">
-					<span class="file df ac sb">{{$.locale.Tr "repo.diff.too_many_files"}}
-						<a class="ui basic tiny button" id="diff-show-more-files-stats" data-href="{{$.Link}}?skip-to={{.Diff.End}}&file-only=true">{{.locale.Tr "repo.diff.show_more"}}</a>
-					</span>
-				</li>
-			{{end}}
-		</ol>
-		<div id="diff-file-boxes">
-			{{range $file := .Diff.Files}}
-				{{/*notice: the index of Diff.Files should not be used for element ID, because the index will be restarted from 0 when doing load-more for PRs with a lot of files*/}}
-				{{$blobBase := call $.GetBlobByPathForCommit $.BaseCommit $file.OldName}}
-				{{$blobHead := call $.GetBlobByPathForCommit $.HeadCommit $file.Name}}
-				{{$isImage := or (call $.IsBlobAnImage $blobBase) (call $.IsBlobAnImage $blobHead)}}
-				{{$isCsv := (call $.IsCsvFile $file)}}
-				{{$showFileViewToggle := or $isImage (and (not $file.IsIncomplete) $isCsv)}}
-				<div class="diff-file-box diff-box file-content {{TabSizeClass $.Editorconfig $file.Name}} mt-3" id="diff-{{$file.NameHash}}" data-old-filename="{{$file.OldName}}" data-new-filename="{{$file.Name}}" {{if $file.ShouldBeHidden}}data-folded="true"{{end}}>
-					<h4 class="diff-file-header sticky-2nd-row ui top attached normal header df ac sb">
-						<div class="df ac">
-							<a role="button" class="fold-file muted mr-2">
-								{{if $file.ShouldBeHidden}}
-									{{svg "octicon-chevron-right" 18}}
-								{{else}}
-									{{svg "octicon-chevron-down" 18}}
-								{{end}}
-							</a>
-							<div class="bold df ac">
-								{{if $file.IsBin}}
-									<span class="ml-1 mr-3">
-										{{$.locale.Tr "repo.diff.bin"}}
-									</span>
-								{{else}}
-									{{template "repo/diff/stats" dict "file" . "root" $}}
-								{{end}}
-							</div>
-							<span class="file mono"><a class="muted" href="#diff-{{$file.NameHash}}">{{if $file.IsRenamed}}{{$file.OldName}} &rarr; {{end}}{{$file.Name}}</a>{{if .IsLFSFile}} ({{$.locale.Tr "repo.stored_lfs"}}){{end}}</span>
-							{{if $file.IsGenerated}}
-								<span class="ui label ml-3">{{$.locale.Tr "repo.diff.generated"}}</span>
-							{{end}}
-							{{if $file.IsVendored}}
-								<span class="ui label ml-3">{{$.locale.Tr "repo.diff.vendored"}}</span>
-							{{end}}
-						</div>
-						<div class="diff-file-header-actions df ac">
-							{{if $showFileViewToggle}}
-								<div class="ui compact icon buttons">
-									<span class="ui tiny basic button tooltip file-view-toggle" data-toggle-selector="#diff-source-{{$file.NameHash}}" data-content="{{$.locale.Tr "repo.file_view_source"}}" data-position="bottom center">{{svg "octicon-code"}}</span>
-									<span class="ui tiny basic button tooltip file-view-toggle active" data-toggle-selector="#diff-rendered-{{$file.NameHash}}" data-content="{{$.locale.Tr "repo.file_view_rendered"}}" data-position="bottom center">{{svg "octicon-file"}}</span>
-								</div>
-							{{end}}
-							{{if $file.IsProtected}}
-								<span class="ui basic label">{{$.locale.Tr "repo.diff.protected"}}</span>
-							{{end}}
-							{{if not (or $file.IsIncomplete $file.IsBin $file.IsSubmodule)}}
-								<a class="ui basic tiny button unescape-button">{{$.locale.Tr "repo.unescape_control_characters"}}</a>
-								<a class="ui basic tiny button escape-button" style="display: none;">{{$.locale.Tr "repo.escape_control_characters"}}</a>
-							{{end}}
-							{{if and (not $file.IsSubmodule) (not $.PageIsWiki)}}
-								{{if $file.IsDeleted}}
-									<a class="ui basic tiny button" rel="nofollow" href="{{$.BeforeSourcePath}}/{{PathEscapeSegments .Name}}">{{$.locale.Tr "repo.diff.view_file"}}</a>
-								{{else}}
-									<a class="ui basic tiny button" rel="nofollow" href="{{$.SourcePath}}/{{PathEscapeSegments .Name}}">{{$.locale.Tr "repo.diff.view_file"}}</a>
-								{{end}}
-							{{end}}
-							{{if and $.IsSigned $.PageIsPullFiles (not $.IsArchived)}}
-								{{if $file.HasChangedSinceLastReview}}
-									<span class="changed-since-last-review unselectable">{{$.locale.Tr "repo.pulls.has_changed_since_last_review"}}</span>
-								{{end}}
-								<label data-link="{{$.Issue.Link}}/viewed-files" data-headcommit="{{$.PullHeadCommitID}}" class="viewed-file-form unselectable{{if $file.IsViewed}} viewed-file-checked-form{{end}}">
-									<input type="checkbox" name="{{$file.GetDiffFileName}}" autocomplete="off"{{if $file.IsViewed}} checked{{end}}> {{$.locale.Tr "repo.pulls.has_viewed_file"}}
-								</label>
-							{{end}}
-						</div>
-					</h4>
-					<div class="diff-file-body ui attached unstackable table segment" {{if $file.IsViewed}}data-folded="true"{{end}}>
-						<div id="diff-source-{{$file.NameHash}}" class="file-body file-code unicode-escaped code-diff{{if $.IsSplitStyle}} code-diff-split{{else}} code-diff-unified{{end}}{{if $showFileViewToggle}} hide{{end}}">
-							{{if or $file.IsIncomplete $file.IsBin}}
-								<div class="diff-file-body binary" style="padding: 5px 10px;">
-									{{if $file.IsIncomplete}}
-										{{if $file.IsIncompleteLineTooLong}}
-											{{$.locale.Tr "repo.diff.file_suppressed_line_too_long"}}
+		<script id="diff-data-script">
+				(() => {
+					const diffData = {
+						files: [{{range $i, $file := .Diff.Files}}{Name:"{{$file.Name}}",NameHash:"{{$file.NameHash}}",Type:{{$file.Type}},IsBin:{{$file.IsBin}},Addition:{{$file.Addition}},Deletion:{{$file.Deletion}}},{{end}}],
+						isIncomplete: {{.Diff.IsIncomplete}},
+						tooManyFilesMessage: "{{$.locale.Tr "repo.diff.too_many_files"}}",
+						binaryFileMessage: "{{$.locale.Tr "repo.diff.bin"}}",
+						showMoreMessage: "{{.locale.Tr "repo.diff.show_more"}}",
+						statisticsMessage: "{{.locale.Tr "repo.diff.stats_desc_file"}}",
+						fileTreeIsVisible: false,
+						fileListIsVisible: false,
+						isLoadingNewData: false,
+						diffEnd: {{.Diff.End}},
+						link: "{{$.Link}}"
+					};
+					if(window.config.pageData.diffFileInfo) {
+						// Page is already loaded - add the data to our existing data
+						window.config.pageData.diffFileInfo.files.push(...diffData.files);
+						window.config.pageData.diffFileInfo.isIncomplete = diffData.isIncomplete;
+						window.config.pageData.diffFileInfo.diffEnd = diffData.diffEnd;
+						window.config.pageData.diffFileInfo.link = diffData.link;
+					} else {
+						// new load of page - populate initial data
+						window.config.pageData.diffFileInfo = diffData;
+					}
+				})();
+				</script>
+		<div id="diff-file-list-container"></div>
+		<div id="diff-container">
+				<div id="diff-file-tree-container"></div>
+				<div id="diff-file-boxes" class="sixteen wide column">
+					{{range $i, $file := .Diff.Files}}
+						{{/*notice: the index of Diff.Files should not be used for element ID, because the index will be restarted from 0 when doing load-more for PRs with a lot of files*/}}
+						{{$blobBase := call $.GetBlobByPathForCommit $.BaseCommit $file.OldName}}
+						{{$blobHead := call $.GetBlobByPathForCommit $.HeadCommit $file.Name}}
+						{{$isImage := or (call $.IsBlobAnImage $blobBase) (call $.IsBlobAnImage $blobHead)}}
+						{{$isCsv := (call $.IsCsvFile $file)}}
+						{{$showFileViewToggle := or $isImage (and (not $file.IsIncomplete) $isCsv)}}
+						<div class="diff-file-box diff-box file-content {{TabSizeClass $.Editorconfig $file.Name}} mt-3" id="diff-{{$file.NameHash}}" data-old-filename="{{$file.OldName}}" data-new-filename="{{$file.Name}}" {{if $file.ShouldBeHidden}}data-folded="true"{{end}}>
+							<h4 class="diff-file-header sticky-2nd-row ui top attached normal header df ac sb">
+								<div class="df ac">
+									<a role="button" class="fold-file muted mr-2">
+										{{if $file.ShouldBeHidden}}
+											{{svg "octicon-chevron-right" 18}}
 										{{else}}
-											{{$.locale.Tr "repo.diff.file_suppressed"}}
-											<a class="ui basic tiny button diff-show-more-button" data-href="{{$.Link}}?file-only=true&files={{$file.Name}}&files={{$file.OldName}}">{{$.locale.Tr "repo.diff.load"}}</a>
+											{{svg "octicon-chevron-down" 18}}
 										{{end}}
-									{{else}}
-										{{$.locale.Tr "repo.diff.bin_not_shown"}}
+									</a>
+									<div class="bold df ac">
+										{{if $file.IsBin}}
+											<span class="ml-1 mr-3">
+												{{$.locale.Tr "repo.diff.bin"}}
+											</span>
+										{{else}}
+											{{template "repo/diff/stats" dict "file" . "root" $}}
+										{{end}}
+									</div>
+									<span class="file mono"><a class="muted" href="#diff-{{$file.NameHash}}">{{if $file.IsRenamed}}{{$file.OldName}} &rarr; {{end}}{{$file.Name}}</a>{{if .IsLFSFile}} ({{$.locale.Tr "repo.stored_lfs"}}){{end}}</span>
+									{{if $file.IsGenerated}}
+										<span class="ui label ml-3">{{$.locale.Tr "repo.diff.generated"}}</span>
+									{{end}}
+									{{if $file.IsVendored}}
+										<span class="ui label ml-3">{{$.locale.Tr "repo.diff.vendored"}}</span>
 									{{end}}
 								</div>
-							{{else}}
-								<table class="chroma" data-new-comment-url="{{$.Issue.HTMLURL}}/files/reviews/new_comment" data-path="{{$file.Name}}">
-									{{if $.IsSplitStyle}}
-										{{template "repo/diff/section_split" dict "file" . "root" $}}
-									{{else}}
-										{{template "repo/diff/section_unified" dict "file" . "root" $}}
+								<div class="diff-file-header-actions df ac">
+									{{if $showFileViewToggle}}
+										<div class="ui compact icon buttons">
+											<span class="ui tiny basic button tooltip file-view-toggle" data-toggle-selector="#diff-source-{{$file.NameHash}}" data-content="{{$.locale.Tr "repo.file_view_source"}}" data-position="bottom center">{{svg "octicon-code"}}</span>
+											<span class="ui tiny basic button tooltip file-view-toggle active" data-toggle-selector="#diff-rendered-{{$file.NameHash}}" data-content="{{$.locale.Tr "repo.file_view_rendered"}}" data-position="bottom center">{{svg "octicon-file"}}</span>
+										</div>
 									{{end}}
-								</table>
-							{{end}}
-						</div>
-						{{if $showFileViewToggle}}
-							<div id="diff-rendered-{{$file.NameHash}}" class="file-body file-code {{if $.IsSplitStyle}} code-diff-split{{else}} code-diff-unified{{end}}">
-								<table class="chroma w-100">
-									{{if $isImage}}
-										{{template "repo/diff/image_diff" dict "file" . "root" $ "blobBase" $blobBase "blobHead" $blobHead}}
-									{{else}}
-										{{template "repo/diff/csv_diff" dict "file" . "root" $ "blobBase" $blobBase "blobHead" $blobHead}}
+									{{if $file.IsProtected}}
+										<span class="ui basic label">{{$.locale.Tr "repo.diff.protected"}}</span>
 									{{end}}
-								</table>
+									{{if not (or $file.IsIncomplete $file.IsBin $file.IsSubmodule)}}
+										<a class="ui basic tiny button unescape-button">{{$.locale.Tr "repo.unescape_control_characters"}}</a>
+										<a class="ui basic tiny button escape-button" style="display: none;">{{$.locale.Tr "repo.escape_control_characters"}}</a>
+									{{end}}
+									{{if and (not $file.IsSubmodule) (not $.PageIsWiki)}}
+										{{if $file.IsDeleted}}
+											<a class="ui basic tiny button" rel="nofollow" href="{{$.BeforeSourcePath}}/{{PathEscapeSegments .Name}}">{{$.locale.Tr "repo.diff.view_file"}}</a>
+										{{else}}
+											<a class="ui basic tiny button" rel="nofollow" href="{{$.SourcePath}}/{{PathEscapeSegments .Name}}">{{$.locale.Tr "repo.diff.view_file"}}</a>
+										{{end}}
+									{{end}}
+									{{if and $.IsSigned $.PageIsPullFiles (not $.IsArchived)}}
+										{{if $file.HasChangedSinceLastReview}}
+											<span class="changed-since-last-review unselectable">{{$.locale.Tr "repo.pulls.has_changed_since_last_review"}}</span>
+										{{end}}
+										<label data-link="{{$.Issue.Link}}/viewed-files" data-headcommit="{{$.PullHeadCommitID}}" class="viewed-file-form unselectable{{if $file.IsViewed}} viewed-file-checked-form{{end}}">
+											<input type="checkbox" name="{{$file.GetDiffFileName}}" autocomplete="off"{{if $file.IsViewed}} checked{{end}}> {{$.locale.Tr "repo.pulls.has_viewed_file"}}
+										</label>
+									{{end}}
+								</div>
+							</h4>
+							<div class="diff-file-body ui attached unstackable table segment" {{if $file.IsViewed}}data-folded="true"{{end}}>
+								<div id="diff-source-{{$file.NameHash}}" class="file-body file-code unicode-escaped code-diff{{if $.IsSplitStyle}} code-diff-split{{else}} code-diff-unified{{end}}{{if $showFileViewToggle}} hide{{end}}">
+									{{if or $file.IsIncomplete $file.IsBin}}
+										<div class="diff-file-body binary" style="padding: 5px 10px;">
+											{{if $file.IsIncomplete}}
+												{{if $file.IsIncompleteLineTooLong}}
+													{{$.locale.Tr "repo.diff.file_suppressed_line_too_long"}}
+												{{else}}
+													{{$.locale.Tr "repo.diff.file_suppressed"}}
+													<a class="ui basic tiny button diff-show-more-button" data-href="{{$.Link}}?file-only=true&files={{$file.Name}}&files={{$file.OldName}}">{{$.locale.Tr "repo.diff.load"}}</a>
+												{{end}}
+											{{else}}
+												{{$.locale.Tr "repo.diff.bin_not_shown"}}
+											{{end}}
+										</div>
+									{{else}}
+										<table class="chroma" data-new-comment-url="{{$.Issue.HTMLURL}}/files/reviews/new_comment" data-path="{{$file.Name}}">
+											{{if $.IsSplitStyle}}
+												{{template "repo/diff/section_split" dict "file" . "root" $}}
+											{{else}}
+												{{template "repo/diff/section_unified" dict "file" . "root" $}}
+											{{end}}
+										</table>
+									{{end}}
+								</div>
+								{{if $showFileViewToggle}}
+									<div id="diff-rendered-{{$file.NameHash}}" class="file-body file-code {{if $.IsSplitStyle}} code-diff-split{{else}} code-diff-unified{{end}}">
+										<table class="chroma w-100">
+											{{if $isImage}}
+												{{template "repo/diff/image_diff" dict "file" . "root" $ "blobBase" $blobBase "blobHead" $blobHead}}
+											{{else}}
+												{{template "repo/diff/csv_diff" dict "file" . "root" $ "blobBase" $blobBase "blobHead" $blobHead}}
+											{{end}}
+										</table>
+									</div>
+								{{end}}
 							</div>
-						{{end}}
-					</div>
-				</div>
-			{{end}}
+						</div>
+					{{end}}
 
-			{{if .Diff.IsIncomplete}}
-				<div class="diff-file-box diff-box file-content mt-3" id="diff-incomplete">
-					<h4 class="ui top attached normal header df ac sb">
-						{{$.locale.Tr "repo.diff.too_many_files"}}
-						<a class="ui basic tiny button" id="diff-show-more-files" data-href="{{$.Link}}?skip-to={{.Diff.End}}&file-only=true">{{.locale.Tr "repo.diff.show_more"}}</a>
-					</h4>
+					{{if .Diff.IsIncomplete}}
+						<div class="diff-file-box diff-box file-content mt-3" id="diff-incomplete">
+							<h4 class="ui top attached normal header df ac sb">
+								{{$.locale.Tr "repo.diff.too_many_files"}}
+								<a class="ui basic tiny button" id="diff-show-more-files" data-href="{{$.Link}}?skip-to={{.Diff.End}}&file-only=true">{{.locale.Tr "repo.diff.show_more"}}</a>
+							</h4>
+						</div>
+					{{end}}
 				</div>
-			{{end}}
 		</div>
 
 		{{if not $.Repository.IsArchived}}

templates/repo/diff/compare.tmpl
@@ -1,7 +1,7 @@
 {{template "base/head" .}}
 <div class="page-content repository diff {{if .PageIsComparePull}}compare pull{{end}}">
 	{{template "repo/header" .}}
-	<div class="ui container {{if .IsSplitStyle}}fluid padded{{end}}">
+	<div class="ui container fluid padded">
 
 	<h2 class="ui header">
 		{{if and $.PageIsComparePull $.IsSigned (not .Repository.IsArchived)}}

templates/repo/diff/options_dropdown.tmpl
@@ -1,7 +1,7 @@
 <div class="ui dropdown tiny basic button icon-button tooltip" data-content="{{.locale.Tr "repo.diff.options_button"}}">
 	{{svg "octicon-kebab-horizontal"}}
 	<div class="menu">
-		<a class="item tiny basic toggle button" data-target="#diff-files">{{.locale.Tr "repo.diff.show_diff_stats"}}</a>
+		<a class="item tiny basic toggle button" id="show-file-list-btn">{{.locale.Tr "repo.diff.show_diff_stats"}}</a>
 		{{if .Issue.Index}}
 			<a class="item" href="{{$.RepoLink}}/pulls/{{.Issue.Index}}.patch" download="{{.Issue.Index}}.patch">{{.locale.Tr "repo.diff.download_patch"}}</a>
 			<a class="item" href="{{$.RepoLink}}/pulls/{{.Issue.Index}}.diff" download="{{.Issue.Index}}.diff">{{.locale.Tr "repo.diff.download_diff"}}</a>

templates/repo/pulls/files.tmpl
@@ -5,7 +5,7 @@
 
 <div class="page-content repository view issue pull files diff">
 	{{template "repo/header" .}}
-	<div class="ui container {{if .IsSplitStyle}}fluid padded{{end}}">
+	<div class="ui container fluid padded">
 		<div class="navbar">
 			{{template "repo/issue/navbar" .}}
 			<div class="ui right">

commit d4f30bb585db92d0021dd937c3b5ba7bee38b9d1

Restore function to Show more button
There was a serious regression in #21012 which broke the Show More
button on the diff page, and the show more button was also broken on the
file tree too.

This PR fixes this by resetting the pageData.diffFiles as the vue
watched value and reattachs a function to the show more button outside
of the file tree view.

Fix #22380

Signed-off-by: Andrew Thornton <art27@cantab.net>

web_src/js/components/DiffFileTree.vue
@@ -29,6 +29,7 @@ export default {
   },
   computed: {
     fileTree() {
+      pageData.diffFileInfo.files = this.files;
       const result = [];
       for (const file of this.files) {
         // Split file into directories

web_src/js/features/repo-diff.js
@@ -119,25 +119,46 @@ function onShowMoreFiles() {
 
 export function doLoadMoreFiles(link, diffEnd, callback) {
   const url = `${link}?skip-to=${diffEnd}&file-only=true`;
+  loadMoreFiles(url, callback);
+}
+
+function loadMoreFiles(url, callback) {
+  const $target = $('a#diff-show-more-files');
+  if ($target.hasClass('disabled')) {
+    callback();
+    return;
+  }
+  $target.addClass('disabled');
   $.ajax({
     type: 'GET',
     url,
   }).done((resp) => {
     if (!resp) {
+      $target.removeClass('disabled');
       callback(resp);
       return;
     }
+    $('#diff-incomplete').replaceWith($(resp).find('#diff-file-boxes').children());
     // By simply rerunning the script we add the new data to our existing
     // pagedata object. this triggers vue and the filetree and filelist will
     // render the new elements.
     $('body').append($(resp).find('script#diff-data-script'));
+    onShowMoreFiles();
     callback(resp);
   }).fail(() => {
+    $target.removeClass('disabled');
     callback();
   });
 }
 
 export function initRepoDiffShowMore() {
+  $(document).on('click', 'a#diff-show-more-files', (e) => {
+    e.preventDefault();
+
+    const $target = $(e.target);
+    loadMoreFiles($target.data('href'), () => {});
+  });
+
   $(document).on('click', 'a.diff-show-more-button', (e) => {
     e.preventDefault();
     const $target = $(e.target);