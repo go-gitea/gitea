0ab6ffaaa2fcb94f4a531d48d1ce290ec1b9dc73
Lunny Xiao <xiaolunwen@gmail.com>
2023-01-10 18:32:12 +0800

    Update modules/setting/mailer.go
    Co-authored-by: zeripath <art27@cantab.net>

--- modules/setting/mailer.go
+++ modules/setting/mailer.go
@@ -70,12 +70,14 @@ func newMailService() {
 	if sec.HasKey("HOST") && !sec.HasKey("SMTP_ADDR") {
 		givenHost := sec.Key("HOST").String()
 		addr, port, err := net.SplitHostPort(givenHost)
-		if err != nil && err.Error() != "missing port in address" {
+		if err != nil && err.Error() == "missing port in address" {
+			addr = givenHost   
+		} else if err != nil {
 			log.Fatal("Invalid mailer.HOST (%s): %v", givenHost, err)
 		}
 		if addr == "" {
 			addr = "127.0.0.1"
-		}
+		}		
 		sec.Key("SMTP_ADDR").MustString(addr)
 		sec.Key("SMTP_PORT").MustString(port)
 	}
