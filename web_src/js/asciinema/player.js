ca060fc45379d8b60821831a12d042569fc9322c
Jason Song <i@wolfogre.com>
2023-01-14 14:52:22 +0800

    fix: style

--- web_src/js/asciinema/player.js
+++ web_src/js/asciinema/player.js
@@ -2,13 +2,9 @@ import * as AsciinemaPlayer from 'asciinema-player';
 
 export function initAsciinemaPlayer() {
   const players = document.getElementsByClassName('asciinema-player-container');
-  console.log('start');
   for (const player of players) {
-    console.log('init');
     AsciinemaPlayer.create(player.getAttribute('data-asciinema-player-src'), player, {
-      fit: 'both',
-      preload: true,
-      poster: 'npt:100:0:0',
+      poster: 'npt:1:0:0',
     });
   }
 }
