662aad59c58b802fcc8fbaa07d5e822e68c0feaf
Jason Song <i@wolfogre.com>
2023-01-16 21:07:30 +0800

    chore: move files

--- web_src/js/index.js
+++ web_src/js/index.js
@@ -90,7 +90,7 @@ import {initRepoCommentForm, initRepository} from './features/repo-legacy.js';
 import {initFormattingReplacements} from './features/formatting.js';
 import {initMcaptcha} from './features/mcaptcha.js';
 import {initCopyContent} from './features/copycontent.js';
-import {initAsciinemaPlayer} from './asciinema/player.js';
+import {initAsciinemaPlayer} from './markup/asciinema.js';
 
 // Run time-critical code as soon as possible. This is safe to do because this
 // script appears at the end of <body> and rendered HTML is accessible at that point.
