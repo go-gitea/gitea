87f8502036c7ec86347770a90fd46cf1a4080348
Jason Song <i@wolfogre.com>
2023-01-17 10:10:21 +0800

    fix: await import

--- web_src/js/markup/asciinema.js
+++ web_src/js/markup/asciinema.js
@@ -1,9 +1,11 @@
-import * as AsciinemaPlayer from 'asciinema-player';
+export async function renderAsciinemaPlayer() {
+  const els = document.querySelectorAll('.asciinema-player-container');
+  if (!els.length) return;
 
-export function initAsciinemaPlayer() {
-  const players = document.getElementsByClassName('asciinema-player-container');
-  for (const player of players) {
-    AsciinemaPlayer.create(player.getAttribute('data-asciinema-player-src'), player, {
+  const player = await import(/* webpackChunkName: "asciinema" */'asciinema-player');
+
+  for (const el of els) {
+    player.create(el.getAttribute('data-asciinema-player-src'), el, {
       poster: 'npt:1:0:0',
     });
   }
