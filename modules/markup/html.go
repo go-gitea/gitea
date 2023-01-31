e7da7bf052b35a75028fd7f44152c6de42e33921
crystal <71373843+CrystalCommunication@users.noreply.github.com>
2023-01-30 20:44:07 -0700

    merge comments

--- modules/markup/html.go
+++ modules/markup/html.go
@@ -358,7 +358,7 @@ func postProcess(ctx *RenderContext, procs []processor, input io.Reader, output 
 }
 
 func visitNode(ctx *RenderContext, procs, textProcs []processor, node *html.Node) {
-	// Add user-content- to IDs if they don't already have them
+	// Add user-content- to IDs and "#" links if they don't already have them
 	for idx, attr := range node.Attr {
 		val := strings.TrimPrefix(attr.Val, "#")
 		notHasPrefix := !(strings.HasPrefix(val, "user-content-") || blackfridayExtRegex.MatchString(val))
@@ -366,7 +366,7 @@ func visitNode(ctx *RenderContext, procs, textProcs []processor, node *html.Node
 		if attr.Key == "id" && notHasPrefix {
 			node.Attr[idx].Val = "user-content-" + attr.Val
 		}
-		// Add user-content- to # links too
+
 		if attr.Key == "href" && strings.HasPrefix(attr.Val, "#") && notHasPrefix {
 			node.Attr[idx].Val = "#user-content-" + val
 		}
