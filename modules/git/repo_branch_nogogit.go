36194cc6e0ba34f578b1de74316fe962af04b12e
wxiaoguang <wxiaoguang@gmail.com>
2023-02-01 09:44:00 +0800

    quote argument names in comment to avoid spelling-check errors

--- modules/git/repo_branch_nogogit.go
+++ modules/git/repo_branch_nogogit.go
@@ -59,8 +59,8 @@ func (repo *Repository) IsBranchExist(name string) bool {
 	return repo.IsReferenceExist(BranchPrefix + name)
 }
 
-// GetBranchNames returns branches from the repository, skipping initial branches and
-// returning at most limit branches, or all branches if limit is 0.
+// GetBranchNames returns branches from the repository, skipping "skip" initial branches and
+// returning at most "limit" branches, or all branches if "limit" is 0.
 func (repo *Repository) GetBranchNames(skip, limit int) ([]string, int, error) {
 	return callShowRef(repo.Ctx, repo.Path, BranchPrefix, TrustedCmdArgs{BranchPrefix, "--sort=-committerdate"}, skip, limit)
 }
