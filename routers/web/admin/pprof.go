b9989b140a851ef7ce2d8fb636a84d004fbbfa44
zeripath <art27@cantab.net>
2023-02-04 11:41:39 +0000

    Add FGProf handler

--- routers/web/admin/pprof.go
+++ routers/web/admin/pprof.go
@@ -11,7 +11,49 @@ import (
 
 	"code.gitea.io/gitea/modules/context"
 	"code.gitea.io/gitea/modules/setting"
+	
+	"github.com/felixge/fgprof"
 )
+
+// PProfFGProfile returns the Full Go Profile from fgprof
+func PProfFGProfile(ctx *context.Context) {
+	durationStr := ctx.FormString("duration")
+	duration := 30 * time.Second
+	if durationStr != "" {
+		var err error
+		duration, err = time.ParseDuration(durationStr)
+		if err != nil {
+			ctx.Flash.Error(ctx.Tr("monitor.pprof.cpuprofile.duration_invalid"))
+			ctx.Redirect(setting.AppSubURL + "/admin/monitor")
+			return
+		}
+	}
+	
+	format := ctx.FormString("format")
+	if format != fgprof.FormatFolded {
+		format = fgprof.FormatPprof
+	}
+
+	start := time.Now()
+
+	ctx.SetServeHeaders(&context.ServeHeaderOptions{
+		Filename:     "fgprof-profile-" + strconv.FormatInt(start.Unix(), 10),
+		LastModified: start,
+	})
+
+	fn := fgprof.Start(ctx.Resp, format)
+	
+	select {
+	case <-time.After(duration):
+	case <-ctx.Done():
+	}
+	
+	err := fn()
+	if err != nil {
+		ctx.ServerError("fgprof.Write", err)
+	}
+}
+
 
 // PProfCPUProfile returns the PProf CPU Profile
 func PProfCPUProfile(ctx *context.Context) {
