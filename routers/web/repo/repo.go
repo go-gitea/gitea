9cd9cebecbfed71f2ccbf52525ac9f76a6f949cc
yp05327 <576951401@qq.com>
2023-02-10 19:57:25 +0900

    fix repo create

--- routers/web/repo/repo.go
+++ routers/web/repo/repo.go
@@ -135,10 +135,7 @@ func Create(ctx *context.Context) {
 	ctx.Data["LabelTemplates"] = repo_module.LabelTemplates
 	ctx.Data["Licenses"] = repo_module.Licenses
 	ctx.Data["Readmes"] = repo_module.Readmes
-	ctx.Data["readme"] = "Default"
-	ctx.Data["private"] = getRepoPrivate(ctx)
 	ctx.Data["IsForcedPrivate"] = setting.Repository.ForcePrivate
-	ctx.Data["default_branch"] = setting.Repository.DefaultBranch
 
 	ctxUser := checkContextUser(ctx, ctx.FormInt64("org"))
 	if ctx.Written() {
@@ -146,14 +143,23 @@ func Create(ctx *context.Context) {
 	}
 	ctx.Data["ContextUser"] = ctxUser
 
-	ctx.Data["repo_template_name"] = ctx.Tr("repo.template_select")
+	ctx.Data["RepoTemplateName"] = ctx.Tr("repo.template_select")
 	templateID := ctx.FormInt64("template_id")
+	repoTemplate := int64(0)
 	if templateID > 0 {
 		templateRepo, err := repo_model.GetRepositoryByID(ctx, templateID)
 		if err == nil && access_model.CheckRepoUnitUser(ctx, templateRepo, ctxUser, unit.TypeCode) {
-			ctx.Data["repo_template"] = templateID
-			ctx.Data["repo_template_name"] = templateRepo.Name
+			repoTemplate = templateID
+			ctx.Data["RepoTemplateName"] = templateRepo.Name
 		}
+	}
+
+	ctx.Data["Form"] = forms.CreateRepoForm{
+		Private:       getRepoPrivate(ctx),
+		DefaultBranch: setting.Repository.DefaultBranch,
+		Readme:        "Default",
+		RepoTemplate:  repoTemplate,
+		TrustModel:    "Default",
 	}
 
 	ctx.Data["CanCreateRepo"] = ctx.Doer.CanCreateRepo()
