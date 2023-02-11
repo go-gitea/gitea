8fa419c4c1e34e4f26adf9323d4886bbb558df25
Gusted <postmaster@gusted.xyz>
2023-02-11 09:11:54 +0100

    Use proxy for pull mirror (#22771) (#22772)
    - Backport #22771
      - Use the proxy (if one is specified) for pull mirrors syncs.
    - Pulled the code from
    https://github.com/go-gitea/gitea/blob/c2774d9e80d9a436d9c2044960369c4db227e3a0/modules/git/repo.go#L164-L170
      - Downstream issue: https://codeberg.org/forgejo/forgejo/issues/302
    
    ---------
    
    Co-authored-by: zeripath <art27@cantab.net>

--- modules/git/repo.go
+++ modules/git/repo.go
@@ -164,10 +164,8 @@ func CloneWithArgs(ctx context.Context, args []CmdArg, from, to string, opts Clo
 
 	envs := os.Environ()
 	u, err := url.Parse(from)
-	if err == nil && (strings.EqualFold(u.Scheme, "http") || strings.EqualFold(u.Scheme, "https")) {
-		if proxy.Match(u.Host) {
-			envs = append(envs, fmt.Sprintf("https_proxy=%s", proxy.GetProxyURL()))
-		}
+	if err == nil {
+		envs = proxy.EnvWithProxy(u)
 	}
 
 	stderr := new(bytes.Buffer)
