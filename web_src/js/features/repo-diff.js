d32af84a1002ceb235c86e4ac569c866ab7816d4
wxiaoguang <wxiaoguang@gmail.com>
2023-02-19 12:06:14 +0800

    Refactor hiding-methods, remove jQuery show/hide, remove `.hide` class, remove inline style=display:none (#22950)
    Close #22847
    
    This PR:
    
    * introduce Gitea's own `showElem` and related functions
    * remove jQuery show/hide
    * remove .hide class
    * remove inline style=display:none 
    
    From now on:
    
    do not use:
    * "[hidden]" attribute: it's too weak, can not be applied to an element
    with "display: flex"
    * ".hidden" class: it has been polluted by Fomantic UI in many cases
    * inline style="display: none": it's difficult to tweak
    * jQuery's show/hide/toggle: it can not show/hide elements with
    "display: xxx !important"
    
    only use:
    * this ".gt-hidden" class
    * showElem/hideElem/toggleElem functions in "utils/dom.js"
    
    cc: @silverwind , this is the all-in-one PR

--- web_src/js/features/repo-diff.js
+++ web_src/js/features/repo-diff.js
@@ -35,8 +35,8 @@ export function initRepoDiffFileViewToggle() {
     $this.addClass('active');
 
     const $target = $($this.data('toggle-selector'));
-    $target.parent().children().addClass('hide');
-    $target.removeClass('hide');
+    $target.parent().children().addClass('gt-hidden');
+    $target.removeClass('gt-hidden');
   });
 }
 
@@ -92,7 +92,7 @@ export function initRepoDiffConversationNav() {
   // Previous/Next code review conversation
   $(document).on('click', '.previous-conversation', (e) => {
     const $conversation = $(e.currentTarget).closest('.comment-code-cloud');
-    const $conversations = $('.comment-code-cloud:not(.hide)');
+    const $conversations = $('.comment-code-cloud:not(.gt-hidden)');
     const index = $conversations.index($conversation);
     const previousIndex = index > 0 ? index - 1 : $conversations.length - 1;
     const $previousConversation = $conversations.eq(previousIndex);
@@ -101,7 +101,7 @@ export function initRepoDiffConversationNav() {
   });
   $(document).on('click', '.next-conversation', (e) => {
     const $conversation = $(e.currentTarget).closest('.comment-code-cloud');
-    const $conversations = $('.comment-code-cloud:not(.hide)');
+    const $conversations = $('.comment-code-cloud:not(.gt-hidden)');
     const index = $conversations.index($conversation);
     const nextIndex = index < $conversations.length - 1 ? index + 1 : 0;
     const $nextConversation = $conversations.eq(nextIndex);
