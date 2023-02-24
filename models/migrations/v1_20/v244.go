5eea2307141e782afa75f0f21739665ff543de44
stuzer05 <stuzer05@gmail.com>
2023-02-24 23:06:41 +0200

    Commit

--- models/migrations/v1_20/v244.go
+++ models/migrations/v1_20/v244.go
@@ -20,3 +20,22 @@ func AddNeedApprovalToActionRun(x *xorm.Engine) error {
 
 	return x.Sync(new(ActionRun))
 }
+
+// AddTimeEstimateColumnToIssueTable: add TimeEstimate column
+func AddTimeEstimateColumnToIssueTable(x *xorm.Engine) error {
+	type Issue struct {
+		TimeEstimate int64
+	}
+
+	return x.Sync(new(Issue))
+}
+
+// AddColumnsToCommentTable: add TimeTracked column
+func AddColumnsToCommentTable(x *xorm.Engine) error {
+	type Comment struct {
+		TimeTracked  int64
+		TimeEstimate int64
+	}
+
+	return x.Sync(new(Comment))
+}
