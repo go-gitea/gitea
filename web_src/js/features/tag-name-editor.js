1d9a9079371ac94953f3b06c602d78e08c82a361
Zettat123 <zettat123@gmail.com>
2023-02-27 14:38:51 +0800

    lint code

--- web_src/js/features/tag-name-editor.js
+++ web_src/js/features/tag-name-editor.js
@@ -1,29 +1,30 @@
 import $ from 'jquery';
+import {hideElem, showElem} from '../utils/dom.js';
 
 export function initTagNameEditor() {
-    const el = document.getElementById('tag-name-editor');
-    if (!el) return;
+  const el = document.getElementById('tag-name-editor');
+  if (!el) return;
 
-    const existingTags = JSON.parse(el.getAttribute('data-existing-tags'))
-    if (!Array.isArray(existingTags)) return
+  const existingTags = JSON.parse(el.getAttribute('data-existing-tags'));
+  if (!Array.isArray(existingTags)) return;
 
-    const defaultTagHelperText = el.getAttribute('data-tag-helper')
-    const newTagHelperText = el.getAttribute('data-tag-helper-new')
-    const existingTagHelperText = el.getAttribute('data-tag-helper-existing')
+  const defaultTagHelperText = el.getAttribute('data-tag-helper');
+  const newTagHelperText = el.getAttribute('data-tag-helper-new');
+  const existingTagHelperText = el.getAttribute('data-tag-helper-existing');
 
-    $('#tag-name').on('keyup', function(e) {
-        const value = e.target.value
-        if (existingTags.includes(value)) {
-            // If the tag already exists, hide the target branch selector.
-            $('#tag-target-selector').hide()
-            $('#tag-helper').text(existingTagHelperText)
-        } else {
-            $('#tag-target-selector').show()
-            if (typeof value == 'string' && value.length > 0) {
-              $('#tag-helper').text(newTagHelperText)
-            } else {
-              $('#tag-helper').text(defaultTagHelperText)
-            }
-        }
-    })
+  $('#tag-name').on('keyup', (e) => {
+    const value = e.target.value;
+    if (existingTags.includes(value)) {
+      // If the tag already exists, hide the target branch selector.
+      hideElem($('#tag-target-selector'));
+      $('#tag-helper').text(existingTagHelperText);
+    } else {
+      showElem($('#tag-target-selector'));
+      if (typeof value === 'string' && value.length > 0) {
+        $('#tag-helper').text(newTagHelperText);
+      } else {
+        $('#tag-helper').text(defaultTagHelperText);
+      }
+    }
+  });
 }
