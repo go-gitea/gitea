c6c6fadb9e1b19eb0ee4e63a31850198680bd2c9
a1012112796 <1012112796@qq.com>
2023-03-03 09:19:34 +0100

    Add localization to tagged by / since this tag

--- routers/web/repo/release.go
+++ routers/web/repo/release.go
@@ -226,7 +226,6 @@ func releasesOrTagsFeed(ctx *context.Context, isReleasesOnly bool, formatType st
 
 // SingleRelease renders a single release's page
 func SingleRelease(ctx *context.Context) {
-	ctx.Data["Title"] = ctx.Tr("repo.release.releases")
 	ctx.Data["PageIsReleaseList"] = true
 	ctx.Data["DefaultBranch"] = ctx.Repo.Repository.DefaultBranch
 
@@ -241,6 +240,12 @@ func SingleRelease(ctx *context.Context) {
 		}
 		ctx.ServerError("GetReleasesByRepoID", err)
 		return
+	}
+	ctx.Data["PageIsSingleTag"] = release.IsTag
+	if release.IsTag {
+		ctx.Data["Title"] = release.TagName
+	} else {
+		ctx.Data["Title"] = release.Title
 	}
 
 	err = repo_model.GetReleaseAttachments(ctx, release)
