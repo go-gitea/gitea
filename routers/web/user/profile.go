67d8115d5daaa29fa719df84cfc2cc0aadd8964a
Nicholas Pease <me@nicholaspease.com>
2023-03-03 05:01:19 +0000

    Will use default branch

--- routers/web/user/profile.go
+++ routers/web/user/profile.go
@@ -92,7 +92,7 @@ func Profile(ctx *context.Context) {
 	}
 
 	// Fetches user's .profile/README.md and adds it to the users profile (if it exists)
-	resp, err := http.Get(setting.AppURL + ctx.ContextUser.Name + "/.profile/raw/branch/main/README.md")
+	resp, err := http.Get(setting.AppURL + ctx.ContextUser.Name + "/.profile/raw/README.md")
 	if err != nil {
 		ctx.ServerError("RenderString", err)
 		return
