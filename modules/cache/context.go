aa1dc7e34d4c7c0db8bdf782d60a3b846f360401
Jason Song <i@wolfogre.com>
2023-03-06 17:20:39 +0800

    feat: redesign cache context

--- modules/cache/context.go
+++ modules/cache/context.go
@@ -18,33 +18,42 @@ type cacheContext struct {
 	data    map[any]map[any]any
 	lock    sync.RWMutex
 	created time.Time
+	discard bool
 }
 
 func (cc *cacheContext) Get(tp, key any) any {
 	cc.lock.RLock()
 	defer cc.lock.RUnlock()
-	if cc.data[tp] == nil {
-		return nil
-	}
 	return cc.data[tp][key]
 }
 
 func (cc *cacheContext) Put(tp, key, value any) {
 	cc.lock.Lock()
 	defer cc.lock.Unlock()
-	if cc.data[tp] == nil {
-		cc.data[tp] = make(map[any]any)
+
+	if cc.discard {
+		return
 	}
-	cc.data[tp][key] = value
+
+	d := cc.data[tp]
+	if d == nil {
+		d = make(map[any]any)
+		cc.data[tp] = d
+	}
+	d[key] = value
 }
 
 func (cc *cacheContext) Delete(tp, key any) {
 	cc.lock.Lock()
 	defer cc.lock.Unlock()
-	if cc.data[tp] == nil {
-		return
-	}
 	delete(cc.data[tp], key)
+}
+
+func (cc *cacheContext) Discard() {
+	cc.lock.Lock()
+	defer cc.lock.Unlock()
+	cc.data = nil
+	cc.discard = true
 }
 
 // cacheContextLifetime is the max lifetime of cacheContext.
@@ -61,6 +70,12 @@ func (cc *cacheContext) Expired() bool {
 var cacheContextKey = struct{}{}
 
 func WithCacheContext(ctx context.Context) context.Context {
+	if c, ok := ctx.Value(cacheContextKey).(*cacheContext); ok {
+		if !c.discard {
+			// reuse parent context
+			return ctx
+		}
+	}
 	return context.WithValue(ctx, cacheContextKey, &cacheContext{
 		data:    make(map[any]map[any]any),
 		created: timeNow(),
@@ -68,7 +83,14 @@ func WithCacheContext(ctx context.Context) context.Context {
 }
 
 func WithNoCacheContext(ctx context.Context) context.Context {
-	return context.WithValue(ctx, cacheContextKey, nil)
+	if c, ok := ctx.Value(cacheContextKey).(*cacheContext); ok {
+		// The caller want to run long-life tasks, but the parent context is a cache context.
+		// So we should disable and clean the cache data, or it will be kept in memory for a long time.
+		c.Discard()
+		return ctx
+	}
+
+	return ctx
 }
 
 func GetContextData(ctx context.Context, tp, key any) any {
