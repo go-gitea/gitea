ecfcd45835117f558bfc44580873b485fc649844
Zettat123 <zettat123@gmail.com>
2023-03-08 16:28:19 +0800

    convert ref to commit id before get note in api

--- routers/api/v1/repo/notes.go
+++ routers/api/v1/repo/notes.go
@@ -58,8 +58,14 @@ func getNote(ctx *context.APIContext, identifier string) {
 		return
 	}
 
+	commitSHA, err := ctx.Repo.GitRepo.ConvertToSHA1(identifier)
+	if err != nil {
+		ctx.NotFound(err)
+		return
+	}
+
 	var note git.Note
-	if err := git.GetNote(ctx, ctx.Repo.GitRepo, identifier, &note); err != nil {
+	if err := git.GetNote(ctx, ctx.Repo.GitRepo, commitSHA.String(), &note); err != nil {
 		if git.IsErrNotExist(err) {
 			ctx.NotFound(identifier)
 			return
