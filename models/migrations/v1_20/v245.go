5bcc68c27ee17cbbc44fe1b63e25a0a082c86a08
yp05327 <576951401@qq.com>
2023-03-09 02:23:39 +0000

    (error preview)

--- models/migrations/v1_20/v245.go
+++ models/migrations/v1_20/v245.go
@@ -4,12 +4,49 @@
 package v1_20 //nolint
 
 import (
+	"fmt"
+
+	"code.gitea.io/gitea/modules/log"
 	"xorm.io/xorm"
 )
 
 // FixIncorrectProjectType: set individual project's type from 3(TypeOrganization) to 1(TypeIndividual)
 func FixIncorrectProjectType(x *xorm.Engine) error {
-	_, err := x.Exec("UPDATE project SET type = ? FROM user WHERE user.id = project.owner_id AND project.type = ? AND user.type = ?", 1, 3, 0)
+	type User struct {
+		ID   int64 `xorm:"pk autoincr"`
+		Type int
+	}
+
+	const (
+		UserTypeIndividual int = 0
+
+		TypeIndividual   uint8 = 1
+		TypeOrganization uint8 = 3
+	)
+
+	type Project struct {
+		OwnerID int64 `xorm:"INDEX"`
+		Type    uint8
+		Owner   *User `xorm:"extends"`
+	}
+
+	ps := make([]Project, 0, 10)
+	count, err := x.Table("project").
+		//err := x.Table("project").
+		Join("INNER", "user", "user.id = project.owner_id").
+		Where("project.type = ? AND user.type = ?", TypeOrganization, UserTypeIndividual).
+		//Find(&ps)
+		Update(&Project{
+			Type: TypeIndividual,
+		})
+
+	if err == nil {
+		log.Debug("Updated %d projects with owner IS UserTypeIndividual", count)
+		for _, p := range ps {
+			fmt.Println(p)
+			fmt.Println(p.Owner)
+		}
+	}
 
 	return err
 }
