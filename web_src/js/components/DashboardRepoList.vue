43d5706431a21ec06c268dcc0b0bca603fb6070b
yp05327 <576951401@qq.com>
2023-03-17 02:06:22 +0000

    remove gt-mr-2

--- web_src/js/components/DashboardRepoList.vue
+++ web_src/js/components/DashboardRepoList.vue
@@ -72,7 +72,7 @@
         <ul class="repo-owner-name-list">
           <li v-for="repo in repos" :class="{'private': repo.private || repo.internal}" :key="repo.id">
             <a class="repo-list-link gt-df gt-ac gt-sb" :href="repo.link">
-              <div class="item-name gt-df gt-ac gt-f1 gt-mr-2">
+              <div class="item-name gt-df gt-ac gt-f1">
                 <svg-icon :name="repoIcon(repo)" :size="16" class-name="gt-mr-2"/>
                 <div class="text gt-bold truncate gt-ml-1">{{ repo.full_name }}</div>
                 <span v-if="repo.archived">
