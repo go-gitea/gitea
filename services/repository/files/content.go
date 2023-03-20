18485f59ae0ae5c234d83a87f2de7b3f53f7bfd7
来自村里的小螃蟹 <yystopf@163.com>
2023-03-20 23:22:32 +0800

    Update services/repository/files/content.go
    add subomodule url is blank condition
    
    Co-authored-by: delvh <dev.lh@web.de>

--- services/repository/files/content.go
+++ services/repository/files/content.go
@@ -214,7 +214,7 @@ func GetContents(ctx context.Context, repo *repo_model.Repository, treePath, ref
 		if err != nil {
 			return nil, err
 		}
-		if submodule != nil {
+		if submodule != nil && submodule.URL != "" {
 			contentsResponse.SubmoduleGitURL = &submodule.URL
 		}
 	}
