159005d297727c911d40ffc46158bd870df71132
Jason Song <i@wolfogre.com>
2023-03-21 11:10:20 +0800

    chore: format code

--- models/migrations/v1_17/v211.go
+++ models/migrations/v1_17/v211.go
@@ -4,11 +4,9 @@
 package v1_17 //nolint
 
 import (
-	"fmt"
-
 	"xorm.io/xorm"
 )
 
-func CreateForeignReferenceTable(x *xorm.Engine) error {
+func CreateForeignReferenceTable(_ *xorm.Engine) error {
 	return nil // This table was dropped in v1_19/v237.go
 }
