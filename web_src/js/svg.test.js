e8e2c5e46236f5fc5e0a51e18c04cc984a994bbd
wxiaoguang <wxiaoguang@gmail.com>
2023-03-22 23:59:28 +0800

    add tests for Vue

--- web_src/js/svg.test.js
+++ web_src/js/svg.test.js
@@ -1,5 +1,6 @@
 import {expect, test} from 'vitest';
-import {svg, svgParseOuterInner} from './svg.js';
+import {svg, SvgIcon, svgParseOuterInner} from './svg.js';
+import {createApp, h} from 'vue';
 
 test('svg', () => {
   expect(svg('octicon-repo')).toMatch(/^<svg/);
@@ -12,4 +13,15 @@ test('svgParseOuterInner', () => {
   expect(svgOuter.nodeName).toMatch('svg');
   expect(svgOuter.classList.contains('octicon-repo')).toBeTruthy();
   expect(svgInnerHtml).toContain('<path');
-})
+});
+
+test('SvgIcon', () => {
+  const root = document.createElement('div');
+  createApp({render: () => h(SvgIcon, {name: 'octicon-link', size: 24, class: 'base', className: 'extra'})}).mount(root);
+  expect(root.firstChild.nodeName).toEqual('svg');
+  expect(root.firstChild.getAttribute('width')).toEqual('24');
+  expect(root.firstChild.getAttribute('height')).toEqual('24');
+  expect(root.firstChild.classList.contains('octicon-link')).toBeTruthy();
+  expect(root.firstChild.classList.contains('base')).toBeTruthy();
+  expect(root.firstChild.classList.contains('extra')).toBeTruthy();
+});
