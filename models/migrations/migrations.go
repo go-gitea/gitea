a0a2480ba73f9e5d48bf85edf4e25554448ff185
yp05327 <576951401@qq.com>
2023-03-24 04:23:25 +0000

    add db migration

--- models/migrations/migrations.go
+++ models/migrations/migrations.go
@@ -475,6 +475,8 @@ var migrations = []Migration{
 	NewMigration("Fix incorrect project type", v1_20.FixIncorrectProjectType),
 	// v248 -> v249
 	NewMigration("Add version column to action_runner table", v1_20.AddVersionToActionRunner),
+	// v249 -> v250
+	NewMigration("Fix incorrect owner team unit access mode", v1_20.FixIncorrectOwnerTeamUnitAccessMode),
 }
 
 // GetCurrentDBVersion returns the current db version
