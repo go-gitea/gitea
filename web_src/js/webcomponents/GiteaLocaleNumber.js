80844f8d8dffd844fe140c251b0c3b979a3e81d3
wxiaoguang <wxiaoguang@gmail.com>
2023-04-02 09:09:25 +0800

    improve code and comment

--- web_src/js/webcomponents/GiteaLocaleNumber.js
+++ web_src/js/webcomponents/GiteaLocaleNumber.js
@@ -2,17 +2,19 @@
 // Or add a tooltip by data-number-in-tooltip attribute. JSON: {message: "count: %s", number: 123}
 window.customElements.define('gitea-locale-number', class extends HTMLElement {
   connectedCallback() {
+    // ideally, the number locale formatting and plural processing should be done by backend with translation strings.
+    // if we have complete backend locale support (eg: Golang "x/text" package), we can drop this component.
     const number = this.getAttribute('data-number');
     if (number) {
       this.attachShadow({mode: 'open'});
       this.shadowRoot.textContent = new Intl.NumberFormat().format(Number(number));
     }
-    let numberInTooltip = this.getAttribute('data-number-in-tooltip');
+    const numberInTooltip = this.getAttribute('data-number-in-tooltip');
     if (numberInTooltip) {
-      numberInTooltip = JSON.parse(numberInTooltip);
-      let msg = numberInTooltip.message;
-      msg = msg.replace(/%[ds]/, new Intl.NumberFormat().format(Number(numberInTooltip.number)));
-      this.setAttribute('data-tooltip-content', msg);
+      // TODO: only 2 usages of this, we can replace it with Golang's "x/text/number" package in the future
+      const {message, number} = JSON.parse(numberInTooltip);
+      const tooltipContent = message.replace(/%[ds]/, new Intl.NumberFormat().format(Number(number)));
+      this.setAttribute('data-tooltip-content', tooltipContent);
     }
   }
 });
