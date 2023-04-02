4cc8623445250179338ee8d20bbbfa70c2f03764
6543 <6543@obermui.de>
2023-04-02 22:12:09 +0200

    Update models/user/user.go

--- models/user/user.go
+++ models/user/user.go
@@ -537,7 +537,7 @@ var (
 		"gitea-actions",
 	}
 
-	// DON'T ADD ANY NEW STUFF, WE SOLVE THIS WITH ALTERNATE PATHS!
+	// DON'T ADD ANY NEW STUFF, WE SOLVE THIS WITH `/user/{obj}`  PATHS!
 	reservedUserPatterns = []string{"*.keys", "*.gpg", "*.rss", "*.atom"}
 )
 
