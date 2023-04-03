1f73dda83eebbc9e7ccc98f339549eed37b2bc9b
wxiaoguang <wxiaoguang@gmail.com>
2023-04-03 23:05:08 +0800

    Update web_src/js/features/repo-wiki.js
    Co-authored-by: silverwind <me@silverwind.io>

--- web_src/js/features/repo-wiki.js
+++ web_src/js/features/repo-wiki.js
@@ -47,6 +47,7 @@ async function initRepoWikiFormEditor() {
     useScene: 'wiki',
     // EasyMDE has some problems of height definition, it has inline style height 300px by default, so we also use inline styles to override it.
     // And another benefit is that we only need to write the style once for both editors.
+    // TODO: Move height style to CSS after EasyMDE removal.
     editorHeights: {minHeight: '300px', height: 'calc(100vh - 600px)'},
     previewMode: 'gfm',
     previewWiki: true,
