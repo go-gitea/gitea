e3a16ff8d1a6cfba2ced4b130743c54482145ab7
harryzcy <harry@harryzheng.com>
2023-04-06 02:45:50 -0400

    Fix SPDX-License-Identifier

--- modules/migration/null_uploader.go
+++ modules/migration/null_uploader.go
@@ -1,6 +1,5 @@
-// Copyright 2022 The Gitea Authors. All rights reserved.
-// Use of this source code is governed by a MIT-style
-// license that can be found in the LICENSE file.
+// Copyright 2023 The Gitea Authors. All rights reserved.
+// SPDX-License-Identifier: MIT
 
 package migration
 
