506d1b31bc9982c0ca62a60738e134ae2a44d275
Yarden Shoham <git@yardenshoham.com>
2023-04-07 21:00:06 +0000

    Use auto-updating, natively hoverable, localized time elements
    - Added GitHub’s `relative-time` element
    - Converted all formatted timestamps to use this element
    - No more flashes of unstyled content around time elements
    - These elements have native tooltips that show the full timestamp and are localized using the HTML’s `lang` property
    - Relative (e.g. the activities in the dashboard) and duration (e.g. server uptime in the admin page) time elements are auto-updated to keep up with the current time without refreshing the page
    - Deleted code that is not needed anymore, such as `formatting.js` and simplified `since.go`
    
    Signed-off-by: Yarden Shoham <git@yardenshoham.com>

--- tests/integration/repo_test.go
+++ tests/integration/repo_test.go
@@ -75,7 +75,10 @@ func testViewRepo(t *testing.T) {
 			}
 		})
 
-		f.commitTime, _ = s.Find("span.time-since").Attr("data-tooltip-content")
+		// convert "Wed, 14 Jun 2017 21:54:21 +0800" to "Wed, 14 Jun 2017 13:54:21 UTC"
+		htmlTimeString, _ := s.Find("relative-time.time-since").Attr("datetime")
+		htmlTime, _ := time.Parse("Mon, 02 Jan 2006 15:04:05 -0700", htmlTimeString)
+		f.commitTime = htmlTime.UTC().Format("Mon, 02 Jan 2006 15:04:05 UTC")
 		items = append(items, f)
 	})
 
