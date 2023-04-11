7852e1f13d2bf721868f25cdd95a368f8827c30e
silverwind <me@silverwind.io>
2023-04-11 11:57:02 +0200

    Fix multiple markdown rendering bugs

--- web_src/js/markup/mermaid.js
+++ web_src/js/markup/mermaid.js
@@ -1,5 +1,6 @@
 import {isDarkTheme} from '../utils.js';
 import {makeCodeCopyButton} from './codecopy.js';
+import {displayError} from './common.js';
 
 const {mermaidMaxSourceCharacters} = window.config;
 
@@ -8,14 +9,6 @@ const iframeCss = `
   body {margin: 0; padding: 0; overflow: hidden}
   #mermaid {display: block; margin: 0 auto}
 `;
-
-function displayError(el, err) {
-  el.closest('pre').classList.remove('is-loading');
-  const errorNode = document.createElement('div');
-  errorNode.setAttribute('class', 'ui message error markup-block-error gt-mono');
-  errorNode.textContent = err.str || err.message || String(err);
-  el.closest('pre').before(errorNode);
-}
 
 export async function renderMermaid() {
   const els = document.querySelectorAll('.markup code.language-mermaid');
@@ -30,18 +23,19 @@ export async function renderMermaid() {
   });
 
   for (const el of els) {
-    const source = el.textContent;
+    const pre = el.closest('pre');
+    if (pre.hasAttribute('data-done')) continue;
 
+    const source = el.textContent;
     if (mermaidMaxSourceCharacters >= 0 && source.length > mermaidMaxSourceCharacters) {
-      displayError(el, new Error(`Mermaid source of ${source.length} characters exceeds the maximum allowed length of ${mermaidMaxSourceCharacters}.`));
+      displayError(pre, new Error(`Mermaid source of ${source.length} characters exceeds the maximum allowed length of ${mermaidMaxSourceCharacters}.`));
       continue;
     }
 
     try {
       await mermaid.parse(source);
     } catch (err) {
-      displayError(el, err);
-      el.closest('pre').classList.remove('is-loading');
+      displayError(pre, err);
       continue;
     }
 
@@ -49,14 +43,15 @@ export async function renderMermaid() {
       // can't use bindFunctions here because we can't cross the iframe boundary. This
       // means js-based interactions won't work but they aren't intended to work either
       const {svg} = await mermaid.render('mermaid', source);
-      const heightStr = (svg.match(/viewBox="(.+?)"/) || ['', ''])[1].split(/\s+/)[3];
-      if (!heightStr) return displayError(el, new Error('Could not determine chart height'));
 
       const iframe = document.createElement('iframe');
       iframe.classList.add('markup-render');
-      iframe.sandbox = 'allow-scripts';
-      iframe.style.height = `${Math.ceil(parseFloat(heightStr))}px`;
+      iframe.sandbox = 'allow-scripts allow-same-origin';
       iframe.srcdoc = `<html><head><style>${iframeCss}</style></head><body>${svg}</body></html>`;
+      iframe.addEventListener('load', (e) => {
+        const height = e.target.contentWindow.document.body.clientHeight;
+        iframe.style.height = `${height}px`;
+      });
 
       const mermaidBlock = document.createElement('div');
       mermaidBlock.classList.add('mermaid-block');
@@ -66,9 +61,9 @@ export async function renderMermaid() {
       btn.setAttribute('data-clipboard-text', source);
 
       mermaidBlock.append(btn);
-      el.closest('pre').replaceWith(mermaidBlock);
+      pre.replaceWith(mermaidBlock);
     } catch (err) {
-      displayError(el, err);
+      displayError(pre, err);
     }
   }
 }
