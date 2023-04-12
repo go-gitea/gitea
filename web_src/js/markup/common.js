38e6431680312b20197799294b77a817e8bfc573
silverwind <me@silverwind.io>
2023-04-12 15:04:49 +0200

    use pre for error node, remove border from it

--- web_src/js/markup/common.js
+++ web_src/js/markup/common.js
@@ -1,7 +1,7 @@
 export function displayError(el, err) {
   el.classList.remove('is-loading');
-  const errorNode = document.createElement('div');
-  errorNode.setAttribute('class', 'ui message error markup-block-error gt-mono');
+  const errorNode = document.createElement('pre');
+  errorNode.setAttribute('class', 'ui message error markup-block-error');
   errorNode.textContent = err.str || err.message || String(err);
   el.before(errorNode);
   el.setAttribute('data-render-done', 'true');
