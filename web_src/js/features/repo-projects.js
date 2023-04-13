892beb818c9593a2a45c624305674592e69294a6
yp05327 <576951401@qq.com>
2023-04-13 06:53:55 +0000

    fix

--- web_src/js/features/repo-projects.js
+++ web_src/js/features/repo-projects.js
@@ -142,7 +142,7 @@ export function initRepoProject() {
   });
 
   $('.delete-project-board').each(function () {
-    $(this).click(function (e) {
+    $(this).on('click', function (e) {
       e.preventDefault();
 
       $.ajax({
@@ -158,7 +158,7 @@ export function initRepoProject() {
     });
   });
 
-  $('#new_board_submit').click(function (e) {
+  $('#new_board_submit').on('click', function (e) {
     e.preventDefault();
 
     const boardTitle = $('#new_board');
