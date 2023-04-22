8af7c48fec4ba0630c620f1aa9275f48e7aac2e2
Yarden Shoham <git@yardenshoham.com>
2023-04-22 15:24:15 +0000

    Escape strings
    Co-authored-by: wxiaoguang <wxiaoguang@gmail.com>

--- modules/timeutil/datetime.go
+++ modules/timeutil/datetime.go
@@ -5,18 +5,21 @@ package timeutil
 
 import (
 	"fmt"
+	"html"
 	"html/template"
 )
 
 // DateTime renders an absolute time HTML given a time as a string
 func DateTime(format, datetime, fallback string) template.HTML {
+	datetimeEscaped := html.EscapeString(datetime)
+	fallbackEscaped := html.EscapeString(fallback)
 	switch format {
 	case "short":
-		return template.HTML(fmt.Sprintf(`<relative-time format="datetime" year="numeric" month="short" day="numeric" weekday="" datetime="%s">%s</relative-time>`, datetime, fallback))
+		return template.HTML(fmt.Sprintf(`<relative-time format="datetime" year="numeric" month="short" day="numeric" weekday="" datetime="%s">%s</relative-time>`, datetimeEscaped, fallbackEscaped))
 	case "long":
-		return template.HTML(fmt.Sprintf(`<relative-time format="datetime" year="numeric" month="long" day="numeric" weekday="" datetime="%s">%s</relative-time>`, datetime, fallback))
+		return template.HTML(fmt.Sprintf(`<relative-time format="datetime" year="numeric" month="long" day="numeric" weekday="" datetime="%s">%s</relative-time>`, datetimeEscaped, fallbackEscaped))
 	case "full":
-		return template.HTML(fmt.Sprintf(`<relative-time format="datetime" weekday="" year="numeric" month="short" day="numeric" hour="numeric" minute="numeric" second="numeric" datetime="%s">%s</relative-time>`, datetime, fallback))
+		return template.HTML(fmt.Sprintf(`<relative-time format="datetime" weekday="" year="numeric" month="short" day="numeric" hour="numeric" minute="numeric" second="numeric" datetime="%s">%s</relative-time>`, datetimeEscaped, fallbackEscaped))
 	}
 	return template.HTML("error in DateTime")
 }
