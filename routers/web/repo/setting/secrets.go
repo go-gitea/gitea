ad374cd8afe36550caa5fedfb69fc4cb63a35b03
HesterG <hestergong@gmail.com>
2023-04-27 14:02:33 +0800

    add TODO

--- routers/web/repo/setting/secrets.go
+++ routers/web/repo/setting/secrets.go
@@ -14,6 +14,7 @@ import (
 )
 
 const (
+	// TODO: Separate secrets from runners when layout is ready
 	tplRepoSecrets base.TplName = "repo/settings/actions"
 	tplOrgSecrets  base.TplName = "org/settings/actions"
 	tplUserSecrets base.TplName = "user/settings/actions"
