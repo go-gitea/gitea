c231bf01c63e59425e49db618026c639215d69d5
yp05327 <576951401@qq.com>
2023-05-01 02:56:47 +0000

    remove nolint

--- models/actions/runner.go
+++ models/actions/runner.go
@@ -53,7 +53,7 @@ type ActionRunner struct {
 }
 
 // RunnerType defines the runner type
-type RunnerType int //nolint:revive
+type RunnerType int
 
 const (
 	// RunnerTypeGlobal defines a global runner
