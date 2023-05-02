b19cf492b98928de797a748e1764b59419a59d31
Matthew Walowski <mattwalowski@gmail.com>
2023-05-02 08:56:05 -0700

    Call counts with opts struct

--- modules/git/commit_test.go
+++ modules/git/commit_test.go
@@ -14,7 +14,14 @@ import (
 func TestCommitsCount(t *testing.T) {
 	bareRepo1Path := filepath.Join(testReposDir, "repo1_bare")
 
-	commitsCount, err := CommitsCount(DefaultContext, bareRepo1Path, "", "8006ff9adbf0cb94da7dad9e537e53817f9fa5c0")
+	commitsCount, err := CommitsCount(DefaultContext,
+		CommitsCountOptions{
+			RepoPath: bareRepo1Path,
+			Not:      "",
+			Revision: []string{"8006ff9adbf0cb94da7dad9e537e53817f9fa5c0"},
+			RelPath:  []string{},
+		})
+
 	assert.NoError(t, err)
 	assert.Equal(t, int64(3), commitsCount)
 }
