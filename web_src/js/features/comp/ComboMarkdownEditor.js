143dcadbc228dd128c3f01692975f1931fdea535
wxiaoguang <wxiaoguang@gmail.com>
2023-05-03 00:53:02 +0800

    fix

--- web_src/js/features/comp/ComboMarkdownEditor.js
+++ web_src/js/features/comp/ComboMarkdownEditor.js
@@ -233,7 +233,6 @@ class ComboMarkdownEditor {
 
   async switchToEasyMDE() {
     if (this.easyMDE) return;
-
     // EasyMDE's CSS should be loaded via webpack config, otherwise our own styles can not overwrite the default styles.
     const {default: EasyMDE} = await import(/* webpackChunkName: "easymde" */'easymde');
     const easyMDEOpt = {
