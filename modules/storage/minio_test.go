4f3c992feafdb7fd3c2375670992d95ba0841e2a
fuxiaohei <fuxiaohei@vip.qq.com>
2023-05-13 20:48:27 +0800

    use test helper in storage iterator tests

--- modules/storage/minio_test.go
+++ modules/storage/minio_test.go
@@ -4,54 +4,15 @@
 package storage
 
 import (
-	"bytes"
-	"context"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestMinioStorageIterator(t *testing.T) {
-	config := MinioStorageConfig{
+	testStorageIterator(t, string(MinioStorageType), MinioStorageConfig{
 		Endpoint:        "127.0.0.1:9000",
 		AccessKeyID:     "123456",
 		SecretAccessKey: "12345678",
 		Bucket:          "gitea",
 		Location:        "us-east-1",
-	}
-	l, err := NewMinioStorage(context.Background(), config)
-	assert.NoError(t, err)
-
-	testFiles := [][]string{
-		{"a/1.txt", "a1"},
-		{"/a/1.txt", "aa1"}, // same as above, but with leading slash that will be trim
-		{"ab/1.txt", "ab1"},
-		{"b/1.txt", "b1"},
-		{"b/2.txt", "b2"},
-		{"b/3.txt", "b3"},
-		{"b/x 4.txt", "bx4"},
-	}
-	for _, f := range testFiles {
-		_, err = l.Save(f[0], bytes.NewBufferString(f[1]), -1)
-		assert.NoError(t, err)
-	}
-
-	expectedList := map[string][]string{
-		"a":           {"a/1.txt"},
-		"b":           {"b/1.txt", "b/2.txt", "b/3.txt", "b/x 4.txt"},
-		"":            {"a/1.txt", "b/1.txt", "b/2.txt", "b/3.txt", "b/x 4.txt", "ab/1.txt"},
-		"/":           {"a/1.txt", "b/1.txt", "b/2.txt", "b/3.txt", "b/x 4.txt", "ab/1.txt"},
-		"a/b/../../a": {"a/1.txt"},
-	}
-	for dir, expected := range expectedList {
-		count := 0
-		err = l.IterateObjects(dir, func(path string, f Object) error {
-			defer f.Close()
-			assert.Contains(t, expected, path)
-			count++
-			return nil
-		})
-		assert.NoError(t, err)
-		assert.Len(t, expected, count)
-	}
+	})
 }
