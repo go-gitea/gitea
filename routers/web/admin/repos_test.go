82f3e2d47574fa2027fe2bfcbc04357f0d852df3
truecode112 <truecoder112@gmail.com>
2023-05-15 12:27:22 +0300

    add test for global repo size limit option

---
+++ routers/web/admin/repos_test.go
@@ -0,0 +1,26 @@
+// Copyright 2019 The Gitea Authors. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+package admin
+
+import (
+	"testing"
+
+	"code.gitea.io/gitea/models/unittest"
+	"code.gitea.io/gitea/modules/test"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUpdateRepoPost(t *testing.T) {
+	unittest.PrepareTestEnv(t)
+	ctx := test.MockContext(t, "admin/repos")
+	test.LoadUser(t, ctx, 1)
+
+	ctx.Req.Form.Set("enable_size_limit", "on")
+	ctx.Req.Form.Set("repo_size_limit", "222 kcmcm")
+
+	UpdateRepoPost(ctx)
+
+	assert.NotEmpty(t, ctx.Flash.ErrorMsg)
+}
