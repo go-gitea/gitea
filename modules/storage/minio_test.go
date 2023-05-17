6c8235dc98322da3a6afdf61f105d4caaf8c152e
Lunny Xiao <xiaolunwen@gmail.com>
2023-05-17 18:00:58 +0800

    Skip TestMinioStorageIterator on CI (#24762)
    Fix https://github.com/go-gitea/gitea/pull/24691#issuecomment-1550987681
    
    ---------
    
    Co-authored-by: silverwind <me@silverwind.io>

--- modules/storage/minio_test.go
+++ modules/storage/minio_test.go
@@ -4,10 +4,15 @@
 package storage
 
 import (
+	"os"
 	"testing"
 )
 
 func TestMinioStorageIterator(t *testing.T) {
+	if os.Getenv("CI") == "" {
+		t.Skip("minioStorage not present on CI")
+		return
+	}
 	testStorageIterator(t, string(MinioStorageType), MinioStorageConfig{
 		Endpoint:        "127.0.0.1:9000",
 		AccessKeyID:     "123456",
