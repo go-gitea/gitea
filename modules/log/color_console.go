9aceb066d166c495080f8baa4289921cacd73784
wxiaoguang <wxiaoguang@gmail.com>
2023-05-18 23:59:10 +0800

    Update modules/log/color_console.go
    Co-authored-by: delvh <dev.lh@web.de>

--- modules/log/color_console.go
+++ modules/log/color_console.go
@@ -1,5 +1,5 @@
 // Copyright 2014 The Gogs Authors. All rights reserved.
-// Copyright 2019 The Gitea Authors. All rights reserved.
+// Copyright 2023 The Gitea Authors. All rights reserved.
 // SPDX-License-Identifier: MIT
 
 package log
