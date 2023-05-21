c71bbd8a78a7f99889be9c2658b5ec1bc8327034
silverwind <me@silverwind.io>
2023-05-21 22:40:24 +0200

    update migration number

--- models/migrations/migrations.go
+++ models/migrations/migrations.go
@@ -491,7 +491,7 @@ var migrations = []Migration{
 	NewMigration("Add ArchivedUnix Column", v1_20.AddArchivedUnixToRepository),
 	// v256 -> v257
 	NewMigration("Add is_internal column to package", v1_20.AddIsInternalColumnToPackage),
-	// v257 -> v258
+	// v258 -> v259
 	NewMigration("Add milestone labels", v1_20.AddMilestoneLabels),
 }
 
