1773a6a3526db91506ff172ef73802004e673420
wxiaoguang <wxiaoguang@gmail.com>
2023-05-23 12:01:46 +0800

    refactor & simplify

--- web_src/js/features/comp/ReactionSelector.js
+++ web_src/js/features/comp/ReactionSelector.js
@@ -1,40 +1,18 @@
 import $ from 'jquery';
-import {createTippy} from '../../modules/tippy.js';
 
 const {csrfToken} = window.config;
 
-export function initCompReactionSelector(parent) {
-  let selector = 'a.label';
-  if (!parent) {
-    parent = $(document);
-    selector = `.reactions ${selector}`;
-  }
-
-  for (const el of parent[0].querySelectorAll(selector)) {
-    createTippy(el, {
-      placement: 'bottom-start',
-      content: el.getAttribute('data-title'),
-      theme: 'tooltip',
-      hideOnClick: true,
-    });
-  }
-
-  parent.find(`.select-reaction > .menu .item, ${selector}`).on('click', async function (e) {
+export function initCompReactionSelector($parent) {
+  $parent.find(`.select-reaction .item.reaction, .comment-reaction-button`).on('click', async function (e) {
     e.preventDefault();
 
     if ($(this).hasClass('disabled')) return;
 
+    this._tippy?.hide();
+
+    const actionUrl = $(this).closest('[data-action-url]').attr('data-action-url');
     const reactionContent = $(this).attr('data-reaction-content');
-    let actionUrl, hasReacted;
-    if ($(this).hasClass('item')) { // in dropdown menu
-      actionUrl = $(this).closest('.select-reaction').data('action-url');
-      const parent = $(this).closest('.segment.reactions');
-      const el = parent.find(`[data-reaction-content="${reactionContent}"]`);
-      hasReacted = el.attr('data-has-reacted') === 'true';
-    } else { // outside of dropdown menu
-      actionUrl = $(this).data('action-url');
-      hasReacted = $(this).attr('data-has-reacted') === 'true';
-    }
+    const hasReacted = $(this).attr('data-has-reacted') === 'true';
 
     const res = await fetch(`${actionUrl}/${hasReacted ? 'unreact' : 'react'}`, {
       method: 'POST',
