a077bd0f7188cbf9396e43b43add272dcb672220
Jonathan Tran <jon@allspice.io>
2023-05-23 21:37:08 -0400

    Fix so that migration doesn't depend on non-migration code

--- models/migrations/v1_20/v259.go
+++ models/migrations/v1_20/v259.go
@@ -1,16 +1,249 @@
 // Copyright 2022 The Gitea Authors. All rights reserved.
 // SPDX-License-Identifier: MIT
 
-package v1_20 // nolint
+package v1_20 //nolint
 
 import (
 	"fmt"
 	"strings"
 
 	"xorm.io/xorm"
-
-	auth_model "code.gitea.io/gitea/models/auth"
 )
+
+// AccessTokenScope represents the scope for an access token.
+type AccessTokenScope string
+
+// for all categories, delete implies write, write implies read
+const (
+	AccessTokenScopeAll        AccessTokenScope = "all"
+	AccessTokenScopePublicOnly AccessTokenScope = "public" // limited to public orgs/repos
+
+	AccessTokenScopeReadActivityPub   AccessTokenScope = "read:activitypub"
+	AccessTokenScopeWriteActivityPub  AccessTokenScope = "write:activitypub"
+	AccessTokenScopeDeleteActivityPub AccessTokenScope = "delete:activitypub"
+
+	AccessTokenScopeReadAdmin   AccessTokenScope = "read:admin"
+	AccessTokenScopeWriteAdmin  AccessTokenScope = "write:admin"
+	AccessTokenScopeDeleteAdmin AccessTokenScope = "delete:admin"
+
+	AccessTokenScopeReadMisc   AccessTokenScope = "read:misc"
+	AccessTokenScopeWriteMisc  AccessTokenScope = "write:misc"
+	AccessTokenScopeDeleteMisc AccessTokenScope = "delete:misc"
+
+	AccessTokenScopeReadNotification   AccessTokenScope = "read:notification"
+	AccessTokenScopeWriteNotification  AccessTokenScope = "write:notification"
+	AccessTokenScopeDeleteNotification AccessTokenScope = "delete:notification"
+
+	AccessTokenScopeReadOrganization   AccessTokenScope = "read:organization"
+	AccessTokenScopeWriteOrganization  AccessTokenScope = "write:organization"
+	AccessTokenScopeDeleteOrganization AccessTokenScope = "delete:organization"
+
+	AccessTokenScopeReadPackage   AccessTokenScope = "read:package"
+	AccessTokenScopeWritePackage  AccessTokenScope = "write:package"
+	AccessTokenScopeDeletePackage AccessTokenScope = "delete:package"
+
+	AccessTokenScopeReadIssue   AccessTokenScope = "read:issue"
+	AccessTokenScopeWriteIssue  AccessTokenScope = "write:issue"
+	AccessTokenScopeDeleteIssue AccessTokenScope = "delete:issue"
+
+	AccessTokenScopeReadRepository   AccessTokenScope = "read:repository"
+	AccessTokenScopeWriteRepository  AccessTokenScope = "write:repository"
+	AccessTokenScopeDeleteRepository AccessTokenScope = "delete:repository"
+
+	AccessTokenScopeReadUser   AccessTokenScope = "read:user"
+	AccessTokenScopeWriteUser  AccessTokenScope = "write:user"
+	AccessTokenScopeDeleteUser AccessTokenScope = "delete:user"
+)
+
+// accessTokenScopeBitmap represents a bitmap of access token scopes.
+type accessTokenScopeBitmap uint64
+
+// Bitmap of each scope, including the child scopes.
+const (
+	// AccessTokenScopeAllBits is the bitmap of all access token scopes
+	accessTokenScopeAllBits accessTokenScopeBitmap = accessTokenScopeDeleteActivityPubBits |
+		accessTokenScopeDeleteAdminBits | accessTokenScopeDeleteMiscBits | accessTokenScopeDeleteNotificationBits |
+		accessTokenScopeDeleteOrganizationBits | accessTokenScopeDeletePackageBits | accessTokenScopeDeleteIssueBits |
+		accessTokenScopeDeleteRepositoryBits | accessTokenScopeDeleteUserBits
+
+	accessTokenScopePublicOnlyBits accessTokenScopeBitmap = 1 << iota
+
+	accessTokenScopeReadActivityPubBits   accessTokenScopeBitmap = 1 << iota
+	accessTokenScopeWriteActivityPubBits  accessTokenScopeBitmap = 1<<iota | accessTokenScopeReadActivityPubBits
+	accessTokenScopeDeleteActivityPubBits accessTokenScopeBitmap = 1<<iota | accessTokenScopeWriteActivityPubBits
+
+	accessTokenScopeReadAdminBits   accessTokenScopeBitmap = 1 << iota
+	accessTokenScopeWriteAdminBits  accessTokenScopeBitmap = 1<<iota | accessTokenScopeReadAdminBits
+	accessTokenScopeDeleteAdminBits accessTokenScopeBitmap = 1<<iota | accessTokenScopeWriteAdminBits
+
+	accessTokenScopeReadMiscBits   accessTokenScopeBitmap = 1 << iota
+	accessTokenScopeWriteMiscBits  accessTokenScopeBitmap = 1<<iota | accessTokenScopeReadMiscBits
+	accessTokenScopeDeleteMiscBits accessTokenScopeBitmap = 1<<iota | accessTokenScopeWriteMiscBits
+
+	accessTokenScopeReadNotificationBits   accessTokenScopeBitmap = 1 << iota
+	accessTokenScopeWriteNotificationBits  accessTokenScopeBitmap = 1<<iota | accessTokenScopeReadNotificationBits
+	accessTokenScopeDeleteNotificationBits accessTokenScopeBitmap = 1<<iota | accessTokenScopeWriteNotificationBits
+
+	accessTokenScopeReadOrganizationBits   accessTokenScopeBitmap = 1 << iota
+	accessTokenScopeWriteOrganizationBits  accessTokenScopeBitmap = 1<<iota | accessTokenScopeReadOrganizationBits
+	accessTokenScopeDeleteOrganizationBits accessTokenScopeBitmap = 1<<iota | accessTokenScopeWriteOrganizationBits
+
+	accessTokenScopeReadPackageBits   accessTokenScopeBitmap = 1 << iota
+	accessTokenScopeWritePackageBits  accessTokenScopeBitmap = 1<<iota | accessTokenScopeReadPackageBits
+	accessTokenScopeDeletePackageBits accessTokenScopeBitmap = 1<<iota | accessTokenScopeWritePackageBits
+
+	accessTokenScopeReadIssueBits   accessTokenScopeBitmap = 1 << iota
+	accessTokenScopeWriteIssueBits  accessTokenScopeBitmap = 1<<iota | accessTokenScopeReadIssueBits
+	accessTokenScopeDeleteIssueBits accessTokenScopeBitmap = 1<<iota | accessTokenScopeWriteIssueBits
+
+	accessTokenScopeReadRepositoryBits   accessTokenScopeBitmap = 1 << iota
+	accessTokenScopeWriteRepositoryBits  accessTokenScopeBitmap = 1<<iota | accessTokenScopeReadRepositoryBits
+	accessTokenScopeDeleteRepositoryBits accessTokenScopeBitmap = 1<<iota | accessTokenScopeWriteRepositoryBits
+
+	accessTokenScopeReadUserBits   accessTokenScopeBitmap = 1 << iota
+	accessTokenScopeWriteUserBits  accessTokenScopeBitmap = 1<<iota | accessTokenScopeReadUserBits
+	accessTokenScopeDeleteUserBits accessTokenScopeBitmap = 1<<iota | accessTokenScopeWriteUserBits
+
+	// The current implementation only supports up to 64 token scopes.
+	// If we need to support > 64 scopes,
+	// refactoring the whole implementation in this file (and only this file) is needed.
+)
+
+// allAccessTokenScopes contains all access token scopes.
+// The order is important: parent scope must precede child scopes.
+var allAccessTokenScopes = []AccessTokenScope{
+	AccessTokenScopePublicOnly,
+	AccessTokenScopeDeleteActivityPub, AccessTokenScopeWriteActivityPub, AccessTokenScopeReadActivityPub,
+	AccessTokenScopeDeleteAdmin, AccessTokenScopeWriteAdmin, AccessTokenScopeReadAdmin,
+	AccessTokenScopeDeleteMisc, AccessTokenScopeWriteMisc, AccessTokenScopeReadMisc,
+	AccessTokenScopeDeleteNotification, AccessTokenScopeWriteNotification, AccessTokenScopeReadNotification,
+	AccessTokenScopeDeleteOrganization, AccessTokenScopeWriteOrganization, AccessTokenScopeReadOrganization,
+	AccessTokenScopeDeletePackage, AccessTokenScopeWritePackage, AccessTokenScopeReadPackage,
+	AccessTokenScopeDeleteIssue, AccessTokenScopeWriteIssue, AccessTokenScopeReadIssue,
+	AccessTokenScopeDeleteRepository, AccessTokenScopeWriteRepository, AccessTokenScopeReadRepository,
+	AccessTokenScopeDeleteUser, AccessTokenScopeWriteUser, AccessTokenScopeReadUser,
+}
+
+// allAccessTokenScopeBits contains all access token scopes.
+var allAccessTokenScopeBits = map[AccessTokenScope]accessTokenScopeBitmap{
+	AccessTokenScopeAll:                accessTokenScopeAllBits,
+	AccessTokenScopePublicOnly:         accessTokenScopePublicOnlyBits,
+	AccessTokenScopeReadActivityPub:    accessTokenScopeReadActivityPubBits,
+	AccessTokenScopeWriteActivityPub:   accessTokenScopeWriteActivityPubBits,
+	AccessTokenScopeDeleteActivityPub:  accessTokenScopeDeleteActivityPubBits,
+	AccessTokenScopeReadAdmin:          accessTokenScopeReadAdminBits,
+	AccessTokenScopeWriteAdmin:         accessTokenScopeWriteAdminBits,
+	AccessTokenScopeDeleteAdmin:        accessTokenScopeDeleteAdminBits,
+	AccessTokenScopeReadMisc:           accessTokenScopeReadMiscBits,
+	AccessTokenScopeWriteMisc:          accessTokenScopeWriteMiscBits,
+	AccessTokenScopeDeleteMisc:         accessTokenScopeDeleteMiscBits,
+	AccessTokenScopeReadNotification:   accessTokenScopeReadNotificationBits,
+	AccessTokenScopeWriteNotification:  accessTokenScopeWriteNotificationBits,
+	AccessTokenScopeDeleteNotification: accessTokenScopeDeleteNotificationBits,
+	AccessTokenScopeReadOrganization:   accessTokenScopeReadOrganizationBits,
+	AccessTokenScopeWriteOrganization:  accessTokenScopeWriteOrganizationBits,
+	AccessTokenScopeDeleteOrganization: accessTokenScopeDeleteOrganizationBits,
+	AccessTokenScopeReadPackage:        accessTokenScopeReadPackageBits,
+	AccessTokenScopeWritePackage:       accessTokenScopeWritePackageBits,
+	AccessTokenScopeDeletePackage:      accessTokenScopeDeletePackageBits,
+	AccessTokenScopeReadIssue:          accessTokenScopeReadIssueBits,
+	AccessTokenScopeWriteIssue:         accessTokenScopeWriteIssueBits,
+	AccessTokenScopeDeleteIssue:        accessTokenScopeDeleteIssueBits,
+	AccessTokenScopeReadRepository:     accessTokenScopeReadRepositoryBits,
+	AccessTokenScopeWriteRepository:    accessTokenScopeWriteRepositoryBits,
+	AccessTokenScopeDeleteRepository:   accessTokenScopeDeleteRepositoryBits,
+	AccessTokenScopeReadUser:           accessTokenScopeReadUserBits,
+	AccessTokenScopeWriteUser:          accessTokenScopeWriteUserBits,
+	AccessTokenScopeDeleteUser:         accessTokenScopeDeleteUserBits,
+}
+
+// hasScope returns true if the string has the given scope
+func (bitmap accessTokenScopeBitmap) hasScope(scope AccessTokenScope) (bool, error) {
+	expectedBits, ok := allAccessTokenScopeBits[scope]
+	if !ok {
+		return false, fmt.Errorf("invalid access token scope: %s", scope)
+	}
+
+	return bitmap&expectedBits == expectedBits, nil
+}
+
+// toScope returns a normalized scope string without any duplicates.
+func (bitmap accessTokenScopeBitmap) toScope() AccessTokenScope {
+	var scopes []string
+
+	// iterate over all scopes, and reconstruct the bitmap
+	// if the reconstructed bitmap doesn't change, then the scope is already included
+	var reconstruct accessTokenScopeBitmap
+
+	for _, singleScope := range allAccessTokenScopes {
+		// no need for error checking here, since we know the scope is valid
+		if ok, _ := bitmap.hasScope(singleScope); ok {
+			current := reconstruct | allAccessTokenScopeBits[singleScope]
+			if current == reconstruct {
+				continue
+			}
+
+			reconstruct = current
+			scopes = append(scopes, string(singleScope))
+		}
+	}
+
+	scope := AccessTokenScope(strings.Join(scopes, ","))
+	scope = AccessTokenScope(strings.ReplaceAll(
+		string(scope),
+		"delete:activitypub,delete:admin,delete:misc,delete:notification,delete:organization,delete:package,delete:issue,delete:repository,delete:user",
+		"all",
+	))
+	return scope
+}
+
+// parse the scope string into a bitmap, thus removing possible duplicates.
+func (s AccessTokenScope) parse() (accessTokenScopeBitmap, error) {
+	var bitmap accessTokenScopeBitmap
+
+	// The following is the more performant equivalent of 'for _, v := range strings.Split(remainingScope, ",")' as this is hot code
+	remainingScopes := string(s)
+	for len(remainingScopes) > 0 {
+		i := strings.IndexByte(remainingScopes, ',')
+		var v string
+		if i < 0 {
+			v = remainingScopes
+			remainingScopes = ""
+		} else if i+1 >= len(remainingScopes) {
+			v = remainingScopes[:i]
+			remainingScopes = ""
+		} else {
+			v = remainingScopes[:i]
+			remainingScopes = remainingScopes[i+1:]
+		}
+		singleScope := AccessTokenScope(v)
+		if singleScope == "" {
+			continue
+		}
+		if singleScope == AccessTokenScopeAll {
+			bitmap |= accessTokenScopeAllBits
+			continue
+		}
+
+		bits, ok := allAccessTokenScopeBits[singleScope]
+		if !ok {
+			return 0, fmt.Errorf("invalid access token scope: %s", singleScope)
+		}
+		bitmap |= bits
+	}
+
+	return bitmap, nil
+}
+
+// Normalize returns a normalized scope string without any duplicates.
+func (s AccessTokenScope) Normalize() (AccessTokenScope, error) {
+	bitmap, err := s.parse()
+	if err != nil {
+		return "", err
+	}
+
+	return bitmap.toScope(), nil
+}
 
 // OldAccessTokenScope represents the scope for an access token.
 type OldAccessTokenScope string
@@ -61,38 +294,38 @@ const (
 	OldAccessTokenScopeSudo OldAccessTokenScope = "sudo"
 )
 
-var accessTokenScopeMap = map[OldAccessTokenScope][]auth_model.AccessTokenScope{
-	OldAccessTokenScopeAll:              {auth_model.AccessTokenScopeAll},
-	OldAccessTokenScopeRepo:             {auth_model.AccessTokenScopeDeleteRepository},
-	OldAccessTokenScopeRepoStatus:       {auth_model.AccessTokenScopeWriteRepository},
-	OldAccessTokenScopePublicRepo:       {auth_model.AccessTokenScopeDeleteRepository, auth_model.AccessTokenScopePublicOnly},
-	OldAccessTokenScopeAdminOrg:         {auth_model.AccessTokenScopeDeleteOrganization},
-	OldAccessTokenScopeWriteOrg:         {auth_model.AccessTokenScopeWriteOrganization},
-	OldAccessTokenScopeReadOrg:          {auth_model.AccessTokenScopeReadOrganization},
-	OldAccessTokenScopeAdminPublicKey:   {auth_model.AccessTokenScopeDeleteUser},
-	OldAccessTokenScopeWritePublicKey:   {auth_model.AccessTokenScopeWriteUser},
-	OldAccessTokenScopeReadPublicKey:    {auth_model.AccessTokenScopeReadUser},
-	OldAccessTokenScopeAdminRepoHook:    {auth_model.AccessTokenScopeDeleteRepository},
-	OldAccessTokenScopeWriteRepoHook:    {auth_model.AccessTokenScopeWriteRepository},
-	OldAccessTokenScopeReadRepoHook:     {auth_model.AccessTokenScopeReadRepository},
-	OldAccessTokenScopeAdminOrgHook:     {auth_model.AccessTokenScopeWriteOrganization},
-	OldAccessTokenScopeNotification:     {auth_model.AccessTokenScopeDeleteNotification},
-	OldAccessTokenScopeUser:             {auth_model.AccessTokenScopeDeleteUser},
-	OldAccessTokenScopeReadUser:         {auth_model.AccessTokenScopeReadUser},
-	OldAccessTokenScopeUserEmail:        {auth_model.AccessTokenScopeWriteUser},
-	OldAccessTokenScopeUserFollow:       {auth_model.AccessTokenScopeWriteUser},
-	OldAccessTokenScopeDeleteRepo:       {auth_model.AccessTokenScopeDeleteRepository},
-	OldAccessTokenScopePackage:          {auth_model.AccessTokenScopeDeletePackage},
-	OldAccessTokenScopeWritePackage:     {auth_model.AccessTokenScopeWritePackage},
-	OldAccessTokenScopeReadPackage:      {auth_model.AccessTokenScopeReadPackage},
-	OldAccessTokenScopeDeletePackage:    {auth_model.AccessTokenScopeDeletePackage},
-	OldAccessTokenScopeAdminGPGKey:      {auth_model.AccessTokenScopeDeleteUser},
-	OldAccessTokenScopeWriteGPGKey:      {auth_model.AccessTokenScopeWriteUser},
-	OldAccessTokenScopeReadGPGKey:       {auth_model.AccessTokenScopeReadUser},
-	OldAccessTokenScopeAdminApplication: {auth_model.AccessTokenScopeDeleteUser},
-	OldAccessTokenScopeWriteApplication: {auth_model.AccessTokenScopeWriteUser},
-	OldAccessTokenScopeReadApplication:  {auth_model.AccessTokenScopeReadUser},
-	OldAccessTokenScopeSudo:             {auth_model.AccessTokenScopeDeleteAdmin},
+var accessTokenScopeMap = map[OldAccessTokenScope][]AccessTokenScope{
+	OldAccessTokenScopeAll:              {AccessTokenScopeAll},
+	OldAccessTokenScopeRepo:             {AccessTokenScopeDeleteRepository},
+	OldAccessTokenScopeRepoStatus:       {AccessTokenScopeWriteRepository},
+	OldAccessTokenScopePublicRepo:       {AccessTokenScopeDeleteRepository, AccessTokenScopePublicOnly},
+	OldAccessTokenScopeAdminOrg:         {AccessTokenScopeDeleteOrganization},
+	OldAccessTokenScopeWriteOrg:         {AccessTokenScopeWriteOrganization},
+	OldAccessTokenScopeReadOrg:          {AccessTokenScopeReadOrganization},
+	OldAccessTokenScopeAdminPublicKey:   {AccessTokenScopeDeleteUser},
+	OldAccessTokenScopeWritePublicKey:   {AccessTokenScopeWriteUser},
+	OldAccessTokenScopeReadPublicKey:    {AccessTokenScopeReadUser},
+	OldAccessTokenScopeAdminRepoHook:    {AccessTokenScopeDeleteRepository},
+	OldAccessTokenScopeWriteRepoHook:    {AccessTokenScopeWriteRepository},
+	OldAccessTokenScopeReadRepoHook:     {AccessTokenScopeReadRepository},
+	OldAccessTokenScopeAdminOrgHook:     {AccessTokenScopeWriteOrganization},
+	OldAccessTokenScopeNotification:     {AccessTokenScopeDeleteNotification},
+	OldAccessTokenScopeUser:             {AccessTokenScopeDeleteUser},
+	OldAccessTokenScopeReadUser:         {AccessTokenScopeReadUser},
+	OldAccessTokenScopeUserEmail:        {AccessTokenScopeWriteUser},
+	OldAccessTokenScopeUserFollow:       {AccessTokenScopeWriteUser},
+	OldAccessTokenScopeDeleteRepo:       {AccessTokenScopeDeleteRepository},
+	OldAccessTokenScopePackage:          {AccessTokenScopeDeletePackage},
+	OldAccessTokenScopeWritePackage:     {AccessTokenScopeWritePackage},
+	OldAccessTokenScopeReadPackage:      {AccessTokenScopeReadPackage},
+	OldAccessTokenScopeDeletePackage:    {AccessTokenScopeDeletePackage},
+	OldAccessTokenScopeAdminGPGKey:      {AccessTokenScopeDeleteUser},
+	OldAccessTokenScopeWriteGPGKey:      {AccessTokenScopeWriteUser},
+	OldAccessTokenScopeReadGPGKey:       {AccessTokenScopeReadUser},
+	OldAccessTokenScopeAdminApplication: {AccessTokenScopeDeleteUser},
+	OldAccessTokenScopeWriteApplication: {AccessTokenScopeWriteUser},
+	OldAccessTokenScopeReadApplication:  {AccessTokenScopeReadUser},
+	OldAccessTokenScopeSudo:             {AccessTokenScopeDeleteAdmin},
 }
 
 type AccessToken struct {
@@ -108,7 +341,7 @@ func ConvertScopedAccessTokens(x *xorm.Engine) error {
 	}
 
 	for _, token := range tokens {
-		allNewScopesMap := make(map[auth_model.AccessTokenScope]bool)
+		allNewScopesMap := make(map[AccessTokenScope]bool)
 		for _, oldScope := range strings.Split(token.Scope, ",") {
 			if newScopes, exists := accessTokenScopeMap[OldAccessTokenScope(oldScope)]; exists {
 				for _, newScope := range newScopes {
@@ -123,7 +356,7 @@ func ConvertScopedAccessTokens(x *xorm.Engine) error {
 		for s := range allNewScopesMap {
 			scopes = append(scopes, string(s))
 		}
-		scope := auth_model.AccessTokenScope(strings.Join(scopes, ","))
+		scope := AccessTokenScope(strings.Join(scopes, ","))
 
 		// normalize the scope
 		normScope, err := scope.Normalize()
