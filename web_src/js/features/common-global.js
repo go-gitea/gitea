ed52ecac4b261451a64edadfe114e5e05a9acd38
silverwind <me@silverwind.io>
2023-06-08 12:37:38 +0200

    rename shown to navbar-menu-open, refactor to vanilla js

--- web_src/js/features/common-global.js
+++ web_src/js/features/common-global.js
@@ -20,18 +20,13 @@ export function initGlobalFormDirtyLeaveConfirm() {
 }
 
 export function initHeadNavbarContentToggle() {
-  const content = $('#navbar');
-  const toggle = $('#navbar-expand-toggle');
+  const navbar = document.getElementById('navbar');
+  const btn = document.getElementById('navbar-expand-toggle');
   let isExpanded = false;
-  toggle.on('click', () => {
+  btn.addEventListener('click', () => {
     isExpanded = !isExpanded;
-    if (isExpanded) {
-      content.addClass('shown');
-      toggle.addClass('active');
-    } else {
-      content.removeClass('shown');
-      toggle.removeClass('active');
-    }
+    navbar.classList.toggle('navbar-menu-open', isExpanded);
+    btn.classList.toggle('active', isExpanded);
   });
 }
 
