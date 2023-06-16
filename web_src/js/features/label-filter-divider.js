ef0294dac14af4f71e9e97359f03e511a4d01bb3
HesterG <hestergong@gmail.com>
2023-06-16 14:26:28 +0800

    change name

--- web_src/js/features/label-filter-divider.js
+++ web_src/js/features/label-filter-divider.js
@@ -1,8 +1,8 @@
 import {toggleElem, onInputDebounce} from '../utils/dom.js';
 
 export function initLabelSearchInput() {
-  if (!document.querySelector('.labels-filter-menu')) return;
   const menu = document.querySelector('.labels-filter-menu');
+  if (!menu) return;
   // toggle dividers according to filtered results
   menu.querySelector('.labels-filter-input').addEventListener('input', onInputDebounce(() => {
     for (const divider of menu.querySelectorAll('[data-divider-index]')) {
