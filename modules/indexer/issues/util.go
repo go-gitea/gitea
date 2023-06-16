836ccf08b420bbedea2b90b0a3619fc2e87c005d
Jason Song <i@wolfogre.com>
2023-06-16 14:51:00 +0800

    feat: query IndexerData

--- modules/indexer/issues/util.go
+++ modules/indexer/issues/util.go
@@ -6,10 +6,39 @@ package issues
 import (
 	"context"
 
+	issue_model "code.gitea.io/gitea/models/issues"
 	"code.gitea.io/gitea/modules/indexer/issues/internal"
 )
 
 func getIssueIndexerData(ctx context.Context, issueID int64) (*internal.IndexerData, bool, error) {
-	// TODO
-	return nil, false, nil
+	issue, err := issue_model.GetIssueByID(ctx, issueID)
+	if err != nil {
+		if issue_model.IsErrIssueNotExist(err) {
+			return nil, false, nil
+		}
+		return nil, false, err
+	}
+
+	// FIXME: what if users want to search for a review comment of a pull request?
+	//        The comment type is CommentTypeCode or CommentTypeReview.
+	//        But LoadDiscussComments only loads CommentTypeComment.
+	if err := issue.LoadDiscussComments(ctx); err != nil {
+		return nil, false, err
+	}
+
+	comments := make([]string, 0, len(issue.Comments))
+	for _, comment := range issue.Comments {
+		if comment.Content != "" {
+			// what ever the comment type is, index the content if it is not empty.
+			comments = append(comments, comment.Content)
+		}
+	}
+
+	return &internal.IndexerData{
+		ID:       issue.ID,
+		RepoID:   issue.RepoID,
+		Title:    issue.Title,
+		Content:  issue.Content,
+		Comments: comments,
+	}, true, nil
 }
