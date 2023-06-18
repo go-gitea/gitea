012978163895458adf3283cea7d4e210144455b4
wxiaoguang <wxiaoguang@gmail.com>
2023-06-19 00:00:36 +0800

    fix typo

--- main.go
+++ main.go
@@ -113,7 +113,7 @@ func main() {
 		cli.StringFlag{
 			Name:  "config, c",
 			Value: setting.CustomConf,
-			Usage: "Set custom config file (default to '{WorkPath}/custom/app.ini')",
+			Usage: "Set custom config file (default to '{WorkPath}/custom/conf/app.ini')",
 		},
 		cli.StringFlag{
 			Name:  "work-path, w",
