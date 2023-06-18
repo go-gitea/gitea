a99f8f1e463e0dad199d8ecdeabd58e6656d2b7d
wxiaoguang <wxiaoguang@gmail.com>
2023-06-18 21:17:51 +0800

    tests

--- modules/setting/path.go
+++ modules/setting/path.go
@@ -21,9 +21,12 @@ var (
 	// If that is not set it is the default set here by the linker or failing that the directory of AppPath.
 	// It is used as the base path for several other paths.
 	AppWorkPath string
+	CustomPath  string // Custom directory path. Env: GITEA_CUSTOM
+	CustomConf  string
 
-	CustomPath string // Custom directory path. Env: GITEA_CUSTOM
-	CustomConf string
+	appWorkPathBuiltin string
+	customPathBuiltin  string
+	customConfBuiltin  string
 )
 
 func getAppPath() (string, error) {
@@ -56,8 +59,14 @@ func init() {
 	if AppPath, err = getAppPath(); err != nil {
 		log.Fatal("Failed to get app path: %v", err)
 	}
-	AppWorkPath = filepath.Dir(AppPath)
-	CustomPath = filepath.Join(AppWorkPath, "custom")
+
+	if AppWorkPath == "" {
+		AppWorkPath = filepath.Dir(AppPath)
+	}
+
+	appWorkPathBuiltin = AppWorkPath
+	customPathBuiltin = CustomPath
+	customConfBuiltin = CustomConf
 }
 
 type ArgWorkPathAndCustomConf struct {
@@ -66,56 +75,80 @@ type ArgWorkPathAndCustomConf struct {
 	CustomConf string
 }
 
+type stringWithDefault struct {
+	Value string
+	IsSet bool
+}
+
+func (s *stringWithDefault) Set(v string) {
+	s.Value = v
+	s.IsSet = true
+}
+
 // InitWorkPathAndCommonConfig will set AppWorkPath, CustomPath and CustomConf, init default config provider by CustomConf and load common settings,
 func InitWorkPathAndCommonConfig(getEnvFn func(name string) string, args ArgWorkPathAndCustomConf) {
+	tryAbsPath := func(paths ...string) string {
+		s := paths[len(paths)-1]
+		for i := len(paths) - 2; i >= 0; i-- {
+			if filepath.IsAbs(s) {
+				break
+			}
+			s = filepath.Join(paths[i], s)
+		}
+		return s
+	}
+
 	var err error
-	var tmpWorkPath, tmpCustomPath, tmpCustomConf string
+	tmpWorkPath := stringWithDefault{Value: appWorkPathBuiltin}
+	if tmpWorkPath.Value == "" {
+		tmpWorkPath.Value = filepath.Dir(AppPath)
+	}
+	tmpCustomPath := stringWithDefault{Value: customPathBuiltin}
+	if tmpCustomPath.Value == "" {
+		tmpCustomPath.Value = "custom"
+	}
+	tmpCustomConf := stringWithDefault{Value: customConfBuiltin}
+	if tmpCustomConf.Value == "" {
+		tmpCustomConf.Value = "conf/app.ini"
+	}
 
 	readFromEnv := func() {
-		// for consistency, prefer to use GITEA_WORK_PATH and GITEA_CUSTOM_PATH
-		// the widely used GITEA_WORK_DIR and GITEA_CUSTOM are also supported
-		envWorkPath := getEnvFn("GITEA_WORK_PATH")
-		if envWorkPath == "" {
-			envWorkPath = getEnvFn("GITEA_WORK_DIR")
-		}
+		envWorkPath := getEnvFn("GITEA_WORK_DIR")
 		if envWorkPath != "" {
-			tmpWorkPath = envWorkPath
-			if !filepath.IsAbs(tmpWorkPath) {
-				log.Fatal("GITEA_WORK_PATH must be absolute path")
+			tmpWorkPath.Set(envWorkPath)
+			if !filepath.IsAbs(tmpWorkPath.Value) {
+				log.Fatal("GITEA_WORK_DIR (work path) must be absolute path")
 			}
 		}
 
-		envCustomPath := getEnvFn("GITEA_CUSTOM_PATH")
-		if envCustomPath == "" {
-			envCustomPath = getEnvFn("GITEA_CUSTOM")
-		}
+		envCustomPath := getEnvFn("GITEA_CUSTOM")
 		if envCustomPath != "" {
-			tmpCustomPath = envCustomPath
-			if !filepath.IsAbs(tmpCustomPath) {
-				log.Fatal("GITEA_CUSTOM_PATH must be absolute path")
+			tmpCustomPath.Set(envCustomPath)
+			if !filepath.IsAbs(tmpCustomPath.Value) {
+				log.Fatal("GITEA_CUSTOM (custom path) must be absolute path")
 			}
 		}
 	}
 
 	readFromArgs := func() {
 		if args.WorkPath != "" {
-			tmpWorkPath = args.WorkPath
-			if !filepath.IsAbs(tmpWorkPath) {
+			tmpWorkPath.Set(args.WorkPath)
+			if !filepath.IsAbs(tmpWorkPath.Value) {
 				log.Fatal("--work-path must be absolute path")
 			}
 		}
 		if args.CustomPath != "" {
-			tmpCustomPath = args.CustomPath // if it is not abs, it will be based on work-path, it shouldn't happen
-			if !filepath.IsAbs(tmpCustomPath) {
+			tmpCustomPath.Set(args.CustomPath) // if it is not abs, it will be based on work-path, it shouldn't happen
+			if !filepath.IsAbs(tmpCustomPath.Value) {
 				log.Error("--custom-path must be absolute path")
 			}
 		}
 		if args.CustomConf != "" {
-			tmpCustomConf = args.CustomConf
-			if !filepath.IsAbs(tmpCustomConf) {
+			tmpCustomConf.Set(args.CustomConf)
+			if !filepath.IsAbs(tmpCustomConf.Value) {
 				// the config path can be relative to the real current working path
-				if tmpCustomConf, err = filepath.Abs(tmpCustomConf); err != nil {
-					log.Fatal("Failed to get absolute path of config %q: %v", tmpCustomConf, err)
+				if tmpCustomConf.Value, err = filepath.Abs(tmpCustomConf.Value); err != nil {
+					log.Fatal("Failed to get absolute path of config %q: %v", tmpCustomConf.Value, err)
 				}
 			}
 		}
@@ -124,57 +157,34 @@ func InitWorkPathAndCommonConfig(getEnvFn func(name string) string, args ArgWork
 	readFromEnv()
 	readFromArgs()
 
-	if tmpCustomConf == "" {
-		// need to guess the config path
-		if tmpWorkPath == "" && tmpCustomPath == "" {
-			// we don't have any info, so we guess it is in AppWorkPath
-			tmpCustomConf = filepath.Join(AppWorkPath, "custom/conf/app.ini")
-		} else if tmpWorkPath != "" {
-			tmpCustomConf = filepath.Join(tmpWorkPath, "custom/conf/app.ini")
-		} else {
-			if filepath.IsAbs(tmpCustomPath) {
-				tmpCustomConf = filepath.Join(tmpCustomPath, "conf/app.ini")
-			} else {
-				tmpCustomConf = filepath.Join(AppWorkPath, tmpCustomPath, "conf/app.ini")
-			}
-		}
+	if !tmpCustomConf.IsSet {
+		tmpCustomConf.Set(tryAbsPath(tmpWorkPath.Value, tmpCustomPath.Value, tmpCustomConf.Value))
 	}
 
 	// only read the config but do not load/init anything more, because the AppWorkPath and CustomPath are not ready
-	InitCfgProvider(tmpCustomConf)
+	InitCfgProvider(tmpCustomConf.Value)
 	configWorkPath := ConfigSectionKeyString(CfgProvider.Section(""), "WORK_PATH")
 	if configWorkPath != "" {
 		if !filepath.IsAbs(configWorkPath) {
 			log.Fatal("WORK_PATH in %q must be absolute path", configWorkPath)
 		}
 		configWorkPath = filepath.Clean(configWorkPath)
-		if tmpWorkPath != "" {
-			envWorkPath := filepath.Clean(tmpWorkPath)
-			_, err1 := filepath.Rel(configWorkPath, envWorkPath)
-			_, err2 := filepath.Rel(envWorkPath, configWorkPath)
-			if err1 != nil || err2 != nil {
+		if tmpWorkPath.Value != "" {
+			fi1, err1 := os.Stat(tmpWorkPath.Value)
+			fi2, err2 := os.Stat(configWorkPath)
+			if err1 != nil || err2 != nil || !os.SameFile(fi1, fi2) {
 				log.Error("WORK_PATH from config %q doesn't match other paths from environment variables or command arguments."+
 					"Only WORK_PATH in config should be set and used. Please remove the other outdated work paths from environment variables and command arguments", tmpCustomConf)
 			}
 		}
-		tmpWorkPath = configWorkPath
+		tmpWorkPath.Set(configWorkPath)
 	}
 
-	if tmpWorkPath == "" {
-		tmpWorkPath = filepath.Clean(AppWorkPath)
-	}
+	tmpCustomPath.Set(tryAbsPath(tmpWorkPath.Value, tmpCustomPath.Value))
 
-	if tmpCustomPath == "" {
-		tmpCustomPath = filepath.Join(tmpWorkPath, "custom")
-	} else if !filepath.IsAbs(tmpCustomPath) {
-		tmpCustomPath = filepath.Join(tmpWorkPath, tmpCustomPath)
-	} else {
-		tmpCustomPath = filepath.Clean(tmpCustomPath)
-	}
-
-	AppWorkPath = tmpWorkPath
-	CustomPath = tmpCustomPath
-	CustomConf = tmpCustomConf
+	AppWorkPath = tmpWorkPath.Value
+	CustomPath = tmpCustomPath.Value
+	CustomConf = tmpCustomConf.Value
 
 	LoadCommonSettings()
 }
