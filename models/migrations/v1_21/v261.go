844af9a7234804151be258ed87e3c31f9ab97b01
a1012112796 <1012112796@qq.com>
2023-06-20 09:45:53 +0000

    update migration
    Signed-off-by: a1012112796 <1012112796@qq.com>

--- models/migrations/v1_21/v261.go
+++ models/migrations/v1_21/v261.go
@@ -4,6 +4,8 @@
 package v1_21 //nolint
 
 import (
+	"fmt"
+
 	"xorm.io/xorm"
 )
 
@@ -14,5 +16,26 @@ func AddGitSizeAndLFSSizeToRepositoryTable(x *xorm.Engine) error {
 		LFSSize int64 `xorm:"NOT NULL DEFAULT 0"`
 	}
 
-	return x.Sync2(new(Repository))
+	sess := x.NewSession()
+	defer sess.Close()
+
+	if err := sess.Begin(); err != nil {
+		return err
+	}
+
+	if err := sess.Sync2(new(Repository)); err != nil {
+		return fmt.Errorf("Sync2: %w", err)
+	}
+
+	_, err := sess.Exec(`UPDATE repository SET lfs_size=(SELECT SUM(size) FROM lfs_meta_object WHERE lfs_meta_object.repository_id=repository.ID) WHERE EXISTS (SELECT 1 FROM lfs_meta_object WHERE lfs_meta_object.repository_id=repository.ID)`)
+	if err != nil {
+		return err
+	}
+
+	_, err = sess.Exec(`UPDATE repository SET git_size = size - lfs_size`)
+	if err != nil {
+		return err
+	}
+
+	return sess.Commit()
 }
