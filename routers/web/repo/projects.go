9bbb4d8d6d812d395d16d9a60ae55338608df73d
Giteabot <teabot@gitea.io>
2023-06-25 22:06:58 -0400

    Improve loadprojects for issue list (#25468) (#25493)

--- routers/web/repo/projects.go
+++ routers/web/repo/projects.go
@@ -378,9 +378,14 @@ func UpdateIssueProject(ctx *context.Context) {
 		return
 	}
 
+	if err := issues.LoadProjects(ctx); err != nil {
+		ctx.ServerError("LoadProjects", err)
+		return
+	}
+
 	projectID := ctx.FormInt64("id")
 	for _, issue := range issues {
-		oldProjectID := issue.ProjectID()
+		oldProjectID := issue.Project.ID
 		if oldProjectID == projectID {
 			continue
 		}
