f7da797dbc5e07a197b7d730bfd4c16517710f87
yp05327 <576951401@qq.com>
2023-06-26 01:40:02 +0000

    fix conflicts

--- models/migrations/migrations.go
+++ models/migrations/migrations.go
@@ -503,7 +503,9 @@ var migrations = []Migration{
 
 	// v260 -> v261
 	NewMigration("Drop custom_labels column of action_runner table", v1_21.DropCustomLabelsColumnOfActionRunner),
-	// v261 -> 262
+	// v261 -> v262
+	NewMigration("Add variable table", v1_21.CreateVariableTable),
+	// v262 -> v263
 	NewMigration("Add Repository Licenses", v1_21.AddRepositoryLicenses),
 }
 
