4d1a4bfae7450b0a02f46213d69d6f6a41f26f35
Sebastian Sauer <sauer.sebastian@gmail.com>
2023-07-04 20:20:50 +0200

    Improve var naming and add comments

--- web_src/js/components/DiffCommitSelector.vue
+++ web_src/js/components/DiffCommitSelector.vue
@@ -2,7 +2,8 @@
   <div
     class="ui jump dropdown basic button custom"
     @click.stop="menuVisible = !menuVisible" @keyup.enter="menuVisible = !menuVisible"
-    :data-tooltip-content="locale.filter_changes_by_commit">
+    :data-tooltip-content="locale.filter_changes_by_commit"
+  >
     <svg-icon name="octicon-git-commit"/>
     <div class="menu left transition commit-selector-menu" :class="{visible: menuVisible}" v-if="menuVisible" v-cloak>
       <a class="vertical item gt-df gt-fc gt-gap-1" :href="issueLink + '/files' + queryParams">
@@ -12,7 +13,7 @@
 
       <template v-for="commit in commits" :key="commit.ID">
         <div class="divider"/>
-        <div class="vertical item gt-df gt-fr gt-gap-2" :class="{hovered: commit.Hovered}" @click.exact="commitClicked(commit.ID)" @click.shift.exact.stop.prevent="commitClickedShift(commit)">
+        <div class="vertical item gt-df gt-fr gt-gap-2" :class="{selected: commit.Selected}" @click.exact="commitClicked(commit.ID)" @click.shift.exact.stop.prevent="commitClickedShift(commit)">
           <div class="gt-f1 gt-df gt-fc gt-gap-1">
             <div class="gt-ellipsis commit-list-summary">
               {{ commit.Summary }}
@@ -24,7 +25,9 @@
               </span>
             </div>
           </div>
-          <div class="gt-mono">{{ commit.ID }}</div>
+          <div class="gt-mono">
+            {{ commit.ID }}
+          </div>
         </div>
       </template>
     </div>
@@ -47,21 +50,31 @@ export default {
     };
   },
   methods: {
+    /** Clicking on a single commit opens this specific commit */
     commitClicked(commitId) {
-      window.location = `${this.issueLink}/files/${commitId}${this.queryParams}`;
+      window.location = `${this.issueLink}/commits/${commitId}${this.queryParams}`;
     },
+    /**
+     * When a commit is clicked with shift this enables the range
+     * selection. Second click (with shift) defines the end of the
+     * range. This opens the diff of this range
+     * Exception: first commit is the first commit of this PR. Then
+     * the diff from beginning of PR up to the second clicked commit is
+     * opened
+     */
     commitClickedShift(commit) {
       this.hoverActivated = !this.hoverActivated;
-      commit.Hovered = true;
+      commit.Selected = true;
+      // Second click -> determine our range and open links accordingly
       if (!this.hoverActivated) {
-        // find all hovered commits and generate a link
-        if (this.commits[0].Hovered) {
-          // first commit is hovered - generate a short url with only target sha
-          const lastCommit = this.commits.findLast((x) => x.Hovered);
+        // find all selected commits and generate a link
+        if (this.commits[0].Selected) {
+          // first commit is selected - generate a short url with only target sha
+          const lastCommit = this.commits.findLast((x) => x.Selected);
           window.location = `${this.issueLink}/files/${lastCommit.ID}${this.queryParams}`;
         } else {
-          const start = this.commits[this.commits.findIndex((x) => x.Hovered) - 1].ID;
-          const end = this.commits.findLast((x) => x.Hovered).ID;
+          const start = this.commits[this.commits.findIndex((x) => x.Selected) - 1].ID;
+          const end = this.commits.findLast((x) => x.Selected).ID;
           window.location = `${this.issueLink}/files/${start}..${end}${this.queryParams}`;
         }
       }
@@ -71,7 +84,7 @@ export default {
 </script>
 
 <style scoped>
-  .hovered {
-    background-color: purple !important;
+  .selected {
+    background-color: var(--color-secondary-active) !important;
   }
 </style>
