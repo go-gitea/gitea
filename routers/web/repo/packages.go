617544c4a013c20ef103f69d314281dcaff9946e
puni9869 <punitinani1@hotmail.com>
2023-07-04 10:11:16 +0530

    fix style in package/repo

--- routers/web/repo/packages.go
+++ routers/web/repo/packages.go
@@ -74,6 +74,6 @@ func Packages(ctx *context.Context) {
 	pager.AddParam(ctx, "q", "Query")
 	pager.AddParam(ctx, "type", "PackageType")
 	ctx.Data["Page"] = pager
-
+	ctx.Data["pageStyleClasses"] = "container"
 	ctx.HTML(http.StatusOK, tplPackagesList)
 }
