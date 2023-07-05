2904f345e0bc626618b5590de08635e4cf0f5867
yp05327 <576951401@qq.com>
2023-07-05 00:21:37 +0000

    fix conflicts

--- models/migrations/migrations.go
+++ models/migrations/migrations.go
@@ -499,13 +499,19 @@ var migrations = []Migration{
 	// v259 -> v260
 	NewMigration("Convert scoped access tokens", v1_20.ConvertScopedAccessTokens),
 
-	// Gitea 1.20.0 ends at 260
+	// Gitea 1.20.0 ends at v260
 
 	// v260 -> v261
 	NewMigration("Drop custom_labels column of action_runner table", v1_21.DropCustomLabelsColumnOfActionRunner),
 	// v261 -> v262
 	NewMigration("Add variable table", v1_21.CreateVariableTable),
 	// v262 -> v263
+	NewMigration("Add TriggerEvent to action_run table", v1_21.AddTriggerEventToActionRun),
+	// v263 -> v264
+	NewMigration("Add git_size and lfs_size columns to repository table", v1_21.AddGitSizeAndLFSSizeToRepositoryTable),
+	// v264 -> v265
+	NewMigration("Add branch table", v1_21.AddBranchTable),
+	// v265 -> v266
 	NewMigration("Add Repository Licenses", v1_21.AddRepositoryLicenses),
 }
 
