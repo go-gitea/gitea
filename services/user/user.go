85634aab1a975dac035b22e479f1c26cb43d4a34
hiifong <i@hiif.ong>
2023-07-07 00:51:39 +0800

    fix lint

--- services/user/user.go
+++ services/user/user.go
@@ -58,10 +58,7 @@ func RenameUser(ctx context.Context, u *user_model.User, newUserName string) err
 			u.Name = oldUserName
 			return err
 		}
-		if err := repo_model.UpdateRepositoryOwnerName(ctx, oldUserName, newUserName); err != nil {
-			return err
-		}
-		return nil
+		return repo_model.UpdateRepositoryOwnerName(ctx, oldUserName, newUserName)
 	}
 
 	ctx, committer, err := db.TxContext(ctx)
