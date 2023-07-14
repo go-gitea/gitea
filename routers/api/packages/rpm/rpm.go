807c5b5bbf4d687a9198f34162a8b5c95f8f7d8b
dragon <dragon@idm.d7z.net>
2023-07-14 23:55:38 +0800

    remove  rpm distribution TrimSpace

--- routers/api/packages/rpm/rpm.go
+++ routers/api/packages/rpm/rpm.go
@@ -33,7 +33,7 @@ func apiError(ctx *context.Context, status int, obj any) {
 
 // https://dnf.readthedocs.io/en/latest/conf_ref.html
 func GetRepositoryConfig(ctx *context.Context) {
-	distribution := strings.TrimSpace(ctx.Params("distribution"))
+	distribution := ctx.Params("distribution")
 	url := fmt.Sprintf("%sapi/packages/%s/rpm", setting.AppURL, ctx.Package.Owner.Name)
 
 	ctx.PlainText(http.StatusOK, `[gitea-`+ctx.Package.Owner.LowerName+`]
@@ -60,7 +60,7 @@ func GetRepositoryKey(ctx *context.Context) {
 
 // Gets a pre-generated repository metadata file
 func GetRepositoryFile(ctx *context.Context) {
-	distribution := strings.TrimSpace(ctx.Params("distribution"))
+	distribution := ctx.Params("distribution")
 	pv, err := rpm_service.GetOrCreateRepositoryVersion(ctx.Package.Owner.ID)
 	if err != nil {
 		apiError(ctx, http.StatusInternalServerError, err)
@@ -88,7 +88,7 @@ func GetRepositoryFile(ctx *context.Context) {
 }
 
 func UploadPackageFile(ctx *context.Context) {
-	distribution := strings.TrimSpace(ctx.Params("distribution"))
+	distribution := ctx.Params("distribution")
 	upload, close, err := ctx.UploadStream()
 	if err != nil {
 		apiError(ctx, http.StatusInternalServerError, err)
@@ -204,7 +204,7 @@ func DeletePackageFile(webctx *context.Context) {
 	name := webctx.Params("name")
 	version := webctx.Params("version")
 	architecture := webctx.Params("architecture")
-	distribution := strings.TrimSpace(webctx.Params("distribution"))
+	distribution := webctx.Params("distribution")
 
 	var pd *packages_model.PackageDescriptor
 
