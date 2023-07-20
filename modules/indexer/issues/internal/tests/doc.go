dc8b61aee4573cadf2b79ce936976fe42e6d02b9
Jason Song <i@wolfogre.com>
2023-07-20 18:55:42 +0800

    test: add comments

--- modules/indexer/issues/internal/tests/doc.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2023 The Gitea Authors. All rights reserved.
-// SPDX-License-Identifier: MIT
-
-package tests
-
-// This package contains tests for the indexer module.
-// All the code in this package is only used for testing.
-// Do not put any production code in this package to avoid it being included in the final binary.
