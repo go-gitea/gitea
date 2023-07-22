5d785961a96e2cfbe144b1437cc176b1c6366523
appleboy <appleboy.tw@gmail.com>
2023-07-22 11:20:54 +0800

    refactor: refactor status filter handling in `actions.go`
    - Remove the status filter from the list function in `actions.go`
    - Add a check for `StatusUnknown` before applying the status filter in `actions.go`
    
    Signed-off-by: appleboy <appleboy.tw@gmail.com>

--- routers/web/repo/actions/actions.go
+++ routers/web/repo/actions/actions.go
@@ -153,7 +153,11 @@ func List(ctx *context.Context) {
 		RepoID:        ctx.Repo.Repository.ID,
 		WorkflowID:    workflow,
 		TriggerUserID: actorID,
-		Status:        []actions_model.Status{actions_model.Status(status)},
+	}
+
+	// if status is not StatusUnknown, it means user has selected a status filter
+	if actions_model.Status(status) != actions_model.StatusUnknown {
+		opts.Status = []actions_model.Status{actions_model.Status(status)}
 	}
 
 	runs, total, err := actions_model.FindRuns(ctx, opts)
