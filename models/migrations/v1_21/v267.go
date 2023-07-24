d7b577fc9cc3d540da108c1f277005215231f50c
Jason Song <i@wolfogre.com>
2023-07-24 12:17:25 +0800

    Apply suggestions from code review

--- models/migrations/v1_21/v267.go
+++ models/migrations/v1_21/v267.go
@@ -4,36 +4,13 @@
 package v1_21 //nolint
 
 import (
-	"code.gitea.io/gitea/models/actions"
-	"code.gitea.io/gitea/modules/timeutil"
-	webhook_module "code.gitea.io/gitea/modules/webhook"
-
 	"xorm.io/xorm"
 )
 
 // UpdateActionsRefIndex updates the index of actions ref field
 func UpdateActionsRefIndex(x *xorm.Engine) error {
 	type ActionRun struct {
-		ID                int64
-		Title             string
-		RepoID            int64  `xorm:"index unique(repo_index)"`
-		OwnerID           int64  `xorm:"index"`
-		WorkflowID        string `xorm:"index"`                    // the name of workflow file
-		Index             int64  `xorm:"index unique(repo_index)"` // a unique number for each run of a repository
-		TriggerUserID     int64  `xorm:"index"`
-		Ref               string `xorm:"index"` // the ref of the run
-		CommitSHA         string
-		IsForkPullRequest bool                         // If this is triggered by a PR from a forked repository or an untrusted user, we need to check if it is approved and limit permissions when running the workflow.
-		NeedApproval      bool                         // may need approval if it's a fork pull request
-		ApprovedBy        int64                        `xorm:"index"` // who approved
-		Event             webhook_module.HookEventType // the webhook event that causes the workflow to run
-		EventPayload      string                       `xorm:"LONGTEXT"`
-		TriggerEvent      string                       // the trigger event defined in the `on` configuration of the triggered workflow
-		Status            actions.Status               `xorm:"index"`
-		Started           timeutil.TimeStamp
-		Stopped           timeutil.TimeStamp
-		Created           timeutil.TimeStamp `xorm:"created"`
-		Updated           timeutil.TimeStamp `xorm:"updated"`
+		Ref string `xorm:"index"` // the ref of the run
 	}
 	return x.Sync(new(ActionRun))
 }
