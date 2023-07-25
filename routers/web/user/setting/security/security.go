8506975d1cb4c3ab2df5a72e4992753002ae4e95
wxiaoguang <wxiaoguang@gmail.com>
2023-07-25 23:27:04 +0800

    fix

--- routers/web/user/setting/security/security.go
+++ routers/web/user/setting/security/security.go
@@ -48,9 +48,7 @@ func DeleteAccountLink(ctx *context.Context) {
 		}
 	}
 
-	ctx.JSON(http.StatusOK, map[string]any{
-		"redirect": setting.AppSubURL + "/user/settings/security",
-	})
+	ctx.JSONRedirect(setting.AppSubURL + "/user/settings/security")
 }
 
 func loadSecurityData(ctx *context.Context) {
