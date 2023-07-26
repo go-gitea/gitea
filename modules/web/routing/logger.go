782b137682cc96ac7c1af1d67a7bfbf6b24f208f
Giteabot <teabot@gitea.io>
2023-07-25 20:37:43 -0400

    Fix incorrect router logger (#26137) (#26143)
    Backport #26137 by @wxiaoguang
    
    A low-level mistake:
    
    * `log.Info` is global `Info` function, which calls "default" logger
    * `logger.Info` is the for router's logger
    
    Co-authored-by: wxiaoguang <wxiaoguang@gmail.com>

--- modules/web/routing/logger.go
+++ modules/web/routing/logger.go
@@ -89,13 +89,13 @@ func logPrinter(logger log.Logger) func(trigger Event, record *requestRecord) {
 		if v, ok := record.responseWriter.(context.ResponseWriter); ok {
 			status = v.Status()
 		}
-		logf := log.Info
+		logf := logger.Info
 		if strings.HasPrefix(req.RequestURI, "/assets/") {
-			logf = log.Trace
+			logf = logger.Trace
 		}
 		message := completedMessage
 		if isUnknownHandler {
-			logf = log.Error
+			logf = logger.Error
 			message = unknownHandlerMessage
 		}
 
