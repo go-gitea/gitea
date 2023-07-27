666038a06df6356a06d309966a925bf00253e3fa
Lunny Xiao <xiaolunwen@gmail.com>
2023-07-27 10:36:54 +0800

    Fix bug when pushing to a pull request which enabled dismiss approval automatically (#25882) (#26158)
    Fix #25858
    Backport #25882 
    
    The option `dissmiss stale approvals` was listed on protected branch but
    never implemented. This PR fixes that.
    
    <img width="1006" alt="图片"
    
    src="https://github.com/go-gitea/gitea/assets/81045/60bfa968-4db7-4c24-b8be-2e5978f91bb9">
    
    <img width="1021" alt="图片"
    
    src="https://github.com/go-gitea/gitea/assets/81045/8dabc14d-2dfe-40c2-94ed-24fcbf6e0e8f">

--- models/issues/issue.go
+++ models/issues/issue.go
@@ -552,7 +552,7 @@ func GetIssueWithAttrsByID(id int64) (*Issue, error) {
 
 // GetIssuesByIDs return issues with the given IDs.
 func GetIssuesByIDs(ctx context.Context, issueIDs []int64) (IssueList, error) {
-	issues := make([]*Issue, 0, 10)
+	issues := make([]*Issue, 0, len(issueIDs))
 	return issues, db.GetEngine(ctx).In("id", issueIDs).Find(&issues)
 }
 
