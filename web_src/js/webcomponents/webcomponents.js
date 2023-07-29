ee3484d8f27f1d60679948fc70420c98d4856da7
silverwind <me@silverwind.io>
2023-07-29 15:57:46 +0200

    add comment

--- web_src/js/webcomponents/webcomponents.js
+++ web_src/js/webcomponents/webcomponents.js
@@ -1,3 +1,5 @@
+// do not use dynamic `import()` in any of these files because the script tag does not
+// have type=module, it need to load synchronously to avoid content flickering
 import '@webcomponents/custom-elements'; // polyfill for some browsers like Pale Moon
 import '@github/relative-time-element';
 import './GiteaOriginUrl.js';
