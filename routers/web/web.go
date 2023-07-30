f44e36d1333e5c0e3876027f054d74ef9ec1afd8
Kerwin Bryant <kerwin612@qq.com>
2023-07-30 08:43:21 +0800

    Update web.go

--- routers/web/web.go
+++ routers/web/web.go
@@ -953,8 +953,10 @@ func registerRoutes(m *web.Route) {
 				addSettingVariablesRoutes()
 			}, actions.MustEnableActions)
 			// the follow handler must be under "settings", otherwise this incomplete repo can't be accessed
-			m.Post("/migrate/retry", repo.MigrateRetryPost)
-			m.Post("/migrate/cancel", repo.MigrateCancelPost)
+			m.Group("/migrate", func() {
+				m.Post("/retry", repo.MigrateRetryPost)
+				m.Post("/cancel", repo.MigrateCancelPost)
+			})
 		}, ctxDataSet("PageIsRepoSettings", true, "LFSStartServer", setting.LFS.StartServer))
 	}, reqSignIn, context.RepoAssignment, context.UnitTypes(), reqRepoAdmin, context.RepoRef())
 
