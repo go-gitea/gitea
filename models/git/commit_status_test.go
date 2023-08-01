cccd2ef453bee2d53c33c5738887393ce55c74cd
yp05327 <576951401@qq.com>
2023-08-01 02:08:24 +0000

    improve test

--- models/git/commit_status_test.go
+++ models/git/commit_status_test.go
@@ -21,25 +21,39 @@ func TestGetCommitStatuses(t *testing.T) {
 	repo1 := unittest.AssertExistsAndLoadBean(t, &repo_model.Repository{ID: 1})
 
 	sha1 := "1234123412341234123412341234123412341234"
+	count := 4
 
 	statuses, maxResults, err := git_model.GetCommitStatuses(db.DefaultContext, repo1, sha1, &git_model.CommitStatusOptions{ListOptions: db.ListOptions{Page: 1, PageSize: 50}})
 	assert.NoError(t, err)
-	assert.Equal(t, int(maxResults), 5)
-	assert.Len(t, statuses, 5)
+	assert.Equal(t, int(maxResults), count)
+	assert.Len(t, statuses, count)
 
-	assert.Equal(t, "ci/awesomeness", statuses[0].Context)
-	assert.Equal(t, structs.CommitStatusPending, statuses[0].State)
-	assert.Equal(t, "https://try.gitea.io/api/v1/repos/user2/repo1/statuses/1234123412341234123412341234123412341234", statuses[0].APIURL(db.DefaultContext))
-
-	assert.Equal(t, "cov/awesomeness", statuses[2].Context)
-	assert.Equal(t, structs.CommitStatusSuccess, statuses[2].State)
-	assert.Equal(t, "https://try.gitea.io/api/v1/repos/user2/repo1/statuses/1234123412341234123412341234123412341234", statuses[2].APIURL(db.DefaultContext))
-
-	assert.Equal(t, "ci/awesomeness", statuses[3].Context)
-	assert.Equal(t, structs.CommitStatusFailure, statuses[3].State)
-	assert.Equal(t, "https://try.gitea.io/api/v1/repos/user2/repo1/statuses/1234123412341234123412341234123412341234", statuses[3].APIURL(db.DefaultContext))
-
-	assert.Equal(t, "deploy/awesomeness", statuses[4].Context)
-	assert.Equal(t, structs.CommitStatusError, statuses[4].State)
-	assert.Equal(t, "https://try.gitea.io/api/v1/repos/user2/repo1/statuses/1234123412341234123412341234123412341234", statuses[4].APIURL(db.DefaultContext))
+	wants := []struct {
+		context      string
+		commitStatus structs.CommitStatusState
+	}{
+		{
+			context:      "deploy/awesomeness",
+			commitStatus: structs.CommitStatusError,
+		},
+		{
+			context:      "ci/awesomeness",
+			commitStatus: structs.CommitStatusFailure,
+		},
+		{
+			context:      "ci/awesomeness",
+			commitStatus: structs.CommitStatusPending,
+		},
+		{
+			context:      "cov/awesomeness",
+			commitStatus: structs.CommitStatusSuccess,
+		},
+	}
+	for i := 0; i < count; i++ {
+		t.Run("", func(t *testing.T) {
+			assert.Equal(t, wants[i].context, statuses[i].Context)
+			assert.Equal(t, wants[i].commitStatus, statuses[i].State)
+			assert.Equal(t, "https://try.gitea.io/api/v1/repos/user2/repo1/statuses/1234123412341234123412341234123412341234", statuses[i].APIURL(db.DefaultContext))
+		})
+	}
 }
