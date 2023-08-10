d866b879edb71e73b1de2434539c8da8087bebcd
silverwind <me@silverwind.io>
2023-08-10 20:55:29 +0200

    make it work for yaml specs as well

--- web_src/js/standalone/swagger.js
+++ web_src/js/standalone/swagger.js
@@ -1,14 +1,17 @@
 import SwaggerUI from 'swagger-ui-dist/swagger-ui-es-bundle.js';
 import 'swagger-ui-dist/swagger-ui.css';
+import {parseUrl} from '../utils.js';
+import {load} from "js-yaml";
 
 window.addEventListener('load', async () => {
-  const url = document.getElementById('swagger-ui').getAttribute('data-source');
-  const res = await fetch(url);
-  const spec = await res.json();
+  const url = parseUrl(document.getElementById('swagger-ui').getAttribute('data-source'));
+  const res = await fetch(url.toString());
+  const text = await res.text();
+  const spec = /\.ya?ml$/i.test(url.pathname) ? load(text) : JSON.parse(text);
 
   // This code is shared for our own spec as well as user-defined specs, this
   // section is for our own spec
-  if (url.endsWith('/swagger.v1.json')) {
+  if (url.pathname.endsWith('/swagger.v1.json')) {
     // Make the page's protocol be at the top of the schemes list
     const proto = window.location.protocol.slice(0, -1);
     spec.schemes.sort((a, b) => {
