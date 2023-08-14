9f48dfc3aca69d4f352976afaf54c6b83c36c5ad
Bo-Yi Wu <appleboy.tw@gmail.com>
2023-08-14 13:17:11 +0800

    feat: add support for Swagger secret list in API v1
    - Add a new file `routers/api/v1/swagger/action.go`
    - Add a new struct `swaggerResponseSecretList` in the `action.go` file
    - Add a new file `templates/swagger/v1_json.tmpl`
    - Modify the `v1_json.tmpl` file to include a new property `Secret` in the `properties` section of the `User` object
    - Modify the `v1_json.tmpl` file to include a new definition `SecretList` in the `definitions` section
    
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

---
+++ routers/api/v1/swagger/action.go
@@ -0,0 +1,13 @@
+// Copyright 2023 The Gitea Authors. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+package swagger
+
+import api "code.gitea.io/gitea/modules/structs"
+
+// SecretList
+// swagger:response SecretList
+type swaggerResponseSecretList struct {
+	// in:body
+	Body []api.Secret `json:"body"`
+}
