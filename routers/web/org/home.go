97808157603a0c332cfd2a660d1cc77ffd9f1483
yp05327 <576951401@qq.com>
2023-08-15 01:54:42 +0000

    fix

--- routers/web/org/home.go
+++ routers/web/org/home.go
@@ -162,7 +162,6 @@ func Home(ctx *context.Context) {
 	pager.SetDefaultParams(ctx)
 	pager.AddParam(ctx, "language", "Language")
 	ctx.Data["Page"] = pager
-	ctx.Data["ContextUser"] = ctx.ContextUser
 
 	ctx.HTML(http.StatusOK, tplOrgHome)
 }
