576644d81555cfcd7c865d444285cad64cc0b9ec
wxiaoguang <wxiaoguang@gmail.com>
2023-08-26 07:35:10 +0800

    Simplify helper CSS classes and avoid abuse (#26728)
    Removed CSS helper classes (some of them are not useful while some of
    them are abused often)
    
    * `gt-db`: in most cases it could be replaced by `gt-df` and the flex
    layout should be encouraged. Other cases: either it does need the
    `gt-df` (eg: by using `div` directly) or it is an abuse (eg: the warning
    message in a form)
    * `gt-di`: it doesn't seem useful, or it could be replaced by `gt-dib`
    in most cases.
    * `gt-dif`: not useful, it could be replaced by `flex-text-inline` or
    `gt-df`
    * `gt-js`: never used
    * All `<i class="icon gt-df gt-ac gt-jc">` could be written as `<i
    class="icon">`
    
    
    ## Some UI samples
    
    ### Admin Notice
    
    
    ![image](https://github.com/go-gitea/gitea/assets/2114189/d02010d4-dc7d-463f-bc99-dcc9b6e2e2ac)
    
    ### Admin Stacktrace
    
    
    ![image](https://github.com/go-gitea/gitea/assets/2114189/4045695c-a8c4-4e37-b720-e77a61b1e965)
    
    ### Org Home
    
    
    ![image](https://github.com/go-gitea/gitea/assets/2114189/069f02d0-76ad-4052-8a80-700d7e501d40)
    
    ### Org Team Repo
    
    
    ![image](https://github.com/go-gitea/gitea/assets/2114189/dc8d6106-bb6b-4f60-83ac-06cb28df3ab5)
    
    ### Release List
    
    
    ![image](https://github.com/go-gitea/gitea/assets/2114189/0845e8a5-d1a9-487a-9d25-3c200ad54c17)
    
    
    ### User Setting Application Token Scope
    
    
    ![image](https://github.com/go-gitea/gitea/assets/2114189/fffbde27-432b-49c6-827e-17b8cd3457ff)
    
    Co-authored-by: Giteabot <teabot@gitea.io>

--- web_src/js/components/DashboardRepoList.vue
+++ web_src/js/components/DashboardRepoList.vue
@@ -17,7 +17,7 @@
       <div class="ui attached segment repos-search">
         <div class="ui fluid right action left icon input" :class="{loading: isLoading}">
           <input type="search" spellcheck="false" maxlength="255" @input="changeReposFilter(reposFilter)" v-model="searchQuery" ref="search" @keydown="reposFilterKeyControl" :placeholder="textSearchRepos">
-          <i class="icon gt-df gt-ac gt-jc"><svg-icon name="octicon-search" :size="16"/></i>
+          <i class="icon"><svg-icon name="octicon-search" :size="16"/></i>
           <div class="ui dropdown icon button" :title="textFilter">
             <svg-icon name="octicon-filter" :size="16"/>
             <div class="menu">
