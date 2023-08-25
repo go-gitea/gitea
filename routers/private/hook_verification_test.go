53cc3d5de40ae8706d149fb45d58f287695aa308
CaiCandong <1290147055@qq.com>
2023-08-25 11:02:24 +0800

    add test

--- routers/private/hook_verification_test.go
+++ routers/private/hook_verification_test.go
@@ -4,22 +4,41 @@
 package private
 
 import (
+	"code.gitea.io/gitea/models/unittest"
 	"context"
 	"testing"
 
-	"code.gitea.io/gitea/models/unittest"
 	"code.gitea.io/gitea/modules/git"
 
 	"github.com/stretchr/testify/assert"
 )
 
+var (
+	testReposDir = "tests/repos/"
+)
+
 func TestVerifyCommits(t *testing.T) {
 	unittest.PrepareTestEnv(t)
 
-	gitRepo, err := git.OpenRepository(context.Background(), ".")
+	gitRepo, err := git.OpenRepository(context.Background(), testReposDir+"repo1_hook_verification")
 	assert.NoError(t, err)
-	defer gitRepo.Close()
 
-	err = verifyCommits(git.EmptySHA, "9c5c60143975a120bf70ac4aed34bf903ef19db6", gitRepo, nil)
-	assert.NoError(t, err)
+	testCases := []struct {
+		base, head string
+		verified   bool
+	}{
+		{"7d92f8f0cf7b989acf2c56a9a4387f5b39a92c41", "64e643f86a640d97f95820b43aeb28631042c629", true},
+		{git.EmptySHA, "451c1608ab9c5277fdc73ea68aea711c046ac7ac", true}, // New branch with verified commit
+		{"e05ec071e40a53cd4785345a99fd12f5dbb6a777", "7d92f8f0cf7b989acf2c56a9a4387f5b39a92c41", false},
+		{git.EmptySHA, "1128cd8e82948ea10b4932b2481210610604ce7e", false}, // New branch with unverified commit
+	}
+
+	for _, tc := range testCases {
+		err = verifyCommits(tc.base, tc.head, gitRepo, nil)
+		if tc.verified {
+			assert.NoError(t, err)
+		} else {
+			assert.Error(t, err)
+		}
+	}
 }
