fdd51d94c78581eda578e340f0d2220e4cddf9aa
Chongyi Zheng <git@zcy.dev>
2023-08-26 03:20:05 -0400

    Avoid cyclic import issue

--- services/mirror/notifier.go
+++ services/mirror/notifier.go
@@ -8,10 +8,14 @@ import (
 
 	repo_model "code.gitea.io/gitea/models/repo"
 	user_model "code.gitea.io/gitea/models/user"
+	"code.gitea.io/gitea/modules/notification"
 	"code.gitea.io/gitea/modules/notification/base"
 	"code.gitea.io/gitea/modules/repository"
-	mirror_service "code.gitea.io/gitea/services/mirror"
 )
+
+func init() {
+	notification.RegisterNotifier(&mirrorNotifier{})
+}
 
 type mirrorNotifier struct {
 	base.NullNotifier
@@ -19,15 +23,10 @@ type mirrorNotifier struct {
 
 var _ base.Notifier = &mirrorNotifier{}
 
-// NewNotifier create a new mirrorNotifier notifier
-func NewNotifier() base.Notifier {
-	return &mirrorNotifier{}
-}
-
 func (m *mirrorNotifier) NotifyPushCommits(ctx context.Context, _ *user_model.User, repo *repo_model.Repository, _ *repository.PushUpdateOptions, _ *repository.PushCommits) {
-	mirror_service.SyncPushMirrorWithSyncOnCommit(ctx, repo.ID)
+	SyncPushMirrorWithSyncOnCommit(ctx, repo.ID)
 }
 
 func (m *mirrorNotifier) NotifySyncPushCommits(ctx context.Context, _ *user_model.User, repo *repo_model.Repository, _ *repository.PushUpdateOptions, _ *repository.PushCommits) {
-	mirror_service.SyncPushMirrorWithSyncOnCommit(ctx, repo.ID)
+	SyncPushMirrorWithSyncOnCommit(ctx, repo.ID)
 }
