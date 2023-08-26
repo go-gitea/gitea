f1e8627438321ba31785c390d248b9f402a72d43
Chongyi Zheng <git@zcy.dev>
2023-08-26 03:10:42 -0400

    WIP: move some code to services

--- modules/notification/mirror/mirror.go
+++ modules/notification/mirror/mirror.go
@@ -8,10 +8,9 @@ import (
 
 	repo_model "code.gitea.io/gitea/models/repo"
 	user_model "code.gitea.io/gitea/models/user"
-	"code.gitea.io/gitea/modules/log"
-	mirror_module "code.gitea.io/gitea/modules/mirror"
 	"code.gitea.io/gitea/modules/notification/base"
 	"code.gitea.io/gitea/modules/repository"
+	mirror_service "code.gitea.io/gitea/services/mirror"
 )
 
 type mirrorNotifier struct {
@@ -26,21 +25,9 @@ func NewNotifier() base.Notifier {
 }
 
 func (m *mirrorNotifier) NotifyPushCommits(ctx context.Context, _ *user_model.User, repo *repo_model.Repository, _ *repository.PushUpdateOptions, _ *repository.PushCommits) {
-	syncPushMirrorWithSyncOnCommit(ctx, repo.ID)
+	mirror_service.SyncPushMirrorWithSyncOnCommit(ctx, repo.ID)
 }
 
 func (m *mirrorNotifier) NotifySyncPushCommits(ctx context.Context, _ *user_model.User, repo *repo_model.Repository, _ *repository.PushUpdateOptions, _ *repository.PushCommits) {
-	syncPushMirrorWithSyncOnCommit(ctx, repo.ID)
-}
-
-func syncPushMirrorWithSyncOnCommit(ctx context.Context, repoID int64) {
-	pushMirrors, err := repo_model.GetPushMirrorsSyncedOnCommit(ctx, repoID)
-	if err != nil {
-		log.Error("repo_model.GetPushMirrorsSyncedOnCommit failed: %v", err)
-		return
-	}
-
-	for _, mirror := range pushMirrors {
-		mirror_module.AddPushMirrorToQueue(mirror.ID)
-	}
+	mirror_service.SyncPushMirrorWithSyncOnCommit(ctx, repo.ID)
 }
