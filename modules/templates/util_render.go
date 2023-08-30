cc90ecc6007715ecf8d87ce7e92dd53797247fe6
silverwind <me@silverwind.io>
2023-08-31 00:37:38 +0200

    Render code blocks in repo description

--- modules/templates/util_render.go
+++ modules/templates/util_render.go
@@ -111,7 +111,7 @@ var codeMatcher = regexp.MustCompile("`([^`]+)`")
 // RenderCodeBlock renders "`…`" as highlighted "<code>" block.
 // Intended for issue and PR titles, these containers should have styles for "<code>" elements
 func RenderCodeBlock(htmlEscapedTextToRender template.HTML) template.HTML {
-	htmlWithCodeTags := codeMatcher.ReplaceAllString(string(htmlEscapedTextToRender), "<code>$1</code>") // replace with HTML <code> tags
+	htmlWithCodeTags := codeMatcher.ReplaceAllString(string(htmlEscapedTextToRender), `<code class="inline-code-block">$1</code>`) // replace with HTML <code> tags
 	return template.HTML(htmlWithCodeTags)
 }
 
