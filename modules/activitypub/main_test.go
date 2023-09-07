371b6341c7456c9cabda59d55099210d28c37e58
Lunny Xiao <xiaolunwen@gmail.com>
2023-09-08 00:41:55 +0800

    Fix check

--- modules/activitypub/main_test.go
+++ modules/activitypub/main_test.go
@@ -7,10 +7,11 @@ import (
 	"path/filepath"
 	"testing"
 
+	"code.gitea.io/gitea/models/unittest"
+
 	_ "code.gitea.io/gitea/models" // https://discourse.gitea.io/t/testfixtures-could-not-clean-table-access-no-such-table-access/4137/4
 	_ "code.gitea.io/gitea/models/actions"
 	_ "code.gitea.io/gitea/models/activities"
-	"code.gitea.io/gitea/models/unittest"
 )
 
 func TestMain(m *testing.M) {
