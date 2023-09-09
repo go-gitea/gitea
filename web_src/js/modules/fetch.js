cbec6f8cdf12697e378a429b09e513e9fdd42658
silverwind <me@silverwind.io>
2023-09-09 14:58:22 +0200

    remove network error code

--- web_src/js/modules/fetch.js
+++ web_src/js/modules/fetch.js
@@ -36,15 +36,3 @@ export const POST = (url, opts) => request(url, {method: 'POST', ...opts});
 export const PATCH = (url, opts) => request(url, {method: 'PATCH', ...opts});
 export const PUT = (url, opts) => request(url, {method: 'PUT', ...opts});
 export const DELETE = (url, opts) => request(url, {method: 'DELETE', ...opts});
-
-// network errors are currently only detectable by error message
-// based on https://github.com/sindresorhus/p-retry/blob/main/index.js
-const networkErrorMsgs = new Set([
-  'Failed to fetch', // Chrome
-  'NetworkError when attempting to fetch resource.', // Firefox
-  'The Internet connection appears to be offline.', // Safari
-]);
-
-export function isNetworkError(msg) {
-  return networkErrorMsgs.has(msg);
-}
--- web_src/js/modules/fetch.test.js
+++ web_src/js/modules/fetch.test.js
@@ -1,14 +1,11 @@
 import {test, expect} from 'vitest';
-import {GET, POST, PATCH, PUT, DELETE, isNetworkError} from './fetch.js';
+import {GET, POST, PATCH, PUT, DELETE} from './fetch.js';
 
+// tests here are only to satisfy the linter for unused functions
 test('exports', () => {
   expect(GET).toBeTruthy();
   expect(POST).toBeTruthy();
   expect(PATCH).toBeTruthy();
   expect(PUT).toBeTruthy();
   expect(DELETE).toBeTruthy();
-});
-
-test('isNetworkError', () => {
-  expect(isNetworkError('Failed to fetch')).toEqual(true);
 });
