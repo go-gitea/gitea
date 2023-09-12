27da6c73c68b0062b71ba3c60fb415b9575ec4ca
caicandong <1290147055@qq.com>
2023-09-12 10:35:29 +0800

    fix lint

--- routers/web/admin/users.go
+++ routers/web/admin/users.go
@@ -317,7 +317,7 @@ func EditUser(ctx *context.Context) {
 	ctx.Data["DisableGravatar"] = system_model.GetSettingWithCacheBool(ctx, system_model.KeyPictureDisableGravatar,
 		setting.GetDefaultDisableGravatar(),
 	)
-  ctx.Data["BaseUrl"] = "/admin/users/" + ctx.Params(":userid")
+	ctx.Data["BaseUrl"] = "/admin/users/" + ctx.Params(":userid")
 
 	prepareUserInfo(ctx)
 	if ctx.Written() {
