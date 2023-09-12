739e47cd80db34f2ab0a63da76adc235f1f2ab06
wxiaoguang <wxiaoguang@gmail.com>
2023-09-13 00:44:48 +0800

    Improve repo/user/org search  (#27030)
    * Fix a regression from #26809 (the `data-org` is missing)
    * Remove unnecessary style
    
    Screenshots:
    
    
    ![image](https://github.com/go-gitea/gitea/assets/2114189/3f5cf628-db7f-4705-898a-7a4a1fbfbba8)
    
    
    ![image](https://github.com/go-gitea/gitea/assets/2114189/453d1fad-1090-4524-bf45-6c5da2465f04)
    
    
    ![image](https://github.com/go-gitea/gitea/assets/2114189/f14d9808-7596-42c8-84b4-0d57a0bf2278)

--- web_src/js/features/repo-settings.js
+++ web_src/js/features/repo-settings.js
@@ -47,7 +47,7 @@ export function initRepoSettingSearchTeamBox() {
   $searchTeamBox.search({
     minCharacters: 2,
     apiSettings: {
-      url: `${appSubUrl}/org/${$searchTeamBox.data('org')}/teams/-/search?q={query}`,
+      url: `${appSubUrl}/org/${$searchTeamBox.attr('data-org-name')}/teams/-/search?q={query}`,
       headers: {'X-Csrf-Token': csrfToken},
       onResponse(response) {
         const items = [];
