745ac10626a16fe64d3e37619c8a235c5e3021e0
Lunny Xiao <xiaolunwen@gmail.com>
2023-09-20 12:40:17 +0800

    Fix the register check

--- routers/api/actions/runner/runner.go
+++ routers/api/actions/runner/runner.go
@@ -47,7 +47,11 @@ func (s *Service) Register(
 
 	runnerToken, err := actions_model.GetRunnerToken(ctx, req.Msg.Token)
 	if err != nil {
-		return nil, errors.New("runner token not found")
+		return nil, errors.New("runner registration token not found")
+	}
+
+	if !runnerToken.IsActive {
+		return nil, errors.New("runner registration token has been invalidated, please use the latest one")
 	}
 
 	labels := req.Msg.Labels
