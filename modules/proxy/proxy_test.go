aad20c15c1fc6511a78126e7dae09413bd6a3395
qdongxu <dongyanxu@live.com>
2023-10-01 15:03:17 +0800

    add unit test

---
+++ modules/proxy/proxy_test.go
@@ -0,0 +1,49 @@
+package proxy
+
+import (
+	"bufio"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"code.gitea.io/gitea/modules/setting"
+)
+
+func TestGetProxyURL(t *testing.T) {
+	os.Setenv("http_proxy", "http://127.0.0.1:1087")
+	os.Setenv("https_proxy", "http://127.0.0.1:1087")
+	os.Setenv("no_proxy", "example2.com")
+
+	cfg := &setting.Proxy
+	cfg.Enabled = false
+	cfg.SMTPProxyEnabled = false
+	cfg.ProxyURL = "https://127.0.0.1:2087"
+	cfg.ProxyHosts = []string{`gitea.io`}
+
+	setting.ParseProxy()
+
+	req, err := http.NewRequest("GET", "https://gitea.io", &bufio.Reader{})
+	assert.Nil(t, err)
+	proxyURL, err := Proxy()(req)
+	assert.Nil(t, proxyURL) // direct
+
+	cfg.Enabled = true
+	proxyURL, err = Proxy()(req)
+	assert.NotNil(t, proxyURL)
+	assert.Equal(t, "127.0.0.1:2087", proxyURL.Host) // in PROXY_HOSTS list
+
+	req, err = http.NewRequest("GET", "https://example.com", &bufio.Reader{})
+	assert.Nil(t, err)
+	proxyURL, err = Proxy()(req)
+	assert.NotNil(t, proxyURL)
+	assert.Equal(t, "127.0.0.1:1087", proxyURL.Host) // not in PROXY_HOSTS, from env
+
+	req, err = http.NewRequest("GET", "https://example2.com", &bufio.Reader{})
+	assert.Nil(t, err)
+	proxyURL, err = Proxy()(req)
+	assert.Nil(t, err)
+	assert.Nil(t, proxyURL) // not in PROXY_HOSTS, from env, ignored by no_proxy
+
+}
--- modules/setting/proxy.go
+++ modules/setting/proxy.go
@@ -13,13 +13,13 @@ import (
 
 // Proxy settings
 var Proxy = struct {
-	Enabled          bool
-	ProxyURL         string
-	ProxyURLFixed    *url.URL
-	ProxyHosts       []string
-	SMTPProxyEnabled bool
+	Enabled          bool     `ini:"PROXY_ENABLED"`
+	ProxyURL         string   `ini:"PROXY_URL"`
+	ProxyHosts       []string `ini:"PROXY_HOSTS"`
+	SMTPProxyEnabled bool     `ini:"SMTP_PROXY_ENABLED"`
 
-	HostMatchers []glob.Glob
+	ProxyURLFixed *url.URL    `-`
+	HostMatchers  []glob.Glob `-`
 }{
 	Enabled:          false,
 	SMTPProxyEnabled: false,
@@ -34,6 +34,12 @@ func loadProxyFrom(rootCfg ConfigProvider) {
 	Proxy.Enabled = sec.Key("PROXY_ENABLED").MustBool(false)
 	Proxy.SMTPProxyEnabled = sec.Key("SMTP_PROXY_ENABLED").MustBool(false)
 	Proxy.ProxyURL = sec.Key("PROXY_URL").MustString("")
+	Proxy.ProxyHosts = sec.Key("PROXY_HOSTS").Strings(",")
+
+	ParseProxy()
+}
+
+func ParseProxy() error {
 	if Proxy.ProxyURL != "" {
 		var err error
 		Proxy.ProxyURLFixed, err = url.Parse(Proxy.ProxyURL)
@@ -43,12 +49,14 @@ func loadProxyFrom(rootCfg ConfigProvider) {
 		}
 	}
 
-	Proxy.ProxyHosts = sec.Key("PROXY_HOSTS").Strings(",")
 	for _, h := range Proxy.ProxyHosts {
 		if g, err := glob.Compile(h); err == nil {
 			Proxy.HostMatchers = append(Proxy.HostMatchers, g)
 		} else {
 			log.Error("glob.Compile %s failed: %v", h, err)
+			return err
 		}
 	}
+
+	return nil
 }
