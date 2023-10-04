ae579f9706279d5d79ffe19abe1181e575f219db
silverwind <me@silverwind.io>
2023-10-04 23:30:31 +0200

    format

--- models/migrations/v1_22/v280.go
+++ models/migrations/v1_22/v280.go
@@ -21,7 +21,6 @@ func RenameUserThemes(x *xorm.Engine) error {
 	if _, err := sess.Exec("UPDATE `user` SET `theme` = 'gitea-dark' WHERE `theme` = 'arc-green'"); err != nil {
 		return err
 	}
-
 	if _, err := sess.Exec("UPDATE `user` SET `theme` = 'gitea-auto' WHERE `theme` = 'auto'"); err != nil {
 		return err
 	}
