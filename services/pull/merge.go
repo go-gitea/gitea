eeb1e0242b24497ba77ac834288fcaa54d4dc588
Lunny Xiao <xiaolunwen@gmail.com>
2023-10-17 23:07:23 +0800

    Fix poster is not loaded in get default merge message (#27657)

--- services/pull/merge.go
+++ services/pull/merge.go
@@ -44,6 +44,9 @@ func getMergeMessage(ctx context.Context, baseGitRepo *git.Repository, pr *issue
 	if err := pr.LoadIssue(ctx); err != nil {
 		return "", "", err
 	}
+	if err := pr.Issue.LoadPoster(ctx); err != nil {
+		return "", "", err
+	}
 
 	isExternalTracker := pr.BaseRepo.UnitEnabled(ctx, unit.TypeExternalTracker)
 	issueReference := "#"
