15dbea5197400031b27fb303c44917c706e667a1
silverwind <me@silverwind.io>
2023-10-18 17:02:54 +0200

    update comment

--- web_src/js/features/clipboard.js
+++ web_src/js/features/clipboard.js
@@ -4,12 +4,14 @@ import {clippie} from 'clippie';
 
 const {copy_success, copy_error} = window.config.i18n;
 
-// For all DOM elements with [data-clipboard-target] or [data-clipboard-text],
-// this copy-to-clipboard will work for them
+// Enable clipboard copy from HTML attributes. These properties are supported:
+// - data-clipboard-text: Direct text to copy, has highest precedence
+// - data-clipboard-target: Holds a selector for a <input> or <textarea> whose content is copied
+// - data-clipboard-text-type: When set to 'url' will convert relative to absolute urls
 export function initGlobalCopyToClipboardListener() {
   document.addEventListener('click', (e) => {
     let target = e.target;
-    // in case <button data-clipboard-text><svg></button>, so we just search
+    // In case <button data-clipboard-text><svg></button>, so we just search
     // up to 3 levels for performance
     for (let i = 0; i < 3 && target; i++) {
       let text = target.getAttribute('data-clipboard-text');
