47ce10c47069f63c28a86c6d7aa90e23441eaebc
Dinesh Salunke <dineshsalunke@consultbop.com>
2023-10-19 22:17:24 +0530

    refactor: empty the previous test's in order to write anew

--- tests/integration/api_project_board_test.go
+++ tests/integration/api_project_board_test.go
@@ -5,148 +5,23 @@
 package integration
 
 import (
-	"fmt"
-	"net/http"
 	"testing"
-
-	project_model "code.gitea.io/gitea/models/project"
-	repo_model "code.gitea.io/gitea/models/repo"
-	"code.gitea.io/gitea/models/unittest"
-	user_model "code.gitea.io/gitea/models/user"
-	api "code.gitea.io/gitea/modules/structs"
-	"code.gitea.io/gitea/tests"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestAPIListProjectBoads(t *testing.T) {
-	defer tests.PrepareTestEnv(t)()
-
-	repo := unittest.AssertExistsAndLoadBean(t, &repo_model.Repository{ID: 1})
-	owner := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: repo.OwnerID})
-
-	session := loginUser(t, owner.Name)
-	token := getTokenForLoggedInUser(t, session)
-
-	urlStr := fmt.Sprintf("/api/v1/projects/%d/boards?token=%s", 1, token)
-
-	req := NewRequest(t, "GET", urlStr)
-	resp := MakeRequest(t, req, http.StatusOK)
-
-	var apiProjectBoards []*api.ProjectBoard
-	DecodeJSON(t, resp, &apiProjectBoards)
-
-	// We include the Uncategorized board by default, so check the len accordingly
-	assert.Len(t, apiProjectBoards, unittest.GetCount(t, &project_model.Board{})+1)
 }
 
 func TestAPICreateProjectBoard(t *testing.T) {
-	defer tests.PrepareTestEnv(t)()
-	title, isDefault, color := "Board 10", false, "#ff0000"
-
-	repo := unittest.AssertExistsAndLoadBean(t, &repo_model.Repository{ID: 1})
-	owner := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: repo.OwnerID})
-	project := unittest.AssertExistsAndLoadBean(t, &project_model.Project{ID: 1})
-
-	session := loginUser(t, owner.Name)
-	token := getTokenForLoggedInUser(t, session)
-
-	urlStr := fmt.Sprintf("/api/v1/projects/%d/boards?token=%s", project.ID, token)
-
-	req := NewRequestWithJSON(t, "POST", urlStr, &api.NewProjectBoardPayload{
-		Title:   title,
-		Default: isDefault,
-		Color:   color,
-		Sorting: 0,
-	})
-	resp := session.MakeRequest(t, req, http.StatusCreated)
-	var apiProjectBoard *api.ProjectBoard
-	DecodeJSON(t, resp, &apiProjectBoard)
-
-	unittest.AssertExistsAndLoadBean(t, &project_model.Board{ID: apiProjectBoard.ID})
-	assert.Equal(t, title, apiProjectBoard.Title)
-	assert.Equal(t, isDefault, apiProjectBoard.Default)
-	assert.Equal(t, color, apiProjectBoard.Color)
 }
 
 func TestAPIGetProjectBoard(t *testing.T) {
-	defer tests.PrepareTestEnv(t)()
-	repo := unittest.AssertExistsAndLoadBean(t, &repo_model.Repository{ID: 1})
-	owner := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: repo.OwnerID})
-	board := unittest.AssertExistsAndLoadBean(t, &project_model.Board{ID: 1})
-
-	session := loginUser(t, owner.Name)
-	token := getTokenForLoggedInUser(t, session)
-
-	urlStr := fmt.Sprintf("/api/v1/projects/boards/%d?token=%s", board.ID, token)
-	req := NewRequest(t, "GET", urlStr)
-	resp := session.MakeRequest(t, req, http.StatusOK)
-
-	var apiProjectBoard *api.ProjectBoard
-	DecodeJSON(t, resp, &apiProjectBoard)
-
-	assert.Equal(t, apiProjectBoard.Title, board.Title)
-	assert.Equal(t, apiProjectBoard.Default, board.Default)
-	assert.Equal(t, apiProjectBoard.Color, board.Color)
-	assert.Equal(t, apiProjectBoard.Sorting, board.Sorting)
 }
 
 func TestAPIGetProjectBoardReqPermission(t *testing.T) {
-	defer tests.PrepareTestEnv(t)()
-	// repo := unittest.AssertExistsAndLoadBean(t, &repo_model.Repository{ID: 1})
-	// owner := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: repo.OwnerID})
-	board := unittest.AssertExistsAndLoadBean(t, &project_model.Board{ID: 1})
-	other_user := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: 23})
-
-	session := loginUser(t, other_user.Name)
-	token := getTokenForLoggedInUser(t, session)
-
-	urlStr := fmt.Sprintf("/api/v1/projects/boards/%d?token=%s", board.ID, token)
-	req := NewRequest(t, "GET", urlStr)
-	session.MakeRequest(t, req, http.StatusUnauthorized)
 }
 
 func TestAPIUpdateProjectBoard(t *testing.T) {
-	defer tests.PrepareTestEnv(t)()
-
-	repo := unittest.AssertExistsAndLoadBean(t, &repo_model.Repository{ID: 1})
-	owner := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: repo.OwnerID})
-	board := unittest.AssertExistsAndLoadBean(t, &project_model.Board{ID: 1})
-
-	session := loginUser(t, owner.Name)
-	token := getTokenForLoggedInUser(t, session)
-
-	urlStr := fmt.Sprintf("/api/v1/projects/boards/%d?token=%s", board.ID, token)
-	payload := &api.UpdateProjectBoardPayload{
-		Title: "Backlog",
-		Color: "#ffaa00",
-	}
-	req := NewRequestWithJSON(t, "PATCH", urlStr, payload)
-	resp := session.MakeRequest(t, req, http.StatusOK)
-
-	var apiProjectBoard *api.ProjectBoard
-	DecodeJSON(t, resp, &apiProjectBoard)
-
-	assert.Equal(t, payload.Title, apiProjectBoard.Title)
-	assert.Equal(t, payload.Color, apiProjectBoard.Color)
-
-	boardAfter := unittest.AssertExistsAndLoadBean(t, &project_model.Board{ID: 1})
-	assert.Equal(t, payload.Title, boardAfter.Title)
-	assert.Equal(t, payload.Color, boardAfter.Color)
 }
 
 func TestAPIDeleteProjectBoard(t *testing.T) {
-	defer tests.PrepareTestEnv(t)()
-
-	repo := unittest.AssertExistsAndLoadBean(t, &repo_model.Repository{ID: 1})
-	owner := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: repo.OwnerID})
-
-	session := loginUser(t, owner.Name)
-	token := getTokenForLoggedInUser(t, session)
-
-	urlStr := fmt.Sprintf("/api/v1/projects/boards/%d?token=%s", 1, token)
-	req := NewRequest(t, "DELETE", urlStr)
-	_ = session.MakeRequest(t, req, http.StatusNoContent)
-
-	unittest.AssertNotExistsBean(t, &project_model.Board{ID: 1})
 }
--- tests/integration/api_project_test.go
+++ tests/integration/api_project_test.go
@@ -5,147 +5,32 @@
 package integration
 
 import (
-	"fmt"
-	"net/http"
-	"net/url"
 	"testing"
-
-	project_model "code.gitea.io/gitea/models/project"
-	repo_model "code.gitea.io/gitea/models/repo"
-	"code.gitea.io/gitea/models/unittest"
-	user_model "code.gitea.io/gitea/models/user"
-	api "code.gitea.io/gitea/modules/structs"
-	"code.gitea.io/gitea/tests"
-
-	"github.com/stretchr/testify/assert"
 )
 
-func TestAPIListRepositoryProjects(t *testing.T) {
-	defer tests.PrepareTestEnv(t)()
-
-	repo := unittest.AssertExistsAndLoadBean(t, &repo_model.Repository{ID: 1})
-	owner := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: repo.OwnerID})
-
-	session := loginUser(t, owner.Name)
-	token := getTokenForLoggedInUser(t, session)
-	link, _ := url.Parse(fmt.Sprintf("/api/v1/repos/%s/%s/projects", owner.Name, repo.Name))
-
-	link.RawQuery = url.Values{"token": {token}}.Encode()
-
-	req := NewRequest(t, "GET", link.String())
-	var apiProjects []*api.Project
-
-	resp := session.MakeRequest(t, req, http.StatusOK)
-	DecodeJSON(t, resp, &apiProjects)
-
-	assert.Len(t, apiProjects, 2)
-	for _, apiProject := range apiProjects {
-		unittest.AssertExistsAndLoadBean(t, &project_model.Project{ID: apiProject.ID, RepoID: repo.ID, IsClosed: apiProject.IsClosed})
-	}
+func TestAPIListUserProjects(t *testing.T) {
 }
 
-func TestAPICreateRepositoryProject(t *testing.T) {
-	defer tests.PrepareTestEnv(t)()
-	const title, description, boardType = "project_name", "project_description", uint8(project_model.BoardTypeBasicKanban)
+func TestAPIListOrgProjects(t *testing.T) {
+}
 
-	repo := unittest.AssertExistsAndLoadBean(t, &repo_model.Repository{ID: 1})
-	owner := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: repo.OwnerID})
+func TestAPIListRepoProjects(t *testing.T) {
+}
 
-	session := loginUser(t, owner.Name)
-	token := getTokenForLoggedInUser(t, session)
-	urlStr := fmt.Sprintf("/api/v1/repos/%s/%s/projects?token=%s", owner.Name, repo.Name, token)
+func TestAPICreateUserProject(t *testing.T) {
+}
 
-	req := NewRequestWithJSON(t, "POST", urlStr, &api.NewProjectPayload{
-		Title:       title,
-		Description: description,
-		BoardType:   boardType,
-	})
-	resp := session.MakeRequest(t, req, http.StatusCreated)
-	var apiProject api.Project
-	DecodeJSON(t, resp, &apiProject)
-	assert.Equal(t, title, apiProject.Title)
-	assert.Equal(t, description, apiProject.Description)
-	assert.Equal(t, boardType, apiProject.BoardType)
-	assert.Equal(t, owner.Name, apiProject.Creator.UserName)
-	assert.Equal(t, owner.FullName, apiProject.Creator.FullName)
-	assert.Equal(t, repo.ID, apiProject.Repo.ID)
-	assert.Equal(t, repo.Name, apiProject.Repo.Name)
-	assert.Equal(t, repo.FullName(), apiProject.Repo.FullName)
-	assert.Equal(t, owner.Name, apiProject.Repo.Owner)
+func TestAPICreateOrgProject(t *testing.T) {
+}
+
+func TestAPICreateRepoProject(t *testing.T) {
 }
 
 func TestAPIGetProject(t *testing.T) {
-	defer tests.PrepareTestEnv(t)()
-
-	repo := unittest.AssertExistsAndLoadBean(t, &repo_model.Repository{ID: 1})
-	owner := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: repo.OwnerID})
-	project := unittest.AssertExistsAndLoadBean(t, &project_model.Project{ID: 1})
-	projectRepo := unittest.AssertExistsAndLoadBean(t, &repo_model.Repository{ID: project.RepoID})
-	projectCreator := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: project.CreatorID})
-
-	session := loginUser(t, owner.Name)
-	token := getTokenForLoggedInUser(t, session)
-
-	urlStr := fmt.Sprintf("/api/v1/projects/%d?token=%s", project.ID, token)
-
-	req := NewRequest(t, "GET", urlStr)
-	resp := session.MakeRequest(t, req, http.StatusOK)
-
-	var apiProject api.Project
-	DecodeJSON(t, resp, &apiProject)
-
-	assert.Equal(t, project.Title, apiProject.Title)
-	assert.Equal(t, project.Description, apiProject.Description)
-	assert.Equal(t, uint8(project.BoardType), apiProject.BoardType)
-	assert.Equal(t, projectCreator.Name, apiProject.Creator.UserName)
-	assert.Equal(t, projectCreator.FullName, apiProject.Creator.FullName)
-	assert.Equal(t, projectRepo.ID, apiProject.Repo.ID)
-	assert.Equal(t, projectRepo.Name, apiProject.Repo.Name)
-	assert.Equal(t, projectRepo.FullName(), apiProject.Repo.FullName)
-	assert.Equal(t, projectRepo.OwnerName, apiProject.Repo.Owner)
 }
 
 func TestAPIUpdateProject(t *testing.T) {
-	defer tests.PrepareTestEnv(t)()
-
-	repo := unittest.AssertExistsAndLoadBean(t, &repo_model.Repository{ID: 1})
-	owner := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: repo.OwnerID})
-	projectBefore := unittest.AssertExistsAndLoadBean(t, &project_model.Project{ID: 1})
-
-	session := loginUser(t, owner.Name)
-	token := getTokenForLoggedInUser(t, session)
-
-	urlStr := fmt.Sprintf("/api/v1/projects/%d?token=%s", projectBefore.ID, token)
-
-	req := NewRequestWithJSON(t, "PATCH", urlStr, &api.UpdateProjectPayload{
-		Title:       "This is new title",
-		Description: "This is new description",
-	})
-	resp := session.MakeRequest(t, req, http.StatusOK)
-
-	var apiProject api.Project
-	DecodeJSON(t, resp, &apiProject)
-	projectAfter := unittest.AssertExistsAndLoadBean(t, &project_model.Project{ID: 1})
-
-	assert.Equal(t, "This is new title", apiProject.Title)
-	assert.Equal(t, "This is new description", apiProject.Description)
-	assert.Equal(t, "This is new title", projectAfter.Title)
-	assert.Equal(t, "This is new description", projectAfter.Description)
 }
 
 func TestAPIDeleteProject(t *testing.T) {
-	defer tests.PrepareTestEnv(t)()
-	repo := unittest.AssertExistsAndLoadBean(t, &repo_model.Repository{ID: 1})
-	owner := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: repo.OwnerID})
-	projectBefore := unittest.AssertExistsAndLoadBean(t, &project_model.Project{ID: 1})
-
-	session := loginUser(t, owner.Name)
-	token := getTokenForLoggedInUser(t, session)
-
-	urlStr := fmt.Sprintf("/api/v1/projects/%d?token=%s", projectBefore.ID, token)
-
-	req := NewRequest(t, "DELETE", urlStr)
-	_ = session.MakeRequest(t, req, http.StatusNoContent)
-
-	unittest.AssertNotExistsBean(t, &project_model.Project{ID: 1})
 }
