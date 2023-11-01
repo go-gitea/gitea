483bab09ccbf188dd0e16dca7ac62777462112be
silverwind <me@silverwind.io>
2023-11-01 17:20:44 +0100

    use data-toggled attribute and improve transition

--- web_src/js/features/repo-issue-pr-status.js
+++ web_src/js/features/repo-issue-pr-status.js
@@ -1,13 +1,11 @@
 export function initRepoPullRequestCommitStatus() {
   const btn = document.querySelector('.hide-all-checks');
-  const prCommitStatus = document.querySelector('.pr-commit-status');
   if (!btn) return;
+
   btn.addEventListener('click', () => {
-    if (prCommitStatus.classList.contains('hide')) {
-      btn.textContent = btn.getAttribute('data-hide-all');
-    } else {
-      btn.textContent = btn.getAttribute('data-show-all');
-    }
-    prCommitStatus.classList.toggle('hide');
+    const prCommitStatus = document.querySelector('.pr-commit-status');
+    const toggled = prCommitStatus.getAttribute('data-toggled') === 'true';
+    btn.textContent = btn.getAttribute(toggled ? 'data-hide-all' : 'data-show-all');
+    prCommitStatus.setAttribute('data-toggled', String(!toggled));
   });
 }
