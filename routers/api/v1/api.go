243005725c5cec78d3a51cad9c802e986f37daaa
Loïc Dachary <loic@dachary.org>
2023-11-02 15:41:10 +0100

    enforce reqRepoReader(unit.TypeIssues) GET /repos/{owner}/{repo}/issues/pinned
    (cherry picked from commit 00fad97fc1b27db40a002c9ab3f709d04dc2cdd1)
    (cherry picked from commit 2bebe443c0d3232c5923ba7d9a099853c76e3bc5)

--- routers/api/v1/api.go
+++ routers/api/v1/api.go
@@ -1149,7 +1149,7 @@ func Routes(ctx gocontext.Context) *web.Route {
 				m.Group("/issues", func() {
 					m.Combo("").Get(repo.ListIssues).
 						Post(reqToken(), mustNotBeArchived, bind(api.CreateIssueOption{}), repo.CreateIssue)
-					m.Get("/pinned", repo.ListPinnedIssues)
+					m.Get("/pinned", reqRepoReader(unit.TypeIssues), repo.ListPinnedIssues)
 					m.Group("/comments", func() {
 						m.Get("", repo.ListRepoIssueComments)
 						m.Group("/{id}", func() {
