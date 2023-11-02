ee19a339c8f1fbcd39f9ee24a97367f276e045ab
wxiaoguang <wxiaoguang@gmail.com>
2023-11-02 11:30:10 +0800

    avoid scrollbar flicker when show/hide the checks

--- web_src/js/features/repo-issue-pr-status.js
+++ web_src/js/features/repo-issue-pr-status.js
@@ -1,10 +1,12 @@
 export function initRepoPullRequestCommitStatus() {
   for (const btn of document.querySelectorAll('.commit-status-hide-checks')) {
+    const panel = btn.closest('.commit-status-panel');
+    const list = panel.querySelector('.commit-status-list');
     btn.addEventListener('click', () => {
-      const panel = btn.closest('.commit-status-panel');
-      const list = panel.querySelector('.commit-status-list');
-      list.style.maxHeight = list.style.maxHeight ? '' : '0px';
+      list.style.maxHeight = list.style.maxHeight ? '' : '0px'; // toggle
+      list.style.overflow = 'hidden'; // hide scrollbar when hiding
       btn.textContent = btn.getAttribute(list.style.maxHeight ? 'data-show-all' : 'data-hide-all');
     });
+    list.addEventListener('animationend', () => list.style.overflow = '');
   }
 }
