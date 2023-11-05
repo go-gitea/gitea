f177e930c25fbeed7fcf2d0b8b5793711783241e
Folke <folke@ashberg.de>
2023-11-05 17:56:29 +0100

    Fixing #27913

--- contrib/environment-to-ini/environment-to-ini.go
+++ contrib/environment-to-ini/environment-to-ini.go
@@ -47,22 +47,26 @@ func main() {
 	on the configuration cheat sheet.`
 	app.Flags = []cli.Flag{
 		&cli.StringFlag{
-			Name:  "custom-path, C",
+			Name:  "custom-path",
+			Aliases: []string{"C"},
 			Value: setting.CustomPath,
 			Usage: "Custom path file path",
 		},
 		&cli.StringFlag{
-			Name:  "config, c",
+			Name:  "config",
+			Aliases: []string{"c"},
 			Value: setting.CustomConf,
 			Usage: "Custom configuration file path",
 		},
 		&cli.StringFlag{
-			Name:  "work-path, w",
+			Name:  "work-path",
+			Aliases: []string{"w"},
 			Value: setting.AppWorkPath,
 			Usage: "Set the gitea working path",
 		},
 		&cli.StringFlag{
-			Name:  "out, o",
+			Name:  "out",
+			Aliases: []string{"o"},
 			Value: "",
 			Usage: "Destination file to write to",
 		},
