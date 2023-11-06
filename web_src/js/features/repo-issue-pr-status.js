ae0125d9660b29f4467fd8a7c73c14724cd2b6f5
silverwind <me@silverwind.io>
2023-11-07 00:12:59 +0100

    make border between items and below header

--- web_src/js/features/repo-issue-pr-status.js
+++ web_src/js/features/repo-issue-pr-status.js
@@ -2,15 +2,21 @@ export function initRepoPullRequestCommitStatus() {
   for (const btn of document.querySelectorAll('.commit-status-hide-checks')) {
     const panel = btn.closest('.commit-status-panel');
     const list = panel.querySelector('.commit-status-list');
-    let hasScrollBar = null;
+    const header = panel.querySelector('.commit-status-header');
+    let hasScrollBar = false;
     btn.addEventListener('click', () => {
-      if (hasScrollBar === null && !list.style.overflow) {
+      if (!hasScrollBar && !list.style.overflow) {
         hasScrollBar = list.scrollHeight > list.clientHeight;
       }
-      list.style.overflow = hasScrollBar ? '' : 'hidden'; // hide the flickering scrollbar when hiding if there was no scrollbar
+      // hide the flickering scrollbar during transition if there was no scrollbar
+      list.style.overflow = hasScrollBar ? '' : 'hidden';
       list.style.maxHeight = list.style.maxHeight ? '' : '0px'; // toggle
+      if (!list.style.maxHeight) header.style.borderBottom = '';
       btn.textContent = btn.getAttribute(list.style.maxHeight ? 'data-show-all' : 'data-hide-all');
     });
-    list.addEventListener('transitionend', () => list.style.overflow = '');
+    list.addEventListener('transitionend', () => {
+      list.style.overflow = '';
+      header.style.borderBottom = list.style.maxHeight ? 'none' : '';
+    });
   }
 }
