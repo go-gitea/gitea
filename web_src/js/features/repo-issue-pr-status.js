9cc1999a98cbaaeab6a5f289681f45747632c64f
wxiaoguang <wxiaoguang@gmail.com>
2023-11-06 17:47:41 +0800

    fix

--- web_src/js/features/repo-issue-pr-status.js
+++ web_src/js/features/repo-issue-pr-status.js
@@ -2,11 +2,15 @@ export function initRepoPullRequestCommitStatus() {
   for (const btn of document.querySelectorAll('.commit-status-hide-checks')) {
     const panel = btn.closest('.commit-status-panel');
     const list = panel.querySelector('.commit-status-list');
+    let hasScrollBar = null;
     btn.addEventListener('click', () => {
+      if (hasScrollBar === null && !list.style.overflow) {
+        hasScrollBar = list.scrollHeight > list.clientHeight;
+      }
+      list.style.overflow = hasScrollBar ? '' : 'hidden'; // hide the flickering scrollbar when hiding if there was no scrollbar
       list.style.maxHeight = list.style.maxHeight ? '' : '0px'; // toggle
-      list.style.overflow = 'hidden'; // hide scrollbar when hiding
       btn.textContent = btn.getAttribute(list.style.maxHeight ? 'data-show-all' : 'data-hide-all');
     });
-    list.addEventListener('animationend', () => list.style.overflow = '');
+    list.addEventListener('transitionend', () => list.style.overflow = '');
   }
 }
