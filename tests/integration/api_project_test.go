ac02a7c5b028219b35e9108c72f25f50fbae025b
Dinesh Salunke <dineshsalunke@consultbop.com>
2023-11-10 10:50:08 +0530

    test: add test for list org projects

--- tests/integration/api_project_test.go
+++ tests/integration/api_project_test.go
@@ -5,13 +5,41 @@
 package integration
 
 import (
+	"fmt"
+	"net/http"
+	"net/url"
 	"testing"
+
+	auth_model "code.gitea.io/gitea/models/auth"
+	"code.gitea.io/gitea/models/unittest"
+	user_model "code.gitea.io/gitea/models/user"
+	api "code.gitea.io/gitea/modules/structs"
+	"code.gitea.io/gitea/tests"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAPIListUserProjects(t *testing.T) {
 }
 
 func TestAPIListOrgProjects(t *testing.T) {
+	defer tests.PrepareTestEnv(t)()
+
+	user := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: 2})
+	org := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: 17})
+
+	session := loginUser(t, user.Name)
+	token := getTokenForLoggedInUser(t, session, auth_model.AccessTokenScopeReadOrganization, auth_model.AccessTokenScopeReadIssue)
+	link, _ := url.Parse(fmt.Sprintf("/api/v1/orgs/%s/projects", org.Name))
+
+	link.RawQuery = url.Values{"token": { token}}.Encode()
+
+	req := NewRequest(t, "GET", link.String())
+	var apiProjects []*api.Project
+
+	resp := session.MakeRequest(t, req, http.StatusOK)
+	DecodeJSON(t, resp, &apiProjects)
+	assert.Len(t, apiProjects, 1)
+
 }
 
 func TestAPIListRepoProjects(t *testing.T) {
