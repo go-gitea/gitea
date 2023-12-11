37b649024388f18c58438d8c0f829059a202737c
Lunny Xiao <xiaolunwen@gmail.com>
2023-12-11 16:00:39 +0800

    Add limit permission check

--- routers/web/web.go
+++ routers/web/web.go
@@ -798,10 +798,19 @@ func registerRoutes(m *web.Route) {
 
 	individualPermsChecker := func(ctx *context.Context) {
 		// org permissions have been checked in context.OrgAssignment(), but individual permissions haven't been checked.
-		if ctx.ContextUser.IsIndividual() && ctx.ContextUser.Visibility == structs.VisibleTypePrivate &&
-			(ctx.Doer == nil || (ctx.ContextUser.ID != ctx.Doer.ID && !ctx.Doer.IsAdmin)) {
-			ctx.NotFound("Visit Project", nil)
-			return
+		if ctx.ContextUser.IsIndividual() {
+			switch {
+			case ctx.ContextUser.Visibility == structs.VisibleTypePrivate:
+				if ctx.Doer == nil || (ctx.ContextUser.ID != ctx.Doer.ID && !ctx.Doer.IsAdmin) {
+					ctx.NotFound("Visit Project", nil)
+					return
+				}
+			case ctx.ContextUser.Visibility == structs.VisibleTypeLimited:
+				if ctx.Doer == nil {
+					ctx.NotFound("Visit Project", nil)
+					return
+				}
+			}
 		}
 	}
 
