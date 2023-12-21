838db2f8911690fa2115c6827ed73687db71bef1
KN4CK3R <admin@oldschoolhack.me>
2023-12-22 00:59:59 +0100

    Convert to url auth to header auth in tests (#28484)
    Related #28390

--- tests/integration/api_nodeinfo_test.go
+++ tests/integration/api_nodeinfo_test.go
@@ -24,7 +24,7 @@ func TestNodeinfo(t *testing.T) {
 	}()
 
 	onGiteaRun(t, func(*testing.T, *url.URL) {
-		req := NewRequestf(t, "GET", "/api/v1/nodeinfo")
+		req := NewRequest(t, "GET", "/api/v1/nodeinfo")
 		resp := MakeRequest(t, req, http.StatusOK)
 		VerifyJSONSchema(t, resp, "nodeinfo_2.1.json")
 
