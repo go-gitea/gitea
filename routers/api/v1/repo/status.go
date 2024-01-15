ae99233db03ac123631d3f98eeaa4bf5c624b292
KN4CK3R <admin@oldschoolhack.me>
2024-01-15 19:30:12 +0100

    Fix `GetCommitStatuses` (#28787) (#28804)
    Backport #28787
    
    Replaces #28802

--- routers/api/v1/repo/status.go
+++ routers/api/v1/repo/status.go
@@ -194,8 +194,10 @@ func getCommitStatuses(ctx *context.APIContext, sha string) {
 
 	listOptions := utils.GetListOptions(ctx)
 
-	statuses, maxResults, err := git_model.GetCommitStatuses(ctx, repo, sha, &git_model.CommitStatusOptions{
+	statuses, maxResults, err := git_model.GetCommitStatuses(ctx, &git_model.CommitStatusOptions{
 		ListOptions: listOptions,
+		RepoID:      repo.ID,
+		SHA:         sha,
 		SortType:    ctx.FormTrim("sort"),
 		State:       ctx.FormTrim("state"),
 	})
