013d92bd5d1e611ecaf1c8a1facf255190f37e5f
Lunny Xiao <xiaolunwen@gmail.com>
2024-01-16 09:56:43 +0800

    Fix lint

--- services/repository/files/temp_repo.go
+++ services/repository/files/temp_repo.go
@@ -68,7 +68,7 @@ func (t *TemporaryUploadRepository) Clone(branch string) error {
 		}
 		return fmt.Errorf("Clone: %w %s", err, stderr)
 	}
-	gitRepo, err := git.OpenRepositoryWithAlternates(t.ctx, t.basePath, t.repo.RepoPath())
+	gitRepo, err := git.OpenRepository(t.ctx, t.basePath)
 	if err != nil {
 		return err
 	}
