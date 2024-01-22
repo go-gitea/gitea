88760aacac4a9595bd43e66ee6816b46e0b26915
yp05327 <576951401@qq.com>
2024-01-22 08:47:26 +0000

    add test

--- modules/git/signature_gogit.go
+++ modules/git/signature_gogit.go
@@ -21,12 +21,10 @@ type Signature = object.Signature
 // Helper to get a signature from the commit line, which looks like these:
 //
 //	author Patrick Gundlach <gundlach@speedata.de> 1378823654 +0200
-//	author Patrick Gundlach <gundlach@speedata.de> Thu, 07 Apr 2005 22:13:13 +0200
+//	author Patrick Gundlach <gundlach@speedata.de> Thu Apr 07 22:13:13 2005 +0200
 //
 // but without the "author " at the beginning (this method should)
 // be used for author and committer.
-//
-// FIXME: include timezone for timestamp!
 func newSignatureFromCommitline(line []byte) (_ *Signature, err error) {
 	sig := new(Signature)
 	emailStart := bytes.IndexByte(line, '<')
@@ -45,18 +43,37 @@ func newSignatureFromCommitline(line []byte) (_ *Signature, err error) {
 		firstChar := line[emailEnd+2]
 		if firstChar >= 48 && firstChar <= 57 {
 			timestop := bytes.IndexByte(line[emailEnd+2:], ' ')
+			if timestop < 0 {
+				return sig, nil
+			}
+
 			timestring := string(line[emailEnd+2 : emailEnd+2+timestop])
 			seconds, _ := strconv.ParseInt(timestring, 10, 64)
 			sig.When = time.Unix(seconds, 0)
+
+			timestop += emailEnd + 3
+			if timestop >= len(line) || timestop+5 > len(line) {
+				return sig, nil
+			}
+
+			timezone := string(line[timestop : timestop+5])
+			tzhours, err1 := strconv.ParseInt(timezone[0:3], 10, 64)
+			tzmins, err2 := strconv.ParseInt(timezone[3:], 10, 64)
+			if err1 != nil || err2 != nil {
+				return sig, err
+			}
+			if tzhours < 0 {
+				tzmins *= -1
+			}
+			tz := time.FixedZone("", int(tzhours*60*60+tzmins*60))
+			sig.When = sig.When.In(tz)
+
 		} else {
 			sig.When, err = time.Parse(GitTimeLayout, string(line[emailEnd+2:]))
 			if err != nil {
 				return nil, err
 			}
 		}
-	} else {
-		// Fall back to unix 0 time
-		sig.When = time.Unix(0, 0)
 	}
 	return sig, nil
 }
---
+++ modules/git/signature_gogit_test.go
@@ -0,0 +1,43 @@
+// Copyright 2024 The Gitea Authors. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+//go:build gogit
+
+package git
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewNewSignatureFromCommitline(t *testing.T) {
+	tz := time.FixedZone("", 2*60*60)
+
+	kases := map[string]Signature{
+		"": Signature{},
+		"author gitea test <test@gitea.com>": Signature{
+			Name:  "author gitea test",
+			Email: "test@gitea.com",
+		},
+		"author gitea test <test@gitea.com> 1705912028 +0200": Signature{
+			Name:  "author gitea test",
+			Email: "test@gitea.com",
+			When:  time.Unix(1705912028, 0).In(tz),
+		},
+		"author gitea test <test@gitea.com> Mon Jan 22 10:27:08 2024 +0200": Signature{
+			Name:  "author gitea test",
+			Email: "test@gitea.com",
+			When:  time.Unix(1705912028, 0).In(tz),
+		},
+	}
+
+	for text, sign := range kases {
+		newSign, err := newSignatureFromCommitline([]byte(text))
+		assert.NoError(t, err)
+		assert.Equal(t, sign.Name, newSign.Name)
+		assert.Equal(t, sign.Email, newSign.Email)
+		assert.Equal(t, sign.When, newSign.When)
+	}
+}
