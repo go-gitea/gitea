ce36ac58d8a6fef8bb0b03351d2974526c7926bb
cchangwen <157893811+cchangwen@users.noreply.github.com>
2024-01-27 21:52:13 +0800

    ETag prefix W/
    https://stackoverflow.com/questions/51973120/where-does-the-w-in-an-etag-appear-from
    
    https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/ETag#

--- modules/httpcache/httpcache.go
+++ modules/httpcache/httpcache.go
@@ -58,6 +58,7 @@ func HandleGenericETagCache(req *http.Request, w http.ResponseWriter, etag strin
 func checkIfNoneMatchIsValid(req *http.Request, etag string) bool {
 	ifNoneMatch := req.Header.Get("If-None-Match")
 	if len(ifNoneMatch) > 0 {
+		ifNoneMatch = strings.TrimPrefix(ifNoneMatch, "W/")
 		for _, item := range strings.Split(ifNoneMatch, ",") {
 			item = strings.TrimSpace(item)
 			if item == etag {
