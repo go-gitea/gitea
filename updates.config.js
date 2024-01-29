a32efe119dd1f62b3bf5772dab04b8b3b618ed33
silverwind <me@silverwind.io>
2024-01-29 19:30:42 +0100

    target es2020

--- updates.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  exclude: [
-    'pretty-ms', // pretty-ms@9 requires BigInt literals which require esbuild target es2020 or newer
-    '@mcaptcha/vanilla-glue', // broken when upgrading from alpha to rc
-  ],
-};
