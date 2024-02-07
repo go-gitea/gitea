4d0e945e7d287875531b437497c4b3d8dd0f6b47
TommyLike <tommylikehu@gmail.com>
2024-02-07 14:33:59 +0800

    Support remove config file

--- modules/setting/setting.go
+++ modules/setting/setting.go
@@ -112,15 +112,6 @@ func LoadCommonSettings() {
 
 // loadCommonSettingsFrom loads common configurations from a configuration provider.
 func loadCommonSettingsFrom(cfg ConfigProvider) error {
-	defer func() {
-		if RmCfg {
-			if err := os.Remove(CustomConf); err != nil {
-				log.Info("* remove config err: %v", err)
-			} else {
-				log.Info("* remove config successfully.")
-			}
-		}
-	}()
 	// WARNING: don't change the sequence except you know what you are doing.
 	loadRunModeFrom(cfg)
 	loadLogGlobalFrom(cfg)
@@ -139,10 +130,10 @@ func loadCommonSettingsFrom(cfg ConfigProvider) error {
 		log.Error("load lfs")
 		return err
 	}
-	if err := loadMQFrom(cfg); err != nil {
-		log.Error("load message queen")
-		return err
-	}
+	//if err := loadMQFrom(cfg); err != nil {
+	//	log.Error("load message queen")
+	//	return err
+	//}
 	loadTimeFrom(cfg)
 	loadRepositoryFrom(cfg)
 	if err := loadAvatarsFrom(cfg); err != nil {
