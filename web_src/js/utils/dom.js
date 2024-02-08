6299b57fd608bdd18781e5457115079ff568463c
wxiaoguang <wxiaoguang@gmail.com>
2024-02-08 09:42:25 +0800

    add el.ownerDocument check, add comments

--- web_src/js/utils/dom.js
+++ web_src/js/utils/dom.js
@@ -59,9 +59,11 @@ export function onDomReady(cb) {
   }
 }
 
+// checks whether an element is owned by the current document, and whether it is a document fragment or element node
+// if it is, it means it is a "normal" element managed by us, which can be modified safely.
 export function isDocumentFragmentOrElementNode(el) {
   try {
-    return el.nodeType === Node.ELEMENT_NODE || el.nodeType === Node.DOCUMENT_FRAGMENT_NODE;
+    return el.ownerDocument === document && el.nodeType === Node.ELEMENT_NODE || el.nodeType === Node.DOCUMENT_FRAGMENT_NODE;
   } catch {
     // in case the el is not in the same origin, then the access to nodeType would fail
     return false;
