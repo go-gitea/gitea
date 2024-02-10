eeac4080a5a8afe5a448c38031e8065f0aea5830
silverwind <me@silverwind.io>
2024-02-10 17:07:18 +0100

    fix lint

--- web_src/js/utils/image.test.js
+++ web_src/js/utils/image.test.js
@@ -3,8 +3,8 @@ import {pngChunks} from './image.js';
 test('pngChunks', async () => {
   const blob = await (await globalThis.fetch('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAAAAAA6fptVAAAADUlEQVQIHQECAP3/AAAAAgABzePRKwAAAABJRU5ErkJggg==')).blob();
   expect(pngChunks(new Uint8Array(await blob.arrayBuffer()))).toEqual([
-    {name: "IHDR", data: new Uint8Array([0, 0, 0, 1, 0, 0, 0, 1, 8, 0, 0, 0, 0])},
-    {name: "IDAT", data: new Uint8Array([8, 29, 1, 2, 0, 253, 255, 0, 0, 0, 2, 0, 1])},
-    {name: "IEND", data: new Uint8Array([])},
+    {name: 'IHDR', data: new Uint8Array([0, 0, 0, 1, 0, 0, 0, 1, 8, 0, 0, 0, 0])},
+    {name: 'IDAT', data: new Uint8Array([8, 29, 1, 2, 0, 253, 255, 0, 0, 0, 2, 0, 1])},
+    {name: 'IEND', data: new Uint8Array([])},
   ]);
 });
