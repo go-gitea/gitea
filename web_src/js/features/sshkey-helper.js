eb8e15ef3edb3de0a48c0ded5b06066d86b010cf
Yarden Shoham <git@yardenshoham.com>
2024-02-16 15:21:18 +0200

    Update web_src/js/features/sshkey-helper.js
    Co-authored-by: silverwind <me@silverwind.io>

--- web_src/js/features/sshkey-helper.js
+++ web_src/js/features/sshkey-helper.js
@@ -3,7 +3,7 @@ export function initSshKeyFormParser() {
   document.getElementById('ssh-key-content')?.addEventListener('input', function () {
     const arrays = this.value.split(' ');
     const title = document.getElementById('ssh-key-title');
-    if (title.value === '' && arrays.length === 3 && arrays[2] !== '') {
+    if (!title.value && arrays.length === 3 && arrays[2] !== '') {
       title.value = arrays[2];
     }
   });
