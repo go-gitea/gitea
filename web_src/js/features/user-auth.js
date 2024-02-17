28c31729747ce70509a1dd6fb32f78ea864a95f6
wxiaoguang <wxiaoguang@gmail.com>
2024-02-18 00:30:28 +0800

    remove initUserAuthLinkAccountView

--- web_src/js/features/user-auth.js
+++ web_src/js/features/user-auth.js
@@ -20,31 +20,3 @@ export function initUserAuthOauth2() {
     });
   }
 }
-
-export function initUserAuthLinkAccountView() {
-  const lnkUserPage = document.querySelector('.page-content.user.link-account');
-  if (!lnkUserPage) {
-    return false;
-  }
-
-  const signinTab = lnkUserPage.querySelector('.item[data-tab="auth-link-signin-tab"]');
-  const signUpTab = lnkUserPage.querySelector('.item[data-tab="auth-link-signup-tab"]');
-  const signInView = lnkUserPage.querySelector('.tab[data-tab="auth-link-signin-tab"]');
-  const signUpView = lnkUserPage.querySelector('.tab[data-tab="auth-link-signup-tab"]');
-
-  signUpTab.addEventListener('click', (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    signinTab.classList.remove('active');
-    signInView.classList.remove('active');
-    signUpTab.classList.add('active');
-    signUpView.classList.add('active');
-  });
-
-  signinTab.addEventListener('click', () => {
-    signUpTab.classList.remove('active');
-    signUpView.classList.remove('active');
-    signinTab.classList.add('active');
-    signInView.classList.add('active');
-  });
-}
