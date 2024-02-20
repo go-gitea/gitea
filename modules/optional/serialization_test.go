182db73fd52906d5b573802fc5e760aa051d664e
m.huber <m.huber@kithara.com>
2024-02-20 15:03:58 +0100

    WIP: make optional serializable

---
+++ modules/optional/serialization_test.go
@@ -0,0 +1,58 @@
+// Copyright 2024 The Gitea Authors. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+package optional
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type testSerializationStruct struct {
+	NormalString string         `json:"normal_string" yaml:"normal_string"`
+	NormalBool   bool           `json:"normal_bool" yaml:"normal_bool"`
+	OptBool      Option[bool]   `json:"optional_bool,omitempty" yaml:"optional_bool,omitempty"`
+	OptString    Option[string] `json:"optional_string,omitempty" yaml:"optional_string,omitempty"`
+}
+
+func TestOptionalToJson(t *testing.T) {
+	tests := []struct {
+		name string
+		obj  *testSerializationStruct
+		want string
+	}{
+		{
+			name: "empty",
+			obj:  new(testSerializationStruct),
+			want: `{"normal_string":"","normal_bool":false}`,
+		},
+		{
+			name: "empty",
+			obj: &testSerializationStruct{
+				NormalString: "a string",
+				NormalBool:   true,
+				OptBool:      Some(false),
+				OptString:    Some(""),
+			},
+			want: `{"normal_string":"a string","normal_bool":true,"normal_bool":false,"optional_string":""}`,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			b, err := json.Marshal(tc.obj)
+			assert.NoError(t, err)
+			assert.EqualValues(t, tc.want, string(b))
+		})
+	}
+}
+
+func TestOptionalFromJson(t *testing.T) {
+}
+
+func TestOptionalToYaml(t *testing.T) {
+}
+
+func TestOptionalFromYaml(t *testing.T) {
+}
