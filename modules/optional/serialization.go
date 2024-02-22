53280267c3e7a507a430791022c1fd2bec39db19
6543 <6543@obermui.de>
2024-02-22 16:36:20 +0100

    finish

--- modules/optional/serialization.go
+++ modules/optional/serialization.go
@@ -35,7 +35,7 @@ func (o *Option[T]) UnmarshalYAML(value *yaml.Node) error {
 	return nil
 }
 
-func (o *Option[T]) MarshalYAML() (interface{}, error) {
+func (o Option[T]) MarshalYAML() (interface{}, error) {
 	if !o.Has() {
 		return nil, nil
 	}
