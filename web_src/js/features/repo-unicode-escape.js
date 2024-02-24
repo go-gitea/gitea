3690c3be4b54a7c1a53be38e6557667d91b2c766
wxiaoguang <wxiaoguang@gmail.com>
2024-02-25 00:26:59 +0800

    fix

--- web_src/js/features/repo-unicode-escape.js
+++ web_src/js/features/repo-unicode-escape.js
@@ -1,27 +1,25 @@
-import {hideElem, showElem, toggleElem} from '../utils/dom.js';
+import {hideElem, queryElemSiblings, showElem, toggleElem} from '../utils/dom.js';
 
 export function initUnicodeEscapeButton() {
   document.addEventListener('click', (e) => {
-    const target = e.target;
-    if (!target.closest('.escape-button, .unescape-button, .toggle-escape-button')) return;
+    const btn = e.target.closest('.escape-button, .unescape-button, .toggle-escape-button');
+    if (!btn) return;
+
     e.preventDefault();
-    const fileContent = target.closest('.file-content, .non-diff-file-content');
+
+    const fileContent = btn.closest('.file-content, .non-diff-file-content');
     const fileView = fileContent?.querySelectorAll('.file-code, .file-view');
-    if (target.closest('.escape-button')) {
+    if (btn.matches('.escape-button')) {
       for (const el of fileView) el.classList.add('unicode-escaped');
-      hideElem(target);
-      const siblings = Array.from(target.parentNode.children).filter((child) => child !== target);
-      showElem(siblings.filter((s) => s.matches('.unescape-button')));
-    } else if (target.closest('.unescape-button')) {
+      hideElem(btn);
+      showElem(queryElemSiblings(btn, '.unescape-button'));
+    } else if (btn.closest('.unescape-button')) {
       for (const el of fileView) el.classList.remove('unicode-escaped');
-      hideElem(target);
-      const siblings = Array.from(target.parentNode.children).filter((child) => child !== target);
-      showElem(siblings.filter((s) => s.matches('.escape-button')));
-    } else if (target.closest('.toggle-escape-button')) {
-      const isEscaped = fileView[0].classList.contains('unicode-escaped');
-      for (const el of fileView) {
-        el.classList.toggle('unicode-escaped', isEscaped);
-      }
+      hideElem(btn);
+      showElem(queryElemSiblings(btn, '.escape-button'));
+    } else if (btn.closest('.toggle-escape-button')) {
+      const isEscaped = fileView[0]?.classList.contains('unicode-escaped');
+      for (const el of fileView) el.classList.toggle('unicode-escaped', !isEscaped);
       toggleElem(fileContent.querySelectorAll('.unescape-button'), !isEscaped);
       toggleElem(fileContent.querySelectorAll('.escape-button'), isEscaped);
     }
