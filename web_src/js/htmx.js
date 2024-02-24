c1512d3d0301a0235a2dc001a473cc82740eda7f
Anbraten <6918444+anbraten@users.noreply.github.com>
2024-02-24 02:06:02 +0100

    rename event to e

--- web_src/js/htmx.js
+++ web_src/js/htmx.js
@@ -1,24 +1,28 @@
-import * as htmx from 'htmx.org';
-import {showErrorToast} from './modules/toast.js';
-import 'htmx.org/dist/ext/ws.js';
+import * as htmx from "htmx.org";
+import { showErrorToast } from "./modules/toast.js";
+import "htmx.org/dist/ext/ws.js";
 
 // https://github.com/bigskysoftware/idiomorph#htmx
-import 'idiomorph/dist/idiomorph-ext.js';
+import "idiomorph/dist/idiomorph-ext.js";
 
 // https://htmx.org/reference/#config
-htmx.config.requestClass = 'is-loading';
+htmx.config.requestClass = "is-loading";
 htmx.config.scrollIntoViewOnBoost = false;
 
 // https://htmx.org/events/#htmx:sendError
-document.body.addEventListener('htmx:sendError', (event) => {
+document.body.addEventListener("htmx:sendError", (event) => {
   // TODO: add translations
-  showErrorToast(`Network error when calling ${event.detail.requestConfig.path}`);
+  showErrorToast(
+    `Network error when calling ${event.detail.requestConfig.path}`
+  );
 });
 
 // https://htmx.org/events/#htmx:responseError
-document.body.addEventListener('htmx:responseError', (event) => {
+document.body.addEventListener("htmx:responseError", (event) => {
   // TODO: add translations
-  showErrorToast(`Error ${event.detail.xhr.status} when calling ${event.detail.requestConfig.path}`);
+  showErrorToast(
+    `Error ${event.detail.xhr.status} when calling ${event.detail.requestConfig.path}`
+  );
 });
 
 // eslint-disable-next-line no-import-assign
@@ -26,9 +30,12 @@ htmx.createWebSocket = (url) => {
   // TODO: reuse websocket from shared webworker
   const sock = new WebSocket(url, []);
   sock.binaryType = htmx.config.wsBinaryType;
+  g;
   return sock;
 };
-document.body.addEventListener('htmx:wsOpen', (evt) => {
-  const socket = evt.detail.socketWrapper;
-  socket.send(JSON.stringify({action: 'subscribe', data: {url: window.location.href}}));
+document.body.addEventListener("htmx:wsOpen", (e) => {
+  const socket = e.detail.socketWrapper;
+  socket.send(
+    JSON.stringify({ action: "subscribe", data: { url: window.location.href } })
+  );
 });
