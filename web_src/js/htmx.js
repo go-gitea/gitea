201fd83233489329c77f5e22bdb59bd8055f2801
Anbraten <6918444+anbraten@users.noreply.github.com>
2024-02-24 02:07:05 +0100

    rename event to e

--- web_src/js/htmx.js
+++ web_src/js/htmx.js
@@ -10,18 +10,16 @@ htmx.config.requestClass = "is-loading";
 htmx.config.scrollIntoViewOnBoost = false;
 
 // https://htmx.org/events/#htmx:sendError
-document.body.addEventListener("htmx:sendError", (event) => {
+document.body.addEventListener("htmx:sendError", (e) => {
   // TODO: add translations
-  showErrorToast(
-    `Network error when calling ${event.detail.requestConfig.path}`
-  );
+  showErrorToast(`Network error when calling ${e.detail.requestConfig.path}`);
 });
 
 // https://htmx.org/events/#htmx:responseError
-document.body.addEventListener("htmx:responseError", (event) => {
+document.body.addEventListener("htmx:responseError", (e) => {
   // TODO: add translations
   showErrorToast(
-    `Error ${event.detail.xhr.status} when calling ${event.detail.requestConfig.path}`
+    `Error ${e.detail.xhr.status} when calling ${e.detail.requestConfig.path}`
   );
 });
 
