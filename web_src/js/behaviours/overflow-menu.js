c2585363396fb7bee3743b5f393b7a77214264a1
silverwind <me@silverwind.io>
2024-02-26 21:39:35 +0100

    progress

--- web_src/js/behaviours/overflow-menu.js
+++ web_src/js/behaviours/overflow-menu.js
@@ -1,54 +1,72 @@
-import {throttle} from 'throttle-debounce';
+import {debounce} from 'throttle-debounce';
 import {svg} from '../svg.js';
 import {createTippy} from '../modules/tippy.js';
 
-const update = throttle(100, (menu) => {
-  const menuItems = menu.querySelectorAll('.overflow-menu-items .item');
-  const buttonItems = [];
+const update = debounce(250, (menu) => {
+  const menuParent = menu.querySelector('.overflow-menu-items');
+  let buttonParent = menu.querySelector('.overflow-menu-button-items');
+  const menuItems = menuParent.querySelectorAll('.item');
 
-  const {right: menuRight} = menu.getBoundingClientRect();
+  if (!buttonParent) {
+    const div = document.createElement('div');
+    div.classList.add('overflow-menu-button-items', 'ui', 'secondary', 'vertical', 'menu', 'tippy-target');
+    menu.append(div);
+    buttonParent = div;
+  }
+
+  const buttonItems = buttonParent?.querySelectorAll('.item') || [];
+
+  for (const item of buttonItems) {
+    menuParent.append(item);
+  }
+
+  // measure which items are outside the element boundary and move them into the button menu
+  const itemsToMove = [];
+  const {right: menuRight} = menu.parentNode.getBoundingClientRect();
   for (const item of menuItems) {
     const {right: itemRight} = item.getBoundingClientRect();
-    if (itemRight >= menuRight) {
-      buttonItems.push(item.cloneNode(true));
-      item.remove();
+    if (menuRight - itemRight < 40) {
+      itemsToMove.push(item);
     }
   }
 
-  if (buttonItems.length && !menu.querySelector('.overflow-menu-button')) {
-    const btn = document.createElement('button');
-    btn.classList.add('overflow-menu-button', 'btn', 'tw-px-2');
-    btn.innerHTML = svg('octicon-kebab-horizontal');
-
-    const itemsMenu = document.createElement('div');
-    itemsMenu.classList.add('overflow-menu-tippy', 'ui', 'secondary', 'vertical', 'menu');
-    for (const item of buttonItems) {
-      itemsMenu.append(item);
+  if (itemsToMove?.length) {
+    for (const item of itemsToMove) {
+      buttonParent.append(item);
     }
 
-    createTippy(btn, {
-      trigger: 'click',
-      hideOnClick: true,
-      interactive: true,
-      placement: 'bottom-end',
-      role: 'menu',
-      content: itemsMenu,
-    });
+    if (!menu.querySelector('.overflow-menu-button')) {
+      const btn = document.createElement('button');
+      btn.classList.add('overflow-menu-button', 'btn', 'tw-px-2');
+      btn.innerHTML = svg('octicon-kebab-horizontal');
+      menu.append(btn);
 
-    menu.append(btn);
+      createTippy(btn, {
+        trigger: 'click',
+        hideOnClick: true,
+        interactive: true,
+        placement: 'bottom-end',
+        role: 'menu',
+        content: buttonParent.cloneNode(true),
+      });
+    }
+  } else {
+    menu.querySelector('.overflow-menu-button')?.remove();
   }
 });
 
 export function initOverflowMenu() {
   for (const el of document.querySelectorAll('.overflow-menu')) {
     update(el);
+    let lastSize;
     (new ResizeObserver((entries) => {
-      // raf seems to avoid 'ResizeObserver loop completed with undelivered notifications' error
-      requestAnimationFrame(() => {
-        for (const entry of entries) {
+      for (const entry of entries) {
+        const newSize = entry.contentBoxSize[0].inlineSize;
+        if (newSize !== lastSize) { // only trigger update on horizontal size change
           update(entry.target);
+          lastSize = newSize;
         }
-      });
+      }
     })).observe(el);
   }
 }
