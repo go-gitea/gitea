80e69d95242ed700430f9105da0db9739633b6ee
techknowlogick <hello@techknowlogick.com>
2024-02-29 05:44:40 +0000

    add tests

---
+++ models/migrations/v1_22/v287_test.go
@@ -0,0 +1,40 @@
+// Copyright 2024 The Gitea Authors. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+package v1_22 //nolint
+
+import (
+	"testing"
+
+	"code.gitea.io/gitea/models/migrations/base"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_UpdateBadgeColName(t *testing.T) {
+
+	type Badge struct {
+		ID          int64  `xorm:"pk autoincr"`
+		ID          int64  `xorm:"pk autoincr"`
+		Slug        string `xorm:"UNIQUE"`
+		Description string
+		ImageURL    string
+	}
+
+	// Prepare and load the testing database
+	x, deferable := base.PrepareTestEnv(t, 0, new(BadgeUnique), new(Badge))
+	defer deferable()
+	if x == nil || t.Failed() {
+		return
+	}
+
+	// TODO: insert example badges
+
+	if err := UseSlugInsteadOfIDForBadges(x); err != nil {
+		assert.NoError(t, err)
+		return
+	}
+
+	// TODO: check if badges have been updated
+
+}
--- routers/api/v1/admin/user_badge.go
+++ routers/api/v1/admin/user_badge.go
@@ -7,9 +7,9 @@ import (
 	"net/http"
 
 	user_model "code.gitea.io/gitea/models/user"
-	"code.gitea.io/gitea/modules/context"
 	api "code.gitea.io/gitea/modules/structs"
 	"code.gitea.io/gitea/modules/web"
+	"code.gitea.io/gitea/services/context"
 )
 
 // ListUserBadges lists all badges belonging to a user
