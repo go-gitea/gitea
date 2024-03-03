e145a5de0bd922abc3311924d559e65a5e8a1efd
silverwind <me@silverwind.io>
2024-03-03 05:10:45 +0100

    split webcomponents into two scripts to avoid all FOUCs

---
+++ web_src/js/webcomponents/defer.js
@@ -0,0 +1,3 @@
+// components here will load in a <script defer> element which may be necessary to prevent
+// whole-page FOUC
+import './overflow-menu.js';
--- web_src/js/webcomponents/index.js
+++ web_src/js/webcomponents/index.js
@@ -1,4 +1,3 @@
 import './polyfills.js';
 import '@github/relative-time-element';
 import './origin-url.js';
-import './overflow-menu.js';
