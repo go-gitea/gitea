18308514d5e863bb1f5b3f4bb062060b8d2a4d53
silverwind <me@silverwind.io>
2024-03-03 14:37:40 +0100

    remove unneeded class

--- web_src/js/webcomponents/overflow-menu.js
+++ web_src/js/webcomponents/overflow-menu.js
@@ -6,7 +6,7 @@ window.customElements.define('overflow-menu', class extends HTMLElement {
   updateItems = throttle(100, () => {
     if (!this.tippyContent) {
       const div = document.createElement('div');
-      div.classList.add('overflow-menu-tippy-content', 'ui', 'vertical', 'menu', 'tippy-target');
+      div.classList.add('ui', 'vertical', 'menu', 'tippy-target');
       this.append(div);
       this.tippyContent = div;
     }
