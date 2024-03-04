0c4b2dfa8f8509e1ad1ff3e4bb490591ea0791aa
stuzer05 <stuzer05@stuzer.link>
2024-03-04 11:59:57 +0200

    Commit

--- models/migrations/v1_22/v288.go
+++ models/migrations/v1_22/v288.go
@@ -7,40 +7,10 @@ import (
 	"xorm.io/xorm"
 )
 
-type BadgeUnique struct {
-	ID   int64  `xorm:"pk autoincr"`
-	Slug string `xorm:"UNIQUE"`
-}
-
-func (BadgeUnique) TableName() string {
-	return "badge"
-}
-
-func UseSlugInsteadOfIDForBadges(x *xorm.Engine) error {
-	type Badge struct {
-		Slug string
+func AddTimeEstimateColumnToIssueTable(x *xorm.Engine) error {
+	type Issue struct {
+		TimeEstimate int64 `xorm:"NOT NULL DEFAULT 0"`
 	}
 
-	err := x.Sync(new(Badge))
-	if err != nil {
-		return err
-	}
-
-	sess := x.NewSession()
-	defer sess.Close()
-	if err := sess.Begin(); err != nil {
-		return err
-	}
-
-	_, err = sess.Exec("UPDATE `badge` SET `slug` = `id` Where `slug` IS NULL")
-	if err != nil {
-		return err
-	}
-
-	err = sess.Sync(new(BadgeUnique))
-	if err != nil {
-		return err
-	}
-
-	return sess.Commit()
+	return x.Sync(new(Issue))
 }
