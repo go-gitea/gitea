34f7e91be4ed1356159b9b7f228ac56c8bd364a2
silverwind <me@silverwind.io>
2024-03-05 20:31:22 +0100

    add comments, style tweaks

--- web_src/js/markup/anchors.js
+++ web_src/js/markup/anchors.js
@@ -2,6 +2,7 @@ import {svg} from '../svg.js';
 
 const headingSelector = '.markup h1, .markup h2, .markup h3, .markup h4, .markup h5, .markup h6';
 
+// scroll to anchor while respecting the `user-content` prefix that exists on the target
 function scrollToAnchor(hash, initial) {
   // abort if the browser has already scrolled to another anchor during page load
   if (initial && document.querySelector(':target')) return;
@@ -19,6 +20,7 @@ function scrollToAnchor(hash, initial) {
 export function initMarkupAnchors() {
   if (!document.querySelector('.markup')) return;
 
+  // create link icons for markup headings, the resulting link href will remove `user-content-`
   for (const heading of document.querySelectorAll(headingSelector)) {
     const originalId = heading.id.replace(/^user-content-/, '');
     const a = document.createElement('a');
@@ -30,20 +32,20 @@ export function initMarkupAnchors() {
     });
     heading.prepend(a);
   }
-  for (const anchor of document.querySelectorAll('.markup a[href^="#"]')) {
-    const href = anchor.getAttribute('href');
-    if (!href.startsWith('#user-content-')) {
-      continue;
-    }
+
+  // handle user-defined `name` anchors like `[Link](#link)` linking to `<a name="link"></a>Link`
+  for (const a of document.querySelectorAll('.markup a[href^="#"]')) {
+    const href = a.getAttribute('href');
+    if (!href.startsWith('#user-content-')) continue;
     const originalId = href.replace(/^#user-content-/, '');
-    anchor.setAttribute('href', `#${encodeURIComponent(originalId)}`);
+    a.setAttribute('href', `#${encodeURIComponent(originalId)}`);
     const anchorDest = document.getElementsByName(originalId);
-    if (anchorDest?.length === 1) {
-      anchor.addEventListener('click', () => {
+    if (anchorDest.length === 1) {
+      a.addEventListener('click', () => {
         anchorDest[0].scrollIntoView();
       });
     } else {
-      anchor.addEventListener('click', (e) => {
+      a.addEventListener('click', (e) => {
         scrollToAnchor(e.currentTarget.getAttribute('href'), false);
       });
     }
