e785862af7b9a65b9f70067ca71fd20f8538f667
yp05327 <576951401@qq.com>
2024-03-08 15:57:45 +0000

    fix

--- modules/markup/markdown/markdown.go
+++ modules/markup/markdown/markdown.go
@@ -104,7 +104,7 @@ func SpecializedMarkdown() goldmark.Markdown {
 							}
 
 							// include language-x class as part of commonmark spec
-							_, err = w.WriteString(`<code class="chroma language-` + string(language) + `">`)
+							_, err = w.WriteString(`<code class="chroma language-` + string(language) + ` display">`)
 							if err != nil {
 								return
 							}
