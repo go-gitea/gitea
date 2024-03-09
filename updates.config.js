b67a929c18664806d930c1d78b90d9d9c2f93695
silverwind <me@silverwind.io>
2024-03-09 16:20:49 +0100

    remove unrelated update.config.js change

--- updates.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  exclude: [
-    '@mcaptcha/vanilla-glue', // breaking changes in rc versions
-    'eslint-plugin-array-func', // need to migrate to eslint flat config first
-  ],
-};
