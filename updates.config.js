21bef3aeccf1ac9c25a301bdad5c3ea0e6074b1c
silverwind <me@silverwind.io>
2024-03-09 16:19:16 +0100

    add error filter and upgrade monaco again

--- updates.config.js
+++ updates.config.js
@@ -2,6 +2,5 @@ export default {
   exclude: [
     '@mcaptcha/vanilla-glue', // breaking changes in rc versions
     'eslint-plugin-array-func', // need to migrate to eslint flat config first
-    'monaco-editor', // https://github.com/microsoft/monaco-editor/issues/4325
   ],
 };
