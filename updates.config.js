676a823d0d8219cd6d86e7bbe624a957f744874d
silverwind <me@silverwind.io>
2024-03-09 03:13:45 +0100

    Update updates.config.js

--- updates.config.js
+++ updates.config.js
@@ -1,7 +1,7 @@
 export default {
   exclude: [
-    'monaco-editor', // https://github.com/microsoft/monaco-editor/issues/4325
     '@mcaptcha/vanilla-glue', // breaking changes in rc versions
     'eslint-plugin-array-func', // need to migrate to eslint flat config first
+    'monaco-editor', // https://github.com/microsoft/monaco-editor/issues/4325
   ],
 };
