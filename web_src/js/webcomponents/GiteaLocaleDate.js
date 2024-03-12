cd9c7fb558251243bee528cb68728e2dc081906e
silverwind <me@silverwind.io>
2024-03-12 19:22:45 +0100

    Update web_src/js/webcomponents/GiteaLocaleDate.js

--- web_src/js/webcomponents/GiteaLocaleDate.js
+++ web_src/js/webcomponents/GiteaLocaleDate.js
@@ -10,7 +10,9 @@ window.customElements.define('gitea-locale-date', class extends HTMLElement {
       this.ownerDocument.documentElement.getAttribute('lang') ||
       '';
 
-    // only extract the `yyyy-mm-dd` part
+    // only extract the `yyyy-mm-dd` part. When converting to Date, the date will be in UTC and when rendered
+    // as locale date, will have a offset towards UTC added. We should eventually use `Temporal.PlainDate` here
+    // to avoid needing to remove this offset: https://tc39.es/proposal-temporal/docs/plaindate.html
     const date = new Date(this.getAttribute('date').substring(0, 10));
 
     // apply negative timezone offset because `new Date()` above assumes that `yyyy-mm-dd` is
