e7af015f835e996f2cc997571d7773e7cde31c9e
silverwind <me@silverwind.io>
2024-03-21 09:16:59 +0100

    Fix JS error and improve error styles
    Fixes: https://github.com/go-gitea/gitea/issues/29956

--- web_src/js/features/repo-common.js
+++ web_src/js/features/repo-common.js
@@ -76,6 +76,8 @@ export function initRepoCommonBranchOrTagDropdown(selector) {
 
 export function initRepoCommonFilterSearchDropdown(selector) {
   const $dropdown = $(selector);
+  if (!$dropdown.length) return;
+
   $dropdown.dropdown({
     fullTextSearch: 'exact',
     selectOnKeydown: false,
