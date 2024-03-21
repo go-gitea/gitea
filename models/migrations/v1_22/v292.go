0093855e40d7eb116f6739c7f602153053b10a7e
Denys Konovalov <kontakt@denyskon.de>
2024-03-21 20:17:18 +0100

    simplify migration\n\nCo-authored-by: devlh <dev.lh@web.de>

--- models/migrations/v1_22/v292.go
+++ models/migrations/v1_22/v292.go
@@ -5,68 +5,28 @@ package v1_22 //nolint
 
 import (
 	"code.gitea.io/gitea/models/project"
-	"code.gitea.io/gitea/modules/setting"
-	"code.gitea.io/gitea/modules/timeutil"
 
 	"xorm.io/xorm"
 )
 
 func AddDefaultProjectColumn(x *xorm.Engine) error {
-	sess := x.NewSession()
-	defer sess.Close()
+	var projectsWithMissingDefault []project.Project
+	x.SQL("SELECT DISTINCT `p`.`id`, `p`.`creator_id` FROM `project` `p` WHERE NOT EXISTS (SELECT 1 FROM `project_board` `pb` WHERE `pb`.`project_id` = `p`.`id` AND `pb`.`default` != 0);").
+		Find(&projectsWithMissingDefault)
 
-	if err := sess.Begin(); err != nil {
+	var boards []project.Board
+	for _, p := range projectsWithMissingDefault {
+		boards = append(boards, project.Board{
+			ProjectID: p.ID,
+			Default:   true,
+			Title:     "Backlog",
+			CreatorID: p.CreatorID,
+		})
+	}
+
+	if _, err := x.Insert(boards); err != nil {
 		return err
 	}
 
-	limit := setting.Database.IterateBufferSize
-	if limit <= 0 {
-		limit = 50
-	}
-
-	start := 0
-
-	for {
-		var projects []*project.Project
-		if err := sess.Select("id, creator_id").Limit(limit, start).Find(&projects); err != nil {
-			return err
-		}
-
-		if len(projects) == 0 {
-			break
-		}
-		start += len(projects)
-
-		for _, p := range projects {
-			var board project.Board
-			exist, err := sess.Where("project_id=? AND `default`=?", p.ID, true).Get(&board)
-			if err != nil {
-				return err
-			}
-
-			if !exist {
-				b := project.Board{
-					CreatedUnix: timeutil.TimeStampNow(),
-					CreatorID:   p.CreatorID,
-					Title:       "Backlog",
-					ProjectID:   p.ID,
-					Default:     true,
-				}
-				if _, err := sess.Insert(b); err != nil {
-					return err
-				}
-			}
-		}
-
-		if start%1000 == 0 {
-			if err := sess.Commit(); err != nil {
-				return err
-			}
-			if err := sess.Begin(); err != nil {
-				return err
-			}
-		}
-	}
-
-	return sess.Commit()
+	return nil
 }
