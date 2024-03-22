37207b978af70b434ec97a35303b266f4c1a70a7
silverwind <me@silverwind.io>
2024-03-22 01:19:56 +0100

    proper fix

--- web_src/js/components/RepoActionView.vue
+++ web_src/js/components/RepoActionView.vue
@@ -622,9 +622,9 @@ export function initRepositoryActionView() {
   width: 70%;
   display: flex;
   flex-direction: column;
-  background: var(--color-console-bg);
   border: 1px solid var(--color-console-border);
   border-radius: var(--border-radius);
+  align-self: flex-start;
 }
 
 /* begin fomantic button overrides */
