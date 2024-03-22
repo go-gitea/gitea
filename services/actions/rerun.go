f91b4dd959f14d2916fa21c8fa9d3fa7ab3efdef
Giteabot <teabot@gitea.io>
2024-03-22 11:57:10 +0800

    Fix bugs in rerunning jobs (#29955) (#29983)
    Backport #29955 by @Zettat123
    
    Fix #28761
    Fix #27884
    Fix #28093
    
    ## Changes
    
    ### Rerun all jobs
    When rerun all jobs, status of the jobs with `needs` will be set to
    `blocked` instead of `waiting`. Therefore, these jobs will not run until
    the required jobs are completed.
    
    ### Rerun a single job
    When a single job is rerun, its dependents should also be rerun, just
    like GitHub does
    (https://github.com/go-gitea/gitea/issues/28761#issuecomment-2008620820).
    In this case, only the specified job will be set to `waiting`, its
    dependents will be set to `blocked` to wait the job.
    
    ### Show warning if every job has `needs`
    If every job in a workflow has `needs`, all jobs will be blocked and no
    job can be run. So I add a warning message.
    
    <img
    src="https://github.com/go-gitea/gitea/assets/15528715/88f43511-2360-465d-be96-ee92b57ff67b"
    width="480px" />
    
    ---------
    
    Co-authored-by: Zettat123 <zettat123@gmail.com>
    Co-authored-by: Lunny Xiao <xiaolunwen@gmail.com>

---
+++ services/actions/rerun.go
@@ -0,0 +1,38 @@
+// Copyright 2024 The Gitea Authors. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+package actions
+
+import (
+	actions_model "code.gitea.io/gitea/models/actions"
+	"code.gitea.io/gitea/modules/container"
+)
+
+// GetAllRerunJobs get all jobs that need to be rerun when job should be rerun
+func GetAllRerunJobs(job *actions_model.ActionRunJob, allJobs []*actions_model.ActionRunJob) []*actions_model.ActionRunJob {
+	rerunJobs := []*actions_model.ActionRunJob{job}
+	rerunJobsIDSet := make(container.Set[string])
+	rerunJobsIDSet.Add(job.JobID)
+
+	for {
+		found := false
+		for _, j := range allJobs {
+			if rerunJobsIDSet.Contains(j.JobID) {
+				continue
+			}
+			for _, need := range j.Needs {
+				if rerunJobsIDSet.Contains(need) {
+					found = true
+					rerunJobs = append(rerunJobs, j)
+					rerunJobsIDSet.Add(j.JobID)
+					break
+				}
+			}
+		}
+		if !found {
+			break
+		}
+	}
+
+	return rerunJobs
+}
