c28fe8be224bb13048bc7b4f1b77bbb7b5b34679
silverwind <me@silverwind.io>
2024-03-23 12:39:25 +0100

    remove unneeded attributes and add comments

--- web_src/js/utils/misc.js
+++ web_src/js/utils/misc.js
@@ -11,10 +11,12 @@ export function getFileViewFileText() {
 }
 
 // create a external link with suitable attributes
+// - no `noopener` attribute because browser defaults to it with target `_blank`
+// - no `noreferrer` attribute because we use `<meta name="referrer" content="no-referrer">`
 export function createExternalLink(props = {}) {
   const a = document.createElement('a');
   a.target = '_blank';
-  a.rel = 'noopener noreferrer nofollow';
+  a.rel = 'nofollow';
   for (const [key, value] of Object.entries(props)) {
     a[key] = value;
   }
