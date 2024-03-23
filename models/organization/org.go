fbc9289dda81c447a7bc0d391a6d90f816f991b7
DrMaxNix <mail@drmaxnix.de>
2024-03-23 22:59:58 +0100

    Respect DEFAULT_ORG_MEMBER_VISIBLE setting when adding creator to org (#30013)
    This PR adds `setting.Service.DefaultOrgMemberVisible` value to dataset
    of user when the initial org creator is being added to the created org.
    
    Fixes #30012.

--- models/organization/org.go
+++ models/organization/org.go
@@ -319,8 +319,9 @@ func CreateOrganization(org *Organization, owner *user_model.User) (err error) {
 
 	// Add initial creator to organization and owner team.
 	if err = db.Insert(ctx, &OrgUser{
-		UID:   owner.ID,
-		OrgID: org.ID,
+		UID:      owner.ID,
+		OrgID:    org.ID,
+		IsPublic: setting.Service.DefaultOrgMemberVisible,
 	}); err != nil {
 		return fmt.Errorf("insert org-user relation: %w", err)
 	}
