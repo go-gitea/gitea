dd9e258651e22d9cab28441a177817a8dbec84b6
Yarden Shoham <git@yardenshoham.com>
2024-03-26 18:49:02 +0000

    element -> el

--- web_src/js/features/repo-branch.js
+++ web_src/js/features/repo-branch.js
@@ -8,38 +8,38 @@ export function initRepoBranchButton() {
 
 function initRepoCreateBranchButton() {
   // 2 pages share this code, one is the branch list page, the other is the commit view page: create branch/tag from current commit (dirty code)
-  for (const element of document.querySelectorAll('.show-create-branch-modal')) {
-    element.addEventListener('click', () => {
-      let modalFormName = element.getAttribute('data-modal-form');
+  for (const el of document.querySelectorAll('.show-create-branch-modal')) {
+    el.addEventListener('click', () => {
+      let modalFormName = el.getAttribute('data-modal-form');
       if (!modalFormName) {
         modalFormName = '#create-branch-form';
       }
       const modalForm = document.querySelector(modalFormName);
       if (!modalForm) return;
-      modalForm.action = `${modalForm.getAttribute('data-base-action')}${element.getAttribute('data-branch-from-urlcomponent')}`;
+      modalForm.action = `${modalForm.getAttribute('data-base-action')}${el.getAttribute('data-branch-from-urlcomponent')}`;
 
-      let fromSpanName = element.getAttribute('data-modal-from-span');
+      let fromSpanName = el.getAttribute('data-modal-from-span');
       if (!fromSpanName) {
         fromSpanName = '#modal-create-branch-from-span';
       }
-      document.querySelector(fromSpanName).textContent = element.getAttribute('data-branch-from');
+      document.querySelector(fromSpanName).textContent = el.getAttribute('data-branch-from');
 
-      $(element.getAttribute('data-modal')).modal('show');
+      $(el.getAttribute('data-modal')).modal('show');
     });
   }
 }
 
 function initRepoRenameBranchButton() {
-  for (const element of document.querySelectorAll('.show-rename-branch-modal')) {
-    element.addEventListener('click', () => {
-      const target = element.getAttribute('data-modal');
+  for (const el of document.querySelectorAll('.show-rename-branch-modal')) {
+    el.addEventListener('click', () => {
+      const target = el.getAttribute('data-modal');
       const modal = document.querySelector(target);
-      const oldBranchName = element.getAttribute('data-old-branch-name');
+      const oldBranchName = el.getAttribute('data-old-branch-name');
       modal.querySelector('input[name=from]').value = oldBranchName;
 
       // display the warning that the branch which is chosen is the default branch
       const warn = modal.querySelector('.default-branch-warning');
-      toggleElem(warn, element.getAttribute('data-is-default-branch') === 'true');
+      toggleElem(warn, el.getAttribute('data-is-default-branch') === 'true');
 
       const text = modal.querySelector('[data-rename-branch-to]');
       text.textContent = text.getAttribute('data-rename-branch-to').replace('%s', oldBranchName);
