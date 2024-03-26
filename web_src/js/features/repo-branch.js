66806db1376f8970c3672b885bbacd8baaebc686
Yarden Shoham <git@yardenshoham.com>
2024-03-26 17:09:22 +0000

    Remove jQuery from the create/rename branch modals (except Fomantic)
    - Switched to plain JavaScript
    - Tested the create/rename branch modals' functionality and they work as before
    
    Signed-off-by: Yarden Shoham <git@yardenshoham.com>

--- web_src/js/features/repo-branch.js
+++ web_src/js/features/repo-branch.js
@@ -8,35 +8,41 @@ export function initRepoBranchButton() {
 
 function initRepoCreateBranchButton() {
   // 2 pages share this code, one is the branch list page, the other is the commit view page: create branch/tag from current commit (dirty code)
-  $('.show-create-branch-modal').on('click', function () {
-    let modalFormName = $(this).attr('data-modal-form');
-    if (!modalFormName) {
-      modalFormName = '#create-branch-form';
-    }
-    $(modalFormName)[0].action = $(modalFormName).attr('data-base-action') + $(this).attr('data-branch-from-urlcomponent');
-    let fromSpanName = $(this).attr('data-modal-from-span');
-    if (!fromSpanName) {
-      fromSpanName = '#modal-create-branch-from-span';
-    }
+  for (const element of document.querySelectorAll('.show-create-branch-modal')) {
+    element.addEventListener('click', () => {
+      let modalFormName = element.getAttribute('data-modal-form');
+      if (!modalFormName) {
+        modalFormName = '#create-branch-form';
+      }
+      const modalForm = document.querySelector(modalFormName);
+      if (!modalForm) return;
+      modalForm.action = modalForm.getAttribute('data-base-action') + element.getAttribute('data-branch-from-urlcomponent');
 
-    $(fromSpanName).text($(this).attr('data-branch-from'));
-    $($(this).attr('data-modal')).modal('show');
-  });
+      let fromSpanName = element.getAttribute('data-modal-from-span');
+      if (!fromSpanName) {
+        fromSpanName = '#modal-create-branch-from-span';
+      }
+      document.querySelector(fromSpanName).textContent = element.getAttribute('data-branch-from');
+
+      $(element.getAttribute('data-modal')).modal('show');
+    });
+  }
 }
 
 function initRepoRenameBranchButton() {
-  $('.show-rename-branch-modal').on('click', function () {
-    const target = $(this).attr('data-modal');
-    const $modal = $(target);
+  for (const element of document.querySelectorAll('.show-rename-branch-modal')) {
+    element.addEventListener('click', () => {
+      const target = element.getAttribute('data-modal');
+      const modal = document.querySelector(target);
+      const oldBranchName = element.getAttribute('data-old-branch-name');
+      modal.querySelector('input[name=from]').value = oldBranchName;
 
-    const oldBranchName = $(this).attr('data-old-branch-name');
-    $modal.find('input[name=from]').val(oldBranchName);
+      // display the warning that the branch which is chosen is the default branch
+      const warn = modal.querySelector('.default-branch-warning');
+      toggleElem(warn, element.getAttribute('data-is-default-branch') === 'true');
 
-    // display the warning that the branch which is chosen is the default branch
-    const $warn = $modal.find('.default-branch-warning');
-    toggleElem($warn, $(this).attr('data-is-default-branch') === 'true');
-
-    const $text = $modal.find('[data-rename-branch-to]');
-    $text.text($text.attr('data-rename-branch-to').replace('%s', oldBranchName));
-  });
+      const text = modal.querySelector('[data-rename-branch-to]');
+      text.textContent = text.getAttribute('data-rename-branch-to').replace('%s', oldBranchName);
+    });
+  }
 }
