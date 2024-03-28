b2e4abbb73e67ae5ddfb300a01fa36737f54a427
silverwind <me@silverwind.io>
2024-03-29 00:16:14 +0100

    Update web_src/js/modules/fomantic/checkbox.js
    Co-authored-by: delvh <dev.lh@web.de>

--- web_src/js/modules/fomantic/checkbox.js
+++ web_src/js/modules/fomantic/checkbox.js
@@ -6,7 +6,7 @@ export function initAriaCheckboxPatch() {
     if (el.hasAttribute('data-checkbox-patched')) continue;
     const label = el.querySelector('label');
     const input = el.querySelector('input');
-    if (!label || !input || input.getAttribute('id')) continue;
+    if (!label || !input || input.getAttribute('id') || label.getAttribute('for')) continue;
     const id = generateAriaId();
     input.setAttribute('id', id);
     label.setAttribute('for', id);
