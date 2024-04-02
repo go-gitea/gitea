30a101da8af38447b056923f89c899636ff56707
silverwind <me@silverwind.io>
2024-04-02 21:12:21 +0200

    fix the js tests

--- web_src/js/utils/color.js
+++ web_src/js/utils/color.js
@@ -1,19 +1,18 @@
 import tinycolor from 'tinycolor2';
 
 // Check similar implementation in modules/util/color.go and keep synchronization
-// Return R, G, B values defined in reletive luminance
 
-// Reference from: https://www.w3.org/WAI/GL/wiki/Relative_luminance
-function getLuminance(r, g, b) {
+// Returns relative luminance for SRGB - https://en.wikipedia.org/wiki/Relative_luminance
+function getLuminance(color) {
+  const {r, g, b} = tinycolor(color).toRgb();
   return (0.2126 * r + 0.7152 * g + 0.0722 * b) / 255;
 }
 
-// Reference from: https://firsching.ch/github_labels.html
+// Given a background color whether to render black or white foreground
 // In the future WCAG 3 APCA may be a better solution.
 // Check if text should use light color based on RGB of background
-export function contrastColor(color) {
-  const {r, g, b} = tinycolor(color).toRgb();
-  return getLuminance(r, g, b) < 0.453 ? '#fff' : '#000';
+export function contrastColor(backgroundColor) {
+  return getLuminance(backgroundColor) < 0.453 ? '#fff' : '#000';
 }
 
 function resolveColors(obj) {
