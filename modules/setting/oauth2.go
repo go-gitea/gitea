a29e505c15d7f5ee82f5ccdbd052a50d5ae44e77
Giteabot <teabot@gitea.io>
2024-04-07 09:43:54 +0800

    Refactor startup deprecation messages (#30305) (#30312)
    Backport #30305 by wxiaoguang
    
    It doesn't change logic, it only does:
    
    1. Rename the variable and function names
    2. Use more consistent format when mentioning config section&key
    3. Improve some messages
    
    Co-authored-by: wxiaoguang <wxiaoguang@gmail.com>

--- modules/setting/oauth2.go
+++ modules/setting/oauth2.go
@@ -168,7 +168,7 @@ func GetGeneralTokenSigningSecret() []byte {
 		}
 		if generalSigningSecret.CompareAndSwap(old, &jwtSecret) {
 			// FIXME: in main branch, the signing token should be refactored (eg: one unique for LFS/OAuth2/etc ...)
-			log.Warn("OAuth2 is not enabled, unable to use a persistent signing secret, a new one is generated, which is not persistent between restarts and cluster nodes")
+			logStartupProblem(1, log.WARN, "OAuth2 is not enabled, unable to use a persistent signing secret, a new one is generated, which is not persistent between restarts and cluster nodes")
 			return jwtSecret
 		}
 		return *generalSigningSecret.Load()
