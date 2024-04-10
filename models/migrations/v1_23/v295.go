32b679041cee276ce52cd952dae76bd026b94c4c
Lunny Xiao <xiaolunwen@gmail.com>
2024-04-10 13:32:38 +0800

    Rename index name on commist_status_summary table

--- models/migrations/v1_23/v295.go
+++ models/migrations/v1_23/v295.go
@@ -8,8 +8,8 @@ import "xorm.io/xorm"
 func AddCommitStatusSummary(x *xorm.Engine) error {
 	type CommitStatusSummary struct {
 		ID     int64  `xorm:"pk autoincr"`
-		RepoID int64  `xorm:"INDEX UNIQUE(repo_sha_index)"`
-		SHA    string `xorm:"VARCHAR(64) NOT NULL INDEX UNIQUE(repo_sha_index)"`
+		RepoID int64  `xorm:"INDEX UNIQUE(repo_id_sha)"`
+		SHA    string `xorm:"VARCHAR(64) NOT NULL INDEX UNIQUE(repo_id_sha)"`
 		State  string `xorm:"VARCHAR(7) NOT NULL"`
 	}
 	// there is no migrations because if there is no data on this table, it will fall back to get data
