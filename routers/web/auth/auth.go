8e846e27907572981f31500f3c34f494e6e03c73
Simon Ermler <simon_ermler@web.de>
2024-04-15 17:33:25 +0200

    Update error message

--- routers/web/auth/auth.go
+++ routers/web/auth/auth.go
@@ -391,7 +391,7 @@ func getUserName(gothUser *goth.User) (string, error) {
 		if exists {
 			return user_model.NormalizeUserName(preferredUsername.(string))
 		} else {
-			return "", fmt.Errorf("preferred_username is missing in received user data but configured as username source. Check if OPENID_CONNECT_SCOPES contains profile")
+			return "", fmt.Errorf("preferred_username is missing in received user data but configured as username source for user_id %q. Check if OPENID_CONNECT_SCOPES contains profile", gothUser.UserID)
 		}
 	case setting.OAuth2UsernameNickname:
 		return user_model.NormalizeUserName(gothUser.NickName)
