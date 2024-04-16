d133ccca71fb7af8942d9f526ddfb89017dd6bc7
silverwind <me@silverwind.io>
2024-04-16 23:11:54 +0200

    Update tests/integration/repo_mergecommit_revert_test.go

--- tests/integration/repo_mergecommit_revert_test.go
+++ tests/integration/repo_mergecommit_revert_test.go
@@ -1,4 +1,6 @@
 // Copyright 2024 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
 // SPDX-License-Identifier: MIT
 
 package integration
