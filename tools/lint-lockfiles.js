265bb8621cd83f2a1498d48815f37c2efa523128
silverwind <me@silverwind.io>
2024-04-26 14:47:04 +0200

    add comment

--- tools/lint-lockfiles.js
+++ tools/lint-lockfiles.js
@@ -12,6 +12,9 @@ const files = [
 const rootPath = fileURLToPath(new URL('..', import.meta.url));
 let hadErrors = false;
 
+// This checks that all "resolved" URLs in package-lock.json point to the official npm registry.
+// If a user is using a npm proxy (private or public), they would write that proxy's URL into
+// the file which we do not want because it could cause issues during installation.
 for (const file of files.map((file) => fileURLToPath(new URL(file, import.meta.url)))) {
   const data = JSON.parse(readFileSync(file));
   for (const [pkg, {resolved}] of Object.entries(data.packages)) {
