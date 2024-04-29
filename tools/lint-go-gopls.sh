29625cdcbceb457fde70939475b2220e640c60aa
silverwind <me@silverwind.io>
2024-04-29 19:41:44 +0200

    add IGNORE_PATTERNS

--- tools/lint-go-gopls.sh
+++ tools/lint-go-gopls.sh
@@ -3,11 +3,15 @@ set -euo pipefail
 
 cd "$(dirname -- "${BASH_SOURCE[0]}")" && cd ..
 
+IGNORE_PATTERNS=(
+  "svc.IsAnInteractiveSession is deprecated"
+)
+
 # lint all go files with 'gopls check' and look for lines starting with the
 # current absolute path, indicating a error was found. This is neccessary
 # because the tool does not set non-zero exit code when errors are found.
 # ref: https://github.com/golang/go/issues/67078
-ERROR_LINES=$("$GO" run "$GOPLS_PACKAGE" check $@ 2>/dev/null | grep -E "^$PWD");
+ERROR_LINES=$("$GO" run "$GOPLS_PACKAGE" check $@ 2>/dev/null | grep -E "^$PWD" | grep -vFf <(printf '%s\n' "${IGNORE_PATTERNS[@]}"));
 NUM_ERRORS=$(echo "$ERROR_LINES" | wc -l)
 
 if [ "$NUM_ERRORS" -eq "0" ]; then
