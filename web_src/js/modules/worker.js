147fdc833cd7af6161e63586146942e542a9fbe8
wxiaoguang <wxiaoguang@gmail.com>
2024-04-30 23:05:11 +0800

    Update worker.js

--- web_src/js/modules/worker.js
+++ web_src/js/modules/worker.js
@@ -1,8 +1,9 @@
+import {sleep} from '../utils.js';
+
 const {appSubUrl} = window.config;
 
-export function logoutFromWorker() {
+export async function logoutFromWorker() {
   // wait for a while because other requests (eg: logout) may be in the flight
-  setTimeout(() => {
-    window.location.href = `${appSubUrl}/`;
-  }, 5000);
+  await sleep(5000);
+  window.location.href = `${appSubUrl}/`;
 }
