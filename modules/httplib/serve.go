fbed6e2ae2c4ae3968998cf9f9e626a338f717d8
wxiaoguang <wxiaoguang@gmail.com>
2024-05-02 08:35:47 +0800

    Update modules/httplib/serve.go
    Co-authored-by: silverwind <me@silverwind.io>

--- modules/httplib/serve.go
+++ modules/httplib/serve.go
@@ -41,7 +41,7 @@ type ServeHeaderOptions struct {
 func ServeSetHeaders(w http.ResponseWriter, opts *ServeHeaderOptions) {
 	header := w.Header()
 
-	skipCompressionExts := container.SetOf(".gz", ".bz2", ".zip", ".xz", ".zst", ".deb", ".apk", ".jar", ".png", ".jpg")
+	skipCompressionExts := container.SetOf(".gz", ".bz2", ".zip", ".xz", ".zst", ".deb", ".apk", ".jar", ".png", ".jpg", ".webp")
 	if skipCompressionExts.Contains(path.Ext(opts.Filename)) {
 		w.Header().Add(gzhttp.HeaderNoCompression, "1")
 	}
