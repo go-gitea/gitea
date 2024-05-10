0754ed7c186f58d9e83e1520b52e7f63f6fe0e00
Carlos Felgueiras <carlosfelgueiras@tecnico.ulisboa.pt>
2024-05-10 16:17:42 +0000

    test(pin): created basic tests for the pin/unpin feature
    - Created unit tests that check the basic functionality of the model
      functions
    - Created integration tests that check the functionality of the POST
      routes, verifying the effects directly on the database.
    
    Co-authored-by: Daniel Carvalho <daniel.m.carvalho@tecnico.ulisboa.pt>

---
+++ models/repo/pin_test.go
@@ -0,0 +1,48 @@
+// Copyright 2017 The Gitea Authors. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+package repo_test
+
+import (
+	"testing"
+
+	"code.gitea.io/gitea/models/db"
+	repo_model "code.gitea.io/gitea/models/repo"
+	"code.gitea.io/gitea/models/unittest"
+	user_model "code.gitea.io/gitea/models/user"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPinRepoFunctionality(t *testing.T) {
+	assert.NoError(t, unittest.PrepareTestDatabase())
+
+	user := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: 2})
+	repo := unittest.AssertExistsAndLoadBean(t, &repo_model.Repository{ID: 1})
+
+	unittest.AssertNotExistsBean(t, &repo_model.Pin{UID: user.ID, RepoID: repo.ID})
+	assert.NoError(t, repo_model.PinRepo(db.DefaultContext, user, repo, true))
+	unittest.AssertExistsAndLoadBean(t, &repo_model.Pin{UID: user.ID, RepoID: repo.ID})
+	assert.NoError(t, repo_model.PinRepo(db.DefaultContext, user, repo, true))
+	unittest.AssertExistsAndLoadBean(t, &repo_model.Pin{UID: user.ID, RepoID: repo.ID})
+	assert.NoError(t, repo_model.PinRepo(db.DefaultContext, user, repo, false))
+	unittest.AssertNotExistsBean(t, &repo_model.Star{UID: user.ID, RepoID: repo.ID})
+}
+
+func TestIsPinned(t *testing.T) {
+	assert.NoError(t, unittest.PrepareTestDatabase())
+
+	user := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: 2})
+	repo := unittest.AssertExistsAndLoadBean(t, &repo_model.Repository{ID: 1})
+
+	unittest.AssertNotExistsBean(t, &repo_model.Pin{UID: user.ID, RepoID: repo.ID})
+	assert.NoError(t, repo_model.PinRepo(db.DefaultContext, user, repo, true))
+	unittest.AssertExistsAndLoadBean(t, &repo_model.Pin{UID: user.ID, RepoID: repo.ID})
+
+	assert.True(t, repo_model.IsPinned(db.DefaultContext, user.ID, repo.ID))
+
+	assert.NoError(t, repo_model.PinRepo(db.DefaultContext, user, repo, false))
+	unittest.AssertNotExistsBean(t, &repo_model.Star{UID: user.ID, RepoID: repo.ID})
+
+	assert.False(t, repo_model.IsPinned(db.DefaultContext, user.ID, repo.ID))
+}
---
+++ tests/integration/repo_pin_test.go
@@ -0,0 +1,60 @@
+package integration
+
+import (
+	"net/http"
+	"net/url"
+	"path"
+	"testing"
+
+	repo_model "code.gitea.io/gitea/models/repo"
+	"code.gitea.io/gitea/models/unittest"
+)
+
+func TestUserRepoPin(t *testing.T) {
+	onGiteaRun(t, func(t *testing.T, giteaURL *url.URL) {
+		session := loginUser(t, "user2")
+		unittest.AssertNotExistsBean(t, &repo_model.Pin{UID: 2, RepoID: 3})
+		testUserPinRepo(t, session, "org3", "repo3", true, false)
+		unittest.AssertExistsAndLoadBean(t, &repo_model.Pin{UID: 2, RepoID: 3})
+		testUserPinRepo(t, session, "org3", "repo3", false, false)
+		unittest.AssertNotExistsBean(t, &repo_model.Pin{UID: 2, RepoID: 3})
+	})
+}
+
+func TestOrgRepoPin(t *testing.T) {
+	onGiteaRun(t, func(t *testing.T, giteaURL *url.URL) {
+		session := loginUser(t, "user2")
+		unittest.AssertNotExistsBean(t, &repo_model.Pin{UID: 3, RepoID: 3})
+		testUserPinRepo(t, session, "org3", "repo3", true, true)
+		unittest.AssertExistsAndLoadBean(t, &repo_model.Pin{UID: 3, RepoID: 3})
+		testUserPinRepo(t, session, "org3", "repo3", false, true)
+		unittest.AssertNotExistsBean(t, &repo_model.Pin{UID: 3, RepoID: 3})
+	})
+}
+
+func testUserPinRepo(t *testing.T, session *TestSession, user, repo string, pin, org bool) error {
+	var action string
+	if pin {
+		action = "pin"
+	} else {
+		action = "unpin"
+	}
+
+	if org {
+		action += "-org"
+	}
+
+	// Get repo page to get the CSRF token
+	reqPage := NewRequest(t, "GET", path.Join(user, repo))
+	respPage := session.MakeRequest(t, reqPage, http.StatusOK)
+
+	htmlDoc := NewHTMLParser(t, respPage.Body)
+
+	reqPath := path.Join(user, repo, "action", action)
+	req := NewRequestWithValues(t, "POST", reqPath, map[string]string{
+		"_csrf": htmlDoc.GetCSRF(),
+	})
+	session.MakeRequest(t, req, http.StatusSeeOther)
+
+	return nil
+}
