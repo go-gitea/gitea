22a247ac4723903dc80e141b08bf7df30ee6b5a5
Lunny Xiao <xiaolunwen@gmail.com>
2024-05-17 12:35:14 +0800

    Fix test bug

--- modules/storage/azureblob_test.go
+++ modules/storage/azureblob_test.go
@@ -20,7 +20,7 @@ func TestAzureBlobStorageIterator(t *testing.T) {
 	testStorageIterator(t, setting.AzureBlobStorageType, &setting.Storage{
 		AzureBlobConfig: setting.AzureBlobStorageConfig{
 			// https://learn.microsoft.com/azure/storage/common/storage-use-azurite?tabs=visual-studio-code#ip-style-url
-			Endpoint: "http://127.0.0.1:10000/devstoreaccount1/",
+			Endpoint: "http://azurite:10000/devstoreaccount1/",
 			// https://learn.microsoft.com/azure/storage/common/storage-use-azurite?tabs=visual-studio-code#well-known-storage-account-and-key
 			AccountName: "devstoreaccount1",
 			AccountKey:  "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==",
