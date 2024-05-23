d1dc24972a5b9cb25250c59f8bd669cb7f9b660a
József Fényes <fenyesj@gmail.com>
2024-05-23 14:55:47 +0200

    Add SPDX-License-Identifiers

--- modules/git/hook.go
+++ modules/git/hook.go
@@ -1,5 +1,4 @@
-// Copyright 2015 The Gogs Authors. All rights reserved.
-// Copyright 2021 The Gitea Authors. All rights reserved.
+// Copyright 2024 The Gitea Authors. All rights reserved.
 // SPDX-License-Identifier: MIT
 
 package git
