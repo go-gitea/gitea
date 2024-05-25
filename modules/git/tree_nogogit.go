ae6370f8f1d6aa854113afbcec3f9ae7b7e9b797
Lunny Xiao <xiaolunwen@gmail.com>
2024-05-25 17:18:19 +0800

    Refactor iterate git tree

--- modules/git/tree_nogogit.go
+++ modules/git/tree_nogogit.go
@@ -108,6 +108,9 @@ func (t *Tree) listEntriesRecursive(ctx context.Context, extraArgs TrustedCmdArg
 		t.entriesRecursive = append(t.entriesRecursive, entry)
 		return nil
 	}, extraArgs)
+	if err == nil {
+		t.entriesRecursiveParsed = true
+	}
 	return t.entriesRecursive, err
 }
 
