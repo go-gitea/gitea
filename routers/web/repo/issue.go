eaea1e76521db01d048c3c434b577636c9527d09
silverwind <me@silverwind.io>
2024-05-30 17:13:33 +0200

    fmt

--- routers/web/repo/issue.go
+++ routers/web/repo/issue.go
@@ -2181,7 +2181,7 @@ func GetIssueInfo(ctx *context.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, map[string]any{
-		"issue": convert.ToIssue(ctx, ctx.Doer, issue),
+		"issue":      convert.ToIssue(ctx, ctx.Doer, issue),
 		"labelsHtml": templates.RenderLabels(ctx, ctx.Locale, issue.Labels, ctx.Repo.RepoLink, issue),
 	})
 }
