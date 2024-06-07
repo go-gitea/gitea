2167d4b112912a54e38309f0968dba1472620250
silverwind <me@silverwind.io>
2024-06-07 09:29:26 +0200

    add test

--- web_src/js/utils/dom.test.js
+++ web_src/js/utils/dom.test.js
@@ -2,4 +2,5 @@ import {createElementFromHTML} from './dom.js';
 
 test('createElementFromHTML', () => {
   expect(createElementFromHTML('<a>foo<span>bar</span></a>').textContent).toEqual('foobar');
+  expect(createElementFromHTML('<a>foo<span>bar</span></a>').outerHTML).toEqual('<a>foo<span>bar</span></a>');
 });
