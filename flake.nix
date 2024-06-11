fefb5ca307f52710dec14c601b52d7a920563238
m.huber <m.huber@kithara.com>
2024-06-11 15:37:26 +0200

    rev

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = nixpkgs.legacyPackages.${system};
       in
       {
         devShells.default = pkgs.mkShell {
