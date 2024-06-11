6ecf6cad8143e87832a3468b9a77ebc9b26146fa
6543 <6543@obermui.de>
2024-06-11 13:12:14 +0200

    update nix flake and a nit

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs { inherit system; };
       in
       {
         devShells.default = pkgs.mkShell {
