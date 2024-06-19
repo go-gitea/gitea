7a7b4583dc9fa0af55e4f3a93f9c2fa7d34de499
Sumit Paul <sumit.18.paul@gmail.com>
2024-06-20 03:14:17 +0530

    Convert original_author_id column data type to numeric

--- models/migrations/v1_23/v300.go
+++ models/migrations/v1_23/v300.go
@@ -6,17 +6,22 @@ package v1_23 //nolint
 import (
 	"strings"
 
+	"code.gitea.io/gitea/modules/setting"
 	"xorm.io/xorm"
 )
 
-func changeOriginalAuthorIDDataTypeToNumeric(x *xorm.Engine) error {
+func ConvertAuthorIDToNumeric(x *xorm.Engine) error {
+	// Google OAuth2 provider may give very long user IDs
+	if !setting.Database.Type.IsPostgreSQL() {
+		return nil
+	}
 	sql := strings.Join([]string{
-		"ALTER TABLE `issue` ALTER COLUMN `original_author_id` TYPE NUMERIC USING `original_author_id`::NUMERIC",
-		"ALTER TABLE `comment` ALTER COLUMN `original_author_id` TYPE NUMERIC USING `original_author_id`::NUMERIC",
-		"ALTER TABLE `release` ALTER COLUMN `original_author_id` TYPE NUMERIC USING `original_author_id`::NUMERIC",
-		"ALTER TABLE `reaction` ALTER COLUMN `original_author_id` TYPE NUMERIC USING `original_author_id`::NUMERIC",
-		"ALTER TABLE `review` ALTER COLUMN `original_author_id` TYPE NUMERIC USING `original_author_id`::NUMERIC",
-	}, "; ")
+		"ALTER TABLE issue ALTER COLUMN original_author_id TYPE NUMERIC USING original_author_id::NUMERIC;",
+		"ALTER TABLE comment ALTER COLUMN original_author_id TYPE NUMERIC USING original_author_id::NUMERIC;",
+		"ALTER TABLE release ALTER COLUMN original_author_id TYPE NUMERIC USING original_author_id::NUMERIC;",
+		"ALTER TABLE reaction ALTER COLUMN original_author_id TYPE NUMERIC USING original_author_id::NUMERIC;",
+		"ALTER TABLE review ALTER COLUMN original_author_id TYPE NUMERIC USING original_author_id::NUMERIC;",
+	}, " ")
 
 	_, err := x.Exec(sql)
 	return err
