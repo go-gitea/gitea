f5cb1d08037ece8d412e487443b50bfc83393a3d
Denys Konovalov <kontakt@denyskon.de>
2024-06-28 23:31:04 +0200

    Update web_src/js/features/oauth2-settings.js
    Co-authored-by: delvh <dev.lh@web.de>

--- web_src/js/features/oauth2-settings.js
+++ web_src/js/features/oauth2-settings.js
@@ -1,9 +1,5 @@
 export function initOAuth2SettingsDisableCheckbox() {
   for (const e of document.querySelectorAll('.disable-setting')) e.addEventListener('change', ({target}) => {
-    if (target.checked) {
-      document.querySelector(e.getAttribute('data-target')).classList.add('disabled');
-    } else {
-      document.querySelector(e.getAttribute('data-target')).classList.remove('disabled');
-    }
+    document.querySelector(e.getAttribute('data-target')).classList.toggle('disabled', target.checked);
   });
 }
