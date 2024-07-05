7e11c6eaf106361b9ddf74f72e1aa867d63e01e4
wxiaoguang <wxiaoguang@gmail.com>
2024-07-05 12:41:24 +0800

    Update v300.go

--- models/migrations/v1_23/v300.go
+++ models/migrations/v1_23/v300.go
@@ -7,17 +7,11 @@ import "xorm.io/xorm"
 
 func AddForcePushBranchProtection(x *xorm.Engine) error {
 	type ProtectedBranch struct {
-		WhitelistUserIDs             []int64 `xorm:"whitelist_user_ids JSON TEXT"`
-		WhitelistTeamIDs             []int64 `xorm:"whitelist_team_ids JSON TEXT"`
-		EnableForcePushAllowlist     bool
-		ForcePushAllowlistUserIDs    []int64 `xorm:"force_push_allowlist_user_ids JSON TEXT"`
-		ForcePushAllowlistTeamIDs    []int64 `xorm:"force_push_allowlist_team_ids JSON TEXT"`
-		ForcePushAllowlistDeployKeys bool    `xorm:"NOT NULL DEFAULT false"`
-		MergeWhitelistUserIDs        []int64 `xorm:"merge_whitelist_user_ids JSON TEXT"`
-		MergeWhitelistTeamIDs        []int64 `xorm:"merge_whitelist_team_ids JSON TEXT"`
-		ApprovalsWhitelistUserIDs    []int64 `xorm:"approvals_whitelist_user_ids JSON TEXT"`
-		ApprovalsWhitelistTeamIDs    []int64 `xorm:"approvals_whitelist_team_ids JSON TEXT"`
-	}
+	CanForcePush                 bool    `xorm:"NOT NULL DEFAULT false"`
+	EnableForcePushAllowlist     bool
+	ForcePushAllowlistUserIDs    []int64  `xorm:"force_push_allowlist_user_ids JSON TEXT"`
+	ForcePushAllowlistTeamIDs    []int64  `xorm:"force_push_allowlist_team_ids JSON TEXT"`
+	ForcePushAllowlistDeployKeys bool     `xorm:"NOT NULL DEFAULT false"`	}
 
 	return x.Sync(new(ProtectedBranch))
 }
