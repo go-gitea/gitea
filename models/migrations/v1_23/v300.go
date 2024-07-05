410eb5980c994167fda200bb2bab5060ef3fe9d4
Henry Goodman <13032204@student.uts.edu.au>
2024-07-05 14:25:58 +1000

    Update migration for ProtectedBranch struct fields

--- models/migrations/v1_23/v300.go
+++ models/migrations/v1_23/v300.go
@@ -7,16 +7,16 @@ import "xorm.io/xorm"
 
 func AddForcePushBranchProtection(x *xorm.Engine) error {
 	type ProtectedBranch struct {
-		WhitelistUserIDs             []int64 `json:"whitelist_user_ids" xorm:"JSON TEXT"`
-		WhitelistTeamIDs             []int64 `json:"whitelist_team_ids" xorm:"JSON TEXT"`
+		WhitelistUserIDs             []int64 `xorm:"whitelist_user_ids JSON TEXT"`
+		WhitelistTeamIDs             []int64 `xorm:"whitelist_team_ids JSON TEXT"`
 		EnableForcePushAllowlist     bool
-		ForcePushAllowlistUserIDs    []int64 `json:"force_push_allowlist_user_ids" xorm:"JSON TEXT"`
-		ForcePushAllowlistTeamIDs    []int64 `json:"force_push_allowlist_team_ids" xorm:"JSON TEXT"`
+		ForcePushAllowlistUserIDs    []int64 `xorm:"force_push_allowlist_user_ids JSON TEXT"`
+		ForcePushAllowlistTeamIDs    []int64 `xorm:"force_push_allowlist_team_ids JSON TEXT"`
 		ForcePushAllowlistDeployKeys bool    `xorm:"NOT NULL DEFAULT false"`
-		MergeWhitelistUserIDs        []int64 `json:"merge_whitelist_user_ids" xorm:"JSON TEXT"`
-		MergeWhitelistTeamIDs        []int64 `json:"merge_whitelist_team_ids" xorm:"JSON TEXT"`
-		ApprovalsWhitelistUserIDs    []int64 `json:"approvals_whitelist_user_ids" xorm:"JSON TEXT"`
-		ApprovalsWhitelistTeamIDs    []int64 `json:"approvals_whitelist_team_ids" xorm:"JSON TEXT"`
+		MergeWhitelistUserIDs        []int64 `xorm:"merge_whitelist_user_ids JSON TEXT"`
+		MergeWhitelistTeamIDs        []int64 `xorm:"merge_whitelist_team_ids JSON TEXT"`
+		ApprovalsWhitelistUserIDs    []int64 `xorm:"approvals_whitelist_user_ids JSON TEXT"`
+		ApprovalsWhitelistTeamIDs    []int64 `xorm:"approvals_whitelist_team_ids JSON TEXT"`
 	}
 
 	return x.Sync(new(ProtectedBranch))
