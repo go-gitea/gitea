04558b8eabdad0a958f875117b8bbb90b5229cd1
silverwind <me@silverwind.io>
2024-07-08 01:11:16 +0200

    Add types for js globals

--- types.d.ts
+++ types.d.ts
@@ -2,3 +2,12 @@ declare module '*.svg' {
   const value: string;
   export default value;
 }
+
+declare let __webpack_public_path__: string;
+
+interface Window {
+  config: import('./web_src/js/types.ts').Config;
+  $: typeof import('@types/jquery'),
+  jQuery: typeof import('@types/jquery'),
+  htmx: typeof import('htmx.org'),
+}
