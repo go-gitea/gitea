9aba15e71bf6ef15a626e69adeb1ae181fecf387
wxiaoguang <wxiaoguang@gmail.com>
2024-08-01 17:52:07 +0800

    fix

--- web_src/js/utils/dom.ts
+++ web_src/js/utils/dom.ts
@@ -297,7 +297,7 @@ export function createElementFromAttrs(tagName, attrs) {
   const el = document.createElement(tagName);
   for (const [key, value] of Object.entries(attrs)) {
     if (value === undefined || value === null) continue;
-    if (value === true) {
+    if (value === true || value === false) {
       el.toggleAttribute(key, value);
     } else {
       el.setAttribute(key, String(value));
