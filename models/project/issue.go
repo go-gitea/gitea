65a2d5a1423e9d0441f6e0c961bdfb253debbdc9
Edip Emre Bodur <emrebdr29@gmail.com>
2024-08-12 23:18:23 +0300

    Update models/project/issue.go
    Co-authored-by: Jason Song <i@wolfogre.com>

--- models/project/issue.go
+++ models/project/issue.go
@@ -144,12 +144,6 @@ func (c *Column) moveIssuesToAnotherColumn(ctx context.Context, newColumn *Colum
 
 // DeleteAllProjectIssueByIssueIDsAndProjectIDs delete all project's issues by issue's and project's ids
 func DeleteAllProjectIssueByIssueIDsAndProjectIDs(ctx context.Context, issueIDs, projectIDs []int64) error {
-	for _, id := range projectIDs {
-		_, err := db.GetEngine(ctx).Where("project_id=?", id).In("issue_id", issueIDs).Delete(&ProjectIssue{})
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
+	_, err := db.GetEngine(ctx).In("project_id", projectIDs).In("issue_id", issueIDs).Delete(&ProjectIssue{})
+	return err
 }
