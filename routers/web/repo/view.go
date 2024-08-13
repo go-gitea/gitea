619397dac841763f7e89c26791a9b01f657fc368
Rowan Bohde <rowan.bohde@gmail.com>
2024-08-13 10:08:05 -0500

    add a warning log when unable to access LFS pointer

--- routers/web/repo/view.go
+++ routers/web/repo/view.go
@@ -235,6 +235,7 @@ func getFileReader(ctx gocontext.Context, repoID int64, blob *git.Blob) ([]byte,
 
 	meta, err := git_model.GetLFSMetaObjectByOid(ctx, repoID, pointer.Oid)
 	if err != nil { // fallback to plain file
+		log.Warn("Unable to access LFS pointer %s in repo %d: %v", pointer.Oid, repoID, err)
 		return buf, dataRc, &fileInfo{isTextFile, false, blob.Size(), nil, st}, nil
 	}
 
