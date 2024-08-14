b53fd2fb12498cb3059d26db1d7291a4437b0958
sillyguodong <gedong_1994@163.com>
2024-08-14 18:02:06 +0800

    chore: delete useless comments

--- web_src/js/components/ScopedAccessTokenSelector.vue
+++ web_src/js/components/ScopedAccessTokenSelector.vue
@@ -72,18 +72,6 @@ const sfc = {
 };
 
 export default sfc;
-
-/**
- * Initialize category toggle sections
- */
-// export function initScopedAccessTokenCategories() {
-//   for (const el of document.querySelectorAll('.scoped-access-token-mount')) {
-//     createApp({})
-//       .component('scoped-access-token-selector', sfc)
-//       .mount(el);
-//   }
-// }
-
 </script>
 <template>
   <div v-for="category in categories" :key="category" class="field tw-pl-1 tw-pb-1 access-token-category">
