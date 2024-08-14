ba52d01b3458d8b5c5aa4d3f14928fed9ec5eb09
sillyguodong <gedong_1994@163.com>
2024-08-14 18:00:55 +0800

    fix: do not import createApp in sfc file

--- web_src/js/components/ScopedAccessTokenSelector.vue
+++ web_src/js/components/ScopedAccessTokenSelector.vue
@@ -1,5 +1,4 @@
 <script lang="ts">
-import {createApp} from 'vue';
 import {hideElem, showElem} from '../utils/dom.ts';
 
 const sfc = {
@@ -77,13 +76,13 @@ export default sfc;
 /**
  * Initialize category toggle sections
  */
-export function initScopedAccessTokenCategories() {
-  for (const el of document.querySelectorAll('.scoped-access-token-mount')) {
-    createApp({})
-      .component('scoped-access-token-selector', sfc)
-      .mount(el);
-  }
-}
+// export function initScopedAccessTokenCategories() {
+//   for (const el of document.querySelectorAll('.scoped-access-token-mount')) {
+//     createApp({})
+//       .component('scoped-access-token-selector', sfc)
+//       .mount(el);
+//   }
+// }
 
 </script>
 <template>
