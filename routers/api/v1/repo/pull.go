5761e338be83f3c9c4557290113ee0e28b9a1113
Job <LordChunk@users.noreply.github.com>
2024-08-18 12:57:12 +0000

    Fix PR creation on forked repositories

--- routers/api/v1/repo/pull.go
+++ routers/api/v1/repo/pull.go
@@ -1124,9 +1124,19 @@ func parseCompareInfo(ctx *context.APIContext, form api.CreatePullRequestOption)
 	// Check if current user has fork of repository or in the same repository.
 	headRepo := repo_model.GetForkedRepo(ctx, headUser.ID, baseRepo.ID)
 	if headRepo == nil && !isSameRepo {
-		log.Trace("parseCompareInfo[%d]: does not have fork or in same repository", baseRepo.ID)
-		ctx.NotFound("GetForkedRepo")
-		return nil, nil, nil, "", ""
+		// Check if the base repository is a fork of the head repository.
+		headRepo, err = repo_model.GetRepositoryByID(ctx, baseRepo.ForkID)
+
+		if err != nil {
+			ctx.Error(http.StatusInternalServerError, "GetRepositoryByID", err)
+			return nil, nil, nil, "", ""
+		}
+
+		if headRepo == nil {
+			log.Trace("parseCompareInfo[%d]: does not have fork or in same repository", baseRepo.ID)
+			ctx.NotFound("GetForkedRepo")
+			return nil, nil, nil, "", ""
+		}
 	}
 
 	var headGitRepo *git.Repository
