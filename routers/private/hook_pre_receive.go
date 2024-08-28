c49fcd8275eee04f81e187e2749bd230b63ab94f
Lunny Xiao <xiaolunwen@gmail.com>
2024-08-27 23:51:31 -0700

    Add comments for the operation

--- routers/private/hook_pre_receive.go
+++ routers/private/hook_pre_receive.go
@@ -125,6 +125,8 @@ func HookPreReceive(ctx *gitea_context.PrivateContext) {
 		case git.DefaultFeatures().SupportProcReceive && refFullName.IsFor():
 			preReceiveFor(ourCtx, refFullName)
 		case refFullName.IsPull():
+			// update/delete refs/pull/123/head are disallowed from push, the refs
+			// are managed by Gitea from internal operations
 			ctx.JSON(http.StatusForbidden, private.Response{
 				UserMsg: "Can't update pull request manually.",
 			})
