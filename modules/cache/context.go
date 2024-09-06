62adfef61287172c3024cfa5419214277bc9db73
Lunny Xiao <xiaolunwen@gmail.com>
2024-09-06 13:03:55 -0700

    Remove expire from context cache

--- modules/cache/context.go
+++ modules/cache/context.go
@@ -6,9 +6,6 @@ package cache
 import (
 	"context"
 	"sync"
-	"time"
-
-	"code.gitea.io/gitea/modules/log"
 )
 
 // cacheContext is a context that can be used to cache data in a request level context
@@ -17,7 +14,6 @@ import (
 type cacheContext struct {
 	data    map[any]map[any]any
 	lock    sync.RWMutex
-	created time.Time
 	discard bool
 }
 
@@ -62,17 +58,6 @@ func (cc *cacheContext) isDiscard() bool {
 	return cc.discard
 }
 
-// cacheContextLifetime is the max lifetime of cacheContext.
-// Since cacheContext is used to cache data in a request level context, 10s is enough.
-// If a cacheContext is used more than 10s, it's probably misuse.
-const cacheContextLifetime = 10 * time.Second
-
-var timeNow = time.Now
-
-func (cc *cacheContext) Expired() bool {
-	return timeNow().Sub(cc.created) > cacheContextLifetime
-}
-
 var cacheContextKey = struct{}{}
 
 /*
@@ -110,8 +95,7 @@ func WithCacheContext(ctx context.Context) context.Context {
 		}
 	}
 	return context.WithValue(ctx, cacheContextKey, &cacheContext{
-		data:    make(map[any]map[any]any),
-		created: timeNow(),
+		data: make(map[any]map[any]any),
 	})
 }
 
@@ -128,12 +112,6 @@ func WithNoCacheContext(ctx context.Context) context.Context {
 
 func GetContextData(ctx context.Context, tp, key any) any {
 	if c, ok := ctx.Value(cacheContextKey).(*cacheContext); ok {
-		if c.Expired() {
-			// The warning means that the cache context is misused for long-life task,
-			// it can be resolved with WithNoCacheContext(ctx).
-			log.Warn("cache context is expired, may be misused for long-life tasks: %v", c)
-			return nil
-		}
 		return c.Get(tp, key)
 	}
 	return nil
@@ -141,12 +119,6 @@ func GetContextData(ctx context.Context, tp, key any) any {
 
 func SetContextData(ctx context.Context, tp, key, value any) {
 	if c, ok := ctx.Value(cacheContextKey).(*cacheContext); ok {
-		if c.Expired() {
-			// The warning means that the cache context is misused for long-life task,
-			// it can be resolved with WithNoCacheContext(ctx).
-			log.Warn("cache context is expired, may be misused for long-life tasks: %v", c)
-			return
-		}
 		c.Put(tp, key, value)
 		return
 	}
@@ -154,12 +126,6 @@ func SetContextData(ctx context.Context, tp, key, value any) {
 
 func RemoveContextData(ctx context.Context, tp, key any) {
 	if c, ok := ctx.Value(cacheContextKey).(*cacheContext); ok {
-		if c.Expired() {
-			// The warning means that the cache context is misused for long-life task,
-			// it can be resolved with WithNoCacheContext(ctx).
-			log.Warn("cache context is expired, may be misused for long-life tasks: %v", c)
-			return
-		}
 		c.Delete(tp, key)
 	}
 }
