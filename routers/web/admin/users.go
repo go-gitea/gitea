28d5d519ba8585532863a4a722621eb10c914ac5
Lunny Xiao <xiaolunwen@gmail.com>
2024-10-12 08:41:55 -0700

    check rename condition in the service method instead of outside

--- routers/web/admin/users.go
+++ routers/web/admin/users.go
@@ -347,7 +347,7 @@ func EditUserPost(ctx *context.Context) {
 		return
 	}
 
-	if form.UserName != "" && form.UserName != u.Name {
+	if form.UserName != "" {
 		if err := user_service.RenameUser(ctx, u, form.UserName); err != nil {
 			switch {
 			case user_model.IsErrUserIsNotLocal(err):
