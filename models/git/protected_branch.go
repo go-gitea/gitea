ccb2e12189e2d234e5de5184164dc42360c1186c
6543 <6543@obermui.de>
2024-10-16 23:07:58 +0200

    optimize loadGlob for protectedBranch

--- models/git/protected_branch.go
+++ models/git/protected_branch.go
@@ -83,14 +83,18 @@ func IsRuleNameSpecial(ruleName string) bool {
 }
 
 func (protectBranch *ProtectedBranch) loadGlob() {
-	if protectBranch.globRule == nil {
+	if protectBranch.globRule == nil && !protectBranch.isPlainName {
+		// detect if it is not glob
+		if !IsRuleNameSpecial(protectBranch.RuleName) {
+			protectBranch.isPlainName = true
+			return
+		}
 		var err error
 		protectBranch.globRule, err = glob.Compile(protectBranch.RuleName, '/')
 		if err != nil {
 			log.Warn("Invalid glob rule for ProtectedBranch[%d]: %s %v", protectBranch.ID, protectBranch.RuleName, err)
 			protectBranch.globRule = glob.MustCompile(glob.QuoteMeta(protectBranch.RuleName), '/')
 		}
-		protectBranch.isPlainName = !IsRuleNameSpecial(protectBranch.RuleName)
 	}
 }
 
