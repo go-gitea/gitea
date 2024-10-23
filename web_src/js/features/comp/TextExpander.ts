e943ddca72ed267a8ea86239ce03780d7d6e0f5a
Anbraten <anton@ju60.de>
2024-10-23 14:24:03 +0200

    Autocomplete issues or pull-request ids

--- web_src/js/features/comp/TextExpander.ts
+++ web_src/js/features/comp/TextExpander.ts
@@ -1,8 +1,8 @@
-import {matchEmoji, matchMention} from '../../utils/match.ts';
+import {matchEmoji, matchMention, matchIssueOrPullRequest} from '../../utils/match.ts';
 import {emojiString} from '../emoji.ts';
 
 export function initTextExpander(expander) {
-  expander?.addEventListener('text-expander-change', ({detail: {key, provide, text}}) => {
+  expander?.addEventListener('text-expander-change', async ({detail: {key, provide, text}}) => {
     if (key === ':') {
       const matches = matchEmoji(text);
       if (!matches.length) return provide({matched: false});
@@ -49,12 +49,36 @@ export function initTextExpander(expander) {
       }
 
       provide({matched: true, fragment: ul});
+    } else if (key === '#') {
+      const url = window.location.href;
+      const matches = matchIssueOrPullRequest(url, text);
+      if (!matches.length) return provide({matched: false});
+
+      const ul = document.createElement('ul');
+      ul.classList.add('suggestions');
+      for (const {value, name, type} of matches) {
+        const li = document.createElement('li');
+        li.setAttribute('role', 'option');
+        li.setAttribute('data-value', `${key}${value}`);
+
+        const icon = document.createElement('span');
+        icon.classList.add('icon', type === 'issue' ? 'issue' : 'pull-request');
+        li.append(icon);
+
+        const nameSpan = document.createElement('span');
+        nameSpan.textContent = name;
+        li.append(nameSpan);
+
+        ul.append(li);
+      }
+
+      provide({matched: true, fragment: ul});
     }
   });
   expander?.addEventListener('text-expander-value', ({detail}) => {
     if (detail?.item) {
-      // add a space after @mentions as it's likely the user wants one
-      const suffix = detail.key === '@' ? ' ' : '';
+      // add a space after @mentions and #issue as it's likely the user wants one
+      const suffix = ['@', '#'].includes(detail.key) ? ' ' : '';
       detail.value = `${detail.item.getAttribute('data-value')}${suffix}`;
     }
   });
--- web_src/js/utils/match.ts
+++ web_src/js/utils/match.ts
@@ -1,8 +1,9 @@
 import emojis from '../../../assets/emoji.json';
+import { request } from '../modules/fetch.ts';
 
 const maxMatches = 6;
 
-function sortAndReduce(map: Map<string, number>) {
+function sortAndReduce<T>(map: Map<T, number>): T[] {
   const sortedMap = new Map(Array.from(map.entries()).sort((a, b) => a[1] - b[1]));
   return Array.from(sortedMap.keys()).slice(0, maxMatches);
 }
@@ -27,16 +28,89 @@ export function matchEmoji(queryText: string): string[] {
   return sortAndReduce(results);
 }
 
-export function matchMention(queryText: string): string[] {
+type Mention = {value: string; name: string; fullname: string; avatar: string};
+export function matchMention(queryText: string): Mention[] {
   const query = queryText.toLowerCase();
 
   // results is a map of weights, lower is better
-  const results = new Map();
+  const results = new Map<Mention, number>();
   for (const obj of window.config.mentionValues ?? []) {
     const index = obj.key.toLowerCase().indexOf(query);
     if (index === -1) continue;
     const existing = results.get(obj);
     results.set(obj, existing ? existing - index : index);
+  }
+
+  return sortAndReduce(results);
+}
+
+type IssueOrPullRequest = {value: string; name: string; type: 'issue' | 'pull-request'};
+export async function matchIssueOrPullRequest(url: string, queryText: string): IssueOrPullRequest[] {
+  const query = queryText.toLowerCase();
+
+  const repository = url.split('/').slice(-2).join('/');
+  const issuePullRequestId = url.split('/').slice(-1)[0];
+
+  console.log('suggestions for', {
+    repository,
+    query,
+  });
+
+  // TODO: fetch data from api
+  // const res = await request('/-/suggestions', {
+  //   method: 'GET',
+  //   data: {
+  //     repository,
+  //     query,
+  //   },
+  // });
+  // console.log(await res.json());
+
+  // results is a map of weights, lower is better
+  const results = new Map<IssueOrPullRequest, number>();
+  // for (const obj of window.config.mentionValues ?? []) {
+  //   const index = obj.key.toLowerCase().indexOf(query);
+  //   if (index === -1) continue;
+  //   const existing = results.get(obj);
+  //   results.set(obj, existing ? existing - index : index);
+  // }
+
+  results.set({
+    value: '28958',
+    name: 'Live removal of issue comments using htmx websocket',
+    type: 'pull-request',
+  }, 0);
+
+  results.set({
+    value: '32234',
+    name: 'Calculate `PublicOnly` for org membership only once',
+    type: 'pull-request',
+  }, 1);
+
+  results.set({
+    value: '32280',
+    name: 'Optimize branch protection rule loading',
+    type: 'pull-request',
+  }, 2);
+
+  results.set({
+    value: '32326',
+    name: 'Shallow Mirroring',
+    type: 'issue',
+  }, 3);
+
+  results.set({
+    value: '32248',
+    name: 'Make admins adhere to branch protection rules',
+    type: 'pull-request',
+  }, 4);
+
+  // filter out current issue/pull request
+  for (const [key] of results.entries()) {
+    if (key.value === issuePullRequestId) {
+      results.delete(key);
+      break;
+    }
   }
 
   return sortAndReduce(results);
