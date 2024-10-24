c72d24a757721f1806689a566fea1faeaf75d1a5
Lunny Xiao <xiaolunwen@gmail.com>
2024-10-23 22:14:26 -0700

    Add new index for action to resolve the performance problem

--- models/migrations/migrations.go
+++ models/migrations/migrations.go
@@ -605,6 +605,8 @@ var migrations = []Migration{
 	NewMigration("Add Repository Licenses", v1_23.AddRepositoryLicenses),
 	// v306 -> v307
 	NewMigration("Add BlockAdminMergeOverride to ProtectedBranch", v1_23.AddBlockAdminMergeOverrideBranchProtection),
+	// v307 -> v308
+	NewMigration("Add index(user_id, is_deleted) for action table", v1_23.AddNewIndexForUserDashboard),
 }
 
 // GetCurrentDBVersion returns the current db version
