3fad332a6a0f7c778f9c34093a67ac1a331d749c
wxiaoguang <wxiaoguang@gmail.com>
2024-10-31 02:02:45 +0800

    fix

--- web_src/js/webcomponents/absolute-date.ts
+++ web_src/js/webcomponents/absolute-date.ts
@@ -1,5 +1,9 @@
-export function toAbsoluteLocaleDate(date: string, lang: string, opts: Intl.DateTimeFormatOptions) {
-  return new Date(date).toLocaleString(lang || [], opts);
+export function toAbsoluteLocaleDate(date: string, lang?: string, opts?: Intl.DateTimeFormatOptions) {
+  // only use the date part, it is guaranteed to be in ISO format (YYYY-MM-DDTHH:mm:ss.sssZ) or (YYYY-MM-DD)
+  // if there is an "Invalid Date" error, there must be something wrong in code and should be fixed.
+  const dateSep = date.indexOf('T');
+  date = dateSep === -1 ? date : date.substring(0, dateSep);
+  return new Date(`${date}T00:00:00`).toLocaleString(lang || [], opts);
 }
 
 window.customElements.define('absolute-date', class extends HTMLElement {
@@ -15,14 +19,8 @@ window.customElements.define('absolute-date', class extends HTMLElement {
     const lang = this.closest('[lang]')?.getAttribute('lang') ||
       this.ownerDocument.documentElement.getAttribute('lang') || '';
 
-    // only use the date part, it is guaranteed to be in ISO format (YYYY-MM-DDTHH:mm:ss.sssZ)
-    let date = this.getAttribute('date');
-    let dateSep = date.indexOf('T');
-    dateSep = dateSep === -1 ? date.indexOf(' ') : dateSep;
-    date = dateSep === -1 ? date : date.substring(0, dateSep);
-
     if (!this.shadowRoot) this.attachShadow({mode: 'open'});
-    this.shadowRoot.textContent = toAbsoluteLocaleDate(date, lang, opt);
+    this.shadowRoot.textContent = toAbsoluteLocaleDate(this.getAttribute('date'), lang, opt);
   };
 
   attributeChangedCallback(_name, oldValue, newValue) {
