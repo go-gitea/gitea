259811617ba15c77ddd89360178a59251d611af2
wxiaoguang <wxiaoguang@gmail.com>
2024-11-03 05:04:53 +0800

    Replace DateTime with proper functions (#32402)
    Follow #32383
    
    This PR cleans up the "Deadline" usages in templates, make them call
    `ParseLegacy` first to get a `Time` struct then display by `DateUtils`.
    
    Now it should be pretty clear how "deadline string" works, it makes it
    possible to do further refactoring and correcting.

--- modules/templates/util_date.go
+++ modules/templates/util_date.go
@@ -6,7 +6,9 @@ package templates
 import (
 	"context"
 	"html/template"
+	"time"
 
+	"code.gitea.io/gitea/modules/setting"
 	"code.gitea.io/gitea/modules/timeutil"
 )
 
@@ -31,4 +33,28 @@ func (du *DateUtils) AbsoluteLong(time any) template.HTML {
 // FullTime renders in "Jan 01, 2006 20:33:44" format
 func (du *DateUtils) FullTime(time any) template.HTML {
 	return timeutil.DateTime("full", time)
+}
+
+// ParseLegacy parses the datetime in legacy format, eg: "2016-01-02" in server's timezone.
+// It shouldn't be used in new code. New code should use Time or TimeStamp as much as possible.
+func (du *DateUtils) ParseLegacy(datetime string) time.Time {
+	return parseLegacy(datetime)
+}
+
+func parseLegacy(datetime string) time.Time {
+	t, err := time.Parse(time.RFC3339, datetime)
+	if err != nil {
+		t, _ = time.ParseInLocation(time.DateOnly, datetime, setting.DefaultUILocation)
+	}
+	return t
+}
+
+func dateTimeLegacy(format string, datetime any, _ ...string) template.HTML {
+	if !setting.IsProd || setting.IsInTesting {
+		panic("dateTimeLegacy is for backward compatibility only, do not use it in new code")
+	}
+	if s, ok := datetime.(string); ok {
+		datetime = parseLegacy(s)
+	}
+	return timeutil.DateTime(format, datetime)
 }
