13e8d8488d7a61318460e9edcbcf96c62229396d
RedCocoon <zzsmd0000@gmail.com>
2024-11-04 03:57:01 +0800

    Undo adding unnecessary ctx Data

--- routers/web/repo/pull_review_test.go
+++ routers/web/repo/pull_review_test.go
@@ -56,25 +56,21 @@ func TestRenderConversation(t *testing.T) {
 	}
 	run("diff with outdated", func(t *testing.T, ctx *context.Context, resp *httptest.ResponseRecorder) {
 		ctx.Data["ShowOutdatedComments"] = true
-		ctx.Data["IsIssue"] = true
 		renderConversation(ctx, preparedComment, "diff")
 		assert.Contains(t, resp.Body.String(), `<div class="content comment-container"`)
 	})
 	run("diff without outdated", func(t *testing.T, ctx *context.Context, resp *httptest.ResponseRecorder) {
 		ctx.Data["ShowOutdatedComments"] = false
-		ctx.Data["IsIssue"] = true
 		renderConversation(ctx, preparedComment, "diff")
 		assert.Contains(t, resp.Body.String(), `conversation-not-existing`)
 	})
 	run("timeline with outdated", func(t *testing.T, ctx *context.Context, resp *httptest.ResponseRecorder) {
 		ctx.Data["ShowOutdatedComments"] = true
-		ctx.Data["IsIssue"] = true
 		renderConversation(ctx, preparedComment, "timeline")
 		assert.Contains(t, resp.Body.String(), `<div id="code-comments-`)
 	})
 	run("timeline is not affected by ShowOutdatedComments=false", func(t *testing.T, ctx *context.Context, resp *httptest.ResponseRecorder) {
 		ctx.Data["ShowOutdatedComments"] = false
-		ctx.Data["IsIssue"] = true
 		renderConversation(ctx, preparedComment, "timeline")
 		assert.Contains(t, resp.Body.String(), `<div id="code-comments-`)
 	})
@@ -82,7 +78,6 @@ func TestRenderConversation(t *testing.T) {
 		err := db.TruncateBeans(db.DefaultContext, &issues_model.Review{})
 		assert.NoError(t, err)
 		ctx.Data["ShowOutdatedComments"] = true
-		ctx.Data["IsIssue"] = true
 		renderConversation(ctx, preparedComment, "diff")
 		assert.Equal(t, http.StatusOK, resp.Code)
 		assert.NotContains(t, resp.Body.String(), `status-page-500`)
@@ -91,7 +86,6 @@ func TestRenderConversation(t *testing.T) {
 		err := db.TruncateBeans(db.DefaultContext, &issues_model.Review{})
 		assert.NoError(t, err)
 		ctx.Data["ShowOutdatedComments"] = true
-		ctx.Data["IsIssue"] = true
 		renderConversation(ctx, preparedComment, "timeline")
 		assert.Equal(t, http.StatusOK, resp.Code)
 		assert.NotContains(t, resp.Body.String(), `status-page-500`)
