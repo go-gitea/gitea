ea7cb33693df731a3a79dcb2d801611fdfc38bb2
Lunny Xiao <xiaolunwen@gmail.com>
2024-11-04 23:58:35 -0800

    Fix test

--- modules/repository/main_test.go
+++ modules/repository/main_test.go
@@ -6,9 +6,10 @@ package repository
 import (
 	"testing"
 
+	"code.gitea.io/gitea/models/unittest"
+
 	_ "code.gitea.io/gitea/models"
 	_ "code.gitea.io/gitea/models/actions"
-	"code.gitea.io/gitea/models/unittest"
 )
 
 func TestMain(m *testing.M) {
