7f55aadd01f71d2a57555227a8946c5c3711a711
Lunny Xiao <xiaolunwen@gmail.com>
2024-11-06 19:28:11 -0800

    Move AddCollabrator and CreateRepositoryByExample to service layer (#32419)
    - [x] Move `CreateRepositoryByExample` to service layer
    - [x] Move `AddCollabrator` to service layer
    - [x] Add a new parameter for `AddCollabrator` so that changing mode
    immediately after that will become unnecessary.

--- services/repository/collaboration_test.go
+++ services/repository/collaboration_test.go
@@ -7,12 +7,28 @@ import (
 	"testing"
 
 	"code.gitea.io/gitea/models/db"
+	"code.gitea.io/gitea/models/perm"
 	repo_model "code.gitea.io/gitea/models/repo"
 	"code.gitea.io/gitea/models/unittest"
 	user_model "code.gitea.io/gitea/models/user"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestRepository_AddCollaborator(t *testing.T) {
+	assert.NoError(t, unittest.PrepareTestDatabase())
+
+	testSuccess := func(repoID, userID int64) {
+		repo := unittest.AssertExistsAndLoadBean(t, &repo_model.Repository{ID: repoID})
+		assert.NoError(t, repo.LoadOwner(db.DefaultContext))
+		user := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: userID})
+		assert.NoError(t, AddOrUpdateCollaborator(db.DefaultContext, repo, user, perm.AccessModeWrite))
+		unittest.CheckConsistencyFor(t, &repo_model.Repository{ID: repoID}, &user_model.User{ID: userID})
+	}
+	testSuccess(1, 4)
+	testSuccess(1, 4)
+	testSuccess(3, 4)
+}
 
 func TestRepository_DeleteCollaboration(t *testing.T) {
 	assert.NoError(t, unittest.PrepareTestDatabase())
