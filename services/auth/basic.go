ad789d8c92466323ca93f1820ed71a67834b3201
Lunny Xiao <xiaolunwen@gmail.com>
2024-11-15 22:05:55 -0800

    Fix basic auth with webauthn

--- services/auth/basic.go
+++ services/auth/basic.go
@@ -5,6 +5,7 @@
 package auth
 
 import (
+	"errors"
 	"net/http"
 	"strings"
 
@@ -138,6 +139,15 @@ func (b *Basic) Verify(req *http.Request, w http.ResponseWriter, store DataStore
 			log.Error("UserSignIn: %v", err)
 		}
 		return nil, err
+	}
+
+	// Check if the user has webAuthn registration
+	hasWebAuthn, err := auth_model.HasWebAuthnRegistrationsByUID(req.Context(), u.ID)
+	if err != nil {
+		return nil, err
+	}
+	if hasWebAuthn {
+		return nil, errors.New("Basic authorization is not allowed while webAuthn enrolled")
 	}
 
 	if skipper, ok := source.Cfg.(LocalTwoFASkipper); !ok || !skipper.IsSkipLocalTwoFA() {
