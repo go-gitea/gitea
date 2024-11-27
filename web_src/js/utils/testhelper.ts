0349b88af7b5db873ae40f76e0424054cdd7a62c
wxiaoguang <wxiaoguang@gmail.com>
2024-11-27 18:12:13 +0800

    Update web_src/js/utils/testhelper.ts

--- web_src/js/utils/testhelper.ts
+++ web_src/js/utils/testhelper.ts
@@ -2,5 +2,5 @@
 // even if backend is in testing mode, frontend could be complied in production mode
 // so this function only checks if the frontend is in unit testing mode (usually from *.test.ts files)
 export function isInFrontendUnitTest() {
-  return process.env.NODE_ENV === 'test';
+  return process.env.TEST === 'true';
 }
