335cd3593b89f28e790d536e2dcacccedc8bd334
wxiaoguang <wxiaoguang@gmail.com>
2024-12-12 15:52:41 +0800

    do not set alt to avoid break page layout

--- web_src/js/components/RepoContributors.vue
+++ web_src/js/components/RepoContributors.vue
@@ -396,7 +396,7 @@ export default {
         <div class="ui top attached header tw-flex tw-flex-1">
           <b class="ui right">#{{ index + 1 }}</b>
           <a :href="contributor.home_link">
-            <img class="ui avatar tw-align-middle" height="40" width="40" :src="contributor.avatar_link" alt="{{ contributor.name }}">
+            <img class="ui avatar tw-align-middle" height="40" width="40" :src="contributor.avatar_link" alt="">
           </a>
           <div class="tw-ml-2">
             <a v-if="contributor.home_link !== ''" :href="contributor.home_link"><h4>{{ contributor.name }}</h4></a>
