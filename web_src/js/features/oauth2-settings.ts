3b7f4ed8e1302e89dd06fd1e510445dc036720bd
silverwind <me@silverwind.io>
2024-12-14 09:27:17 +0100

    rename var

--- web_src/js/features/oauth2-settings.ts
+++ web_src/js/features/oauth2-settings.ts
@@ -1,6 +1,6 @@
 export function initOAuth2SettingsDisableCheckbox() {
-  for (const e of document.querySelectorAll('.disable-setting')) {
-    e.addEventListener('change', (e: Event & {target: HTMLInputElement}) => {
+  for (const el of document.querySelectorAll('.disable-setting')) {
+    el.addEventListener('change', (e: Event & {target: HTMLInputElement}) => {
       document.querySelector(e.target.getAttribute('data-target')).classList.toggle('disabled', e.target.checked);
     });
   }
