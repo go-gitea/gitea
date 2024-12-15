8dfb7c3588addf33b1380ed9abc5c32bc9ef244e
silverwind <me@silverwind.io>
2024-12-15 06:58:28 +0100

    use repo.release.latest, restore label change

--- tests/integration/release_test.go
+++ tests/integration/release_test.go
@@ -86,7 +86,7 @@ func TestCreateRelease(t *testing.T) {
 	session := loginUser(t, "user2")
 	createNewRelease(t, session, "/user2/repo1", "v0.0.1", "v0.0.1", false, false)
 
-	checkLatestReleaseAndCount(t, session, "/user2/repo1", "v0.0.1", translation.NewLocale("en-US").TrString("latest"), 4)
+	checkLatestReleaseAndCount(t, session, "/user2/repo1", "v0.0.1", translation.NewLocale("en-US").TrString("repo.release.latest"), 4)
 }
 
 func TestCreateReleasePreRelease(t *testing.T) {
@@ -128,7 +128,7 @@ func TestCreateReleasePaging(t *testing.T) {
 
 	// Check that user4 does not see draft and still see 10 latest releases
 	session2 := loginUser(t, "user4")
-	checkLatestReleaseAndCount(t, session2, "/user2/repo1", "v0.0.11", translation.NewLocale("en-US").TrString("latest"), 10)
+	checkLatestReleaseAndCount(t, session2, "/user2/repo1", "v0.0.11", translation.NewLocale("en-US").TrString("repo.release.latest"), 10)
 }
 
 func TestViewReleaseListNoLogin(t *testing.T) {
