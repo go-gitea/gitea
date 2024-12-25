9181fd27b0393f32f5d24e2a777a14707c67cc56
Lunny Xiao <xiaolunwen@gmail.com>
2024-12-24 21:12:10 -0800

    Fix test

--- services/issue/milestone_test.go
+++ services/issue/milestone_test.go
@@ -34,6 +34,7 @@ func TestChangeMilestoneAssign(t *testing.T) {
 	unittest.CheckConsistencyFor(t, &issues_model.Milestone{}, &issues_model.Issue{})
 	assert.NotNil(t, issue.Milestone)
 
+	oldMilestoneID = issue.MilestoneID
 	issue.MilestoneID = 0
 	assert.NoError(t, ChangeMilestoneAssign(db.DefaultContext, issue, doer, oldMilestoneID))
 	assert.EqualValues(t, 0, issue.MilestoneID)
