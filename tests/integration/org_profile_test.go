873dc8fbf11d61a8c7981ee66f24f0e8ee6a7209
wxiaoguang <wxiaoguang@gmail.com>
2024-12-30 23:20:17 +0800

    improve tests

--- tests/integration/org_profile_test.go
+++ tests/integration/org_profile_test.go
@@ -13,6 +13,7 @@ import (
 
 	auth_model "code.gitea.io/gitea/models/auth"
 	api "code.gitea.io/gitea/modules/structs"
+	"code.gitea.io/gitea/modules/util"
 	"code.gitea.io/gitea/routers/web/shared/user"
 
 	"github.com/stretchr/testify/assert"
@@ -56,98 +57,76 @@ func TestOrgProfile(t *testing.T) {
 }
 
 func testOrgProfile(t *testing.T, u *url.URL) {
-	// selectors: "[data-profile-view-as-member=true/false]", "#org-home-view-as-dropdown" (indicate whether the view as dropdown menu is present)
+	const contentPublicReadme = "Public Readme Content"
+	const contentPrivateReadme = "Private Readme Content"
+	// HTML: "#org-home-view-as-dropdown" (indicate whether the view as dropdown menu is present)
 
 	// PART 1: Test Both Private and Public
-	createTestProfile(t, "org3", user.RepoNameProfile, "Public Readme")
-	createTestProfile(t, "org3", user.RepoNameProfilePrivate, "Private Readme")
+	createTestProfile(t, "org3", user.RepoNameProfile, contentPublicReadme)
+	createTestProfile(t, "org3", user.RepoNameProfilePrivate, contentPrivateReadme)
 
 	// Anonymous User
 	req := NewRequest(t, "GET", "org3")
 	resp := MakeRequest(t, req, http.StatusOK)
-	htmlDoc := NewHTMLParser(t, resp.Body)
-
-	profileDivs := htmlDoc.doc.Find("[data-profile-view-as-member=false]")
-	assert.EqualValues(t, 1, profileDivs.Length())
-
-	dropDownDiv := htmlDoc.doc.Find("#org-home-view-as-dropdown")
-	assert.EqualValues(t, 0, dropDownDiv.Length())
+	bodyString := util.UnsafeBytesToString(resp.Body.Bytes())
+	assert.Contains(t, bodyString, contentPublicReadme)
+	assert.NotContains(t, bodyString, `id="org-home-view-as-dropdown"`)
 
 	// Logged in but not member
 	session := loginUser(t, "user24")
 	req = NewRequest(t, "GET", "org3")
 	resp = session.MakeRequest(t, req, http.StatusOK)
-	htmlDoc = NewHTMLParser(t, resp.Body)
-
-	profileDivs = htmlDoc.doc.Find("[data-profile-view-as-member=false]")
-	assert.EqualValues(t, 1, profileDivs.Length())
-
-	dropDownDiv = htmlDoc.doc.Find("#org-home-view-as-dropdown")
-	assert.EqualValues(t, 0, dropDownDiv.Length())
+	bodyString = util.UnsafeBytesToString(resp.Body.Bytes())
+	assert.Contains(t, bodyString, contentPublicReadme)
+	assert.NotContains(t, bodyString, `id="org-home-view-as-dropdown"`)
 
 	// Site Admin
 	session = loginUser(t, "user1")
 	req = NewRequest(t, "GET", "org3")
 	resp = session.MakeRequest(t, req, http.StatusOK)
-	htmlDoc = NewHTMLParser(t, resp.Body)
-
-	profileDivs = htmlDoc.doc.Find("[data-profile-view-as-member=false]")
-	assert.EqualValues(t, 1, profileDivs.Length())
-
-	dropDownDiv = htmlDoc.doc.Find("#org-home-view-as-dropdown")
-	assert.EqualValues(t, 1, dropDownDiv.Length())
+	bodyString = util.UnsafeBytesToString(resp.Body.Bytes())
+	assert.Contains(t, bodyString, contentPublicReadme)
+	assert.Contains(t, bodyString, `id="org-home-view-as-dropdown"`)
 
 	req = NewRequest(t, "GET", "/org3?view_as=member")
 	resp = session.MakeRequest(t, req, http.StatusOK)
-	htmlDoc = NewHTMLParser(t, resp.Body)
-
-	profileDivs = htmlDoc.doc.Find("[data-profile-view-as-member=true]")
-	assert.EqualValues(t, 1, profileDivs.Length())
+	bodyString = util.UnsafeBytesToString(resp.Body.Bytes())
+	assert.Contains(t, bodyString, contentPrivateReadme)
+	assert.Contains(t, bodyString, `id="org-home-view-as-dropdown"`)
 
 	req = NewRequest(t, "GET", "/org3?view_as=public")
 	resp = session.MakeRequest(t, req, http.StatusOK)
-	htmlDoc = NewHTMLParser(t, resp.Body)
-
-	profileDivs = htmlDoc.doc.Find("[data-profile-view-as-member=false]")
-	assert.EqualValues(t, 1, profileDivs.Length())
+	bodyString = util.UnsafeBytesToString(resp.Body.Bytes())
+	assert.Contains(t, bodyString, contentPublicReadme)
+	assert.Contains(t, bodyString, `id="org-home-view-as-dropdown"`)
 
 	// PART 2: Each org has either one of private pr public profile
-	createTestProfile(t, "org41", user.RepoNameProfile, "Public Readme")
-	createTestProfile(t, "org42", user.RepoNameProfilePrivate, "Private Readme")
+	createTestProfile(t, "org41", user.RepoNameProfile, contentPublicReadme)
+	createTestProfile(t, "org42", user.RepoNameProfilePrivate, contentPrivateReadme)
 
 	// Anonymous User
 	req = NewRequest(t, "GET", "/org41")
 	resp = MakeRequest(t, req, http.StatusOK)
-	htmlDoc = NewHTMLParser(t, resp.Body)
-	profileDivs = htmlDoc.doc.Find("[data-profile-view-as-member=false]")
-	assert.EqualValues(t, 1, profileDivs.Length())
-	dropDownDiv = htmlDoc.doc.Find("#org-home-view-as-dropdown")
-	assert.EqualValues(t, 0, dropDownDiv.Length())
+	bodyString = util.UnsafeBytesToString(resp.Body.Bytes())
+	assert.Contains(t, bodyString, contentPublicReadme)
+	assert.NotContains(t, bodyString, `id="org-home-view-as-dropdown"`)
 
 	req = NewRequest(t, "GET", "/org42")
 	resp = MakeRequest(t, req, http.StatusOK)
-	htmlDoc = NewHTMLParser(t, resp.Body)
-	profileDivs = htmlDoc.doc.Find("[data-profile-view-as-member=false]")
-	assert.EqualValues(t, 0, profileDivs.Length())
-	profileDivs = htmlDoc.doc.Find("[data-profile-view-as-member=false]")
-	assert.EqualValues(t, 0, profileDivs.Length())
-	dropDownDiv = htmlDoc.doc.Find("#org-home-view-as-dropdown")
-	assert.EqualValues(t, 0, dropDownDiv.Length())
+	bodyString = util.UnsafeBytesToString(resp.Body.Bytes())
+	assert.NotContains(t, bodyString, contentPrivateReadme)
+	assert.NotContains(t, bodyString, `id="org-home-view-as-dropdown"`)
 
 	// Site Admin
 	req = NewRequest(t, "GET", "/org41")
 	resp = session.MakeRequest(t, req, http.StatusOK)
-	htmlDoc = NewHTMLParser(t, resp.Body)
-	profileDivs = htmlDoc.doc.Find("[data-profile-view-as-member=false]")
-	assert.EqualValues(t, 1, profileDivs.Length())
-	dropDownDiv = htmlDoc.doc.Find("#org-home-view-as-dropdown")
-	assert.EqualValues(t, 0, dropDownDiv.Length())
+	bodyString = util.UnsafeBytesToString(resp.Body.Bytes())
+	assert.Contains(t, bodyString, contentPublicReadme)
+	assert.NotContains(t, bodyString, `id="org-home-view-as-dropdown"`)
 
 	req = NewRequest(t, "GET", "/org42")
 	resp = session.MakeRequest(t, req, http.StatusOK)
-	htmlDoc = NewHTMLParser(t, resp.Body)
-	profileDivs = htmlDoc.doc.Find("[data-profile-view-as-member=true]")
-	assert.EqualValues(t, 1, profileDivs.Length())
-	dropDownDiv = htmlDoc.doc.Find("#org-home-view-as-dropdown")
-	assert.EqualValues(t, 0, dropDownDiv.Length())
+	bodyString = util.UnsafeBytesToString(resp.Body.Bytes())
+	assert.Contains(t, bodyString, contentPrivateReadme)
+	assert.NotContains(t, bodyString, `id="org-home-view-as-dropdown"`)
 }
