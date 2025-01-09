23023ca09d8f0db9f999e95e776144feb8daa1d7
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2025-01-09 16:12:55 -0500

    changes
    * move error-related code for groups to its own file
    
    * update group avatar logic
    
    remove unused/duplicate logic
    
    * update `FindGroupsOptions.ToConds()`
    
    allow passing `-1` as the `ParentGroupID`, meaning "find matching groups regardless of the parent group id"
    
    * add `DedupeBy` function to container module
    
    this removes duplicate items from a slice using a custom function
    
    * add `SliceMap` util
    
    works like javascripts's `Array.prototoype.map`, taking in a slice and transforming each element with the provided function
    
    * add group service
    
    functions included so far:
    - avatar uploading/deletion
    - group deletion
    - group creation
    - group moving (including moving item inside a group)
    - group update
    - team management
      - add team
      - remove team
      - update team permissions
      - recalculating team access (in event of group move)
    - group searching (only used in frontend/web components for now)

--- modules/container/filter.go
+++ modules/container/filter.go
@@ -19,3 +19,16 @@ func FilterSlice[E any, T comparable](s []E, include func(E) (T, bool)) []T {
 	}
 	return slices.Clip(filtered)
 }
+
+func DedupeBy[E any, I comparable](s []E, id func(E) I) []E {
+	filtered := make([]E, 0, len(s)) // slice will be clipped before returning
+	seen := make(map[I]bool, len(s))
+	for i := range s {
+		itemId := id(s[i])
+		if _, ok := seen[itemId]; !ok {
+			filtered = append(filtered, s[i])
+			seen[itemId] = true
+		}
+	}
+	return slices.Clip(filtered)
+}
