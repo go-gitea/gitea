83bd95ebd3034a70ab789e8e90a438751a263f24
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2025-01-09 18:49:55 -0500

    add group-related url segments to list of reserved usernames

--- models/user/user.go
+++ models/user/user.go
@@ -589,6 +589,7 @@ var (
 		"avatar",  // avatar by email hash
 		"avatars", // user avatars by file name
 		"repo-avatars",
+		"group-avatars",
 
 		"captcha",
 		"login", // oauth2 login
@@ -599,6 +600,8 @@ var (
 		"explore",
 		"issues",
 		"pulls",
+		"groups",
+		"group",
 		"milestones",
 		"notifications",
 
