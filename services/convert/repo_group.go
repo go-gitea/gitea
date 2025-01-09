3113fa8ec4da9969d303224a4834e2ab46e2b77f
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2025-01-09 18:33:33 -0500

    add conversion functions for repository groups

---
+++ services/convert/repo_group.go
@@ -0,0 +1,40 @@
+package convert
+
+import (
+	"context"
+
+	group_model "code.gitea.io/gitea/models/group"
+	repo_model "code.gitea.io/gitea/models/repo"
+	"code.gitea.io/gitea/models/unit"
+	user_model "code.gitea.io/gitea/models/user"
+	api "code.gitea.io/gitea/modules/structs"
+)
+
+func ToAPIGroup(ctx context.Context, g *group_model.Group, actor *user_model.User) (*api.Group, error) {
+	err := g.LoadAttributes(ctx)
+	if err != nil {
+		return nil, err
+	}
+	apiGroup := &api.Group{
+		ID:            g.ID,
+		Owner:         ToUser(ctx, g.Owner, actor),
+		Name:          g.Name,
+		Description:   g.Description,
+		ParentGroupID: g.ParentGroupID,
+		Link:          g.GroupLink(),
+		SortOrder:     g.SortOrder,
+	}
+	if apiGroup.NumSubgroups, err = group_model.CountGroups(ctx, &group_model.FindGroupsOptions{
+		ParentGroupID: g.ID,
+	}); err != nil {
+		return nil, err
+	}
+	if _, apiGroup.NumRepos, err = repo_model.SearchRepositoryByCondition(ctx, &repo_model.SearchRepoOptions{
+		GroupID: g.ID,
+		Actor:   actor,
+		OwnerID: g.OwnerID,
+	}, repo_model.AccessibleRepositoryCondition(actor, unit.TypeInvalid), true); err != nil {
+		return nil, err
+	}
+	return apiGroup, nil
+}
