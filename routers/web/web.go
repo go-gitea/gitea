b15d01b0cec32f34fafc41eaa97887305b1376f7
wxiaoguang <wxiaoguang@gmail.com>
2025-01-16 04:05:18 +0800

    Prepare for support performance trace (#33286)
    For #32973

--- routers/web/web.go
+++ routers/web/web.go
@@ -1622,7 +1622,7 @@ func registerRoutes(m *web.Router) {
 
 	m.NotFound(func(w http.ResponseWriter, req *http.Request) {
 		ctx := context.GetWebContext(req)
-		routing.UpdateFuncInfo(ctx, routing.GetFuncInfo(ctx.NotFound, "WebNotFound"))
+		defer routing.RecordFuncInfo(ctx, routing.GetFuncInfo(ctx.NotFound, "WebNotFound"))()
 		ctx.NotFound("", nil)
 	})
 }
