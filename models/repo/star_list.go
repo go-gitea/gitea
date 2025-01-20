83cd897d46bb5b7566ebf56cb18e35a3f3080f74
hiifong <f@ilo.nz>
2025-01-20 11:07:38 +0800

    Update

--- models/repo/star_list.go
+++ models/repo/star_list.go
@@ -10,10 +10,10 @@ import (
 
 // StarList ...
 type StarList struct {
-	ID   int64 `xorm:"pk autoincr"`
-	UID  int64 `xorm:INDEX uid`
-	SID  int64 `xorm:INDEX sid`
-	Name string
+	ID   int64  `xorm:"pk autoincr"`
+	UID  int64  `xorm:"INDEX NOT NULL"`
+	SID  int64  `xorm:"INDEX NOT NULL"`
+	Name string `xorm:"NOT NULL"`
 	Desc string
 
 	CreatedUnix timeutil.TimeStamp `xorm:"INDEX created"`
