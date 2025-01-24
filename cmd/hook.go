54cb733f016ff69788fdaa611cd0efeae6be0604
TheFox0x7 <thefox0x7@gmail.com>
2025-01-24 17:09:00 +0100

    disable create message in services instead of hook

--- cmd/hook.go
+++ cmd/hook.go
@@ -705,8 +705,7 @@ Gitea or set your environment appropriately.`, "")
 
 	if err == nil {
 		for _, res := range resp.Results {
-			// AGit always creates a pull request so there is no point in prompting user to create one
-			hookPrintResult(res.ShouldShowMessage, false, res.HeadBranch, res.URL)
+			hookPrintResult(res.ShouldShowMessage, res.IsCreatePR, res.HeadBranch, res.URL)
 		}
 	}
 
