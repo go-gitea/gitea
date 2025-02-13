40377f322bbe6f3480d8cb834c2443e7fb12eafd
Lunny Xiao <xiaolunwen@gmail.com>
2025-02-13 14:51:18 -0800

    improvements

--- routers/web/repo/pull.go
+++ routers/web/repo/pull.go
@@ -784,15 +784,17 @@ func viewPullFiles(ctx *context.Context, specifiedStartCommit, specifiedEndCommi
 		return
 	}
 
+	allComments := issues_model.CommentList{}
 	for _, file := range diff.Files {
 		for _, section := range file.Sections {
 			for _, line := range section.Lines {
-				if err := line.Comments.LoadAttachments(ctx); err != nil {
-					ctx.ServerError("LoadAttachments", err)
-					return
-				}
+				allComments = append(allComments, line.Comments...)
 			}
 		}
+	}
+	if err := allComments.LoadAttachments(ctx); err != nil {
+		ctx.ServerError("LoadAttachments", err)
+		return
 	}
 
 	pb, err := git_model.GetFirstMatchProtectedBranchRule(ctx, pull.BaseRepoID, pull.BaseBranch)
