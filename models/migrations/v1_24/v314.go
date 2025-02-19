febad275f8aa3fdfc87f0ed50764e999e64ebe13
Lunny Xiao <xiaolunwen@gmail.com>
2025-02-18 16:47:37 -0800

    Fix bug

--- models/migrations/v1_24/v314.go
+++ models/migrations/v1_24/v314.go
@@ -11,9 +11,9 @@ func UpdateOwnerIDOfRepoLevelRunners(x *xorm.Engine) error {
 	if _, err := x.Exec("UPDATE `action_runner` SET `owner_id` = 0 WHERE `repo_id` > 0 AND `owner_id` > 0"); err != nil {
 		return err
 	}
-	if _, err := x.Exec("UPDATE `action_runner` SET `owner_id` = 0 WHERE `repo_id` > 0 AND `owner_id` > 0"); err != nil {
+	if _, err := x.Exec("UPDATE `action_variable` SET `owner_id` = 0 WHERE `repo_id` > 0 AND `owner_id` > 0"); err != nil {
 		return err
 	}
-	_, err := x.Exec("UPDATE `action_runner` SET `owner_id` = 0 WHERE `repo_id` > 0 AND `owner_id` > 0")
+	_, err := x.Exec("UPDATE `secret` SET `owner_id` = 0 WHERE `repo_id` > 0 AND `owner_id` > 0")
 	return err
 }
