4f6d92044b0782378971259e273ce6c7c50a4cc9
Jimmy Praet <jimmy.praet@telenet.be>
2025-02-22 13:22:50 +0100

    Revert "Differentiate between bleve and elastic_search in failing test"
    This reverts commit e2488786bf39f368aa70ecb464074202b3c34f6c.

--- modules/indexer/code/indexer_test.go
+++ modules/indexer/code/indexer_test.go
@@ -29,7 +29,6 @@ import (
 type codeSearchResult struct {
 	Filename string
 	Content  string
-	Indexer  string
 }
 
 func TestMain(m *testing.M) {
@@ -180,7 +179,6 @@ func testIndexer(name string, t *testing.T, indexer internal.Indexer) {
 					{
 						Filename: "ham.md",
 						Content:  "This is also not cheese",
-						Indexer:  "elastic_search",
 					},
 				},
 			},
@@ -251,18 +249,8 @@ func testIndexer(name string, t *testing.T, indexer internal.Indexer) {
 
 				hits := make([]codeSearchResult, 0, len(res))
 
-				expectedResults := make([]codeSearchResult, 0, len(kw.Results))
-				for _, expected := range kw.Results {
-					if expected.Indexer == "" || expected.Indexer == name {
-						expectedResults = append(expectedResults, codeSearchResult{
-							Filename: expected.Filename,
-							Content:  expected.Content,
-						})
-					}
-				}
-
 				if total > 0 {
-					assert.NotEmpty(t, expectedResults, "The given scenario does not provide any expected results")
+					assert.NotEmpty(t, kw.Results, "The given scenario does not provide any expected results")
 				}
 
 				for _, hit := range res {
@@ -274,7 +262,7 @@ func testIndexer(name string, t *testing.T, indexer internal.Indexer) {
 
 				lastIndex := -1
 
-				for _, expected := range expectedResults {
+				for _, expected := range kw.Results {
 					index := slices.Index(hits, expected)
 					if index == -1 {
 						assert.Failf(t, "Result not found", "Expected %v in %v", expected, hits)
