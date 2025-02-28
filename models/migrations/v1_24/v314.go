4bf5911d2c94319fd18fbe9442d7fef43eec4407
Lunny Xiao <xiaolunwen@gmail.com>
2025-02-28 14:21:05 -0800

    Fix the migration function name

--- models/migrations/v1_24/v314.go
+++ models/migrations/v1_24/v314.go
@@ -7,7 +7,7 @@ import (
 	"xorm.io/xorm"
 )
 
-func UpdateOwnerIDOfRepoLevelRunners(x *xorm.Engine) error {
+func UpdateOwnerIDOfRepoLevelActionsTables(x *xorm.Engine) error {
 	if _, err := x.Exec("UPDATE `action_runner` SET `owner_id` = 0 WHERE `repo_id` > 0 AND `owner_id` > 0"); err != nil {
 		return err
 	}
