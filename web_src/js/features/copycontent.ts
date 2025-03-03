204e6ffda17325f0c9bb75dc2eabe64b9e0dec55
wxiaoguang <wxiaoguang@gmail.com>
2025-03-03 17:33:42 +0800

    clean up

--- web_src/js/features/copycontent.ts
+++ web_src/js/features/copycontent.ts
@@ -7,17 +7,17 @@ import {registerGlobalEventFunc} from '../modules/observer.ts';
 const {i18n} = window.config;
 
 export function initCopyContent() {
-  registerGlobalEventFunc('click', 'onCopyContentButtonClick', async (el: HTMLInputElement) => {
-    if (el.classList.contains('is-loading')) return;
+  registerGlobalEventFunc('click', 'onCopyContentButtonClick', async (btn: HTMLInputElement) => {
+    if (btn.classList.contains('disabled') || btn.classList.contains('is-loading')) return;
     let content;
     let isRasterImage = false;
-    const link = el.getAttribute('data-link');
+    const link = btn.getAttribute('data-link');
 
     // when data-link is present, we perform a fetch. this is either because
-    // the text to copy is not in the DOM or it is an image which should be
+    // the text to copy is not in the DOM, or it is an image which should be
     // fetched to copy in full resolution
     if (link) {
-      el.classList.add('is-loading', 'loading-icon-2px');
+      btn.classList.add('is-loading', 'loading-icon-2px');
       try {
         const res = await GET(link, {credentials: 'include', redirect: 'follow'});
         const contentType = res.headers.get('content-type');
@@ -29,25 +29,25 @@ export function initCopyContent() {
           content = await res.text();
         }
       } catch {
-        return showTemporaryTooltip(el, i18n.copy_error);
+        return showTemporaryTooltip(btn, i18n.copy_error);
       } finally {
-        el.classList.remove('is-loading', 'loading-icon-2px');
+        btn.classList.remove('is-loading', 'loading-icon-2px');
       }
     } else { // text, read from DOM
       const lineEls = document.querySelectorAll('.file-view .lines-code');
       content = Array.from(lineEls, (el) => el.textContent).join('');
     }
 
-    // try copy original first, if that fails and it's an image, convert it to png
+    // try copy original first, if that fails, and it's an image, convert it to png
     const success = await clippie(content);
     if (success) {
-      showTemporaryTooltip(el, i18n.copy_success);
+      showTemporaryTooltip(btn, i18n.copy_success);
     } else {
       if (isRasterImage) {
         const success = await clippie(await convertImage(content as Blob, 'image/png'));
-        showTemporaryTooltip(el, success ? i18n.copy_success : i18n.copy_error);
+        showTemporaryTooltip(btn, success ? i18n.copy_success : i18n.copy_error);
       } else {
-        showTemporaryTooltip(el, i18n.copy_error);
+        showTemporaryTooltip(btn, i18n.copy_error);
       }
     }
   });
