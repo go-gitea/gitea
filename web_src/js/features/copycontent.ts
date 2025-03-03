4489e03f77fc2034c745b2ff1eedbce2902557e7
Kerwin Bryant <kerwin612@qq.com>
2025-03-03 08:10:03 +0000

    Refactor initCopyContent using the new registerGlobalEventFunc

--- web_src/js/features/copycontent.ts
+++ web_src/js/features/copycontent.ts
@@ -2,24 +2,22 @@ import {clippie} from 'clippie';
 import {showTemporaryTooltip} from '../modules/tippy.ts';
 import {convertImage} from '../utils.ts';
 import {GET} from '../modules/fetch.ts';
+import {registerGlobalEventFunc} from '../modules/observer.ts';
 
 const {i18n} = window.config;
 
 export function initCopyContent() {
-  const btn = document.querySelector('#copy-content');
-  if (!btn || btn.classList.contains('disabled')) return;
-
-  btn.addEventListener('click', async () => {
-    if (btn.classList.contains('is-loading')) return;
+  registerGlobalEventFunc('click', 'onCopyContentButtonClick', async (el: HTMLInputElement) => {
+    if (el.classList.contains('is-loading')) return;
     let content;
     let isRasterImage = false;
-    const link = btn.getAttribute('data-link');
+    const link = el.getAttribute('data-link');
 
     // when data-link is present, we perform a fetch. this is either because
     // the text to copy is not in the DOM or it is an image which should be
     // fetched to copy in full resolution
     if (link) {
-      btn.classList.add('is-loading', 'loading-icon-2px');
+      el.classList.add('is-loading', 'loading-icon-2px');
       try {
         const res = await GET(link, {credentials: 'include', redirect: 'follow'});
         const contentType = res.headers.get('content-type');
@@ -31,9 +29,9 @@ export function initCopyContent() {
           content = await res.text();
         }
       } catch {
-        return showTemporaryTooltip(btn, i18n.copy_error);
+        return showTemporaryTooltip(el, i18n.copy_error);
       } finally {
-        btn.classList.remove('is-loading', 'loading-icon-2px');
+        el.classList.remove('is-loading', 'loading-icon-2px');
       }
     } else { // text, read from DOM
       const lineEls = document.querySelectorAll('.file-view .lines-code');
@@ -43,13 +41,13 @@ export function initCopyContent() {
     // try copy original first, if that fails and it's an image, convert it to png
     const success = await clippie(content);
     if (success) {
-      showTemporaryTooltip(btn, i18n.copy_success);
+      showTemporaryTooltip(el, i18n.copy_success);
     } else {
       if (isRasterImage) {
         const success = await clippie(await convertImage(content as Blob, 'image/png'));
-        showTemporaryTooltip(btn, success ? i18n.copy_success : i18n.copy_error);
+        showTemporaryTooltip(el, success ? i18n.copy_success : i18n.copy_error);
       } else {
-        showTemporaryTooltip(btn, i18n.copy_error);
+        showTemporaryTooltip(el, i18n.copy_error);
       }
     }
   });
