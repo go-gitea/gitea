90feb81fba9819524d3f2634c2c998645d570d4b
Lunny Xiao <xiaolunwen@gmail.com>
2025-03-13 16:39:53 -0700

    Add comment

--- modules/gitrepo/gitrepo.go
+++ modules/gitrepo/gitrepo.go
@@ -70,6 +70,7 @@ func RepositoryFromRequestContextOrOpen(ctx reqctx.RequestContext, repo Reposito
 	return gitRepo, nil
 }
 
+// IsRepositoryExist returns true if the repository directory exists in the disk
 func IsRepositoryExist(ctx context.Context, repo Repository) (bool, error) {
 	return util.IsExist(repoPath(repo))
 }
