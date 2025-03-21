5587b9d1b314e110ca9cb3a8f91defa8324e0136
Kerwin Bryant <kerwin612@qq.com>
2025-03-21 01:10:31 +0000

    fix

--- services/repository/files/tree_test.go
+++ services/repository/files/tree_test.go
@@ -4,12 +4,14 @@
 package files
 
 import (
-	"html/template"
 	"testing"
 
 	"code.gitea.io/gitea/models/unittest"
+	"code.gitea.io/gitea/modules/fileicon"
 	"code.gitea.io/gitea/modules/git"
+	"code.gitea.io/gitea/modules/reqctx"
 	api "code.gitea.io/gitea/modules/structs"
+	"code.gitea.io/gitea/modules/templates"
 	"code.gitea.io/gitea/services/contexttest"
 
 	"github.com/stretchr/testify/assert"
@@ -62,6 +64,7 @@ func TestGetTreeViewNodes(t *testing.T) {
 	contexttest.LoadUser(t, ctx, 2)
 	contexttest.LoadGitRepo(t, ctx)
 	defer ctx.Repo.GitRepo.Close()
+	renderUtils := templates.NewRenderUtils(reqctx.FromContext(ctx))
 
 	treeNodes, err := GetTreeViewNodes(ctx, ctx.Repo.Commit, "", "")
 	assert.NoError(t, err)
@@ -69,8 +72,11 @@ func TestGetTreeViewNodes(t *testing.T) {
 		{
 			EntryName: "docs",
 			EntryMode: "tree",
-			FileIcon:  template.HTML(`<svg id="svg-mfi-folder-docs" class="svg git-entry-icon octicon-file-directory-fill" width="16" height="16" aria-hidden="true" viewBox='0 0 32 32'><path fill='#0277bd' d='m13.844 7.536-1.288-1.072A2 2 0 0 0 11.276 6H4a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h24a2 2 0 0 0 2-2V10a2 2 0 0 0-2-2H15.124a2 2 0 0 1-1.28-.464'/><path fill='#b3e5fc' d='M24 10h-7a1 1 0 0 0-1 1v16a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V16Zm0 16h-6v-2h6Zm4-4H18v-2h10Zm-4.828-5.172V12L28 16.828Z'/></svg>`),
-			FullPath:  "docs",
+			FileIcon: renderUtils.RenderFileIcon(&fileicon.FileIcon{
+				Name:      "docs",
+				EntryMode: git.EntryModeTree,
+			}),
+			FullPath: "docs",
 		},
 	}, treeNodes)
 
@@ -80,14 +86,20 @@ func TestGetTreeViewNodes(t *testing.T) {
 		{
 			EntryName: "docs",
 			EntryMode: "tree",
-			FileIcon:  template.HTML(`<svg class="svg git-entry-icon octicon-file-directory-fill" width="16" height="16" aria-hidden="true"><use xlink:href="#svg-mfi-folder-docs"></use></svg>`),
-			FullPath:  "docs",
+			FileIcon: renderUtils.RenderFileIcon(&fileicon.FileIcon{
+				Name:      "docs",
+				EntryMode: git.EntryModeTree,
+			}),
+			FullPath: "docs",
 			Children: []*TreeViewNode{
 				{
 					EntryName: "README.md",
 					EntryMode: "blob",
-					FileIcon:  template.HTML(`<svg id="svg-mfi-readme" class="svg git-entry-icon octicon-file" width="16" height="16" aria-hidden="true" fill='none' viewBox='0 0 16 16'><path d='M0 0h24v24H0z'/><path fill='#42a5f5' d='M8 1C4.136 1 1 4.136 1 8s3.136 7 7 7 7-3.136 7-7-3.136-7-7-7m1 11H7V7.5h2zm0-6H7V4h2z'/></svg>`),
-					FullPath:  "docs/README.md",
+					FileIcon: renderUtils.RenderFileIcon(&fileicon.FileIcon{
+						Name:      "README.md",
+						EntryMode: git.EntryModeBlob,
+					}),
+					FullPath: "docs/README.md",
 				},
 			},
 		},
@@ -99,8 +111,11 @@ func TestGetTreeViewNodes(t *testing.T) {
 		{
 			EntryName: "README.md",
 			EntryMode: "blob",
-			FileIcon:  template.HTML(`<svg class="svg git-entry-icon octicon-file" width="16" height="16" aria-hidden="true"><use xlink:href="#svg-mfi-readme"></use></svg>`),
-			FullPath:  "docs/README.md",
+			FileIcon: renderUtils.RenderFileIcon(&fileicon.FileIcon{
+				Name:      "README.md",
+				EntryMode: git.EntryModeBlob,
+			}),
+			FullPath: "docs/README.md",
 		},
 	}, treeNodes)
 }
