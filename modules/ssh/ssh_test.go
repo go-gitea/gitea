e1230541ba0d2acf49e799320b1f3e2a67eaf5fe
TheFox0x7 <thefox0x7@gmail.com>
2025-03-22 17:35:33 +0100

    prepare test for more keys

--- modules/ssh/ssh_test.go
+++ modules/ssh/ssh_test.go
@@ -14,20 +14,33 @@ import (
 )
 
 func TestGenKeyPair(t *testing.T) {
-	path := t.TempDir() + "/gitea.rsa"
-	require.NoError(t, ssh.GenKeyPair(path, ssh.RSA))
+	testCases := []struct {
+		keyType      ssh.KeyType
+		expectedType any
+	}{
+		{
+			keyType:      ssh.RSA,
+			expectedType: &rsa.PrivateKey{},
+		},
+	}
+	for _, tC := range testCases {
+		t.Run("Generate"+string(tC.keyType), func(t *testing.T) {
+			path := t.TempDir() + "/gitea." + string(tC.keyType)
+			require.NoError(t, ssh.GenKeyPair(path, tC.keyType))
 
-	file, err := os.Open(path)
-	require.NoError(t, err)
+			file, err := os.Open(path)
+			require.NoError(t, err)
 
-	bytes, err := io.ReadAll(file)
-	require.NoError(t, err)
+			bytes, err := io.ReadAll(file)
+			require.NoError(t, err)
 
-	block, _ := pem.Decode(bytes)
-	require.NotNil(t, block)
-	assert.Equal(t, "PRIVATE KEY", block.Type)
+			block, _ := pem.Decode(bytes)
+			require.NotNil(t, block)
+			assert.Equal(t, "PRIVATE KEY", block.Type)
 
-	privateKey, err := x509.ParsePKCS8PrivateKey(block.Bytes)
-	require.NoError(t, err)
-	assert.IsType(t, &rsa.PrivateKey{}, privateKey)
+			privateKey, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+			require.NoError(t, err)
+			assert.IsType(t, tC.expectedType, privateKey)
+		})
+	}
 }
