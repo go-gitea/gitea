998f9ffeebd194028660d3afe94660b51b9bf20a
Lunny Xiao <xiaolunwen@gmail.com>
2025-03-22 17:23:42 -0700

    Remove unused function

--- modules/gitrepo/commit.go
+++ modules/gitrepo/commit.go
@@ -9,15 +9,6 @@ import (
 	"code.gitea.io/gitea/modules/git"
 )
 
-func GetCommit(ctx context.Context, repo Repository, commitID string) (*git.Commit, error) {
-	gitRepo, err := git.OpenRepository(ctx, repoPath(repo))
-	if err != nil {
-		return nil, err
-	}
-	defer gitRepo.Close()
-	return gitRepo.GetCommit(commitID)
-}
-
 func CommitsCount(ctx context.Context, repo Repository, ref string) (int64, error) {
 	return git.CommitsCount(ctx, git.CommitsCountOptions{
 		RepoPath: repoPath(repo),
