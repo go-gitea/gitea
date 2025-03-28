819485e1c9c8b4b0feb88893f49af6769b98c5d3
wxiaoguang <wxiaoguang@gmail.com>
2025-03-28 13:54:16 +0800

    add comment

--- models/perm/access/repo_permission.go
+++ models/perm/access/repo_permission.go
@@ -25,8 +25,8 @@ type Permission struct {
 	units     []*repo_model.RepoUnit
 	unitsMode map[unit.Type]perm_model.AccessMode
 
-	everyoneAccessMode  map[unit.Type]perm_model.AccessMode
-	anonymousAccessMode map[unit.Type]perm_model.AccessMode
+	everyoneAccessMode  map[unit.Type]perm_model.AccessMode // the unit's minimal access mode for every signed-in user
+	anonymousAccessMode map[unit.Type]perm_model.AccessMode // the unit's minimal access mode for anonymous (non-signed-in) user
 }
 
 // IsOwner returns true if current user is the owner of repository.
