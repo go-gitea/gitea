7b4c3e2a87daa429ab8e7882b53fd62d71396393
Lunny Xiao <xiaolunwen@gmail.com>
2025-03-28 20:46:41 -0700

    Fix bug

--- models/migrations/v1_24/v318.go
+++ models/migrations/v1_24/v318.go
@@ -4,31 +4,14 @@
 package v1_24 //nolint
 
 import (
-<<<<<<< HEAD
-	"code.gitea.io/gitea/modules/timeutil"
-=======
 	"code.gitea.io/gitea/models/perm"
->>>>>>> main
 
 	"xorm.io/xorm"
 )
 
-<<<<<<< HEAD
-func CreateTableIssueDevLink(x *xorm.Engine) error {
-	type IssueDevLink struct {
-		ID           int64 `xorm:"pk autoincr"`
-		IssueID      int64 `xorm:"INDEX"`
-		LinkType     int
-		LinkedRepoID int64              `xorm:"INDEX"` // it can link to self repo or other repo
-		LinkIndex    string             // branch name, pull request number or commit sha
-		CreatedUnix  timeutil.TimeStamp `xorm:"INDEX created"`
-	}
-	return x.Sync(new(IssueDevLink))
-=======
 func AddRepoUnitAnonymousAccessMode(x *xorm.Engine) error {
 	type RepoUnit struct { //revive:disable-line:exported
 		AnonymousAccessMode perm.AccessMode `xorm:"NOT NULL DEFAULT 0"`
 	}
 	return x.Sync(&RepoUnit{})
->>>>>>> main
 }
