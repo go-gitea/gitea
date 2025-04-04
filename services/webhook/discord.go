0916ac6a1b3e09303598f6c930726f2c21d9d9cf
Valeri Petkov <mopchowork@gmail.com>
2025-04-04 12:55:14 +0300

    Improve comment

--- services/webhook/discord.go
+++ services/webhook/discord.go
@@ -101,6 +101,11 @@ var (
 	redColor         = color("ff3232")
 )
 
+// https://discord.com/developers/docs/resources/message#embed-object-embed-limits
+// Discord has some limits in place for the embeds.
+// According to some tests, there is no consistent limit for different characet sets.
+// For example: 4096 ASCII letters are allowed, but only 2490 emoji characters are allowed.
+// To keep it simple, we currently truncate at 2000.
 const discordDescriptionCharactersLimit = 2000
 
 type discordConvertor struct {
@@ -315,7 +320,7 @@ func (d discordConvertor) createPayload(s *api.User, title, text, url string, co
 		Embeds: []DiscordEmbed{
 			{
 				Title:       title,
-				Description: util.TruncateRunes(text, discordDescriptionCharactersLimit), // Discord has some limits in place for the embeds, to keep it simple we currently truncate at 2000
+				Description: util.TruncateRunes(text, discordDescriptionCharactersLimit),
 				URL:         url,
 				Color:       color,
 				Author: DiscordEmbedAuthor{
