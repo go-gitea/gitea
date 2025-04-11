3729e9ff964a7544aad912598f67dcb21381b3f8
Christopher Homberger <christopher.homberger@web.de>
2025-04-11 17:09:49 +0200

    fix test stability

--- tests/integration/api_admin_test.go
+++ tests/integration/api_admin_test.go
@@ -412,15 +412,20 @@ func TestAPIRunnerAdminApi(t *testing.T) {
 	DecodeJSON(t, runnerListResp, &runnerList)
 
 	assert.Len(t, runnerList.Entries, 4)
-	assert.Equal(t, "runner_to_be_deleted", runnerList.Entries[0].Name)
-	assert.Equal(t, int64(34349), runnerList.Entries[0].ID)
-	assert.False(t, runnerList.Entries[0].Ephemeral)
-	assert.Len(t, runnerList.Entries[0].Labels, 2)
-	assert.Equal(t, "runner_to_be_deleted", runnerList.Entries[0].Labels[0].Name)
-	assert.Equal(t, "linux", runnerList.Entries[0].Labels[1].Name)
+
+	var hasRunner bool
 
 	// Verify all returned runners can be requested and deleted
 	for _, expectedRunner := range runnerList.Entries {
+		if int64(34349) == expectedRunner.ID {
+			assert.Equal(t, "runner_to_be_deleted", expectedRunner.Name)
+			assert.False(t, expectedRunner.Ephemeral)
+			assert.Len(t, expectedRunner.Labels, 2)
+			assert.Equal(t, "runner_to_be_deleted", expectedRunner.Labels[0].Name)
+			assert.Equal(t, "linux", expectedRunner.Labels[1].Name)
+			hasRunner = true
+		}
+
 		// Verify get the runner by id
 		req = NewRequest(t, "GET", fmt.Sprintf("/api/v1/admin/actions/runners/%d", expectedRunner.ID)).AddTokenAuth(token)
 		runnerResp := MakeRequest(t, req, http.StatusOK)
@@ -440,4 +445,6 @@ func TestAPIRunnerAdminApi(t *testing.T) {
 		req = NewRequest(t, "GET", fmt.Sprintf("/api/v1/admin/actions/runners/%d", expectedRunner.ID)).AddTokenAuth(token)
 		MakeRequest(t, req, http.StatusNotFound)
 	}
+
+	assert.True(t, hasRunner, "Expected runner not found in list")
 }
