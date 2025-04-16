4188181475bbb9e4ae4f7b4edbf57f7e20b96c4f
wxiaoguang <wxiaoguang@gmail.com>
2025-04-17 07:07:37 +0800

    Update web_src/js/markup/codecopy.ts

--- web_src/js/markup/codecopy.ts
+++ web_src/js/markup/codecopy.ts
@@ -15,6 +15,7 @@ export function initMarkupCodeCopy(elMarkup: HTMLElement): void {
     const btn = makeCodeCopyButton();
     // remove final trailing newline introduced during HTML rendering
     btn.setAttribute('data-clipboard-text', el.textContent.replace(/\r?\n$/, ''));
+    // we only want to use `.code-block-container` if it exists, no matter `.code-block` exists or not.
     const btnContainer = el.closest('.code-block-container') ?? el.closest('.code-block');
     btnContainer.append(btn);
   });
