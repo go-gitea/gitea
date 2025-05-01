a633f446109cbdf56663f127af7e17f742431282
NorthRealm <155140859+NorthRealm@users.noreply.github.com>
2025-05-01 19:09:52 +0800

    Refactor

--- routers/web/repo/actions/view.go
+++ routers/web/repo/actions/view.go
@@ -581,7 +581,6 @@ func Approve(ctx *context_module.Context) {
 	ctx.JSON(http.StatusOK, struct{}{})
 }
 
-// TODO: When deleting a run, it should at lease delete artifacts, tasks logs, database record.
 func Delete(ctx *context_module.Context) {
 	runIndex := getRunIndex(ctx)
 
@@ -599,75 +598,10 @@ func Delete(ctx *context_module.Context) {
 
 	repoID := ctx.Repo.Repository.ID
 
-	tasks := []*actions_model.ActionTask{}
-
-	for _, job := range jobs {
-		tasks0, err := db.Find[actions_model.ActionTask](ctx, actions_model.FindTaskOptions{
-			RepoID: repoID,
-			JobID:  job.ID,
-		})
-		if err != nil {
-			ctx.HTTPError(http.StatusInternalServerError, err.Error())
-			return
-		}
-		tasks = append(tasks, tasks0...)
-	}
-
-	artifacts, err := db.Find[actions_model.ActionArtifact](ctx, actions_model.FindArtifactsOptions{
-		RepoID: repoID,
-		RunID:  run.ID,
-	})
-	if err != nil {
+	if err := actions_model.DeleteRun(ctx, repoID, run, jobs); err != nil {
 		ctx.HTTPError(http.StatusInternalServerError, err.Error())
 		return
 	}
-
-	recordsToDelete := []any{}
-
-	for _, task := range tasks {
-		recordsToDelete = append(recordsToDelete, &actions_model.ActionTask{
-			RepoID: repoID,
-			ID:     task.ID,
-		})
-		recordsToDelete = append(recordsToDelete, &actions_model.ActionTaskStep{
-			RepoID: repoID,
-			TaskID: task.ID,
-		})
-	}
-	recordsToDelete = append(recordsToDelete, &actions_model.ActionRunJob{
-		RepoID: repoID,
-		RunID:  run.ID,
-	})
-	recordsToDelete = append(recordsToDelete, &actions_model.ActionRun{
-		RepoID: repoID,
-		ID:     run.ID,
-	})
-	recordsToDelete = append(recordsToDelete, &actions_model.ActionArtifact{
-		RepoID: repoID,
-		RunID:  run.ID,
-	})
-
-	if err := db.WithTx(ctx, func(ctx context.Context) error {
-		return db.DeleteBeans(ctx, recordsToDelete...)
-	}); err != nil {
-		ctx.HTTPError(http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	// Delete files on storage
-	for _, task := range tasks {
-		err := actions.RemoveLogs(ctx, task.LogInStorage, task.LogFilename)
-		if err != nil {
-			log.Error("remove log file %q: %v", task.LogFilename, err)
-		}
-	}
-	for _, art := range artifacts {
-		if err := storage.ActionsArtifacts.Delete(art.StoragePath); err != nil {
-			log.Error("remove artifact file %q: %v", art.StoragePath, err)
-		}
-	}
-
-	// TODO: Delete commit status? Looks like it has no direct reference to a run/task/job. Not quite feasible without modifying db models (Dangerous).
 
 	ctx.JSON(http.StatusOK, struct{}{})
 }
