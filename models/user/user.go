a60a7227844d7d3aeca20a6d475a1069b0865e00
Kerwin Bryant <kerwin612@qq.com>
2025-05-21 14:03:48 +0000

    fix

--- models/user/user.go
+++ models/user/user.go
@@ -835,10 +835,7 @@ type CountUserFilter struct {
 // It performs a much more efficient check than counting all users.
 func HasUsers(ctx context.Context) (bool, error) {
 	sess := db.GetEngine(ctx)
-	cond := builder.NewCond()
-	cond = cond.And(builder.Eq{"type": UserTypeIndividual})
-
-	exists, err := sess.Where(cond).Limit(1).Exist(new(User))
+	exists, err := sess.Exist(new(User))
 	if err != nil {
 		return false, fmt.Errorf("error checking user existence: %w", err)
 	}
