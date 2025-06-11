bb6778500f5be44ca8bb4ae58fcb77004998ad95
Thomas Sayen <69324626+Chi-Iroh@users.noreply.github.com>
2025-06-11 09:45:58 +0200

    Removed jquery

--- web_src/js/features/repo-commit.ts
+++ web_src/js/features/repo-commit.ts
@@ -1,7 +1,6 @@
 import {createTippy} from '../modules/tippy.ts';
 import {toggleElem} from '../utils/dom.ts';
 import {registerGlobalEventFunc, registerGlobalInitFunc} from '../modules/observer.ts';
-import $ from 'jquery';
 
 export function initRepoEllipsisButton() {
   registerGlobalEventFunc('click', 'onRepoEllipsisButtonClick', async (el: HTMLInputElement, e: Event) => {
@@ -26,24 +25,28 @@ export function initCommitStatuses() {
   });
 }
 
-window.addEventListener('DOMContentLoaded', () => {
-  ($('input[name=history-enable-follow-renames]')[0] as HTMLInputElement).checked = location.toString().includes('history_follow_rename=true');
-});
+export function initCommitFileHistoryFollowRename() {
+  const checkbox : HTMLInputElement | null = document.querySelector('input[name=history-enable-follow-renames]');
 
-$('input[name=history-enable-follow-renames]').on('change', function() {
-  const checked = ($(this)[0] as HTMLInputElement).matches(':checked');
-  let url = location.toString();
-
-  url = url.replaceAll(/history_follow_rename=(true|false)&*/g, '');
-  if (url.endsWith('?')) {
-    url = url.slice(0, -1);
+  if (!checkbox) {
+    return;
   }
-  if (url.includes('?')) {
-    url += '&';
-  } else {
-    url += '?';
-  }
+  checkbox.checked = location.toString().includes('history_follow_rename=true');
 
-  url += `history_follow_rename=${checked}`;
-  window.location.href = url;
-});
+  checkbox.addEventListener('change', () => {
+    let url = location.toString();
+
+    url = url.replaceAll(/history_follow_rename=(true|false)&*/g, '');
+    if (url.endsWith('?')) {
+      url = url.slice(0, -1);
+    }
+    if (url.includes('?')) {
+      url += '&';
+    } else {
+      url += '?';
+    }
+
+    url += `history_follow_rename=${checkbox.checked}`;
+    window.location.href = url;
+  });
+}
