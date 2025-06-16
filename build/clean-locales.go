18c22b59b540d8b4eb395ac84010fa29ac9749c2
Lunny Xiao <xiaolunwen@gmail.com>
2025-06-15 23:53:35 -0700

    Add a tool to detect possible unused language keys and remove those which can be confirmed

---
+++ build/clean-locales.go
@@ -0,0 +1,92 @@
+// Copyright 2025 The Gitea Authors. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+//go:build ignore
+
+package main
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"code.gitea.io/gitea/modules/setting"
+)
+
+func searchDirs(key string) (bool, error) {
+	for _, dir := range []string{
+		"cmd",
+		"models",
+		"modules",
+		"routers",
+		"services",
+		"templates",
+	} {
+		found, err := searchLocaleFiles(dir, key)
+		if err != nil {
+			return false, err
+		}
+		if found {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+func searchLocaleFiles(dir, key string) (bool, error) {
+	errFound := errors.New("found")
+	err := filepath.WalkDir(dir, func(path string, d os.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() || (!strings.HasSuffix(d.Name(), ".go") && !strings.HasSuffix(d.Name(), ".tmpl")) {
+			return nil
+		}
+
+		bs, err := os.ReadFile(path)
+		if err != nil {
+			return err
+		}
+		if strings.Contains(string(bs), `"`+key+`"`) {
+			return errFound
+		}
+
+		return nil
+	})
+	if err == errFound {
+		return true, nil
+	}
+	return false, err
+}
+
+func main() {
+	if len(os.Args) != 1 {
+		println("usage: clean-locales")
+		os.Exit(1)
+	}
+
+	iniFile, err := setting.NewConfigProviderForLocale("options/locale/locale_en-US.ini")
+	if err != nil {
+		panic(err)
+	}
+
+	for _, section := range iniFile.Sections() {
+		for _, key := range section.Keys() {
+			var trKey string
+			if section.Name() == "" || section.Name() == "DEFAULT" {
+				trKey = key.Name()
+			} else {
+				trKey = section.Name() + "." + key.Name()
+			}
+
+			found, err := searchDirs(trKey)
+			if err != nil {
+				panic(err)
+			}
+			if !found {
+				println("unused locale key:", trKey)
+			}
+		}
+	}
+}
