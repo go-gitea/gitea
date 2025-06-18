8efc4ca334caf1f32aa5e017772f14fb1ac5ab64
wxiaoguang <wxiaoguang@gmail.com>
2025-06-19 03:04:24 +0800

    Refactor packages (func name & UI) (#34773)
    1. Use `OpenXxx` instead of `GetXxx` because the returned readers should
    be correctly closed, and clarify the behaviors of the functions: they
    increase the download counter
    2. Use `packages-content` styles instead of `issue-content`

--- routers/api/packages/container/container.go
+++ routers/api/packages/container/container.go
@@ -705,7 +705,7 @@ func DeleteManifest(ctx *context.Context) {
 func serveBlob(ctx *context.Context, pfd *packages_model.PackageFileDescriptor) {
 	serveDirectReqParams := make(url.Values)
 	serveDirectReqParams.Set("response-content-type", pfd.Properties.GetByName(container_module.PropertyMediaType))
-	s, u, _, err := packages_service.GetPackageBlobStream(ctx, pfd.File, pfd.Blob, serveDirectReqParams)
+	s, u, _, err := packages_service.OpenBlobForDownload(ctx, pfd.File, pfd.Blob, serveDirectReqParams)
 	if err != nil {
 		apiError(ctx, http.StatusInternalServerError, err)
 		return
