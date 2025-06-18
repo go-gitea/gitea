54138dcb0e2821c8a2b0069d792f8d4d87a62863
silverwind <me@silverwind.io>
2025-06-18 19:07:23 +0200

    Update tests/integration/api_issue_test.go

--- tests/integration/api_issue_test.go
+++ tests/integration/api_issue_test.go
@@ -267,7 +267,7 @@ func TestAPISearchIssues(t *testing.T) {
 	defer tests.PrepareTestEnv(t)()
 
 	// as this API was used in the frontend, it uses UI page size
-	expectedIssueCount := min(20, setting.UI.IssuePagingNum) // from the fixtures
+	expectedIssueCount := min(20, setting.UI.IssuePagingNum) // 20 is from the fixtures
 
 	link, _ := url.Parse("/api/v1/repos/issues/search")
 	token := getUserToken(t, "user1", auth_model.AccessTokenScopeReadIssue)
