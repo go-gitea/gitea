a2ae7c69da638d4cf3801d8bc73567670608b19e
wxiaoguang <wxiaoguang@gmail.com>
2025-06-19 00:32:43 +0800

    Fix some package registry problems (#34759)
    1. Fix #33787
    2. Fix container image display

--- models/packages/package_property.go
+++ models/packages/package_property.go
@@ -92,8 +92,8 @@ func DeletePropertyByID(ctx context.Context, propertyID int64) error {
 	return err
 }
 
-// DeletePropertyByName deletes properties by name
-func DeletePropertyByName(ctx context.Context, refType PropertyType, refID int64, name string) error {
+// DeletePropertiesByName deletes properties by name
+func DeletePropertiesByName(ctx context.Context, refType PropertyType, refID int64, name string) error {
 	_, err := db.GetEngine(ctx).Where("ref_type = ? AND ref_id = ? AND name = ?", refType, refID, name).Delete(&PackageProperty{})
 	return err
 }
