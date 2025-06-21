bbc3145f3e8567d1b7ef389268aebc873e9abab6
Lunny Xiao <xiaolunwen@gmail.com>
2025-06-21 12:58:10 -0700

    Fix a bug when setting repository private

--- routers/web/repo/setting/setting.go
+++ routers/web/repo/setting/setting.go
@@ -209,7 +209,7 @@ func handleSettingsPostUpdate(ctx *context.Context) {
 
 	// Visibility of forked repository is forced sync with base repository.
 	if repo.IsFork {
-		form.Private = repo.BaseRepo.IsPrivate || repo.BaseRepo.Owner.Visibility == structs.VisibleTypePrivate
+		repo.IsPrivate = repo.BaseRepo.IsPrivate || repo.BaseRepo.Owner.Visibility == structs.VisibleTypePrivate
 	}
 
 	if err := repo_service.UpdateRepository(ctx, repo, false); err != nil {
