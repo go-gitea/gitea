54f6a8b031cbef5c5b438c9741f2a69be1ca8e49
TheFox0x7 <thefox0x7@gmail.com>
2025-06-27 09:36:31 +0200

    fix typo

--- modules/optional/serialization_test.go
+++ modules/optional/serialization_test.go
@@ -4,7 +4,7 @@
 package optional_test
 
 import (
-	std_json "encoding/json" //nolint:depguard // for comparision with std
+	std_json "encoding/json" //nolint:depguard // for comparison with std
 	"testing"
 
 	"code.gitea.io/gitea/modules/json"
