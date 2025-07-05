2af39526531745a0a58745e196506a31dae49836
wxiaoguang <wxiaoguang@gmail.com>
2025-07-05 20:02:10 +0800

    fix

--- web_src/fomantic/build/fomantic.js
+++ web_src/fomantic/build/fomantic.js
@@ -1,6 +1,8 @@
+// Hard-forked from Fomantic UI 2.8.7, patches are commented with "GITEA-PATCH"
+import jquery from 'jquery';
 import './components/api.js';
 import './components/dropdown.js';
 import './components/modal.js';
 import './components/search.js';
 
-// Hard-forked from Fomantic UI 2.8.7, patches are commented with "GITEA-PATCH"
+window.$ = window.jQuery = jquery;
--- web_src/js/globals.ts
+++
@@ -1,5 +0,0 @@
-import jquery from 'jquery';
-import htmx from 'htmx.org/dist/htmx.esm.js';
-
-window.$ = window.jQuery = jquery;
-window.htmx = htmx;
