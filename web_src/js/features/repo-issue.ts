86e09329701df9a20af64d75b78f8f518e2d804c
wxiaoguang <wxiaoguang@gmail.com>
2025-07-25 19:12:28 +0800

    Apply suggestion
    Signed-off-by: wxiaoguang <wxiaoguang@gmail.com>

--- web_src/js/features/repo-issue.ts
+++ web_src/js/features/repo-issue.ts
@@ -544,7 +544,7 @@ function initIssueTemplateCommentEditors(commentForm: HTMLFormElement) {
       hideElem(commentForm.querySelectorAll('.combo-editor-dropzone .combo-markdown-editor'));
       queryElems(commentForm, '.combo-editor-dropzone .form-field-dropzone', (dropzoneContainer) => {
         // if "form-field-dropzone" exists, then "dropzone" must also exist
-        const dropzone = dropzoneContainer.querySelector('.dropzone').dropzone;
+        const dropzone = dropzoneContainer.querySelector<HTMLElement>('.dropzone').dropzone;
         const hasUploadedFiles = dropzone.files.length !== 0;
         toggleElem(dropzoneContainer, hasUploadedFiles);
       });
