a075ef76906c5d7da4e6990ef8f94dd23e41daba
AdhamSaif16 <adhamsaif16@gmail.com>
2025-08-11 09:55:45 +0300

    updated token

--- tests/api/config.py
+++ tests/api/config.py
@@ -1,4 +1,10 @@
 import os
-API_TOKEN = 'b32712d989aeee1e78f112ba4fc8e9f93ae7fe78'
+
+#local API_TOKEN
+#API_TOKEN = 'b32712d989aeee1e78f112ba4fc8e9f93ae7fe78'
+
+#remote API_TOKEN:
+API_TOKEN = 'f1600abdf735b9e11dbfc45e57db04319d58235e'
+
 #BASE_URL = os.getenv('GITEA_URL', 'http://localhost:3000')
 BASE_URL = 'http://108.130.122.66:3000'
