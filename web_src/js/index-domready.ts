8a1d8806da8da9074a885fdaab461f98063794f5
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2025-08-14 21:17:21 -0400

    add drag-and-drop functionality to org group sidebar

--- web_src/js/index-domready.ts
+++ web_src/js/index-domready.ts
@@ -64,6 +64,7 @@ import {initGlobalButtonClickOnEnter, initGlobalButtons, initGlobalDeleteButton}
 import {initGlobalComboMarkdownEditor, initGlobalEnterQuickSubmit, initGlobalFormDirtyLeaveConfirm} from './features/common-form.ts';
 import {callInitFunctions} from './modules/init.ts';
 import {initRepoViewFileTree} from './features/repo-view-file-tree.ts';
+import {initCommonGroup, initGroup} from "./features/group.ts";
 
 const initStartTime = performance.now();
 const initPerformanceTracer = callInitFunctions([
@@ -159,6 +160,9 @@ const initPerformanceTracer = callInitFunctions([
   initOAuth2SettingsDisableCheckbox,
 
   initRepoFileView,
+
+  initCommonGroup,
+  initGroup,
 ]);
 
 // it must be the last one, then the "querySelectorAll" only needs to be executed once for global init functions.
