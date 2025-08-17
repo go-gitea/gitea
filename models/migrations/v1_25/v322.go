ae7a9d51c3a48f9c74432a5dd1ba8ae980c34af7
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2025-08-17 16:13:28 -0400

    add indices to group_id and group_sort_order column add migration for repository table

---
+++ models/migrations/v1_25/v322.go
@@ -0,0 +1,18 @@
+// Copyright 2025 The Gitea Authors. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+package v1_25
+
+import "xorm.io/xorm"
+
+func AddGroupColumnsToRepositoryTable(x *xorm.Engine) error {
+	type Repository struct {
+		GroupID        int64 `xorm:"DEFAULT NULL"`
+		GroupSortOrder int
+	}
+	_, err := x.SyncWithOptions(xorm.SyncOptions{
+		IgnoreConstrains: false,
+		IgnoreIndices:    false,
+	}, new(Repository))
+	return err
+}
