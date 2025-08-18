b83a32f8f96d0083c8c1dc4bbf2d8911dedb7b6c
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2025-08-17 21:57:33 -0400

    add group ID column to repository table's unique constraint

--- models/migrations/v1_26/v324.go
+++ models/migrations/v1_26/v324.go
@@ -4,7 +4,7 @@ import "xorm.io/xorm"
 
 func AddGroupColumnsToRepositoryTable(x *xorm.Engine) error {
 	type Repository struct {
-		GroupID        int64 `xorm:"DEFAULT NULL"`
+		GroupID        int64 `xorm:"UNIQUE(s) INDEX DEFAULT NULL"`
 		GroupSortOrder int
 	}
 	_, err := x.SyncWithOptions(xorm.SyncOptions{
