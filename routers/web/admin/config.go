4793538b8b8fff566002e3b81fa8be1607c25c9c
wxiaoguang <wxiaoguang@gmail.com>
2025-08-21 13:48:08 +0800

    simplify

--- routers/web/admin/config.go
+++ routers/web/admin/config.go
@@ -198,27 +198,18 @@ func ConfigSettings(ctx *context.Context) {
 func ChangeConfig(ctx *context.Context) {
 	cfg := setting.Config()
 
-	marshalBool := func(v string) (string, error) { //nolint:unparam // error is always nil
-		if b, _ := strconv.ParseBool(v); b {
-			return "true", nil
-		}
-		return "false", nil
+	marshalBool := func(v string) ([]byte, error) { //nolint:unparam // error is always nil
+		b, _ := strconv.ParseBool(v)
+		return json.Marshal(b)
 	}
 
-	marshalString := func(emptyDefault string) func(v string) (string, error) {
-		return func(v string) (string, error) {
-			if v == "" {
-				v = emptyDefault
-			}
-			b, err := json.Marshal(v)
-			if err != nil {
-				return "", err
-			}
-			return string(b), nil
+	marshalString := func(emptyDefault string) func(v string) ([]byte, error) {
+		return func(v string) ([]byte, error) {
+			return json.Marshal(util.IfZero(v, emptyDefault))
 		}
 	}
 
-	marshalOpenWithApps := func(value string) (string, error) {
+	marshalOpenWithApps := func(value string) ([]byte, error) {
 		lines := strings.Split(value, "\n")
 		var openWithEditorApps setting.OpenWithEditorAppsType
 		for _, line := range lines {
@@ -236,13 +227,9 @@ func ChangeConfig(ctx *context.Context) {
 				OpenURL:     strings.TrimSpace(openURL),
 			})
 		}
-		b, err := json.Marshal(openWithEditorApps)
-		if err != nil {
-			return "", err
-		}
-		return string(b), nil
+		return json.Marshal(openWithEditorApps)
 	}
-	marshallers := map[string]func(string) (string, error){
+	marshallers := map[string]func(string) ([]byte, error){
 		cfg.Picture.DisableGravatar.DynKey():       marshalBool,
 		cfg.Picture.EnableFederatedAvatar.DynKey(): marshalBool,
 		cfg.Repository.OpenWithEditorApps.DynKey(): marshalOpenWithApps,
@@ -250,15 +237,16 @@ func ChangeConfig(ctx *context.Context) {
 	}
 
 	_ = ctx.Req.ParseForm()
-	queryKeys := ctx.Req.Form["key"]
-	queryValues := ctx.Req.Form["value"]
+	configKeys := ctx.Req.Form["key"]
+	configValues := ctx.Req.Form["value"]
+	configSettings := map[string]string{}
 loop:
-	for i, key := range queryKeys {
-		if i >= len(queryValues) {
+	for i, key := range configKeys {
+		if i >= len(configValues) {
 			ctx.JSONError(ctx.Tr("admin.config.set_setting_failed", key))
 			break loop
 		}
-		value := queryValues[i]
+		value := configValues[i]
 
 		marshaller, hasMarshaller := marshallers[key]
 		if !hasMarshaller {
@@ -271,15 +259,15 @@ loop:
 			ctx.JSONError(ctx.Tr("admin.config.set_setting_failed", key))
 			break loop
 		}
-
-		if err = system_model.SetSettings(ctx, map[string]string{key: marshaledValue}); err != nil {
-			ctx.JSONError(ctx.Tr("admin.config.set_setting_failed", key))
-			break loop
-		}
+		configSettings[key] = string(marshaledValue)
 	}
-
+	if ctx.Written() {
+		return
+	}
+	if err := system_model.SetSettings(ctx, configSettings); err != nil {
+		ctx.ServerError("SetSettings", err)
+		return
+	}
 	config.GetDynGetter().InvalidateCache()
-	if !ctx.Written() {
-		ctx.JSONOK()
-	}
+	ctx.JSONOK()
 }
