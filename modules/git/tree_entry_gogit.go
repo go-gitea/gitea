e40369c62ae40810e41232a556844dea46794b45
Lunny Xiao <xiaolunwen@gmail.com>
2025-08-25 12:59:07 -0700

    Fix lint

--- modules/git/tree_entry_gogit.go
+++ modules/git/tree_entry_gogit.go
@@ -8,6 +8,7 @@ package git
 
 import (
 	"code.gitea.io/gitea/modules/log"
+
 	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/filemode"
 	"github.com/go-git/go-git/v5/plumbing/object"
