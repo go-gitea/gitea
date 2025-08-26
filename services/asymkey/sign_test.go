234896bef0ea7a0dc329aae03300df29ccc2fd8c
lyric <lyricwulf@gmail.com>
2025-08-25 22:46:41 -0700

    test: add test cases for userHasPubkeys

---
+++ services/asymkey/sign_test.go
@@ -0,0 +1,39 @@
+// Copyright 2019 The Gitea Authors. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+package asymkey
+
+import (
+	"testing"
+
+	"code.gitea.io/gitea/models/unittest"
+
+	user_model "code.gitea.io/gitea/models/user"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUserHasPubkeys(t *testing.T) {
+	ctx := t.Context()
+
+	t.Run("AllowUserWithGPGKey", func(t *testing.T) {
+		user := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: 36}) // user has GPG key
+		hasKeys, err := userHasPubkeys(ctx, user)
+		assert.NoError(t, err)
+		assert.True(t, hasKeys)
+	})
+
+	t.Run("AllowUserWithSSHKey", func(t *testing.T) {
+		user := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: 2}) // user has SSH key
+		hasKeys, err := userHasPubkeys(ctx, user)
+		assert.NoError(t, err)
+		assert.True(t, hasKeys)
+	})
+
+	t.Run("DenyUserWithNoKeys", func(t *testing.T) {
+		user := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: 1})
+		hasKeys, err := userHasPubkeys(ctx, user)
+		assert.NoError(t, err)
+		assert.False(t, hasKeys)
+	})
+}
