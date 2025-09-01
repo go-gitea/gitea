43436882e6d8dbfa32668c57969e2e6d6da30ae1
Lunny Xiao <xiaolunwen@gmail.com>
2025-08-31 20:56:04 -0700

    Fix package link setting can only list limited repositories

--- services/forms/user_form.go
+++ services/forms/user_form.go
@@ -416,8 +416,8 @@ func (f *WebauthnDeleteForm) Validate(req *http.Request, errs binding.Errors) bi
 
 // PackageSettingForm form for package settings
 type PackageSettingForm struct {
-	Action string
-	RepoID int64 `form:"repo_id"`
+	Action       string
+	RepoFullName string `form:"repo_full_name"`
 }
 
 // Validate validates the fields
