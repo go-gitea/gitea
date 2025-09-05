59036b39566aa16bd7fefe25c0886dcd7902d1af
silverwind <me@silverwind.io>
2025-09-05 18:01:23 +0200

    await

--- tools/generate-images.js
+++ tools/generate-images.js
@@ -40,7 +40,7 @@ async function generate(svg, path, {size, bg}) {
 }
 
 async function main() {
-  await initWasm(readFile(new URL(import.meta.resolve('@resvg/resvg-wasm/index_bg.wasm'))));
+  await initWasm(await readFile(new URL(import.meta.resolve('@resvg/resvg-wasm/index_bg.wasm'))));
   const gitea = argv.slice(2).includes('gitea');
   const logoSvg = await readFile(new URL('../assets/logo.svg', import.meta.url), 'utf8');
   const faviconSvg = await readFile(new URL('../assets/favicon.svg', import.meta.url), 'utf8');
