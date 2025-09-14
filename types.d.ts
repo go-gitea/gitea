b596b75ba2cb3083d80023281efa3c5bee6063ea
silverwind <me@silverwind.io>
2025-09-14 07:19:23 +0200

    Revert "Replace webpack with rspack (#35460)"
    This reverts commit 866c636f52287d45e9c8d3d43b2107637b293a5f.

--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,8 @@
+declare module 'add-asset-webpack-plugin' {
+  const plugin: any;
+  export = plugin
+}
+
 declare module '@techknowlogick/license-checker-webpack-plugin' {
   const plugin: any;
   export = plugin
