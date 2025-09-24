c858a190ae82e64560d00bb9a218410c1e2619ec
Lunny Xiao <xiaolunwen@gmail.com>
2025-09-24 12:48:14 -0700

    improvements

--- modules/gitrepo/main_test.go
+++ modules/gitrepo/main_test.go
@@ -4,11 +4,13 @@
 package gitrepo
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
 
 	"code.gitea.io/gitea/modules/setting"
+	"code.gitea.io/gitea/modules/util"
 )
 
 const (
@@ -16,8 +18,17 @@ const (
 )
 
 func TestMain(m *testing.M) {
+	// since all test repository is defined by testReposDir, we need to set it here
+	// So that all functions could work properly because Repository will only contain
+	// Relative Path to RepoRootPath
 	setting.RepoRootPath, _ = filepath.Abs(testReposDir)
-	setting.Git.HomePath = filepath.Join(setting.RepoRootPath, ".home")
+	// TODO: run command requires a home directory, we could set it to a temp dir
+	gitHomePath, err := os.MkdirTemp(os.TempDir(), "git-home")
+	if err != nil {
+		panic(fmt.Errorf("unable to create temp dir: %w", err))
+	}
+	defer util.RemoveAll(gitHomePath)
+	setting.Git.HomePath = gitHomePath
 
 	exitStatus := m.Run()
 	os.Exit(exitStatus)
