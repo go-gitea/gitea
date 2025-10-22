2b51f0ad8062e8ae92c39af0914c89cd794f9cd0
wxiaoguang <wxiaoguang@gmail.com>
2025-10-22 09:37:28 +0800

    fix test

--- models/user/user.go
+++ models/user/user.go
@@ -253,7 +253,7 @@ func (u *User) MaxCreationLimit() int {
 // It ONLY checks the repo number LIMIT or whether owner user is real. If new checks are added, those functions should be revised.
 // TODO: the callers can only return ErrReachLimitOfRepo, need to fine tune to support other error types in the future.
 func (u *User) CanCreateRepoIn(owner *User) bool {
-	if u.ID <= 0 {
+	if u.ID <= 0 || owner.ID <= 0 {
 		return false // fake user like Ghost or Actions user
 	}
 	if u.IsAdmin {
