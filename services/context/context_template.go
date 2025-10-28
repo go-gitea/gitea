6b5563c54a05e3ed925822d3126da38fac241b08
wxiaoguang <wxiaoguang@gmail.com>
2025-10-28 18:25:00 +0800

    Support selecting theme on the footer (#35741)
    Fixes: https://github.com/go-gitea/gitea/pull/27576

--- services/context/context_template.go
+++ services/context/context_template.go
@@ -5,13 +5,16 @@ package context
 
 import (
 	"context"
+	"net/http"
 	"time"
+
+	"code.gitea.io/gitea/services/webtheme"
 )
 
 var _ context.Context = TemplateContext(nil)
 
-func NewTemplateContext(ctx context.Context) TemplateContext {
-	return TemplateContext{"_ctx": ctx}
+func NewTemplateContext(ctx context.Context, req *http.Request) TemplateContext {
+	return TemplateContext{"_ctx": ctx, "_req": req}
 }
 
 func (c TemplateContext) parentContext() context.Context {
@@ -32,4 +35,20 @@ func (c TemplateContext) Err() error {
 
 func (c TemplateContext) Value(key any) any {
 	return c.parentContext().Value(key)
+}
+
+func (c TemplateContext) CurrentWebTheme() *webtheme.ThemeMetaInfo {
+	req := c["_req"].(*http.Request)
+	var themeName string
+	if webCtx := GetWebContext(c); webCtx != nil {
+		if webCtx.Doer != nil {
+			themeName = webCtx.Doer.Theme
+		}
+	}
+	if themeName == "" {
+		if cookieTheme, _ := req.Cookie("gitea_theme"); cookieTheme != nil {
+			themeName = cookieTheme.Value
+		}
+	}
+	return webtheme.GuaranteeGetThemeMetaInfo(themeName)
 }
