72356186c67b724cf8164a5149ebe48af4168c1a
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2025-11-23 23:35:00 -0500

    fix repository renaming

--- services/repository/transfer.go
+++ services/repository/transfer.go
@@ -367,7 +367,7 @@ func changeRepositoryName(ctx context.Context, repo *repo_model.Repository, newR
 	}
 
 	if err = gitrepo.RenameRepository(ctx, repo,
-		repo_model.StorageRepo(repo_model.RelativePath(repo.OwnerName, newRepoName, 0))); err != nil {
+		repo_model.StorageRepo(repo_model.RelativePath(repo.OwnerName, newRepoName, repo.GroupID))); err != nil {
 		return fmt.Errorf("rename repository directory: %w", err)
 	}
 
