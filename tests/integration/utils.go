ed9a9bd5c4e7a5adbd849f5a2a8b9a1a8e12587c
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2025-11-23 21:57:31 -0500

    fix: `maybeGroupSegment` func, run formatter

--- tests/integration/utils.go
+++ tests/integration/utils.go
@@ -7,15 +7,7 @@ import "fmt"
 
 func maybeGroupSegment(gid int64) string {
 	if gid > 0 {
-		return fmt.Sprintf("%d/", gid)
-	}
-	return ""
-}
-
-func maybeWebGroupSegment(gid int64) string {
-	gs := maybeGroupSegment(gid)
-	if gs != "" {
-		return "group/" + gs
+		return fmt.Sprintf("group/%d/", gid)
 	}
 	return ""
 }
