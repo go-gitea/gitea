66c304abcf699670e5c29456c15166ef414468e8
silverwind <me@silverwind.io>
2025-12-03 02:33:22 +0100

    remove useless assignement to null

--- web_src/js/features/repo-diff.ts
+++ web_src/js/features/repo-diff.ts
@@ -56,8 +56,6 @@ function initRepoDiffConversationForm() {
       const idx = newConversationHolder.getAttribute('data-idx');
 
       form.closest('.conversation-holder')!.replaceWith(newConversationHolder);
-      // @ts-expect-error -- this assignment likely does nothing
-      form = null; // prevent further usage of the form because it should have been replaced
 
       if (trLineType) {
         // if there is a line-type for the "tr", it means the form is on the diff page
