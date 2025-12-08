fa7ee5a9dc969aba8ba30fa4f0619349fa9d5d31
Elisei Roca <eroca@suse.de>
2025-12-08 17:19:05 +0100

    Enable logout redirection for reverse proxy setups
    When authentication is handled externally by a reverse proxy or SSO provider,
    users can be redirected to an external logout URL or relative path
    defined on the reverse proxy.

--- tests/integration/signout_test.go
+++ tests/integration/signout_test.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 	"testing"
 
+	"code.gitea.io/gitea/modules/setting"
+	"code.gitea.io/gitea/modules/test"
 	"code.gitea.io/gitea/tests"
 )
 
@@ -16,7 +18,34 @@ func TestSignOut(t *testing.T) {
 	session := loginUser(t, "user2")
 
 	req := NewRequest(t, "POST", "/user/logout")
-	session.MakeRequest(t, req, http.StatusOK)
+	resp := session.MakeRequest(t, req, http.StatusSeeOther)
+
+	expected := "/"
+	loc := resp.Header().Get("Location")
+	if loc != expected {
+		t.Fatalf("expected redirect to %q, got %q", expected, loc)
+	}
+
+	// try to view a private repo, should fail
+	req = NewRequest(t, "GET", "/user2/repo2")
+	session.MakeRequest(t, req, http.StatusNotFound)
+}
+
+func TestSignOut_ReverseProxyLogoutRedirect(t *testing.T) {
+	defer tests.PrepareTestEnv(t)()
+
+	defer test.MockVariableValue(&setting.ReverseProxyLogoutRedirect, "/mellon/logout?ReturnTo=/")()
+
+	session := loginUser(t, "user2")
+
+	req := NewRequest(t, "POST", "/user/logout")
+	resp := session.MakeRequest(t, req, http.StatusSeeOther)
+
+	expected := "/mellon/logout?ReturnTo=/"
+	loc := resp.Header().Get("Location")
+	if loc != expected {
+		t.Fatalf("expected redirect to %q, got %q", expected, loc)
+	}
 
 	// try to view a private repo, should fail
 	req = NewRequest(t, "GET", "/user2/repo2")
