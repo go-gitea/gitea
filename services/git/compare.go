17e915e08a35268c3b67e4a1203ea23ece418c5d
Lunny Xiao <xiaolunwen@gmail.com>
2025-12-28 18:24:07 -0800

    revert unnecessary change

--- services/git/compare.go
+++ services/git/compare.go
@@ -55,7 +55,10 @@ func (ci *CompareInfo) OtherSeparator() string {
 
 // GetCompareInfo generates and returns compare information between base and head branches of repositories.
 func GetCompareInfo(ctx context.Context, baseRepo, headRepo *repo_model.Repository, headGitRepo *git.Repository, baseRef, headRef git.RefName, directComparison, fileOnly bool) (_ *CompareInfo, err error) {
-	var tmpRemote string
+	var (
+		remoteBranch string
+		tmpRemote    string
+	)
 
 	// We don't need a temporary remote for same repository.
 	if baseRepo.ID != headRepo.ID {
@@ -85,30 +88,42 @@ func GetCompareInfo(ctx context.Context, baseRepo, headRepo *repo_model.Reposito
 		compareInfo.HeadCommitID = headRef.String()
 	}
 
-	compareInfo.BaseCommitID, err = gitrepo.GetFullCommitID(ctx, baseRepo, baseRef.String())
-	if err != nil {
-		compareInfo.BaseCommitID = baseRef.String()
-	}
-
-	compareInfo.MergeBase, _, err = headGitRepo.GetMergeBase(tmpRemote, compareInfo.BaseCommitID, compareInfo.HeadCommitID)
-	if err != nil {
-		return nil, fmt.Errorf("GetMergeBase: %w", err)
-	}
-
-	// We have a common base - therefore we know that ... should work
-	if !fileOnly {
-		compareInfo.Commits, err = headGitRepo.ShowPrettyFormatLogToList(ctx, compareInfo.BaseCommitID+compareInfo.Separator()+compareInfo.HeadCommitID)
+	// FIXME: It seems we don't need mergebase if it's a direct comparison?
+	compareInfo.MergeBase, remoteBranch, err = headGitRepo.GetMergeBase(tmpRemote, baseRef.String(), headRef.String())
+	if err == nil {
+		compareInfo.BaseCommitID, err = gitrepo.GetFullCommitID(ctx, headRepo, remoteBranch)
 		if err != nil {
-			return nil, fmt.Errorf("ShowPrettyFormatLogToList: %w", err)
+			compareInfo.BaseCommitID = remoteBranch
+		}
+		separator := "..."
+		baseCommitID := compareInfo.MergeBase
+		if directComparison {
+			separator = ".."
+			baseCommitID = compareInfo.BaseCommitID
+		}
+
+		// We have a common base - therefore we know that ... should work
+		if !fileOnly {
+			compareInfo.Commits, err = headGitRepo.ShowPrettyFormatLogToList(ctx, baseCommitID+separator+headRef.String())
+			if err != nil {
+				return nil, fmt.Errorf("ShowPrettyFormatLogToList: %w", err)
+			}
+		} else {
+			compareInfo.Commits = []*git.Commit{}
 		}
 	} else {
 		compareInfo.Commits = []*git.Commit{}
+		compareInfo.MergeBase, err = gitrepo.GetFullCommitID(ctx, headRepo, remoteBranch)
+		if err != nil {
+			compareInfo.MergeBase = remoteBranch
+		}
+		compareInfo.BaseCommitID = compareInfo.MergeBase
 	}
 
 	// Count number of changed files.
 	// This probably should be removed as we need to use shortstat elsewhere
 	// Now there is git diff --shortstat but this appears to be slower than simply iterating with --nameonly
-	compareInfo.NumFiles, err = headGitRepo.GetDiffNumChangedFiles(compareInfo.BaseCommitID, compareInfo.HeadCommitID, directComparison)
+	compareInfo.NumFiles, err = headGitRepo.GetDiffNumChangedFiles(remoteBranch, headRef.String(), directComparison)
 	if err != nil {
 		return nil, err
 	}
