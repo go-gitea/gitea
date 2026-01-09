a0fadc0e0286f367f0bdf16f1da67353d400f126
Lunny Xiao <xiaolunwen@gmail.com>
2026-01-09 15:04:15 -0800

    Add tests

--- routers/web/org/projects_test.go
+++ routers/web/org/projects_test.go
@@ -4,11 +4,14 @@
 package org_test
 
 import (
+	"net/http"
 	"testing"
 
 	"code.gitea.io/gitea/models/unittest"
+	"code.gitea.io/gitea/modules/web"
 	"code.gitea.io/gitea/routers/web/org"
 	"code.gitea.io/gitea/services/contexttest"
+	"code.gitea.io/gitea/services/forms"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -25,4 +28,62 @@ func TestCheckProjectColumnChangePermissions(t *testing.T) {
 	assert.NotNil(t, project)
 	assert.NotNil(t, column)
 	assert.False(t, ctx.Written())
+}
+
+func TestChangeProjectStatusRejectsForeignProjects(t *testing.T) {
+	unittest.PrepareTestEnv(t)
+	// project 4 is owned by user2 not user1
+	ctx, resp := contexttest.MockContext(t, "user1/-/projects/4/close")
+	contexttest.LoadUser(t, ctx, 1)
+	ctx.ContextUser = ctx.Doer
+	ctx.SetPathParam("action", "close")
+	ctx.SetPathParam("id", "4")
+
+	org.ChangeProjectStatus(ctx)
+
+	assert.Equal(t, http.StatusUnprocessableEntity, ctx.Resp.WrittenStatus())
+	assert.Contains(t, resp.Body.String(), "not in Owner")
+}
+
+func TestChangeProjectStatusRejectsUnsupportedProjectTypes(t *testing.T) {
+	unittest.PrepareTestEnv(t)
+
+	// project 4 is owned by user2 but it is a project with type repository
+	ctx, resp := contexttest.MockContext(t, "user2/-/projects/4/close")
+	contexttest.LoadUser(t, ctx, 2)
+	ctx.ContextUser = ctx.Doer
+	ctx.SetPathParam("action", "close")
+	ctx.SetPathParam("id", "4")
+
+	org.ChangeProjectStatus(ctx)
+
+	assert.Equal(t, http.StatusUnprocessableEntity, ctx.Resp.WrittenStatus())
+	assert.Contains(t, resp.Body.String(), "not of a type")
+}
+
+func TestAddColumnToProjectPostRejectsForeignProjects(t *testing.T) {
+	unittest.PrepareTestEnv(t)
+	ctx, _ := contexttest.MockContext(t, "user1/-/projects/4/columns/new")
+	contexttest.LoadUser(t, ctx, 1)
+	ctx.ContextUser = ctx.Doer
+	ctx.SetPathParam("id", "4")
+	web.SetForm(ctx, &forms.EditProjectColumnForm{Title: "foreign"})
+
+	org.AddColumnToProjectPost(ctx)
+
+	assert.Equal(t, http.StatusUnprocessableEntity, ctx.Resp.WrittenStatus())
+}
+
+func TestAddColumnToProjectPostRejectsUnsupportedProjectTypes(t *testing.T) {
+	unittest.PrepareTestEnv(t)
+
+	ctx, _ := contexttest.MockContext(t, "user2/-/projects/4/columns/new")
+	contexttest.LoadUser(t, ctx, 2)
+	ctx.ContextUser = ctx.Doer
+	ctx.SetPathParam("id", "4")
+	web.SetForm(ctx, &forms.EditProjectColumnForm{Title: "unsupported"})
+
+	org.AddColumnToProjectPost(ctx)
+
+	assert.Equal(t, http.StatusUnprocessableEntity, ctx.Resp.WrittenStatus())
 }
