c7a3b81106679b7a0f558b7ad053a5c71eae74db
Pascal Zimmermann <pascal.zimmermann@theiotstudio.com>
2026-01-21 09:32:59 +0100

    fix: Remove MatrixID and Capacity functionality

--- models/migrations/v1_26/v325.go
+++ models/migrations/v1_26/v325.go
@@ -7,18 +7,9 @@ import (
 	"xorm.io/xorm"
 )
 
-func AddRunnerCapacityAndJobMaxParallel(x *xorm.Engine) error {
-	type ActionRunner struct {
-		Capacity int `xorm:"NOT NULL DEFAULT 1"`
-	}
-
+func AddJobMaxParallel(x *xorm.Engine) error {
 	type ActionRunJob struct {
-		MatrixID    string `xorm:"VARCHAR(255) INDEX"`
 		MaxParallel int
-	}
-
-	if err := x.Sync(new(ActionRunner)); err != nil {
-		return err
 	}
 
 	return x.Sync(new(ActionRunJob))
