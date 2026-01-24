c818b36fc5c2710c5145aab8b80f97b415ac3a18
silverwind <me@silverwind.io>
2026-01-24 02:16:17 +0100

    remove unneeded config

--- knip.config.ts
+++ knip.config.ts
@@ -2,6 +2,5 @@ import type {KnipConfig} from 'knip';
 
 export default {
   project: ['web_src/**/*.{ts,vue}'],
-  ignore: ['updates.config.ts'],
   exclude: ['dependencies', 'devDependencies'],
 } satisfies KnipConfig;
