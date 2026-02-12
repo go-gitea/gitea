95c4e57a9234f0ace8fe4c69ac17eff0e2faef56
Tyrone Yeh <siryeh@gmail.com>
2026-02-12 15:33:53 +0800

    unify DEFAULT_SHOW_FULL_NAME output in templates and dropdown

--- web_src/js/features/repo-issue-list.ts
+++ web_src/js/features/repo-issue-list.ts
@@ -138,8 +138,9 @@ function initDropdownUserRemoteSearch(el: Element) {
         // the content is provided by backend IssuePosters handler
         processedResults.length = 0;
         for (const item of resp.results) {
-          let nameHtml = html`<img class="ui avatar tw-align-middle" src="${item.avatar_link}" aria-hidden="true" alt width="20" height="20"><span class="gt-ellipsis">${item.username}</span>`;
-          if (item.full_name) nameHtml += html`<span class="search-fullname tw-ml-2">${item.full_name}</span>`;
+          let nameHtml = html`<img class="ui avatar tw-align-middle" src="${item.avatar_link}" aria-hidden="true" alt width="20" height="20"><span class="gt-ellipsis">`;
+          nameHtml += item.full_name ? html`<span class="search-fullname">${item.full_name}</span>` : item.username;
+          nameHtml += html`</span>`;
           if (selectedUsername.toLowerCase() === item.username.toLowerCase()) selectedUsername = item.username;
           processedResults.push({value: item.username, name: nameHtml});
         }
