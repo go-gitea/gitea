72e49f3d4249a651c0bd9320f5ae6182f03e2c34
rmawatson <rmawatson@hotmail.com>
2026-02-15 17:18:45 +0000

    removed ws

--- routers/api/actions/runner/interceptor.go
+++ routers/api/actions/runner/interceptor.go
@@ -51,10 +51,8 @@ var withRunner = connect.WithInterceptors(connect.UnaryInterceptorFunc(func(unar
 			if runner.Deleted > 0 && timeutil.TimeStampNow()-runner.Deleted > timeutil.TimeStamp(10) {
 				return nil, status.Error(codes.Internal, "Runner is deleted")
 			}
-
 			runner.LastActive = timeutil.TimeStampNow()
 			cols = append(cols, "last_active")
-
 		} else if runner.Deleted != 0 {
 			return nil, status.Error(codes.Internal, "Runner is deleted")
 		}
