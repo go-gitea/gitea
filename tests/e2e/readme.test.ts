6bc667d7d3975f9b817d2506f0adef0b2b518d04
silverwind <me@silverwind.io>
2026-02-20 04:11:30 +0100

    rename test

--- tests/e2e/readme.test.ts
+++ tests/e2e/readme.test.ts
@@ -2,7 +2,7 @@ import {env} from 'node:process';
 import {test, expect} from '@playwright/test';
 import {apiCreateRepo, apiDeleteRepo} from './utils.ts';
 
-test('README renders on repository page', async ({page}) => {
+test('repo readme', async ({page}) => {
   const repoName = `e2e-readme-${Date.now()}`;
   await apiCreateRepo(page.request, {name: repoName});
   await page.goto(`/${env.GITEA_TEST_E2E_USER}/${repoName}`);
