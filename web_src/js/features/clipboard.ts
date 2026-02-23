b428c269e7b0ccefb0b7ab55f63268b2054cb696
wxiaoguang <wxiaoguang@gmail.com>
2026-02-24 03:13:41 +0800

    fix

--- web_src/js/features/clipboard.ts
+++ web_src/js/features/clipboard.ts
@@ -6,7 +6,7 @@ const {copy_success, copy_error} = window.config.i18n;
 
 // Enable clipboard copy from HTML attributes. These properties are supported:
 // - data-clipboard-text: Direct text to copy
-// - data-clipboard-target: Holds a selector for a <input> or <textarea> whose content is copied
+// - data-clipboard-target: Holds a selector for an element. "value" of <input> or <textarea>, or "textContent" of other elements will be copied
 // - data-clipboard-text-type: When set to 'url' will convert relative to absolute urls
 export function initGlobalCopyToClipboardListener() {
   document.addEventListener('click', async (e) => {
@@ -17,7 +17,15 @@ export function initGlobalCopyToClipboardListener() {
 
     let text = target.getAttribute('data-clipboard-text');
     if (!text) {
-      text = document.querySelector<HTMLInputElement>(target.getAttribute('data-clipboard-target')!)?.value ?? null;
+      const textSelector = target.getAttribute('data-clipboard-target')!;
+      const textTarget = document.querySelector(textSelector)!;
+      if (textTarget.nodeName === 'INPUT' || textTarget.nodeName === 'TEXTAREA') {
+        text = (textTarget as HTMLInputElement).value;
+      } else if (textTarget.nodeName === 'DIV') {
+        text = textTarget.textContent;
+      } else {
+        throw new Error(`Unsupported element for clipboard target: ${textSelector}`);
+      }
     }
 
     if (text && target.getAttribute('data-clipboard-text-type') === 'url') {
