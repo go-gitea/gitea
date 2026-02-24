b3648a896dc3d935598997f3559ff279a8f5f6b3
silverwind <me@silverwind.io>
2026-02-24 07:06:28 +0100

    Apply suggestion from @silverwind
    Signed-off-by: silverwind <me@silverwind.io>

--- web_src/js/features/repo-issue-edit.ts
+++ web_src/js/features/repo-issue-edit.ts
@@ -61,7 +61,7 @@ async function tryOnEditContent(e: Event) {
       renderContent = newRenderContent;
 
       rawContent.textContent = comboMarkdownEditor.value();
-      # update "Copy Source" menu item disabled state
+      // update "Copy Source" menu item disabled state
       document.querySelector(`[data-clipboard-target="#${rawContent.id}"]`)?.classList.toggle('disabled', !comboMarkdownEditor.value());
 
       if (!commentContent.querySelector('.dropzone-attachments')) {
