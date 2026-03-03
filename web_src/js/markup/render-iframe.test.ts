e135dafa7abf75701165bc232cc67def11e4e66f
wxiaoguang <wxiaoguang@gmail.com>
2026-03-03 15:37:27 +0800

    fine tune

--- web_src/js/markup/render-iframe.test.ts
+++ web_src/js/markup/render-iframe.test.ts
@@ -13,39 +13,47 @@ test('safeLinkHref', () => {
   expect(safeLinkHref(null)).toBe('http://localhost:3000/null');
 });
 
-test('navigateToIframeLink', () => {
+describe('navigateToIframeLink', () => {
   const openSpy = vi.spyOn(window, 'open').mockImplementation(() => null);
   const assignSpy = vi.spyOn(window.location, 'assign').mockImplementation(() => undefined);
 
-  navigateToIframeLink('http://example.com', '_blank');
-  expect(openSpy).toHaveBeenCalledWith('http://example.com/', '_blank', 'noopener,noreferrer');
-  vi.clearAllMocks();
+  test('safe links', () => {
+    navigateToIframeLink('http://example.com', '_blank');
+    expect(openSpy).toHaveBeenCalledWith('http://example.com/', '_blank', 'noopener,noreferrer');
+    vi.clearAllMocks();
 
-  navigateToIframeLink('https://example.com', '_self');
-  expect(assignSpy).toHaveBeenCalledWith('https://example.com/');
-  vi.clearAllMocks();
+    navigateToIframeLink('https://example.com', '_self');
+    expect(assignSpy).toHaveBeenCalledWith('https://example.com/');
+    vi.clearAllMocks();
 
-  navigateToIframeLink('https://example.com', null);
-  expect(assignSpy).toHaveBeenCalledWith('https://example.com/');
-  vi.clearAllMocks();
+    navigateToIframeLink('https://example.com', null);
+    expect(assignSpy).toHaveBeenCalledWith('https://example.com/');
+    vi.clearAllMocks();
 
-  navigateToIframeLink('/path', '');
-  expect(assignSpy).toHaveBeenCalledWith('http://localhost:3000/path');
-  vi.clearAllMocks();
+    navigateToIframeLink('/path', '');
+    expect(assignSpy).toHaveBeenCalledWith('http://localhost:3000/path');
+    vi.clearAllMocks();
 
-  // input can be any type & any value, keep the same behavior as `window.location.href = 123`
-  navigateToIframeLink(123, {});
-  expect(assignSpy).toHaveBeenCalledWith('http://localhost:3000/123');
-  vi.clearAllMocks();
+    // input can be any type & any value, keep the same behavior as `window.location.href = 123`
+    navigateToIframeLink(123, {});
+    expect(assignSpy).toHaveBeenCalledWith('http://localhost:3000/123');
+    vi.clearAllMocks();
+  });
 
-  // eslint-disable-next-line no-script-url
-  navigateToIframeLink('javascript:void(0);', '_blank');
-  expect(openSpy).toHaveBeenCalledTimes(0);
-  expect(assignSpy).toHaveBeenCalledTimes(0);
-  vi.clearAllMocks();
+  test('unsafe links', () => {
+    window.location.href = 'http://localhost:3000/';
 
-  navigateToIframeLink('data:image/svg+xml;utf8,<svg></svg>', '');
-  expect(openSpy).toHaveBeenCalledTimes(0);
-  expect(assignSpy).toHaveBeenCalledTimes(0);
-  vi.clearAllMocks();
+    // eslint-disable-next-line no-script-url
+    navigateToIframeLink('javascript:void(0);', '_blank');
+    expect(openSpy).toHaveBeenCalledTimes(0);
+    expect(assignSpy).toHaveBeenCalledTimes(0);
+    expect(window.location.href).toBe('http://localhost:3000/');
+    vi.clearAllMocks();
+
+    navigateToIframeLink('data:image/svg+xml;utf8,<svg></svg>', '');
+    expect(openSpy).toHaveBeenCalledTimes(0);
+    expect(assignSpy).toHaveBeenCalledTimes(0);
+    expect(window.location.href).toBe('http://localhost:3000/');
+    vi.clearAllMocks();
+  });
 });
