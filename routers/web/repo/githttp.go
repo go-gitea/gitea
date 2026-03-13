dda0dff5500867fd727893f1801fbc2e29057a38
wxiaoguang <wxiaoguang@gmail.com>
2026-03-13 18:42:30 +0800

    remove duplicate EnvRepoID, it has already been set in DoerPushingEnvironment

--- routers/web/repo/githttp.go
+++ routers/web/repo/githttp.go
@@ -268,7 +268,6 @@ func httpBase(ctx *context.Context, optGitService ...string) *serviceHandler {
 	if !isPull {
 		// if not "pull", then must be "push", and doer must exist
 		environ = repo_module.DoerPushingEnvironment(ctx.Doer, repo, isWiki)
-		environ = append(environ, repo_module.EnvRepoID+fmt.Sprintf("=%d", repo.ID))
 	}
 
 	return &serviceHandler{serviceType, repo, isWiki, environ}
