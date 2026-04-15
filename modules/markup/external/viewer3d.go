76b22d9957792f62627128e6d4f1e6b95d09c0b7
wxiaoguang <wxiaoguang@gmail.com>
2026-04-15 19:31:43 +0800

    fine tune

--- modules/markup/external/viewer3d.go
+++ modules/markup/external/viewer3d.go
@@ -5,11 +5,10 @@ package external
 
 import (
 	"encoding/base64"
-	"fmt"
-	"html"
 	"io"
 	"path"
 
+	"code.gitea.io/gitea/modules/htmlutil"
 	"code.gitea.io/gitea/modules/markup"
 	"code.gitea.io/gitea/modules/public"
 	"code.gitea.io/gitea/modules/setting"
@@ -31,6 +30,16 @@ func (p *viewer3dRenderer) NeedPostProcess() bool {
 }
 
 func (p *viewer3dRenderer) FileNamePatterns() []string {
+	// TODO: the file extensions are ambiguous, even if the file name matches, it doesn't mean that the file is a 3D model
+	// There are some approaches to make it more accurate, but they are all complicated:
+	// A. Make backend know everything (detect a file is a 3D model or not)
+	// B. Let frontend renders to try render one by one
+	//
+	// If there would be more frontend renders in the future, we need to implement the "frontend" approach:
+	// 1. Make parent window collect the supported extensions of frontend renders
+	// 2. If the current file matches any extension, start the general iframe embedded render
+	//   * pass the file name & data via postMessage (iframe can't read the file due to different origin)
+	// 3. The iframe window calls the frontend renders one by one, and report the render result to parent by postMessage
 	return []string{
 		"*.3dm", "*.3ds", "*.3mf", "*.amf", "*.bim", "*.brep",
 		"*.dae", "*.fbx", "*.fcstd", "*.glb", "*.gltf",
@@ -46,7 +55,7 @@ func (p *viewer3dRenderer) SanitizerRules() []setting.MarkupSanitizerRule {
 func (p *viewer3dRenderer) GetExternalRendererOptions() (ret markup.ExternalRendererOptions) {
 	ret.SanitizerDisabled = true
 	ret.DisplayInIframe = true
-	ret.ContentSandbox = "allow-scripts"
+	ret.ContentSandbox = "allow-scripts allow-forms allow-modals allow-popups allow-downloads"
 	return ret
 }
 
@@ -56,24 +65,33 @@ func (p *viewer3dRenderer) Render(ctx *markup.RenderContext, input io.Reader, ou
 		return markup.RenderIFrame(ctx, &opts, output)
 	}
 
-	if _, err := fmt.Fprintf(output,
+	if _, err := htmlutil.HTMLPrintf(output,
 		`<!DOCTYPE html>
 <html>
-<head><meta charset="utf-8"><style>html,body{margin:0;height:100%%}#viewer{width:100%%;height:100%%}#viewer>div{text-align:center;padding:1em}</style></head>
-<body><div id="viewer"></div><script type="application/octet-stream" id="modelData" data-filename="%s">`,
-		html.EscapeString(path.Base(ctx.RenderOptions.RelativePath)),
+<head>
+	<meta charset="utf-8">
+	<style>
+		html,body{margin:0;height:100%%}
+		#viewer{width:100%%;height:100%%}
+		#viewer>div{text-align:center;padding:1em}
+	</style>
+</head>
+<body>
+	<div id="viewer"></div>
+	<script type="application/octet-stream" id="modelData" data-filename="%s">`,
+		path.Base(ctx.RenderOptions.RelativePath),
 	); err != nil {
 		return err
 	}
 
 	b64 := base64.NewEncoder(base64.StdEncoding, output)
-	if _, err := io.Copy(b64, io.LimitReader(input, setting.UI.MaxDisplayFileSize)); err != nil {
+	defer b64.Close()
+	_, err := io.Copy(b64, io.LimitReader(input, setting.UI.MaxDisplayFileSize))
+	if err != nil {
 		return err
 	}
-	if err := b64.Close(); err != nil {
-		return err
-	}
+	_ = b64.Close()
 
-	_, err := fmt.Fprintf(output, `</script><script type="module" src="%s"></script></body></html>`, public.AssetURI("js/viewer-3d.js"))
+	_, err = htmlutil.HTMLPrintf(output, `</script><script type="module" src="%s"></script></body></html>`, public.AssetURI("js/viewer-3d.js"))
 	return err
 }
