41e88cbcaa05124b338047db51263ff13b696ffe
wxiaoguang <wxiaoguang@gmail.com>
2026-04-17 21:03:38 +0800

    optimize

--- routers/web/org/teams.go
+++ routers/web/org/teams.go
@@ -22,6 +22,7 @@ import (
 	"code.gitea.io/gitea/modules/log"
 	"code.gitea.io/gitea/modules/setting"
 	"code.gitea.io/gitea/modules/templates"
+	"code.gitea.io/gitea/modules/util"
 	"code.gitea.io/gitea/modules/web"
 	shared_user "code.gitea.io/gitea/routers/web/shared/user"
 	"code.gitea.io/gitea/services/context"
@@ -54,33 +55,37 @@ func Teams(ctx *context.Context) {
 	ctx.Data["Title"] = org.FullName
 	ctx.Data["PageIsOrgTeams"] = true
 
-	keyword := ctx.FormTrim("q")
+	keyword := strings.TrimSpace(ctx.FormTrim("q"))
 	page := max(ctx.FormInt("page"), 1)
+	pagingNum := setting.UI.MembersPagingNum
 
-	opts := &org_model.SearchTeamOptions{
-		OrgID: org.ID,
-		ListOptions: db.ListOptions{
-			Page:     page,
-			PageSize: setting.UI.MembersPagingNum,
-		},
+	searchTeams := func() (teams []*org_model.Team, count int64, err error) {
+		if keyword == "" {
+			// fast path, use existing teams in context if no need to filter from database
+			count = int64(len(ctx.Org.Teams))
+			start := (page - 1) * pagingNum
+			if start > len(ctx.Org.Teams) {
+				return nil, count, nil
+			}
+			end := min(start+pagingNum, len(ctx.Org.Teams))
+			return ctx.Org.Teams[start:end], count, nil
+		}
+
+		shouldSeeAllOrgTeams, err := context.UserShouldSeeAllOrgTeams(ctx)
+		if err != nil {
+			return nil, 0, err
+		}
+		opts := &org_model.SearchTeamOptions{
+			OrgID:       org.ID,
+			UserID:      util.Iif(shouldSeeAllOrgTeams, 0, ctx.Doer.ID),
+			Keyword:     keyword,
+			IncludeDesc: true,
+			ListOptions: db.ListOptions{Page: page, PageSize: pagingNum},
+		}
+		return org_model.SearchTeam(ctx, opts)
 	}
 
-	shouldSeeAllOrgTeams, err := context.UserShouldSeeAllOrgTeams(ctx)
-	if err != nil {
-		ctx.ServerError("UserShouldSeeAllOrgTeams", err)
-		return
-	}
-
-	if !shouldSeeAllOrgTeams {
-		opts.UserID = ctx.Doer.ID
-	}
-
-	if keyword != "" {
-		opts.Keyword = keyword
-		opts.IncludeDesc = true
-	}
-
-	teams, count, err := org_model.SearchTeam(ctx, opts)
+	teams, count, err := searchTeams()
 	if err != nil {
 		ctx.ServerError("SearchTeam", err)
 		return
@@ -92,6 +97,7 @@ func Teams(ctx *context.Context) {
 			return
 		}
 	}
+
 	ctx.Data["OrgListTeams"] = teams
 	ctx.Data["Keyword"] = keyword
 	pager := context.NewPagination(count, setting.UI.MembersPagingNum, page, 5)
