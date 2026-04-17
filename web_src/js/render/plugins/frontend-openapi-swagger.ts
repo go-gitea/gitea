78e11b3e450fbabbab7b3806a610305262ba8390
wxiaoguang <wxiaoguang@gmail.com>
2026-04-17 12:49:44 +0800

    add more comments

--- web_src/js/render/plugins/frontend-openapi-swagger.ts
+++ web_src/js/render/plugins/frontend-openapi-swagger.ts
@@ -1,10 +1,12 @@
 import type {FrontendRenderFunc} from '../plugin.ts';
+import {initSwaggerUI} from '../swagger.ts';
 
 // HINT: SWAGGER-CSS-IMPORT: this import is also necessary when swagger is used as a frontend external render
+// It must be on top-level, doesn't work in a function
+// Static import doesn't work (it needs to use manifest.json to manually add the CSS file)
 await import('../../../css/swagger.css');
 
 export const frontendRender: FrontendRenderFunc = async (opts): Promise<boolean> => {
-  const {initSwaggerUI} = await import('../swagger.ts');
   try {
     await initSwaggerUI(opts.container, {specText: opts.contentString()});
     return true;
--- web_src/js/render/plugins/inplace-pdf-viewer.ts
+++ web_src/js/render/plugins/inplace-pdf-viewer.ts
@@ -11,6 +11,7 @@ export function newInplacePluginPdfViewer(): InplaceRenderPlugin {
     async render(container: HTMLElement, fileUrl: string): Promise<void> {
       const PDFObject = await import('pdfobject');
       // TODO: the PDFObject library does not support dynamic height adjustment,
+      // TODO: it seems that this render must be an inplace render, because the URL must be accessible from the current context
       container.style.height = `${window.innerHeight - 100}px`;
       if (!PDFObject.default.embed(fileUrl, container)) {
         throw new Error('Unable to render the PDF file');
