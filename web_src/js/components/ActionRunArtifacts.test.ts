7d7a2aa6d46aef8f9fead451d8d4c61a22ffbaa3
wxiaoguang <wxiaoguang@gmail.com>
2026-04-19 14:19:02 +0800

    cover one more edge case

--- web_src/js/components/ActionRunArtifacts.test.ts
+++ web_src/js/components/ActionRunArtifacts.test.ts
@@ -8,14 +8,14 @@ describe('buildArtifactTooltipHtml', () => {
       size: 1024 * 1024,
       status: 'completed',
       expiresUnix: Date.UTC(2026, 2, 20, 12, 0, 0) / 1000,
-    }, 'Expires at %s');
+    }, 'Expires at %s (extra)');
 
     expect(normalizeTestHtml(result)).toBe(normalizeTestHtml(`<span class="flex-text-inline">
 <span>Expires at </span>
   <relative-time datetime="2026-03-20T12:00:00.000Z" threshold="P0Y" prefix="" weekday="" year="numeric" month="short" hour="numeric" minute="2-digit">
     2026-03-20T12:00:00.000Z
   </relative-time>
-  <span></span>
+  <span> (extra)</span>
   <span class="inline-divider">,</span>
   <span>1.0 MiB</span>
 </span>
