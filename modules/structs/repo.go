f95b359c30996681276c50f56390cb9243bec540
pomidorry <marymarusya0@gmail.com>
2026-05-06 22:14:42 +0300

    linter fix

--- modules/structs/repo.go
+++ modules/structs/repo.go
@@ -125,12 +125,12 @@ type Repository struct {
 	// ObjectFormatName of the underlying git repository
 	ObjectFormatName ObjectFormatName `json:"object_format_name"`
 	// swagger:strfmt date-time
-	MirrorUpdated time.Time     `json:"mirror_updated"`
+	MirrorUpdated time.Time `json:"mirror_updated"`
 	// swagger:strfmt date-time
-	LastPullSyncSuccess time.Time `json:"last_pull_sync_success"`
-	RepoTransfer  *RepoTransfer `json:"repo_transfer,omitempty"`
-	Topics        []string      `json:"topics"`
-	Licenses      []string      `json:"licenses"`
+	LastPullSyncSuccess time.Time     `json:"last_pull_sync_success"`
+	RepoTransfer        *RepoTransfer `json:"repo_transfer,omitempty"`
+	Topics              []string      `json:"topics"`
+	Licenses            []string      `json:"licenses"`
 }
 
 // CreateRepoOption options when creating repository
