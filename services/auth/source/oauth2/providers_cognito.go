94a0bc348525e9469404809344e40b9339b42413
Tom Thornton <tom.thornton@sony.com>
2026-05-08 17:46:37 +0100

    refactored to reduce duplication

--- services/auth/source/oauth2/providers_cognito.go
+++ services/auth/source/oauth2/providers_cognito.go
@@ -3,22 +3,9 @@
 
 package oauth2
 
-import (
-	"html/template"
-
-	"code.gitea.io/gitea/modules/log"
-	"code.gitea.io/gitea/modules/setting"
-	"code.gitea.io/gitea/modules/svg"
-
-	"github.com/markbates/goth"
-	"github.com/markbates/goth/providers/openidConnect"
-)
-
-// CognitoProvider is a GothProvider for AWS Cognito
-type CognitoProvider struct{}
-
-func (c *CognitoProvider) SupportSSHPublicKey() bool {
-	return true
+// CognitoProvider is a GothProvider for AWS Cognito (based on OpenID Connect)
+type CognitoProvider struct {
+	OpenIDProvider
 }
 
 // Name provides the technical name for this provider
@@ -29,36 +16,6 @@ func (c *CognitoProvider) Name() string {
 // DisplayName returns the friendly name for this provider
 func (c *CognitoProvider) DisplayName() string {
 	return "AWS Cognito"
-}
-
-// IconHTML returns icon HTML for this provider
-func (c *CognitoProvider) IconHTML(size int) template.HTML {
-	return svg.RenderHTML("gitea-openid", size)
-}
-
-// CreateGothProvider creates a GothProvider from this Provider
-func (c *CognitoProvider) CreateGothProvider(providerName, callbackURL string, source *Source) (goth.Provider, error) {
-	scopes := setting.OAuth2Client.OpenIDConnectScopes
-	if len(scopes) == 0 {
-		scopes = append(scopes, source.Scopes...)
-	}
-
-	provider, err := openidConnect.New(source.ClientID, source.ClientSecret, callbackURL, source.OpenIDConnectAutoDiscoveryURL, scopes...)
-	if err != nil {
-		log.Warn("Failed to create AWS Cognito Provider with name '%s' with url '%s': %v", providerName, source.OpenIDConnectAutoDiscoveryURL, err)
-		return nil, err
-	}
-	if source.ExternalIDClaim != "" {
-		// UserIdClaims is a fallback list; goth returns the first non-empty matching claim.
-		// A single entry is sufficient because the admin explicitly chooses one claim (e.g. "sub" for Cognito).
-		provider.UserIdClaims = []string{source.ExternalIDClaim}
-	}
-	return provider, nil
-}
-
-// CustomURLSettings returns the custom url settings for this provider
-func (c *CognitoProvider) CustomURLSettings() *CustomURLSettings {
-	return nil
 }
 
 var _ GothProvider = &CognitoProvider{}
