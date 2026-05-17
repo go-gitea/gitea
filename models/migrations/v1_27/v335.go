8baeb3385f634669ea59d3b89c8c01d275dd2534
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2026-05-17 15:56:10 -0400

    refactor: update function signature for repo group migration

--- models/migrations/v1_27/v335.go
+++ models/migrations/v1_27/v335.go
@@ -5,18 +5,19 @@ package v1_27
 
 import (
 	"code.gitea.io/gitea/models/db"
-
 	"xorm.io/xorm"
 )
 
-func AddCancellingSupportToActionRunner(x db.EngineMigration) error {
-	type ActionRunner struct {
-		HasCancellingSupport bool `xorm:"has_cancelling_support NOT NULL DEFAULT false"`
+func AddGroupColumnsToRepositoryTable(x db.EngineMigration) error {
+	type Repository struct {
+		LowerName      string `xorm:"UNIQUE(s) UNIQUE(g) INDEX NOT NULL"`
+		GroupID        int64  `xorm:"UNIQUE(s) INDEX DEFAULT 0"`
+		OwnerID        int64  `xorm:"UNIQUE(s) UNIQUE(g) index"`
+		GroupSortOrder int
 	}
-
 	_, err := x.SyncWithOptions(xorm.SyncOptions{
-		IgnoreConstrains:  true,
-		IgnoreDropIndices: true,
-	}, new(ActionRunner))
+		IgnoreConstrains: false,
+		IgnoreIndices:    false,
+	}, new(Repository))
 	return err
 }
