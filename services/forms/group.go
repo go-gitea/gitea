1e91d51f6f44d11bf93be7d4c75fbd92926cd68e
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2026-05-22 18:10:34 -0400

    feat: add structs describing group-related forms

---
+++ services/forms/group.go
@@ -0,0 +1,33 @@
+// Copyright 2025 The Gitea Authors. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+package forms
+
+import "code.gitea.io/gitea/modules/structs"
+
+// CreateGroupForm form for creating a repository group
+type CreateGroupForm struct {
+	GroupName          string `binding:"Required;MaxSize(255)"`
+	Description        string `binding:"MaxSize(2048)"`
+	Permission         string
+	CanCreateGroupRepo bool
+	ParentGroupID      int64
+}
+
+type MovedGroupItemForm struct {
+	IsGroup   bool  `json:"isGroup"`
+	ItemID    int64 `json:"id"`
+	NewParent int64 `json:"newParent"`
+	NewPos    int   `json:"newPos"`
+}
+type CreateGroupTeamForm struct {
+	Permission              string
+	Access                  string
+	CanCreateRepoOrSubGroup bool
+}
+
+type UpdateGroupSettingForm struct {
+	Name        string `binding:"Required;MaxSize(255)" locale:"group.group_name_holder"`
+	Description string `binding:"MaxSize(2048)"`
+	Visibility  structs.VisibleType
+}
