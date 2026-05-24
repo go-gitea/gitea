d2c952008bc4f75dee6e6aedf2de901812a85167
Alexandre Bontems <alexandre.bontems@gmail.com>
2026-05-24 16:21:20 +0200

    feat: use tippy instead of modal

--- web_src/js/features/repo-watch-options.ts
+++ web_src/js/features/repo-watch-options.ts
@@ -1,35 +1,20 @@
-import {fomanticQuery} from '../modules/fomantic/base.ts';
+// import {fomanticQuery} from '../modules/fomantic/base.ts';
+import {createTippy} from '../modules/tippy.ts';
 import {registerGlobalInitFunc} from '../modules/observer.ts';
-import {submitFormFetchAction} from './common-fetch-action.ts';
+// import {submitFormFetchAction} from './common-fetch-action.ts';
 
 export function initRepoWatchOptions() {
-  const elModal = document.querySelector<HTMLElement>('#repo-watch-options-modal');
-  if (!elModal) return;
-
-  const elPullRequests = elModal.querySelector<HTMLInputElement>('[name="pull_requests"]')!;
-  const elIssues = elModal.querySelector<HTMLInputElement>('[name="issues"]')!;
-  const elReleases = elModal.querySelector<HTMLInputElement>('[name="releases"]')!;
-
-  const showModal = (btn: HTMLElement) => {
-    const form = elModal.querySelector<HTMLFormElement>('form')!;
-    elPullRequests.checked = btn.getAttribute('data-watch-pull-requests') === 'true';
-    elIssues.checked = btn.getAttribute('data-watch-issues') === 'true';
-    elReleases.checked = btn.getAttribute('data-watch-releases') === 'true';
-    form.action = `${btn.getAttribute('data-repo-link')!}/action/watch/options`;
-
-    fomanticQuery(elModal).modal({
-      autofocus: false,
-      onApprove() {
-        submitFormFetchAction(form);
-        return false;
-      },
-    }).modal('show');
-  };
-
   registerGlobalInitFunc('initWatchOptions', (el: HTMLElement) => {
-    el.addEventListener('click', (e) => {
-      e.preventDefault();
-      showModal(el);
+    const popup = el.lastElementChild!;
+    if (!popup.matches('.tippy-target')) throw new Error('Expected last child to be a tippy target');
+    createTippy(el, {
+      content: popup,
+      placement: 'bottom-end',
+      trigger: 'click',
+      interactive: true,
+      hideOnClick: true,
+      role: 'dialog',
+      theme: 'default',
     });
   });
 }
