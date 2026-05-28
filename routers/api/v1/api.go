66e09f27dfdcdbc46c45ade49f205372031c4cb8
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2026-05-28 18:33:23 -0400

    refactor: allow subgroups to be created in personal accounts

--- routers/api/v1/api.go
+++ routers/api/v1/api.go
@@ -1297,6 +1297,10 @@ func Routes() *web.Router {
 			m.Combo("/repos", tokenRequiresScopes(auth_model.AccessTokenScopeCategoryRepository)).Get(user.ListMyRepos).
 				Post(rejectPublicOnly(), bind(api.CreateRepoOption{}), repo.Create)
 
+			m.Group("/groups", func() {
+				m.Get("", group.GetUserGroups)
+				m.Post("/new", rejectPublicOnly(), bind(api.NewGroupOption{}), group.NewUserGroup)
+			})
 			// (repo scope)
 			m.Group("/starred", func() {
 				m.Get("", user.GetMyStarredRepos)
@@ -1887,8 +1891,8 @@ func Routes() *web.Router {
 				})
 			}, reqToken(), reqOrgOwnership())
 			m.Group("/groups", func() {
-				m.Get("", org.GetOrgGroups)
-				m.Post("/new", reqToken(), reqOrgAdmin(), bind(api.NewGroupOption{}), group.NewGroup)
+				m.Get("", group.GetOrgGroups)
+				m.Post("/new", reqToken(), reqOrgAdmin(), bind(api.NewGroupOption{}), group.NewOrgGroup)
 			})
 		}, tokenRequiresScopes(auth_model.AccessTokenScopeCategoryOrganization), orgAssignment(true), checkTokenPublicOnly())
 		m.Group("/teams/{teamid}", func() {
