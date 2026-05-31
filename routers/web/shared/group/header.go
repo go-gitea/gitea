96b7b7e1bd483e4624c9e0ab01de8088585bf7e6
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2026-05-31 18:26:36 -0400

    refactor: update `LoadHeaderCount` func for groups
    make it so we also load the owner's header values if the owner is an individual

--- routers/web/shared/group/header.go
+++ routers/web/shared/group/header.go
@@ -7,10 +7,16 @@ import (
 	repo_model "gitea.dev/models/repo"
 	"gitea.dev/modules/optional"
 	"gitea.dev/modules/setting"
+	"gitea.dev/routers/web/shared/user"
 	"gitea.dev/services/context"
 )
 
 func LoadHeaderCount(ctx *context.Context) error {
+	if ctx.RepoGroup.Group.Owner.IsIndividual() {
+		if _, err := user.RenderUserOrgHeader(ctx); err != nil {
+			return err
+		}
+	}
 	repoCount, err := repo_model.CountRepository(ctx, repo_model.SearchRepoOptions{
 		Actor:              ctx.Doer,
 		Private:            ctx.IsSigned,
