7b992f09c8c19667e4023b8613646f920b7b6f86
wxiaoguang <wxiaoguang@gmail.com>
2026-06-07 13:03:48 +0800

    fix lint

--- modules/markup/markdown/transform_codespan.go
+++ modules/markup/markdown/transform_codespan.go
@@ -68,7 +68,7 @@ func cssColorHandler(value string) bool {
 }
 
 func (g *ASTTransformer) transformCodeSpan(_ *markup.RenderContext, v *ast.CodeSpan, reader text.Reader) {
-	colorContent := v.Text(reader.Source()) //nolint:staticcheck // Text is deprecated
+	colorContent := v.Text(reader.Source())
 	if cssColorHandler(string(colorContent)) {
 		v.AppendChild(v, NewColorPreview(colorContent))
 	}
