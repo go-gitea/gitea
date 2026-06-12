9ba263d6e03186d9df06b2bee3f86d190c64e99b
wxiaoguang <wxiaoguang@gmail.com>
2026-06-12 09:33:20 +0800

    fine tune

--- web_src/js/modules/fomantic/dropdown.test.ts
+++ web_src/js/modules/fomantic/dropdown.test.ts
@@ -2,16 +2,15 @@ import '../../../fomantic/build/fomantic.js';
 import {createElementFromHTML} from '../../utils/dom.ts';
 import {hideScopedEmptyDividers} from './dropdown.ts';
 
-test('dropdown keeps the literal "false" choice text selected', () => {
+test('dropdown-item-literal-text', () => {
   // a "choice" workflow_dispatch input can offer the string "false" as an option.
   // jQuery `.data()` would coerce `data-text="false"` to the boolean `false`, which then renders as empty text.
-  const select = createElementFromHTML<HTMLSelectElement>(`<select class="ui dropdown">
+  const $dropdown = $(`<select class="ui dropdown">
     <option value="1">1</option>
     <option value="0">0</option>
     <option value="true">true</option>
     <option value="false">false</option>
-  </select>`);
-  const $dropdown = $(select).dropdown();
+  </select>`).dropdown();
   for (const value of ['1', '0', 'true', 'false']) {
     $dropdown.dropdown('set selected', value);
     expect($dropdown.dropdown('get text')).toEqual(value);
