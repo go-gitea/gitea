f6ef5b86de0a97e4271e483beb8908e9788d1dce
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2026-06-13 22:13:48 -0400

    refactor: update group reordering web handler
    add data containing new full names/paths of moved item to response. this will allow the frontend to update each sortable item's link, so the urls remain working

--- routers/web/group/edit.go
+++ routers/web/group/edit.go
@@ -4,11 +4,26 @@
 package group
 
 import (
+	"net/http"
+
+	group_model "gitea.dev/models/group"
+	repo_model "gitea.dev/models/repo"
 	"gitea.dev/modules/json"
 	"gitea.dev/services/context"
 	"gitea.dev/services/forms"
 	group_service "gitea.dev/services/group"
 )
+
+type movedSubItem struct {
+	ID       int64  `json:"id"`
+	IsGroup  bool   `json:"isGroup"`
+	NewPath  string `json:"newPath"`
+	FullName string `json:"fullName,omitempty"`
+}
+type moveResult struct {
+	NewPath  string         `json:"newPath"`
+	FullName string         `json:"fullName,omitempty"`
+}
 
 func MoveGroupItem(ctx *context.Context) {
 	form := &forms.MovedGroupItemForm{}
@@ -25,5 +40,25 @@ func MoveGroupItem(ctx *context.Context) {
 		ctx.ServerError("MoveGroupItem", err)
 		return
 	}
-	ctx.JSONOK()
+	var newPath, fullName string
+	if form.IsGroup {
+		grp, err := group_model.GetGroupByID(ctx, form.ItemID)
+		if err != nil {
+			ctx.ServerError("GetGroupByID", err)
+			return
+		}
+		newPath = grp.GroupLink()
+	} else {
+		repo, err := repo_model.GetRepositoryByID(ctx, form.ItemID)
+		if err != nil {
+			ctx.ServerError("GetRepositoryByID", err)
+			return
+		}
+		fullName = repo.FullName()
+		newPath = repo.Link()
+	}
+	ctx.JSON(http.StatusOK, moveResult{
+		NewPath:  newPath,
+		FullName: fullName,
+	})
 }
