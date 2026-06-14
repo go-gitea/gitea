c70d3b9c22460e3390a8102613f1e6f94738002c
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2026-06-14 00:53:51 -0400

    refactor: ensure that `UpdateGroupSettingForm` uses same max length as other forms

--- services/forms/group.go
+++ services/forms/group.go
@@ -28,7 +28,7 @@ type CreateGroupTeamForm struct {
 }
 
 type UpdateGroupSettingForm struct {
-	Name          string `binding:"Required;MaxSize(255)" locale:"group.group_name_holder"`
+	Name          string `binding:"Required;MaxSize(100)" locale:"group.group_name_holder"`
 	ParentGroupID int64  `binding:"Required"`
 	Description   string `binding:"MaxSize(2048)"`
 	Visibility    structs.VisibleType
