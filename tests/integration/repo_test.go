81c3f9e528433763d3942199ae2c11437862caaf
wxiaoguang <wxiaoguang@gmail.com>
2026-07-20 20:37:44 +0800

    fix lint

--- tests/integration/repo_test.go
+++ tests/integration/repo_test.go
@@ -1,6 +1,7 @@
 // Copyright 2017 The Gitea Authors. All rights reserved.
 // SPDX-License-Identifier: MIT
 
+//nolint:govet // disable "composites: gitea.dev/modules/git.FastImportFile struct literal uses unkeyed fields"
 package integration
 
 import (
