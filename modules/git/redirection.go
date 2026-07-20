ad2b1985cc207016c5ed52afc482af47966fc714
wxiaoguang <wxiaoguang@gmail.com>
2026-07-20 17:17:50 +0800

    fix

--- modules/git/redirection.go
+++ modules/git/redirection.go
@@ -11,4 +11,5 @@ func HandleGitCmdHTTPRedirection(cmd *gitcmd.Command, targets ...string) {
 	// However, we can't do so at the moment:
 	// this fails due to 301: git -c http.followRedirects=false clone -v https://gitlab.com/{owner}/{repo}
 	// this succeeds: git -c http.followRedirects=false clone -v https://gitlab.com/{owner}/{repo}.git
+	// FIXME: GIT-CLONE-HTTP-REDIRECT-SSRF: need a complete solution in the future
 }
