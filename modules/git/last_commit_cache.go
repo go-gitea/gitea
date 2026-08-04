07183c726d1f49979a4037075e0f0fc7428b504a
wxiaoguang <wxiaoguang@gmail.com>
2026-08-04 12:35:27 +0800

    clarify var name

--- modules/git/last_commit_cache.go
+++ modules/git/last_commit_cache.go
@@ -31,45 +31,43 @@ func (c *LastCommitCache) Put(ref, entryPath, commitID string) error {
 
 // Get gets the last commit information by commit id and entry path
 func (c *LastCommitCache) Get(ctx context.Context, ref, entryPath string) (*Commit, error) {
-	commitID, ok := c.cache.Get(getCacheKey(c.repo, ref, entryPath))
-	if !ok || commitID == "" {
+	lastCommitID, ok := c.cache.Get(getCacheKey(c.repo, ref, entryPath))
+	if !ok || lastCommitID == "" {
 		return nil, nil //nolint:nilnil // return nil when cache miss
 	}
 
-	log.Debug("LastCommitCache hit level 1: [%s:%s:%s]", ref, entryPath, commitID)
-	if c.commitCache != nil {
-		if commit, ok := c.commitCache[commitID]; ok {
-			log.Debug("LastCommitCache hit level 2: [%s:%s:%s]", ref, entryPath, commitID)
-			return commit, nil
-		}
+	log.Debug("LastCommitCache hit level 1: [%s:%s:%s]", ref, entryPath, lastCommitID)
+	if lastCommit, ok := c.commitCache[lastCommitID]; ok {
+		log.Debug("LastCommitCache hit level 2: [%s:%s:%s]", ref, entryPath, lastCommitID)
+		return lastCommit, nil
 	}
 
-	commit, err := c.repo.GetCommit(ctx, commitID)
+	lastCommit, err := c.repo.GetCommit(ctx, lastCommitID)
 	if err != nil {
 		return nil, err
 	}
 	if c.commitCache == nil {
 		c.commitCache = make(map[string]*Commit)
 	}
-	c.commitCache[commitID] = commit
-	return commit, nil
+	c.commitCache[lastCommitID] = lastCommit
+	return lastCommit, nil
 }
 
 // GetCommitByPath gets the last commit for the entry in the provided commit
-func (c *LastCommitCache) GetCommitByPath(ctx context.Context, commitID ObjectID, entryPath string) (*Commit, error) {
-	commitIDStr := commitID.String()
-	lastCommit, err := c.Get(ctx, commitIDStr, entryPath)
+func (c *LastCommitCache) GetCommitByPath(ctx context.Context, entryCommitID ObjectID, entryPath string) (*Commit, error) {
+	entryCommitIDStr := entryCommitID.String()
+	lastCommit, err := c.Get(ctx, entryCommitIDStr, entryPath)
 	if err != nil || lastCommit != nil {
 		return lastCommit, err
 	}
 
-	lastCommit, err = c.repo.getCommitByPathWithID(ctx, commitID, entryPath)
+	lastCommit, err = c.repo.getCommitByPathWithID(ctx, entryCommitID, entryPath)
 	if err != nil {
 		return nil, err
 	}
 
-	if err := c.Put(commitIDStr, entryPath, lastCommit.ID.String()); err != nil {
-		log.Error("Unable to cache %s as the last commit for %q in %s %s. Error %v", lastCommit.ID.String(), entryPath, commitID, c.repo.LogString(), err)
+	if err := c.Put(entryCommitIDStr, entryPath, lastCommit.ID.String()); err != nil {
+		log.Error("Unable to cache %s as the last commit for %q in %s %s. Error %v", lastCommit.ID.String(), entryPath, entryCommitID, c.repo.LogString(), err)
 	}
 
 	return lastCommit, nil
