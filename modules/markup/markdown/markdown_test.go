0911889013affff6d36e44e0341b8809be8ea2ec
wxiaoguang <wxiaoguang@gmail.com>
2026-08-06 16:05:08 +0800

    fix tests

--- modules/markup/markdown/markdown_test.go
+++ modules/markup/markdown/markdown_test.go
@@ -618,6 +618,5 @@ func TestMarkdownCodeBlock(t *testing.T) {
 	testRender("```js:app.ts\ncode\n```", jsCommon)
 	testRender("```js,ignore\ncode\n```", jsCommon)
 	testRender("```js ignore\ncode\n```", jsCommon)
-	testRender("    code\n", prefix+`<code>code`+nl+`</code>`+suffix)
-	testRender("    <script>alert(1)</script>\n", prefix+`<code>&lt;script&gt;alert(1)&lt;/script&gt;`+nl+`</code>`+suffix)
+	testRender("    <any&content>\n", prefix+`<code class="chroma language-text">&lt;any&amp;content&gt;`+nl+`</code>`+suffix)
 }
