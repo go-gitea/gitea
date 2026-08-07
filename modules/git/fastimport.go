2657756cacd9f9685f7a7bd029779b6c7fe40aa0
water <672684719@qq.com>
2026-08-07 22:11:03 +0800

    enhance: fall back to DEFAULT_TEMPLATE.md when style-specific template is missing (#38803)
    Closes #38801
    
    Introduce `DEFAULT_TEMPLATE.md` as the default message for all merge
    styles.
    
    https://gitea.com/gitea/docs/pulls/491
    
    By the way, fix incorrect os.Expand usage for merge message & repo
    template
    
    ---------
    
    Co-authored-by: waterWang <waterWang@users.noreply.github.com>
    Co-authored-by: wxiaoguang <wxiaoguang@gmail.com>

--- modules/git/fastimport.go
+++ modules/git/fastimport.go
@@ -7,10 +7,17 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"os"
 
 	"gitea.dev/modules/git/gitcmd"
+	"gitea.dev/modules/git/gitrepo"
 	"gitea.dev/modules/util"
 )
+
+type FastImportInit struct {
+	Bare         bool
+	ObjectFormat string
+}
 
 type FastImportFile struct {
 	Mode    EntryMode
@@ -22,6 +29,20 @@ type FastImportCommit struct {
 	Ref     string
 	Message string
 	Files   []FastImportFile
+}
+
+// ForceFastImportWithInit is for mainly for testing purpose
+func ForceFastImportWithInit(ctx context.Context, repoLocalPath string, commits []FastImportCommit, initOpts ...FastImportInit) (RepositoryFacade, error) {
+	repo := gitrepo.RepositoryUnmanaged(repoLocalPath)
+	initOpt := util.OptionalArg(initOpts, FastImportInit{Bare: true})
+	if exist, _ := IsRepositoryExist(ctx, repo); !exist {
+		_ = os.MkdirAll(repoLocalPath, 0o755)
+		err := InitRepositoryLocal(ctx, repoLocalPath, initOpt.Bare, util.IfZero(initOpt.ObjectFormat, "sha1"))
+		if err != nil {
+			return nil, err
+		}
+	}
+	return repo, ForceFastImport(ctx, repo, commits)
 }
 
 // ForceFastImport is for mainly for testing purpose
