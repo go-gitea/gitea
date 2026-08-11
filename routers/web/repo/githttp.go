b3f547ac16e4f133ef874c1bd610c92ce43d91a3
wxiaoguang <wxiaoguang@gmail.com>
2026-08-11 15:37:44 +0800

    refactor: api token scope check (#38862)
    "ApiTokenScope" already means "IsApiToken=true".
    
    All "IsApiToken" should be removed.
    
    ---------
    
    Signed-off-by: wxiaoguang <wxiaoguang@gmail.com>

--- routers/web/repo/githttp.go
+++ routers/web/repo/githttp.go
@@ -163,7 +163,7 @@ func httpBase(ctx *context.Context, optGitService ...string) *serviceHandler {
 			return nil
 		}
 
-		if ctx.IsBasicAuth && ctx.Data["IsApiToken"] != true && !ctx.Doer.IsGiteaActions() {
+		if ctx.IsBasicAuth && ctx.Data["ApiTokenScope"] == nil && !ctx.Doer.IsGiteaActions() {
 			_, err = auth_model.GetTwoFactorByUID(ctx, ctx.Doer.ID)
 			if err == nil {
 				// TODO: This response should be changed to "invalid credentials" for security reasons once the expectation behind it (creating an app token to authenticate) is properly documented
