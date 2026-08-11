d990857f0c31aead4dc9dd9f8cf19a547ce834c8
bircni <bircni@icloud.com>
2026-08-11 13:17:46 +0200

    cleanup

---
+++ services/forms/admin_test.go
@@ -0,0 +1,31 @@
+// Copyright 2026 The Gitea Authors. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+package forms
+
+import (
+	"testing"
+
+	"gitea.com/go-chi/binding"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAdminCreateUserFormUserType(t *testing.T) {
+	for _, userType := range []string{"", "individual", "bot"} {
+		form := &AdminCreateUserForm{
+			LoginType: "local",
+			UserName:  "user",
+			UserType:  userType,
+			Email:     "user@example.com",
+		}
+		assert.Empty(t, binding.RawValidate(form))
+	}
+
+	form := &AdminCreateUserForm{
+		LoginType: "local",
+		UserName:  "user",
+		UserType:  "invalid",
+		Email:     "user@example.com",
+	}
+	assert.NotEmpty(t, binding.RawValidate(form))
+}
