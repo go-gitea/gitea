363a85ef90a710b4187d2fed51a942997e5c9ef5
wxiaoguang <wxiaoguang@gmail.com>
2026-08-22 19:37:43 +0800

    fix Iterate

--- models/db/iterate.go
+++ models/db/iterate.go
@@ -5,6 +5,7 @@ package db
 
 import (
 	"context"
+	"fmt"
 
 	"gitea.dev/modules/setting"
 
@@ -13,30 +14,47 @@ import (
 
 // Iterate iterates all the Bean object
 func Iterate[Bean any](ctx context.Context, cond builder.Cond, f func(ctx context.Context, bean *Bean) error) error {
-	var start int
 	batchSize := setting.Database.IterateBufferSize
-	sess := GetEngine(ctx)
+	table, err := xormEngine.TableInfo(new(Bean))
+	if err != nil {
+		return err
+	}
+	if len(table.PrimaryKeys) != 1 {
+		return fmt.Errorf("table %s has %d primary keys, only the table with exactly one primary key can be iterated", table.Name, len(table.PrimaryKeys))
+	}
+
+	primaryKeyColumn := table.GetColumn(table.PrimaryKeys[0])
+	var lastPrimaryValue any
 	for {
 		select {
 		case <-ctx.Done():
 			return ctx.Err()
 		default:
-			beans := make([]*Bean, 0, batchSize)
-			if cond != nil {
-				sess = sess.Where(cond)
-			}
-			if err := sess.Limit(batchSize, start).Find(&beans); err != nil {
-				return err
-			}
-			if len(beans) == 0 {
-				return nil
-			}
-			start += len(beans)
+		}
+		beans := make([]*Bean, 0, batchSize)
+		query := GetEngine(ctx).Table(table.Name).OrderBy(table.PrimaryKeys[0])
+		batchCond := cond
+		if lastPrimaryValue != nil {
+			batchCond = builder.And(cond, builder.Gt{primaryKeyColumn.Name: lastPrimaryValue})
+		}
+		if batchCond != nil {
+			query = query.Where(batchCond)
+		}
+		if err := query.Limit(batchSize).Find(&beans); err != nil {
+			return err
+		}
+		if len(beans) == 0 {
+			return nil
+		}
 
-			for _, bean := range beans {
-				if err := f(ctx, bean); err != nil {
-					return err
-				}
+		reflectVal, err := primaryKeyColumn.ValueOf(beans[len(beans)-1])
+		if err != nil {
+			return err
+		}
+		lastPrimaryValue = reflectVal.Interface()
+		for _, bean := range beans {
+			if err := f(ctx, bean); err != nil {
+				return err
 			}
 		}
 	}
