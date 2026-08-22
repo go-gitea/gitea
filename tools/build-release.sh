03219378334ef3576bdd1201cba504e572ccef26
wxiaoguang <wxiaoguang@gmail.com>
2026-08-22 16:38:23 +0800

    add step info log, make it easier to figure out which step fails

--- tools/build-release.sh
+++ tools/build-release.sh
@@ -16,41 +16,45 @@ RELEASE_PLATFORMS_DEFAULT=(
 RELEASE_PLATFORMS_GOGIT=(windows/amd64 windows/arm64)
 
 build() {
-	local target="${1:?}"
-	local variant="${2:-}"
+  echo "building ${*} ..."
+  local target="${1:?}"
+  local variant="${2:-}"
 
-	local goos="${target%%/*}"
-	local goarch="${target##*/}"
-	local goarm=""
-	if [[ "$goarch" == arm-* ]]; then
-		goarm="${goarch#arm-}"
-		goarch="arm"
-	fi
+  local goos="${target%%/*}"
+  local goarch="${target##*/}"
+  local goarm=""
+  if [[ "$goarch" == arm-* ]]; then
+    goarm="${goarch#arm-}"
+    goarch="arm"
+  fi
 
-	local tags="${RELEASE_TAGS}"
-	local suffix
-	if [[ "$variant" == "gogit" ]]; then
-		tags="gogit${tags:+ ${tags}}"
-		suffix="-gogit-${target//\//-}"
-	else
-		suffix="-${target//\//-}"
-	fi
+  local tags="${RELEASE_TAGS}"
+  local suffix
+  if [[ "$variant" == "gogit" ]]; then
+    tags="gogit${tags:+ ${tags}}"
+    suffix="-gogit-${target//\//-}"
+  else
+    suffix="-${target//\//-}"
+  fi
 
-	local output="${RELEASE_PATH_PREFIX}${suffix}"
-	local args=()
-	if [[ "$goos" == "windows" ]]; then
-		output="${output}.exe"
-	fi
+  local output="${RELEASE_PATH_PREFIX}${suffix}"
+  local args=()
+  if [[ "$goos" == "windows" ]]; then
+    output="${output}.exe"
+  fi
   args+=(-tags "$tags")
   args+=(-ldflags "$RELEASE_LDFLAGS")
+  echo "  args: ${args[*]}"
+  echo "  output: ${output}"
   # must disable CGO (host compiler & linker) to get host-independent static builds
-	CGO_ENABLED=0 GOOS="$goos" GOARCH="$goarch" GOARM="$goarm" "$GO" build "${args[@]}" -o "$output" .
+  CGO_ENABLED=0 GOOS="$goos" GOARCH="$goarch" GOARM="$goarm" "$GO" build "${args[@]}" -o "$output" .
 }
 
 main() {
   # When building release binaries, some TAGS (bindata) are needed by the release script,
   # so here it needs to use the TAGS to generate the assets (embed bindata).
-  "$GO" generate -tags "RELEASE_TAGS" ./...
+  echo "generating assets with tags=${RELEASE_TAGS}"
+  "$GO" generate -tags "${RELEASE_TAGS}" ./...
   local platform="${1:-}"
   for target in "${RELEASE_PLATFORMS_DEFAULT[@]}"; do
     if [[ -z "$platform" || "$target" == "$platform/"* ]]; then
