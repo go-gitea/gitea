9ef96942b7035080a6bdfb575d624d4a4b098c2e
wxiaoguang <wxiaoguang@gmail.com>
2026-08-22 15:07:15 +0800

    fine tune and remove 386 builds

--- tools/build-release.sh
+++ tools/build-release.sh
@@ -7,28 +7,29 @@ RELEASE_LDFLAGS="-s -w $LDFLAGS"
 RELEASE_TAGS="bindata $TAGS"
 RELEASE_PATH_PREFIX="${DIST}/binaries/gitea-${VERSION##*/}"
 
-RELEASE_PLATFORMS_DEFAULT=(linux/amd64 linux/386 linux/arm-5 linux/arm-6 linux/arm64 linux/riscv64 \
-  windows/386 windows/amd64 windows/arm64 \
+RELEASE_PLATFORMS_DEFAULT=(
+  linux/amd64 linux/arm-5 linux/arm-6 linux/arm64 linux/riscv64 \
+  windows/amd64 windows/arm64 \
   darwin/amd64 darwin/arm64 \
-  freebsd/amd64)
+  freebsd/amd64
+)
 
-RELEASE_PLATFORMS_GOGIT=(windows/386 windows/amd64 windows/arm64)
+RELEASE_PLATFORMS_GOGIT=(windows/amd64 windows/arm64)
 
 build() {
 	local target="${1:?}"
 	local variant="${2:-}"
-	local goos goarch goarm tags suffix output args
 
-	goos="${target%%/*}"
-	goarch="${target##*/}"
-	goarm=""
-
+	local goos="${target%%/*}"
+	local goarch="${target##*/}"
+	local goarm=""
 	if [[ "$goarch" == arm-* ]]; then
 		goarm="${goarch#arm-}"
 		goarch="arm"
 	fi
 
-	tags="${RELEASE_TAGS:-}"
+	local tags="${RELEASE_TAGS:-}"
+	local suffix
 	if [[ "$variant" == "gogit" ]]; then
 		tags="gogit${tags:+ ${tags}}"
 		suffix="-gogit-${target//\//-}"
@@ -36,18 +37,19 @@ build() {
 		suffix="-${target//\//-}"
 	fi
 
-	output="${RELEASE_PATH_PREFIX}${suffix}"
-	args=()
+	local output="${RELEASE_PATH_PREFIX}${suffix}"
+	local args=()
 	if [[ "$goos" == "windows" ]]; then
 		output="${output}.exe"
 	fi
   args+=(-tags "$tags")
   args+=(-ldflags "$RELEASE_LDFLAGS")
+  # must disable CGO (host complier & linker) to get host-independent static builds
 	CGO_ENABLED=0 GOOS="$goos" GOARCH="$goarch" GOARM="$goarm" "${GO:-go}" build "${args[@]}" -o "$output" .
 }
 
 main() {
-  platform="${1:-}"
+  local platform="${1:-}"
   for target in "${RELEASE_PLATFORMS_DEFAULT[@]}"; do
     if [[ -z "$platform" || "$target" == "$platform/"* ]]; then
       build "$target"
--- tools/lint-shell.sh
+++ tools/lint-shell.sh
@@ -3,10 +3,10 @@ set -euo pipefail
 
 CONTAINER_RUNTIME="${CONTAINER_RUNTIME:-docker}"
 VERSION=$(echo "$SHELLCHECK_IMAGE" | sed -E 's/.*:v([0-9.]+)@.*/\1/')
-EXCLUDE_RULES="SC2153" # False-alert "Possible misspelling: TAGS may not be assigned. Did you mean tags?". We already use strict mode.
+EXCLUDED_RULES="SC2153" # False-alert "Possible misspelling: TAGS may not be assigned. Did you mean tags?". We already use strict mode.
 
 if hash shellcheck 2>/dev/null && shellcheck --version | grep -qx "version: $VERSION"; then
-  exec shellcheck --color=always -e "$EXCLUDE_RULES" "$@"
+  exec shellcheck --color=always -e "$EXCLUDED_RULES" "$@"
 else
-  exec "$CONTAINER_RUNTIME" run --rm -v "$PWD":/mnt -w /mnt "$SHELLCHECK_IMAGE" --color=always -e "$EXCLUDE_RULES" "$@"
+  exec "$CONTAINER_RUNTIME" run --rm -v "$PWD":/mnt -w /mnt "$SHELLCHECK_IMAGE" --color=always -e "$EXCLUDED_RULES" "$@"
 fi
