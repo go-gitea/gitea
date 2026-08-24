996cff4e32337ce299ba0995e40c4c2a8584c51e
wxiaoguang <wxiaoguang@gmail.com>
2026-08-25 01:18:03 +0800

    Update services/pull/update.go
    Signed-off-by: wxiaoguang <wxiaoguang@gmail.com>

--- services/pull/update.go
+++ services/pull/update.go
@@ -64,6 +64,9 @@ func Update(pr *issues_model.PullRequest, doer *user_model.User, message string,
 		return fmt.Errorf("unable to load HeadRepo for PR[%d] during update-by-merge: %w", pr.ID, err)
 	}
 
+	// TODO: The code is from https://github.com/go-gitea/gitea/pull/9784,
+	// it seems a simple copy-paste from https://github.com/go-gitea/gitea/pull/7082 without a real reason.
+	// TODO: DUPLICATE-PR-TASK: search and see another TODO comment for more details
 	defer addTestPullRequestTaskAfterWebOperation(pr, doer)
 
 	if rebase {
