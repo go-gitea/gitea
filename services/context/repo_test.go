03a184bc6e2de2d5812e3d98493a7ce8c53abbb7
water <672684719@qq.com>
2026-08-25 08:31:10 +0800

    test: update CompareHeadRef test to expect FullName() always (#39074)

--- services/context/repo_test.go
+++ services/context/repo_test.go
@@ -7,19 +7,10 @@ import (
 	"testing"
 
 	repo_model "gitea.dev/models/repo"
-	"gitea.dev/modules/setting"
-	"gitea.dev/modules/test"
-
 	"github.com/stretchr/testify/assert"
 )
 
 func TestCompareHeadRef(t *testing.T) {
-	defer test.MockVariableValue(&setting.Repository.AllowForkIntoSameOwner)()
-	headRepo := &repo_model.Repository{OwnerName: "user", Name: "fork"}
-
-	setting.Repository.AllowForkIntoSameOwner = false
-	assert.Equal(t, "user:my-branch", CompareHeadRef(headRepo, "my-branch"))
-
-	setting.Repository.AllowForkIntoSameOwner = true
+	headRepo := \&repo_model.Repository{OwnerName: "user", Name: "fork"}
 	assert.Equal(t, "user/fork:my-branch", CompareHeadRef(headRepo, "my-branch"))
 }
