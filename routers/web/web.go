c85b33625baaf0eafc6ba6ea0dab7d8f4dc5b84a
wxiaoguang <wxiaoguang@gmail.com>
2026-08-29 15:13:16 +0800

    revert

--- routers/web/web.go
+++ routers/web/web.go
@@ -895,8 +895,8 @@ func registerWebRoutes(m *web.Router, webAuth *AuthMiddleware) {
 		m.Group("/actions", func() {
 			m.Get("", misc.LocationRedirect("./actions/runners"))
 			addSettingsRunnersRoutes()
-			addSettingsVariablesRoutes()
 			m.Post("/runners/bulk", shared_actions.RunnerBulkActionPost)
+			addSettingsVariablesRoutes()
 			addSettingsScopedWorkflowsRoutes()
 		})
 	}, adminReq, ctxDataSet(reqctx.ContextData{"EnableOAuth2": setting.OAuth2.Enabled, "EnablePackages": setting.Packages.Enabled}))
