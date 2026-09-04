484e1a2f16b60a72e089b6d8d00e399af1c307b9
wxiaoguang <wxiaoguang@gmail.com>
2026-09-04 13:49:26 +0800

    fix

--- services/automerge/automerge.go
+++ services/automerge/automerge.go
@@ -222,6 +222,9 @@ func handlePullRequestAutoMerge(ctx context.Context, pr *issues_model.PullReques
 	}
 
 	if err := pull_service.Merge(pr, doer, scheduledPRM.MergeStyle, expectedHeadCommitID, scheduledPRM.Message, true); err != nil {
+		if pull_service.IsErrSHADoesNotMatch(err) {
+			return errors.Join(errSkipAutoMerge, err)
+		}
 		// FIXME: if merge failed, we should display some error message to the pull request page, or retry later.
 		// The resolution is add a new column on automerge table named `error_message` to store the error message and displayed
 		// on the pull request page. But this should not be finished in a bug fix PR which will be backport to release branch.
