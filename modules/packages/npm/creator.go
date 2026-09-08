2196c24c5587fdffedf6da567df52ddae2a5d5f2
wxiaoguang <wxiaoguang@gmail.com>
2026-09-08 22:24:24 +0800

    regexp lazy init

--- modules/packages/npm/creator.go
+++ modules/packages/npm/creator.go
@@ -14,6 +14,7 @@ import (
 	"io"
 	"regexp"
 	"strings"
+	"sync"
 	"time"
 
 	"gitea.dev/modules/json"
@@ -38,7 +39,10 @@ var (
 
 const RegexpNamePart = `[a-z0-9-][a-z0-9-._]*`
 
-var nameMatch = regexp.MustCompile(`^(@` + RegexpNamePart + `/)?` + RegexpNamePart + `$`)
+var nameMatch = sync.OnceValue(func() *regexp.Regexp {
+	// either "@scope/pkg" or "pkg", where "scope" and "pkg" are RegexpNamePart
+	return regexp.MustCompile(`^(@` + RegexpNamePart + `/)?` + RegexpNamePart + `$`)
+})
 
 // Package represents a npm package
 type Package struct {
@@ -465,7 +469,7 @@ func validateName(name string) bool {
 	if len(name) == 0 || len(name) > 214 {
 		return false
 	}
-	return nameMatch.MatchString(name)
+	return nameMatch().MatchString(name)
 }
 
 // PackageDeprecation is the result of parsing an npm deprecate request body.
