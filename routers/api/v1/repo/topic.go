29312b9a28cdef56dc794d5be8db654a7cda89a5
Rathina Devan E M <emrathinadev@gmail.com>
2026-09-08 14:58:57 +0530

    fix(api): enforce mustNotBeArchived on repo topics routes (#39260)
    Co-authored-by: silverwind <me@silverwind.io>

--- routers/api/v1/repo/topic.go
+++ routers/api/v1/repo/topic.go
@@ -100,6 +100,8 @@ func UpdateTopics(ctx *context.APIContext) {
 	//     "$ref": "#/responses/notFound"
 	//   "422":
 	//     "$ref": "#/responses/invalidTopicsError"
+	//   "423":
+	//     "$ref": "#/responses/repoArchivedError"
 
 	form := web.GetForm(ctx).(*api.RepoTopicOptions)
 	topicNames := form.Topics
@@ -161,6 +163,8 @@ func AddTopic(ctx *context.APIContext) {
 	//     "$ref": "#/responses/notFound"
 	//   "422":
 	//     "$ref": "#/responses/invalidTopicsError"
+	//   "423":
+	//     "$ref": "#/responses/repoArchivedError"
 
 	topicName := strings.TrimSpace(strings.ToLower(ctx.PathParam("topic")))
 
@@ -228,6 +232,8 @@ func DeleteTopic(ctx *context.APIContext) {
 	//     "$ref": "#/responses/notFound"
 	//   "422":
 	//     "$ref": "#/responses/invalidTopicsError"
+	//   "423":
+	//     "$ref": "#/responses/repoArchivedError"
 
 	topicName := strings.TrimSpace(strings.ToLower(ctx.PathParam("topic")))
 
