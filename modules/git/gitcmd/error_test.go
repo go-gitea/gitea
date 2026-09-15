c538c8cb186f7373a915870e4fdf81f10ca546c9
wxiaoguang <wxiaoguang@gmail.com>
2026-09-15 10:13:38 +0800

    clean up

--- modules/git/gitcmd/error_test.go
+++ modules/git/gitcmd/error_test.go
@@ -17,7 +17,7 @@ func TestIsStderr(t *testing.T) {
 		{StderrUnknownRevisionOrPath, "fatal: ambiguous argument 'origin': unknown revision or path not in the working tree...."},
 		{StderrNoMergeBase, "fatal: origin/main..HEAD: no merge base...."},
 		{StderrFileNoEnoughLines, "fatal: file foo/bar has only 1 line"},
-		{StderrAuthenticationFailed, "Cloning into 'repo'...\nremote: Invalid username or token.\nfatal: Authentication failed for 'https://host/repo.git/'\n"},
+		{StderrAuthenticationFailed, "Cloning into 'repo'...\r\nremote: Invalid username or token.\r\nfatal: Authentication failed for 'https://host/repo.git/'\r\n"},
 	}
 	for _, tc := range cases {
 		assert.True(t, IsStderr(&runStdError{stderr: tc.stderr}, tc.check), "stderr: %s", tc.stderr)
