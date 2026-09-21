0af2506f49891e4eddd15f4804085e66f6c96e68
techknowlogick <git@tklk.dev>
2026-09-21 18:36:11 -0400

    NewLimitedDecompressor for arch/metadata.go
    Signed-off-by: techknowlogick <git@tklk.dev>

--- modules/packages/arch/metadata.go
+++ modules/packages/arch/metadata.go
@@ -34,6 +34,9 @@ const (
 	RepositoryVersion = "_repository"
 
 	AnyArch = "any"
+
+	maxPKGINFOSize    = 1 << 20
+	maxZstdWindowSize = 128 << 20
 )
 
 var (
@@ -100,7 +103,7 @@ func ParsePackage(r io.Reader) (*Package, error) {
 	var inner io.Reader
 	var compressionType string
 	if bytes.HasPrefix(header, []byte{0x28, 0xB5, 0x2F, 0xFD}) { // zst
-		zr, err := zstd.NewReader(r)
+		zr, err := zstd.NewReader(r, zstd.WithDecoderMaxMemory(maxZstdWindowSize))
 		if err != nil {
 			return nil, err
 		}
@@ -148,7 +151,7 @@ func ParsePackage(r io.Reader) (*Package, error) {
 
 		filename := hd.FileInfo().Name()
 		if filename == ".PKGINFO" {
-			p, err = ParsePackageInfo(tr)
+			p, err = ParsePackageInfo(packages.NewLimitedDecompressor(tr, maxPKGINFOSize))
 			if err != nil {
 				return nil, err
 			}
