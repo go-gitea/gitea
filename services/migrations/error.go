0af28d95fedd9eafa0ac2e8493c293960c046ec9
Giteabot <teabot@gitea.io>
2026-09-21 09:40:54 +0000

    chore(deps): update dependencies

--- services/migrations/error.go
+++ services/migrations/error.go
@@ -10,7 +10,7 @@ import (
 
 	"gitea.dev/modules/git/gitcmd"
 
-	"github.com/google/go-github/v91/github"
+	"github.com/google/go-github/v92/github"
 )
 
 // ErrRepoNotCreated returns the error that repository not created
