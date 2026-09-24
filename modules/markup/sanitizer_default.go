c7eec4c737c8e161d682f1b08287cf67d8d0a9d3
silverwind <me@silverwind.io>
2026-09-24 19:43:44 +0200

    Apply suggestion from @silverwind
    Signed-off-by: silverwind <me@silverwind.io>

--- modules/markup/sanitizer_default.go
+++ modules/markup/sanitizer_default.go
@@ -34,6 +34,7 @@ func (st *Sanitizer) createDefaultPolicy() *bluemonday.Policy {
 	// Line numbers on codepreview
 	policy.AllowAttrs("data-line-number").OnElements("span")
 
+  // emoji aliases for dark theme inversion
 	policy.AllowAttrs("data-alias").OnElements("span")
 
 	// HINT: CUSTOM-URL-SCHEMES-ALLOW: setting custom means also allow them besides http/https, no custom means "allow all"
